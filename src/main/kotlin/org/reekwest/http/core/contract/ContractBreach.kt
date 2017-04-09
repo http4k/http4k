1d2a009b77e7a9827881e80093d15e985bd473d6
David Denton <denton.david@gmail.com>
2017-04-09 10:13:11 +0100

    refactor messagepart

--- src/main/kotlin/org/reekwest/http/core/contract/ContractBreach.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/ContractBreach.kt
@@ -1,7 +1,7 @@
 package org.reekwest.http.core.contract
 
-sealed class ContractBreach(param: MessagePart<*, *>) : Exception(param.toString())
+sealed class ContractBreach(param: MessagePart<*, *, *>) : Exception(param.toString())
 
-class Missing(param: MessagePart<*, *>) : ContractBreach(param)
-class Invalid(param: MessagePart<*, *>) : ContractBreach(param)
+class Missing(param: MessagePart<*, *, *>) : ContractBreach(param)
+class Invalid(param: MessagePart<*, *, *>) : ContractBreach(param)
 
--- src/main/kotlin/org/reekwest/http/core/contract/MessagePart.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/MessagePart.kt
@@ -3,31 +3,32 @@ package org.reekwest.http.core.contract
 import org.reekwest.http.core.Request
 import org.reekwest.http.core.Response
 
-open class MessagePart<in IN, out OUT>(val fn: (IN) -> OUT?) {
-    internal fun <X> mapValid(next: (OUT) -> X): (IN) -> X? {
-        return {
-            try {
-                fn(it)?.let(next)
-            } catch (e: Exception) {
-                throw Invalid(this)
-            }
+abstract class MessagePart<in IN, out RAW, out FINAL>(val fn: (IN) -> RAW?) {
+    abstract operator fun get(m: IN): FINAL
+}
+
+operator fun <T, FINAL> Request.get(param: MessagePart<Request, T, FINAL>): FINAL = param[this]
+operator fun <T, FINAL> Response.get(param: MessagePart<Response, T, FINAL>): FINAL = param[this]
+
+open class Optional<in IN, out OUT>(fn: (IN) -> OUT?) : MessagePart<IN, OUT, OUT?>(fn) {
+    override operator fun get(m: IN): OUT? = fn(m)
+    fun <X> map(next: (OUT) -> X) = Optional<IN, X>({
+        try {
+            fn(it)?.let(next)
+        } catch (e: Exception) {
+            throw Invalid(this)
         }
-    }
+    })
 }
 
-operator fun <T> Request.get(param: Optional<Request, T>): T? = param[this]
-operator fun <T> Response.get(param: Optional<Response, T>): T? = param[this]
-
-operator fun <T> Request.get(param: Required<Request, T>): T = param[this]
-operator fun <T> Response.get(param: Required<Response, T>): T = param[this]
-
-open class Optional<in IN, out OUT>(fn: (IN) -> OUT?) : MessagePart<IN, OUT>(fn) {
-    operator fun get(m: IN): OUT? = fn(m)
-    fun <X> map(next: (OUT) -> X) = Optional(mapValid(next))
-}
-
-open class Required<in IN, out OUT>(fn: (IN) -> OUT?) : MessagePart<IN, OUT>(fn) {
-    operator fun get(m: IN): OUT = fn(m) ?: throw Missing(this)
-    fun <X> map(next: (OUT) -> X) = Required(mapValid(next))
+open class Required<in IN, out OUT>(fn: (IN) -> OUT?) : MessagePart<IN, OUT, OUT>(fn) {
+    override operator fun get(m: IN): OUT = fn(m) ?: throw Missing(this)
+    fun <X> map(next: (OUT) -> X) = Required<IN, X>({
+        try {
+            fn(it)?.let(next)
+        } catch (e: Exception) {
+            throw Invalid(this)
+        }
+    })
 }
 
