e84e013aec5a07da5f9c74cda48ed47479570cfc
David Denton <denton.david@gmail.com>
2017-04-09 10:16:10 +0100

    refactor messagepart

--- src/main/kotlin/org/reekwest/http/core/contract/MessagePart.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/MessagePart.kt
@@ -3,8 +3,17 @@ package org.reekwest.http.core.contract
 import org.reekwest.http.core.Request
 import org.reekwest.http.core.Response
 
-abstract class MessagePart<in IN, out RAW, out FINAL>(val fn: (IN) -> RAW?) {
-    abstract operator fun get(m: IN): FINAL
+abstract class MessagePart<in IN, out RAW, out OUT>(val fn: (IN) -> RAW?) {
+    abstract operator fun get(m: IN): OUT
+
+    internal fun <NEXT> mapValid(next: (RAW) -> NEXT): (IN) -> NEXT? = {
+        try {
+            fn(it)?.let(next)
+        } catch (e: Exception) {
+            throw Invalid(this)
+        }
+    }
+
 }
 
 operator fun <T, FINAL> Request.get(param: MessagePart<Request, T, FINAL>): FINAL = param[this]
@@ -12,23 +21,11 @@ operator fun <T, FINAL> Response.get(param: MessagePart<Response, T, FINAL>): FI
 
 open class Optional<in IN, out OUT>(fn: (IN) -> OUT?) : MessagePart<IN, OUT, OUT?>(fn) {
     override operator fun get(m: IN): OUT? = fn(m)
-    fun <X> map(next: (OUT) -> X) = Optional<IN, X>({
-        try {
-            fn(it)?.let(next)
-        } catch (e: Exception) {
-            throw Invalid(this)
-        }
-    })
+    fun <X> map(next: (OUT) -> X) = Optional(mapValid(next))
 }
 
 open class Required<in IN, out OUT>(fn: (IN) -> OUT?) : MessagePart<IN, OUT, OUT>(fn) {
     override operator fun get(m: IN): OUT = fn(m) ?: throw Missing(this)
-    fun <X> map(next: (OUT) -> X) = Required<IN, X>({
-        try {
-            fn(it)?.let(next)
-        } catch (e: Exception) {
-            throw Invalid(this)
-        }
-    })
+    fun <X> map(next: (OUT) -> X) = Required(mapValid(next))
 }
 
