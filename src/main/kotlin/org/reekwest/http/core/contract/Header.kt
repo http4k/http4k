a45f3625ba0f6bfbc1e402d6c15702d740c43698
David Denton <denton.david@gmail.com>
2017-04-09 19:31:11 +0100

    headers are on messages!

--- src/main/kotlin/org/reekwest/http/core/contract/Header.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/Header.kt
@@ -1,10 +1,10 @@
 package org.reekwest.http.core.contract
 
 import org.reekwest.http.core.ContentType
-import org.reekwest.http.core.Request
+import org.reekwest.http.core.HttpMessage
 import org.reekwest.http.core.headerValues
 
-object Header : Spec<Request, String>("header", Request::headerValues) {
+object Header : Spec<HttpMessage, String>("header", HttpMessage::headerValues) {
     object Common {
         val CONTENT_TYPE = map(::ContentType).optional("Content-Type")
     }
