81f7addf942ea0f92a15b5c9765fa1df475f3c0e
David Denton <denton.david@gmail.com>
2017-04-14 11:39:06 +0100

    tweak

--- src/main/kotlin/org/reekwest/http/core/contract/extensions.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/extensions.kt
@@ -2,7 +2,7 @@ package org.reekwest.http.core.contract
 
 import java.nio.ByteBuffer
 
-fun String.toByteBuffer() = ByteBuffer.wrap(this.toByteArray())
+fun String.toByteBuffer(): ByteBuffer = ByteBuffer.wrap(this.toByteArray())
 
 fun <A, B, C> Function1<A, B>.then(next: Function1<B, C>): Function1<A, C> = { next.invoke(this.invoke(it)) }
 
