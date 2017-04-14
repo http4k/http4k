6c8f15ff47629908924a30340932ba4ec367add8
David Denton <denton.david@gmail.com>
2017-04-14 14:27:17 +0100

    fixed tests

--- src/main/kotlin/org/reekwest/http/core/contract/Body.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/Body.kt
@@ -3,7 +3,6 @@ package org.reekwest.http.core.contract
 import org.reekwest.http.core.*
 import org.reekwest.http.core.ContentType.Companion.APPLICATION_FORM_URLENCODED
 import org.reekwest.http.core.body.string
-import org.reekwest.http.core.contract.Body.locator
 import org.reekwest.http.core.contract.Header.Common.CONTENT_TYPE
 import java.nio.ByteBuffer
 
@@ -16,14 +15,14 @@ open class BodySpec<OUT : Any>(private val delegate: LensSpec<HttpMessage, OUT>)
     fun required(description: String? = null) = delegate.required("body", description)
 }
 
-object Body : BodySpec<ByteBuffer>(LensSpec(locator, { it }, { it })) {
-    val string = Body.map({ it: ByteBuffer -> String(it.array()) }, { it.toByteBuffer() })
+internal object BodyLocator : Locator<HttpMessage, ByteBuffer> {
+    override val name = "body"
+    override fun get(target: HttpMessage, name: String) = listOf(target.body)
+    override fun set(target: HttpMessage, name: String, values: List<ByteBuffer>) = values.fold(target) { a, b -> a.copy(body = b) }
+}
 
-    internal val locator = object : Locator<HttpMessage, ByteBuffer> {
-        override val name = "body"
-        override fun get(target: HttpMessage, name: String) = listOf(target.body)
-        override fun set(target: HttpMessage, name: String, values: List<ByteBuffer>) = values.fold(target) { a, b -> a.copy(body = b) }
-    }
+object Body : BodySpec<ByteBuffer>(LensSpec(BodyLocator, { it }, { it })) {
+    val string = Body.map({ it: ByteBuffer -> String(it.array()) }, { it.toByteBuffer() })
 }
 
 /**
