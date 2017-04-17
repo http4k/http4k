8fc3f4ef558dfa0915f31480e585d54ec34d0d7a
David Denton <denton.david@gmail.com>
2017-04-17 10:17:02 +0100

    adding map() to Locator

--- src/main/kotlin/org/reekwest/http/core/contract/Locator.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/Locator.kt
@@ -1,18 +1,27 @@
 package org.reekwest.http.core.contract
 
-import org.reekwest.http.asString
 import org.reekwest.http.asByteBuffer
+import org.reekwest.http.asString
 import java.nio.ByteBuffer
 
 interface Locator<IN, OUT> {
     val name: String
     fun get(target: IN, name: String): List<OUT?>?
     fun set(target: IN, name: String, values: List<OUT>): IN
+
+    fun <NEXT> map(inFn: (OUT) -> NEXT, outFn: (NEXT) -> OUT): Locator<IN, NEXT> {
+        val sup = this
+        return object : Locator<IN, NEXT> {
+            override val name = sup.name
+            override fun get(target: IN, name: String) = sup.get(target, name)?.map { it?.let(inFn) }
+            override fun set(target: IN, name: String, values: List<NEXT>) = sup.set(target, name, values.map(outFn))
+        }
+    }
 }
 
 internal class StringLocator<IN>(override val name: String,
-                        val getFn: (IN, String) -> List<String?>?,
-                        val setFn: (IN, String, List<String>) -> IN) : Locator<IN, ByteBuffer> {
+                                 val getFn: (IN, String) -> List<String?>?,
+                                 val setFn: (IN, String, List<String>) -> IN) : Locator<IN, ByteBuffer> {
     override fun get(target: IN, name: String) = getFn(target, name)?.mapNotNull { it -> it?.asByteBuffer() }
     override fun set(target: IN, name: String, values: List<ByteBuffer>) = setFn(target, name, values.map(ByteBuffer::asString))
 }
