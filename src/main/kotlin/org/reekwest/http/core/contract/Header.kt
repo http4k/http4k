ad1c195ebc1b4cdb63d35b87a1fc1243fce1baa3
David Denton <denton.david@gmail.com>
2017-04-17 10:08:37 +0100

    remove StringLensSpec

--- src/main/kotlin/org/reekwest/http/core/contract/Header.kt
+++ src/main/kotlin/org/reekwest/http/core/contract/Header.kt
@@ -18,13 +18,11 @@ object Header : LensSpec<HttpMessage, String>(
         val CONTENT_TYPE = map(::ContentType).optional("Content-Type")
     }
 }
-//
-//object Cookies {
-//    private val allCookies = StringLensSpec<HttpMessage>("header",
-//        { request, _ -> request.headerValues("Cookie") },
-//        { req, _, values -> values.fold(req, { m, next -> m.header("Cookie", next) }) })
-//        .map { it.toCookieList().map { it.name to it }.toMap() }
-//
-//    fun named(cookieName: String): LensSpec<HttpMessage, Cookie?> =
-//        allCookies.map({ it[cookieName] })
-//
+
+object Cookies {
+    private val allCookies = LensSpec<HttpMessage, String>(
+        StringLocator("header",
+            { request, name -> request.headerValues(name) },
+            { req, name, values -> values.fold(req, { m, next -> m.header(name, next) }) }),
+        ByteBuffer::asString, String::asByteBuffer)
+}
