836121880bc8c568ff43f1b72aa875e9325bcb15
David Denton <denton.david@gmail.com>
2017-04-21 22:24:49 +0100

    refactor of lens

--- src/main/kotlin/org/reekwest/http/contract/Lens.kt
+++ src/main/kotlin/org/reekwest/http/contract/Lens.kt
@@ -9,10 +9,10 @@ data class BiDiLens<IN, OUT>(val getLens: Get<IN, OUT>, val setLens: Set<IN, OUT
 class GetLens<in IN, MID, out OUT>(private val rootFn: (String, IN) -> List<MID>, private val fn: (MID) -> OUT) {
     operator fun invoke(name: String) = { target: IN -> rootFn(name, target).map(fn) }
 
-    fun <NEXT> map(nextFn: (OUT) -> NEXT): GetLens<IN, MID, NEXT> = GetLens(rootFn, { nextFn(fn(it)) })
+    fun <NEXT> map(nextFn: (OUT) -> NEXT) = GetLens(rootFn, { nextFn(fn(it)) })
 }
 
 class SetLens<IN, MID, in OUT>(private val rootFn: (String, List<MID>, IN) -> IN, private val fn: (OUT) -> MID) {
     operator fun invoke(name: String) = { values: List<OUT>, target: IN -> rootFn(name, values.map(fn), target) }
-    fun <NEXT> map(nextFn: (NEXT) -> OUT): SetLens<IN, MID, NEXT> = SetLens(rootFn, { fn(nextFn(it)) })
+    fun <NEXT> map(nextFn: (NEXT) -> OUT) = SetLens(rootFn, { value: NEXT -> fn(nextFn(value)) })
 }
