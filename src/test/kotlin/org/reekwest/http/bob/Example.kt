f945181bc719137903f57c88cf7441d7ec23ef4e
David Denton <denton.david@gmail.com>
2017-04-21 21:18:01 +0100

    adding Form

--- src/test/kotlin/org/reekwest/http/bob/Example.kt
+++ src/test/kotlin/org/reekwest/http/bob/Example.kt
@@ -1,6 +1,5 @@
 package org.reekwest.http.bob
 
-import org.reekwest.http.contract.BiDiMetaLens
 import org.reekwest.http.core.ContentType
 import org.reekwest.http.core.Method.GET
 import org.reekwest.http.core.Request
@@ -10,19 +9,20 @@ import org.reekwest.http.newcontract.*
 
 
 data class CustomType(val s: String)
+
 fun main(args: Array<String>) {
-    val optional: BiDiMetaLens<Request, String, String?> = Query.optional("name")
+    val optional = Query.optional("name")
     val o2 = Header.map { it + it }.optional("name")
     val requiredHeader = Header.long().multi.required("name")
     val b = Body.string.map(::CustomType).required("description")
     val request = Request(GET, uri("/bob"), listOf(
         "Content-Type" to ContentType.APPLICATION_FORM_URLENCODED.value,
         "name" to "123",
-        "name" to "1232"), "hello=body".toBody())
+        "name" to "1232"), "hello=body&hello=world".toBody())
 
     val form = Body.form()
 
-    val formField = FormField.required("hello")
+    val formField = FormField.multi.required("hello")
     println(formField(form(request)))
     println(o2(request))
     println(optional(request))
