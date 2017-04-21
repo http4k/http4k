668e17a7dcf028bbde33400b041d7af5e9281bfb
David Denton <daviddenton@users.noreply.github.com>
2017-04-21 09:28:58 +0100

    Work on refactoring lens (#3)
    * tidy
    
    * simplify

--- src/main/kotlin/org/reekwest/http/contract/LensSpec.kt
+++ src/main/kotlin/org/reekwest/http/contract/LensSpec.kt
@@ -16,9 +16,10 @@ open class LensSpec<IN, OUT>(
     private val createLens: (String) -> Lens<IN, ByteBuffer>,
     private val mapper: BiDiMapper<ByteBuffer, OUT>
 ) {
-    private val mappingLens = { name: String ->
+    private fun mappingLens(name: String): Lens<IN, OUT> {
         val lens = createLens(name)
-        object : Lens<IN, OUT> {
+
+        return object : Lens<IN, OUT> {
             override fun invoke(target: IN): List<OUT> = lens(target).let { it.map { it.let { mapper.mapIn(it) } } }
             override fun invoke(values: List<OUT>, target: IN): IN = lens(values.map { mapper.mapOut(it) }, target)
         }
@@ -32,7 +33,7 @@ open class LensSpec<IN, OUT>(
      * Create a lens which resolves to a single optional (nullable) value
      */
     fun optional(name: String, description: String? = null) =
-        object : ContractualLens<IN, OUT, OUT?>(Meta(name, location, false, description), mappingLens) {
+        object : ContractualLens<IN, OUT, OUT?>(Meta(name, location, false, description), mappingLens(name)) {
             override fun convertOut(o: OUT?) = o?.let { listOf(it) } ?: emptyList()
             override fun convertIn(o: List<OUT>) = o.firstOrNull()
         }
@@ -40,7 +41,7 @@ open class LensSpec<IN, OUT>(
     /**
      * Create a lens which resolves to a single required (non-nullable) value.
      */
-    fun required(name: String, description: String? = null) = object : ContractualLens<IN, OUT, OUT>(Meta(name, location, true, description), mappingLens) {
+    fun required(name: String, description: String? = null) = object : ContractualLens<IN, OUT, OUT>(Meta(name, location, true, description), mappingLens(name)) {
         override fun convertIn(o: List<OUT>) = o.firstOrNull() ?: throw Missing(this)
         override fun convertOut(o: OUT) = listOf(o)
     }
@@ -50,7 +51,7 @@ open class LensSpec<IN, OUT>(
         /**
          * Create a lens which resolves to a optional (nullable) list value
          */
-        override fun optional(name: String, description: String?): ContractualLens<IN, OUT, List<OUT>?> = object : ContractualLens<IN, OUT, List<OUT>?>(Meta(name, location, false, description), mappingLens) {
+        override fun optional(name: String, description: String?): ContractualLens<IN, OUT, List<OUT>?> = object : ContractualLens<IN, OUT, List<OUT>?>(Meta(name, location, false, description), mappingLens(name)) {
             override fun convertIn(o: List<OUT>): List<OUT>? = if (o.isEmpty()) null else o
             override fun convertOut(o: List<OUT>?): List<OUT> = o ?: emptyList()
         }
@@ -58,7 +59,7 @@ open class LensSpec<IN, OUT>(
         /**
          * Create a lens which resolves to required (non-nullable) list value.
          */
-        override fun required(name: String, description: String?) = object : ContractualLens<IN, OUT, List<OUT>>(Meta(name, location, true, description), mappingLens) {
+        override fun required(name: String, description: String?) = object : ContractualLens<IN, OUT, List<OUT>>(Meta(name, location, true, description), mappingLens(name)) {
             override fun convertOut(o: List<OUT>): List<OUT> = o
             override fun convertIn(o: List<OUT>): List<OUT> = if (o.isEmpty()) throw Missing(this) else o
         }
