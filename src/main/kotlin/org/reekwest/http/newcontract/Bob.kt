385b4056e444fa3e695a0eed42d410b653f0c8f0
David Denton <denton.david@gmail.com>
2017-04-21 20:40:36 +0100

    new version of Get/Set

--- src/main/kotlin/org/reekwest/http/newcontract/Bob.kt
+++ src/main/kotlin/org/reekwest/http/newcontract/Bob.kt
@@ -20,12 +20,12 @@ class MappableSetLens<IN, MID, in OUT>(private val rootFn: (String, List<MID>, I
     fun <NEXT> map(nextFn: (NEXT) -> OUT): MappableSetLens<IN, MID, NEXT> = MappableSetLens(rootFn, { fn(nextFn(it)) })
 }
 
-interface MultiLensSpec<in IN, OUT> {
+interface MultiGetLensSpec<in IN, OUT> {
     fun optional(name: String, description: String? = null): MetaLens<IN, OUT, List<OUT>?>
     fun required(name: String, description: String? = null): MetaLens<IN, OUT, List<OUT>>
 }
 
-open class GetLensSpec<IN, MID, OUT>(internal val location: String, internal val createGetLens: MappableGetLens<IN, MID, OUT> ) {
+open class GetLensSpec<IN, MID, OUT>(internal val location: String, internal val createGetLens: MappableGetLens<IN, MID, OUT>) {
     fun <NEXT> map(nextIn: (OUT) -> NEXT): GetLensSpec<IN, MID, NEXT> = GetLensSpec(location, createGetLens.map(nextIn))
 
     open fun optional(name: String, description: String? = null): MetaLens<IN, OUT, OUT?> =
@@ -38,7 +38,7 @@ open class GetLensSpec<IN, MID, OUT>(internal val location: String, internal val
             override fun convertIn(o: List<OUT>): OUT = o.firstOrNull() ?: throw Missing(this)
         }
 
-    val multi = object: MultiLensSpec<IN, OUT> {
+    open val multi = object : MultiGetLensSpec<IN, OUT> {
         override fun optional(name: String, description: String?): MetaLens<IN, OUT, List<OUT>?> =
             object : MetaLens<IN, OUT, List<OUT>?>(Meta(name, location, false, description), createGetLens(name)) {
                 override fun convertIn(o: List<OUT>): List<OUT>? = if (o.isEmpty()) null else o
@@ -51,50 +51,66 @@ open class GetLensSpec<IN, MID, OUT>(internal val location: String, internal val
     }
 }
 
-open class SetLensSpec<IN, MID, OUT>(location: String, createGetLens: MappableGetLens<IN, MID, OUT>,
-                                     private val createSetLens: MappableSetLens<IN, MID, OUT> ) : GetLensSpec<IN, MID, OUT>(location, createGetLens) {
 
-    fun <NEXT> map(nextIn: (OUT) -> NEXT, nextOut: (NEXT) -> OUT): SetLensSpec<IN, MID, NEXT> =
-        SetLensSpec(location,
+interface BiDiMultiLensSpec<IN, OUT>: MultiGetLensSpec<IN, OUT> {
+    override fun optional(name: String, description: String?): BiDiMetaLens<IN, OUT, List<OUT>?>
+    override fun required(name: String, description: String?): BiDiMetaLens<IN, OUT, List<OUT>>
+}
+
+open class BiDiLensSpec<IN, MID, OUT>(location: String, createGetLens: MappableGetLens<IN, MID, OUT>,
+                                      private val createSetLens: MappableSetLens<IN, MID, OUT>) : GetLensSpec<IN, MID, OUT>(location, createGetLens) {
+
+    private fun biDiLensFor(name: String): BiDiLens<IN, OUT> {
+        val getLens = createGetLens(name)
+        val setLens = createSetLens(name)
+
+        return object : BiDiLens<IN, OUT> {
+            override fun invoke(target: IN): List<OUT> = getLens(target)
+            override fun invoke(values: List<OUT>, target: IN): IN = setLens(values, target)
+        }
+    }
+
+    fun <NEXT> map(nextIn: (OUT) -> NEXT, nextOut: (NEXT) -> OUT): BiDiLensSpec<IN, MID, NEXT> =
+        BiDiLensSpec(location,
             createGetLens.map(nextIn),
             createSetLens.map(nextOut))
 
-    override fun optional(name: String, description: String?): BiDiMetaLens<IN, OUT, OUT?> {
-        val getLens = createGetLens(name)
-        val setLens = createSetLens(name)
-
-        return object : BiDiMetaLens<IN, OUT, OUT?>(Meta(name, location, false, description), object : BiDiLens<IN, OUT> {
-            override fun invoke(target: IN): List<OUT> = getLens(target)
-            override fun invoke(values: List<OUT>, target: IN): IN = setLens(values, target)
-        }) {
+    override fun optional(name: String, description: String?): BiDiMetaLens<IN, OUT, OUT?> =
+        object : BiDiMetaLens<IN, OUT, OUT?>(Meta(name, location, false, description), biDiLensFor(name)) {
             override fun convertIn(o: List<OUT>): OUT? = o.firstOrNull()
             override fun convertOut(o: OUT?): List<OUT> = o?.let { listOf(it) } ?: emptyList()
         }
-    }
 
-    override fun required(name: String, description: String?): BiDiMetaLens<IN, OUT, OUT> {
-        val getLens = createGetLens(name)
-        val setLens = createSetLens(name)
-
-        return object : BiDiMetaLens<IN, OUT, OUT>(Meta(name, location, true, description), object : BiDiLens<IN, OUT> {
-            override fun invoke(target: IN): List<OUT> = getLens(target)
-            override fun invoke(values: List<OUT>, target: IN): IN = setLens(values, target)
-        }) {
+    override fun required(name: String, description: String?): BiDiMetaLens<IN, OUT, OUT> =
+        object : BiDiMetaLens<IN, OUT, OUT>(Meta(name, location, true, description), biDiLensFor(name)) {
             override fun convertIn(o: List<OUT>): OUT = o.firstOrNull() ?: throw Missing(this)
             override fun convertOut(o: OUT): List<OUT> = listOf(o)
         }
+
+    override val multi = object : BiDiMultiLensSpec<IN, OUT> {
+        override fun optional(name: String, description: String?): BiDiMetaLens<IN, OUT, List<OUT>?> =
+            object : BiDiMetaLens<IN, OUT, List<OUT>?>(Meta(name, location, false, description), biDiLensFor(name)) {
+                override fun convertOut(o: List<OUT>?): List<OUT> = o ?: emptyList()
+                override fun convertIn(o: List<OUT>): List<OUT>? = if (o.isEmpty()) null else o
+            }
+
+        override fun required(name: String, description: String?): BiDiMetaLens<IN, OUT, List<OUT>> =
+            object : BiDiMetaLens<IN, OUT, List<OUT>>(Meta(name, location, false, description), biDiLensFor(name)) {
+                override fun convertOut(o: List<OUT>): List<OUT> = o
+                override fun convertIn(o: List<OUT>): List<OUT> = if (o.isEmpty()) throw Missing(this) else o
+            }
     }
 }
 
-object Query : SetLensSpec<Request, String, String>("query",
+object Query : BiDiLensSpec<Request, String, String>("query",
     MappableGetLens({ name, target -> target.queries(name).map { it ?: "" } }, { it }),
     MappableSetLens({ name, values, target -> values.fold(target, { m, next -> m.query(name, next) }) }, { it })
 )
 
-object Header : SetLensSpec<HttpMessage, String, String>("header",
+object Header : BiDiLensSpec<HttpMessage, String, String>("header",
     MappableGetLens({ name, target -> target.headerValues(name).map { it ?: "" } }, { it }),
     MappableSetLens({ name, values, target -> values.fold(target, { m, next -> m.header(name, next) }) }, { it })
 )
 
-fun <IN> SetLensSpec<IN, String, String>.int(): SetLensSpec<IN, String, Int> = this.map(String::toInt, Int::toString)
-fun <IN> SetLensSpec<IN, String, String>.long(): SetLensSpec<IN, String, Long> = this.map(String::toLong, Long::toString)
+fun <IN> BiDiLensSpec<IN, String, String>.int(): BiDiLensSpec<IN, String, Int> = this.map(String::toInt, Int::toString)
+fun <IN> BiDiLensSpec<IN, String, String>.long(): BiDiLensSpec<IN, String, Long> = this.map(String::toLong, Long::toString)
--- src/test/kotlin/org/reekwest/http/bob/Example.kt
+++ src/test/kotlin/org/reekwest/http/bob/Example.kt
@@ -1,5 +1,6 @@
 package org.reekwest.http.bob
 
+import org.reekwest.http.contract.BiDiMetaLens
 import org.reekwest.http.core.Method.GET
 import org.reekwest.http.core.Request
 import org.reekwest.http.core.Uri.Companion.uri
@@ -9,12 +10,14 @@ import org.reekwest.http.newcontract.long
 
 
 fun main(args: Array<String>) {
-    val optional = Query.optional("name")
-    val requiredHeader = Header.long().required("name")
+    val optional: BiDiMetaLens<Request, String, String?> = Query.optional("name")
+    val o2 = Header.map { it + it }.optional("name")
+    val multi = Header.long().multi
+    val requiredHeader = multi.required("name")
     val request = Request(GET, uri("/bob"), listOf("name" to "123", "name" to "1232"))
 
+    println(o2(request))
     println(optional(request))
-    println(optional(request))
-    println(requiredHeader(request))
-    println(requiredHeader(4444, request))
+//    println(requiredHeader(request))
+    println(requiredHeader(listOf(4444L, 4124L), request))
 }
