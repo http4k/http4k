f7cfff980579fb2049dda3e4547a288246114d05
David Denton <denton.david@gmail.com>
2017-04-21 13:41:11 +0100

    split out get and set lens

--- src/main/kotlin/org/reekwest/http/contract/BiDiLens.kt
+++ src/main/kotlin/org/reekwest/http/contract/BiDiLens.kt
@@ -4,11 +4,14 @@ import org.reekwest.http.asByteBuffer
 import org.reekwest.http.asString
 import java.nio.ByteBuffer
 
-interface GetLens<in IN, out OUT> : (IN) -> List<OUT>
-
-interface BiDiLens<IN, OUT> : GetLens<IN, OUT> {
+interface GetLens<in IN, out OUT> {
+    operator fun invoke(target: IN): List<OUT>
+}
+interface SetLens<IN, in OUT> {
     operator fun invoke(values: List<OUT>, target: IN): IN
 }
+
+interface BiDiLens<IN, OUT> : GetLens<IN, OUT>, SetLens<IN, OUT>
 
 fun <IN> Function1<String, BiDiLens<IN, String>>.asByteBuffers(): (String) -> BiDiLens<IN, ByteBuffer> = {
     object : BiDiLens<IN, ByteBuffer> {
