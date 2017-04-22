93770823e37c469edfaba147d29f9d6bc2fb81fd
David Denton <denton.david@gmail.com>
2017-04-22 11:17:39 +0100

    combine ContractBreach.kt and ExtractionFailure.kt

--- src/main/kotlin/org/reekwest/http/contract/ExtractionFailure.kt
+++ src/main/kotlin/org/reekwest/http/contract/ExtractionFailure.kt
@@ -10,3 +10,12 @@ data class Missing(override val meta: Meta) : ExtractionFailure() {
 data class Invalid(override val meta: Meta) : ExtractionFailure() {
     constructor(lens: Lens<*, *>) : this(lens.meta)
 }
+
+data class ContractBreach(val failures: List<ExtractionFailure>) : Exception(failures.map { it.toString() }.joinToString()) {
+
+    constructor(vararg failures: ExtractionFailure): this(failures.asList())
+
+    companion object {
+        operator fun invoke(vararg failures: ExtractionFailure) = ContractBreach(failures.toList())
+    }
+}
