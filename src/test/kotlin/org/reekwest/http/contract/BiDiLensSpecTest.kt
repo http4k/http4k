b3301c60d960ba711c9bc84a4690dbf588b8ae66
David Denton <denton.david@gmail.com>
2017-04-23 16:23:02 +0100

    more tests for extension functionsπ

--- src/test/kotlin/org/reekwest/http/contract/BiDiLensSpecTest.kt
+++ src/test/kotlin/org/reekwest/http/contract/BiDiLensSpecTest.kt
@@ -9,8 +9,9 @@ import org.junit.Test
 class BiDiLensSpecContract {
 
     private val spec = BiDiLensSpec("location", Get { _: String, str: String ->
-        if (str.isBlank()) emptyList() else listOf(str) },
-        Set { str: String, _: List<String>, _: String -> str })
+        if (str.isBlank()) emptyList() else listOf(str)
+    },
+        Set { _: String, values: List<String>, str: String -> values.fold(str, { _, next -> next }) })
 
     @Test
     fun `int`() = checkContract(spec.int(), "123", 123)
@@ -19,21 +20,23 @@ class BiDiLensSpecContract {
     fun `long`() = checkContract(spec.long(), "123", 123)
 
     @Test
-    fun `float`() = checkContract(spec.float(), "123", 123f)
+    fun `float`() = checkContract(spec.float(), "123.0", 123f)
 
     @Test
-    fun `double`() = checkContract(spec.double(), "123", 123.0)
+    fun `double`() = checkContract(spec.double(), "123.0", 123.0)
 
-    private fun <T> checkContract(spec: BiDiLensSpec<String, String, T>, validValue: String, expectedValue: T) {
+    private fun <T> checkContract(spec: BiDiLensSpec<String, String, T>, valueAsString: String, tValue: T) {
         val optionalLens = spec.optional("hello")
-        assertThat(optionalLens(validValue), equalTo(expectedValue))
+        assertThat(optionalLens(valueAsString), equalTo(tValue))
         assertThat(optionalLens(""), absent())
         assertThat({ optionalLens("hello") }, throws(equalTo(ContractBreach(Invalid(optionalLens)))))
+        assertThat(optionalLens(tValue, "original"), equalTo(valueAsString))
 
         val requiredLens = spec.required("hello")
-        assertThat(requiredLens(validValue), equalTo(expectedValue))
+        assertThat(requiredLens(valueAsString), equalTo(tValue))
         assertThat({ requiredLens("") }, throws(equalTo(ContractBreach(Missing(requiredLens)))))
         assertThat({ requiredLens("hello") }, throws(equalTo(ContractBreach(Invalid(requiredLens)))))
+        assertThat(requiredLens(tValue, "original"), equalTo(valueAsString))
     }
 
 
