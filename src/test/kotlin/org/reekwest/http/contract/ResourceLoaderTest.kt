93200be322435476487dfe57cb57845d0bf71b7a
David Denton <denton.david@gmail.com>
2017-04-25 20:33:05 +0100

    resource loader

--- src/test/kotlin/org/reekwest/http/contract/ResourceLoaderTest.kt
+++ src/test/kotlin/org/reekwest/http/contract/ResourceLoaderTest.kt
@@ -1,24 +1,42 @@
 package org.reekwest.http.contract
 
+import com.natpryce.hamkrest.absent
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
+import org.reekwest.http.contract.ResourceLoader.Companion.Classpath
+import org.reekwest.http.contract.ResourceLoader.Companion.Directory
 
 class ResourceLoaderTest {
 
     @Test
     fun `classpath loader loads existing file`() {
-        checkContents(ResourceLoader.Classpath("/"), "mybob.xml", "<xml>content</xml>")
+        checkContents(Classpath("/"), "mybob.xml", "<xml>content</xml>")
     }
 
     @Test
     fun `classpath loader loads existing child file`() {
-        checkContents(ResourceLoader.Classpath("/"), "org/index.html", "hello from the io index.html")
+        checkContents(Classpath("/"), "org/index.html", "hello from the io index.html")
     }
 
     @Test
     fun `classpath loader for missing file`() {
-        assertThat(false, equalTo(false))
+        assertThat(Classpath("/").load("notAFile"), absent())
+    }
+
+    @Test
+    fun `directory loader loads existing file`() {
+        checkContents(Directory("./src/test/resources"), "mybob.xml", "<xml>content</xml>")
+    }
+
+    @Test
+    fun `directory loader loads existing child file`() {
+        checkContents(Directory("./src/test/resources"), "org/index.html", "hello from the io index.html")
+    }
+
+    @Test
+    fun `directory loader for missing file`() {
+        assertThat(Directory("./src/test/resources").load("notAFile"), absent())
     }
 
     private fun checkContents(loader: ResourceLoader, path: String, expected: String) {
@@ -26,7 +44,7 @@ class ResourceLoaderTest {
     }
 
 //
-//    describe("Classpath loader") {
+//    describe("Directory() loader") {
 //        val loader = ResourceLoader.Classpath("/")
 //        describe("for an existing file") {
 //            it("looks up contents") {
