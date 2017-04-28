2860fd369f32e46697b6178f4694d0f0fd590070
Ivan Sanchez <s4nchez@gmail.com>
2017-04-28 16:20:18 +0100

    Turn BasicAuthentication into a filter

--- reekwest/src/main/kotlin/org/reekwest/http/basicauth/BasicAuthentication.kt
+++ reekwest/src/main/kotlin/org/reekwest/http/basicauth/BasicAuthentication.kt
@@ -1,38 +1,44 @@
 package org.reekwest.http.basicauth
 
-import org.reekwest.http.core.HttpClient
+import org.reekwest.http.base64Decoded
+import org.reekwest.http.base64Encode
+import org.reekwest.http.core.Filter
 import org.reekwest.http.core.HttpHandler
 import org.reekwest.http.core.Request
 import org.reekwest.http.core.Response
 import org.reekwest.http.core.Status
 import org.reekwest.http.core.header
-import java.util.*
 
 data class Credentials(val user: String, val password: String)
 
-fun HttpClient.basicAuth(user: String, password: String): HttpClient = basicAuth(Credentials(user, password))
+class BasicAuthClient(private val provider: () -> Credentials) : Filter {
+    constructor(user: String, password: String) : this(Credentials(user, password))
+    constructor(credentials: Credentials) : this({ credentials })
 
-fun HttpClient.basicAuth(credentials: Credentials): HttpClient = { request: Request ->
-    this(request.header("Authorization", "Basic ${credentials.base64Encoded()}"))
-}
-
-fun HttpHandler.basicAuthProtected(realm: String, user: String, password: String): HttpHandler =
-    basicAuthProtected(realm, { candidate -> candidate == org.reekwest.http.basicauth.Credentials(user, password) })
-
-fun HttpHandler.basicAuthProtected(realm: String, authorize: (Credentials) -> Boolean): HttpHandler = {
-    request: Request ->
-    val credentials = request.basicAuthenticationCredentials()
-    if (credentials == null || !authorize(credentials)) {
-        Response(Status.UNAUTHORIZED).header("WWW-Authenticate", "Basic Realm=\"$realm\"")
-    } else {
-        this(request)
+    override fun invoke(handler: HttpHandler): HttpHandler {
+        return { request: Request ->
+            handler(request.header("Authorization", "Basic ${provider().base64Encoded()}"))
+        }
     }
+
+    private fun Credentials.base64Encoded(): String = "$user:$password".base64Encode()
 }
 
-private fun Request.basicAuthenticationCredentials(): Credentials? = header("Authorization")?.replace("Basic ", "")?.toCredentials()
+class BasicAuthServer(private val realm: String, private val authorize: (Credentials) -> Boolean) : Filter {
+    constructor(realm: String, user: String, password: String) : this(realm, Credentials(user, password))
+    constructor(realm: String, credentials: Credentials) : this(realm, { it == credentials })
 
-private fun String.toCredentials(): Credentials? = base64Decoded().split(":").let { Credentials(it.getOrElse(0, { "" }), it.getOrElse(1, { "" })) }
+    override fun invoke(handler: HttpHandler): HttpHandler = {
+        request: Request ->
+        val credentials = request.basicAuthenticationCredentials()
+        if (credentials == null || !authorize(credentials)) {
+            Response(Status.UNAUTHORIZED).header("WWW-Authenticate", "Basic Realm=\"$realm\"")
+        } else {
+            handler(request)
+        }
+    }
 
-private fun String.base64Decoded(): String = String(Base64.getDecoder().decode(this))
+    private fun Request.basicAuthenticationCredentials(): Credentials? = header("Authorization")?.replace("Basic ", "")?.toCredentials()
 
-private fun Credentials.base64Encoded() = String(Base64.getEncoder().encode("$user:$password".toByteArray()))
+    private fun String.toCredentials(): Credentials? = base64Decoded().split(":").let { Credentials(it.getOrElse(0, { "" }), it.getOrElse(1, { "" })) }
+}
--- reekwest/src/test/kotlin/org/reekwest/http/basicauth/BasicAuthenticationTest.kt
+++ reekwest/src/test/kotlin/org/reekwest/http/basicauth/BasicAuthenticationTest.kt
@@ -8,11 +8,12 @@ import org.reekwest.http.core.Request.Companion.get
 import org.reekwest.http.core.Response.Companion.ok
 import org.reekwest.http.core.Status.Companion.OK
 import org.reekwest.http.core.Status.Companion.UNAUTHORIZED
+import org.reekwest.http.core.then
 
 class BasicAuthenticationTest {
     @Test
     fun fails_to_authenticate() {
-        val handler = { _: Request -> ok() }.basicAuthProtected("my realm", "user", "password")
+        val handler = BasicAuthServer("my realm", "user", "password").then { _: Request -> ok() }
         val response = handler(get("/"))
         assertThat(response.status, equalTo(UNAUTHORIZED))
         assertThat(response.header("WWW-Authenticate"), equalTo("Basic Realm=\"my realm\""))
@@ -20,23 +21,29 @@ class BasicAuthenticationTest {
 
     @Test
     fun authenticate_using_client_extension() {
-        val handler = { _: Request -> ok() }.basicAuthProtected("my realm", "user", "password")
-        val response = handler.basicAuth("user", "password")(get("/"))
+        val handler = BasicAuthServer("my realm", "user", "password").then { _: Request -> ok() }
+        val response = BasicAuthClient("user", "password").then(handler)(get("/"))
         assertThat(response.status, equalTo(OK))
     }
 
     @Test
     fun fails_to_authenticate_if_credentials_do_not_match() {
-        val handler = { _: Request -> ok() }.basicAuthProtected("my realm", "user", "password")
-        val response = handler.basicAuth("user", "wrong")(get("/"))
+        val handler = BasicAuthServer("my realm", "user", "password").then { _: Request -> ok() }
+        val response = BasicAuthClient("user", "wrong").then(handler)(get("/"))
         assertThat(response.status, equalTo(UNAUTHORIZED))
     }
 
     @Test
     fun allow_injecting_authorize_function() {
-        val credential = Credentials("user", "pass")
-        val handler = { _: Request -> ok() }.basicAuthProtected("my realm", { candidate -> candidate == credential })
-        val response = handler.basicAuth(credential)(get("/"))
+        val handler = BasicAuthServer("my realm", { it.user == "user" && it.password == "password" }).then { _: Request -> ok() }
+        val response = BasicAuthClient("user", "password").then(handler)(get("/"))
+        assertThat(response.status, equalTo(OK))
+    }
+
+    @Test
+    fun allow_injecting_credential_provider() {
+        val handler = BasicAuthServer("my realm", "user", "password").then { _: Request -> ok() }
+        val response = BasicAuthClient({ Credentials("user", "password") }).then(handler)(get("/"))
         assertThat(response.status, equalTo(OK))
     }
 }
