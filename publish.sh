4e792194743e32e3f6b7fa22db3cd20ecef414bf
David Denton <denton.david@gmail.com>
2017-04-28 14:47:00 +0100

    adding packages to publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Releasing and publishing v$1
 
-./gradlew -PreleaseVersion=$1 clean build :reekwest:bintrayUpload :kontrakt:bintrayUpload
+./gradlew -PreleaseVersion=$1 clean build :reekwest:bintrayUpload :reekwest-client-apache:bintrayUpload :reekwest-server-jetty:bintrayUpload :kontrakt:bintrayUpload
