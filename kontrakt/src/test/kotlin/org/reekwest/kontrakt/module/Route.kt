74d6421c82c05d428e6f4578bf3eb5591e7a4748
David Denton <denton.david@gmail.com>
2017-04-28 10:32:48 +0100

    refactoring pathbinder

--- kontrakt/src/test/kotlin/org/reekwest/kontrakt/module/Route.kt
+++ kontrakt/src/test/kotlin/org/reekwest/kontrakt/module/Route.kt
@@ -1,12 +1,15 @@
 package org.reekwest.kontrakt.module
 
 import org.reekwest.http.core.ContentType
+import org.reekwest.http.core.Filter
 import org.reekwest.http.core.HttpHandler
 import org.reekwest.http.core.HttpMessage
 import org.reekwest.http.core.Method
 import org.reekwest.http.core.Request
 import org.reekwest.http.core.Status
 import org.reekwest.kontrakt.BodyLens
+import org.reekwest.kontrakt.ContractBreach
+import org.reekwest.kontrakt.ExtractionFailure
 import org.reekwest.kontrakt.HeaderLens
 import org.reekwest.kontrakt.Lens
 import org.reekwest.kontrakt.PathLens
--- kontrakt/src/test/kotlin/org/reekwest/kontrakt/module/RouteModule.kt
+++ kontrakt/src/test/kotlin/org/reekwest/kontrakt/module/RouteModule.kt
@@ -2,11 +2,8 @@ package org.reekwest.kontrakt.module
 
 import org.reekwest.http.core.Filter
 import org.reekwest.http.core.HttpHandler
-import org.reekwest.http.core.Request
 import org.reekwest.http.core.then
 import org.reekwest.kontrakt.CatchContractBreach
-import org.reekwest.kontrakt.ContractBreach
-import org.reekwest.kontrakt.ExtractionFailure
 
 class RouteModule private constructor(private val core: Core) : Module {
 
@@ -15,35 +12,16 @@ class RouteModule private constructor(private val core: Core) : Module {
 
     override fun toRouter(): Router = {
         core.routes.fold<ServerRoute<*>, HttpHandler?>(null, { memo, serverRoute ->
-            val validator = core.filter.then(validationFilter(serverRoute))
+            val validator = core.filter.then(serverRoute.pathBinder.validationFilter())
             memo ?: serverRoute.match(core.rootPath)(it.method, BasePath(it.uri.path))?.let { validator.then(it) }
         })
     }
-
-    private fun validationFilter(serverRoute: ServerRoute<*>) = ValidationFilter(serverRoute.pathBinder.core.route)
 
     fun withRoute(new: ServerRoute<*>) = withRoutes(new)
     fun withRoutes(vararg new: ServerRoute<*>) = withRoutes(new.toList())
     fun withRoutes(new: Iterable<ServerRoute<*>>) = RouteModule(core.withRoutes(new.toList()))
 
     companion object {
-        private class ValidationFilter(private val route: Route) : Filter {
-            private fun validate(request: Request): List<ExtractionFailure> =
-                route.fold(emptyList<ExtractionFailure>()) { memo, next ->
-                    try {
-                        next(request)
-                        memo
-                    } catch (e: ContractBreach) {
-                        memo.plus(e.failures)
-                    }
-                }
-
-            override fun invoke(next: HttpHandler): HttpHandler = {
-                validate(it).let { errors ->
-                    if (errors.isEmpty()) next(it) else throw ContractBreach(errors)
-                }
-            }
-        }
 
         private data class Core(val rootPath: BasePath,
                                 val routes: List<ServerRoute<*>>,
