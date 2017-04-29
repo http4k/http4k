8c6e04a69da5b4eb2b6abdad42e32ea2ea9d1d08
David Denton <denton.david@gmail.com>
2017-04-29 16:53:14 +0100

    fix publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo Releasing and publishing v$1
 
-./gradlew -PreleaseVersion=$1 clean build \\
-:reekwest:bintrayUpload \\
-:reekwest-client-apache:bintrayUpload \\
-:reekwest-server-jetty:bintrayUpload \\
-:reekwest-contract:bintrayUpload \\
-:reekwest-templates-handlebars:bintrayUpload
-:reekwest-formats-argo:bintrayUpload
+./gradlew -PreleaseVersion=$1 clean build \
+    :reekwest:bintrayUpload \
+    :reekwest-client-apache:bintrayUpload \
+    :reekwest-server-jetty:bintrayUpload \
+    :reekwest-contract:bintrayUpload \
+    :reekwest-templates-handlebars:bintrayUpload \
+    :reekwest-formats-argo:bintrayUpload
