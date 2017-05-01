389e0cedece6d160e7c5ad2c32f2fa1f370f1183
Ivan Sanchez <s4nchez@gmail.com>
2017-05-01 22:10:28 +0100

    Improve cookie parsing a bit

--- reekwest/src/main/kotlin/org/reekwest/http/core/cookie/Cookie.kt
+++ reekwest/src/main/kotlin/org/reekwest/http/core/cookie/Cookie.kt
@@ -2,6 +2,7 @@ package org.reekwest.http.core.cookie
 
 import org.reekwest.http.core.Parameters
 import org.reekwest.http.quoted
+import org.reekwest.http.unquoted
 
 data class Cookie(val name: String, val value: String, val attributes: Parameters = listOf()) {
     override fun toString(): String = "$name=${value.quoted()}; ${attributes.cookieString()}"
@@ -10,14 +11,13 @@ data class Cookie(val name: String, val value: String, val attributes: Parameter
 
     companion object {
         fun parse(cookieValue: String): Cookie? {
-            "(.+)=\"(.+)\"; (.*)".toRegex().matchEntire(cookieValue)?.let {
-                val (name, value, attributesString) = it.destructured
-                val attrib = attributesString.split("; ")
-                    .filter { it.contains("=") }
-                    .map { attrString -> attrString.split("=").let { it[0] to it[1] } }
-                return Cookie(name, value, attrib)
-            }
-            return null
+            val pair = cookieValue.split("=", limit = 2)
+            if (pair.size < 2) return null
+            val valueAndAttributes = pair[1].split("; ")
+            val attrib = valueAndAttributes.drop(1)
+                .filter { it.contains("=") }
+                .map { attrString -> attrString.split("=").let { it[0] to it[1] } }
+            return Cookie(pair[0], valueAndAttributes[0].unquoted(), attrib)
         }
     }
 }
