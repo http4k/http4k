b68ff5ef040e975087afb945e7a67450af8d2976
David Denton <denton.david@gmail.com>
2017-05-01 20:13:22 +0100

    tidy interface

--- reekwest/src/main/kotlin/org/reekwest/http/formats/Json.kt
+++ reekwest/src/main/kotlin/org/reekwest/http/formats/Json.kt
@@ -11,6 +11,7 @@ import java.nio.ByteBuffer
  * This is the contract for all JSON implementations
  */
 interface Json<ROOT : NODE, NODE> {
+    // Contract methods to be implemented
     fun ROOT.asPrettyJsonString(): String
     fun ROOT.asCompactJsonString(): String
     fun String.asJsonObject(): ROOT
@@ -23,7 +24,9 @@ interface Json<ROOT : NODE, NODE> {
     fun Boolean?.asJsonValue(): NODE
     fun <T : Iterable<NODE>> T.asJsonArray(): ROOT
     fun <LIST : Iterable<Pair<String, NODE>>> LIST.asJsonObject(): ROOT
+    fun typeOf(value: NODE): JsonType
 
+    // Utility methods - used when we don't know which implementation we are using
     fun string(value: String): NODE = value.asJsonValue()
     fun number(value: Int): NODE = value.asJsonValue()
     fun number(value: Double): NODE = value.asJsonValue()
@@ -31,25 +34,20 @@ interface Json<ROOT : NODE, NODE> {
     fun number(value: BigDecimal): NODE = value.asJsonValue()
     fun number(value: BigInteger): NODE = value.asJsonValue()
     fun boolean(value: Boolean): NODE = value.asJsonValue()
-
-    fun typeOf(value: NODE): JsonType
-
     fun <T : NODE> array(value: Iterable<T>): ROOT = value.asJsonArray()
     fun <T : NODE> obj(value: Iterable<Pair<String, T>>): ROOT = value.asJsonObject()
     fun <T : NODE> obj(vararg fields: Pair<String, T>): ROOT = obj(fields.asIterable())
-    fun parse(s: String): ROOT = s.asJsonObject()
-    fun pretty(node: ROOT): String = node.asPrettyJsonString()
-    fun compact(node: ROOT): String = node.asCompactJsonString()
-
-    fun <IN> lens(spec: BiDiLensSpec<IN, String, String>) = spec.map({ parse(it) }, { compact(it) })
-    fun <IN> BiDiLensSpec<IN, String, String>.json() = lens(this)
-    fun body(): BiDiBodySpec<ByteBuffer, ROOT> = Body.string.map({ parse(it) }, { compact(it) })
-    fun Body.json(): BiDiBodySpec<ByteBuffer, ROOT> = body()
-
     fun nullNode(): NODE {
         val i: Int? = null
         return i.asJsonValue()
     }
+    fun parse(s: String): ROOT = s.asJsonObject()
+    fun pretty(node: ROOT): String = node.asPrettyJsonString()
+    fun compact(node: ROOT): String = node.asCompactJsonString()
+    fun <IN> lens(spec: BiDiLensSpec<IN, String, String>) = spec.map({ parse(it) }, { compact(it) })
+    fun <IN> BiDiLensSpec<IN, String, String>.json() = lens(this)
+    fun body(): BiDiBodySpec<ByteBuffer, ROOT> = Body.string.map({ parse(it) }, { compact(it) })
+    fun Body.json(): BiDiBodySpec<ByteBuffer, ROOT> = body()
 }
 
 enum class JsonType {
