55ad978836a47898a84c38d33b29dff25b387385
Ivan Sanchez <s4nchez@gmail.com>
2017-05-02 22:47:36 +0100

    Add FollowRedirect to be used combined with other filters that are interested in redirection state (e.g. ClientCookies)

---
+++ reekwest/src/main/kotlin/org/reekwest/http/filters/ClientFilters.kt
@@ -0,0 +1,43 @@
+package org.reekwest.http.filters
+
+import org.reekwest.http.core.Filter
+import org.reekwest.http.core.HttpHandler
+import org.reekwest.http.core.Method
+import org.reekwest.http.core.Request
+import org.reekwest.http.core.Response
+import org.reekwest.http.core.Uri
+
+object ClientFilters {
+
+    fun FollowRedirects(): Filter = Filter { next ->
+        { request ->
+            makeRequest(next, request)
+        }
+    }
+
+    private fun makeRequest(next: HttpHandler, request: Request, attempt: Int = 1): Response {
+        val response = next(request)
+        return if (response.isRedirection() && request.allowsRedirection()) {
+            if (attempt == 10) throw IllegalStateException("Too many redirection")
+            val location = response.header("location").orEmpty()
+            makeRequest(next, request.copy(uri = request.newLocation(location)), attempt + 1)
+        } else {
+            response
+        }
+    }
+
+    private fun Request.newLocation(location: String): Uri {
+        val locationUri = Uri.uri(location)
+        return if (locationUri.host.isBlank()) {
+            uri.copy(path = location)
+        } else locationUri
+    }
+
+    private fun Response.isRedirection(): Boolean {
+        return status.redirection && header("location")?.let(String::isNotBlank) ?: false
+    }
+
+    private fun Request.allowsRedirection(): Boolean = method != Method.POST && method != Method.PUT
+}
+
+
---
+++ reekwest/src/test/kotlin/org/reekwest/http/filters/ClientFiltersTest.kt
@@ -0,0 +1,66 @@
+package org.reekwest.http.filters
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import junit.framework.Assert.fail
+import org.junit.Test
+import org.reekwest.http.core.Request
+import org.reekwest.http.core.Request.Companion.get
+import org.reekwest.http.core.Request.Companion.post
+import org.reekwest.http.core.Request.Companion.put
+import org.reekwest.http.core.Response.Companion.movedPermanently
+import org.reekwest.http.core.Response.Companion.movedTemporarily
+import org.reekwest.http.core.Response.Companion.ok
+import org.reekwest.http.core.Response.Companion.serverError
+import org.reekwest.http.core.body
+import org.reekwest.http.core.then
+
+class ClientFiltersTest {
+    val server = { request: Request ->
+        when (request.uri.path) {
+            "/redirect" -> movedTemporarily(listOf("location" to "/ok"))
+            "/loop" -> movedTemporarily(listOf("location" to "/loop"))
+            "/absolute-target" -> if (request.uri.host == "example.com") ok().body("absolute") else serverError()
+            "/absolute-redirect" -> movedPermanently(listOf("location" to "http://example.com/absolute-target"))
+            else -> ok()
+        }
+    }
+
+    val client = ClientFilters.FollowRedirects().then(server)
+
+    @Test
+    fun `does not follow redirect by default`() {
+        val defaultClient = server
+        assertThat(defaultClient(get("/redirect")), equalTo(movedTemporarily(listOf("location" to "/ok"))))
+    }
+
+    @Test
+    fun `follows redirect for temporary redirect response`() {
+        assertThat(client(get("/redirect")), equalTo(ok()))
+    }
+
+    @Test
+    fun `does not follow redirect for post`() {
+        assertThat(client(post("/redirect")), equalTo(movedTemporarily(listOf("location" to "/ok"))))
+    }
+
+    @Test
+    fun `does not follow redirect for put`() {
+        assertThat(client(put("/redirect")), equalTo(movedTemporarily(listOf("location" to "/ok"))))
+    }
+
+    @Test
+    fun `supports absolute redirects`() {
+        assertThat(client(get("/absolute-redirect")), equalTo(ok().body("absolute")))
+    }
+
+    @Test
+    fun `prevents redirection loop after 10 redirects`() {
+        try {
+            client(get("/loop"))
+            fail("should have looped")
+        } catch (e: IllegalStateException) {
+            assertThat(e.message, equalTo("Too many redirection"))
+        }
+    }
+}
