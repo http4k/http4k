96da431d9e86dffbfbca66e1eaae6f15d9be9324
Ivan Sanchez <s4nchez@gmail.com>
2017-05-06 15:54:17 +0100

    Improve Cookie attribute representation

--- reekwest/src/main/kotlin/org/reekwest/http/core/cookie/Cookie.kt
+++ reekwest/src/main/kotlin/org/reekwest/http/core/cookie/Cookie.kt
@@ -1,24 +1,68 @@
 package org.reekwest.http.core.cookie
 
-import org.reekwest.http.core.Parameters
 import org.reekwest.http.quoted
 import org.reekwest.http.unquoted
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
-data class Cookie(val name: String, val value: String, val attributes: Parameters = listOf()) {
-    override fun toString(): String = "$name=${value.quoted()}; ${attributes.cookieString()}"
+data class Cookie(val name: String, val value: String,
+                  val maxAge: Long? = null,
+                  val expires: LocalDateTime? = null,
+                  val domain: String? = null,
+                  val path: String? = null,
+                  val secure: Boolean = false,
+                  val httpOnly: Boolean = false) {
 
-    private fun Parameters.cookieString(): String = map { "${it.first}=${it.second}" }.joinToString("; ")
+    fun domain(domain: String) = copy(domain = domain)
+    fun maxAge(seconds: Long) = copy(maxAge = seconds)
+    fun path(path: String) = copy(path = path)
+    fun secure() = copy(secure = true)
+    fun httpOnly() = copy(httpOnly = true)
+    fun expires(date: LocalDateTime): Cookie = copy(expires = date)
+
+    override fun toString(): String = "$name=${value.quoted()}; ${attributes()}"
+
+    private fun attributes(): String {
+        val builder = mutableListOf<String>()
+        builder.appendIfPresent(maxAge, "Max-Age=$maxAge")
+        builder.appendIfPresent(expires, "Expires=${expires?.let { ZonedDateTime.of(it, ZoneId.of("GMT")).format(RFC822) }}")
+        builder.appendIfPresent(domain, "Domain=$domain")
+        builder.appendIfPresent(path, "Path=$path")
+        builder.appendIfTrue(secure, "secure")
+        builder.appendIfTrue(httpOnly, "HttpOnly")
+        return builder.joinToString("; ")
+    }
 
     companion object {
         fun parse(cookieValue: String): Cookie? {
             val pair = cookieValue.split("=", limit = 2)
             if (pair.size < 2) return null
             val valueAndAttributes = pair[1].split("; ")
-            val attrib = valueAndAttributes.drop(1)
-                .filter { it.contains("=") }
-                .map { attrString -> attrString.split("=").let { it[0] to it[1] } }
-            return Cookie(pair[0], valueAndAttributes[0].unquoted(), attrib)
+            val attributes = valueAndAttributes.drop(1)
+                .map { attrString -> attrString.split("=").let { it[0] to it.getOrNull(1) } }
+            return Cookie(pair[0], valueAndAttributes[0].unquoted(),
+                attributes.maxAge(), attributes.expires(), attributes.domain(),
+                attributes.path(), attributes.secure(), attributes.httpOnly())
         }
+
+        private fun List<Pair<String, String?>>.maxAge(): Long? = find { it.first.equals("Max-Age", true) }?.second?.toLong()
+        private fun List<Pair<String, String?>>.expires(): LocalDateTime? = find { it.first.equals("Expires", true) }?.second?.let { LocalDateTime.parse(it, RFC822) }
+        private fun List<Pair<String, String?>>.domain(): String? = find { it.first.equals("Domain", true) }?.second
+        private fun List<Pair<String, String?>>.path(): String? = find { it.first.equals("Path", true) }?.second
+        private fun List<Pair<String, String?>>.secure(): Boolean = find { it.first.equals("secure", true) } != null
+        private fun List<Pair<String, String?>>.httpOnly(): Boolean = find { it.first.equals("HttpOnly", true) } != null
+    }
+
+    private fun MutableList<String>.appendIfPresent(toCheck: Any?, toInclude: String) {
+        if (toCheck != null) add(toInclude)
+    }
+
+    private fun MutableList<String>.appendIfTrue(toCheck: Boolean, toInclude: String) {
+        if (toCheck) add(toInclude)
     }
 }
+
+private val RFC822 = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz")
 
--- reekwest/src/main/kotlin/org/reekwest/http/core/cookie/CookieExtensions.kt
+++ reekwest/src/main/kotlin/org/reekwest/http/core/cookie/CookieExtensions.kt
@@ -2,52 +2,9 @@ package org.reekwest.http.core.cookie
 
 import org.reekwest.http.core.Request
 import org.reekwest.http.core.Response
-import org.reekwest.http.core.cookie.CookieAttribute.Companion.COMMENT
-import org.reekwest.http.core.cookie.CookieAttribute.Companion.DOMAIN
-import org.reekwest.http.core.cookie.CookieAttribute.Companion.EXPIRES
-import org.reekwest.http.core.cookie.CookieAttribute.Companion.HTTP_ONLY
-import org.reekwest.http.core.cookie.CookieAttribute.Companion.MAX_AGE
-import org.reekwest.http.core.cookie.CookieAttribute.Companion.PATH
-import org.reekwest.http.core.cookie.CookieAttribute.Companion.SECURE
 import org.reekwest.http.unquoted
 import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-
-data class CookieAttribute(val name: String) {
-    companion object {
-        val COMMENT = CookieAttribute("Comment")
-        val DOMAIN = CookieAttribute("Domain")
-        val MAX_AGE = CookieAttribute("Max-Age")
-        val PATH = CookieAttribute("Path")
-        val SECURE = CookieAttribute("Secure")
-        val HTTP_ONLY = CookieAttribute("HttpOnly")
-        val EXPIRES = CookieAttribute("Expires")
-    }
-}
-
-fun Cookie.comment(comment: String) = attribute(COMMENT, comment)
-
-fun Cookie.domain(domain: String) = attribute(DOMAIN, domain)
-
-fun Cookie.maxAge(seconds: Int) = attribute(MAX_AGE, "$seconds")
-
-fun Cookie.path(path: String) = attribute(PATH, path)
-
-fun Cookie.secure() = attribute(SECURE, "")
-
-fun Cookie.httpOnly() = attribute(HTTP_ONLY, "")
-
-fun Cookie.expires(date: LocalDateTime): Cookie = attribute(EXPIRES, ZonedDateTime.of(date, ZoneId.of("GMT")).format(RFC822))
-
-private fun Cookie.attribute(name: String, value: String): Cookie = copy(attributes = attributes.plus(name to value))
-
-private fun Cookie.attribute(attribute: CookieAttribute): String? = this.attributes.find { it.first == attribute.name }?.second
-
-private fun Cookie.attribute(attribute: CookieAttribute, value: String): Cookie = this.attribute(attribute.name, value)
-
-private val RFC822 = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz")
+import java.time.ZoneOffset
 
 fun Response.cookie(cookie: Cookie): Response = header("Set-Cookie", cookie.toString())
 
@@ -63,8 +20,10 @@ internal fun String.toCookieList(): List<Cookie> = split("; ").filter { it.trim(
 
 fun Request.cookies(): List<Cookie> = headers.find { it.first == "Cookie" }?.second?.toCookieList() ?: listOf()
 
-fun Request.cookie(name: String): Cookie? = cookies().filter { it.name == name }.sortedByDescending { it.attribute(PATH)?.length ?: 0 }.firstOrNull()
+fun Request.cookie(name: String): Cookie? = cookies().filter { it.name == name }.sortedByDescending { it.path?.length ?: 0 }.firstOrNull()
 
 private fun List<Cookie>.toCookieString() = map(Cookie::toString).joinToString("")
 
 fun Response.cookies(): List<Cookie> = headerValues("set-cookie").filterNotNull().map { Cookie.parse(it) }.filterNotNull()
+
+fun Cookie.invalidate(): Cookie = copy(value = "").maxAge(0).expires(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC))
