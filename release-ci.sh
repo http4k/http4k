19a948afa57f519d5828d5104caf3263aa492617
David Denton <denton.david@gmail.com>
2017-05-08 10:55:52 +0100

    fix artifact name for release version check. again

--- release-ci.sh
+++ release-ci.sh
@@ -9,7 +9,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LOCAL_VERSION=`jq -r .http4k.version $DIR/version.json`
 
-BINTRAY_VERSION=`curl -s https://bintray.com/api/v1/packages/http4k-core/maven/http4k-core/versions/_latest | jq -r .name`
+BINTRAY_VERSION=`curl -s https://bintray.com/api/v1/packages/http4k/maven/http4k-core/versions/_latest | jq -r .name`
 
 if [[ "$LOCAL_VERSION" == "$BINTRAY_VERSION" ]]; then
     echo "Version has not changed"
