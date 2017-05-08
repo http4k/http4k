ef4ec9b36e78858ebc9fba5631872cc5f81b8945
David Denton <denton.david@gmail.com>
2017-05-08 10:50:37 +0100

    fix artifact name for release version check

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ set -e
 
 NEW_VERSION=$1
 
-BINTRAY_VERSION=`curl -s https://bintray.com/api/v1/packages/http4k-core/maven/http4k/versions/_latest | tools/jq -r .name`
+BINTRAY_VERSION=`curl -s https://bintray.com/api/v1/packages/http4k-core/maven/http4k-core/versions/_latest | tools/jq -r .name`
 
 git stash
 
