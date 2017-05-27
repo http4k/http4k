70cbe4296b2f69d82a3867c6ccd5f15e36e2c255
David Denton <denton.david@gmail.com>
2017-05-27 21:56:16 +0100

    adding testing examples

--- src/test/kotlin/cookbook/testing_endpoints.kt
+++ src/test/kotlin/cookbook/testing_endpoints.kt
@@ -1,7 +1,7 @@
 package cookbook
 
-import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.should.shouldMatch
 import org.http4k.client.OkHttp
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
@@ -9,8 +9,13 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.Uri
+import org.http4k.core.then
+import org.http4k.filter.ClientFilters.SetHostFrom
 import org.http4k.routing.by
+import org.http4k.routing.path
 import org.http4k.routing.routes
+import org.http4k.server.Http4kServer
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
 import org.junit.After
@@ -21,64 +26,95 @@ import org.junit.Test
  * This example shows the various styles of testing endpoints
  */
 
-fun myMathsEndpoint(fn: (Int, Int) -> Int): HttpHandler = {
+class AnswerRecorder(private val httpClient: HttpHandler) : (Int) -> Unit {
+    override fun invoke(answer: Int): Unit {
+        httpClient(Request(GET, "/" + answer.toString()))
+    }
+}
+
+fun myMathsEndpoint(fn: (Int, Int) -> Int, recorder: (Int) -> Unit): HttpHandler = {
     req ->
     val answer = fn(req.query("first")!!.toInt(), req.query("second")!!.toInt())
+    recorder(answer)
     Response(OK).body("the answer is $answer")
 }
 
 class EndpointUnitTest {
     @Test
-    fun `adds numbers`() {
-        val unit = myMathsEndpoint { first, second -> first + second }
+    fun `adds numbers and records answer`() {
+        var answer: Int? = null
+        val unit = myMathsEndpoint({ first, second -> first + second }, { answer = it })
         val response = unit(Request(GET, "/").query("first", "123").query("second", "456"))
-        assertThat(response.status, equalTo(OK))
-        assertThat(response.bodyString(), equalTo("the answer is 579"))
+        answer shouldMatch equalTo(579)
+        response.status shouldMatch equalTo(OK)
+        response.bodyString() shouldMatch equalTo("the answer is 579")
     }
 }
 
-fun MyMathsApp(fn: (Int, Int) -> Int) =
+fun MyMathsApp(recorderHttp: HttpHandler) =
     routes(
-        GET to "/add" by myMathsEndpoint(fn)
+        GET to "/add" by myMathsEndpoint({ first, second -> first + second }, AnswerRecorder(recorderHttp))
     )
+
+class FakeRecorderHttp : HttpHandler {
+    val calls = mutableListOf<Int>()
+
+    private val app = routes(
+        GET to "/{answer}" by { request -> calls.add(request.path("answer")!!.toInt()); Response(OK) }
+    )
+
+    override fun invoke(p1: Request): Response = app(p1)
+}
 
 class ApplicationUnitTest {
 
-    private val app = MyMathsApp { first, second -> first + second }
+    private val recorderHttp = FakeRecorderHttp()
+    private val app = MyMathsApp(recorderHttp)
 
     @Test
     fun `adds numbers`() {
         val response = app(Request(GET, "/add").query("first", "123").query("second", "456"))
-        assertThat(response.status, equalTo(OK))
-        assertThat(response.bodyString(), equalTo("the answer is 579"))
+        response.status shouldMatch equalTo(OK)
+        response.bodyString() shouldMatch equalTo("the answer is 579")
+        recorderHttp.calls shouldMatch equalTo(listOf(579))
     }
 
     @Test
     fun `not found`() {
         val response = app(Request(GET, "/nothing").query("first", "123").query("second", "456"))
-        assertThat(response.status, equalTo(NOT_FOUND))
+        response.status shouldMatch equalTo(NOT_FOUND)
     }
 }
 
-fun MyMathServer(port: Int) = MyMathsApp { first, second -> first + second }.asServer(Jetty(port))
+fun MyMathServer(port: Int, recorderUri: Uri): Http4kServer {
+    val recorderHttp = SetHostFrom(recorderUri).then(OkHttp())
+    return MyMathsApp(recorderHttp).asServer(Jetty(port))
+}
 
 class ServerIntegrationTest {
     private val client = OkHttp()
-    private val server = MyMathServer(8000)
+    private val recorderHttp = FakeRecorderHttp()
+    private val recorder = recorderHttp.asServer(Jetty(8001))
+    private val server = MyMathServer(8000, Uri.of("http://localhost:8001"))
 
     @Before
     fun setup(): Unit {
+        recorder.start()
         server.start()
     }
 
     @After
-    fun teardown(): Unit = server.stop()
+    fun teardown(): Unit {
+        server.stop()
+        recorder.stop()
+    }
 
     @Test
     fun `adds numbers`() {
         val response = client(Request(GET, "http://localhost:8000/add").query("first", "123").query("second", "456"))
-        assertThat(response.status, equalTo(OK))
-        assertThat(response.bodyString(), equalTo("the answer is 579"))
+        response.bodyString() shouldMatch equalTo("the answer is 579")
+        response.status shouldMatch equalTo(OK)
+        recorderHttp.calls shouldMatch equalTo(listOf(579))
     }
 
 }
