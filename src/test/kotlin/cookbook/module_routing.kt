cfd632e701757acde4b47da6951903ae0c9d8165
David Denton <denton.david@gmail.com>
2017-05-29 18:35:50 +0100

    reimplementation of routing to use RoutedRouter and module system

--- src/test/kotlin/cookbook/module_routing.kt
+++ src/test/kotlin/cookbook/module_routing.kt
@@ -12,17 +12,14 @@ import org.http4k.routing.routes
 
 fun main(args: Array<String>) {
 
-    val routesMk2 = routes(
+    val routes = routes(
         GET to "/get/{name}" by { req: Request -> Response(OK).body(req.path("name")!!) },
         POST to "/post/{name}" by { _: Request -> Response(OK) }
     )
-    println(routesMk2(Request(GET, "/get/value")))
+    println(routes(Request(GET, "/get/value")))
 
     val app = routes(
-        "/bob" by routes(
-            GET to "/get/{name}" by { req: Request -> Response(OK).body(req.path("name")!!) },
-            POST to "/post/{name}" by { _: Request -> Response(OK) }
-        ),
+        "/bob" by routes,
         "/rita" by routes(
             DELETE to "/delete/{name}" by { _: Request -> Response(OK) },
             POST to "/post/{name}" by { _: Request -> Response(OK) }
