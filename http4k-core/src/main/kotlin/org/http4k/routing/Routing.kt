8bb40d6360c26f019b021890047fff92376be6be
David Denton <denton.david@gmail.com>
2017-05-29 18:34:34 +0100

    reimplementation of routing to use RoutedRouter and module system

--- http4k-core/src/main/kotlin/org/http4k/routing/Routing.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/Routing.kt
@@ -11,26 +11,16 @@ import org.http4k.core.findSingle
 
 data class Route(val method: Method, val template: UriTemplate, val handler: HttpHandler)
 
-infix fun Pair<Method, String>.by(action: HttpHandler): Route = Route(first, uriTemplate(second), action)
-
-fun Request.path(name: String): String? = uriTemplate().extract(uri.toString())[name]
-
-infix fun String.by(router: RoutesRouter): Module = object : Module {
-    override fun toRouter(): Router = RoutesRouter(router.routes.map { it.copy(template = it.template.prefixedWith(this@by)) })
-}
-
-private fun Request.withUriTemplate(uriTemplate: UriTemplate): Request = header("x-uri-template", uriTemplate.toString())
-
-private fun Request.uriTemplate(): UriTemplate = headers.findSingle("x-uri-template")?.let { UriTemplate.uriTemplate(it) } ?: throw IllegalStateException("x-uri-template header not present in the request")
-
 fun routes(vararg routes: Route): RoutesRouter = RoutesRouter(routes.asList())
 
 fun routes(module: Module, vararg then: Module): HttpHandler = then.fold(module) { memo, next -> memo.then(next) }.toHttpHandler()
 
-fun Route.asRouter(): Router = object : Router {
-    override fun match(request: Request): HttpHandler? = if (template.matches(request.uri.path) && method == request.method) {
-        { req: Request -> handler(req.withUriTemplate(template)) }
-    } else null
+fun Request.path(name: String): String? = uriTemplate().extract(uri.toString())[name]
+
+infix fun Pair<Method, String>.by(action: HttpHandler): Route = Route(first, uriTemplate(second), action)
+
+infix fun String.by(router: RoutesRouter): Module = object : Module {
+    override fun toRouter(): Router = RoutesRouter(router.routes.map { it.copy(template = it.template.prefixedWith(this@by)) })
 }
 
 data class RoutesRouter(internal val routes: List<Route>): Router, HttpHandler {
@@ -41,3 +31,13 @@ data class RoutesRouter(internal val routes: List<Route>): Router, HttpHandler {
 
     override fun match(request: Request): HttpHandler? = routers.fold(noMatch, { memo, router -> memo ?: router.match(request) })
 }
+
+private fun Route.asRouter(): Router = object : Router {
+    override fun match(request: Request): HttpHandler? = if (template.matches(request.uri.path) && method == request.method) {
+        { req: Request -> handler(req.withUriTemplate(template)) }
+    } else null
+}
+
+private fun Request.withUriTemplate(uriTemplate: UriTemplate): Request = header("x-uri-template", uriTemplate.toString())
+
+private fun Request.uriTemplate(): UriTemplate = headers.findSingle("x-uri-template")?.let { UriTemplate.uriTemplate(it) } ?: throw IllegalStateException("x-uri-template header not present in the request")
