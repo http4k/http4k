8968b03b9dd2ed47d404883ea13e9b97e553d1c2
David Denton <denton.david@gmail.com>
2017-05-30 14:18:40 +0100

    refactor ContractRouter

--- http4k-contract/src/main/kotlin/org/http4k/contract/ContractRouter.kt
+++ http4k-contract/src/main/kotlin/org/http4k/contract/ContractRouter.kt
@@ -11,56 +11,47 @@ import org.http4k.filter.ServerFilters
 import org.http4k.lens.Header.X_URI_TEMPLATE
 import org.http4k.routing.Router
 
-class ContractRouter private constructor(private val router: ModuleRouter) : Router {
-    override fun match(request: Request): HttpHandler? = router.match(request)
+data class ContractRouter private constructor(private val contractRoot: BasePath,
+                                              private val renderer: ContractRenderer,
+                                              private val filter: Filter,
+                                              private val security: Security,
+                                              private val descriptionPath: (BasePath) -> BasePath,
+                                              private val routes: List<ServerRoute>) : Router {
 
     constructor(contractRoot: BasePath, renderer: ContractRenderer = NoRenderer, filter: Filter = Filter { it })
-        : this(ModuleRouter(contractRoot, renderer, ServerFilters.CatchLensFailure.then(filter)))
+        : this(contractRoot, renderer, ServerFilters.CatchLensFailure.then(filter), NoSecurity, { it }, emptyList())
 
-    fun securedBy(new: Security) = ContractRouter(router.securedBy(new))
-    fun withDescriptionPath(fn: (BasePath) -> BasePath) = ContractRouter(router.copy(descriptionPath = fn))
+    fun withDescriptionPath(fn: (BasePath) -> BasePath) = copy(descriptionPath = fn)
     fun withRoute(new: ServerRoute) = withRoutes(new)
     fun withRoutes(vararg new: ServerRoute) = withRoutes(new.toList())
-    fun withRoutes(new: Iterable<ServerRoute>) = ContractRouter(router.withRoutes(new.toList()))
+    fun withRoutes(new: Iterable<ServerRoute>) = copy(routes = routes + new)
+    fun securedBy(new: Security) = copy(security = new)
 
-    companion object {
-        private data class ModuleRouter(val contractRoot: BasePath,
-                                        val renderer: ContractRenderer,
-                                        val filter: Filter,
-                                        val security: Security = NoSecurity,
-                                        val descriptionPath: (BasePath) -> BasePath = { it },
-                                        val routes: List<ServerRoute> = emptyList()) : Router {
-            private val descriptionRoute = descriptionRoute()
+    private val descriptionRoute = descriptionRoute()
 
-            private val routers: List<Pair<Router, Filter>> = routes
-                .map { it.router(contractRoot) to security.filter.then(identify(it)).then(filter) }
-                .plus(descriptionRoute.router(contractRoot) to identify(descriptionRoute).then(filter))
+    private val routers: List<Pair<Router, Filter>> = routes
+        .map { it.router(contractRoot) to security.filter.then(identify(it)).then(filter) }
+        .plus(descriptionRoute.router(contractRoot) to identify(descriptionRoute).then(filter))
 
-            private val noMatch: HttpHandler? = null
+    private val noMatch: HttpHandler? = null
 
-            override fun match(request: Request): HttpHandler? =
-                if (request.isIn(contractRoot)) {
-                    routers.fold(noMatch, { memo, (router, routeFilter) ->
-                        memo ?: router.match(request)?.let { routeFilter.then(it) }
-                    })
-                } else null
+    override fun match(request: Request): HttpHandler? =
+        if (request.isIn(contractRoot)) {
+            routers.fold(noMatch, { memo, (router, routeFilter) ->
+                memo ?: router.match(request)?.let { routeFilter.then(it) }
+            })
+        } else null
 
-            fun withRoutes(new: List<ServerRoute>) = copy(routes = routes + new)
-            fun securedBy(new: Security) = copy(security = new)
+    private fun descriptionRoute(): ServerRoute =
+        PathBinder0(Core(Route("description route"), GET, descriptionPath)) bind
+            { renderer.description(contractRoot, security, routes) }
 
-            private fun descriptionRoute(): ServerRoute =
-                PathBinder0(Core(Route("description route"), GET, descriptionPath)) bind
-                    { renderer.description(contractRoot, security, routes) }
-
-            private fun identify(route: ServerRoute): Filter {
-                val routeIdentity = route.describeFor(contractRoot)
-                return Filter {
-                    { req ->
-                        it(req.with(X_URI_TEMPLATE of if (routeIdentity.isEmpty()) "/" else routeIdentity))
-                    }
-                }
+    private fun identify(route: ServerRoute): Filter {
+        val routeIdentity = route.describeFor(contractRoot)
+        return Filter {
+            { req ->
+                it(req.with(X_URI_TEMPLATE of if (routeIdentity.isEmpty()) "/" else routeIdentity))
             }
-
         }
     }
 }
