ebed590ca127028b9216b1741937c742f59223e2
David Denton <denton.david@gmail.com>
2017-06-03 10:08:46 +0100

    rewrite contracts

---
+++ http4k-contract/src/test/kotlin/org/http4k/example.kt
@@ -0,0 +1,27 @@
+package org.http4k
+
+import org.http4k.contract.NoRenderer
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.lens.Path
+import org.http4k.lens.int
+import org.http4k.routing.routes
+
+
+fun bob(i: Int): HttpHandler = TODO()
+
+fun main(args: Array<String>) {
+
+    val a = routes(
+        "/contract" by cont(NoRenderer)(
+            GET to "/" bindTo { request: Request -> Response(OK) } describedBy Desc(),
+            GET to "value" / Path.of("hello") / Path.int().of("world") bindTo ::bob describedBy Desc(),
+            GET to "value2" / Path.of("hello") / Path.int().of("world") bindTo ::bob
+        )
+    )
+
+    a(Request(GET, "/contract/value2/bob/bob2"))
+}
--- http4k-contract/src/test/kotlin/org/http4k/extensions.kt
+++ http4k-contract/src/test/kotlin/org/http4k/extensions.kt
@@ -1,26 +1,18 @@
 package org.http4k
 
+import org.http4k.SBB.Companion.Core
 import org.http4k.contract.BasePath
 import org.http4k.contract.ContractRenderer
 import org.http4k.contract.ContractRoutingHttpHandler
 import org.http4k.contract.NoRenderer
 import org.http4k.contract.NoSecurity
-import org.http4k.contract.Route
 import org.http4k.contract.Security
-import org.http4k.contract.ServerRoute
-import org.http4k.contract.basePath
-import org.http4k.contract.without
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
-import org.http4k.core.Method.GET
 import org.http4k.core.Request
-import org.http4k.core.then
-import org.http4k.lens.LensFailure
 import org.http4k.lens.Path
 import org.http4k.lens.PathLens
-import org.http4k.lens.int
 import org.http4k.routing.Router
-import org.http4k.routing.routes
 import org.http4k.contract.ContractRoutingHttpHandler.Companion.Handler as ContractHandler
 
 infix fun String.by(router: ContractRoutingHttpHandler): ContractRoutingHttpHandler = router.withBasePath(this)
@@ -34,86 +26,65 @@ fun cont(renderer: ContractRenderer = NoRenderer, descriptionPath: String = "", 
         override fun invoke(vararg sbbs: SBB): ContractRoutingHttpHandler = TODO()
     }
 
-class ServerRoute2 internal constructor(private val pathBinder: PB, private val toHandler: (ExtractedParts) -> HttpHandler) {
-    internal val core = pathBinder.core.route.core
-    internal val method = pathBinder.core.method
-    internal fun router(contractRoot: BasePath): Router = pathBinder.toRouter(contractRoot, toHandler)
+class ServerRoute2 internal constructor(private val sbb: SBB, private val toHandler: (ExtractedParts) -> HttpHandler) {
+    internal fun router(contractRoot: BasePath): Router = sbb.toRouter(contractRoot, toHandler)
 }
 
-abstract class PB internal constructor(internal val core: Core, internal vararg val pathLenses: PathLens<*>) {
+abstract class PB internal constructor(val core: PCore, vararg val pathLenses: PathLens<*>) {
     abstract infix operator fun <T> div(next: PathLens<T>): PB
 
     open infix operator fun div(next: String) = div(Path.fixed(next))
-
-    internal fun toRouter(contractRoot: BasePath, toHandler: (ExtractedParts) -> HttpHandler): Router = object : Router {
-        override fun match(request: Request): HttpHandler? = core.matches(contractRoot, request, pathLenses.toList(), toHandler)
+    internal data class PCore(val pathFn: (BasePath) -> BasePath) {
+        infix operator fun div(next: String) = copy(pathFn = { pathFn(it) / next })
     }
 
-    internal fun describe(contractRoot: BasePath): String {
-        return "${core.pathFn(contractRoot)}${if (pathLenses.isNotEmpty()) "/${pathLenses.joinToString("/")}" else ""}"
-    }
-
-    companion object {
-        internal data class Core(val route: Route, val method: Method, val pathFn: (BasePath) -> BasePath) {
-            infix operator fun div(next: String) = copy(pathFn = { pathFn(it) / next })
-
-            fun matches(contractRoot: BasePath, request: Request, lenses: List<PathLens<*>>, toHandler: (ExtractedParts) -> HttpHandler): HttpHandler? =
-                if (request.method == method && request.basePath().startsWith(pathFn(contractRoot))) {
-                    try {
-                        request.without(pathFn(contractRoot)).extract(lenses)?.let { route.core.validationFilter.then(toHandler(it)) }
-                    } catch (e: LensFailure) {
-                        null
-                    }
-                } else null
-        }
-    }
 }
 
-class PB0 internal constructor(core: PB.Companion.Core) : PB(core) {
+class PB0 internal constructor(pathFn: (BasePath) -> BasePath) : PB(PCore(pathFn)) {
 
-    override infix operator fun div(next: String) = PB0(core)
+    override infix operator fun div(next: String) = PB0 { it }
 
-    override infix operator fun <NEXT> div(next: PathLens<NEXT>) = PB1<NEXT>(core)
-
-    infix fun bind(handler: HttpHandler): ServerRoute = TODO()
+    override infix operator fun <NEXT> div(next: PathLens<NEXT>) = PB1(core, next)
 }
 
-class PB1<out A> internal constructor(core: PB.Companion.Core) : PB(core) {
+class PB1<out A> internal constructor(core: PCore, val a: PathLens<A>) : PB(core) {
     override infix operator fun div(next: String) = throw UnsupportedOperationException("no longer paths!")
 
     override infix operator fun <NEXT> div(next: PathLens<NEXT>) = throw UnsupportedOperationException("no longer paths!")
-
-    infix fun bind(fn: (A) -> HttpHandler): ServerRoute = TODO()
 }
 
-operator fun String.div(pathLens: PathLens<*>): PB0 = TODO()
+operator fun <A> String.div(next: PathLens<A>): PB1<A> = PB0 { it } / next
 
-fun bob(i: Int): HttpHandler = TODO()
+abstract class SBB(val core: Core, val desc: Desc) {
+    abstract infix fun describedBy(new: Desc): SBB
+    abstract fun toServerRoute(): ServerRoute2
 
-interface SBB {
-    infix fun describedBy(spec: Desc): SBB
+    internal fun toRouter(contractRoot: BasePath, toHandler: (ExtractedParts) -> HttpHandler): Router = object : Router {
+        override fun match(request: Request): HttpHandler? = TODO()
+    }
 
-    fun toServerRoute(): ServerRoute2
+    companion object {
+        data class Core(val method: Method)
+    }
 }
 
-fun main(args: Array<String>) {
-
-    val a = routes(
-        "/contract" by cont(NoRenderer)(
-            GET to "value" / Path.of("hello") / Path.int().of("world") bindTo ::bob describedBy Desc(),
-            GET to "value2" / Path.of("hello") / Path.int().of("world") bindTo ::bob
-        )
-    )
-
-    a(Request(GET, "/contract/value2/bob/bob2"))
-}
-
-infix fun <A> Pair<Method, PB1<A>>.bindTo(fn: (A) -> HttpHandler): SBB = object : SBB {
-    override infix fun describedBy(spec: Desc): SBB = TODO()
+class SBB0(core: Core, desc: Desc, private val fn: HttpHandler) : SBB(core, desc) {
+    override infix fun describedBy(new: Desc): SBB = SBB0(core, desc, fn)
     override fun toServerRoute(): ServerRoute2 = TODO()
 }
 
-//infix fun <A> Pair<Method, PB1<A>>.speccedBy(spec: ContractSpec, b: String): ServerRoute = TODO()
+class SBB1<A>(core: Core, desc: Desc, private val fn: (A) -> HttpHandler) : SBB(core, desc) {
+    override infix fun describedBy(new: Desc): SBB = SBB1(core, desc, fn)
+    override fun toServerRoute(): ServerRoute2 = TODO()
+}
+
+infix fun Pair<Method, String>.bindTo(fn: HttpHandler): SBB = SBB0(Core(first), Desc(), fn)
+
+@JvmName("bind0")
+infix fun Pair<Method, PB0>.bindTo(fn: HttpHandler): SBB = SBB0(Core(first), Desc(), fn)
+
+@JvmName("bind1")
+infix fun <A> Pair<Method, PB1<A>>.bindTo(fn: (A) -> HttpHandler): SBB = SBB1(Core(first), Desc(), fn)
 
 internal class ExtractedParts(private val mapping: Map<PathLens<*>, *>) {
     @Suppress("UNCHECKED_CAST")
