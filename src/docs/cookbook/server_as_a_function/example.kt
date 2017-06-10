0b46cfc5faea221473830ca2d8c184c5e52c0b01
David Denton <denton.david@gmail.com>
2017-06-10 18:56:06 +0100

    add cookbook

--- src/docs/cookbook/server_as_a_function/example.kt
+++ src/docs/cookbook/server_as_a_function/example.kt
@@ -6,10 +6,6 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 
-/**
- * This is the simplest example of "Server as a function". There is no routing involved, nor any server. Testing
- * can be done out-of-container.
- */
 fun main(args: Array<String>) {
 
     val app: HttpHandler = { request: Request -> Response(Status.OK).body("Hello, ${request.query("name")}!") }
---
+++ src/docs/cookbook/server_as_a_function/index.md
@@ -0,0 +1,3 @@
+This is the simplest example of "Server as a function". There is no routing involved, nor any server. Testing can be done out-of-container.
+
+<script src="http://gist-it.appspot.com/https://github.com/http4k/http4k/blob/master/src/docs/cookbook/server_as_a_function/example .kt"></script>
