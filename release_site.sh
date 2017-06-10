977368d58035ef8297bd9bb1fc08d6ec4b8c8300
David Denton <denton.david@gmail.com>
2017-06-10 19:00:40 +0100

    more tweaks

--- release_site.sh
+++ release_site.sh
@@ -7,6 +7,8 @@ TMP=/tmp/http4k.github.io/
 rm -rf ${TMP}
 git clone git@github.com:http4k/http4k.github.io.git ${TMP}
 cp -R src/docs ${TMP}/
+cp CONTRIBUTING.md ${TMP}/contributing/index.md
+cp CHANGELOG.md ${TMP}/changelog/index.md
 cp -R src/mkdocs.yml ${TMP}
 
 cd ${TMP}
