28fbbd3d9bff41671c9520c3361dfc901acf89ff
David Denton <denton.david@gmail.com>
2017-06-11 09:07:16 +0100

    adding site building to travis

--- release_site.sh
+++ release_site.sh
@@ -7,7 +7,7 @@ TMP=/tmp/http4k.github.io/
 rm -rf ${TMP}
 git clone git@github.com:http4k/http4k.github.io.git ${TMP}
 cp -R src/docs ${TMP}/
-echo `pwd`
+
 cp CONTRIBUTING.md ${TMP}docs/contributing/index.md
 cp CHANGELOG.md ${TMP}docs/changelog/index.md
 cp README.md ${TMP}docs/index.md
