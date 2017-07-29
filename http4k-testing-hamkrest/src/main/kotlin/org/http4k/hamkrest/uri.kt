0b37477e127235ded1fe4aa8519eeeb473af8245
David Denton <denton.david@gmail.com>
2017-07-29 15:14:48 +0100

    adding testing-hamcrest package

--- http4k-testing-hamkrest/src/main/kotlin/org/http4k/hamkrest/uri.kt
+++ http4k-testing-hamkrest/src/main/kotlin/org/http4k/hamkrest/uri.kt
@@ -4,28 +4,27 @@ import com.natpryce.hamkrest.Matcher
 import com.natpryce.hamkrest.equalTo
 import org.http4k.core.Uri
 
-
-fun path(expected: String) = object : Matcher<Uri> {
+fun hasUriPath(expected: String) = object : Matcher<Uri> {
     override val description = "Uri with Path $expected"
     override fun invoke(actual: Uri) = equalTo(expected)(actual.path)
 }
 
-fun query(expected: String) = object : Matcher<Uri> {
+fun hasUriQuery(expected: String) = object : Matcher<Uri> {
     override val description = "Uri with Query $expected"
     override fun invoke(actual: Uri) = equalTo(expected)(actual.query)
 }
 
-fun authority(expected: String) = object : Matcher<Uri> {
+fun hasAuthority(expected: String) = object : Matcher<Uri> {
     override val description = "Uri with Authority $expected"
     override fun invoke(actual: Uri) = equalTo(expected)(actual.authority)
 }
 
-fun host(expected: String) = object : Matcher<Uri> {
+fun hasHost(expected: String) = object : Matcher<Uri> {
     override val description = "Uri with Host $expected"
     override fun invoke(actual: Uri) = equalTo(expected)(actual.host)
 }
 
-fun port(expected: Int?) = object : Matcher<Uri> {
+fun hasPort(expected: Int?) = object : Matcher<Uri> {
     override val description = "Uri with Port $expected"
     override fun invoke(actual: Uri) = equalTo(expected)(actual.port)
 }
---
+++ http4k-testing-hamkrest/src/test/kotlin/org/http4k/hamkrest/UriMatchersTest.kt
@@ -0,0 +1,22 @@
+package org.http4k.hamkrest
+
+import org.http4k.core.Uri
+import org.junit.Test
+
+class UriMatchersTest {
+
+    @Test
+    fun `path`() = assertMatchAndNonMatch(Uri.of("/bob"), hasUriPath("/bob"), hasUriPath("bill"))
+
+    @Test
+    fun `authority`() = assertMatchAndNonMatch(Uri.of("http://bob:80"), hasAuthority("bob:80"), hasAuthority("bill:80"))
+
+    @Test
+    fun `host`() = assertMatchAndNonMatch(Uri.of("http://bob:80"), hasHost("bob"), hasHost("bill"))
+
+    @Test
+    fun `port`() = assertMatchAndNonMatch(Uri.of("http://bob:80"), hasPort(80), hasPort(81))
+
+    @Test
+    fun `query`() = assertMatchAndNonMatch(Uri.of("http://bob:80?query=bob"), hasUriQuery("query=bob"), hasUriQuery("query=bill"))
+}
