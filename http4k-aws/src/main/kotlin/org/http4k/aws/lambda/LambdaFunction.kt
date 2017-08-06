85f8d1d5881ac9e8b206a277b29c992c1cad8613
David Denton <denton.david@gmail.com>
2017-08-06 16:02:22 +0100

    null safety when coming from java

--- http4k-aws/src/main/kotlin/org/http4k/aws/lambda/LambdaFunction.kt
+++ http4k-aws/src/main/kotlin/org/http4k/aws/lambda/LambdaFunction.kt
@@ -11,7 +11,7 @@ import org.http4k.core.toUrlEncoded
  * This is the main entry point for the lambda. It uses the local environment
  * to instantiate the Http4k handler which can be used for further invocations.
  */
-class LambdaFunction(private val env: Map<String, String> = System.getenv()) {
+class LambdaFunction(env: Map<String, String> = System.getenv()) {
     private val app = BootstrapAppLoader(env)
 
     fun handle(request: ApiGatewayProxyRequest) = app(request.asHttp4k()).asApiGateway()
@@ -19,10 +19,10 @@ class LambdaFunction(private val env: Map<String, String> = System.getenv()) {
 
 internal fun Response.asApiGateway() = ApiGatewayProxyResponse(status.code, headers.toMap(), bodyString())
 
-internal fun ApiGatewayProxyRequest.asHttp4k() = headers.toList().fold(
+internal fun ApiGatewayProxyRequest.asHttp4k() = (headers ?: emptyMap()).toList().fold(
     Request(Method.valueOf(httpMethod), uri())
         .body(body?.let(::Body) ?: Body.EMPTY)) { memo, (first, second) ->
     memo.header(first, second)
 }
 
-internal fun ApiGatewayProxyRequest.uri() = Uri.of(path).query(queryStringParameters.toList().toUrlEncoded())
+internal fun ApiGatewayProxyRequest.uri() = Uri.of(path ?: "").query((queryStringParameters ?: emptyMap()).toList().toUrlEncoded())
