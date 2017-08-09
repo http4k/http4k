2fecfbe7e93571d31220ec396c1755315d512fa1
David Denton <denton.david@gmail.com>
2017-08-09 13:41:47 +0100

    add text/xml content type - this is used by AWS

--- http4k-core/src/main/kotlin/org/http4k/core/ContentType.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/ContentType.kt
@@ -7,6 +7,7 @@ data class ContentType(val value: String) {
         val APPLICATION_FORM_URLENCODED = ContentType("application/x-www-form-urlencoded")
         val APPLICATION_XML = ContentType("application/xml")
         val TEXT_HTML = ContentType("text/html")
+        val TEXT_XML = ContentType("text/xml")
         val TEXT_PLAIN = ContentType("text/plain")
         val OCTET_STREAM = ContentType("application/octet-stream")
     }
