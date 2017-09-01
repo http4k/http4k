1358665440fa78c72e45415f9be81a120ffe2bcb
David Denton <denton.david@gmail.com>
2017-09-01 19:08:50 +0100

    revert of removing method

--- http4k-multipart/src/main/kotlin/org/http4k/multipart/part/Part.kt
+++ http4k-multipart/src/main/kotlin/org/http4k/multipart/part/Part.kt
@@ -1,15 +1,22 @@
 package org.http4k.multipart.part
 
+import java.io.ByteArrayInputStream
 import java.io.Closeable
 import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
 import java.nio.charset.Charset
 import java.nio.file.FileSystemException
 
 sealed class Part(fieldName: String?, formField: Boolean, contentType: String?, fileName: String?, headers: Map<String, String>, val length: Int) : PartMetaData(fieldName, formField, contentType, fileName, headers), Closeable {
 
+    abstract val newInputStream: InputStream
+
     abstract val bytes: ByteArray
 
     class DiskBacked(part: PartMetaData, internal val theFile: File) : Part(part.fieldName, part.isFormField, part.contentType, part.fileName, part.headers, theFile.length().toInt()) {
+        override val newInputStream: InputStream
+            get() = FileInputStream(theFile)
 
         override val bytes
             get() = throw IllegalStateException("Cannot get bytes from a DiskBacked Part. Check with isInMemory()")
@@ -24,6 +31,9 @@ sealed class Part(fieldName: String?, formField: Boolean, contentType: String?, 
     class InMemory(original: PartMetaData,
                    override val bytes: ByteArray /* not immutable*/,
                    internal val encoding: Charset) : Part(original.fieldName, original.isFormField, original.contentType, original.fileName, original.headers, bytes.size) {
+
+        override val newInputStream: InputStream
+            get() = ByteArrayInputStream(bytes)
 
         override fun close() {
             // do nothing
