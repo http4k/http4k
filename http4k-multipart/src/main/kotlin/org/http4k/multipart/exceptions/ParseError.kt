342ef1b048d0e7e475f6e6046530bb93e7389d58
David Denton <denton.david@gmail.com>
2017-09-01 14:17:11 +0100

    convert to kotlin

---
+++ http4k-multipart/src/main/kotlin/org/http4k/multipart/exceptions/ParseError.kt
@@ -0,0 +1,7 @@
+package org.http4k.multipart.exceptions
+
+class ParseError : RuntimeException {
+    constructor(e: Exception) : super(e) {}
+
+    constructor(message: String) : super(message) {}
+}
