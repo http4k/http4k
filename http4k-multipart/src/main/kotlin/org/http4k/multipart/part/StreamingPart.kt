2c95eeacdb24ac37d45be01650d6f2b4a24eac7d
David Denton <denton.david@gmail.com>
2017-09-03 20:49:33 +0100

    simplify - remove stream reading

--- http4k-multipart/src/main/kotlin/org/http4k/multipart/part/StreamingPart.kt
+++ http4k-multipart/src/main/kotlin/org/http4k/multipart/part/StreamingPart.kt
@@ -1,14 +1,10 @@
 package org.http4k.multipart.part
 
-import org.http4k.multipart.stream.StreamUtil
 import java.io.InputStream
-import java.nio.charset.StandardCharsets
 
 class StreamingPart(fieldName: String, formField: Boolean, contentType: String?, fileName: String?, val inputStream: InputStream, headers: Map<String, String>)
     : PartMetaData(fieldName, formField, contentType, fileName, headers) {
 
     val contentsAsString: String
-        get() = StreamUtil.readStringFromInputStream(inputStream, StandardCharsets.UTF_8, 4096)
-
-
+        get() = inputStream.use { it.reader().readText() }
 }
