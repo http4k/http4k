cf41577535eccd83fe0e0481456ad992c639728c
Ivan Sanchez <s4nchez@gmail.com>
2017-09-05 22:12:22 +0100

    Make Jetty configurable with custom Server

--- http4k-server-jetty/src/main/kotlin/org/http4k/server/Jetty.kt
+++ http4k-server-jetty/src/main/kotlin/org/http4k/server/Jetty.kt
@@ -7,22 +7,15 @@ import org.eclipse.jetty.servlet.ServletHolder
 import org.http4k.core.HttpHandler
 import org.http4k.servlet.asServlet
 
-/**
- * Exposed to allow for customisation of the Jetty server instance
- */
-class Http4kJettyServer(port: Int = 8000, app: HttpHandler) : Server(port) {
-    init {
-        handler = ServletContextHandler(SESSIONS).apply {
-            addServlet(ServletHolder(app.asServlet()), "/*")
-        }
-    }
-}
+class Jetty(private val server: Server) : ServerConfig {
+    constructor(port: Int = 8000) : this(Server(port))
 
-data class Jetty(val port: Int = 8000) : ServerConfig {
     override fun toServer(handler: HttpHandler): Http4kServer {
-        return object : Http4kServer {
-            private val server = Http4kJettyServer(port, handler)
+        server.insertHandler(ServletContextHandler(SESSIONS).apply {
+            addServlet(ServletHolder(handler.asServlet()), "/*")
+        })
 
+        return object : Http4kServer {
             override fun start(): Http4kServer {
                 server.start()
                 return this
---
+++ http4k-server-jetty/src/test/kotlin/org/http4k/server/JettyStreamingTest.kt
@@ -0,0 +1,61 @@
+package org.http4k.server
+
+import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.should.shouldMatch
+import junit.framework.Assert.fail
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
+import org.eclipse.jetty.util.thread.QueuedThreadPool
+import org.http4k.client.ApacheClient
+import org.http4k.client.ResponseBodyMode
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.junit.Test
+
+
+class JettyStreamingTest {
+    @Test
+    fun `release streaming connections`() {
+        val threadPool = QueuedThreadPool(4)
+        val server = Server(threadPool)
+        val connector = ServerConnector(server)
+        server.addConnector(connector)
+
+        val sizableResponse = (1..5000000).joinToString("x")
+
+        val app = { _: Request -> Response(Status.OK).body(sizableResponse.byteInputStream()) }
+        app.asServer(Jetty(server)).start()
+
+        val initialUsedThreads = threadPool.busyThreads
+
+        val port = server.uri.port
+
+        val client = ApacheClient(bodyMode = ResponseBodyMode.Stream)
+
+        val response = client(Request(Method.GET, "http://localhost:$port/"))
+
+        threadPool.busyThreads.shouldMatch(equalTo(initialUsedThreads + 1))
+
+        response.body.stream.close()
+
+        tryFor(10, "request thread was not released") {
+            threadPool.busyThreads.shouldMatch(equalTo(initialUsedThreads))
+        }
+    }
+
+    private fun tryFor(timeoutInSeconds: Int, description: String, assertion: () -> Unit) {
+        val start = System.currentTimeMillis()
+        do {
+            try {
+                assertion()
+                return
+            } catch (e: AssertionError) {
+                Thread.sleep(100)
+            }
+        } while (System.currentTimeMillis() - start < timeoutInSeconds * 1000)
+        fail("Test timed out: $description")
+    }
+}
+
