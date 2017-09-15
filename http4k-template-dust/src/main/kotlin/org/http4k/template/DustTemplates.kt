1cc3fe6cd4c6f37c8d2f2dbc5884474674a84924
Nat Pryce <sw@natpryce.com>
2017-09-15 11:34:25 +0100

    Dust templates implemented by running the Dust.js Javascript library in Nashorn.
    See http://www.dustjs.com/

---
+++ http4k-template-dust/src/main/kotlin/org/http4k/template/DustTemplates.kt
@@ -0,0 +1,30 @@
+package org.http4k.template
+
+import org.http4k.template.dust.Dust
+import org.http4k.template.dust.TemplateLoader
+import org.http4k.template.dust.loadFromFilesIn
+import org.http4k.template.dust.loadFromResourcesIn
+
+
+class DustTemplates(private val precache: Int = 0) : Templates {
+    override fun Caching(baseTemplateDir: String): TemplateRenderer {
+        return dust4Http4K(true, loadFromFilesIn(baseTemplateDir))
+    }
+    
+    override fun CachingClasspath(baseClasspathPackage: String): TemplateRenderer {
+        return dust4Http4K(true, loadFromResourcesIn(baseClasspathPackage))
+    }
+    
+    override fun HotReload(baseTemplateDir: String): TemplateRenderer {
+        return dust4Http4K(false, loadFromFilesIn(baseTemplateDir))
+    }
+    
+    private fun dust4Http4K(cacheTemplates: Boolean, loader: TemplateLoader): (ViewModel) -> String {
+        val dust = Dust(cacheTemplates = cacheTemplates, precache = precache, loader = loader)
+        
+        return fun(viewModel: ViewModel) =
+            dust.withTemplates { it.expandTemplate(viewModel.template(), viewModel,
+                onMissingTemplate = { throw ViewNotFound(viewModel) })
+            }
+    }
+}
