26bde1d92f1288d0e3a6b83138f45b9afd576c62
David Denton <denton.david@gmail.com>
2017-09-22 18:34:42 +0100

    First stab at Record Traffic. API might need a little work

--- http4k-core/src/main/kotlin/org/http4k/filter/RecordTraffic.kt
+++ http4k-core/src/main/kotlin/org/http4k/filter/RecordTraffic.kt
@@ -8,125 +8,145 @@ import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.parse
 import org.http4k.core.then
+import org.http4k.filter.TrafficCache.Disk
+import org.http4k.filter.TrafficCache.Memory
 import java.io.File
 import java.util.*
 
-object ServeCachedTrafficFrom {
-    object Disk {
-        operator fun invoke(baseDir: String = ".") = Filter { next ->
-            {
-                it.toFile(baseDir.toBaseFolder()).run {
-                    if (exists()) Response.parse(String(readBytes())) else next(it)
-                }
-            }
-        }
-    }
+interface TrafficStorage {
+    operator fun set(request: Request, response: Response)
+}
 
-    object Memory {
-        operator fun invoke(cache: Map<Request, Response>) = Filter { next ->
-            {
-                cache[it] ?: next(it)
-            }
-        }
-    }
+interface TrafficReplay {
+    operator fun get(request: Request): Response?
 }
 
 object Replay {
-    object RequestsFrom {
-        object Disk {
-            operator fun invoke(baseDir: String = ".",
-                                shouldReplay: (Request) -> Boolean = { true }): Iterator<Request> =
-                read(baseDir, Request.Companion::parse, shouldReplay, "request.txt")
-        }
-    }
-
-    private fun <T : HttpMessage> read(baseDir: String,
-                                       convert: (String) -> T,
-                                       shouldReplay: (T) -> Boolean,
-                                       file: String): Iterator<T> =
-        baseDir.toBaseFolder()
-            .listFiles()
-            .map { File(it, file).run { convert(String(readBytes())) } }
-            .filter(shouldReplay)
-            .iterator()
-
-
-    object ResponsesFrom {
-        object Disk {
-            operator fun invoke(baseDir: String = ".",
-                                shouldReplay: (Response) -> Boolean = { true }): HttpHandler {
-                val responses = read(baseDir, Response.Companion::parse, shouldReplay, "response.txt")
-                return {
-                    if (responses.hasNext()) responses.next() else Response(Status.SERVICE_UNAVAILABLE)
-                }
-            }
-        }
-    }
-
-}
-
-object RecordTrafficTo {
-    object Disk {
-        operator fun invoke(baseDir: String = ".",
-                            shouldSave: (HttpMessage) -> Boolean = { true },
-                            id: () -> String = { System.currentTimeMillis().toString() }): Filter =
-            Filter { next ->
-                {
-                    if (shouldSave(it)) it.writeTo(File(baseDir.toBaseFolder(), id()))
-                    next(it).apply {
-                        if (shouldSave(this)) writeTo(File(baseDir.toBaseFolder(), id()))
-                    }
-                }
-            }
-    }
-
-    object Memory {
-        operator fun invoke(list: MutableList<Pair<Request, Response>>, shouldSave: (HttpMessage) -> Boolean = { true }): Filter =
-            Filter { next ->
-                { req ->
-                    next(req).apply {
-                        if (shouldSave(req) || shouldSave(this)) list += req to this
-                    }
-                }
-            }
-
-    }
-}
-
-object CacheTrafficTo {
-
-    object Disk {
-        operator fun invoke(baseDir: String = ".", shouldSave: (HttpMessage) -> Boolean = { true }) = Filter { next ->
+    fun requestsFrom(queue: TrafficQueue): Iterator<Request> = queue.requests()
+    fun responsesFrom(queue: TrafficQueue): HttpHandler =
+        queue.responses().run {
             {
-                val requestFolder = File(File(baseDir.toBaseFolder(), it.uri.path), String(Base64.getEncoder().encode(it.toString().toByteArray())))
-
-                if (shouldSave(it)) it.writeTo(requestFolder)
-
-                next(it).apply {
-                    if (shouldSave(this)) this.writeTo(requestFolder)
-                }
+                if (hasNext()) next()
+                else Response(Status.SERVICE_UNAVAILABLE.description("no more traffic to replay"))
             }
+        }
+}
+
+interface TrafficQueue : TrafficStorage {
+    fun requests(): Iterator<Request>
+    fun responses(): Iterator<Response>
+
+    class Disk(private val baseDir: String = ".",
+               private val shouldReplay: (HttpMessage) -> Boolean = { true },
+               private val id: () -> String = { System.currentTimeMillis().toString() + UUID.randomUUID().toString() }) : TrafficQueue {
+        override fun requests(): Iterator<Request> = read(baseDir, Request.Companion::parse, shouldReplay, "request.txt")
+
+        override fun responses(): Iterator<Response> = read(baseDir, Response.Companion::parse, shouldReplay, "response.txt")
+
+        override fun set(request: Request, response: Response) {
+
+//            if (shouldSave(it)) it.writeTo(File(baseDir.toBaseFolder(), id()))
+//            next(it).apply {
+//                if (shouldSave(this)) writeTo(File(baseDir.toBaseFolder(), id()))
+//            }
+        }
+
+        private fun <T : HttpMessage> read(baseDir: String,
+                                           convert: (String) -> T,
+                                           shouldReplay: (T) -> Boolean,
+                                           file: String): Iterator<T> =
+            baseDir.toBaseFolder()
+                .listFiles()
+                .map { File(it, file).run { convert(String(readBytes())) } }
+                .filter(shouldReplay)
+                .iterator()
+    }
+
+    class Memory(private val queue: MutableList<Pair<Request, Response>>,
+                 private val shouldReplay: (HttpMessage) -> Boolean = { true }) : TrafficQueue {
+        override fun set(request: Request, response: Response) {
+            queue += request to response
+        }
+
+        override fun requests(): Iterator<Request> = queue.filter { shouldReplay(it.first) }.map { it.first }.iterator()
+        override fun responses(): Iterator<Response> = queue.filter { shouldReplay(it.second) }.map { it.second }.iterator()
+    }
+}
+
+interface TrafficCache : TrafficStorage, TrafficReplay {
+
+    class Disk(private val baseDir: String = ".",
+               private val shouldStore: (HttpMessage) -> Boolean = { true }) : TrafficCache {
+        override fun get(request: Request): Response? =
+            request.toFile(baseDir.toBaseFolder()).run {
+                if (exists()) Response.parse(String(readBytes())) else null
+            }
+
+        override fun set(request: Request, response: Response) {
+            val requestFolder = File(File(baseDir.toBaseFolder(), request.uri.path), String(Base64.getEncoder().encode(request.toString().toByteArray())))
+            if (shouldStore(request)) request.writeTo(requestFolder)
+            if (shouldStore(response)) request.writeTo(requestFolder)
         }
     }
 
-    object Memory {
-        operator fun invoke(cache: MutableMap<Request, Response>, shouldSave: (HttpMessage) -> Boolean = { true }) = Filter { next ->
-            { req: Request ->
-                next(req).apply {
-                    if (shouldSave(req) || shouldSave(this)) cache[req] = this
-                }
+    class Memory(private val shouldStore: (Request) -> Boolean = { true },
+                 private val cache: MutableMap<Request, Response> = linkedMapOf()) : TrafficCache {
+        override fun get(request: Request): Response? = cache[request]
+
+        override fun set(request: Request, response: Response) {
+            if (shouldStore(request)) cache[request] = response
+        }
+    }
+}
+
+object ServeCachedTraffic {
+    fun from(store: TrafficCache): Filter = Filter { next ->
+        {
+            store[it] ?: next(it)
+        }
+    }
+}
+
+object RecordTraffic {
+    fun to(store: TrafficStorage): Filter = Filter { next ->
+        {
+            next(it).apply {
+                store[it] = this
             }
         }
     }
 }
 
-object SimpleCachingFrom {
-    fun Disk(baseDir: String = ".", shouldSave: (HttpMessage) -> Boolean = { true }): Filter =
-        ServeCachedTrafficFrom.Disk(baseDir).then(CacheTrafficTo.Disk(baseDir, shouldSave))
+//object CacheTrafficTo {
+//
+//    object Disk {
+//        operator fun invoke(baseDir: String = ".", shouldSave: (HttpMessage) -> Boolean = { true }) = Filter { next ->
+//            {
+//                val requestFolder = File(File(baseDir.toBaseFolder(), it.uri.path), String(Base64.getEncoder().encode(it.toString().toByteArray())))
+//
+//                if (shouldSave(it)) it.writeTo(requestFolder)
+//
+//                next(it).apply {
+//                    if (shouldSave(this)) this.writeTo(requestFolder)
+//                }
+//            }
+//        }
+//    }
+//
+//    object Memory {
+//        operator fun invoke(cache: MutableMap<Request, Response>, shouldSave: (HttpMessage) -> Boolean = { true }) = Filter { next ->
+//            { req: Request ->
+//                next(req).apply {
+//                    if (shouldSave(req) || shouldSave(this)) cache[req] = this
+//                }
+//            }
+//        }
+//    }
+//}
 
-    fun Memory(shouldSave: (HttpMessage) -> Boolean = { true }): Filter {
-        val cache = linkedMapOf<Request, Response>()
-        return ServeCachedTrafficFrom.Memory(cache).then(CacheTrafficTo.Memory(cache, shouldSave))
+object SimpleCaching {
+    fun from(store: TrafficCache): Filter {
+        return ServeCachedTraffic.from(store).then(RecordTraffic.to(store))
     }
 }
 
@@ -142,3 +162,15 @@ private fun String.toBaseFolder(): File = File(if (isEmpty()) "." else this)
 
 private fun HttpMessage.toFile(folder: File): File = File(folder, if (this is Request) "request.txt" else "response.txt")
 
+fun main(args: Array<String>) {
+    SimpleCaching.from(Disk())
+    SimpleCaching.from(Memory())
+    RecordTraffic.to(Disk())
+    RecordTraffic.to(TrafficQueue.Memory(mutableListOf()))
+
+    Replay.requestsFrom(TrafficQueue.Disk())
+    Replay.responsesFrom(TrafficQueue.Disk())
+
+    Replay.requestsFrom(TrafficQueue.Memory(mutableListOf()))
+    Replay.responsesFrom(TrafficQueue.Memory(mutableListOf()))
+}
