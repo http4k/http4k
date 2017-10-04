c33d3e8753b91563d4b83cd98de53bf52aa5ad09
David Denton <denton.david@gmail.com>
2017-10-04 10:51:06 +0100

    remove rawPart

---
+++ http4k-multipart/src/main/kotlin/org/http4k/multipart/MultipartForm.kt
@@ -0,0 +1,11 @@
+package org.http4k.multipart
+
+import org.http4k.multipart.part.Part
+
+data class MultipartForm(val parts: List<Part>)
+
+fun main(args: Array<String>) {
+
+    val builder = ValidMultipartFormBuilder("bob".toByteArray())
+//    builder.rawPart()
+}
