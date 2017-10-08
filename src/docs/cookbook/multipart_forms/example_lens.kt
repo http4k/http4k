1163b55a0d2c9dfa0bccc580a8cc7390d36adfb3
David Denton <denton.david@gmail.com>
2017-10-08 14:58:27 +0100

    documentation for multipart

---
+++ src/docs/cookbook/multipart_forms/example_lens.kt
@@ -0,0 +1,47 @@
+package cookbook.multipart_forms
+
+import org.http4k.client.ApacheClient
+import org.http4k.core.Body
+import org.http4k.core.ContentType
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.with
+import org.http4k.lens.MultipartForm
+import org.http4k.lens.MultipartFormField
+import org.http4k.lens.MultipartFormFile
+import org.http4k.lens.Validator
+import org.http4k.lens.multipartForm
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+
+data class Name(val value: String)
+
+fun main(args: Array<String>) {
+    // define fields using the standard lens syntax
+    val nameField = MultipartFormField.map(::Name, Name::value).required("name")
+    val imageFile = MultipartFormFile.optional("image")
+
+    // add fields to a form definition, along with a validator
+    val strictFormBody = Body.multipartForm(Validator.Strict, nameField, imageFile).toLens()
+
+    val server = { r: Request ->
+        // to extract the contents, we first extract the form and then extract the fields from it using the lenses
+        val validForm = strictFormBody.extract(r)
+        println(nameField.extract(validForm))
+        println(imageFile.extract(validForm))
+
+        Response(Status.OK)
+    }.asServer(SunHttp(8000)).start()
+
+    // creating valid form using "with()" and setting it onto the request
+    val multipartform = MultipartForm().with(
+        nameField of Name("rita"),
+        imageFile of MultipartFormFile("image.txt", ContentType.OCTET_STREAM, "somebinarycontent".byteInputStream()))
+    val validRequest = Request(Method.POST, "http://localhost:8000").with(strictFormBody of multipartform)
+
+    println(ApacheClient()(validRequest))
+
+    server.stop()
+}
---
+++ src/docs/cookbook/multipart_forms/index.md
@@ -0,0 +1,16 @@
+Multipart form support is provided on 2 levels:
+
+1. Through the creation of a `MultipartFormBody` which can be set on a `Request`.
+1. Using the Lens system, which adds the facility to define form fields in a typesafe way, and to validate form contents (in either a strict (400) or "feedback" mode).
+
+### Gradle setup
+```
+    compile group: "org.http4k", name: "http4k-core", version: "2.29.4"
+    compile group: "org.http4k", name: "http4k-multipart", version: "2.29.4"
+```
+
+### Standard (non-typesafe) API
+<script src="https://gist-it.appspot.com/https://github.com/http4k/http4k/blob/master/src/docs/cookbook/multipart_forms/example_standard.kt"></script>
+
+### Lens (typesafe, validating) API
+<script src="https://gist-it.appspot.com/https://github.com/http4k/http4k/blob/master/src/docs/cookbook/multipart_forms/example_lens.kt"></script>
