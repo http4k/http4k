0cb511d53e596906e2647be3775666afc6a0e20a
David Denton <denton.david@gmail.com>
2017-11-18 11:32:29 +0000

    meta dsl

--- http4k-contract/src/main/kotlin/org/http4k/contract/routeMeta.kt
+++ http4k-contract/src/main/kotlin/org/http4k/contract/routeMeta.kt
@@ -1,6 +1,9 @@
 package org.http4k.contract
 
+import org.http4k.core.Body
 import org.http4k.core.ContentType
+import org.http4k.core.ContentType.Companion.APPLICATION_XML
+import org.http4k.core.ContentType.Companion.TEXT_PLAIN
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
@@ -8,6 +11,7 @@ import org.http4k.core.Status
 import org.http4k.core.with
 import org.http4k.lens.BiDiBodyLens
 import org.http4k.lens.Header
+import org.http4k.lens.string
 
 data class Tag(val name: String, val description: String? = null)
 
@@ -38,4 +42,47 @@ data class RouteMeta(val summary: String,
     fun producing(vararg new: ContentType) = copy(produces = produces.plus(new))
     fun consuming(vararg new: ContentType) = copy(consumes = consumes.plus(new))
 
+}
+
+class MetaDsl {
+    var summary: String = "<unknown>"
+    var description: String? = null
+    var request: Request? = null
+    val tags: MutableSet<Tag> = mutableSetOf()
+    val produces: MutableSet<ContentType> = mutableSetOf()
+    val consumes: MutableSet<ContentType> = mutableSetOf()
+    internal val responses: MutableMap<Status, Pair<String, Response>> = mutableMapOf()
+
+    @JvmName("returningResponse")
+    fun returning(new: Pair<String, Response>) {
+        produces.plus(Header.Common.CONTENT_TYPE(new.second)?.let { listOf(it) } ?: emptyList())
+        responses += responses.plus(new.second.status to new)
+    }
+
+    @JvmName("returningStatus")
+    fun returning(new: Pair<String, Status>) = returning(new.first to Response(new.second))
+
+    @JvmName("returningStatus")
+    fun returning(new: Status) = returning("" to Response(new))
+
+    fun <T> receiving(new: Pair<BiDiBodyLens<T>, T>) {
+        request = Request(GET, "").with(new.first of new.second)
+    }
+}
+
+fun meta(fn: MetaDsl.() -> Unit): MetaDsl = MetaDsl().apply(fn).apply {
+    RouteMeta(summary, description, request, tags, produces, consumes, responses)
+}
+
+fun main(args: Array<String>) {
+    meta {
+        summary = "summary"
+        description = "description"
+        request = Request(GET, "")
+        produces += APPLICATION_XML
+        tags += Tag("name", "description")
+        consumes += ContentType.TEXT_PLAIN
+        returning("ok" to Status.OK)
+        receiving(Body.string(TEXT_PLAIN).toLens() to "")
+    }
 }
