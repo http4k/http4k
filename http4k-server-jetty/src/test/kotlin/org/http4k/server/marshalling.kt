b562c47b50b3c702ccf5cf4a4a55ecd1f365fe23
David Denton <denton.david@gmail.com>
2017-11-26 11:00:13 +0000

    moar websockets..

--- http4k-server-jetty/src/test/kotlin/org/http4k/server/marshalling.kt
+++ http4k-server-jetty/src/test/kotlin/org/http4k/server/marshalling.kt
@@ -8,7 +8,7 @@ data class Wrapper(val v: Int)
 fun main(args: Array<String>) {
     val session = WsSession()
 
-    val body = WsMessage.string().map({ Wrapper(it.toInt()) }, { it.v.toString() }).toLens()
+    val body = WsMessage.string().map { Wrapper(it.toInt()) }.toLens()
 
     session(WsMessage("1"))
     session(WsMessage("2"))
