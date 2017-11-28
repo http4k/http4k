020f9e5c18251ee9c1bdb1799d8e2771130e9e9b
David Denton <denton.david@gmail.com>
2017-11-28 11:48:44 +0000

    tweaks to internal

--- http4k-core/src/main/kotlin/org/http4k/routing/internal.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/internal.kt
@@ -58,34 +58,34 @@ data class StaticRoutingHttpHandler(private val pathSegments: String,
 
     override fun match(request: Request): HttpHandler? = invoke(request).let { if (it.status != NOT_FOUND) { _: Request -> it } else null }
 
-    private val handler = filter.then(ResourceLoadingHandler(pathSegments, resourceLoader, extraPairs))
+    private val httpHandler = filter.then(ResourceLoadingHandler(pathSegments, resourceLoader, extraPairs))
 
-    override fun invoke(req: Request): Response = handler(req)
+    override fun invoke(req: Request): Response = httpHandler(req)
 }
 
-data class TemplatingRoutingWsHandler(val template: UriTemplate,
-                                      val router: WsConsumer) : RoutingWsHandler {
-    override operator fun invoke(request: Request): WsConsumer? = if (template.matches(request.uri.path)) router else null
-
-    override fun withBasePath(new: String): TemplatingRoutingWsHandler = copy(template = UriTemplate.from("$new/$template"))
-}
-
-private fun Request.withUriTemplate(uriTemplate: UriTemplate): Request = header("x-uri-template", uriTemplate.toString())
-
-data class TemplateRoutingHttpHandler(val method: Method?,
-                                      val template: UriTemplate,
-                                      val handler: HttpHandler) : RoutingHttpHandler {
+data class TemplateRoutingHttpHandler(private val method: Method?,
+                                      private val template: UriTemplate,
+                                      private val httpHandler: HttpHandler) : RoutingHttpHandler {
     override fun match(request: Request): HttpHandler? =
         if (template.matches(request.uri.path) && (method == null || method == request.method))
-            { r: Request -> handler(r.withUriTemplate(template)) }
+            { r: Request -> httpHandler(r.withUriTemplate(template)) }
         else null
 
     override fun invoke(request: Request): Response =
         match(request)?.invoke(request) ?: Response(NOT_FOUND.description("Route not found"))
 
-    override fun withFilter(new: Filter): RoutingHttpHandler = copy(handler = new.then(handler))
+    override fun withFilter(new: Filter): RoutingHttpHandler = copy(httpHandler = new.then(httpHandler))
 
     override fun withBasePath(new: String): RoutingHttpHandler = copy(template = UriTemplate.from("$new/$template"))
 }
+
+data class TemplatingRoutingWsHandler(private val template: UriTemplate,
+                                      private val consumer: WsConsumer) : RoutingWsHandler {
+    override operator fun invoke(request: Request): WsConsumer? = if (template.matches(request.uri.path)) consumer else null
+
+    override fun withBasePath(new: String): TemplatingRoutingWsHandler = copy(template = UriTemplate.from("$new/$template"))
+}
+
+private fun Request.withUriTemplate(uriTemplate: UriTemplate): Request = header("x-uri-template", uriTemplate.toString())
 
 internal fun Request.uriTemplate(): UriTemplate = headers.findSingle("x-uri-template")?.let { UriTemplate.from(it) } ?: throw IllegalStateException("x-uri-template header not present in the request")
