165e8f56a834c0c7f32549114d66d24ae0fc38f2
David Denton <denton.david@gmail.com>
2017-11-28 10:44:38 +0000

    more chat server examples

--- src/docs/cookbook/websockets/chatServer.kt
+++ src/docs/cookbook/websockets/chatServer.kt
@@ -1,11 +1,7 @@
 package cookbook.websockets
 
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.ResourceLoader
 import org.http4k.routing.bind
-import org.http4k.routing.routes
 import org.http4k.routing.static
 import org.http4k.routing.websockets
 import org.http4k.server.Jetty
@@ -15,16 +11,10 @@ import org.http4k.websocket.WsMessage
 import java.util.concurrent.ConcurrentHashMap
 
 fun main(args: Array<String>) {
-
-    val app = routes("/ping" bind { _: Request -> Response(OK) },
-        static(ResourceLoader.Directory("src/test/resources/cookbook/websockets"))
-    )
-
     val messages = mutableListOf<String>()
     val participants = ConcurrentHashMap<String, WebSocket>()
 
     fun addMessage(new: String) {
-        println("received $new")
         messages.add(new)
         participants.values.forEach { it.send(WsMessage(new)) }
     }
@@ -43,6 +33,7 @@ fun main(args: Array<String>) {
         }
     }
 
+    val app = static(ResourceLoader.Directory("src/test/resources/cookbook/websockets"))
     val websockets = websockets("/ws" bind ::newConnection)
 
     (app to websockets).asServer(Jetty(9001)).start()
