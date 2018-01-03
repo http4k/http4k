3d07c6c392c2d2f27de15780d42f891a413c707d
Andrew Dowell <adowellphoto@gmail.com>
2018-01-03 19:22:43 +0000

    #13 Create extension methods for Response to add caching headers (#59)
    * #13 [Hacktoberfest]: Add Cache-Control extension functions to Response
    
    * #13 [Hacktoberfest]: add remaining Cache-Control headers
    
    - no longer removes CachingHeader if already set - to allow chaining
    together (and shouldn't remove the header anyway)
    
    * Andy/Dave: remove unnecessary headers and introduce overwriting of
    
    - existing Cacheability headers
    
    * Andy/Dave: max-age uses Duration instead of seconds
    
    * Andy/Dave: stale-if-error & stale-while-revalidate uses Duration instead of seconds
    
    * Andy/Dave: use Cacheability for public/private only

---
+++ http4k-core/src/main/kotlin/org/http4k/core/ResponseCacheExt.kt
@@ -0,0 +1,46 @@
+package org.http4k.core
+
+import org.http4k.filter.MaxAgeTtl
+import org.http4k.filter.StaleIfErrorTtl
+import org.http4k.filter.StaleWhenRevalidateTtl
+import java.time.Duration
+
+fun Response.public() = addCacheability(Cacheability.public)
+
+fun Response.private() = addCacheability(Cacheability.private)
+
+fun Response.noCache() = addCacheability("no-cache")
+
+fun Response.onlyIfCached() = addCacheability("only-if-cached")
+
+fun Response.mustRevalidate() = addCacheability("must-revalidate")
+
+fun Response.noStore() = addCacheability("no-store")
+
+fun Response.maxAge(duration: Duration) = replaceHeader("Cache-Control", MaxAgeTtl(duration).replaceIn(header("Cache-Control")))
+
+fun Response.staleWhileRevalidate(duration: Duration) = replaceHeader("Cache-Control", StaleWhenRevalidateTtl(duration).replaceIn(header("Cache-Control")))
+
+fun Response.staleIfError(duration: Duration) = replaceHeader("Cache-Control", StaleIfErrorTtl(duration).replaceIn(header("Cache-Control")))
+
+private fun Response.addCacheability(value: String): Response =
+        replaceHeader("Cache-Control", value.ensureOnlyOnceIn(header("Cache-Control")))
+
+private fun Response.addCacheability(cacheability: Cacheability): Response =
+        replaceHeader("Cache-Control", cacheability(header("Cache-Control")))
+
+private enum class Cacheability {
+    public,
+    private;
+
+    operator fun invoke(currentValue: String?): String =
+            currentValue?.let {
+                val split = currentValue.split(",")
+                (listOf(name) + split
+                        .map(String::trim)
+                        .filterNot { values().map { it.name }.contains(it) }).joinToString(", ")
+            } ?: name
+}
+
+private fun String.ensureOnlyOnceIn(currentValue: String?): String =
+        currentValue?.split(",")?.map(String::trim)?.toSet()?.plus(this)?.joinToString(", ") ?: this
---
+++ http4k-core/src/test/kotlin/org/http4k/core/ResponseCacheExtTest.kt
@@ -0,0 +1,91 @@
+package org.http4k.core
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.junit.Test
+import java.time.Duration
+
+class ResponseCacheExtTest {
+
+    @Test
+    fun `adds public to response Cache-Control header`() {
+        val publicResponse = Response(Status.OK).public()
+
+        assertThat(publicResponse.header("Cache-Control"), equalTo("public"))
+    }
+
+    @Test
+    fun `adds private to response Cache-Control header`() {
+        val privateResponse = Response(Status.OK).private()
+
+        assertThat(privateResponse.header("Cache-Control"), equalTo("private"))
+    }
+
+    @Test
+    fun `adds no-cache to response Cache-Control header`() {
+        val noCacheResponse = Response(Status.OK).noCache()
+
+        assertThat(noCacheResponse.header("Cache-Control"), equalTo("no-cache"))
+    }
+
+    @Test
+    fun `adds only-if-cached to response Cache-Control header`() {
+        val onlyIfCachedResponse = Response(Status.OK).onlyIfCached()
+
+        assertThat(onlyIfCachedResponse.header("Cache-Control"), equalTo("only-if-cached"))
+    }
+
+    @Test
+    fun `adds must-revalidate to response Cache-Control header`() {
+        val mustRevalidateResponse = Response(Status.OK).mustRevalidate()
+
+        assertThat(mustRevalidateResponse.header("Cache-Control"), equalTo("must-revalidate"))
+    }
+
+    @Test
+    fun `adds no-store to response Cache-Control header`() {
+        val noStoreResponse = Response(Status.OK).noStore()
+
+        assertThat(noStoreResponse.header("Cache-Control"), equalTo("no-store"))
+    }
+
+    @Test
+    fun `adds max-age to response Cache-Control header`() {
+        val maxAgeResponse = Response(Status.OK).maxAge(Duration.ofMinutes(1))
+
+        assertThat(maxAgeResponse.header("Cache-Control"), equalTo("max-age=60"))
+    }
+
+    @Test
+    fun `adds stale-while-revalidate to response Cache-Control header`() {
+        val maxAgeResponse = Response(Status.OK).staleWhileRevalidate(Duration.ofMinutes(1))
+
+        assertThat(maxAgeResponse.header("Cache-Control"), equalTo("stale-while-revalidate=60"))
+    }
+
+    @Test
+    fun `adds stale-if-error to response Cache-Control header`() {
+        val maxAgeResponse = Response(Status.OK).staleIfError(Duration.ofMinutes(1))
+
+        assertThat(maxAgeResponse.header("Cache-Control"), equalTo("stale-if-error=60"))
+    }
+
+    @Test
+    fun `can chain together multiple calls to add to the header`() {
+        val chainedResponse = Response(Status.OK).public().private().maxAge(Duration.ofMinutes(1)).maxAge(Duration.ofMinutes(2))
+
+        assertThat(chainedResponse.header("Cache-Control"), equalTo("private, max-age=120"))
+    }
+
+    @Test
+    fun `should overwrite existing headers with new values`() {
+        val chainedResponse = Response(Status.OK)
+                .public().private()
+                .noCache().noCache()
+                .noStore().noStore()
+                .maxAge(Duration.ofMinutes(1)).maxAge(Duration.ofMinutes(2))
+                .staleIfError(Duration.ofMinutes(2)).staleIfError(Duration.ofMinutes(2))
+
+        assertThat(chainedResponse.header("Cache-Control"), equalTo("private, no-cache, no-store, max-age=120, stale-if-error=120"))
+    }
+}
