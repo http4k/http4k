865b9d2a82eed5af6dae8db8d793e676d3e792ed
David Denton <denton.david@gmail.com>
2018-01-24 09:09:06 +0000

    convert function to extension

--- http4k-contract/src/main/kotlin/org/http4k/util/JsonSchema.kt
+++ http4k-contract/src/main/kotlin/org/http4k/util/JsonSchema.kt
@@ -19,27 +19,23 @@ class JsonToJsonSchema<ROOT : NODE, NODE : Any>(private val json: Json<ROOT, NOD
         when (json.typeOf(input.node)) {
             JsonType.Object -> objectSchema(input)
             JsonType.Array -> arraySchema(input)
-            JsonType.String -> JsonSchema(paramTypeSchema(StringParam), input.definitions)
+            JsonType.String -> JsonSchema(StringParam.schema(), input.definitions)
             JsonType.Number -> numberSchema(input)
-            JsonType.Boolean -> JsonSchema(paramTypeSchema(BooleanParam), input.definitions)
+            JsonType.Boolean -> JsonSchema(BooleanParam.schema(), input.definitions)
             JsonType.Null -> throw IllegalSchemaException("Cannot use a null value in a schema!")
             else -> throw IllegalSchemaException("unknown type")
         }
 
-    private fun paramTypeSchema(paramMeta: ParamMeta): NODE = json.obj("type" to json.string(paramMeta.value))
-
     private fun numberSchema(input: JsonSchema<NODE>): JsonSchema<NODE> =
-        JsonSchema(paramTypeSchema(if (json.text(input.node).contains(".")) NumberParam else IntegerParam), input.definitions)
+        JsonSchema((if (json.text(input.node).contains(".")) NumberParam else IntegerParam).schema(), input.definitions)
 
     private fun arraySchema(input: JsonSchema<NODE>): JsonSchema<NODE> {
-        val (node, definitions) = json.elements(input.node).toList().firstOrNull()?.let { toSchema(JsonSchema(it, input.definitions)) } ?: throw
-        IllegalSchemaException("Cannot use an empty list to generate a schema!")
+        val (node, definitions) = json.elements(input.node).toList().firstOrNull()?.let { toSchema(JsonSchema(it, input.definitions)) } ?: throw IllegalSchemaException("Cannot use an empty list to generate a schema!")
         return JsonSchema(json.obj("type" to json.string("array"), "items" to node), definitions)
     }
 
     private fun objectSchema(input: JsonSchema<NODE>): JsonSchema<NODE> {
-        val (fields, subDefinitions) = json.fields(input.node).fold(listOf<Pair<String, NODE>>() to input.definitions, {
-            (memoFields, memoDefinitions), (first, second) ->
+        val (fields, subDefinitions) = json.fields(input.node).fold(listOf<Pair<String, NODE>>() to input.definitions, { (memoFields, memoDefinitions), (first, second) ->
             val next = toSchema(JsonSchema(second, memoDefinitions))
             memoFields.plus(first to next.node) to next.definitions
         })
@@ -50,6 +46,6 @@ class JsonToJsonSchema<ROOT : NODE, NODE : Any>(private val json: Json<ROOT, NOD
         return JsonSchema(json.obj("\$ref" to json.string("#/definitions/$definitionId")), allDefinitions)
     }
 
-
+    private fun ParamMeta.schema(): NODE = json.obj("type" to json.string(value))
 }
 
