91a4582c12375470fb9308c33a75852df76e6b48
Elifarley <elifarley@gmail.com>
2018-03-02 10:09:13 -0300

    Http4k JsonIter JSON support - http://jsoniter.com/

---
+++ http4k-format-jsoniter/src/test/kotlin/org/http4k/format/JsonIterTest.kt
@@ -0,0 +1,44 @@
+package org.http4k.format
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.core.Body
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.with
+import org.junit.Ignore
+import org.junit.Test
+
+class JsonIterAutoTest : AutoMarshallingContract(JsonIter) {
+
+    @Test
+    fun `roundtrip arbitary object to and from JSON element`() {
+        val obj = ArbObject("hello", ArbObject("world", null, listOf(1), true), emptyList(), false)
+        val out = JsonIter.asJsonObject(obj)
+        assertThat(JsonIter.asA(out, ArbObject::class), equalTo(obj))
+    }
+
+    @Test
+    fun `roundtrip list of arbitary objects to and from body`() {
+        val body = Body.auto<Array<ArbObject>>().toLens()
+
+        val obj = ArbObject("hello", ArbObject("world", null, listOf(1), true), emptyList(), false)
+
+        assertThat(body(Response(Status.OK).with(body of arrayOf(obj))).asList(), equalTo(arrayOf(obj).asList()))
+    }
+
+    @Test
+    @Ignore("JsonIter does not currently have Kotlin class support")
+    override fun `fails decoding when a required value is null`() {
+    }
+
+}
+
+class JsonIterTest : JsonContract<JsonElement, JsonElement>(JsonIter) {
+    override val prettyString = """{
+  "hello": "world"
+}"""
+}
+
+class JsonIterJsonErrorResponseRendererContractTest : JsonErrorResponseRendererContract<JsonElement, JsonElement>(JsonIter)
+class JsonIterGenerateDataClassesTest : GenerateDataClassesContract<JsonElement, JsonElement>(JsonIter)
