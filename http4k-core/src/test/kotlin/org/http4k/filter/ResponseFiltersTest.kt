35ae5347d40be9410e9abf84c0201312e021c5a2
David Denton <denton.david@gmail.com>
2018-03-14 09:12:51 +0000

    added test for response filters to prove that response filters don't pick up the routingGroup if present outside of a routes block.

--- http4k-core/src/test/kotlin/org/http4k/filter/ResponseFiltersTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/filter/ResponseFiltersTest.kt
@@ -6,25 +6,28 @@ import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.should.shouldMatch
 import junit.framework.TestCase.assertTrue
 import org.http4k.core.HttpTransaction
-import org.http4k.core.Method
+import org.http4k.core.HttpTransaction.Companion.ROUTING_GROUP_LABEL
+import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.core.toBody
 import org.http4k.core.with
-import org.http4k.core.HttpTransaction.Companion.ROUTING_GROUP_LABEL
 import org.http4k.filter.ResponseFilters.ReportHttpTransaction
 import org.http4k.filter.ResponseFilters.ReportLatency
 import org.http4k.hamkrest.hasBody
 import org.http4k.hamkrest.hasHeader
 import org.http4k.lens.Header
+import org.http4k.routing.bind
+import org.http4k.routing.routes
 import org.http4k.toHttpHandler
 import org.http4k.util.TickingClock
 import org.junit.Test
 import java.time.Clock
 import java.time.Clock.fixed
-import java.time.Duration
+import java.time.Duration.ZERO
+import java.time.Duration.ofSeconds
 import java.time.Instant.EPOCH
 import java.time.ZoneId.systemDefault
 
@@ -34,21 +37,21 @@ class ResponseFiltersTest {
     fun `tap passes response through to function`() {
         var called = false
         val response = Response(OK)
-        ResponseFilters.Tap { called = true; assertThat(it, equalTo(response)) }.then(response.toHttpHandler())(Request(Method.GET, ""))
+        ResponseFilters.Tap { called = true; assertThat(it, equalTo(response)) }.then(response.toHttpHandler())(Request(GET, ""))
         assertTrue(called)
     }
 
     @Test
     fun `reporting latency for request`() {
         var called = false
-        val request = Request(Method.GET, "")
+        val request = Request(GET, "")
         val response = Response(OK)
 
         ReportLatency(TickingClock, { req, resp, duration ->
             called = true
             assertThat(req, equalTo(request))
             assertThat(resp, equalTo(response))
-            assertThat(duration, equalTo(Duration.ofSeconds(1)))
+            assertThat(duration, equalTo(ofSeconds(1)))
         }).then { response }(request)
 
         assertTrue(called)
@@ -58,7 +61,7 @@ class ResponseFiltersTest {
     fun `gzip response and adds gzip content encoding if the request has accept-encoding of gzip`() {
         fun assertSupportsZipping(body: String) {
             val zipped = ResponseFilters.GZip().then { Response(OK).body(body) }
-            zipped(Request(Method.GET, "").header("accept-encoding", "gzip")) shouldMatch hasBody(equalTo(body.toBody().gzipped())).and(hasHeader("content-encoding", "gzip"))
+            zipped(Request(GET, "").header("accept-encoding", "gzip")) shouldMatch hasBody(equalTo(body.toBody().gzipped())).and(hasHeader("content-encoding", "gzip"))
         }
         assertSupportsZipping("foobar")
         assertSupportsZipping("")
@@ -68,7 +71,7 @@ class ResponseFiltersTest {
     fun `gunzip response which has gzip content encoding`() {
         fun assertSupportsUnzipping(body: String) {
             val handler = ResponseFilters.GunZip().then { Response(OK).header("content-encoding", "gzip").body(body.toBody().gzipped()) }
-            handler(Request(Method.GET, "")) shouldMatch hasBody(body).and(hasHeader("content-encoding", "gzip"))
+            handler(Request(GET, "")) shouldMatch hasBody(body).and(hasHeader("content-encoding", "gzip"))
         }
         assertSupportsUnzipping("foobar")
         assertSupportsUnzipping("")
@@ -78,7 +81,7 @@ class ResponseFiltersTest {
     fun `passthrough gunzip response with no content encoding when request has no accept-encoding of gzip`() {
         val body = "foobar"
         val handler = ResponseFilters.GunZip().then { Response(OK).header("content-encoding", "zip").body(body) }
-        handler(Request(Method.GET, "")) shouldMatch hasBody(body).and(!hasHeader("content-encoding", "gzip"))
+        handler(Request(GET, "")) shouldMatch hasBody(body).and(!hasHeader("content-encoding", "gzip"))
     }
 
     @Test
@@ -87,7 +90,7 @@ class ResponseFiltersTest {
         val filter = ResponseFilters.ReportRouteLatency(Clock.systemUTC(), { identity, _ -> called = identity })
         val handler = filter.then { Response(OK) }
 
-        handler(Request(Method.GET, ""))
+        handler(Request(GET, ""))
 
         assertThat(called, equalTo("GET.UNMAPPED.2xx.200"))
     }
@@ -98,7 +101,7 @@ class ResponseFiltersTest {
         val filter = ResponseFilters.ReportRouteLatency(Clock.systemUTC(), { identity, _ -> called = identity })
         val handler = filter.then { Response(OK) }
 
-        handler(Request(Method.GET, "").with(Header.X_URI_TEMPLATE of "/path/dir/someFile.html"))
+        handler(Request(GET, "").with(Header.X_URI_TEMPLATE of "/path/dir/someFile.html"))
 
         assertThat(called, equalTo("GET._path_dir_someFile_html.2xx.200"))
     }
@@ -112,22 +115,35 @@ class ResponseFiltersTest {
 
         val handler = filter.then { Response(OK) }
 
-        handler(Request(Method.GET, ""))
+        handler(Request(GET, ""))
 
-        assertThat(transaction, equalTo(HttpTransaction(Request(Method.GET, ""), Response(OK), Duration.ZERO)))
+        assertThat(transaction, equalTo(HttpTransaction(Request(GET, ""), Response(OK), ZERO)))
     }
 
     @Test
     fun `reporting http transaction for known route`() {
         var transaction: HttpTransaction? = null
 
-        val filter = ReportHttpTransaction(fixed(EPOCH, systemDefault()))  { transaction = it }
+        val filter = ReportHttpTransaction(fixed(EPOCH, systemDefault())) { transaction = it }
 
         val handler = filter.then { Response(OK) }
 
-        val request = Request(Method.GET, "").with(Header.X_URI_TEMPLATE of "someValue")
+        val request = Request(GET, "").with(Header.X_URI_TEMPLATE of "someValue")
         handler(request)
 
-        assertThat(transaction, equalTo(HttpTransaction(request, Response(OK), Duration.ZERO, mapOf(ROUTING_GROUP_LABEL to "someValue"))))
+        assertThat(transaction, equalTo(HttpTransaction(request, Response(OK), ZERO, mapOf(ROUTING_GROUP_LABEL to "someValue"))))
+    }
+
+    @Test
+    fun `reporting http transaction for known route inside a routes block`() {
+        var transaction: HttpTransaction? = null
+
+        val filter = ReportHttpTransaction(fixed(EPOCH, systemDefault())) { transaction = it }
+
+        val handler = routes("/bob" bind GET to filter.then( { Response(OK) }))
+
+        handler(Request(GET, "/bob"))
+
+        assertThat(transaction, equalTo(HttpTransaction(Request(GET, "/bob").with(Header.X_URI_TEMPLATE of "bob"), Response(OK), ZERO, mapOf(ROUTING_GROUP_LABEL to "bob"))))
     }
 }
