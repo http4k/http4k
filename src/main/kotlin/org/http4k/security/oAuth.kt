b097d79683880c0da2852cc857f3f5ff241f1167
David Denton <denton.david@gmail.com>
2018-03-25 10:35:32 +0100

    upgrade kotlin + tweak

--- src/main/kotlin/org/http4k/security/oAuth.kt
+++ src/main/kotlin/org/http4k/security/oAuth.kt
@@ -32,60 +32,14 @@ data class OAuthConfig(
     val authUri = authBase.path(authPath)
 }
 
-class OAuth(client: HttpHandler,
-            private val clientConfig: OAuthConfig,
-            private val callbackUri: Uri,
-            private val scopes: List<String>,
-            private val generateCrsf: () -> String = { BigInteger(130, SecureRandom()).toString(32) },
-            private val generateNonce: () -> String = { UUID.randomUUID().toString() }) {
-
-    private val serviceCsrfName = "${clientConfig.serviceName}Csrf"
-    private val serviceAccessTokenName = "${clientConfig.serviceName}AccessToken"
-
-    val api = ClientFilters.SetHostFrom(clientConfig.apiBase).then(client)
-
-    val authFilter = object : Filter {
-        override fun invoke(next: HttpHandler): HttpHandler {
-            return { req ->
-                req.cookie(serviceAccessTokenName)?.let { next(req) } ?: redirectToAuth(req.uri)
-            }
-        }
-    }
-
-    val callback: HttpHandler = {
-        it.query("code")
-            ?.let { code -> it.cookie(serviceCsrfName)?.let { code to it } }
-            ?.let { (code, csrfCookie) ->
-                println("hit callback$ $code, $csrfCookie")
-                when {
-                    csrfCookie.value != it.query(serviceCsrfName) -> Response(FORBIDDEN).invalidateCookie(serviceCsrfName)
-                    else -> codeToAccessToken(code)
-                }
-            } ?: Response(FORBIDDEN)
-    }
-
-    /**
-     * POST www.googleapis.com/oauth2/v4/token
-//    Content-Type: application/x-www-form-urlencoded
-//    code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7&
-//    client_id=8819981768.apps.googleusercontent.com&
-//    client_secret={client_secret}&
-//    redirect_uri=https://oauth2-login-demo.example.com/code&
-//    grant_type=authorization_code
-     */
-
-    private fun codeToAccessToken(code: String): Response {
-        val accessToken = api(Request(POST, clientConfig.tokenPath)
-            .form("grant_type", "authorization_code")
-            .form("redirect_uri", callbackUri.toString())
-            .form("client_id", clientConfig.credentials.user)
-            .form("client_secret", clientConfig.credentials.password)
-            .form("code", code))
-
-        println(accessToken.bodyString())
-        return Response(Status.TEMPORARY_REDIRECT).cookie(Cookie(serviceAccessTokenName, accessToken.bodyString()))
-    }
-
+internal class AuthRedirectionFilter(
+    private val clientConfig: OAuthConfig,
+    private val serviceCsrfName: String,
+    private val serviceAccessTokenName: String,
+    private val callbackUri: Uri,
+    private val scopes: List<String>,
+    private val generateCrsf: () -> String,
+    private val generateNonce: () -> String) : Filter {
     private fun redirectToAuth(originalUri: Uri) = generateCrsf().let {
         Response(Status.TEMPORARY_REDIRECT).with(LOCATION of clientConfig.authUri
             .query("client_id", clientConfig.credentials.user)
@@ -96,6 +50,71 @@ class OAuth(client: HttpHandler,
             .query("nonce", generateNonce()))
             .cookie(Cookie(serviceCsrfName, it))
     }
+
+    override fun invoke(next: HttpHandler): HttpHandler {
+        return { req ->
+            req.cookie(serviceAccessTokenName)?.let { next(req) } ?: redirectToAuth(req.uri)
+        }
+    }
+}
+
+internal class AuthCallback(
+    private val api: HttpHandler,
+    private val clientConfig: OAuthConfig,
+    private val callbackUri: Uri,
+    private val csrfName: String,
+    private val accessTokenName: String
+) : HttpHandler {
+    /**
+     * POST www.googleapis.com/oauth2/v4/token
+    //    Content-Type: application/x-www-form-urlencoded
+    //    code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7&
+    //    client_id=8819981768.apps.googleusercontent.com&
+    //    client_secret={client_secret}&
+    //    redirect_uri=https://oauth2-login-demo.example.com/code&
+    //    grant_type=authorization_code
+     */
+
+    private fun codeToAccessToken(code: String): Response {
+        val accessTokenResponse = api(Request(POST, clientConfig.tokenPath)
+            .form("grant_type", "authorization_code")
+            .form("redirect_uri", callbackUri.toString())
+            .form("client_id", clientConfig.credentials.user)
+            .form("client_secret", clientConfig.credentials.password)
+            .form("code", code))
+
+        // this is wrong!
+        println(accessTokenResponse.bodyString())
+
+        return Response(Status.TEMPORARY_REDIRECT).cookie(Cookie(accessTokenName, accessTokenResponse.bodyString()))
+    }
+
+    override fun invoke(p1: Request) = p1.query("code")
+        ?.let { code -> p1.cookie(csrfName)?.let { code to it } }
+        ?.let { (code, csrfCookie) ->
+            println("hit callback$ $code, $csrfCookie")
+            when {
+                csrfCookie.value != p1.query(csrfName) -> Response(FORBIDDEN).invalidateCookie(csrfName)
+                else -> codeToAccessToken(code)
+            }
+        } ?: Response(FORBIDDEN)
+}
+
+class OAuth(client: HttpHandler,
+            clientConfig: OAuthConfig,
+            callbackUri: Uri,
+            scopes: List<String>,
+            generateCrsf: () -> String = { BigInteger(130, SecureRandom()).toString(32) },
+            generateNonce: () -> String = { UUID.randomUUID().toString() }) {
+
+    private val csrfName = "${clientConfig.serviceName}Csrf"
+    private val accessTokenName = "${clientConfig.serviceName}AccessToken"
+
+    val api = ClientFilters.SetHostFrom(clientConfig.apiBase).then(client)
+
+    val authFilter: Filter = AuthRedirectionFilter(clientConfig, csrfName, accessTokenName, callbackUri, scopes, generateCrsf, generateNonce)
+
+    val callback: HttpHandler = AuthCallback(api, clientConfig, callbackUri, csrfName, accessTokenName)
 
     companion object
 }
