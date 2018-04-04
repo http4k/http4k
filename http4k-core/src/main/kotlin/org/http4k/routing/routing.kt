2016f0e0be4d420a96d7b3facb29e7e634bfb858
Ivan Sanchez <ivan@gourame.com>
2018-04-04 20:52:53 +0100

    Preserve routing information on request/response manipulation

--- http4k-core/src/main/kotlin/org/http4k/routing/routing.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/routing.kt
@@ -1,14 +1,18 @@
 package org.http4k.routing
 
+import org.http4k.core.Body
 import org.http4k.core.ContentType
 import org.http4k.core.Filter
+import org.http4k.core.Headers
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Uri
 import org.http4k.core.UriTemplate
 import org.http4k.websocket.WsConsumer
 import org.http4k.websocket.WsHandler
+import java.io.InputStream
 
 interface Router {
     fun match(request: Request): HttpHandler?
@@ -35,7 +39,7 @@ fun websockets(vararg list: RoutingWsHandler): RoutingWsHandler = object : Routi
     override fun withBasePath(new: String): RoutingWsHandler = websockets(*list.map { it.withBasePath(new) }.toTypedArray())
 }
 
-fun Request.path(name: String): String? = when(this) {
+fun Request.path(name: String): String? = when (this) {
     is RoutedRequest -> xUriTemplate.extract(uri.path)[name]
     else -> throw IllegalStateException("Request was not routed, so no uri-template not present")
 }
@@ -62,12 +66,32 @@ infix fun String.bind(consumer: WsConsumer): RoutingWsHandler = TemplateRoutingW
 
 infix fun String.bind(wsHandler: RoutingWsHandler): RoutingWsHandler = wsHandler.withBasePath(this)
 
-class RoutedRequest(private val delegate: Request, val xUriTemplate: UriTemplate) : Request by delegate {
+data class RoutedRequest(private val delegate: Request, val xUriTemplate: UriTemplate) : Request by delegate {
     override fun equals(other: Any?): Boolean = delegate == other
 
     override fun hashCode(): Int = delegate.hashCode()
 
     override fun toString(): String = delegate.toString()
+
+    override fun method(method: Method): Request = RoutedRequest(delegate.method(method), xUriTemplate)
+
+    override fun uri(uri: Uri): Request = RoutedRequest(delegate.uri(uri), xUriTemplate)
+
+    override fun query(name: String, value: String): Request = RoutedRequest(delegate.query(name, value), xUriTemplate)
+
+    override fun header(name: String, value: String?): Request = RoutedRequest(delegate.header(name, value), xUriTemplate)
+
+    override fun headers(headers: Headers): Request = RoutedRequest(delegate.headers(headers), xUriTemplate)
+
+    override fun replaceHeader(name: String, value: String?): Request = RoutedRequest(delegate.replaceHeader(name, value), xUriTemplate)
+
+    override fun removeHeader(name: String): Request = RoutedRequest(delegate.removeHeader(name), xUriTemplate)
+
+    override fun body(body: Body): Request = RoutedRequest(delegate.body(body), xUriTemplate)
+
+    override fun body(body: String): Request = RoutedRequest(delegate.body(body), xUriTemplate)
+
+    override fun body(body: InputStream, length: Long?): Request = RoutedRequest(delegate.body(body, length), xUriTemplate)
 }
 
 class RoutedResponse(private val delegate: Response, val xUriTemplate: UriTemplate) : Response by delegate {
@@ -76,4 +100,16 @@ class RoutedResponse(private val delegate: Response, val xUriTemplate: UriTempla
     override fun hashCode(): Int = delegate.hashCode()
 
     override fun toString(): String = delegate.toString()
+
+    override fun header(name: String, value: String?): Response = RoutedResponse(delegate.header(name, value), xUriTemplate)
+
+    override fun replaceHeader(name: String, value: String?): Response = RoutedResponse(delegate.replaceHeader(name, value), xUriTemplate)
+
+    override fun removeHeader(name: String): Response = RoutedResponse(delegate.removeHeader(name), xUriTemplate)
+
+    override fun body(body: Body): Response = RoutedResponse(delegate.body(body), xUriTemplate)
+
+    override fun body(body: String): Response = RoutedResponse(delegate.body(body), xUriTemplate)
+
+    override fun body(body: InputStream, length: Long?): Response = RoutedResponse(delegate.body(body, length), xUriTemplate)
 }
---
+++ http4k-core/src/test/kotlin/org/http4k/routing/RoutedMessageTest.kt
@@ -0,0 +1,46 @@
+package org.http4k.routing
+
+import com.natpryce.hamkrest.isA
+import com.natpryce.hamkrest.should.shouldMatch
+import org.http4k.core.Body
+import org.http4k.core.HttpMessage
+import org.http4k.core.Method
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.NOT_FOUND
+import org.http4k.core.Uri
+import org.http4k.core.UriTemplate.Companion.from
+import org.junit.Test
+
+class RoutedMessageTest {
+    private val template = from("an-uri-template")
+
+    @Test
+    fun `request manipulations maintain the same type`() {
+        val request = RoutedRequest(Request(GET, "/"), template)
+
+        request.method(Method.POST).shouldMatch(isA<RoutedRequest>())
+        request.uri(Uri.of("/changed")).shouldMatch(isA<RoutedRequest>())
+        request.query("foo", "bar").shouldMatch(isA<RoutedRequest>())
+        request.headers(listOf("foo" to "bar")).shouldMatch(isA<RoutedRequest>())
+
+        checkMessageFields<RoutedRequest>(request)
+    }
+
+    @Test
+    fun `response manipulations maintain the same type`() {
+        val response = RoutedResponse(Response(NOT_FOUND), template)
+
+        checkMessageFields<RoutedResponse>(response)
+    }
+
+    private inline fun <reified T : Any> checkMessageFields(request: HttpMessage) {
+        request.header("foo", "bar").shouldMatch(isA<T>())
+        request.replaceHeader("foo", "bar").shouldMatch(isA<T>())
+        request.removeHeader("foo").shouldMatch(isA<T>())
+        request.body("foo").shouldMatch(isA<T>())
+        request.body(Body.EMPTY).shouldMatch(isA<T>())
+        request.body("foo".byteInputStream()).shouldMatch(isA<T>())
+    }
+}
