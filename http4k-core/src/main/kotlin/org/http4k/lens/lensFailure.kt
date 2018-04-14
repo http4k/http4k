1492e38f6550f311b46d0373daa7e5e108481bb3
David Denton <denton.david@gmail.com>
2018-04-14 12:37:57 +0100

    Revert: tweak to method ref

--- http4k-core/src/main/kotlin/org/http4k/lens/lensFailure.kt
+++ http4k-core/src/main/kotlin/org/http4k/lens/lensFailure.kt
@@ -4,13 +4,14 @@ data class LensFailure(val failures: List<Failure>, override val cause: Exceptio
 
     constructor(vararg failures: Failure, cause: Exception? = null, target: Any? = null) : this(failures.asList(), cause, target)
 
-    fun overall(): Failure.Type = with(failures.map(Failure::type)) {
-        when {
-            contains(Failure.Type.Unsupported) -> Failure.Type.Unsupported
-            isEmpty() || contains(Failure.Type.Invalid) -> Failure.Type.Invalid
-            else -> Failure.Type.Missing
+    fun overall(): Failure.Type =
+        with(failures.map { it.type }) {
+            when {
+                contains(Failure.Type.Unsupported) -> Failure.Type.Unsupported
+                isEmpty() || contains(Failure.Type.Invalid) -> Failure.Type.Invalid
+                else -> Failure.Type.Missing
+            }
         }
-    }
 }
 
 sealed class Failure(val type: Type, open val error: Exception? = null) {
@@ -27,7 +28,6 @@ data class Missing(override val meta: Meta) : Failure(Type.Missing) {
 
 data class Invalid(override val meta: Meta, override val error: Exception?) : Failure(Type.Invalid, error) {
     constructor(meta: Meta) : this(meta, null)
-
     override fun toString(): String = "${meta.location} '${meta.name}' must be ${meta.paramMeta.value}"
 }
 
