0d34f6898c3da589652dd83e38434e2dae3ca45e
dgliosca <diego.gliosca@springer.com>
2018-05-16 12:29:57 +0100

    added etag parser

---
+++ http4k-core/src/main/kotlin/org/http4k/core/etag/ETagValidationRequestParser.kt
@@ -0,0 +1,41 @@
+package org.http4k
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+data class ETag(val value: String, val weak: Boolean = false)
+
+sealed class FieldValue {
+    data class ETags(val value: List<ETag>) : FieldValue() {
+        constructor(vararg eTags: ETag) : this(eTags.toList())
+    }
+
+    object Wildcard : FieldValue()
+}
+
+/**
+ * Implemented according to https://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.11
+ */
+class ETagValidationRequestParser {
+    companion object {
+        private const val w = "W/"
+        private val eTag = Pattern.compile("^(?:($w)?\"([^\"]+)\")")
+        private val separator = Pattern.compile("(?<=\")\\s*,\\s*(?=($w)?\")")
+
+        fun parse(headerValue: String): FieldValue {
+            if (headerValue == "*") return FieldValue.Wildcard
+            return FieldValue.ETags(headerValue.split(separator).mapNotNull { parseTag(it.trim('\t', ' ')) })
+        }
+
+        private fun parseTag(value: String): ETag? {
+            val matcher = eTag.matcher(value)
+            while (matcher.find()) {
+                return ETag(matcher.tag(), matcher.isWeak())
+            }
+            return null
+        }
+
+        private fun Matcher.isWeak(): Boolean = this.group(1) == w
+        private fun Matcher.tag(): String = this.group(2)
+    }
+}
---
+++ http4k-core/src/test/kotlin/org/http4k/core/etag/ETagValidationRequestParserTest.kt
@@ -0,0 +1,106 @@
+package org.http4k
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.ETagValidationRequestParser.Companion.parse
+import org.http4k.FieldValue.ETags
+import org.junit.Test
+
+
+class ETagValidationRequestParserTest {
+
+    @Test
+    fun `strong etag with empty quotes`() {
+        assertThat(parse("\"\""), equalTo<FieldValue>(ETags()))
+    }
+
+    @Test
+    fun `weak etag with empty quotes`() {
+        assertThat(parse("W/\"\""), equalTo<FieldValue>(ETags()))
+    }
+
+    @Test
+    fun `no etags`() {
+        assertThat(parse("something"), equalTo<FieldValue>(ETags()))
+    }
+
+    @Test
+    fun `strong etag`() {
+        assertThat(parse("\"something\""), equalTo<FieldValue>(ETags(ETag("something"))))
+    }
+
+    @Test
+    fun `ignore content after matching quotes`() =
+        assertThat(parse("\"something\"hey,a\""), equalTo<FieldValue>(ETags(ETag("something"))))
+
+    @Test
+    fun `ignore content before matching quotes`() {
+        assertThat(parse("something\"hey,a\""), equalTo<FieldValue>(ETags()))
+    }
+
+    @Test
+    fun `weak etag`() {
+        assertThat(parse("W/\"hey\""), equalTo<FieldValue>(ETags(ETag("hey", weak = true))))
+    }
+
+    @Test
+    fun `weak etag followed by a comma`() {
+        assertThat(parse("W/\"hey\","), equalTo<FieldValue>(ETags(ETag("hey", weak = true))))
+    }
+
+    @Test
+    fun `weak etag followed by a space and a comma`() {
+        assertThat(parse("W/\"hey\" ,"), equalTo<FieldValue>(ETags(ETag("hey", weak = true))))
+    }
+
+    @Test
+    fun `weak etag preceded by spaces followed by a space and a comma`() {
+        assertThat(parse("\t   W/\"hey\" ,"), equalTo<FieldValue>(ETags(ETag("hey", weak = true))))
+    }
+
+    @Test
+    fun `two weak etags comma and space separated`() {
+        assertThat(parse("W/\"hey\", W/\"mate\""), equalTo<FieldValue>(ETags(ETag("hey", weak = true), ETag("mate", weak = true))))
+    }
+
+    @Test
+    fun `two weak etags with no separator extract just the first one`() {
+        assertThat(parse("W/\"hey\" W/\"mate\""), equalTo<FieldValue>(ETags(ETag("hey", weak = true))))
+    }
+
+    @Test
+    fun `two strong etags comma separated`() {
+        assertThat(parse("\"hey\", \"mate\""), equalTo<FieldValue>(ETags(ETag("hey"), ETag("mate"))))
+    }
+
+    @Test
+    fun `strong etag followed by few commas and quotes`() {
+        assertThat(parse("\"hey\",\",\",\""), equalTo<FieldValue>(ETags(ETag("hey"))))
+    }
+
+    @Test
+    fun `strong etag followed by comma quote letter comma`() {
+        assertThat(parse("\"hey\",\"y,\" , \""), equalTo<FieldValue>(ETags(ETag("hey"), ETag("y,"))))
+    }
+
+    @Test
+    fun `mix of weak and strong etags with invalid tag in between`() {
+        val fieldValue = parse("""W/"hey",${"\"\"\""}b", ", ${"\t"}a ,text   W/"kk"  W/"text"  W/" sample""")
+        assertThat(fieldValue, equalTo<FieldValue>(ETags(ETag("hey", weak = true), ETag(", \ta ,text   W/"))))
+    }
+
+    @Test
+    fun `a mix of well formed weak and strong etags`() {
+        val fieldValue = parse("""
+            W/"hello", "hey", "this", W/"is", "cool"
+        """.trimIndent())
+
+        assertThat(fieldValue, equalTo<FieldValue>(ETags(
+            ETag("hello", weak = true),
+            ETag("hey"),
+            ETag("this"),
+            ETag("is", weak = true),
+            ETag("cool")
+        )))
+    }
+}
