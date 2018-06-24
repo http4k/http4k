3f1c9d6157b279be033ba1d765c7315a31517944
David Denton <denton.david@gmail.com>
2018-06-24 20:09:20 +0100

    chaos period

--- http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosFilters.kt
+++ http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosFilters.kt
@@ -4,12 +4,14 @@ import org.http4k.chaos.ChaosPeriod.Companion.BlockThread
 import org.http4k.chaos.ChaosPeriod.Companion.Latency
 import org.http4k.chaos.ChaosPeriod.Companion.Repeat
 import org.http4k.chaos.ChaosPeriod.Companion.Wait
+import org.http4k.chaos.ChaosPolicy.Companion.PercentageBased
 import org.http4k.core.Filter
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.with
 import org.http4k.lens.Header
 import java.time.Duration
+import java.util.concurrent.ThreadLocalRandom
 
 val Header.Common.CHAOS; get() = Header.required("x-http4k-chaos")
 
@@ -25,19 +27,56 @@ object ChaosFilters {
             }
         }
     }
+
+    operator fun invoke(chaosPeriod: ChaosPeriod) = Filter { next ->
+        {
+            chaosPeriod(next(chaosPeriod(it)))
+        }
+    }
 }
 
 interface ChaosPeriod {
+    operator fun invoke(request: Request) = request
+    operator fun invoke(response: Response) = response
+
     companion object {
         fun Repeat(period: ChaosPeriod): ChaosPeriod = TODO()
-        fun Repeat(times: Int, period: ChaosPeriod): ChaosPeriod = TODO()
 
-        object Wait: ChaosPeriod
+        object Wait : ChaosPeriod
 
-        fun BlockThread(): ChaosPeriod = TODO()
-        fun Latency(): ChaosPeriod = TODO()
-        // eat memory
-        // stack overflow
+        fun BlockThread() = object : ChaosPeriod {
+            override fun invoke(request: Request) = request.apply { Thread.currentThread().join() }
+        }
+
+        fun Latency(minDelay: Duration = Duration.ofMillis(100),
+                    maxDelay: Duration = Duration.ofMillis(500)) = object : ChaosPeriod {
+            override fun invoke(response: Response): Response = response.apply {
+                val delay = ThreadLocalRandom.current().nextInt(minDelay.toMillis().toInt(), maxDelay.toMillis().toInt())
+                Thread.sleep(delay.toLong())
+            }
+        }
+
+        @Suppress("unused")
+        fun EatMemory(): ChaosPeriod = object : ChaosPeriod {
+            override fun invoke(response: Response): Response = response.apply {
+                val list = mutableListOf<ByteArray>()
+                while (true) list += ByteArray(1048576)
+            }
+        }
+
+        @Suppress("unused")
+        fun KillProcess(): ChaosPeriod = object : ChaosPeriod {
+            override fun invoke(request: Request) = request.apply { System.exit(1) }
+        }
+
+        @Suppress("unused")
+        fun StackOverflow(): ChaosPeriod = object : ChaosPeriod {
+            override fun invoke(request: Request) = request.apply { StackOverflow() }
+        }
+
+        fun ThrowException(e: Exception = ChaosException("Chaos behaviour injected!")) = object : ChaosPeriod {
+            override fun invoke(request: Request) = throw e
+        }
     }
 }
 
@@ -45,10 +84,11 @@ fun ChaosPeriod.then(next: ChaosPeriod): ChaosPeriod = TODO()
 
 @JvmName("untilResponse")
 fun ChaosPeriod.until(trigger: (Response) -> Boolean): ChaosPeriod = TODO()
+
 fun ChaosPeriod.until(trigger: (Request) -> Boolean): ChaosPeriod = TODO()
 fun ChaosPeriod.until(trigger: () -> Boolean): ChaosPeriod = TODO()
 fun ChaosPeriod.until(period: Duration): ChaosPeriod = TODO()
 
-val blockThread = Wait.until(Duration.ofSeconds(100)).then(BlockThread())
-val goSlow = Wait.until(Duration.ofSeconds(100)).then(Latency())
-val a = Repeat(blockThread.then(goSlow)).until { _: Response -> true}
+val blockThread = Wait.until(Duration.ofSeconds(100)).then(PercentageBased(100)(BlockThread()))
+val goSlow = Wait.until(Duration.ofSeconds(100)).then(Latency(Duration.ofMillis(1)))
+val a = Repeat(blockThread.then(goSlow)).until { _: Response -> true }
--- http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosPolicy.kt
+++ http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosPolicy.kt
@@ -6,10 +6,24 @@ import java.util.Random
 import java.util.concurrent.ThreadLocalRandom
 
 interface ChaosPolicy {
-    fun shouldInject(request: Request): Boolean = false
-    fun shouldInject(response: Response): Boolean = false
+    fun shouldInject(request: Request) = false
+    fun shouldInject(response: Response) = false
+
+    operator fun invoke(chaosPeriod: ChaosPeriod) = let { it ->
+        object : ChaosPeriod {
+            override fun invoke(request: Request) = if (it.shouldInject(request)) chaosPeriod(request) else request
+            override fun invoke(response: Response) = if (it.shouldInject(response)) chaosPeriod(response) else response
+        }
+    }
 
     companion object {
+        fun RequestBased(inject: (Request) -> Boolean) = object : ChaosPolicy {
+            override fun shouldInject(request: Request) = inject(request)
+        }
+        fun ResponseBased(inject: (Response) -> Boolean) = object : ChaosPolicy {
+            override fun shouldInject(response: Response) = inject(response)
+        }
+
         fun Always(injectRequest: Boolean = true, injectResponse: Boolean = true) = object : ChaosPolicy {
             override fun shouldInject(request: Request) = injectRequest
             override fun shouldInject(response: Response) = injectResponse
@@ -19,6 +33,7 @@ interface ChaosPolicy {
             override fun shouldInject(response: Response) = selector.nextInt(100) <= injectionFrequency
         }
 
-        fun PercentageBasedFromEnv() = PercentageBased((System.getenv("CHAOS_INJECTION_FREQUENCY")?.toInt() ?: 50))
+        fun PercentageBasedFromEnv() = PercentageBased((System.getenv("CHAOS_INJECTION_FREQUENCY")?.toInt()
+                ?: 50))
     }
 }
