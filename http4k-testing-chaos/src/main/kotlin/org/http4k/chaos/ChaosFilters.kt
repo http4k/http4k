926590e1540c4c5bc565d59f383ae247cde72ae2
David Denton <denton.david@gmail.com>
2018-06-24 19:16:29 +0100

    chaos period

--- http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosFilters.kt
+++ http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosFilters.kt
@@ -1,8 +1,15 @@
 package org.http4k.chaos
 
+import org.http4k.chaos.ChaosPeriod.Companion.BlockThread
+import org.http4k.chaos.ChaosPeriod.Companion.Latency
+import org.http4k.chaos.ChaosPeriod.Companion.Repeat
+import org.http4k.chaos.ChaosPeriod.Companion.Wait
 import org.http4k.core.Filter
+import org.http4k.core.Request
+import org.http4k.core.Response
 import org.http4k.core.with
 import org.http4k.lens.Header
+import java.time.Duration
 
 val Header.Common.CHAOS; get() = Header.required("x-http4k-chaos")
 
@@ -19,3 +26,32 @@ object ChaosFilters {
         }
     }
 }
+
+typealias RequestTrigger = (Request) -> Boolean
+typealias ResponseTrigger = (Response) -> Boolean
+
+interface ChaosPeriod {
+    companion object {
+        fun Repeat(period: ChaosPeriod): ChaosPeriod = TODO()
+        fun Repeat(times: Int, period: ChaosPeriod): ChaosPeriod = TODO()
+
+        object Wait: ChaosPeriod
+
+        fun BlockThread(): ChaosPeriod = TODO()
+        fun Latency(): ChaosPeriod = TODO()
+        // eat memory
+        // stack overflow
+    }
+}
+
+fun ChaosPeriod.then(next: ChaosPeriod): ChaosPeriod = TODO()
+
+@JvmName("untilResponse")
+fun ChaosPeriod.until(trigger: (Response) -> Boolean): ChaosPeriod = TODO()
+fun ChaosPeriod.until(trigger: (Request) -> Boolean): ChaosPeriod = TODO()
+fun ChaosPeriod.until(trigger: () -> Boolean): ChaosPeriod = TODO()
+fun ChaosPeriod.until(period: Duration): ChaosPeriod = TODO()
+
+val blockThread = Wait.until(Duration.ofSeconds(100)).then(BlockThread())
+val goSlow = Wait.until(Duration.ofSeconds(100)).then(Latency())
+val a = Repeat(blockThread.then(goSlow)).until { _: Response -> true}
