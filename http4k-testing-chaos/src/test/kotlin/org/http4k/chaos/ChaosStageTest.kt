8e4d49d018a7406a7ca0c43daf5f6beb1335ac21
David Denton <denton.david@gmail.com>
2018-06-27 19:58:44 +0100

    test for repeat

--- http4k-testing-chaos/src/test/kotlin/org/http4k/chaos/ChaosStageTest.kt
+++ http4k-testing-chaos/src/test/kotlin/org/http4k/chaos/ChaosStageTest.kt
@@ -2,16 +2,23 @@ package org.http4k.chaos
 
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.should.shouldMatch
+import org.http4k.chaos.ChaosStage.Companion.Repeat
 import org.http4k.chaos.ChaosStage.Companion.Wait
 import org.http4k.core.HttpTransaction
+import org.http4k.core.Method.DELETE
 import org.http4k.core.Method.GET
+import org.http4k.core.Method.OPTIONS
 import org.http4k.core.Method.POST
+import org.http4k.core.Method.TRACE
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.Status.Companion.GATEWAY_TIMEOUT
 import org.http4k.core.Status.Companion.I_M_A_TEAPOT
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class ChaosStageTest {
@@ -25,9 +32,7 @@ class ChaosStageTest {
 
     @Test
     fun `until stops when the trigger is hit`() {
-        val app = object : ChaosStage {
-            override fun invoke(tx: HttpTransaction) = Response(NOT_FOUND)
-        }.until { it.request.method == POST }
+        val app = chaosStage(NOT_FOUND).until { it.request.method == POST }
                 .asFilter().then { response }
 
         app(Request(GET, "")) shouldMatch equalTo(Response(NOT_FOUND))
@@ -37,17 +42,36 @@ class ChaosStageTest {
 
     @Test
     fun `then moves onto the next stage`() {
-        val app = object : ChaosStage {
-            override fun invoke(tx: HttpTransaction) = Response(I_M_A_TEAPOT)
-        }
+        val app = chaosStage(I_M_A_TEAPOT)
                 .until { it.request.method == POST }
-                .then(object : ChaosStage {
-                    override fun invoke(tx: HttpTransaction) = Response(NOT_FOUND)
-                })
+                .then(chaosStage(NOT_FOUND))
                 .asFilter().then { response }
 
         app(Request(GET, "")) shouldMatch equalTo(Response(I_M_A_TEAPOT))
         app(Request(POST, "")) shouldMatch equalTo(Response(NOT_FOUND))
         app(Request(GET, "")) shouldMatch equalTo(Response(NOT_FOUND))
+    }
+
+    @Test
+    @Disabled
+    fun `repeat starts again at the beginning`() {
+        val app = Repeat {
+            chaosStage(I_M_A_TEAPOT).until { it.request.method == POST }
+                    .then(chaosStage(NOT_FOUND)).until { it.request.method == OPTIONS }
+                    .then(chaosStage(GATEWAY_TIMEOUT)).until { it.request.method == TRACE }
+        }.until { it.request.method == DELETE }
+                .asFilter().then { response }
+
+        app(Request(GET, "")) shouldMatch equalTo(Response(I_M_A_TEAPOT))
+        app(Request(POST, "")) shouldMatch equalTo(Response(NOT_FOUND))
+        app(Request(GET, "")) shouldMatch equalTo(Response(NOT_FOUND))
+        app(Request(OPTIONS, "")) shouldMatch equalTo(Response(GATEWAY_TIMEOUT))
+        app(Request(GET, "")) shouldMatch equalTo(Response(GATEWAY_TIMEOUT))
+        app(Request(TRACE, "")) shouldMatch equalTo(Response(I_M_A_TEAPOT))
+        app(Request(DELETE, "")) shouldMatch equalTo(response)
+    }
+
+    private fun chaosStage(status: Status): ChaosStage = object : ChaosStage {
+        override fun invoke(tx: HttpTransaction): Response = Response(status)
     }
 }
