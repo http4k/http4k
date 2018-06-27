902328b1d134b9276020c8db831476e4e304453a
David Denton <denton.david@gmail.com>
2018-06-27 19:52:46 +0100

    moar chaos!

--- http4k-testing-chaos/src/test/kotlin/org/http4k/chaos/ChaosStageTest.kt
+++ http4k-testing-chaos/src/test/kotlin/org/http4k/chaos/ChaosStageTest.kt
@@ -8,6 +8,7 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
+import org.http4k.core.Status.Companion.I_M_A_TEAPOT
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
@@ -32,5 +33,21 @@ class ChaosStageTest {
         app(Request(GET, "")) shouldMatch equalTo(Response(NOT_FOUND))
         app(Request(POST, "")) shouldMatch equalTo(response)
         app(Request(GET, "")) shouldMatch equalTo(response)
+    }
+
+    @Test
+    fun `then moves onto the next stage`() {
+        val app = object : ChaosStage {
+            override fun invoke(tx: HttpTransaction) = Response(I_M_A_TEAPOT)
+        }
+                .until { it.request.method == POST }
+                .then(object : ChaosStage {
+                    override fun invoke(tx: HttpTransaction) = Response(NOT_FOUND)
+                })
+                .asFilter().then { response }
+
+        app(Request(GET, "")) shouldMatch equalTo(Response(I_M_A_TEAPOT))
+        app(Request(POST, "")) shouldMatch equalTo(Response(NOT_FOUND))
+        app(Request(GET, "")) shouldMatch equalTo(Response(NOT_FOUND))
     }
 }
