bd8745b911375767a01fd6f4e5f3716edbb5f7f9
Duncan McGregor <duncan@oneeyedmen.com>
2018-07-10 09:03:32 +0100

    Conversions between Parameters and map https://github.com/http4k/http4k/issues/143

--- http4k-core/src/main/kotlin/org/http4k/core/body/FormBody.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/body/FormBody.kt
@@ -1,15 +1,17 @@
 package org.http4k.core.body
 
-import org.http4k.core.Body
-import org.http4k.core.Parameters
-import org.http4k.core.Request
-import org.http4k.core.findSingle
-import org.http4k.core.toParameters
-import org.http4k.core.toUrlFormEncoded
+import org.http4k.core.*
 
 typealias Form = Parameters
 
+/**
+ * Returns the first form parameter with [name].
+ *
+ * Use [formAsMap] if you don't want to decode the body every invocation.
+ */
 fun Request.form(name: String): String? = form().findSingle(name)
+
+fun Request.formAsMap(): Map<String, List<String?>> = form().toParametersMap()
 
 fun Form.toBody(): Body = Body(toUrlFormEncoded())
 
