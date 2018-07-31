d59a32fdac0071f0da92692b6676a994af41b853
David Denton <denton.david@gmail.com>
2018-07-31 21:45:18 +0100

    revert mistaken removal

--- http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
@@ -1,5 +1,7 @@
 package org.http4k.core
 
+import org.http4k.routing.RoutingHttpHandler
+
 typealias HttpHandler = (Request) -> Response
 
 interface Filter : (HttpHandler) -> HttpHandler {
@@ -15,3 +17,5 @@ val Filter.Companion.NoOp: Filter get() = Filter { next -> { next(it) } }
 fun Filter.then(next: Filter): Filter = Filter { this(next(it)) }
 
 fun Filter.then(next: HttpHandler): HttpHandler = { this(next)(it) }
+
+fun Filter.then(routingHttpHandler: RoutingHttpHandler): RoutingHttpHandler = routingHttpHandler.withFilter(this)
