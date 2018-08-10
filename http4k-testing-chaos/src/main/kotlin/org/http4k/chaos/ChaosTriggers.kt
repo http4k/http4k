f1697217b1a46f64ca2e4909949d7958f09eb7f6
David Denton <denton.david@gmail.com>
2018-08-10 10:56:39 +0100

    changes to chaos triggers

--- http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosTriggers.kt
+++ http4k-testing-chaos/src/main/kotlin/org/http4k/chaos/ChaosTriggers.kt
@@ -1,6 +1,20 @@
 package org.http4k.chaos
 
+import com.natpryce.hamkrest.Matcher
+import com.natpryce.hamkrest.and
+import com.natpryce.hamkrest.anything
 import org.http4k.core.HttpTransaction
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.hamkrest.hasBody
+import org.http4k.hamkrest.hasHeader
+import org.http4k.hamkrest.hasQuery
+import org.http4k.hamkrest.hasRequest
+import org.http4k.hamkrest.hasResponse
+import org.http4k.hamkrest.hasStatus
+import org.http4k.hamkrest.hasUri
+import org.http4k.hamkrest.hasUriPath
 import java.time.Clock
 import java.time.Duration
 import java.time.Instant
@@ -8,18 +22,69 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 typealias ChaosTrigger = (HttpTransaction) -> Boolean
 
+abstract class SerializableTrigger(val type: String) {
+    abstract operator fun invoke(clock: Clock): ChaosTrigger
+}
+
 object ChaosTriggers {
     /**
      * Activates after a particular instant in time.
      */
-    fun Deadline(endTime: Instant, clock: Clock = Clock.systemUTC()): ChaosTrigger = { clock.instant().isAfter(endTime) }
+    data class Deadline(val endTime: Instant) : SerializableTrigger("deadline") {
+        override operator fun invoke(clock: Clock) = Instant.now(clock).let {
+            object : ChaosTrigger {
+                override fun invoke(p1: HttpTransaction) = clock.instant().isAfter(it)
+                override fun toString() = "Deadline ($it)"
+            }
+        }
+    }
 
     /**
      * Activates after a particular delay (compared to instantiation).
      */
-    fun Delay(period: Duration, clock: Clock = Clock.systemUTC()): ChaosTrigger =
-            clock.instant().plus(period).let { endTime -> { clock.instant().isAfter(endTime) } }
+    data class Delay(val endTime: Instant) : SerializableTrigger("delay") {
+        constructor(period: Duration, clock: Clock = Clock.systemUTC()) : this(Instant.now(clock).plus(period))
 
+        override operator fun invoke(clock: Clock): ChaosTrigger = object : ChaosTrigger {
+            override fun invoke(p1: HttpTransaction) = clock.instant().isAfter(endTime)
+            override fun toString() = "Delay (expires $endTime)"
+        }
+    }
+
+    abstract class HttpTransactionTrigger(type: String, matcher: Matcher<HttpTransaction>)
+        : SerializableTrigger(type), Matcher<HttpTransaction> by matcher {
+        override operator fun invoke(clock: Clock) = object : ChaosTrigger {
+            override fun invoke(p1: HttpTransaction) = this@HttpTransactionTrigger.asPredicate()(p1)
+            override fun toString() = this@HttpTransactionTrigger.description
+        }
+    }
+
+    data class MatchRequest(val path: Regex?,
+                            val headers: Map<String, Regex>?,
+                            val queries: Map<String, String>?,
+                            val body: Regex?) : HttpTransactionTrigger("request", {
+        val headerMatchers = headers?.map { hasHeader(it.key, it.value) } ?: emptyList()
+        val queriesMatchers = queries?.map { hasQuery(it.key, it.value) } ?: emptyList()
+        val pathMatchers = path?.let { listOf(hasUri(hasUriPath(it))) } ?: emptyList()
+        val bodyMatchers = body?.let { listOf(hasBody(it)) } ?: emptyList()
+        hasRequest(
+                (headerMatchers + queriesMatchers + pathMatchers + bodyMatchers)
+                        .fold<Matcher<Request>, Matcher<Request>>(anything) { acc, next -> acc.and(next) }
+        )
+    }())
+
+    data class MatchResponse(val status: Int?,
+                             val headers: Map<String, Regex>?,
+                             val body: Regex?) : HttpTransactionTrigger("response", {
+        val headerMatchers = headers?.map { hasHeader(it.key, it.value) } ?: emptyList()
+        val statusMatcher = status?.let { listOf(hasStatus((Status(it, "")))) } ?: emptyList()
+        val bodyMatchers = body?.let { listOf(hasBody(it)) } ?: emptyList()
+
+        hasResponse(
+                (headerMatchers + statusMatcher + bodyMatchers)
+                        .fold<Matcher<Response>, Matcher<Response>>(anything) { acc, next -> acc.and(next) }
+        )
+    }())
 }
 
 /**
--- http4k-testing-chaos/src/test/kotlin/org/http4k/chaos/ChaosTriggersTest.kt
+++ http4k-testing-chaos/src/test/kotlin/org/http4k/chaos/ChaosTriggersTest.kt
@@ -12,15 +12,21 @@ import java.lang.Thread.sleep
 import java.time.Clock
 import java.time.Duration
 import java.time.Duration.ZERO
-import java.time.Instant
+import java.time.Instant.EPOCH
+import java.time.Instant.now
+import java.time.ZoneId.of
 
 class ChaosTriggersTest {
     private val tx = HttpTransaction(Request(GET, ""), Response(OK), ZERO)
 
+    private val dayDot = Clock.fixed(EPOCH, of("UTC"))
+
     @Test
     fun `deadline trigger`() {
+        ChaosTriggers.Deadline(now(dayDot))(dayDot).toString() shouldMatch equalTo("Deadline (1970-01-01T00:00:00Z)")
+
         val clock = Clock.systemDefaultZone()
-        val trigger = ChaosTriggers.Deadline(Instant.now().plusMillis(100), clock)
+        val trigger = ChaosTriggers.Deadline(now(clock).plusMillis(100))(clock)
         trigger(tx) shouldMatch equalTo(false)
         sleep(110)
         trigger(tx) shouldMatch equalTo(true)
@@ -28,8 +34,10 @@ class ChaosTriggersTest {
 
     @Test
     fun `delay trigger`() {
+        ChaosTriggers.Delay(Duration.ofMillis(100), dayDot)(dayDot).toString() shouldMatch equalTo("Delay (expires 1970-01-01T00:00:00.100Z)")
+
         val clock = Clock.systemDefaultZone()
-        val trigger = ChaosTriggers.Delay(Duration.ofMillis(100), clock)
+        val trigger = ChaosTriggers.Delay(Duration.ofMillis(100), clock)(clock)
         trigger(tx) shouldMatch equalTo(false)
         sleep(110)
         trigger(tx) shouldMatch equalTo(true)
@@ -37,20 +45,22 @@ class ChaosTriggersTest {
 
     @Test
     fun `switch trigger`() {
-        val switch = SwitchTrigger(true)
-        switch(tx) shouldMatch equalTo(true)
-        switch.toggle()
-        switch(tx) shouldMatch equalTo(false)
-        switch.toggle(false)
-        switch(tx) shouldMatch equalTo(false)
-        switch.toggle(true)
-        switch(tx) shouldMatch equalTo(true)
+        val trigger = SwitchTrigger(true)
+        trigger.toString() shouldMatch equalTo("SwitchTrigger (active = true)")
+        trigger(tx) shouldMatch equalTo(true)
+        trigger.toggle()
+        trigger(tx) shouldMatch equalTo(false)
+        trigger.toggle(false)
+        trigger(tx) shouldMatch equalTo(false)
+        trigger.toggle(true)
+        trigger(tx) shouldMatch equalTo(true)
     }
 
     @Test
     fun `inversion of trigger`() {
         (!{ _: HttpTransaction -> false })(tx) shouldMatch equalTo(true)
         (!{ _: HttpTransaction -> true })(tx) shouldMatch equalTo(false)
+        (!{ _: HttpTransaction -> true }).toString() shouldMatch equalTo("NOT (org.http4k.core.HttpTransaction) -> kotlin.Boolean")
     }
 
 }
