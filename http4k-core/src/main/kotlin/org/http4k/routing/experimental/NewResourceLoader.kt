00e3d9162f51809f6050165840e04933824b8034
Duncan McGregor <duncan@oneeyedmen.com>
2018-08-10 13:18:18 +0100

    Spiking improved resource abstraction - support last modified for classpath

--- http4k-core/src/main/kotlin/org/http4k/routing/experimental/NewResourceLoader.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/experimental/NewResourceLoader.kt
@@ -2,6 +2,8 @@ package org.http4k.routing.experimental
 
 import java.io.File
 import java.net.URL
+import java.time.Instant
+import java.time.temporal.ChronoUnit
 
 
 interface NewResourceLoader {
@@ -9,11 +11,18 @@ interface NewResourceLoader {
     fun resourceFor(path: String): Resource?
 
     companion object {
-        fun Classpath(basePackagePath: String = "/") = object : NewResourceLoader {
-            override fun resourceFor(path: String) = javaClass.getResource(finalBasePath + path)?.toResource()
 
-            private val withStarting = if (basePackagePath.startsWith("/")) basePackagePath else "/$basePackagePath"
-            private val finalBasePath = if (withStarting.endsWith("/")) withStarting else "$withStarting/"
+        fun Classpath(
+            basePackagePath: String = "/",
+            constantLastModified: Instant? = Instant.now().truncatedTo(ChronoUnit.SECONDS), // see * below,
+            lastModifiedFinder: (path: String) -> Instant? = { constantLastModified }
+        ): ClasspathResourceLoader {
+            // We don't want to be grubbing about in jar files for last modified dates, so we default to the creation
+            // time of the loader, which make everything out of date every time an app is started, but in date after that.
+
+            // * Must truncate if we are not always to be ahead of the If-Last-Modified header because it doesn't support
+            // fractions of seconds.
+            return ClasspathResourceLoader(basePackagePath, lastModifiedFinder)
         }
 
         fun Directory(baseDir: String) = object : NewResourceLoader {
@@ -27,5 +36,19 @@ interface NewResourceLoader {
     }
 }
 
-private fun URL.toResource(): Resource = URLResource(this)
+class ClasspathResourceLoader(
+    basePackagePath: String,
+    private val lastModifiedFinder: (path: String) -> Instant?
+) : NewResourceLoader {
+
+    override fun resourceFor(path: String): Resource? {
+        val resourcePath = finalBasePath + path
+        return javaClass.getResource(resourcePath)?.toResource(lastModifiedFinder(resourcePath))
+    }
+
+    private val withStarting = if (basePackagePath.startsWith("/")) basePackagePath else "/$basePackagePath"
+    private val finalBasePath = if (withStarting.endsWith("/")) withStarting else "$withStarting/"
+}
+
+private fun URL.toResource(lastModified: Instant?): Resource = URLResource(this, lastModified)
 
