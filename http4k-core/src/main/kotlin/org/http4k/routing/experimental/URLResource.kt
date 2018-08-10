7783bfb43d9eff121a92f63eb41036ab8afb4ef5
Duncan McGregor <duncan@oneeyedmen.com>
2018-08-10 13:18:18 +0100

    Spiking improved resource abstraction - support last modified for classpath

--- http4k-core/src/main/kotlin/org/http4k/routing/experimental/URLResource.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/experimental/URLResource.kt
@@ -2,7 +2,11 @@ package org.http4k.routing.experimental
 
 import java.io.InputStream
 import java.net.URL
+import java.time.Instant
 
-data class URLResource(val url: URL) : Resource {
+data class URLResource(
+    val url: URL,
+    override val lastModified: Instant? = null
+) : Resource {
     override fun openStream(): InputStream = url.openStream()
 }
