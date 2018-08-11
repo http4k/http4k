3fa76126268b9f86398becd8b1c0367597762a05
Duncan McGregor <duncan@oneeyedmen.com>
2018-08-11 21:01:52 +0100

    Spiking improved resource abstraction - test tidy

--- http4k-core/src/test/kotlin/org/http4k/routing/experimental/NewResourceLoaderTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/routing/experimental/NewResourceLoaderTest.kt
@@ -7,43 +7,36 @@ import org.http4k.routing.experimental.NewResourceLoader.Companion.Classpath
 import org.http4k.routing.experimental.NewResourceLoader.Companion.Directory
 import org.junit.jupiter.api.Test
 
-class NewResourceLoaderTest {
+abstract class ResourceLoaderContract(protected val loader: NewResourceLoader) {
 
     @Test
     fun `classpath loader loads existing file`() {
-        checkContents(Classpath("/"), "mybob.xml", "<xml>content</xml>")
+        checkContents("mybob.xml", "<xml>content</xml>")
     }
 
     @Test
     fun `classpath loader loads existing child file`() {
-        checkContents(Classpath("/"), "org/index.html", "hello from the io index.html")
+        checkContents("org/index.html", "hello from the io index.html")
     }
 
     @Test
     fun `classpath loader for missing file`() {
-        assertThat(Classpath("/").resourceFor("notAFile"), absent())
+        checkContents("notAFile", null)
     }
 
-    @Test
-    fun `directory loader loads existing file`() {
-        checkContents(Directory("./src/test/resources"), "mybob.xml", "<xml>content</xml>")
-    }
-
-    @Test
-    fun `directory loader loads existing child file`() {
-        checkContents(Directory("./src/test/resources"), "org/index.html", "hello from the io index.html")
-    }
-
-    @Test
-    fun `directory loader for missing file`() {
-        assertThat(Directory("./src/test/resources").resourceFor("notAFile"), absent())
-    }
-
-    private fun checkContents(loader: NewResourceLoader, path: String, expected: String) {
-        val resource = loader.resourceFor(path)!!
-        val content = resource.openStream().use { it.readBytes() }
-        assertThat(String(content), equalTo(expected))
-        if (resource.length != null)
-            assertThat(content.size.toLong(), equalTo(resource.length))
+    open fun checkContents(path: String, expected: String?) {
+        if (expected == null)
+            assertThat(loader.resourceFor("notAFile"), absent())
+        else {
+            val resource = loader.resourceFor(path)!!
+            val content = resource.openStream().use { it.readBytes() }
+            assertThat(String(content), equalTo(expected))
+            if (resource.length != null)
+                assertThat(content.size.toLong(), equalTo(resource.length))
+        }
     }
 }
+
+class NewResourceLoaderTest : ResourceLoaderContract(Classpath("/"))
+
+class DirectoryResourceLoaderTest : ResourceLoaderContract(Directory("./src/test/resources"))
