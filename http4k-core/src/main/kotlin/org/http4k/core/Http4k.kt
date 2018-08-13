a8f5fba10dc79b8919778ae75cb1e8ac94e9d39a
David Denton <denton.david@gmail.com>
2018-08-13 17:02:22 +0100

    name param

--- http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
@@ -2,7 +2,7 @@ package org.http4k.core
 
 import org.http4k.routing.RoutingHttpHandler
 
-typealias HttpHandler = (Request) -> Response
+typealias HttpHandler = (request: Request) -> Response
 
 interface Filter : (HttpHandler) -> HttpHandler {
     companion object {
