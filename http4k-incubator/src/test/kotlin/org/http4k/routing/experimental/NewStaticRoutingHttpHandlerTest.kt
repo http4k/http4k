774915d9ee3d2e478ce8e0d997a16bcbac40ccb4
David Denton <denton.david@gmail.com>
2018-08-26 23:33:33 +0100

    added dependency on core tests

--- http4k-incubator/src/test/kotlin/org/http4k/routing/experimental/NewStaticRoutingHttpHandlerTest.kt
+++ http4k-incubator/src/test/kotlin/org/http4k/routing/experimental/NewStaticRoutingHttpHandlerTest.kt
@@ -3,8 +3,6 @@ package org.http4k.routing.experimental
 import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.StaticRoutingHttpHandlerTest
 
-// I can't move this into experimental for some reason
-
 class NewStaticRoutingHttpHandlerTest : StaticRoutingHttpHandlerTest() {
     override val handler: RoutingHttpHandler = StaticRoutingHttpHandler(
         pathSegments = validPath,
