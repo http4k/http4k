63c1018041f4430d11a8e8caa6aa80dceb33af9b
David Denton <denton.david@gmail.com>
2018-08-27 00:05:34 +0100

    add incubator to main project

--- http4k-jsonrpc/src/main/kotlin/org/http4k/jsonrpc/AutoMethodMappingsBuilder.kt
+++ http4k-jsonrpc/src/main/kotlin/org/http4k/jsonrpc/AutoMethodMappingsBuilder.kt
@@ -2,27 +2,19 @@ package org.http4k.jsonrpc
 
 import org.http4k.format.JsonLibAutoMarshallingJson
 
-class AutoMethodMappingsBuilder<ROOT: Any>(override val json: JsonLibAutoMarshallingJson<ROOT>) :
+class AutoMethodMappingsBuilder<ROOT : Any>(override val json: JsonLibAutoMarshallingJson<ROOT>) :
         ManualMethodMappingsBuilder<ROOT, ROOT>(json) {
 
-    inline fun <reified IN: Any, OUT: Any> handler(paramsFieldNames: Set<String>,
-                                                   noinline block: (IN) -> OUT): RequestHandler<ROOT, ROOT> {
-        val paramsLens = Params<ROOT, IN> {
-            json.asA(it, IN::class)
-        }
-        val resultLens = Result<OUT, ROOT> {
-            json.asJsonObject(it)
-        }
-        return handler(paramsFieldNames, paramsLens, resultLens, block)
-    }
+    inline fun <reified IN : Any, OUT : Any> handler(paramsFieldNames: Set<String>,
+                                                     noinline block: (IN) -> OUT): RequestHandler<ROOT, ROOT> =
+            handler(paramsFieldNames,
+                    Params { json.asA(it, IN::class) },
+                    Result { json.asJsonObject(it) },
+                    block)
 
-    inline fun <reified IN: Any, OUT: Any> handler(noinline block: (IN) -> OUT): RequestHandler<ROOT, ROOT> =
+    inline fun <reified IN : Any, OUT : Any> handler(noinline block: (IN) -> OUT): RequestHandler<ROOT, ROOT> =
             handler(IN::class.javaObjectType.declaredFields.map { it.name }.toSet(), block)
 
-    fun <OUT: Any> handler(block: () -> OUT): RequestHandler<ROOT, ROOT> {
-        val resultLens = Result<OUT, ROOT> {
-            json.asJsonObject(it)
-        }
-        return handler(resultLens, block)
-    }
+    fun <OUT : Any> handler(block: () -> OUT): RequestHandler<ROOT, ROOT> =
+            handler(Result { json.asJsonObject(it) }, block)
 }
