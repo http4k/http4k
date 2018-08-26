4e6fee994203c61b1aa52f55eae50ca42a7649e3
David Denton <denton.david@gmail.com>
2018-08-26 23:46:44 +0100

    fixing some static routing stuff

---
+++ http4k-incubator/src/test/kotlin/org/http4k/routing/NewStaticRoutingHttpHandlerTest.kt
@@ -0,0 +1,11 @@
+package org.http4k.routing
+
+import org.http4k.routing.experimental.ResourceLoaders.Classpath
+import org.http4k.routing.experimental.StaticRoutingHttpHandler
+
+class NewStaticRoutingHttpHandlerTest : StaticRoutingHttpHandlerTest() {
+    override val handler: RoutingHttpHandler = StaticRoutingHttpHandler(
+            pathSegments = validPath,
+            resourceLoader = Classpath()
+    )
+}
