1deaeb2c47023672b78112de4449f96ea4ad1db7
David Denton <denton.david@gmail.com>
2018-08-27 17:21:49 +0100

    test for oauth providers

--- http4k-security-oauth/src/test/kotlin/org/http4k/security/OAuthProviderTest.kt
+++ http4k-security-oauth/src/test/kotlin/org/http4k/security/OAuthProviderTest.kt
@@ -21,27 +21,6 @@ import org.http4k.hamkrest.hasStatus
 import org.junit.jupiter.api.Test
 
 class OAuthProviderTest {
-
-    class FakeOAuthPersistence : OAuthPersistence {
-
-        var csrf: CrossSiteRequestForgeryToken? = null
-        var accessToken: AccessTokenContainer? = null
-
-        override fun retrieveCsrf(request: Request): CrossSiteRequestForgeryToken? = csrf
-
-        override fun assignCsrf(redirect: Response, csrf: CrossSiteRequestForgeryToken): Response {
-            this.csrf = csrf
-            return redirect.header("action", "assignCsrf")
-        }
-
-        override fun retrieveToken(request: Request): AccessTokenContainer? = accessToken
-
-        override fun assignToken(request: Request, redirect: Response, accessToken: AccessTokenContainer): Response {
-            this.accessToken = accessToken
-            return redirect.header("action", "assignToken")
-        }
-    }
-
     private val providerConfig = OAuthProviderConfig(
         Uri.of("http://authHost"),
         "/auth",
