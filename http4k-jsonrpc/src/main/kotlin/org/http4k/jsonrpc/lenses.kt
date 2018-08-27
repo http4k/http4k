4c69737bb4058352855a5aa0335154101b824fdb
David Denton <denton.david@gmail.com>
2018-08-27 21:02:16 +0100

    tidy

--- http4k-jsonrpc/src/main/kotlin/org/http4k/jsonrpc/lenses.kt
+++ http4k-jsonrpc/src/main/kotlin/org/http4k/jsonrpc/lenses.kt
@@ -4,8 +4,6 @@ import org.http4k.lens.Lens
 import org.http4k.lens.Meta
 import org.http4k.lens.ParamMeta.ObjectParam
 
-class Params<NODE, IN>(convert: (NODE) -> IN) : Lens<NODE, IN>(
-        Meta(true, "request", ObjectParam, "params"), convert)
+class Params<NODE, IN>(convert: (NODE) -> IN) : Lens<NODE, IN>(Meta(true, "request", ObjectParam, "params"), convert)
 
-class Result<OUT, NODE>(convert: (OUT) -> NODE) : Lens<OUT, NODE>(
-        Meta(true, "response", ObjectParam, "result"), convert)
+class Result<OUT, NODE>(convert: (OUT) -> NODE) : Lens<OUT, NODE>(Meta(true, "response", ObjectParam, "result"), convert)
