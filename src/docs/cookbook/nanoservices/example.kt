1bd08aea21eaf9ea0bbc86cd0c199dcfe7f3d9e2
David Denton <denton.david@gmail.com>
2018-08-29 21:18:33 +0100

    micro -> nano

--- src/docs/cookbook/nanoservices/example.kt
+++ src/docs/cookbook/nanoservices/example.kt
@@ -1,17 +1,16 @@
-package cookbook.micro_apps
+package cookbook.nanoservices
 
 import org.http4k.chaos.ChaosBehaviours.Latency
 import org.http4k.chaos.ChaosTriggers.Always
 import org.http4k.chaos.appliedWhen
 import org.http4k.chaos.withChaosControls
 import org.http4k.client.JavaHttpClient
-import org.http4k.client.OkHttp
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters.PrintRequestAndResponse
 import org.http4k.filter.RequestFilters.ProxyHost
 import org.http4k.filter.ResponseFilters.ReportRouteLatency
 import org.http4k.filter.TrafficFilters.RecordTo
-import org.http4k.routing.ResourceLoader
+import org.http4k.routing.ResourceLoader.Companion.Directory
 import org.http4k.routing.static
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
@@ -46,11 +45,10 @@ fun `latency injection proxy (between 100ms-500ms)`() {
 
 fun `latency reporting proxy`() {
     ProxyHost()
-            .then(ReportRouteLatency { req, ms -> println("$req took $ms")})
-            .then(OkHttp())
+            .then(ReportRouteLatency { req, ms -> println("$req took $ms") })
+            .then(JavaHttpClient())
             .asServer(SunHttp(8000))
-            .start()
-            .block()
+            .start().block()
 }
 
 fun `recording traffic to disk proxy`() {
@@ -69,7 +67,7 @@ fun `replay previously recorded traffic from a disk store`() {
 }
 
 fun `static file server`() {
-    static(ResourceLoader.Directory())
+    static(Directory())
             .asServer(SunHttp(8000))
             .start()
             .block()
