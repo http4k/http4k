20774f6ce916edaf41ef6d8087d07e7bd41e6395
David Denton <denton.david@gmail.com>
2018-08-29 20:58:40 +0100

    micro apps

--- src/docs/cookbook/micro_apps/example.kt
+++ src/docs/cookbook/micro_apps/example.kt
@@ -1,5 +1,9 @@
 package cookbook.micro_apps
 
+import org.http4k.chaos.ChaosBehaviours.Latency
+import org.http4k.chaos.ChaosTriggers.Always
+import org.http4k.chaos.appliedWhen
+import org.http4k.chaos.withChaosControls
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters.PrintRequestAndResponse
@@ -12,6 +16,13 @@ import org.http4k.server.asServer
 import org.http4k.traffic.Replay
 import org.http4k.traffic.Sink.Companion.DiskTree
 
+fun `static file server`() {
+    static(Directory())
+            .asServer(SunHttp(8000))
+            .start()
+            .block()
+}
+
 fun `traffic sniffing proxy`() {
     PrintRequestAndResponse()
             .then(ProxyHost())
@@ -21,9 +32,11 @@ fun `traffic sniffing proxy`() {
             .block()
 }
 
-fun `static file server`() {
-    static(Directory())
-            .asServer(SunHttp(10000))
+fun `latency injection proxy (between 100ms-500ms)`() {
+    ProxyHost()
+            .then(JavaHttpClient())
+            .withChaosControls(Latency().appliedWhen(Always))
+            .asServer(SunHttp(8000))
             .start()
             .block()
 }
@@ -32,7 +45,7 @@ fun `recording traffic to disk proxy`() {
     RecordTo(DiskTree())
             .then(ProxyHost())
             .then(JavaHttpClient())
-            .asServer(SunHttp(9000))
+            .asServer(SunHttp(8000))
             .start()
             .block()
 }
