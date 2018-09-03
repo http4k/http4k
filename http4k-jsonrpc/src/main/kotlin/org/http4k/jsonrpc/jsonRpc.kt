fe451e624b7ca97791fbdde0dec6ed0c7a786010
David Denton <daviddenton@users.noreply.github.com>
2018-09-03 12:40:45 +0300

    Jsonrpc refactor (#175)
    * refactor and rename of Json Rpc classes
    
    * Code cleanup (#174)

---
+++ http4k-jsonrpc/src/main/kotlin/org/http4k/jsonrpc/jsonRpc.kt
@@ -0,0 +1,20 @@
+package org.http4k.jsonrpc
+
+import org.http4k.format.Json
+import org.http4k.format.JsonLibAutoMarshallingJson
+
+object JsonRpc {
+    fun <ROOT : NODE, NODE : Any> auto(json: JsonLibAutoMarshallingJson<ROOT>,
+                                       errorHandler: ErrorHandler = defaultErrorHandler,
+                                       definitions: AutoMethodMappingsBuilder<ROOT>.() -> Unit): JsonRpcService<ROOT, ROOT> =
+            JsonRpcService(json, errorHandler, AutoMethodMappingsBuilder(json).apply(definitions).mappings())
+
+    fun <ROOT : NODE, NODE : Any> manual(json: Json<ROOT, NODE>,
+                                         errorHandler: ErrorHandler = defaultErrorHandler,
+                                         definitions: ManualMethodMappingsBuilder<ROOT, NODE>.() -> Unit): JsonRpcService<ROOT, NODE> =
+            JsonRpcService(json, errorHandler, ManualMethodMappingsBuilder(json).apply(definitions).mappings())
+}
+
+typealias JsonRpcHandler<IN, OUT> = (IN) -> OUT
+
+private val defaultErrorHandler: ErrorHandler = { null }
