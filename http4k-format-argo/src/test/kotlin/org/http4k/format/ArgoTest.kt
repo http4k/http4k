5c6ea6505f838ce792a4d2959049cf380a0c1134
fnordin <fred.nordin@springernature.com>
2018-09-17 11:26:48 +0100

    Fix Argo and Gson json-rpc contract test for invalid request

--- http4k-format-argo/src/test/kotlin/org/http4k/format/ArgoTest.kt
+++ http4k-format-argo/src/test/kotlin/org/http4k/format/ArgoTest.kt
@@ -2,6 +2,7 @@ package org.http4k.format
 
 import argo.jdom.JsonNode
 import argo.jdom.JsonRootNode
+import org.http4k.jsonrpc.ManualMappingJsonRpcServiceContract
 
 class ArgoTest : JsonContract<JsonRootNode, JsonNode>(Argo) {
     override val prettyString = """{
@@ -10,4 +11,4 @@ class ArgoTest : JsonContract<JsonRootNode, JsonNode>(Argo) {
 }
 class ArgoJsonErrorResponseRendererContractTest : JsonErrorResponseRendererContract<JsonRootNode, JsonNode>(Argo)
 class ArgoGenerateDataClassesTest : GenerateDataClassesContract<JsonRootNode, JsonNode>(Argo)
-//class ArgoManualMappingJsonRpcServiceTest : ManualMappingJsonRpcServiceContract<JsonRootNode, JsonNode>(Argo)
+class ArgoManualMappingJsonRpcServiceTest : ManualMappingJsonRpcServiceContract<JsonRootNode, JsonNode>(Argo)
