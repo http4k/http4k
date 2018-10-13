2664ca3bd2ba78116db7bcb0d09d77821e2280ac
Jean-Michel Fayard <jmfayard@gmail.com>
2018-10-13 19:03:23 +0200

    Refactor some build logic to the buildSrc & gradle kotlin-dsl (#185)
    Files buildSrc/src/main/java/{Versions.kt,Libs.kt} generated by kotlinpoet
    and the task
       $ ./gradlew syncLibs
    from the plugin gradle-kotlin-dsl-libs
    
    Signed-off-by: Jean-Michel Fayard <jmfayard@gmail.com>

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,7 @@
+object Config {
+
+  val TestDependencies: List<String> = listOf(
+    Libs.junit_jupiter_api, Libs.junit_jupiter_engine, Libs.rerunner_jupiter, Libs.kotlin_reflect, Libs.hamkrest
+  )
+
+}
