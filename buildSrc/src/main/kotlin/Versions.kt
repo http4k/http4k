f0ce5a07daa52d4f183514445687b6b4e01dc86a
Ivan Sanchez <ivan@gourame.com>
2018-10-24 15:38:50 +0100

    Remove misleading comments

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,43 +5,43 @@
  *   `$ ./gradlew syncLibs`
  * to update this file */
 object Versions {
-    const val alpn_boot: String = "8.1.12.v20180117" // up-to-date
+    const val alpn_boot: String = "8.1.12.v20180117"
 
-    const val argo: String = "5.4" // up-to-date
+    const val argo: String = "5.4"
 
-    const val cobertura: String = "2.1.1" // up-to-date
+    const val cobertura: String = "2.1.1"
 
-    const val com_github_kt3k_coveralls_gradle_plugin: String = "2.8.2" // up-to-date
+    const val com_github_kt3k_coveralls_gradle_plugin: String = "2.8.2"
 
-    const val com_jfrog_bintray_gradle_plugin: String = "1.8.4" // up-to-date
+    const val com_jfrog_bintray_gradle_plugin: String = "1.8.4"
 
-    const val commons_pool2: String = "2.6.0" // up-to-date
+    const val commons_pool2: String = "2.6.0"
 
-    const val coveralls_gradle_plugin: String = "2.8.2" // up-to-date
+    const val coveralls_gradle_plugin: String = "2.8.2"
 
-    const val dokka_gradle_plugin: String = "0.9.17" // up-to-date
+    const val dokka_gradle_plugin: String = "0.9.17"
 
-    const val gradle_cobertura_plugin: String = "2.5.4" // up-to-date
+    const val gradle_cobertura_plugin: String = "2.5.4"
 
-    const val gson: String = "2.8.5" // up-to-date
+    const val gson: String = "2.8.5"
 
-    const val hamkrest: String = "1.6.0.0" // up-to-date
+    const val hamkrest: String = "1.6.0.0"
 
-    const val handlebars: String = "4.1.0" // up-to-date
+    const val handlebars: String = "4.1.0"
 
     const val http2_server: String = "9.4.12.v20180830"
 
-    const val httpasyncclient: String = "4.1.4" // up-to-date
+    const val httpasyncclient: String = "4.1.4"
 
-    const val httpclient: String = "4.5.6" // up-to-date
+    const val httpclient: String = "4.5.6"
 
-    const val httpcore: String = "4.4.10" // up-to-date
+    const val httpcore: String = "4.4.10"
 
-    const val jackson_module_kotlin: String = "2.9.7" // up-to-date
+    const val jackson_module_kotlin: String = "2.9.7"
 
-    const val java_websocket: String = "1.3.9" // up-to-date
+    const val java_websocket: String = "1.3.9"
 
-    const val javax_servlet_api: String = "4.0.1" // up-to-date
+    const val javax_servlet_api: String = "4.0.1"
 
     const val javax_websocket_server_impl: String = "9.4.12.v20180830"
 
@@ -56,55 +56,55 @@ object Versions {
     const val jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin: String =
             "0.2.4" // exceed the version found: 0.2.3
 
-    const val json: String = "20180813" // up-to-date
+    const val json: String = "20180813"
 
-    const val jsoup: String = "1.11.3" // up-to-date
+    const val jsoup: String = "1.11.3"
 
-    const val junit_jupiter_api: String = "5.3.1" // up-to-date
+    const val junit_jupiter_api: String = "5.3.1"
 
-    const val junit_jupiter_engine: String = "5.3.1" // up-to-date
+    const val junit_jupiter_engine: String = "5.3.1"
 
-    const val kotlin_gradle_plugin: String = "1.2.71" // up-to-date
+    const val kotlin_gradle_plugin: String = "1.2.71"
 
-    const val kotlin_reflect: String = "1.2.71" // up-to-date
+    const val kotlin_reflect: String = "1.2.71"
 
-    const val kotlin_scripting_compiler_embeddable: String = "1.2.71" // up-to-date
+    const val kotlin_scripting_compiler_embeddable: String = "1.2.71"
 
-    const val kotlin_stdlib_jdk8: String = "1.2.71" // up-to-date
+    const val kotlin_stdlib_jdk8: String = "1.2.71"
 
     const val micrometer_core: String = "1.0.6" // available milestone: "1.0.7"
 
-    const val moshi: String = "1.7.0" // up-to-date
+    const val moshi: String = "1.7.0"
 
-    const val moshi_kotlin: String = "1.7.0" // up-to-date
+    const val moshi_kotlin: String = "1.7.0"
 
     const val nebula_provided_base_gradle_plugin: String = "3.0.3" // available milestone: "4.0.1"
 
-    const val net_saliman_cobertura_gradle_plugin: String = "2.5.4" // up-to-date
+    const val net_saliman_cobertura_gradle_plugin: String = "2.5.4"
 
     const val netty_codec_http2: String = "4.1.23.Final" // available milestone: "5.0.0.Alpha2"
 
-    const val okhttp: String = "3.11.0" // up-to-date
+    const val okhttp: String = "3.11.0"
 
-    const val pebble: String = "3.0.5" // up-to-date
+    const val pebble: String = "3.0.5"
 
-    const val rerunner_jupiter: String = "1.1.1" // up-to-date
+    const val rerunner_jupiter: String = "1.1.1"
 
-    const val resilience4j_bulkhead: String = "0.13.1" // up-to-date
+    const val resilience4j_bulkhead: String = "0.13.1"
 
-    const val resilience4j_circuitbreaker: String = "0.13.1" // up-to-date
+    const val resilience4j_circuitbreaker: String = "0.13.1"
 
-    const val resilience4j_ratelimiter: String = "0.13.1" // up-to-date
+    const val resilience4j_ratelimiter: String = "0.13.1"
 
-    const val resilience4j_retry: String = "0.13.1" // up-to-date
+    const val resilience4j_retry: String = "0.13.1"
 
-    const val selenium_api: String = "3.14.0" // up-to-date
+    const val selenium_api: String = "3.14.0"
 
-    const val thymeleaf: String = "3.0.10.RELEASE" // up-to-date
+    const val thymeleaf: String = "3.0.10.RELEASE"
 
-    const val undertow_core: String = "2.0.13.Final" // up-to-date
+    const val undertow_core: String = "2.0.13.Final"
 
-    const val undertow_servlet: String = "2.0.13.Final" // up-to-date
+    const val undertow_servlet: String = "2.0.13.Final"
 
     /**
      *
