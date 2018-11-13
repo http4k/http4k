573f350c880284351a88055f8d7361ca6ee34383
David Denton <denton.david@gmail.com>
2018-11-13 07:39:52 +0000

    upgrades

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -77,7 +77,7 @@ object Versions {
 
     const val alpn_boot: String = "8.1.13.v20181017"
 
-    const val selenium_api: String = "3.41.0"
+    const val selenium_api: String = "3.14.0"
 
     const val thymeleaf: String = "3.0.11.RELEASE"
 
