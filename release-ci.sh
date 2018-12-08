f5566b75b8bc9db861e3b248e35cd5877985a4fa
David Denton <denton.david@gmail.com>
2018-12-08 18:56:30 +0000

    add to release

--- release-ci.sh
+++ release-ci.sh
@@ -48,6 +48,7 @@ echo "Attempting to release $LOCAL_VERSION (old version $BINTRAY_VERSION)"
     :http4k-security-oauth:bintrayUpload \
     :http4k-server-apache:bintrayUpload \
     :http4k-server-jetty:bintrayUpload \
+    :http4k-server-ktorcio:bintrayUpload \
     :http4k-server-netty:bintrayUpload \
     :http4k-server-undertow:bintrayUpload \
     :http4k-serverless-lambda:bintrayUpload \
