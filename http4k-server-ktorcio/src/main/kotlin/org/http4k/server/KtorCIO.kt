d4539db0eac546b672143036673f908e5e3f222e
David Denton <denton.david@gmail.com>
2018-12-08 20:44:27 +0000

    stop barfing on stop

--- http4k-server-ktorcio/src/main/kotlin/org/http4k/server/KtorCIO.kt
+++ http4k-server-ktorcio/src/main/kotlin/org/http4k/server/KtorCIO.kt
@@ -12,33 +12,37 @@ import io.ktor.response.ApplicationResponse
 import io.ktor.response.header
 import io.ktor.response.respondOutputStream
 import io.ktor.server.cio.CIO
+import io.ktor.server.cio.CIOApplicationEngine
 import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.stopServerOnCancellation
 import kotlinx.coroutines.io.jvm.javaio.toInputStream
-import org.http4k.client.JavaHttpClient
+import kotlinx.coroutines.runBlocking
 import org.http4k.core.Headers
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
-import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status.Companion.OK
 import org.http4k.lens.Header
-import java.util.concurrent.TimeUnit.SECONDS
 import io.ktor.http.Headers as KHeaders
 
 @Suppress("EXPERIMENTAL_API_USAGE")
 data class KtorCIO(val port: Int = 8000) : ServerConfig {
 
     override fun toServer(httpHandler: HttpHandler): Http4kServer = object : Http4kServer {
-        private val engine = embeddedServer(CIO, port) {
+        private val engine: CIOApplicationEngine = embeddedServer(CIO, port) {
             intercept(Call) {
                 with(context) { response.fromHttp4K(httpHandler(request.asHttp4k())) }
+                finish()
             }
         }
 
         override fun start() = apply { engine.start() }
 
-        override fun stop() = apply { engine.stop(1, 1, SECONDS) }
+        override fun stop() = apply {
+            runBlocking {
+                engine.stopServerOnCancellation()
+            }
+        }
 
         override fun port() = engine.environment.connectors[0].port
     }
@@ -60,13 +64,4 @@ suspend fun ApplicationResponse.fromHttp4K(response: Response) {
 
 private fun KHeaders.toHttp4kHeaders(): Headers = names().flatMap { name ->
     (getAll(name) ?: emptyList()).map { name to it }
-}
-
-fun main(args: Array<String>) {
-    { r: Request ->
-        Response(OK).body(r.body).header("foo", "bar").header("foo", "bar2")
-    }
-        .asServer(KtorCIO(9000)).start().use {
-            println(JavaHttpClient()(Request(POST, "http://localhost:9000/foo").body("hello")))
-        }
 }
