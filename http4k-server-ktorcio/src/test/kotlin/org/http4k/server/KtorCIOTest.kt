7b1f39c9683bf6fffa77fea5754454467bb21d8d
David Denton <denton.david@gmail.com>
2018-12-09 11:27:29 +0000

    rerandomise ktor port

--- http4k-server-ktorcio/src/test/kotlin/org/http4k/server/KtorCIOTest.kt
+++ http4k-server-ktorcio/src/test/kotlin/org/http4k/server/KtorCIOTest.kt
@@ -2,8 +2,9 @@ package org.http4k.server
 
 import org.http4k.client.ApacheClient
 import org.junit.jupiter.api.Test
+import java.util.Random
 
-class KtorCIOTest : ServerContract({ KtorCIO(9898) }, ApacheClient()) {
+class KtorCIOTest : ServerContract({ KtorCIO(Random().nextInt(1000) + 8000) }, ApacheClient()) {
 
     @Test
     override fun `ok when length already set`() {
