9b625b77bf5dcb7260ded126019c66701b44b817
David Denton <denton.david@gmail.com>
2018-12-29 16:29:00 +0000

    remove reification/inline from RequestContextKey.kt

--- http4k-core/src/main/kotlin/org/http4k/lens/RequestContextKey.kt
+++ http4k-core/src/main/kotlin/org/http4k/lens/RequestContextKey.kt
@@ -9,20 +9,19 @@ import java.util.UUID
 typealias RequestContextLens<T> = BiDiLens<Request, T>
 
 object RequestContextKey {
-
-    inline fun <reified T> required(contexts: RequestContexts, name: String = UUID.randomUUID().toString()): RequestContextLens<T> {
+    fun <T> required(contexts: RequestContexts, name: String = UUID.randomUUID().toString()): RequestContextLens<T> {
         val meta = Meta(true, "context", ObjectParam, name)
         return BiDiLens(meta, { target ->
             contexts[target].let { it[name] ?: throw LensFailure(Missing(meta), target = it) }
         }, { value: T, target: Request -> contexts[target][name] = value; target })
     }
 
-    inline fun <reified T : Any?> optional(contexts: RequestContexts, name: String = UUID.randomUUID().toString()) =
+    fun <T : Any?> optional(contexts: RequestContexts, name: String = UUID.randomUUID().toString()) =
         BiDiLens(Meta(false, "context", ObjectParam, name), { target -> contexts[target][name] },
             { value: T?, target: Request -> contexts[target][name] = value; target }
         )
 
-    inline fun <reified T : Any?> defaulted(contexts: RequestContexts, default: T, name: String = UUID.randomUUID().toString()) =
+    fun <T : Any?> defaulted(contexts: RequestContexts, default: T, name: String = UUID.randomUUID().toString()) =
         BiDiLens(Meta(false, "context", ObjectParam, name), { target -> contexts[target][name] ?: default },
             { value: T, target: Request -> contexts[target][name] = value; target }
         )
