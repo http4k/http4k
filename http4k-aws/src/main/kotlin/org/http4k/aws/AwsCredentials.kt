c0246075c7b8efb872fb288be8e7b661fdd874d5
Darren <darren.hobbs@gmail.com>
2019-01-03 16:31:43 +0000

    Added optional sessionToken to AwsCredentials and include it in the signed request if present
    Signed-off-by: Darren Hobbs <darren.hobbs@gmail.com>

--- http4k-aws/src/main/kotlin/org/http4k/aws/AwsCredentials.kt
+++ http4k-aws/src/main/kotlin/org/http4k/aws/AwsCredentials.kt
@@ -1,3 +1,3 @@
 package org.http4k.aws
 
-data class AwsCredentials(val accessKey: String, val secretKey: String)
+data class AwsCredentials(val accessKey: String, val secretKey: String, val sessionToken: String? = null)
