2beb77d8c29416c2261fe591fe01d3d0fec0cc88
Ivan Sanchez <ivan@gourame.com>
2019-01-06 17:49:21 +0000

    Skip maven release for packages already published

--- release-maven-central.sh
+++ release-maven-central.sh
@@ -18,12 +18,19 @@ LOCAL_VERSION=`jq -r .http4k.version $DIR/version.json`
 function maven_publish {
     local PACKAGE=$1
     local PAYLOAD="{\"username\": \"${SONATYPE_USER}\", \"password\": \"${SONATYPE_KEY}\"}"
-    echo "Publishing $PACKAGE..."
-    RESULT=$(curl -s -X POST -u "$BINTRAY_USER:$BINTRAY_KEY" -H "Content-Type: application/json" --data "$PAYLOAD" "https://bintray.com/api/v1/maven_central_sync/http4k/maven/$PACKAGE/versions/$LOCAL_VERSION")
 
-    if [[ ! "${RESULT}" =~ .*Successful.* ]]; then
-       echo "Failed: ${RESULT}"
-       exit 1
+    local PUBLISHED=$(curl --fail --silent -o /dev/null https://mvnrepository.com/artifact/org.http4k/${PACKAGE}/${LOCAL_VERSION} ; echo $?)
+    
+    if [[ $PUBLISHED == "0" ]]; then
+        echo "$PACKAGE is already published. Skipping"
+    else
+        echo "Publishing $PACKAGE..."
+        RESULT=$(curl -s -X POST -u "$BINTRAY_USER:$BINTRAY_KEY" -H "Content-Type: application/json" --data "$PAYLOAD" "https://bintray.com/api/v1/maven_central_sync/http4k/maven/$PACKAGE/versions/$LOCAL_VERSION")
+
+        if [[ ! "${RESULT}" =~ .*Successful.* ]]; then
+           echo "Failed: ${RESULT}"
+           exit 1
+        fi
     fi
 }
 
