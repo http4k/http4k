369f3a8c3eabb0d85f54409a465170af2b1a2d9a
David Denton <denton.david@gmail.com>
2019-01-23 17:12:25 +0000

    Revert: replace shouldMatch with assertThat

--- http4k-aws/src/test/kotlin/org/http4k/aws/AwsCanonicalRequestTest.kt
+++ http4k-aws/src/test/kotlin/org/http4k/aws/AwsCanonicalRequestTest.kt
@@ -18,36 +18,36 @@ class AwsCanonicalRequestTest {
     fun `creates canonical version of simple request`() {
         val canonical = AwsCanonicalRequest.of(Request(Method.GET, "http://www.google.com/a/b").query("foo", "bar").header("abc", "def"), canonicalPayload)
         assertThat(canonical.value, equalTo("""GET
-    /a/b
-    foo=bar
-    abc:def
+/a/b
+foo=bar
+abc:def
 
-    abc
-    e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"""))
+abc
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"""))
     }
 
     @Test
     fun `normalises path`() {
         val canonical = AwsCanonicalRequest.of(Request(Method.GET, "http://www.google.com/a:b:c/d e/f"), canonicalPayload)
         assertThat(canonical.value, equalTo("""GET
-    /a%3Ab%3Ac/d+e/f
+/a%3Ab%3Ac/d+e/f
 
 
 
 
-    e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"""))
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"""))
     }
 
     @Test
     fun `normalises empty path`() {
         val canonical = AwsCanonicalRequest.of(Request(Method.GET, "http://www.google.com"), canonicalPayload)
         assertThat(canonical.value, equalTo("""GET
-    /
+/
 
 
 
 
-    e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"""))
+e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"""))
     }
 
     @Test
@@ -56,11 +56,11 @@ class AwsCanonicalRequestTest {
         val body = Body(ByteBuffer.wrap(image))
         val canonical = AwsCanonicalRequest.of(Request(Method.GET, "http://www.google.com").body(body), CanonicalPayload(AwsHmacSha256.hash(body.payload.array()), body.payload.array().size.toLong()))
         assertThat(canonical.value, equalTo("""GET
-    /
+/
 
 
 
 
-    0fa4d114b9fbb132f096d727713aab9ea8d415b69c86053b6d2c819c4eb95db6"""))
+0fa4d114b9fbb132f096d727713aab9ea8d415b69c86053b6d2c819c4eb95db6"""))
     }
 }
