609fc1a2c60be69fa4c50600706a4a73cb3f21e7
Ivan Sanchez <ivan@gourame.com>
2019-02-19 14:30:14 -0300

    Formatting

--- http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/AuthenticationCompleteFilter.kt
+++ http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/AuthenticationCompleteFilter.kt
@@ -11,19 +11,16 @@ class AuthenticationCompleteFilter(
     private val persistence: OAuthRequestPersistence
 ) : Filter {
 
-    override fun invoke(next: HttpHandler): HttpHandler {
-        return { request ->
-            val response = next(request)
-            val authorizationRequest = persistence.retrieve(request)
-            val postAuthResponse = if (response.status.successful) {
-                Response(Status.TEMPORARY_REDIRECT)
-                    .header("location", authorizationRequest.redirectUri
-                        .query("code", authorizationCodes.create().value)
-                        .query("state", authorizationRequest.state)
-                        .toString())
-            } else response
-            persistence.clear(authorizationRequest, postAuthResponse)
-        }
+    override fun invoke(next: HttpHandler): HttpHandler = { request ->
+        val response = next(request)
+        val authorizationRequest = persistence.retrieve(request)
+        val postAuthResponse = if (response.status.successful) {
+            Response(Status.TEMPORARY_REDIRECT)
+                .header("location", authorizationRequest.redirectUri
+                    .query("code", authorizationCodes.create().value)
+                    .query("state", authorizationRequest.state)
+                    .toString())
+        } else response
+        persistence.clear(authorizationRequest, postAuthResponse)
     }
-
 }
