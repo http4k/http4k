f9b74ac18ffd6d89063f1c3d1594ed9554e37887
David Denton <denton.david@gmail.com>
2019-02-21 08:59:26 +0000

    add secret consumption to the example

--- src/docs/guide/modules/cloud_native/example_k8s.kt
+++ src/docs/guide/modules/cloud_native/example_k8s.kt
@@ -5,21 +5,18 @@ import org.http4k.cloudnative.Http4kK8sServer
 import org.http4k.cloudnative.asK8sServer
 import org.http4k.cloudnative.env.Environment
 import org.http4k.cloudnative.env.EnvironmentKey
+import org.http4k.cloudnative.env.Secret
 import org.http4k.cloudnative.health.Completed
 import org.http4k.cloudnative.health.Health
 import org.http4k.cloudnative.health.ReadinessCheck
 import org.http4k.cloudnative.health.ReadinessCheckResult
-import org.http4k.core.Filter
+import org.http4k.core.*
 import org.http4k.core.Method.GET
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.Uri
-import org.http4k.core.then
 import org.http4k.filter.ClientFilters
 import org.http4k.filter.DebuggingFilters
 import org.http4k.lens.Lens
+import org.http4k.lens.secret
 import org.http4k.routing.bind
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
@@ -30,17 +27,20 @@ object App {
     // settings
     private val otherServiceUri: Lens<Environment, Uri> = EnvironmentKey.k8s.serviceUriFor("otherservice")
     private val dbRole = EnvironmentKey.required("database.user.role")
+    private val dbPassword = EnvironmentKey.secret().required("database.user.password")
 
     operator fun invoke(env: Environment): Http4kK8sServer {
         // define the main app API - it proxies to the "other" service
         val mainApp = ClientFilters.SetHostFrom(otherServiceUri(env))
-            .then(rewriteUriToLocalhostAsWeDoNotHaveDns) // this line only here to make the example work!
-            .then(JavaHttpClient())
+                .then(rewriteUriToLocalhostAsWeDoNotHaveDns) // this line only here to make the example work!
+                .then(JavaHttpClient())
 
         // define the health app API
         val healthApp = Health(
-            "/config" bind GET to { Response(OK).body(env.toString()) },
-            checks = listOf(DatabaseCheck(RandomlyFailingDatabase(dbRole(env))))
+                "/config" bind GET to { Response(OK).body(env.toString()) },
+                checks = listOf(DatabaseCheck(
+                        RandomlyFailingDatabase(dbRole(env), dbPassword(env)
+                        )))
         )
         return mainApp.asK8sServer(::SunHttp, env, healthApp)
     }
@@ -54,7 +54,12 @@ object App {
 }
 
 // this is a database client that we are going to health check
-class RandomlyFailingDatabase(private val user: String) {
+class RandomlyFailingDatabase(private val user: String, password: Secret) {
+    init {
+        // the secret is a single-shot value whose value will be discarded after use
+        password.use { println("setting up the database connection with creds: $user/$it") }
+    }
+
     fun insertARecord() {
         if (Random(1).nextBoolean()) throw Exception("oh no! $user has no access")
     }
@@ -71,20 +76,21 @@ class DatabaseCheck(private val db: RandomlyFailingDatabase) : ReadinessCheck {
 
 /** file app.properties contains
 database.user.role=admin
+database.user.password=myPassword
  */
 
 fun main() {
     val defaultConfig = Environment.defaults(
-        EnvironmentKey.k8s.SERVICE_PORT of 8000,
-        EnvironmentKey.k8s.HEALTH_PORT of 8001,
-        EnvironmentKey.k8s.serviceUriFor("otherservice") of Uri.of("https://localhost:8000")
+            EnvironmentKey.k8s.SERVICE_PORT of 8000,
+            EnvironmentKey.k8s.HEALTH_PORT of 8001,
+            EnvironmentKey.k8s.serviceUriFor("otherservice") of Uri.of("https://localhost:8000")
     )
 
     // standard chaining order for properties is local file -> JVM -> Environment -> defaults -> boom!
     val k8sPodEnv = Environment.fromResource("app.properties") overrides
-        Environment.JVM_PROPERTIES overrides
-        Environment.ENV overrides
-        defaultConfig
+            Environment.JVM_PROPERTIES overrides
+            Environment.ENV overrides
+            defaultConfig
 
     // the end-server that we will proxy to
     val upstream = { _: Request -> Response(Status.OK).body("HELLO!") }.asServer(SunHttp(9000)).start()
