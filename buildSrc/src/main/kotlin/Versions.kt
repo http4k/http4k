71144a9e2462110fe494db5995537ee75c1e38b3
David Denton <denton.david@gmail.com>
2019-02-24 19:32:10 +0000

    upgrade deps.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,83 +5,83 @@
  *
  * YOU are responsible for updating manually the dependency version. */
 object Versions {
-    const val jackson_module_kotlin: String = "2.9.8"
+    const val jackson_module_kotlin = "2.9.8"
 
-    const val handlebars: String = "4.1.2"
+    const val handlebars = "4.1.2"
 
-    const val com_github_kt3k_coveralls_gradle_plugin: String = "2.8.2"
+    const val com_github_kt3k_coveralls_gradle_plugin = "2.8.2"
 
-    const val gson: String = "2.8.5"
+    const val gson = "2.8.5"
 
-    const val com_jfrog_bintray_gradle_plugin: String = "1.8.4"
+    const val com_jfrog_bintray_gradle_plugin = "1.8.4"
 
-    const val hamkrest: String = "1.7.0.0"
+    const val hamkrest = "1.7.0.0"
 
-    const val com_squareup_moshi: String = "1.8.0"
+    const val com_squareup_moshi = "1.8.0"
 
-    const val okhttp: String = "3.12.1"
+    const val okhttp = "3.12.1" //available: "3.13.1"
 
-    const val rerunner_jupiter: String = "1.1.1"
+    const val rerunner_jupiter = "1.1.1"
 
-    const val io_github_resilience4j: String = "0.13.2"
+    const val io_github_resilience4j = "0.13.2"
 
-    const val ktor_server_cio: String = "1.1.2"
+    const val ktor_server_cio = "1.1.3"
 
-    const val micrometer_core: String = "1.1.2"
+    const val micrometer_core = "1.1.3"
 
-    const val netty_codec_http2: String = "4.1.33.Final"
+    const val netty_codec_http2 = "4.1.33.Final"
 
-    const val pebble: String = "3.0.7"
+    const val pebble = "3.0.8"
 
-    const val io_undertow: String = "2.0.17.Final"
+    const val io_undertow = "2.0.19.Final"
 
-    const val javax_servlet_api: String = "4.0.1"
+    const val javax_servlet_api = "4.0.1"
 
-    const val jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin: String = "0.2.6"
+    const val jmfayard_github_io_gradle_kotlin_dsl_libs_gradle_plugin = "0.2.6"
 
-    const val nebula_provided_base_gradle_plugin: String = "3.0.3" //available: "5.0.0"
+    const val nebula_provided_base_gradle_plugin = "3.0.3" //available: "5.0.0"
 
-    const val net_saliman_cobertura_gradle_plugin: String = "2.6.0"
+    const val net_saliman_cobertura_gradle_plugin = "2.6.0"
 
-    const val gradle_cobertura_plugin: String = "2.6.0"
+    const val gradle_cobertura_plugin = "2.6.0"
 
-    const val argo: String = "5.4"
+    const val argo = "5.5"
 
-    const val cobertura: String = "2.1.1"
+    const val cobertura = "2.1.1"
 
-    const val commons_pool2: String = "2.6.0"
+    const val commons_pool2 = "2.6.1"
 
-    const val httpasyncclient: String = "4.1.4"
+    const val httpasyncclient = "4.1.4"
 
-    const val httpclient: String = "4.5.7"
+    const val httpclient = "4.5.7"
 
-    const val httpcore: String = "4.4.11"
+    const val httpcore = "4.4.11"
 
-    const val http2_server: String = "9.4.14.v20181114"
+    const val http2_server = "9.4.15.v20190215"
 
-    const val javax_websocket_server_impl: String = "9.4.14.v20181114"
+    const val javax_websocket_server_impl = "9.4.15.v20190215"
 
-    const val org_eclipse_jetty: String = "9.4.14.v20181114"
+    const val org_eclipse_jetty = "9.4.15.v20190215"
 
-    const val java_websocket: String = "1.3.9"
+    const val java_websocket = "1.4.0"
 
-    const val dokka_gradle_plugin: String = "0.9.17"
+    const val dokka_gradle_plugin = "0.9.17"
 
-    const val org_jetbrains_kotlin: String = "1.3.20"
+    const val org_jetbrains_kotlin = "1.3.21"
 
-    const val json: String = "20180813"
+    const val json = "20180813"
 
-    const val jsoup: String = "1.11.3"
+    const val jsoup = "1.11.3"
 
-    const val org_junit_jupiter: String = "5.3.2"
+    const val org_junit_jupiter = "5.4.0"
 
-    const val coveralls_gradle_plugin: String = "2.8.2"
+    const val coveralls_gradle_plugin = "2.8.2"
 
-    const val alpn_boot: String = "8.1.13.v20181017"
+    const val alpn_boot = "8.1.13.v20181017"
 
-    const val selenium_api: String = "3.141.59"
+    const val selenium_api = "3.141.59"
 
-    const val thymeleaf: String = "3.0.11.RELEASE"
+    const val thymeleaf = "3.0.11.RELEASE"
 
     /**
      *
@@ -89,12 +89,12 @@ object Versions {
      *      ./gradle/wrapper/gradle-wrapper.properties
      */
     object Gradle {
-        const val runningVersion: String = "5.0"
+        const val runningVersion = "5.0"
 
-        const val currentVersion: String = "5.1.1"
+        const val currentVersion = "5.2.1"
 
-        const val nightlyVersion: String = "5.3-20190126000049+0000"
+        const val nightlyVersion = "5.4-20190224000056+0000"
 
-        const val releaseCandidate: String = ""
+        const val releaseCandidate = ""
     }
 }
