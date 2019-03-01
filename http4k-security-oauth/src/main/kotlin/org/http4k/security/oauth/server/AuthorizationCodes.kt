af02643f8a40a9a0d705b2987698ad1db2f415eb
Ivan Sanchez <ivan@gourame.com>
2019-03-01 19:51:25 +0000

    Add doc note regarding authorization code generation

--- http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/AuthorizationCodes.kt
+++ http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/AuthorizationCodes.kt
@@ -9,6 +9,8 @@ import java.time.Instant
 interface AuthorizationCodes {
     /**
      * Create new authorization code to be given to client after the user successfully authorize access
+     * The generated authorization code needs to be associated with the clientId and redirectUri for later verification.
+     * It should also be associated with a given expire date (recommended to be shorter than 10 minutes)
      */
     fun create(clientId: ClientId, redirectUri: Uri): AuthorizationCode
 
