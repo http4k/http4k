e305375a812744b591ab2d11725b0b2a21143f1a
David Denton <denton.david@gmail.com>
2019-03-09 10:26:09 +0000

    add none validator

--- http4k-core/src/main/kotlin/org/http4k/lens/Validator.kt
+++ http4k-core/src/main/kotlin/org/http4k/lens/Validator.kt
@@ -1,12 +1,18 @@
 package org.http4k.lens
 
+/**
+ * Runs through a list of lenses and extracts the values from each one, collecting the results
+ */
 enum class Validator(private val actOn: (List<Failure>) -> List<Failure>) {
     Strict({ if (it.isNotEmpty()) throw LensFailure(it, target = null) else it }),
     Feedback({ it }),
-    Ignore({ emptyList<Failure>() });
+    Ignore({ emptyList<Failure>() }),
+    None({ emptyList<Failure>() }) {
+        override fun <T : Any> invoke(entity: T, vararg lenses: Lens<T, *>) = emptyList<Failure>()
+    };
 
-    operator fun <T : Any> invoke(entity: T, vararg formFields: Lens<T, *>): List<Failure> =
-        formFields.fold(emptyList<Failure>()) { memo, next ->
+    open operator fun <T : Any> invoke(entity: T, vararg lenses: Lens<T, *>): List<Failure> =
+        lenses.fold(emptyList<Failure>()) { memo, next ->
             try {
                 next(entity)
                 memo
--- http4k-core/src/test/kotlin/org/http4k/lens/ValidatorTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/lens/ValidatorTest.kt
@@ -5,6 +5,7 @@ import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.throws
 import org.http4k.lens.Validator.Feedback
 import org.http4k.lens.Validator.Ignore
+import org.http4k.lens.Validator.None
 import org.http4k.lens.Validator.Strict
 import org.junit.jupiter.api.Test
 
@@ -12,20 +13,31 @@ class ValidatorTest {
     private val formFields = FormField.required("bob")
 
     @Test
-    fun `strict`() {
+    fun strict() {
         assertThat({
             Strict(WebForm(), formFields)
         }, throws<LensFailure>())
     }
 
     @Test
-    fun `feedback`() {
+    fun feedback() {
         assertThat(Feedback(WebForm(), formFields), equalTo(listOf<Failure>(Missing(formFields.meta))))
     }
 
     @Test
-    fun `ignore`() {
+    fun ignore() {
         assertThat(Ignore(WebForm(), formFields), equalTo(emptyList()))
     }
 
+    @Test
+    fun none() {
+        var called = false
+        val formFields = FormField.map {
+            called = true
+            it
+        }.required("bob")
+
+        assertThat(None(WebForm(mapOf("bob" to listOf("asd"))), formFields), equalTo(emptyList()))
+        assertThat(called, equalTo(false))
+    }
 }
