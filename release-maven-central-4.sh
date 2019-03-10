1d5fc122f0adfdd0ee5e01105015373804129f50
David Denton <denton.david@gmail.com>
2019-03-10 17:22:50 +0000

    made freemarker templates last in the queue to get maven synced.

--- release-maven-central-4.sh
+++ release-maven-central-4.sh
@@ -40,12 +40,12 @@ ensure_release_commit
 
 echo "Making $LOCAL_VERSION available in Maven central..."
 
-maven_publish "http4k-template-dust"
-maven_publish "http4k-template-freemarker"
-maven_publish "http4k-template-handlebars"
-maven_publish "http4k-template-pebble"
-maven_publish "http4k-template-thymeleaf"
 maven_publish "http4k-cloudnative"
 maven_publish "http4k-resilience4j"
 maven_publish "http4k-security-oauth"
 maven_publish "http4k-metrics-micrometer"
+maven_publish "http4k-template-dust"
+maven_publish "http4k-template-handlebars"
+maven_publish "http4k-template-pebble"
+maven_publish "http4k-template-thymeleaf"
+maven_publish "http4k-template-freemarker"
