187e3ff3706331d8ba30515b6f6fb42c105b3891
David Denton <denton.david@gmail.com>
2019-03-12 11:34:22 +0000

    doc

--- http4k-cloudnative/src/main/kotlin/org/http4k/cloudnative/UpstreamRequestFailed.kt
+++ http4k-cloudnative/src/main/kotlin/org/http4k/cloudnative/UpstreamRequestFailed.kt
@@ -3,6 +3,8 @@ package org.http4k.cloudnative
 /**
  * This hierarchy of exceptions should be used to indicate that an upstream remote system has failed with a
  * non-successful status code which caused us to stop processing. They are designed to be used with the
+ * Server and Client filters which will allow automatic handling and propagation of erroneous responses from
+ * upstream.
  */
 open class UpstreamRequestFailed(message: String) : RuntimeException(message)
 
