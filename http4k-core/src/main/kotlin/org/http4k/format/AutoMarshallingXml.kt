aca0e35179ba56660365f55e750cf4a2f2add9f3
David Denton <denton.david@gmail.com>
2019-03-15 08:47:58 +0000

    rejig methods

--- http4k-core/src/main/kotlin/org/http4k/format/AutoMarshallingXml.kt
+++ http4k-core/src/main/kotlin/org/http4k/format/AutoMarshallingXml.kt
@@ -11,14 +11,17 @@ import org.http4k.lens.httpBodyRoot
 import kotlin.reflect.KClass
 
 abstract class AutoMarshallingXml {
-    abstract fun Any.asXmlString(): String
+    abstract fun <T : Any> asA(input: String, target: KClass<T>): T
 
     inline fun <reified T : Any> String.asA(): T = asA(this, T::class)
 
-    abstract fun <T : Any> asA(input: String, target: KClass<T>): T
-
     @JvmName("stringAsA")
     fun <T : Any> String.asA(target: KClass<T>): T = asA(this, target)
+
+    abstract fun Any.asXmlString(): String
+
+    @JvmName("anyAsXmlString")
+    fun asXmlString(input: Any): String = input.asXmlString()
 
     inline fun <reified T : Any> Body.Companion.auto(description: String? = null, contentNegotiation: ContentNegotiation = ContentNegotiation.None): BiDiBodyLensSpec<T> =
         httpBodyRoot(listOf(Meta(true, "body", ObjectParam, "body", description)), APPLICATION_XML, contentNegotiation)
