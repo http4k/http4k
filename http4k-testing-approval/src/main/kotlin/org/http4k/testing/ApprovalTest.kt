3fa2880a41f65e4d36693cd67ca4fafa10dab6ef
David Denton <denton.david@gmail.com>
2019-03-24 19:38:13 +0000

    add support for HTML and XML approval tests

--- http4k-testing-approval/src/main/kotlin/org/http4k/testing/ApprovalTest.kt
+++ http4k-testing-approval/src/main/kotlin/org/http4k/testing/ApprovalTest.kt
@@ -1,9 +1,14 @@
 package org.http4k.testing
 
-import argo.saj.InvalidSyntaxException
+import com.github.underscore.lodash.Json
+import com.github.underscore.lodash.Json.JsonStringBuilder.Step.FOUR_SPACES
+import com.github.underscore.lodash.U.formatJson
+import com.github.underscore.lodash.U.formatXml
+import org.http4k.core.ContentType
 import org.http4k.core.ContentType.Companion.APPLICATION_JSON
+import org.http4k.core.ContentType.Companion.APPLICATION_XML
+import org.http4k.core.ContentType.Companion.TEXT_HTML
 import org.http4k.core.HttpMessage
-import org.http4k.format.Argo
 import org.http4k.lens.Header.CONTENT_TYPE
 import org.http4k.testing.ApprovalContent.Companion.HttpBodyOnly
 import org.http4k.testing.TestNamer.Companion.ClassAndMethod
@@ -15,6 +20,7 @@ import org.junit.jupiter.api.extension.ParameterContext
 import org.junit.jupiter.api.extension.ParameterResolver
 import org.opentest4j.AssertionFailedError
 import java.io.File
+
 
 /**
  * Base JUnit extension for injecting an Approver into a JUnit5 test-case. Implement this
@@ -49,25 +55,53 @@ class ApprovalTest : BaseApprovalTest {
 }
 
 /**
- * Approval JUnit5 extension configured to compare prettified-JSON messages.
+ * Content-type aware Approval JUnit5 extension.
  */
-class JsonApprovalTest : BaseApprovalTest {
+abstract class ContentTypeApprovalTest(private val contentType: ContentType) : BaseApprovalTest {
     override fun approverFor(context: ExtensionContext) = object : Approver {
+        override fun <T : HttpMessage> invoke(fn: () -> T) = delegate {
+            fn().apply { assertEquals(contentType, CONTENT_TYPE(this)) }
+        }
+
         private val delegate = NamedResourceApprover(
             ClassAndMethod.nameFor(context.requiredTestClass, context.requiredTestMethod),
-            HttpBodyOnly {
-                try {
-                    Argo.prettify(it)
-                } catch (e: InvalidSyntaxException) {
-                    throw AssertionFailedError("Invalid JSON generated", "<valid JSON>", it)
-                }
-            },
+            HttpBodyOnly(::format),
             FileSystemApprovalSource(File("src/test/resources"))
         )
+    }
 
-        override fun <T : HttpMessage> invoke(fn: () -> T) =
-            delegate {
-                fn().apply { assertEquals(APPLICATION_JSON, CONTENT_TYPE(this)) }
-            }
+    abstract fun format(input: String): String
+}
+
+/**
+ * Approval JUnit5 extension configured to compare prettified-JSON messages.
+ */
+class JsonApprovalTest : ContentTypeApprovalTest(APPLICATION_JSON) {
+    override fun format(input: String) = try {
+        formatJson(input, FOUR_SPACES)
+    } catch (e: Json.ParseException) {
+        throw AssertionFailedError("Invalid JSON generated", "<valid JSON>", input)
+    }
+}
+
+/**
+ * Approval JUnit5 extension configured to compare prettified-HTML messages.
+ */
+class HtmlApprovalTest : ContentTypeApprovalTest(TEXT_HTML) {
+    override fun format(input: String) = try {
+        formatXml(input)
+    } catch (e: IllegalArgumentException) {
+        throw AssertionFailedError("Invalid HTML generated", "<valid HTML>", input)
+    }
+}
+
+/**
+ * Approval JUnit5 extension configured to compare prettified-XML messages.
+ */
+class XmlApprovalTest : ContentTypeApprovalTest(APPLICATION_XML) {
+    override fun format(input: String) = try {
+        formatXml(input)
+    } catch (e: IllegalArgumentException) {
+        throw AssertionFailedError("Invalid HTML generated", "<valid HTML>", input)
     }
 }
