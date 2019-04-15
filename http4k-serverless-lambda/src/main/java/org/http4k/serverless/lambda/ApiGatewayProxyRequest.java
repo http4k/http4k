992379118635b6071988754eb2526d86eab7ae7c
Erwin Carlos Debusschere Luoni <Charlyzzz@users.noreply.github.com>
2019-04-14 23:09:28 -0300

    Makes ApiGatewayProxyRequest a POJO

--- http4k-serverless-lambda/src/main/java/org/http4k/serverless/lambda/ApiGatewayProxyRequest.java
+++ http4k-serverless-lambda/src/main/java/org/http4k/serverless/lambda/ApiGatewayProxyRequest.java
@@ -9,4 +9,44 @@ public class ApiGatewayProxyRequest {
     public Map<String, String> headers = Collections.emptyMap();
     public Map<String, String> queryStringParameters = Collections.emptyMap();
     public String body = "";
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getHttpMethod() {
+        return httpMethod;
+    }
+
+    public void setHttpMethod(String httpMethod) {
+        this.httpMethod = httpMethod;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public Map<String, String> getQueryStringParameters() {
+        return queryStringParameters;
+    }
+
+    public void setQueryStringParameters(Map<String, String> queryStringParameters) {
+        this.queryStringParameters = queryStringParameters;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
