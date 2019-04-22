b01b6d4733fe4fb2496542c61707c32e7e674a19
Ivan Sanchez <ivan@gourame.com>
2019-04-22 09:29:27 -0300

    Extract OAuthServerTest setup to its own file

--- http4k-security-oauth/src/test/kotlin/org/http4k/security/oauth/server/OAuthServerTest.kt
+++ http4k-security-oauth/src/test/kotlin/org/http4k/security/oauth/server/OAuthServerTest.kt
@@ -2,29 +2,23 @@ package org.http4k.security.oauth.server
 
 import com.natpryce.hamkrest.and
 import com.natpryce.hamkrest.assertion.assertThat
-import org.http4k.core.*
+import org.http4k.core.Filter
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
+import org.http4k.core.NoOp
+import org.http4k.core.Request
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.Status.Companion.TEMPORARY_REDIRECT
 import org.http4k.core.body.form
+import org.http4k.core.then
 import org.http4k.filter.ClientFilters
-import org.http4k.filter.DebuggingFilters
 import org.http4k.hamkrest.hasBody
 import org.http4k.hamkrest.hasStatus
-import org.http4k.routing.RoutingHttpHandler
-import org.http4k.routing.bind
-import org.http4k.routing.routes
-import org.http4k.security.InsecureCookieBasedOAuthPersistence
-import org.http4k.security.OAuthProvider
-import org.http4k.security.OAuthProviderConfig
-import org.http4k.util.FixedClock
 import org.junit.jupiter.api.Test
 
 
 class OAuthServerTest {
 
-    private val debug = true
+    private val debug = false
 
     @Test
     fun `can follow authorization code flow`() {
@@ -75,69 +69,4 @@ class OAuthServerTest {
         val postConfirmationResponse = browserWithRedirection(Request(POST, verifyPage))
         assertThat(postConfirmationResponse, hasStatus(OK) and hasBody("user resource"))
     }
-
-    private fun customOauthAuthorizationServer(): RoutingHttpHandler {
-        val server = OAuthServer(
-            tokenPath = "/oauth2/token",
-            authRequestTracking = DummyOAuthAuthRequestTracking(),
-            clientValidator = DummyClientValidator(),
-            authorizationCodes = InMemoryAuthorizationCodes(FixedClock),
-            accessTokens = DummyAccessTokens(),
-            clock = FixedClock
-        )
-
-        return routes(
-            server.tokenRoute,
-            "/my-login-page" bind GET to server.authenticationStart.then { Response(OK).body("Please authenticate") },
-            "/my-login-page" bind POST to server.authenticationComplete.then { Response(OK) }
-        )
-    }
-
-    private fun customOauthAuthorizationServerWithPersistence(): RoutingHttpHandler {
-        val requestPersistence = InsecureCookieBasedAuthRequestTracking()
-
-        val server = OAuthServer(
-            tokenPath = "/oauth2/token",
-            authRequestTracking = requestPersistence,
-            clientValidator = DummyClientValidator(),
-            authorizationCodes = InMemoryAuthorizationCodes(FixedClock),
-            accessTokens = DummyAccessTokens(),
-            clock = FixedClock
-        )
-
-        return routes(
-            server.tokenRoute,
-            "/my-login-page" bind GET to server.authenticationStart.then { Response(OK).body("Please authenticate") },
-            "/my-login-page" bind POST to { Response(TEMPORARY_REDIRECT).header("location", "/verify-scope") },
-            "/verify-scope" bind GET to {
-                val flow = requestPersistence.resolveAuthRequest(it) ?: error("flow was not persisted")
-                Response(OK).body("Allow ${flow.client.value} to access ${flow.scopes.joinToString(" and ")}?")
-            },
-            "/verify-scope" bind POST to server.authenticationComplete.then { Response(OK) }
-        )
-    }
-
-    private fun oauthClientApp(tokenClient: HttpHandler, debug: Boolean): RoutingHttpHandler {
-        val persistence = InsecureCookieBasedOAuthPersistence("oauthTest")
-
-        val oauthProvider = OAuthProvider(
-            OAuthProviderConfig(Uri.of("http://irrelevant"),
-                "/my-login-page", "/oauth2/token",
-                Credentials("my-app", "somepassword"),
-                Uri.of("https://irrelevant")),
-            debugFilter(debug).then(tokenClient),
-            Uri.of("/my-callback"),
-            listOf("name", "age"),
-            persistence
-        )
-
-        return routes(
-            "/my-callback" bind GET to oauthProvider.callback,
-            "/a-protected-resource" bind GET to oauthProvider.authFilter.then { Response(Status.OK).body("user resource") }
-        )
-    }
 }
-
-private fun debugFilter(active: Boolean) = Filter.switchable(active, DebuggingFilters.PrintRequestAndResponse())
-private fun Filter.Companion.switchable(active: Boolean, next: Filter) = if (active) next else Filter.NoOp
-private operator fun RoutingHttpHandler.plus(other: RoutingHttpHandler) = { request: Request -> routes(this, other)(request) }
---
+++ http4k-security-oauth/src/test/kotlin/org/http4k/security/oauth/server/setup.kt
@@ -0,0 +1,77 @@
+package org.http4k.security.oauth.server
+
+import org.http4k.core.*
+import org.http4k.filter.DebuggingFilters
+import org.http4k.routing.RoutingHttpHandler
+import org.http4k.routing.bind
+import org.http4k.routing.routes
+import org.http4k.security.InsecureCookieBasedOAuthPersistence
+import org.http4k.security.OAuthProvider
+import org.http4k.security.OAuthProviderConfig
+import org.http4k.util.FixedClock
+
+fun customOauthAuthorizationServer(): RoutingHttpHandler {
+    val server = OAuthServer(
+            tokenPath = "/oauth2/token",
+            authRequestTracking = DummyOAuthAuthRequestTracking(),
+            clientValidator = DummyClientValidator(),
+            authorizationCodes = InMemoryAuthorizationCodes(FixedClock),
+            accessTokens = DummyAccessTokens(),
+            clock = FixedClock
+    )
+
+    return routes(
+            server.tokenRoute,
+            "/my-login-page" bind Method.GET to server.authenticationStart.then { Response(Status.OK).body("Please authenticate") },
+            "/my-login-page" bind Method.POST to server.authenticationComplete.then { Response(Status.OK) }
+    )
+}
+
+fun customOauthAuthorizationServerWithPersistence(): RoutingHttpHandler {
+    val requestPersistence = InsecureCookieBasedAuthRequestTracking()
+
+    val server = OAuthServer(
+            tokenPath = "/oauth2/token",
+            authRequestTracking = requestPersistence,
+            clientValidator = DummyClientValidator(),
+            authorizationCodes = InMemoryAuthorizationCodes(FixedClock),
+            accessTokens = DummyAccessTokens(),
+            clock = FixedClock
+    )
+
+    return routes(
+            server.tokenRoute,
+            "/my-login-page" bind Method.GET to server.authenticationStart.then { Response(Status.OK).body("Please authenticate") },
+            "/my-login-page" bind Method.POST to { Response(Status.TEMPORARY_REDIRECT).header("location", "/verify-scope") },
+            "/verify-scope" bind Method.GET to {
+                val flow = requestPersistence.resolveAuthRequest(it) ?: error("flow was not persisted")
+                Response(Status.OK).body("Allow ${flow.client.value} to access ${flow.scopes.joinToString(" and ")}?")
+            },
+            "/verify-scope" bind Method.POST to server.authenticationComplete.then { Response(Status.OK) }
+    )
+}
+
+fun oauthClientApp(tokenClient: HttpHandler, debug: Boolean): RoutingHttpHandler {
+    val persistence = InsecureCookieBasedOAuthPersistence("oauthTest")
+
+    val oauthProvider = OAuthProvider(
+            OAuthProviderConfig(Uri.of("http://irrelevant"),
+                    "/my-login-page", "/oauth2/token",
+                    Credentials("my-app", "somepassword"),
+                    Uri.of("https://irrelevant")),
+            debugFilter(debug).then(tokenClient),
+            Uri.of("/my-callback"),
+            listOf("name", "age"),
+            persistence
+    )
+
+    return routes(
+            "/my-callback" bind Method.GET to oauthProvider.callback,
+            "/a-protected-resource" bind Method.GET to oauthProvider.authFilter.then { Response(Status.OK).body("user resource") }
+    )
+}
+
+fun debugFilter(active: Boolean) = Filter.switchable(active, DebuggingFilters.PrintRequestAndResponse())
+private fun Filter.Companion.switchable(active: Boolean, next: Filter) = if (active) next else Filter.NoOp
+
+operator fun RoutingHttpHandler.plus(other: RoutingHttpHandler) = { request: Request -> routes(this, other)(request) }
