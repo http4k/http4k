d89f2c1cb1e1f1cb888925ab57596a63c0e83687
Ivan Sanchez <ivan@gourame.com>
2019-04-22 11:19:12 -0300

    Add support for consuming access token from JSON response

--- http4k-security-oauth/src/main/kotlin/org/http4k/security/AccessTokenFetcher.kt
+++ http4k-security-oauth/src/main/kotlin/org/http4k/security/AccessTokenFetcher.kt
@@ -1,9 +1,12 @@
 package org.http4k.security
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.http4k.core.*
 import org.http4k.core.Method.POST
 import org.http4k.core.body.form
+import org.http4k.format.Jackson.auto
 import org.http4k.lens.Header
+import org.http4k.lens.Header.CONTENT_TYPE
 
 class AccessTokenFetcher(
     private val api: HttpHandler,
@@ -17,5 +20,17 @@ class AccessTokenFetcher(
         .form("client_id", providerConfig.credentials.user)
         .form("client_secret", providerConfig.credentials.password)
         .form("code", code))
-        .let { if (it.status == Status.OK) AccessTokenContainer(it.bodyString()) else null }
+        .let { if (it.status != Status.OK) null else it }
+        ?.let {
+            if (CONTENT_TYPE(it) == ContentType.APPLICATION_JSON) {
+                accessTokenResponseBody(it).accessToken
+            } else
+                it.bodyString()
+        }?.let(::AccessTokenContainer)
+
+    companion object {
+        val accessTokenResponseBody = Body.auto<AccessTokenResponse>().toLens()
+    }
 }
+
+data class AccessTokenResponse(@JsonProperty("access_token") val accessToken: String)
--- http4k-security-oauth/src/test/kotlin/org/http4k/security/AccessTokenFetcherTest.kt
+++ http4k-security-oauth/src/test/kotlin/org/http4k/security/AccessTokenFetcherTest.kt
@@ -3,12 +3,10 @@ package org.http4k.security
 import com.natpryce.hamkrest.absent
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.core.Credentials
-import org.http4k.core.Request
-import org.http4k.core.Response
+import org.http4k.core.*
 import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.Uri
+import org.http4k.security.AccessTokenFetcher.Companion.accessTokenResponseBody
 import org.junit.jupiter.api.Test
 
 internal class AccessTokenFetcherTest {
@@ -17,6 +15,17 @@ internal class AccessTokenFetcherTest {
     @Test
     fun `can get access token from plain text body`() {
         val api = { _: Request -> Response(OK).body("some-access-token") }
+
+        val fetcher = AccessTokenFetcher(api, Uri.of("irrelevant"), config)
+
+        assertThat(fetcher.fetch("some-code"), equalTo(AccessTokenContainer("some-access-token")))
+    }
+
+
+    @Test
+    fun `can get access token from json body`(){
+        //see https://tools.ietf.org/html/rfc6749#section-4.1.4
+        val api = {_:Request -> Response(OK).with(accessTokenResponseBody of AccessTokenResponse("some-access-token"))}
 
         val fetcher = AccessTokenFetcher(api, Uri.of("irrelevant"), config)
 
