7e7f52f43563ca66b79daae28adaf69ceb171580
Ivan Sanchez <ivan@gourame.com>
2019-04-25 22:24:48 -0300

    Refactor access token error response creation

--- http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/GenerateAccessToken.kt
+++ http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/GenerateAccessToken.kt
@@ -3,6 +3,7 @@ package org.http4k.security.oauth.server
 import com.natpryce.get
 import com.natpryce.map
 import com.natpryce.mapFailure
+import org.http4k.core.ContentType
 import org.http4k.core.HttpHandler
 import org.http4k.core.Request
 import org.http4k.core.Response
@@ -11,10 +12,14 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Status.Companion.UNAUTHORIZED
 import org.http4k.core.with
 import org.http4k.format.AutoMarshallingJson
+import org.http4k.lens.Header
 import org.http4k.security.AccessTokenResponse
 import org.http4k.security.ResponseType.Code
 import org.http4k.security.ResponseType.CodeIdToken
 import org.http4k.security.accessTokenResponseBody
+import org.http4k.security.oauth.server.AccessTokenError.InvalidClient
+import org.http4k.security.oauth.server.AccessTokenError.InvalidGrant
+import org.http4k.security.oauth.server.AccessTokenError.UnsupportedGrantType
 import java.time.Clock
 
 class GenerateAccessToken(
@@ -31,26 +36,26 @@ class GenerateAccessToken(
         val accessTokenRequest = request.accessTokenRequest()
 
         if (accessTokenRequest.grantType != "authorization_code") {
-            return Response(BAD_REQUEST).body(errorResponse(Error.unsupported_grant_type, "${accessTokenRequest.grantType} is not supported"))
+            return Response(BAD_REQUEST).withError(UnsupportedGrantType, "${accessTokenRequest.grantType} is not supported")
         }
 
         if (!clientValidator.validateCredentials(accessTokenRequest.clientId, accessTokenRequest.clientSecret)) {
-            return Response(UNAUTHORIZED).body(errorResponse(Error.invalid_client, "Client id and secret unrecognised"))
+            return Response(UNAUTHORIZED).withError(InvalidClient, "Client id and secret unrecognised")
         }
 
         val code = accessTokenRequest.authorizationCode
         val codeDetails = authorizationCodes.detailsFor(code)
 
         if (codeDetails.expiresAt.isBefore(clock.instant())) {
-            return Response(BAD_REQUEST).body(errorResponse(Error.invalid_grant, "The authorization code has expired"))
+            return Response(BAD_REQUEST).withError(InvalidGrant, "The authorization code has expired")
         }
 
         if (codeDetails.clientId != accessTokenRequest.clientId) {
-            return Response(BAD_REQUEST).body(errorResponse(Error.invalid_grant, "The 'client_id' parameter does not match the authorization request"))
+            return Response(BAD_REQUEST).withError(InvalidGrant, "The 'client_id' parameter does not match the authorization request")
         }
 
         if (codeDetails.redirectUri != accessTokenRequest.redirectUri) {
-            return Response(BAD_REQUEST).body(errorResponse(Error.invalid_grant, "The 'redirect_uri' parameter does not match the authorization request"))
+            return Response(BAD_REQUEST).withError(InvalidGrant, "The 'redirect_uri' parameter does not match the authorization request")
         }
 
         val accessTokenResult = accessTokens.create(code)
@@ -66,19 +71,23 @@ class GenerateAccessToken(
                 }
             }
         }.mapFailure {
-            Response(BAD_REQUEST).body(errorResponse(Error.invalid_grant, "The authorization code has already been used"))
+            Response(BAD_REQUEST).withError(InvalidGrant, "The authorization code has already been used")
         }.get()
     }
 
-    private fun errorResponse(errorCode: Error, errorDescription: String) = json.asJsonString(ErrorResponse(errorCode, errorDescription, documentationUri))
+    private fun Response.withError(error: AccessTokenError, errorDescription: String) =
+        with(Header.CONTENT_TYPE of ContentType.APPLICATION_JSON)
+            .body(json.asJsonString(ErrorResponse(error.rfcValue, errorDescription, documentationUri)))
+
 }
 
-enum class Error {
-    invalid_grant,
-    unsupported_grant_type,
-    invalid_client,
-    invalid_request,
-    unauthorized_client
+// represents errors according to https://tools.ietf.org/html/rfc6749#section-5.2
+private enum class AccessTokenError(val rfcValue: String) {
+    InvalidGrant("invalid_grant"),
+    UnsupportedGrantType("unsupported_grant_type"),
+    InvalidClient("invalid_client"),
+    InvalidRequest("invalid_request"),
+    UnauthorizedClient("unauthorized_client")
 }
 
-private data class ErrorResponse(val error: Error, val error_description: String, val error_uri: String)
+private data class ErrorResponse(val error: String, val error_description: String, val error_uri: String)
