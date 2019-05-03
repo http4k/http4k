0be6e9ae0f0f7ddd4b03d4eaea6eeb4b98c8856f
David Denton <denton.david@gmail.com>
2019-05-03 08:55:18 +0100

    added types to schema output

--- http4k-contract/src/main/kotlin/org/http4k/contract/AutoOpenApi.kt
+++ http4k-contract/src/main/kotlin/org/http4k/contract/AutoOpenApi.kt
@@ -34,11 +34,11 @@ private data class OpenApiPath<NODE>(
 
 private data class OpenApiResponse<NODE>(val description: String?, val schema: NODE?)
 
-private sealed class OpenApiParameter(val `in`: String, val name: String, val required: Boolean, val description: String?)
+private sealed class OpenApiParameter(val type: String, val `in`: String, val name: String, val required: Boolean, val description: String?)
 
-private class SchemaParameter<NODE>(meta: Meta, val schema: NODE) : OpenApiParameter(meta.location, meta.name, meta.required, meta.description)
+private class SchemaParameter<NODE>(meta: Meta, val schema: NODE) : OpenApiParameter(meta.paramMeta.value, meta.location, meta.name, meta.required, meta.description)
 
-private class PrimitiveParameter(meta: Meta) : OpenApiParameter(meta.location, meta.name, meta.required, meta.description)
+private class PrimitiveParameter(meta: Meta) : OpenApiParameter(meta.paramMeta.value, meta.location, meta.name, meta.required, meta.description)
 
 open class AutoOpenApi<out NODE : Any>(
     private val apiInfo: ApiInfo,
--- http4k-format-jackson/src/main/kotlin/org/http4k/util/JacksonJsonSchemaCreator.kt
+++ http4k-format-jackson/src/main/kotlin/org/http4k/util/JacksonJsonSchemaCreator.kt
@@ -3,8 +3,9 @@ package org.http4k.util
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator
 import org.http4k.format.ConfigurableJackson
+import org.http4k.format.Jackson
 
-class JacksonJsonSchemaCreator(private val json: ConfigurableJackson) : JsonSchemaCreator<Any, JsonNode> {
+class JacksonJsonSchemaCreator(private val json: ConfigurableJackson = Jackson) : JsonSchemaCreator<Any, JsonNode> {
     private val jsonSchemaGenerator = JsonSchemaGenerator(json.mapper)
 
     override fun toSchema(obj: Any, overrideDefinitionId: String?): JsonSchema<JsonNode> {
