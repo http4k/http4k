3b21023aa119b0b6f054beeef9a28b2557e30ecf
David Denton <denton.david@gmail.com>
2019-05-04 13:01:15 +0100

    fall back to json schema

--- http4k-format-jackson/src/main/kotlin/org/http4k/util/JacksonJsonSchemaCreator.kt
+++ http4k-format-jackson/src/main/kotlin/org/http4k/util/JacksonJsonSchemaCreator.kt
@@ -6,17 +6,20 @@ import org.http4k.format.ConfigurableJackson
 import org.http4k.format.Jackson
 
 class JacksonJsonSchemaCreator(private val json: ConfigurableJackson = Jackson) : JsonSchemaCreator<Any, JsonNode> {
+    private val jsonToJsonSchema = JsonToJsonSchema(json)
     private val jsonSchemaGenerator = JsonSchemaGenerator(json.mapper)
 
-    override fun toSchema(obj: Any, overrideDefinitionId: String?): JsonSchema<JsonNode> {
-        val node = jsonSchemaGenerator.generateSchema(obj::class.java)
-        println(node)
-        return with(json.asJsonString(node)) {
-            JsonSchema(
-                json {
-                    obj("\$ref" to string("#/definitions/${overrideDefinitionId ?: node.id}"))
-                },
-                setOf("" to json.parse(replace("\"type\":\"any\"", "\"type\":\"string\""))))
+    override fun toSchema(obj: Any, overrideDefinitionId: String?) =
+        when (obj) {
+            is JsonNode -> jsonToJsonSchema.toSchema(obj, overrideDefinitionId)
+            else -> {
+                val node = jsonSchemaGenerator.generateSchema(obj::class.java)
+                val id = "#/definitions/${overrideDefinitionId ?: node.id}"
+                with(json.asJsonString(node)) {
+                    JsonSchema(
+                        json { obj("\$ref" to string(id)) },
+                        setOf(id to json.parse(replace("\"type\":\"any\"", "\"type\":\"string\""))))
+                }
+            }
         }
-    }
 }
