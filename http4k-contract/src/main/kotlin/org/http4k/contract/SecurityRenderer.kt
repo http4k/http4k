7ca132bdef763e0eb019b2b04fb34558e9a2be43
David Denton <denton.david@gmail.com>
2019-05-09 21:11:33 +0100

    add composite renderer

--- http4k-contract/src/main/kotlin/org/http4k/contract/SecurityRenderer.kt
+++ http4k-contract/src/main/kotlin/org/http4k/contract/SecurityRenderer.kt
@@ -7,4 +7,14 @@ typealias Render<NODE> = Json<NODE>.() -> NODE
 interface SecurityRenderer {
     fun <NODE> full(security: Security): Render<NODE>?
     fun <NODE> ref(security: Security): Render<NODE>?
+
+    companion object {
+        operator fun invoke(vararg renderers: SecurityRenderer): SecurityRenderer = object : SecurityRenderer {
+            override fun <NODE> full(security: Security) =
+                renderers.asSequence().mapNotNull { it.full<NODE>(security) }.firstOrNull()
+
+            override fun <NODE> ref(security: Security): Render<NODE>? =
+                renderers.asSequence().mapNotNull { it.ref<NODE>(security) }.firstOrNull()
+        }
+    }
 }
