e586958059f17174397ef7d4669d1c99e517fd68
David Denton <denton.david@gmail.com>
2019-05-11 15:39:48 +0100

    bulking out openapi3 manual version

--- http4k-contract/src/main/kotlin/org/http4k/contract/openapi/v3/StandardApiRenderer.kt
+++ http4k-contract/src/main/kotlin/org/http4k/contract/openapi/v3/StandardApiRenderer.kt
@@ -48,16 +48,38 @@ class StandardApiRenderer<NODE>(private val json: Json<NODE>) : ApiRenderer<Api<
     private fun ApiPath<NODE>.toJson(): NODE =
         json {
             obj(
-                "summary" to string(summary),
-                "description" to (description?.let { string(it) } ?: nullNode()),
-                "tags" to (tags?.map { string(it) }?.let { array(it) } ?: nullNode()),
-                "parameters" to (parameters?.asJson() ?: nullNode()),
-                "requestBody" to string(requestBody.toString()),
-                "responses" to string(responses.toString()),
-                "security" to (security ?: nullNode()),
-                "operationId" to (operationId?.let { string(it) } ?: nullNode())
+                "summary" to summary.asJson(),
+                "description" to (description.asJson()),
+                "tags" to (tags?.map { string(it) }?.let { array(it) }.orNullNode()),
+                "parameters" to (parameters?.asJson().orNullNode()),
+                "requestBody" to (requestBody?.asJson().orNullNode()),
+                "responses" to responses.asJson(),
+                "security" to (security.orNullNode()),
+                "operationId" to operationId.asJson()
             )
         }
+
+
+    private fun RequestContents<NODE>.asJson(): NODE = json {
+        obj("contents" to (content
+            ?.map { it.key to it.value.asJson() }
+            ?.let { obj(it) }.orNullNode()))
+    }
+
+    private fun ResponseContents<NODE>.asJson(): NODE = json {
+        obj(
+            "description" to description.asJson(),
+            "contents" to (content.map { it.key to it.value.asJson() }.let { obj(it) }.orNullNode()))
+    }
+
+    private fun BodyContent.asJson(): NODE = json {
+        obj()
+    }
+
+    @JvmName("responseAsJson")
+    private fun Map<String, ResponseContents<NODE>>.asJson(): NODE = json {
+        obj(map { it.key to it.value.asJson() })
+    }
 
     private fun List<RequestParameter<NODE>>.asJson(): NODE = json {
         array(
@@ -71,8 +93,8 @@ class StandardApiRenderer<NODE>(private val json: Json<NODE>) : ApiRenderer<Api<
             "in" to string(`in`),
             "name" to string(name),
             "required" to boolean(required),
-            "description" to (description?.let { string(it) } ?: nullNode()),
-            "schema" to (schema ?: nullNode())
+            "description" to (description.asJson()),
+            "schema" to (schema.orNullNode())
         )
     }
 
@@ -81,7 +103,7 @@ class StandardApiRenderer<NODE>(private val json: Json<NODE>) : ApiRenderer<Api<
             "in" to string(`in`),
             "name" to string(name),
             "required" to boolean(required),
-            "description" to (description?.let { string(it) } ?: nullNode()),
+            "description" to (description?.let { string(it) }.orNullNode()),
             "schema" to schema
         )
     }
@@ -91,7 +113,7 @@ class StandardApiRenderer<NODE>(private val json: Json<NODE>) : ApiRenderer<Api<
             obj(
                 listOf(
                     "name" to string(name),
-                    "description" to (description?.let { json.string(it) } ?: nullNode())
+                    "description" to (description?.let { json.string(it) }.orNullNode())
                 )
             )
         }
@@ -99,6 +121,9 @@ class StandardApiRenderer<NODE>(private val json: Json<NODE>) : ApiRenderer<Api<
     private fun ApiInfo.asJson() = json {
         obj("title" to string(title), "version" to string(version), "description" to string(description ?: ""))
     }
+
+    private fun String?.asJson() = this?.let { json.string(it) } ?: json.nullNode()
+    private fun NODE?.orNullNode() = this ?: json.nullNode()
 
     @Suppress("UNCHECKED_CAST")
     override fun toSchema(obj: Any, overrideDefinitionId: String?): JsonSchema<NODE> =
