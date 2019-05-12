abda9aa416fae69d5b61191203641d33c409eb14
David Denton <denton.david@gmail.com>
2019-05-12 09:48:13 +0100

    openapi2 test

--- http4k-contract/src/test/kotlin/org/http4k/contract/openapi/v2/OpenApiTest.kt
+++ http4k-contract/src/test/kotlin/org/http4k/contract/openapi/v2/OpenApiTest.kt
@@ -2,6 +2,6 @@ package org.http4k.contract.openapi.v2
 
 import org.http4k.contract.ContractRendererContract
 import org.http4k.contract.openapi.ApiInfo
-import org.http4k.format.Jackson
+import org.http4k.format.Argo
 
-class OpenApi2Test : ContractRendererContract(OpenApi2(ApiInfo("title", "1.2", "module description"), Jackson))
+class OpenApi2Test : ContractRendererContract(OpenApi2(ApiInfo("title", "1.2", "module description"), Argo))
