975083f97d9387268e1150b275d29fbc6a7ad54b
David Denton <denton.david@gmail.com>
2019-05-12 19:46:59 +0100

    test for schema creator

--- http4k-format-jackson/src/test/kotlin/org/http4k/util/JacksonJsonSchemaCreatorTest.kt
+++ http4k-format-jackson/src/test/kotlin/org/http4k/util/JacksonJsonSchemaCreatorTest.kt
@@ -1,26 +1,38 @@
 package org.http4k.util
 
+import org.http4k.core.ContentType
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
+import org.http4k.core.with
 import org.http4k.format.Jackson
 import org.http4k.format.Jackson.asJsonString
-import org.http4k.testing.ApprovalTest
+import org.http4k.lens.Header.CONTENT_TYPE
 import org.http4k.testing.Approver
+import org.http4k.testing.JsonApprovalTest
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 data class Simple(val nested: List<Nested>)
 data class Nested(val int: Int)
 
-@ExtendWith(ApprovalTest::class)
+@ExtendWith(JsonApprovalTest::class)
 class JacksonJsonSchemaCreatorTest {
 
     @Test
-    fun `generates schema for simple object`(approver: Approver) {
-        assertApproved(approver, Simple(listOf(Nested(123))))
+    fun `generates schema for normal object`(approver: Approver) {
+        approver.assertApproved(Simple(listOf(Nested(123))))
     }
 
-    private fun assertApproved(approver: Approver, obj: Simple) {
-        approver.assertApproved(Response(OK).body(asJsonString(JacksonJsonSchemaCreator(Jackson).toSchema(obj))))
+    @Test
+    @Disabled
+    fun `generates schema for list`(approver: Approver) {
+        approver.assertApproved(listOf(Nested(123)))
+    }
+
+    private fun Approver.assertApproved(obj: Any) {
+        assertApproved(Response(OK)
+            .with(CONTENT_TYPE of ContentType.APPLICATION_JSON)
+            .body(asJsonString(JacksonJsonSchemaCreator(Jackson).toSchema(obj))))
     }
 }
