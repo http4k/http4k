c4d6d65144444fd19bafd19b6d0682bc997b3969
David Denton <denton.david@gmail.com>
2019-05-12 19:50:41 +0100

    test for schema creator

--- http4k-format-jackson/src/test/kotlin/org/http4k/util/JacksonJsonSchemaCreatorTest.kt
+++ http4k-format-jackson/src/test/kotlin/org/http4k/util/JacksonJsonSchemaCreatorTest.kt
@@ -1,6 +1,6 @@
 package org.http4k.util
 
-import org.http4k.core.ContentType
+import org.http4k.core.ContentType.Companion.APPLICATION_JSON
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
@@ -18,6 +18,7 @@ data class Nested(val int: Int)
 
 @ExtendWith(JsonApprovalTest::class)
 class JacksonJsonSchemaCreatorTest {
+    private val creator = JacksonJsonSchemaCreator(Jackson)
 
     @Test
     fun `generates schema for normal object`(approver: Approver) {
@@ -32,7 +33,7 @@ class JacksonJsonSchemaCreatorTest {
 
     private fun Approver.assertApproved(obj: Any) {
         assertApproved(Response(OK)
-            .with(CONTENT_TYPE of ContentType.APPLICATION_JSON)
-            .body(asJsonString(JacksonJsonSchemaCreator(Jackson).toSchema(obj))))
+            .with(CONTENT_TYPE of APPLICATION_JSON)
+            .body(asJsonString(creator.toSchema(obj))))
     }
 }
