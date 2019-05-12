c84232fb10e4fe5f5e9fb5e9b09d099ec725f201
David Denton <denton.david@gmail.com>
2019-05-12 09:56:23 +0100

    extract contract

--- http4k-contract/src/test/kotlin/org/http4k/contract/openapi/v3/OpenApi3Test.kt
+++ http4k-contract/src/test/kotlin/org/http4k/contract/openapi/v3/OpenApi3Test.kt
@@ -1,33 +1,7 @@
 package org.http4k.contract.openapi.v3
 
-import org.http4k.contract.BearerAuthSecurity
-import org.http4k.contract.ContractRendererContract
-import org.http4k.contract.meta
+import argo.jdom.JsonNode
 import org.http4k.contract.openapi.ApiInfo
-import org.http4k.core.Body
-import org.http4k.core.Method.GET
-import org.http4k.core.Method.POST
-import org.http4k.core.Response
-import org.http4k.core.Status
-import org.http4k.core.Status.Companion.SEE_OTHER
 import org.http4k.format.Argo
-import org.http4k.format.Jackson.auto
 
-class OpenApi3Test : ContractRendererContract(OpenApi3(ApiInfo("title", "1.2", "module description"), Argo)) {
-    override fun specificRoutes() = listOf(
-        "/body_auto_schema" meta {
-            receiving(Body.auto<ArbObject2>().toLens() to ArbObject2(
-                "s",
-                ArbObject1(Foo.bar),
-                listOf(1),
-                true
-            ))
-        } bindContract POST to { Response(Status.OK) },
-        "/body_auto_schema" meta {
-            returning(SEE_OTHER, Body.auto<ArbObject1>().toLens() to ArbObject1(Foo.bing))
-        } bindContract GET to { Response(Status.OK) },
-        "/bearer_auth" meta {
-            security = BearerAuthSecurity("foo")
-        } bindContract POST to { Response(Status.OK) }
-    )
-}
+class OpenApi3Test : OpenApi3Contract<JsonNode>(OpenApi3(ApiInfo("title", "1.2", "module description"), Argo))
