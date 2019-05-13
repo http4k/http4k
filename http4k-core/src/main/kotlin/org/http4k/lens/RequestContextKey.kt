80555a18e2be7ac6817f04396b1a7bf467dd3b59
Andrew McGhie <andrew.mcghie@gmail.com>
2019-05-13 23:42:14 +1000

    Replace RequestContexts with reference to Store<RequestContext> interface

--- http4k-core/src/main/kotlin/org/http4k/lens/RequestContextKey.kt
+++ http4k-core/src/main/kotlin/org/http4k/lens/RequestContextKey.kt
@@ -1,7 +1,8 @@
 package org.http4k.lens
 
 import org.http4k.core.Request
-import org.http4k.core.RequestContexts
+import org.http4k.core.RequestContext
+import org.http4k.core.Store
 import org.http4k.lens.ParamMeta.ObjectParam
 import java.util.UUID
 
@@ -9,19 +10,19 @@ import java.util.UUID
 typealias RequestContextLens<T> = BiDiLens<Request, T>
 
 object RequestContextKey {
-    fun <T> required(contexts: RequestContexts, name: String = UUID.randomUUID().toString()): RequestContextLens<T> {
+    fun <T> required(contexts: Store<RequestContext>, name: String = UUID.randomUUID().toString()): RequestContextLens<T> {
         val meta = Meta(true, "context", ObjectParam, name)
         return BiDiLens(meta, { target ->
             contexts[target].let { it[name] ?: throw LensFailure(Missing(meta), target = it) }
         }, { value: T, target: Request -> contexts[target][name] = value; target })
     }
 
-    fun <T : Any?> optional(contexts: RequestContexts, name: String = UUID.randomUUID().toString()) =
+    fun <T : Any?> optional(contexts: Store<RequestContext>, name: String = UUID.randomUUID().toString()) =
         BiDiLens(Meta(false, "context", ObjectParam, name), { target -> contexts[target][name] },
             { value: T?, target: Request -> contexts[target][name] = value; target }
         )
 
-    fun <T : Any?> defaulted(contexts: RequestContexts, default: T, name: String = UUID.randomUUID().toString()) =
+    fun <T : Any?> defaulted(contexts: Store<RequestContext>, default: T, name: String = UUID.randomUUID().toString()) =
         BiDiLens(Meta(false, "context", ObjectParam, name), { target -> contexts[target][name] ?: default },
             { value: T, target: Request -> contexts[target][name] = value; target }
         )
