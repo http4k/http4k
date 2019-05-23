1b3f21ee283605b90820054d0b723ab1f3038bb3
David Denton <denton.david@gmail.com>
2019-05-23 20:37:32 +0100

    starting to use reflection

--- http4k-contract/src/main/kotlin/org/http4k/util/AutoJsonToJsonSchema.kt
+++ http4k-contract/src/main/kotlin/org/http4k/util/AutoJsonToJsonSchema.kt
@@ -8,6 +8,8 @@ import org.http4k.lens.ParamMeta.BooleanParam
 import org.http4k.lens.ParamMeta.IntegerParam
 import org.http4k.lens.ParamMeta.NumberParam
 import org.http4k.lens.ParamMeta.StringParam
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaGetter
 
 class AutoJsonToJsonSchema<NODE : Any>(
     private val json: JsonLibAutoMarshallingJson<NODE>,
@@ -18,43 +20,49 @@ class AutoJsonToJsonSchema<NODE : Any>(
         JsonSchema(
             obj(
                 (overrideDefinitionId ?: obj.javaClass.simpleName) to
-                    json.asJsonObject(json.asJsonObject(obj).toObjectSchema(obj))
+                    json.asJsonObject(json.asJsonObject(obj).toObjectSchema(obj, false))
             ), emptySet())
     }
 
-    private fun NODE.toSchema(paramMeta: ParamMeta) = SchemaNode.Primitive(paramMeta, this)
+    private fun NODE.toSchema(paramMeta: ParamMeta, isNullable: Boolean) = SchemaNode.Primitive(paramMeta, isNullable, this)
 
-    private fun NODE.toArraySchema() = SchemaNode.Array(this)
+    private fun NODE.toArraySchema(obj: Any, isNullable: Boolean) = SchemaNode.Array(isNullable, this)
 
-    private fun NODE.toObjectSchema(obj: Any): SchemaNode.Object {
-        val properties = json.fields(this).map { (name, field) ->
+    private fun NODE.toObjectSchema(obj: Any, isNullable: Boolean): SchemaNode {
+        val fields = obj::class.memberProperties.toList()
+        val properties = json.fields(this).mapIndexed { index, (name, field) ->
+            val kfield = fields[index]
+            val fieldIsNullable = kfield.returnType.isMarkedNullable
             name to when (json.typeOf(field)) {
-                JsonType.String -> field.toSchema(StringParam)
-                JsonType.Integer -> field.toSchema(IntegerParam)
-                JsonType.Number -> field.toSchema(NumberParam)
-                JsonType.Boolean -> field.toSchema(BooleanParam)
-                JsonType.Array -> field.toArraySchema()
-                JsonType.Object -> field.toObjectSchema(obj)
+                JsonType.String -> field.toSchema(StringParam, fieldIsNullable)
+                JsonType.Integer -> field.toSchema(IntegerParam, fieldIsNullable)
+                JsonType.Number -> field.toSchema(NumberParam, fieldIsNullable)
+                JsonType.Boolean -> field.toSchema(BooleanParam, fieldIsNullable)
+                JsonType.Array -> field.toArraySchema(kfield.javaGetter!!.invoke(obj), fieldIsNullable)
+                JsonType.Object -> field.toObjectSchema(kfield.javaGetter!!.invoke(obj), fieldIsNullable)
                 JsonType.Null -> throw IllegalSchemaException("Cannot use a null value in a schema!")
                 else -> throw IllegalSchemaException("unknown type")
             }
         }.map { it.first to it.second }.toMap()
-        return SchemaNode.Object(properties, properties.keys, this)
+
+        return SchemaNode.Object(isNullable, properties, this)
     }
 }
 
-private sealed class SchemaNode(val example: Any?) {
-    class Primitive(paramMeta: ParamMeta, example: Any?) : SchemaNode(example) {
+private sealed class SchemaNode(private val isNullable: Boolean, val example: Any?) {
+
+    fun nullable() = isNullable
+
+    class Primitive(paramMeta: ParamMeta, isNullable: Boolean, example: Any?) : SchemaNode(isNullable, example) {
         val type = paramMeta.value
     }
 
-    class Array(example: Any?) : SchemaNode(example) {
+    class Array(isNullable: Boolean, example: Any?) : SchemaNode(isNullable, example) {
         val type = ArrayParam.value
     }
 
-    class Object(val properties: Map<String, SchemaNode>,
-                 required: Set<String>,
-                 example: Any?) : SchemaNode(example) {
-        val required = required.sorted()
+    class Object(isNullable: Boolean, val properties: Map<String, SchemaNode>,
+                 example: Any?) : SchemaNode(isNullable, example) {
+        val required = properties.filterNot { it.value.isNullable }.keys.sorted()
     }
 }
