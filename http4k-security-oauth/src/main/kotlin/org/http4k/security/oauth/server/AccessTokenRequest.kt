4e3df5754398a1cf88adb7763855dc6abada836e
Ivan Sanchez <ivan@gourame.com>
2019-05-30 13:42:06 -0300

    Replace `when` with polymorphism

--- http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/AccessTokenRequest.kt
+++ http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/AccessTokenRequest.kt
@@ -3,6 +3,7 @@ package org.http4k.security.oauth.server
 import com.natpryce.Failure
 import com.natpryce.Result
 import com.natpryce.Success
+import com.natpryce.map
 import org.http4k.core.Body
 import org.http4k.core.Request
 import org.http4k.core.Uri
@@ -10,8 +11,19 @@ import org.http4k.lens.FormField
 import org.http4k.lens.Validator
 import org.http4k.lens.uri
 import org.http4k.lens.webForm
+import org.http4k.security.AccessTokenDetails
+import org.http4k.security.ResponseType
+import java.time.Clock
 
-sealed class AccessTokenRequest
+sealed class AccessTokenRequest {
+    abstract fun generate(
+        clientValidator: ClientValidator,
+        authorizationCodes: AuthorizationCodes,
+        accessTokens: AccessTokens,
+        clock: Clock,
+        idTokens: IdTokens
+    ): Result<AccessTokenDetails, AccessTokenError>
+}
 
 data class AuthorizationCodeAccessTokenRequest(
     val grantType: String,
@@ -19,9 +31,41 @@ data class AuthorizationCodeAccessTokenRequest(
     val clientSecret: String,
     val redirectUri: Uri,
     val authorizationCode: AuthorizationCode
-) : AccessTokenRequest()
+) : AccessTokenRequest() {
+    override fun generate(
+        clientValidator: ClientValidator,
+        authorizationCodes: AuthorizationCodes,
+        accessTokens: AccessTokens,
+        clock: Clock,
+        idTokens: IdTokens
+    ) = when {
+        this.grantType != "authorization_code" -> Failure(UnsupportedGrantType(this.grantType))
+        !clientValidator.validateCredentials(this.clientId, this.clientSecret) -> Failure(InvalidClientCredentials)
+        else -> {
+            val code = this.authorizationCode
+            val codeDetails = authorizationCodes.detailsFor(code)
 
-object ClientCredentialsTokenRequest : AccessTokenRequest()
+            when {
+                codeDetails.expiresAt.isBefore(clock.instant()) -> Failure(AuthorizationCodeExpired)
+                codeDetails.clientId != this.clientId -> Failure(ClientIdMismatch)
+                codeDetails.redirectUri != this.redirectUri -> Failure(RedirectUriMismatch)
+                else -> accessTokens.create(code)
+                    .map { token ->
+                        when (codeDetails.responseType) {
+                            ResponseType.Code -> AccessTokenDetails(token)
+                            ResponseType.CodeIdToken -> AccessTokenDetails(token, idTokens.createForAccessToken(code))
+                        }
+                    }
+            }
+        }
+    }
+}
+
+object ClientCredentialsTokenRequest : AccessTokenRequest() {
+    override fun generate(clientValidator: ClientValidator, authorizationCodes: AuthorizationCodes, accessTokens: AccessTokens, clock: Clock, idTokens: IdTokens): Result<AccessTokenDetails, AccessTokenError> {
+        return Failure(UnsupportedGrantType("client_credentials"))
+    }
+}
 
 fun Request.accessTokenRequest(): Result<AccessTokenRequest, AccessTokenError> {
     grantTypeForm(this).let {
--- http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/GenerateAccessToken.kt
+++ http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/GenerateAccessToken.kt
@@ -1,15 +1,15 @@
 package org.http4k.security.oauth.server
 
-import com.natpryce.*
+import com.natpryce.flatMap
+import com.natpryce.get
+import com.natpryce.map
+import com.natpryce.mapFailure
 import org.http4k.core.HttpHandler
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
-import org.http4k.security.AccessTokenDetails
 import org.http4k.security.AccessTokenResponse
-import org.http4k.security.ResponseType.Code
-import org.http4k.security.ResponseType.CodeIdToken
 import org.http4k.security.accessTokenResponseBody
 import java.time.Clock
 
@@ -24,7 +24,7 @@ class GenerateAccessToken(
 
     override fun invoke(request: Request): Response {
         return request.accessTokenRequest()
-            .flatMap(this::generateAccessToken)
+            .flatMap { it.generate(clientValidator, authorizationCodes, accessTokens, clock, idTokens) }
             .map { token ->
                 Response(OK).let {
                     when {
@@ -34,33 +34,4 @@ class GenerateAccessToken(
                 }
             }.mapFailure(errorRenderer::response).get()
     }
-
-    private fun generateAccessToken(accessTokenRequest: AccessTokenRequest) =
-        when (accessTokenRequest) {
-            is ClientCredentialsTokenRequest -> Failure(UnsupportedGrantType("client_credentials"))
-            is AuthorizationCodeAccessTokenRequest -> generateAccessTokenForAuthorizationCode(accessTokenRequest)
-        }
-
-    private fun generateAccessTokenForAuthorizationCode(accessTokenRequest: AuthorizationCodeAccessTokenRequest) =
-        when {
-            accessTokenRequest.grantType != "authorization_code" -> Failure(UnsupportedGrantType(accessTokenRequest.grantType))
-            !clientValidator.validateCredentials(accessTokenRequest.clientId, accessTokenRequest.clientSecret) -> Failure(InvalidClientCredentials)
-            else -> {
-                val code = accessTokenRequest.authorizationCode
-                val codeDetails = authorizationCodes.detailsFor(code)
-
-                when {
-                    codeDetails.expiresAt.isBefore(clock.instant()) -> Failure(AuthorizationCodeExpired)
-                    codeDetails.clientId != accessTokenRequest.clientId -> Failure(ClientIdMismatch)
-                    codeDetails.redirectUri != accessTokenRequest.redirectUri -> Failure(RedirectUriMismatch)
-                    else -> accessTokens.create(code)
-                        .map { token ->
-                            when (codeDetails.responseType) {
-                                Code -> AccessTokenDetails(token)
-                                CodeIdToken -> AccessTokenDetails(token, idTokens.createForAccessToken(code))
-                            }
-                        }
-                }
-            }
-        }
 }
