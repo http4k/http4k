5a7e9b31fe9673a6914418c7b8c7753734c87493
David Denton <denton.david@gmail.com>
2019-06-20 08:54:28 +0100

    optimize imports

--- http4k-contract/src/main/kotlin/org/http4k/contract/security/OAuthSecurity.kt
+++ http4k-contract/src/main/kotlin/org/http4k/contract/security/OAuthSecurity.kt
@@ -2,6 +2,7 @@ package org.http4k.contract.security
 
 import org.http4k.core.Filter
 import org.http4k.core.Uri
+import org.http4k.core.then
 import org.http4k.filter.ServerFilters
 import org.http4k.security.OAuthProvider
 
@@ -9,15 +10,17 @@ data class OAuthSecurity(
     val authorizationUrl: Uri,
     val tokenUrl: Uri,
     val scopes: List<OAuthScope> = emptyList(),
-    override val filter: Filter = ServerFilters.BearerAuth { true },
+    override val filter: Filter,
     val name: String = "oauthSecurity"
 ) : Security {
     companion object {
-        operator fun invoke(oAuthProvider: OAuthProvider, customScopes: List<OAuthScope>? = null) = OAuthSecurity(
+        operator fun invoke(oAuthProvider: OAuthProvider,
+                            authFilter: Filter = ServerFilters.BearerAuth { true },
+                            customScopes: List<OAuthScope>? = null) = OAuthSecurity(
             oAuthProvider.providerConfig.authUri,
             oAuthProvider.providerConfig.tokenUri,
             customScopes ?: oAuthProvider.scopes.map { OAuthScope(it, "") },
-            oAuthProvider.authFilter
+            oAuthProvider.authFilter.then(authFilter)
         )
     }
 }
