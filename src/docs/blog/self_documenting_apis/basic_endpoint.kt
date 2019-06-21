73b065db7dbe9dd78a5ac170e9a15bbecc7e65f1
David Denton <denton.david@gmail.com>
2019-06-21 21:19:04 +0100

    moar blog

--- src/docs/blog/self_documenting_apis/basic_endpoint.kt
+++ src/docs/blog/self_documenting_apis/basic_endpoint.kt
@@ -3,6 +3,7 @@ package blog.self_documenting_apis
 import org.http4k.contract.div
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
+import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.lens.Path
@@ -14,11 +15,10 @@ data class Age(val value: Int) {
     }
 }
 
-fun handler(name: String, age: Age): HttpHandler =
-    {
-        val beverage = if (age.value >= 18) "beer" else "lemonade"
-        Response(OK).body("Hello $name, would you like some $beverage?")
-    }
+fun handler(name: String, age: Age): HttpHandler = { req: Request ->
+    val beverage = if (age.value >= 18) "beer" else "lemonade"
+    Response(OK).body("Hello $name, would you like some $beverage?")
+}
 
 val route = "/greet" / Path.of("name") / Path.int().map(::Age).of("age") bindContract GET to ::handler
 
