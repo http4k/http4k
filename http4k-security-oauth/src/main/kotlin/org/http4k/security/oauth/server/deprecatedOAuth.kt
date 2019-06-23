c2f84e85c08a4f9c48407e0673ddc3be0edec3b3
David Denton <denton.david@gmail.com>
2019-06-23 17:32:13 +0100

    fix deprecated annotation

--- http4k-security-oauth/src/main/kotlin/org/http4k/security/oauth/server/deprecatedOAuth.kt
+++
@@ -1,6 +0,0 @@
-package org.http4k.security.oauth.server
-
-import org.http4k.security.AccessToken
-
-@Deprecated("use renamed version", ReplaceWith("org.http4k.security.AccessToken"))
-typealias AccessTokenContainer = AccessToken
