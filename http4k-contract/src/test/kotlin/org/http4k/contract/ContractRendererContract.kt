7419f82717c910e953d1499b55ab0f3a2921c4a8
David Denton <daviddenton@users.noreply.github.com>
2019-07-29 12:10:13 +0100

    Customize errors (#272)
    * tweaks to blog
    
    * change to ErrorResponseRenderer to take a LensFailure instead of a list of Failure objects. This allows for a more customised experience when rendering badrequest responses

--- http4k-contract/src/test/kotlin/org/http4k/contract/ContractRendererContract.kt
+++ http4k-contract/src/test/kotlin/org/http4k/contract/ContractRendererContract.kt
@@ -28,6 +28,7 @@ import org.http4k.format.Json
 import org.http4k.lens.FormField
 import org.http4k.lens.Header
 import org.http4k.lens.Invalid
+import org.http4k.lens.LensFailure
 import org.http4k.lens.Meta
 import org.http4k.lens.Missing
 import org.http4k.lens.ParamMeta.NumberParam
@@ -52,9 +53,9 @@ import org.junit.jupiter.api.extension.ExtendWith
 abstract class ContractRendererContract<NODE>(private val json: Json<NODE>, protected val rendererToUse: ContractRenderer) {
     @Test
     fun `can build 400`() {
-        val response = rendererToUse.badRequest(listOf(
+        val response = rendererToUse.badRequest(LensFailure(listOf(
             Missing(Meta(true, "location1", StringParam, "name1")),
-            Invalid(Meta(false, "location2", NumberParam, "name2"))))
+            Invalid(Meta(false, "location2", NumberParam, "name2")))))
         assertThat(response.bodyString(),
             equalTo("""{"message":"Missing/invalid parameters","params":[{"name":"name1","type":"location1","datatype":"string","required":true,"reason":"Missing"},{"name":"name2","type":"location2","datatype":"number","required":false,"reason":"Invalid"}]}"""))
     }
