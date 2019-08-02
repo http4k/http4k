09594fec36cfcadd7830c6e5b292a546c3ec627c
David Denton <denton.david@gmail.com>
2019-08-02 08:28:02 +0100

    add warning for static classpath resource loader

--- http4k-core/src/main/kotlin/org/http4k/routing/ResourceLoader.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/ResourceLoader.kt
@@ -7,9 +7,20 @@ interface ResourceLoader {
     fun load(path: String): URL?
 
     companion object {
-        fun Classpath(basePackagePath: String = "/") = object : ResourceLoader {
+        fun Classpath(basePackagePath: String = "/", muteWarning: Boolean = false) = object : ResourceLoader {
+
             private val withStarting = if (basePackagePath.startsWith("/")) basePackagePath else "/$basePackagePath"
+
             private val finalBasePath = if (withStarting.endsWith("/")) withStarting else "$withStarting/"
+
+            init {
+                if (!muteWarning && finalBasePath == "/") {
+                    System.err.println("WARNING - http4k Classpath ResourceLoader is configured to serve ALL files " +
+                        "from the root of the Java classpath.\n" +
+                        "For security you should serve from a non-code package eg. /public, " +
+                        "or mute this warning using the flag on construction.")
+                }
+            }
 
             override fun load(path: String): URL? = javaClass.getResource(finalBasePath + path)
         }
