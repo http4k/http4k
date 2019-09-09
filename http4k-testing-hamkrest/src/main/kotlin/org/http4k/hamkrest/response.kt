e01c3ed3b7b2ee043dc3d567c02c844d20e2440d
Albert Latacz <albert.latacz@gmail.com>
2019-09-09 13:00:30 +0100

    added http message to hamkrest mismatch (#289)

--- http4k-testing-hamkrest/src/main/kotlin/org/http4k/hamkrest/response.kt
+++ http4k-testing-hamkrest/src/main/kotlin/org/http4k/hamkrest/response.kt
@@ -8,8 +8,9 @@ import org.http4k.core.Status
 import org.http4k.core.cookie.Cookie
 import org.http4k.core.cookie.cookies
 
-fun hasStatus(expected: Status): Matcher<Response> = has(Response::status, equalTo(expected))
+fun hasStatus(expected: Status): Matcher<Response> = httpMessageHas("Status", Response::status, equalTo(expected))
 
 fun hasSetCookie(expected: Cookie): Matcher<Response> = hasSetCookie(expected.name, equalTo(expected))
 
-fun hasSetCookie(name: String, expected: Matcher<Cookie>): Matcher<Response> = has("Cookie '$name'", { r: Response -> r.cookies().find { name == it.name }!! }, expected)
+fun hasSetCookie(name: String, expected: Matcher<Cookie>): Matcher<Response> = httpMessageHas("Cookie '$name'", { r: Response -> r.cookies().find { name == it.name }!! }, expected)
+
