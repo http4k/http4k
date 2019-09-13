a0b4816c3bab2c9dc2e82962f9544f80fd84de6b
David Denton <denton.david@gmail.com>
2019-09-13 19:08:05 +0100

    using static traces inside of AutoEvents contract

--- http4k-core/src/test/kotlin/org/http4k/format/AutoJsonEventsContract.kt
+++ http4k-core/src/test/kotlin/org/http4k/format/AutoJsonEventsContract.kt
@@ -9,6 +9,8 @@ import org.http4k.events.EventFilters
 import org.http4k.events.MyEvent
 import org.http4k.events.plus
 import org.http4k.events.then
+import org.http4k.filter.TraceId
+import org.http4k.filter.ZipkinTraces
 import org.http4k.lens.Header.CONTENT_TYPE
 import org.http4k.testing.Approver
 import org.http4k.testing.JsonApprovalTest
@@ -26,6 +28,12 @@ abstract class AutoJsonEventsContract(private val j: AutoMarshallingJson) {
     fun `event serialises to json`(approver: Approver) {
         val final = MyEvent() + ("first" to "1") + ("second" to 2)
         val w = StringWriter()
+
+        ZipkinTraces.THREAD_LOCAL.set(ZipkinTraces(
+            TraceId("61a2ebc48552f603"),
+            TraceId("f494afd6389ff8f0"),
+            TraceId("b429b2d2803523b9")
+        ))
 
         val pipeline = EventFilters.AddZipkinTraces()
             .then(EventFilters.AddTimestamp(Clock.fixed(Instant.EPOCH, ZoneId.systemDefault())))
