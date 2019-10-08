24173cc9d40fb462fd0f71741d78de6703dac35e
MrBergin <jordan.j.bergin@protonmail.com>
2019-10-08 17:01:18 +0100

    Getting more tests passing.

--- http4k-format-kotlinx-serialization/src/main/kotlin/org/http4k/format/KotlinxSerialization.kt
+++ http4k-format-kotlinx-serialization/src/main/kotlin/org/http4k/format/KotlinxSerialization.kt
@@ -11,24 +11,23 @@ import java.math.BigInteger
 object KotlinxSerialization : Json<JsonElement> {
 
     private val json = Json(JsonConfiguration.Stable)
+    private val prettyJson = Json(JsonConfiguration(prettyPrint = true, indent = "\t"))
 
     override fun typeOf(value: JsonElement): JsonType =
-            when (value) {
-                is JsonLiteral -> when {
-                    value.isString -> JsonType.String
-                    value.booleanOrNull != null -> JsonType.Boolean
-                    value.doubleOrNull != null -> JsonType.Number
-                    else -> throw RuntimeException()
-                }
-                is JsonArray -> JsonType.Array
-                is JsonObject -> JsonType.Object
-                is JsonNull -> JsonType.Null
-                else -> throw IllegalArgumentException("Don't know now to translate $value")
+        when (value) {
+            is JsonLiteral -> when {
+                value.isString -> JsonType.String
+                value.booleanOrNull != null -> JsonType.Boolean
+                value.doubleOrNull != null -> JsonType.Number
+                else -> throw RuntimeException()
             }
+            is JsonArray -> JsonType.Array
+            is JsonObject -> JsonType.Object
+            is JsonNull -> JsonType.Null
+            else -> throw IllegalArgumentException("Don't know now to translate $value")
+        }
 
-    override fun JsonElement.asPrettyJsonString() = json.stringify(JsonElement.serializer(), this)
-
-    //TODO what does this mean?
+    override fun JsonElement.asPrettyJsonString() = prettyJson.stringify(JsonElement.serializer(), this)
     override fun JsonElement.asCompactJsonString() = json.stringify(JsonElement.serializer(), this)
 
     override fun String.asJsonObject() = json.parse(JsonObjectSerializer, this)
@@ -45,35 +44,33 @@ object KotlinxSerialization : Json<JsonElement> {
         json.parse(JsonLiteralSerializer, "$it")
     } ?: JsonNull
 
-    override fun BigInteger?.asJsonValue() = this?.let {
-        json.parse(JsonLiteralSerializer, "$it")
-    } ?: JsonNull
+    override fun BigInteger?.asJsonValue() = this?.let { JsonLiteral(it) } ?: JsonNull
 
-    override fun Boolean?.asJsonValue() = this?.let {
-        json.parse(JsonLiteralSerializer, "$it")
-    } ?: JsonNull
+    override fun Boolean?.asJsonValue() = this?.let { JsonLiteral(it) } ?: JsonNull
 
     override fun <T : Iterable<JsonElement>> T.asJsonArray() = JsonArray(this.toList())
 
     override fun <LIST : Iterable<Pair<String, JsonElement>>> LIST.asJsonObject() = JsonObject(this.toMap())
 
     override fun fields(node: JsonElement) =
-            if (node !is JsonObject) emptyList() else node.toList()
+        if (node !is JsonObject) emptyList() else node.toList()
 
-    override fun elements(value: JsonElement) =
-            if (value !is JsonObject) emptyList() else value.values
-
+    override fun elements(value: JsonElement) = when(value) {
+        is JsonObject -> value.values
+        is JsonArray -> value.content
+        else -> emptyList()
+    }
     override fun text(value: JsonElement) =
-        value.toString()
+        value.content
 
     override fun bool(value: JsonElement) = value.boolean
 
     override fun integer(value: JsonElement) = value.long
 
-    override fun decimal(value: JsonElement): BigDecimal = BigDecimal.valueOf(value.double)
+    override fun decimal(value: JsonElement): BigDecimal = BigDecimal(value.content)
 
     override fun textValueOf(node: JsonElement, name: String) = when (node) {
-        is JsonObject -> node[name].toString()
+        is JsonObject -> node[name]?.content
         else -> throw IllegalArgumentException("node is not an object")
     }
 }
