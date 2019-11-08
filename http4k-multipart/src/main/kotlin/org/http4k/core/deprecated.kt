bbf9464c7b87c0e376cd6d741b29427e329c8d13
David Denton <denton.david@gmail.com>
2019-11-08 07:29:26 +0000

    tweak comment

--- http4k-multipart/src/main/kotlin/org/http4k/core/deprecated.kt
+++ http4k-multipart/src/main/kotlin/org/http4k/core/deprecated.kt
@@ -2,5 +2,5 @@ package org.http4k.core
 
 import org.http4k.lens.MultipartFormFile
 
-@Deprecated("use ", ReplaceWith("org.http4k.lens.MultipartFormFile"))
+@Deprecated("Repackaged", ReplaceWith("org.http4k.lens.MultipartFormFile"))
 typealias FormFile = MultipartFormFile
