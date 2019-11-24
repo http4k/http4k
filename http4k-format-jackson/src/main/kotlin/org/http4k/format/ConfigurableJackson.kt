c1b15b3a016c27d6fe68e7c46844a419315d6615
David Denton <denton.david@gmail.com>
2019-11-24 09:23:16 +0000

    changelog and tidy imports

--- http4k-format-jackson/src/main/kotlin/org/http4k/format/ConfigurableJackson.kt
+++ http4k-format-jackson/src/main/kotlin/org/http4k/format/ConfigurableJackson.kt
@@ -1,6 +1,5 @@
 package org.http4k.format
 
-import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ArrayNode
