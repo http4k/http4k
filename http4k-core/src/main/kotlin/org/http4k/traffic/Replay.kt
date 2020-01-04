1ca6d85a584589a7922b6dcd16091af7883785ab
David Denton <denton.david@gmail.com>
2020-01-04 16:59:37 +0000

    null safety

--- http4k-core/src/main/kotlin/org/http4k/traffic/Replay.kt
+++ http4k-core/src/main/kotlin/org/http4k/traffic/Replay.kt
@@ -23,7 +23,7 @@ interface Replay {
             override fun responses() = read(Response.Companion::parse, "response.txt").asSequence()
 
             private fun <T : HttpMessage> read(convert: (String) -> T, file: String) =
-                baseDir.toBaseFolder().listFiles().sortedBy { it.name }
+                (baseDir.toBaseFolder().listFiles() ?: emptyArray<File>()).sortedBy { it.name }
                     .map { File(it, file).run { convert(String(readBytes())) } }
         }
 
