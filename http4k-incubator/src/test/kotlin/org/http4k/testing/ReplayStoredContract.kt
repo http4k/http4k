f0c72614d6be908755945880071e1dc88d98d389
David Denton <denton.david@gmail.com>
2020-01-05 20:03:12 +0000

    adding replaying of tsts

--- http4k-incubator/src/test/kotlin/org/http4k/testing/ReplayStoredContract.kt
+++ http4k-incubator/src/test/kotlin/org/http4k/testing/ReplayStoredContract.kt
@@ -1,14 +1,19 @@
 package org.http4k.testing
 
-import org.http4k.core.HttpHandler
+import org.http4k.core.Filter
+import org.http4k.core.then
+import org.http4k.traffic.ReadWriteStream
+import org.http4k.traffic.Servirtium
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
-import org.junit.jupiter.api.extension.RegisterExtension
+import java.io.File
 import java.lang.invoke.MethodHandle
 import java.lang.invoke.MethodHandles
 import java.lang.reflect.Proxy
+import java.util.concurrent.atomic.AtomicInteger
 
 class ReplayStoredContract {
 
@@ -16,16 +21,28 @@ class ReplayStoredContract {
     private val testClass = Class.forName(name)
     private val instance = Proxy.newProxyInstance(testClass.classLoader, arrayOf(testClass)) { _, _, _ -> null }
 
-    @JvmField
-    @RegisterExtension
-    val replay = ServirtiumReplay("getName"().invokeWithArguments() as String)
-
     @TestFactory
-    fun `replay stored tests`(handler: HttpHandler): List<DynamicTest> {
+    fun `replay stored tests`(): List<DynamicTest> {
+        val mainName = "getName"().invokeWithArguments() as String
         return testClass.methods
             .filter { it.annotations.any { it.annotationClass == Test::class } }
             .map {
-                dynamicTest(it.name) { it.name().invokeWithArguments(handler) }
+                dynamicTest(it.name) {
+                    val readWriteStream = ReadWriteStream.Servirtium(File("."), mainName + "." + it.name)
+                    val zipped = readWriteStream.requests().zip(readWriteStream.responses()).iterator()
+                    val count = AtomicInteger()
+
+                    val handler = Filter {
+                        {
+                            val (request, response) = zipped.next()
+                            assertEquals(it.toString(), request.toString(), "Unexpected request received for Interaction " + count.get())
+                            response
+                        }
+                    }.then { throw IllegalAccessException("Should never get here") }
+
+
+                    it.name().invokeWithArguments(handler)
+                }
             }
     }
 
