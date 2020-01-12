99f106a893b0a5187645e2ef4e8a3f5bfd11aea3
Tom Malone <tomjmalone@gmail.com>
2020-01-12 20:29:51 +0000

    Adding token type to access token response, and tidying up response to strip out nulls

--- http4k-security-oauth/src/main/kotlin/org/http4k/security/AccessToken.kt
+++ http4k-security-oauth/src/main/kotlin/org/http4k/security/AccessToken.kt
@@ -1,18 +1,22 @@
 package org.http4k.security
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.http4k.core.Body
 import org.http4k.format.Jackson.auto
 import org.http4k.security.openid.IdToken
 
-data class AccessToken(val value: String, val scope: String? = null)
+data class AccessToken(val value: String, val type: String? = "Bearer", val scope: String? = null)
 
 data class AccessTokenDetails(val accessToken: AccessToken, val idToken: IdToken? = null)
 
+@JsonInclude(NON_NULL)
 data class AccessTokenResponse(
-    @JsonProperty("access_token") val accessToken: String,
-    @JsonProperty("id_token") val idToken: String? = null,
-    @JsonProperty("scope") val scope: String? = null
+        @JsonProperty("access_token") val accessToken: String,
+        @JsonProperty("token_type") val tokenType: String? = null,
+        @JsonProperty("id_token") val idToken: String? = null,
+        @JsonProperty("scope") val scope: String? = null
 )
 
 val accessTokenResponseBody = Body.auto<AccessTokenResponse>().toLens()
