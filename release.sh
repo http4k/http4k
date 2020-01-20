5cdcf225989177e293dc6ddb5453eb4717ab91a4
David Denton <denton.david@gmail.com>
2020-01-20 09:31:23 +0000

    quotes in scripts

--- release.sh
+++ release.sh
@@ -7,14 +7,14 @@ set -o nounset
 
 NEW_VERSION=$1
 
-BINTRAY_VERSION=`curl -s https://bintray.com/api/v1/packages/http4k/maven/http4k-core/versions/_latest | tools/jq -r .name`
+BINTRAY_VERSION=$(curl -s https://bintray.com/api/v1/packages/http4k/maven/http4k-core/versions/_latest | tools/jq -r .name)
 
 git stash
 
-echo Upgrade from $BINTRAY_VERSION to $NEW_VERSION
+echo Upgrade from "$BINTRAY_VERSION" to "$NEW_VERSION"
 
-find . -name "*.md" | grep -v "CHANGELOG" | xargs  sed -i '' s/$BINTRAY_VERSION/$NEW_VERSION/g
-sed -i '' s/$BINTRAY_VERSION/$NEW_VERSION/g version.json
+find . -name "*.md" | grep -v "CHANGELOG" | xargs  sed -i '' s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g
+sed -i '' s/"$BINTRAY_VERSION"/"$NEW_VERSION"/g version.json
 
 git commit -am"Release $NEW_VERSION"
 git push
