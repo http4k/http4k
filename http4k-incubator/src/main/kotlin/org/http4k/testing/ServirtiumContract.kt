d3b01c8f6c8b45e3b216e87c3a8c0369b2ff5f26
David Denton <denton.david@gmail.com>
2020-01-24 16:31:48 +0000

    docs

--- http4k-incubator/src/main/kotlin/org/http4k/testing/ServirtiumContract.kt
+++ http4k-incubator/src/main/kotlin/org/http4k/testing/ServirtiumContract.kt
@@ -7,6 +7,14 @@ import org.http4k.core.NoOp
  * Defines a test contract which can be used to implement recording or replaying of Servirtium-formatted tests
  */
 interface ServirtiumContract {
+    /**
+     * Provides the base name for this contract's test cases.
+     */
     val name: String
-    val manipulations: Filter get() = Filter.NoOp
+
+    /**
+     * Custom manipulations to apply to the request and responses going into the recorded version of the contract
+     * HTTP traffic.
+     */
+    val manipulations get() = Filter.NoOp
 }
