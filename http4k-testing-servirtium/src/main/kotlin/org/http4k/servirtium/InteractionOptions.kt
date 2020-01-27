198f64083ac035db51be0e16b4f151f1c4794a29
David Denton <denton.david@gmail.com>
2020-01-27 17:28:13 +0000

    docs

--- http4k-testing-servirtium/src/main/kotlin/org/http4k/servirtium/InteractionOptions.kt
+++ http4k-testing-servirtium/src/main/kotlin/org/http4k/servirtium/InteractionOptions.kt
@@ -4,9 +4,26 @@ import org.http4k.core.ContentType
 import org.http4k.core.Request
 import org.http4k.core.Response
 
+/**
+ * This controls how the Servirtium interactions are recorded to the storage format. The manipulations are
+ * used to replace/remove any dynamic parts of the request (eg. Date headers) so that the traffic can be
+ * correctly matched during the replay process.
+ */
 interface InteractionOptions {
+
+    /**
+     * Modify received requests before they are stored
+     */
     fun modify(request: Request): Request = request
+
+    /**
+     * Modify received responses before they are stored
+     */
     fun modify(response: Response): Response = response
+
+    /**
+     * Determine if the content type should be treated as binary. Binary messages are
+     */
     fun isBinary(contentType: ContentType): Boolean = false
 
     companion object {
