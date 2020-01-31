402cdb41dda1f9c8fc2e91f64a0299cecfd06ff0
David Denton <denton.david@gmail.com>
2020-01-31 20:43:43 +0000

    Fix #362 - websocket reference is nullable instead of lateinit - fixes early disconnect problem

--- http4k-server-jetty/src/main/kotlin/org/http4k/server/Http4kWebSocketListener.kt
+++ http4k-server-jetty/src/main/kotlin/org/http4k/server/Http4kWebSocketListener.kt
@@ -30,18 +30,18 @@ internal fun ServletUpgradeRequest.asHttp4kRequest(): Request =
 private fun ServletUpgradeRequest.headerParameters(): Headers = headers.asSequence().fold(listOf()) { memo, next -> memo + next.value.map { next.key to it } }
 
 internal class Http4kWebSocketListener(private val wSocket: WsConsumer, private val upgradeRequest: Request) : WebSocketListener {
-    private lateinit var websocket: Http4kWebSocketAdapter
+    private var websocket: Http4kWebSocketAdapter? = null
 
     override fun onWebSocketClose(statusCode: Int, reason: String?) {
-        websocket.onClose(statusCode, reason)
+        websocket?.onClose(statusCode, reason)
     }
 
     override fun onWebSocketConnect(session: Session) {
         websocket = Http4kWebSocketAdapter(object : PushPullAdaptingWebSocket(upgradeRequest) {
             override fun send(message: WsMessage) {
                 when (message.body) {
-                    is StreamBody -> session.remote.sendBytes(message.body.payload)
-                    else -> session.remote.sendString(message.bodyString())
+                    is StreamBody -> session.remote.sendBytesByFuture(message.body.payload).get()
+                    else -> session.remote.sendStringByFuture(message.bodyString()).get()
                 }
             }
 
@@ -52,14 +52,14 @@ internal class Http4kWebSocketListener(private val wSocket: WsConsumer, private 
     }
 
     override fun onWebSocketText(message: String) {
-        websocket.onMessage(Body(message))
+        websocket?.onMessage(Body(message))
     }
 
     override fun onWebSocketBinary(payload: ByteArray, offset: Int, len: Int) {
-        websocket.onMessage(Body(ByteBuffer.wrap(payload, offset, len)))
+        websocket?.onMessage(Body(ByteBuffer.wrap(payload, offset, len)))
     }
 
     override fun onWebSocketError(cause: Throwable) {
-        websocket.onError(cause)
+        websocket?.onError(cause)
     }
 }
