cb7ab31e62edb1c0517bbcb919715c761c460a24
David Denton <denton.david@gmail.com>
2020-02-09 18:38:55 +0000

    example

--- src/docs/guide/modules/chaos/example_chaos_fake_system.kt
+++ src/docs/guide/modules/chaos/example_chaos_fake_system.kt
@@ -4,20 +4,20 @@ import org.http4k.chaos.ChaosBehaviours.ReturnStatus
 import org.http4k.chaos.ChaosEngine
 import org.http4k.chaos.ChaosTriggers.Once
 import org.http4k.chaos.appliedWhen
+import org.http4k.chaos.withChaosApi
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status
 import org.http4k.core.Status.Companion.I_M_A_TEAPOT
-import org.http4k.core.then
+import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 
 class FakeSystem : HttpHandler {
     private val chaosEngine = ChaosEngine()
 
-    private val app = chaosEngine.then(routes("/hello" bind GET to { Response(Status.OK) }))
+    private val app = routes("/hello" bind GET to { Response(OK) }).withChaosApi(chaosEngine)
 
     fun blowUpOnce() {
         chaosEngine.enable(ReturnStatus(I_M_A_TEAPOT).appliedWhen(Once()))
