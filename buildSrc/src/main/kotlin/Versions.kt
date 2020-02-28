6f15a858260dd033530177f76f73306d8c3e4c55
David Denton <denton.david@gmail.com>
2020-02-28 15:13:46 +0000

    upgrade some versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -14,13 +14,13 @@ import org.gradle.plugin.use.PluginDependencySpec
 object Versions {
      const val org_jetbrains_kotlinx_kotlinx_serialization: String = "0.14.0"
 
-     const val io_github_resilience4j: String = "1.3.1"
+     const val io_github_resilience4j: String = "1.3.1" // available: "1.3.1"
 
      const val org_jetbrains_kotlin: String = "1.3.61"
 
      const val com_squareup_moshi: String = "1.9.2"
 
-     const val org_eclipse_jetty: String = "9.4.26.v20200117"
+    const val org_eclipse_jetty: String = "9.4.27.v20200227"
 
      const val org_junit_jupiter: String = "5.6.0"
 
@@ -28,7 +28,7 @@ object Versions {
 
      const val de_fayard_buildsrcversions_gradle_plugin: String = "0.7.0"
 
-     const val com_github_kt3k_coveralls_gradle_plugin: String = "2.10.0"
+    const val com_github_kt3k_coveralls_gradle_plugin: String = "2.10.1"
 
      const val net_saliman_cobertura_gradle_plugin: String = "3.0.0"
 
@@ -39,6 +39,7 @@ object Versions {
      const val openapi_generator_gradle_plugin: String = "4.2.3"
 
      const val javax_websocket_server_impl: String = "9.4.26.v20200117"
+    // available: "9.4.27.v20200227"
 
      const val coveralls_gradle_plugin: String = "2.8.3"
 
@@ -52,17 +53,17 @@ object Versions {
 
      const val aws_lambda_java_core: String = "1.2.0"
 
-     const val dokka_gradle_plugin: String = "0.10.0"
+    const val dokka_gradle_plugin: String = "0.10.1"
 
      const val javax_servlet_api: String = "4.0.1"
 
-     const val netty_codec_http2: String = "4.1.45.Final"
+    const val netty_codec_http2: String = "4.1.46.Final"
 
      const val jackson_databind: String = "2.10.2"
 
      const val httpasyncclient: String = "4.1.4"
 
-     const val ktor_server_cio: String = "1.3.0"
+    const val ktor_server_cio: String = "1.3.1"
 
      const val micrometer_core: String = "1.3.5"
 
@@ -70,7 +71,7 @@ object Versions {
 
      const val commons_pool2: String = "2.8.0"
 
-     const val http2_server: String = "9.4.26.v20200117"
+    const val http2_server: String = "9.4.27.v20200227"
 
      const val selenium_api: String = "3.141.59"
 
@@ -88,13 +89,13 @@ object Versions {
 
      const val thymeleaf: String = "3.0.11.RELEASE"
 
-     const val hamkrest: String = "1.7.0.0"
+    const val hamkrest: String = "1.7.0.2"
 
      const val httpcore: String = "4.4.13"
 
      const val result4k: String = "2.0.0"
 
-     const val jade4j: String = "1.3.0"
+    const val jade4j: String = "1.3.1"
 
      const val okhttp: String = "4.4.0"
 
