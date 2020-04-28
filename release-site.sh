5528592d7b5510b4d220d9f953440fbb258c7180
Ivan Sanchez <29394+s4nchez@users.noreply.github.com>
2020-04-28 05:09:11 +0100

    Add script to embed code in markdown files before creating website (#403)

--- release-site.sh
+++ release-site.sh
@@ -7,14 +7,13 @@ set -o nounset
 
 pip install -r requirements.txt
 
+./tools/embed_code.py
+
 TMP=/tmp/http4k.github.io/
 rm -rf ${TMP}
 git clone https://${GH_TOKEN}@github.com/http4k/http4k.github.io.git ${TMP}
-cp -R src/docs ${TMP}/
+cp -R build/docs-website/docs ${TMP}/
 
-cp CONTRIBUTING.md ${TMP}docs/contributing/index.md
-cp CHANGELOG.md ${TMP}docs/changelog/index.md
-cp README.md ${TMP}docs/index.md
 cp -R src/mkdocs.yml ${TMP}
 
 cd ${TMP}
---
+++ tools/embed_code.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import re
+import glob
+import shutil
+import requests
+import os
+
+
+def pull_content(uri):
+    uri = uri \
+        .replace('github.com', 'raw.githubusercontent.com') \
+        .replace('/blob/', '/')
+
+    print('Fetching ' + uri)
+    response = requests.get(uri)
+    if response.status_code > 299:
+        raise Exception('Server responded with ' + str(response.status_code))
+    return '```kotlin\n' + response.text + '```'
+
+
+if __name__ == "__main__":
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    project_root = script_dir + '/../'
+    working_dir = script_dir + '/../build/docs-website'
+
+    shutil.rmtree(working_dir, ignore_errors=True)
+    os.makedirs(working_dir, exist_ok=True)
+    shutil.copytree(project_root + '/src/docs', working_dir + '/docs')
+    shutil.copy(project_root + '/CONTRIBUTING.md', working_dir + '/docs/contributing/index.md')
+    shutil.copy(project_root + '/CHANGELOG.md', working_dir + '/docs/changelog/index.md')
+    shutil.copy(project_root + '/README.md', working_dir + '/docs/index.md')
+
+    pages = [f for f in glob.glob(working_dir + "/**/*.md", recursive=True)]
+    for page in pages:
+        print('Processing ' + page)
+        with open(page, 'r', encoding="utf-8") as file:
+            page_contents = file.read()
+        page_contents = re.sub(r"<script src=\"https://gist-it\.appspot\.com/(.*)\"></script>",
+                               lambda m: pull_content(m.group(1)),
+                               page_contents)
+        with open(page, 'w', encoding="utf-8") as file:
+            file.write(page_contents)
