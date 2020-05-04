ac00208120ab8c121614629eff2140ab9c5af7c3
David Denton <denton.david@gmail.com>
2020-05-04 14:56:03 +0100

    mov openapi generation into incubator, and add testresources

---
+++ http4k-core/src/test/kotlin/org/http4k/junit/TestResources.kt
@@ -0,0 +1,30 @@
+package org.http4k.junit
+
+import org.junit.jupiter.api.extension.ExtensionContext
+import org.junit.jupiter.api.extension.ParameterContext
+import org.junit.jupiter.api.extension.ParameterResolver
+import java.io.InputStream
+
+interface ResourceLoader {
+    fun text(name: String): String
+    fun bytes(name: String): ByteArray
+    fun stream(name: String): InputStream
+}
+
+class TestResources : ParameterResolver {
+    override fun supportsParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext) =
+        parameterContext.parameter.type == ResourceLoader::class.java
+
+    override fun resolveParameter(parameterContext: ParameterContext, extensionContext: ExtensionContext) =
+        object : ResourceLoader {
+            override fun text(name: String): String = stream(name).reader().readText().trim()
+
+            override fun bytes(name: String): ByteArray = stream(name).readAllBytes()
+
+            override fun stream(name: String): InputStream {
+                val resource = "/${extensionContext.testClass.get().packageName.replace('.', '/')}/${extensionContext.testMethod.get().name}_$name"
+                return extensionContext.testClass.get().getResourceAsStream(resource)
+                    ?: throw IllegalStateException("Cannot find resource `$resource`")
+            }
+        }
+}
--- http4k-incubator/src/main/kotlin/org/http4k/openapi/main.kt
+++ http4k-incubator/src/main/kotlin/org/http4k/openapi/main.kt
@@ -8,12 +8,12 @@ import java.io.File
 fun main(args: Array<String>) {
     val generationOptions = GenerationOptions("org.http4k", File("src/main/kotlin"))
 
-    val targetGeneratedDir = File("src/main/kotlin").apply { mkdirs() }
+    val targetGeneratedDir = generationOptions.destinationFolder.apply { mkdirs() }
     val spec = OpenApiJson.asA(File(args[0]).readText(), OpenApi3Spec::class)
-
 
     listOf(ModelApiGenerator, ClientApiGenerator, ServerApiGenerator)
         .flatMap { it(spec, generationOptions) }
+
         .forEach { it.writeTo(targetGeneratedDir) }
 }
 
