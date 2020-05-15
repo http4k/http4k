d93d5f0c37b51ff301e3cfb91d8b404a67645875
David Denton <denton.david@gmail.com>
2020-05-15 06:22:26 +0000

    model work

--- src/test/kotlin/org/http4k/openapi/v3/main.kt
+++ src/test/kotlin/org/http4k/openapi/v3/main.kt
@@ -7,13 +7,15 @@ import org.http4k.openapi.v3.server.ServerApiGenerator
 import java.io.File
 
 fun main() {
-    val generationOptions = GenerationOptions("org.http4k", File("src/main/kotlin"))
+    val generationOptions = GenerationOptions("org.http4k", File("http4k/src/main/kotlin"))
 
     val targetGeneratedDir = generationOptions.destinationFolder.apply { mkdirs() }
     val spec = File("http4k/src/test/resources/org/http4k/openapi/v3/apiSpec.json").readText().asA(OpenApi3Spec::class)
 
     listOf(ModelApiGenerator, ClientApiGenerator, ServerApiGenerator)
         .flatMap { it(spec, generationOptions) }
-        .forEach { it.writeTo(targetGeneratedDir) }
+        .forEach {
+            println(targetGeneratedDir.absolutePath)
+            it.writeTo(targetGeneratedDir) }
 }
 
