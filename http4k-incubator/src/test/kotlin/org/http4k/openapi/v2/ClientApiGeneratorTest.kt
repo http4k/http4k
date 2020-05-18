7940631d005925ee689d92a98ee564a244749585
David Denton <denton.david@gmail.com>
2020-05-18 18:42:59 +0100

    tests for v2

--- http4k-incubator/src/test/kotlin/org/http4k/openapi/v2/ClientApiGeneratorTest.kt
+++ http4k-incubator/src/test/kotlin/org/http4k/openapi/v2/ClientApiGeneratorTest.kt
@@ -1,10 +1,5 @@
 package org.http4k.openapi.v2
 
-import org.http4k.junit.ResourceLoader
 import org.http4k.openapi.ApiGeneratorContract
-import org.http4k.testing.Approver
 
-class ClientApiGeneratorTest : ApiGeneratorContract<OpenApi2Spec>(OpenApi2Spec::class, ClientApiGenerator) {
-    override fun `route with path`(app: Approver, rl: ResourceLoader) {
-    }
-}
+class ClientApiGeneratorTest : ApiGeneratorContract<OpenApi2Spec>(OpenApi2Spec::class, ClientApiGenerator)
