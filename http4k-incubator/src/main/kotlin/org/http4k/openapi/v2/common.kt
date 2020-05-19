2d3bbcc5f4f1d1a52297cce140be89ba87772cd9
David Denton <denton.david@gmail.com>
2020-05-19 06:09:46 +0000

    add test cases for v2

--- http4k-incubator/src/main/kotlin/org/http4k/openapi/v2/common.kt
+++ http4k-incubator/src/main/kotlin/org/http4k/openapi/v2/common.kt
@@ -6,25 +6,25 @@ import org.http4k.openapi.v3.OpenApi3Spec
 import org.http4k.openapi.v3.PathV3Spec
 import org.http4k.openapi.v3.ParameterSpec as ParameterSpecV3
 
-fun PathV2Spec.asV3() = PathV3Spec(operationId, emptyMap(), null, parameters.filter { it.type != "body" }.map { it.asV3() })
-
-private fun ParameterSpec.asV3(): ParameterSpecV3 {
-    val schema = when (type) {
-        "string" -> SchemaSpec.StringSpec
-        "integer" -> SchemaSpec.IntegerSpec
-        "number" -> SchemaSpec.NumberSpec
-        "boolean" -> SchemaSpec.BooleanSpec
-        else -> throw UnsupportedOperationException("cannot support parameter type of $type")
-    }
-    return when (`in`) {
-        "path" -> ParameterSpecV3.PathSpec(name, required, schema)
-        "header" -> ParameterSpecV3.HeaderSpec(name, required, schema)
-        "query" -> ParameterSpecV3.QuerySpec(name, required, schema)
-        "cookie" -> ParameterSpecV3.CookieSpec(name, required, schema)
-        else -> throw UnsupportedOperationException("cannot support parameter location of $`in`")
-    }
-}
-
 fun OpenApi2Spec.asV3() = OpenApi3Spec(
     info, paths.mapValues { it.value.mapValues { it.value.asV3() } }, ComponentsV3Spec(components)
 )
+
+private fun PathV2Spec.asV3() = PathV3Spec(operationId, emptyMap(), null, parameters.filterNot { it is ParameterSpec.BodySpec }.mapNotNull { it.asV3() })
+
+private fun ParameterSpec.asV3(): ParameterSpecV3? =
+    when (this) {
+        is ParameterSpec.CookieSpec -> ParameterSpecV3.CookieSpec(name, required, type.asSchema())
+        is ParameterSpec.HeaderSpec -> ParameterSpecV3.HeaderSpec(name, required, type.asSchema())
+        is ParameterSpec.PathSpec -> ParameterSpecV3.PathSpec(name, required, type.asSchema())
+        is ParameterSpec.QuerySpec -> ParameterSpecV3.QuerySpec(name, required, type.asSchema())
+        else -> null
+    }
+
+private fun String.asSchema(): SchemaSpec = when (this) {
+    "string" -> SchemaSpec.StringSpec
+    "integer" -> SchemaSpec.IntegerSpec
+    "number" -> SchemaSpec.NumberSpec
+    "boolean" -> SchemaSpec.BooleanSpec
+    else -> throw UnsupportedOperationException("cannot support parameter type of $this")
+}
