c325f52ae6b3038a4220ca33542ab415ed418af8
David Denton <denton.david@gmail.com>
2020-05-19 06:41:07 +0000

    bind to body

--- http4k-incubator/src/main/kotlin/org/http4k/openapi/v3/server/endpoint.kt
+++ http4k-incubator/src/main/kotlin/org/http4k/openapi/v3/server/endpoint.kt
@@ -1,10 +1,13 @@
 package org.http4k.openapi.v3.server
 
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FunSpec
 import org.http4k.core.Method
+import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.openapi.v3.PathV3
+import org.http4k.openapi.v3.client.bindFirstToHttpMessage
 import org.http4k.poet.Property
 import org.http4k.poet.addCodeBlocks
 import org.http4k.poet.packageMember
@@ -14,13 +17,21 @@ import org.http4k.poet.responseLensDeclarations
 import org.http4k.routing.RoutingHttpHandler
 
 fun PathV3.buildEndpoint(modelPackageName: String) = with(this) {
+    val body = CodeBlock.builder().add("%T(%T.OK)", Property<Response>().type, Property<Status>().type)
+    responseSchemas().bindFirstToHttpMessage("TODO()").firstOrNull()?.also { body.add(it) }
+
+    val handler = CodeBlock.builder()
+        .addStatement("{ req: %T ->", Property<Request>().type)
+        .indent()
+        .add(body.build())
+        .unindent()
+        .add("\n}")
+        .build()
+
     FunSpec.builder(uniqueName)
         .returns(Property<RoutingHttpHandler>().type)
         .addCodeBlocks(requestLensDeclarations(modelPackageName) + responseLensDeclarations(modelPackageName) + parameterLensDeclarations())
-        .addStatement("return·\"$urlPathPattern\"·%M·%T.${method}·to·{ %T(%T.OK) }",
-            packageMember<RoutingHttpHandler>("bind"),
-            Property<Method>().type,
-            Property<Response>().type,
-            Property<Status>().type)
+        .addCode("return·\"$urlPathPattern\"·%M·%T.${method}·to·", packageMember<RoutingHttpHandler>("bind"), Property<Method>().type)
+        .addCode(handler)
         .build()
 }
