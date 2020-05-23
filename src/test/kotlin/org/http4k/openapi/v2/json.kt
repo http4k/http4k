9ebcacab23917d421940b91f2464780f2fea4050
David Denton <denton.david@gmail.com>
2020-05-23 16:41:40 +0100

    add YAML option

--- src/test/kotlin/org/http4k/openapi/v2/json.kt
+++ src/test/kotlin/org/http4k/openapi/v2/json.kt
@@ -1,13 +1,13 @@
 package org.http4k.openapi.v2
 
+import org.http4k.format.Jackson.asA
 import org.http4k.openapi.GenerationOptions
-import org.http4k.openapi.OpenApiJson.asA
 import java.io.File
 
 fun main() {
-    val generationOptions = GenerationOptions("org.http4k", File("http4k/src/main/kotlin"))
+    val generationOptions = GenerationOptions("org.http4k", File("src/main/kotlin"))
     val targetGeneratedDir = generationOptions.destinationFolder.apply { mkdirs() }
-    val spec = File("apiSpec.json").readText().asA(OpenApi2Spec::class)
+    val spec = File("../apiSpec.json").readText().asA(OpenApi2Spec::class)
 
     println("Generating files to: " + targetGeneratedDir.absolutePath)
 
