3467ad48a0cfd0be26a7d8b19aec0ce78eb1261a
Ivan Sanchez <ivan@gourame.com>
2020-06-18 09:12:13 +0100

    Add test

---
+++ http4k-serverless-lambda/test-function/src/test/kotlin/org/http4k/testing/lambda/TestAppTest.kt
@@ -0,0 +1,17 @@
+package org.http4k.testing.lambda
+
+import com.natpryce.hamkrest.assertion.assertThat
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Status
+import org.http4k.hamkrest.hasStatus
+import org.junit.jupiter.api.Test
+
+class TestAppTest {
+    @Test
+    fun `can invoke handler`() {
+        val app = TestApp(emptyMap())
+
+        assertThat(app(Request(Method.GET, "/")), hasStatus(Status.OK))
+    }
+}
