c37fad5177ddaead166fdf86854717a671a9a61f
David <denton.david@gmail.com>
2020-06-23 07:55:48 +0100

    fix modules in maven

--- release-maven-central-2.sh
+++ release-maven-central-2.sh
@@ -3,16 +3,16 @@
 source ./release-functions.sh
 
 maven_publish "http4k-client-apache"
-maven_publish "http4k-client-apache4"
+#maven_publish "http4k-client-apache4"
 maven_publish "http4k-client-apache-async"
-maven_publish "http4k-client-apache4-async"
+#maven_publish "http4k-client-apache4-async"
 maven_publish "http4k-client-jetty"
 maven_publish "http4k-client-okhttp"
 maven_publish "http4k-client-websocket"
 
 maven_publish "http4k-serverless-gcf"
 maven_publish "http4k-serverless-lambda"
-#maven_publish "http4k-serverless-openwhisk"
+maven_publish "http4k-serverless-openwhisk"
 maven_publish "http4k-jsonrpc"
 maven_publish "http4k-multipart"
 maven_publish "http4k-aws"
