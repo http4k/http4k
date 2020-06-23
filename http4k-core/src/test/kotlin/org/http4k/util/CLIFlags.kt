64af60bc3eb9bb770c09f0afd8ad69aab12bc5b8
David Denton <denton.david@gmail.com>
2020-06-23 18:54:04 +0100

    comments on CLI code

--- http4k-core/src/test/kotlin/org/http4k/util/CLIFlags.kt
+++ http4k-core/src/test/kotlin/org/http4k/util/CLIFlags.kt
@@ -3,19 +3,26 @@ package org.http4k.util
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-abstract class CliFlags(val args: Array<String>)
+/**
+ * Nano CLI flag library. Uses the name of the property to load from the passed array of args, else fetch blows up.
+ */
+abstract class CliFlags(val args: Array<String>) {
+    fun requiredFlag() = CLIFlag()
+    fun defaultedFlag(default: String?) = CLIFlag(default)
+}
 
-class CLIFlag private constructor(private val default: String? = null) : ReadOnlyProperty<CliFlags, String> {
+/**
+ * Delegate that loads the named flag from the comment line
+ */
+class CLIFlag internal constructor(private val default: String? = null) : ReadOnlyProperty<CliFlags, String> {
     override fun getValue(thisRef: CliFlags, property: KProperty<*>): String {
         val windowed = thisRef.args.toList().windowed(2).map { it[0] to it[1] }.toMap()
         return windowed["--${property.name}"] ?: default
         ?: throw IllegalArgumentException("no --${property.name} passed")
     }
-
-    companion object {
-        fun required() = CLIFlag()
-        fun defaulted(default: String?) = CLIFlag(default)
-    }
 }
 
+/**
+ * Useful method to encapsulate usage of command line flags
+ */
 fun <T : CliFlags> T.use(fn: T.() -> Unit): Unit = fn(this)
