352f28569cd184b604ac9dd31067c9b8ea42f0fe
David Denton <denton.david@gmail.com>
2020-06-29 18:32:10 +0100

    update flags

--- http4k-serverless-openwhisk/integration-test/src/test/kotlin/org/http4k/serverless/OpenWhiskCliFlags.kt
+++ http4k-serverless-openwhisk/integration-test/src/test/kotlin/org/http4k/serverless/OpenWhiskCliFlags.kt
@@ -8,7 +8,7 @@ import dev.forkhandles.bunting.Bunting
 class OpenWhiskCliFlags(args: Array<String>) : Bunting(args) {
     val insecure by noValueFlag("Disable cert checking (use for local OW deployments)")
     val main by requiredFlag("Class containing the main action to invoke")
-    val version by defaultedFlag("0.0.0", "The version of the Action")
+    val version by defaultedFlag("0.0.1", "The version")
     val jarFile by requiredFlag("Path to the JAR file containing Action code")
     val namespace by requiredFlag("OpenWhisk Namespace")
     val actionName by requiredFlag("OpenWhisk Action name")
