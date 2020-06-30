7dc0609d491c15d143e84ed3ddf425af50cb0311
David Denton <denton.david@gmail.com>
2020-06-30 06:20:16 +0000

    various fixes to openwhisk deployment code

--- http4k-core/src/test/kotlin/org/http4k/serverless/TestServerlessFunction.kt
+++ http4k-core/src/test/kotlin/org/http4k/serverless/TestServerlessFunction.kt
@@ -1,11 +1,9 @@
 package org.http4k.serverless
 
 import org.http4k.client.ServerForClientContract
-import org.http4k.core.HttpHandler
 import org.http4k.core.then
-import org.http4k.filter.DebuggingFilters
+import org.http4k.filter.DebuggingFilters.PrintRequestAndResponse
 
 object TestServerlessFunction : AppLoader {
-    override fun invoke(env: Map<String, String>): HttpHandler =
-        DebuggingFilters.PrintRequestAndResponse().then(ServerForClientContract)
+    override fun invoke(env: Map<String, String>) = PrintRequestAndResponse().then(ServerForClientContract)
 }
