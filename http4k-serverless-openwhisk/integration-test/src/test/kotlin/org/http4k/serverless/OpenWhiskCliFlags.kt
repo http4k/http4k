e1b027f04dc35e6f7b0c76912121ab63d27f1623
David Denton <denton.david@gmail.com>
2020-07-15 20:44:44 +0100

    upgrade bunting

--- http4k-serverless-openwhisk/integration-test/src/test/kotlin/org/http4k/serverless/OpenWhiskCliFlags.kt
+++ http4k-serverless-openwhisk/integration-test/src/test/kotlin/org/http4k/serverless/OpenWhiskCliFlags.kt
@@ -8,14 +8,14 @@ import java.io.File
  */
 class OpenWhiskCliFlags(args: Array<String>) : Bunting(args) {
     val credentialsFile by option("Path to OW credentials file")
-        .defaultsTo("${System.getenv("HOME")}/.wskprops")
         .map(::File)
+        .defaultsTo(File("${System.getenv("HOME")}/.wskprops"))
     val insecure by switch("Disable cert checking (use for local OW deployments)")
-    val main by option("Class containing the main action to invoke")
+    val main by option("Class containing the main action to invoke").required()
     val version by option("The version").defaultsTo("0.0.1")
-    val jarFile by option("Path to the JAR file containing Action code")
-    val namespace by option("OpenWhisk Namespace")
-    val actionName by option("OpenWhisk Action name")
-    val packageName by option("OpenWhisk Package name")
+    val jarFile by option("Path to the JAR file containing Action code").required()
+    val namespace by option("OpenWhisk Namespace").required()
+    val actionName by option("OpenWhisk Action name").required()
+    val packageName by option("OpenWhisk Package name").required()
     val verbose by switch("Print all HTTP traffic")
 }
