fcc2bb888e744125545efccbff868987911f4d79
Niklas Lochschmidt <nlochschmidt@gmail.com>
2020-08-05 17:52:20 +0200

    Refactor SunHttp to allow reuse of handler

--- http4k-core/src/main/kotlin/org/http4k/server/SunHttp.kt
+++ http4k-core/src/main/kotlin/org/http4k/server/SunHttp.kt
@@ -1,6 +1,7 @@
 package org.http4k.server
 
 import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler as SunHttpHandler
 import com.sun.net.httpserver.HttpServer
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
@@ -11,6 +12,36 @@ import org.http4k.core.Uri
 import org.http4k.core.safeLong
 import java.net.InetSocketAddress
 
+class HttpExchangeHandler(private val handler: HttpHandler): SunHttpHandler {
+    private fun HttpExchange.populate(httpResponse: Response) {
+        httpResponse.headers.forEach { (key, value) -> responseHeaders.add(key, value) }
+        if (requestMethod == "HEAD") {
+            sendResponseHeaders(httpResponse.status.code, -1)
+        } else {
+            sendResponseHeaders(httpResponse.status.code, 0)
+            httpResponse.body.stream.use { input -> responseBody.use { input.copyTo(it) } }
+        }
+    }
+
+    private fun HttpExchange.toRequest(): Request =
+        Request(Method.valueOf(requestMethod),
+            requestURI.rawQuery?.let { Uri.of(requestURI.rawPath).query(requestURI.rawQuery) }
+                ?: Uri.of(requestURI.rawPath))
+            .body(requestBody, requestHeaders.getFirst("Content-Length").safeLong())
+            .headers(requestHeaders.toList().flatMap { (key, values) -> values.map { key to it } })
+            .source(RequestSource(localAddress.address.hostAddress, localAddress.port))
+
+    override fun handle(exchange: HttpExchange) {
+        with(exchange) {
+            try {
+                populate(handler(toRequest()))
+            } catch (e: Exception) {
+                sendResponseHeaders(500, 0)
+            }
+            close()
+        }
+    }
+}
 
 data class SunHttp(val port: Int = 8000) : ServerConfig {
     override fun toServer(httpHandler: HttpHandler): Http4kServer = object : Http4kServer {
@@ -18,35 +49,10 @@ data class SunHttp(val port: Int = 8000) : ServerConfig {
 
         private val server = HttpServer.create(InetSocketAddress(port), 0)
         override fun start(): Http4kServer = apply {
-            server.createContext("/") {
-                try {
-                    it.populate(httpHandler(it.toRequest()))
-                } catch (e: Exception) {
-                    it.sendResponseHeaders(500, 0)
-                }
-                it.close()
-            }
+            server.createContext("/", HttpExchangeHandler(httpHandler))
             server.start()
         }
 
         override fun stop() = apply { server.stop(0) }
     }
 }
-
-private fun HttpExchange.populate(httpResponse: Response) {
-    httpResponse.headers.forEach { (key, value) -> responseHeaders.add(key, value) }
-    if (requestMethod == "HEAD") {
-        sendResponseHeaders(httpResponse.status.code, -1)
-    } else {
-        sendResponseHeaders(httpResponse.status.code, 0)
-        httpResponse.body.stream.use { input -> responseBody.use { input.copyTo(it) } }
-    }
-}
-
-private fun HttpExchange.toRequest(): Request =
-    Request(Method.valueOf(requestMethod),
-        requestURI.rawQuery?.let { Uri.of(requestURI.rawPath).query(requestURI.rawQuery) }
-            ?: Uri.of(requestURI.rawPath))
-        .body(requestBody, requestHeaders.getFirst("Content-Length").safeLong())
-        .headers(requestHeaders.toList().flatMap { (key, values) -> values.map { key to it } })
-        .source(RequestSource(localAddress.address.hostAddress, localAddress.port))
