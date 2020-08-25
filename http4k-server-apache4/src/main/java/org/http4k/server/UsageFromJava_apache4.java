54220597369cb64bd85aaf3ea77bce2486bb4bac
David Denton <denton.david@gmail.com>
2020-08-25 13:17:27 +0100

    rename - jacoco!!!!

--- http4k-server-apache4/src/main/java/org/http4k/server/UsageFromJava_apache4.java
+++ http4k-server-apache4/src/main/java/org/http4k/server/UsageFromJava_apache4.java
@@ -5,7 +5,7 @@ import org.http4k.core.Status;
 
 import static org.http4k.server.Http4kServerKt.asServer;
 
-public interface UsageFromJava_apache {
+public interface UsageFromJava_apache4 {
     Apache4Server apache = new Apache4Server(8000);
     Http4kServer http4kServer = asServer(req -> Response.Companion.create(Status.ACCEPTED), apache);
 }
