d8addb05adc38c7d5483b6f48e0c7453a4cdf48e
David Denton <denton.david@gmail.com>
2020-08-26 17:48:56 +0100

    deprecations for result4k

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -11,25 +11,24 @@ import org.gradle.plugin.use.PluginDependencySpec
  * YOU are responsible for updating manually the dependency version.
  */
 object Versions {
-    const val org_jetbrains_kotlinx_kotlinx_serialization: String = "1.0.0-RC2"
+    const val org_jetbrains_kotlinx_kotlinx_serialization: String = "0.20.0"
+             // available: "0.20.0-1.4-M1-release-99"
 
-    const val com_fasterxml_jackson_dataformat: String = "2.11.3"
+    const val com_fasterxml_jackson_dataformat: String = "2.11.2"
 
     const val io_github_resilience4j: String = "1.5.0"
 
-    const val software_amazon_awssdk: String = "2.15.2"
+    const val org_jetbrains_kotlin: String = "1.3.72" // available: "1.4.0"
 
-    const val org_jetbrains_kotlin: String = "1.4.10"
+    const val com_squareup_moshi: String = "1.9.3"
 
-    const val com_squareup_moshi: String = "1.10.0"
+    const val org_eclipse_jetty: String = "9.4.31.v20200723"
 
-    const val org_eclipse_jetty: String = "9.4.32.v20200930"
+    const val org_junit_jupiter: String = "5.6.2"
 
-    const val org_junit_jupiter: String = "5.7.0"
+    const val io_undertow: String = "2.1.3.Final"
 
-    const val io_undertow: String = "2.2.0.Final"
-
-    const val io_ktor: String = "1.4.1"
+    const val io_ktor: String = "1.3.2" // available: "1.3.2-1.4-M1-release-99"
 
     const val de_fayard_buildsrcversions_gradle_plugin: String = "0.7.0"
 
@@ -39,45 +38,43 @@ object Versions {
 
     const val openapi_generator_gradle_plugin: String = "4.3.1"
 
-    const val javax_websocket_server_impl: String = "9.4.32.v20200930"
+    const val javax_websocket_server_impl: String = "9.4.31.v20200723"
 
-    const val kotest_assertions_core_jvm: String = "4.2.5"
+    const val kotest_assertions_core_jvm: String = "4.1.3"
 
     const val coveralls_gradle_plugin: String = "2.8.3"
 
-    const val functions_framework_api: String = "1.0.2"
+    const val functions_framework_api: String = "1.0.1"
 
-    const val aws_lambda_java_events: String = "3.3.1"
+    const val aws_lambda_java_events: String = "3.2.0"
 
-    const val jackson_module_kotlin: String = "2.11.3"
+    const val jackson_module_kotlin: String = "2.11.2"
 
     const val aws_lambda_java_core: String = "1.2.1"
 
-    const val aws_java_sdk_bundle: String = "1.11.875"
-
-    const val dokka_gradle_plugin: String = "0.10.1" // available: "1.4.10"
+    const val dokka_gradle_plugin: String = "0.10.1"
 
     const val javax_servlet_api: String = "4.0.1"
 
-    const val netty_codec_http2: String = "4.1.52.Final"
+    const val netty_codec_http2: String = "4.1.51.Final"
 
-    const val jackson_databind: String = "2.11.3"
+    const val jackson_databind: String = "2.11.2"
 
     const val httpasyncclient: String = "4.1.4"
 
-    const val micrometer_core: String = "1.5.5"
+    const val micrometer_core: String = "1.5.4"
 
     const val java_websocket: String = "1.5.1"
 
-    const val commons_pool2: String = "2.9.0"
+    const val commons_pool2: String = "2.8.1"
 
-    const val http2_server: String = "9.4.32.v20200930"
+    const val http2_server: String = "9.4.31.v20200723"
 
     const val ratpack_core: String = "1.8.0"
 
     const val selenium_api: String = "3.141.59"
 
-    const val httpclient5: String = "5.0.2"
+    const val httpclient5: String = "5.0.1"
 
     const val freemarker: String = "2.3.30"
 
@@ -85,13 +82,13 @@ object Versions {
 
     const val httpclient: String = "4.5.12"
 
-    const val underscore: String = "1.59"
+    const val underscore: String = "1.57"
 
     const val alpn_boot: String = "8.1.13.v20181017"
 
-    const val bunting4k: String = "1.2.0.0"
+    const val bunting4k: String = "0.18.0.0"
 
-    const val httpcore5: String = "5.0.2"
+    const val httpcore5: String = "5.0.1"
 
     const val joda_time: String = "2.10.6"
 
@@ -99,15 +96,15 @@ object Versions {
 
     const val thymeleaf: String = "3.0.11.RELEASE"
 
-    const val hamkrest: String = "1.8.0.1"
+    const val hamkrest: String = "1.7.0.3"
 
     const val httpcore: String = "4.4.13"
 
-    const val result4k: String = "1.2.0.0"
+    const val result4k: String = "0.18.0.0"
 
     const val jade4j: String = "1.3.2"
 
-    const val okhttp: String = "4.9.0"
+    const val okhttp: String = "4.8.1"
 
     const val pebble: String = "3.1.4"
 
@@ -126,7 +123,7 @@ object Versions {
      * See issue 19: How to update Gradle itself?
      * https://github.com/jmfayard/buildSrcVersions/issues/19
      */
-    const val gradleLatestVersion: String = "6.6.1"
+    const val gradleLatestVersion: String = "6.6"
 }
 
 /**
@@ -135,4 +132,4 @@ object Versions {
  */
 val PluginDependenciesSpec.buildSrcVersions: PluginDependencySpec
     inline get() =
-        id("de.fayard.buildSrcVersions").version(Versions.de_fayard_buildsrcversions_gradle_plugin)
+            id("de.fayard.buildSrcVersions").version(Versions.de_fayard_buildsrcversions_gradle_plugin)
