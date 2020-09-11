05abf4fe814741410a48d44b05b93b04601c1923
David Denton <denton.david@gmail.com>
2020-09-11 14:39:48 +0100

    [skip travis] pass github token

--- release-site.sh
+++ release-site.sh
@@ -9,9 +9,11 @@ pip install -r requirements.txt
 
 ./tools/embed_code.py
 
+export TOKEN_TO_USE=${$GH_TOKEN:$1}
+
 TMP=/tmp/http4k.github.io/
 rm -rf ${TMP}
-git clone https://${GH_TOKEN}@github.com/http4k/http4k.github.io.git ${TMP}
+git clone https://${TOKEN_TO_USE}@github.com/http4k/http4k.github.io.git ${TMP}
 cp -R build/docs-website/docs ${TMP}/
 
 cp -R src/mkdocs.yml ${TMP}
