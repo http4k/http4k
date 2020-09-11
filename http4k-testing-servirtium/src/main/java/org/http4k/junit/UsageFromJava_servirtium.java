a30dce071f15e93d6d65c050fb0d98495996726e
David Denton <denton.david@gmail.com>
2020-09-11 12:02:20 +0100

    added server examples

--- http4k-testing-servirtium/src/main/java/org/http4k/junit/UsageFromJava_servirtium.java
+++ http4k-testing-servirtium/src/main/java/org/http4k/junit/UsageFromJava_servirtium.java
@@ -1,10 +1,35 @@
 package org.http4k.junit;
 
+import org.http4k.client.ApacheClient;
 import org.http4k.core.Response;
 import org.http4k.core.Status;
+import org.http4k.core.Uri;
+import org.http4k.server.SunHttp;
+import org.http4k.servirtium.InteractionOptions;
 import org.http4k.servirtium.InteractionStorage;
+import org.http4k.servirtium.ServirtiumServer;
 
 public interface UsageFromJava_servirtium {
+    // junit extension
     ServirtiumRecording servirtiumRecording = new ServirtiumRecording("", req -> Response.Companion.create(Status.ACCEPTED), InteractionStorage.InMemory());
     ServirtiumReplay servirtiumReplay = new ServirtiumReplay("", InteractionStorage.InMemory());
+
+    // standalone server
+    ServirtiumServer recordingServer = ServirtiumServer.Recording(
+        "name of interaction",
+        Uri.of("http://localhost:8000"),
+        InteractionStorage.InMemory(),
+        InteractionOptions.Defaults,
+        9000,
+        SunHttp::new,
+        ApacheClient.create()
+    );
+
+    ServirtiumServer replayServer = ServirtiumServer.Replay(
+        "name of interaction",
+        InteractionStorage.InMemory(),
+        InteractionOptions.Defaults,
+        0,
+        SunHttp::new
+    );
 }
