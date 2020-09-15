fbf56714b43da2a195d9b0224b28520d5dc8d77d
Albert Latacz <albert.latacz@gmail.com>
2020-09-15 06:49:47 +0100

    ignore bad requests from google analytics

--- http4k-google/src/test/kotlin/org/http4k/google/GoogleAnalyticsTest.kt
+++ http4k-google/src/test/kotlin/org/http4k/google/GoogleAnalyticsTest.kt
@@ -9,6 +9,7 @@ import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.BAD_REQUEST
+import org.http4k.core.Status.Companion.CLIENT_TIMEOUT
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.UriTemplate
 import org.http4k.core.body.form
@@ -31,8 +32,7 @@ class GoogleAnalyticsTest {
 
     @Test
     fun `logs request as page view`() {
-        val request = Request(GET, "https://www.http4k.org/some/world")
-        val response = analytics(request)
+        val response = analytics(Request(GET, "https://www.http4k.org/some/world"))
 
         assertThat(response, hasStatus(OK))
         assertPageView("/some/world", "/some/world", "www.http4k.org")
@@ -40,8 +40,7 @@ class GoogleAnalyticsTest {
 
     @Test
     fun `logs routed request as page view`() {
-        val request = RoutedRequest(Request(GET, "/some/world"), UriTemplate.from("/some/{hello}"))
-        val response = analytics(request)
+        val response = analytics(RoutedRequest(Request(GET, "/some/world"), UriTemplate.from("/some/{hello}")))
 
         assertThat(response, hasStatus(OK))
         assertPageView("some/{hello}", "some/{hello}", "")
@@ -49,17 +48,25 @@ class GoogleAnalyticsTest {
 
     @Test
     fun `logs request with host as page view`() {
-        val request = Request(GET, "/some/world").header("host", "www.http4k.org")
-        val response = analytics(request)
+        val response = analytics(Request(GET, "/some/world").header("host", "www.http4k.org"))
 
         assertThat(response, hasStatus(OK))
         assertPageView("/some/world", "/some/world", "www.http4k.org")
     }
 
     @Test
+    fun `ignore bad responses from google analytics`() {
+        testHttpClient.response = Response(CLIENT_TIMEOUT)
+
+        val response = analytics(Request(GET, "some/world"))
+
+        assertThat(response, hasStatus(OK))
+        assertPageView("some/world", "some/world", "")
+    }
+
+    @Test
     fun `do not logs page view on unsuccessful response`() {
-        val request = Request(GET, "/fail")
-        val response = analytics(request)
+        val response = analytics(Request(GET, "/fail"))
 
         assertThat(response, hasStatus(BAD_REQUEST))
         assertNoPageView()
@@ -67,12 +74,12 @@ class GoogleAnalyticsTest {
 
     private fun assertPageView(title: String, path: String, host: String) {
         assertThat(testHttpClient.captured, equalTo(Request(POST, "/collect")
-                .form(VERSION, "1")
-                .form(MEASUREMENT_ID, "TEST-MEASUREMENT-ID")
-                .form(CLIENT_ID, "TEST-CLIENT-ID")
-                .form(DOCUMENT_TITLE, title)
-                .form(DOCUMENT_PATH, path)
-                .form(DOCUMENT_HOST, host)
+            .form(VERSION, "1")
+            .form(MEASUREMENT_ID, "TEST-MEASUREMENT-ID")
+            .form(CLIENT_ID, "TEST-CLIENT-ID")
+            .form(DOCUMENT_TITLE, title)
+            .form(DOCUMENT_PATH, path)
+            .form(DOCUMENT_HOST, host)
         ))
     }
 
@@ -83,9 +90,10 @@ class GoogleAnalyticsTest {
 
 class CapturingHttpHandler : HttpHandler {
     var captured: Request? = null
+    var response: Response = Response(OK)
 
     override fun invoke(request: Request): Response {
         captured = request
-        return Response(OK)
+        return response
     }
 }
