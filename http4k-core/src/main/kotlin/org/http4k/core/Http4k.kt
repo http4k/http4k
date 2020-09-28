78a7f38c4c55051853f04041affe188301216820
David Denton <denton.david@gmail.com>
2020-09-28 09:51:50 +0100

    remove Filter.create as is now SAM

--- http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/Http4k.kt
@@ -5,10 +5,7 @@ import org.http4k.routing.RoutingHttpHandler
 typealias HttpHandler = (Request) -> Response
 
 fun interface Filter : (HttpHandler) -> HttpHandler {
-    companion object {
-        @JvmStatic
-        fun create(fn: (HttpHandler) -> HttpHandler): Filter = Filter { next -> fn(next) }
-    }
+    companion object
 }
 
 val Filter.Companion.NoOp: Filter get() = Filter { next -> { next(it) } }
