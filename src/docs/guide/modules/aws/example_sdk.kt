c7950c1159ecd8443894f56101aa4619fa0f198a
David Denton <denton.david@gmail.com>
2020-09-28 19:34:56 +0100

    [skip travis] add creds to AWS example

--- src/docs/guide/modules/aws/example_sdk.kt
+++ src/docs/guide/modules/aws/example_sdk.kt
@@ -4,6 +4,8 @@ import org.http4k.aws.AwsSdkClient
 import org.http4k.client.OkHttp
 import org.http4k.core.then
 import org.http4k.filter.DebuggingFilters
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.regions.Region.EU_WEST_1
 import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.s3.model.CreateBucketRequest
 
@@ -11,6 +13,8 @@ fun main() {
     val fakeS3 = DebuggingFilters.PrintRequestAndResponse().then(OkHttp())
 
     val s3 = S3Client.builder()
+        .region(EU_WEST_1)
+        .credentialsProvider { AwsBasicCredentials.create("accessKey", "secret") }
         .httpClient(AwsSdkClient(fakeS3))
         .build()
 
