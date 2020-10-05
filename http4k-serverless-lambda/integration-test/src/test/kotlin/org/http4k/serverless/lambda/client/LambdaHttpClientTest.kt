b3a4176d062b44ccc728236a67bc3e199c941986
Ivan Sanchez <ivan@gourame.com>
2020-10-05 19:52:33 +0100

    Extract NoOpServer

--- http4k-serverless-lambda/integration-test/src/test/kotlin/org/http4k/serverless/lambda/client/LambdaHttpClientTest.kt
+++ http4k-serverless-lambda/integration-test/src/test/kotlin/org/http4k/serverless/lambda/client/LambdaHttpClientTest.kt
@@ -5,14 +5,11 @@ import org.http4k.client.HttpClientContract
 import org.http4k.client.JavaHttpClient
 import org.http4k.cloudnative.env.Environment
 import org.http4k.core.Filter
-import org.http4k.core.HttpHandler
 import org.http4k.core.NoOp
 import org.http4k.core.Request
 import org.http4k.core.then
 import org.http4k.filter.AwsAuth
 import org.http4k.filter.ClientFilters
-import org.http4k.server.Http4kServer
-import org.http4k.server.ServerConfig
 import org.junit.jupiter.api.Assumptions.assumeTrue
 import org.junit.jupiter.api.BeforeEach
 
@@ -40,14 +37,4 @@ class LambdaHttpClientTest : HttpClientContract({ NoOpServerConfig }, client, cl
     override fun `connection refused are converted into 503`() = assumeTrue(false, "Unsupported client feature")
     override fun `unknown host are converted into 503`() = assumeTrue(false, "Unsupported client feature")
     override fun `send binary data`() = assumeTrue(false, "Unsupported client feature")
-}
-
-private object NoOpHttp4kServer : Http4kServer {
-    override fun start(): Http4kServer = this
-    override fun stop(): Http4kServer = this
-    override fun port(): Int = 0
-}
-
-internal object NoOpServerConfig : ServerConfig {
-    override fun toServer(httpHandler: HttpHandler): Http4kServer = NoOpHttp4kServer
 }
