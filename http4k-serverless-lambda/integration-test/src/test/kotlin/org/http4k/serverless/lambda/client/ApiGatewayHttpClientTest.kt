bf2948d4ea126707169b1b30436c2a2154522afc
Ivan Sanchez <ivan@gourame.com>
2020-10-05 17:11:24 +0100

    Disable unsupported API gateway contract tests

--- http4k-serverless-lambda/integration-test/src/test/kotlin/org/http4k/serverless/lambda/client/ApiGatewayHttpClientTest.kt
+++ http4k-serverless-lambda/integration-test/src/test/kotlin/org/http4k/serverless/lambda/client/ApiGatewayHttpClientTest.kt
@@ -5,9 +5,14 @@ import org.http4k.client.OkHttp
 import org.http4k.core.Uri
 import org.http4k.core.then
 import org.http4k.filter.ClientFilters
+import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.Disabled
 
-val apiClient = ClientFilters.SetBaseUriFrom(Uri.of("https://something.execute-api.us-east-1.amazonaws.com/")).then(OkHttp())
+private val apiClient = ClientFilters.SetBaseUriFrom(Uri.of("https://<apiId>.execute-api.us-east-1.amazonaws.com/")).then(OkHttp())
 
 @Disabled
-class ApiGatewayHttpClientTest : HttpClientContract({ NoOpServerConfig }, apiClient, apiClient)
+class ApiGatewayHttpClientTest : HttpClientContract({ NoOpServerConfig }, apiClient, apiClient) {
+    override fun `connection refused are converted into 503`() = Assumptions.assumeTrue(false, "Unsupported client feature")
+    override fun `handles response with custom status message`() = Assumptions.assumeTrue(false, "Unsupported client feature")
+    override fun `unknown host are converted into 503`()  = Assumptions.assumeTrue(false, "Unsupported client feature")
+}
