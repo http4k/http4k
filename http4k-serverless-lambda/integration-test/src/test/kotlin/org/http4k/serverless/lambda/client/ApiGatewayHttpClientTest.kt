00582995d461d6c92e54155fb49c6524b11877a6
Ivan Sanchez <ivan@gourame.com>
2020-10-06 09:08:44 +0100

    Allow testing both versions in ApiGatewayHttpClientTest

--- http4k-serverless-lambda/integration-test/src/test/kotlin/org/http4k/serverless/lambda/client/ApiGatewayHttpClientTest.kt
+++ http4k-serverless-lambda/integration-test/src/test/kotlin/org/http4k/serverless/lambda/client/ApiGatewayHttpClientTest.kt
@@ -2,26 +2,33 @@ package org.http4k.serverless.lambda.client
 
 import org.http4k.aws.ApiIntegrationVersion
 import org.http4k.aws.ApiIntegrationVersion.v1
+import org.http4k.aws.ApiIntegrationVersion.v2
 import org.http4k.client.HttpClientContract
 import org.http4k.client.OkHttp
 import org.http4k.core.Request
+import org.http4k.core.Response
 import org.http4k.core.then
 import org.http4k.filter.ClientFilters
 import org.http4k.serverless.lambda.DeployApiGateway.apiName
 import org.junit.jupiter.api.Assumptions.assumeTrue
+import org.junit.jupiter.api.Disabled
 import org.opentest4j.TestAbortedException
 
-private val apiClient by lazy {
-    val api = apiGatewayClient.listApis().find { it.name == apiName(ApiIntegrationVersion.v2) }
+private fun client(version: ApiIntegrationVersion): (Request) -> Response {
+    val api = apiGatewayClient.listApis().find { it.name == apiName(version) }
         ?: throw TestAbortedException("API hasn't been deployed")
-    ClientFilters.SetBaseUriFrom(api.apiEndpoint).then(OkHttp())
+    val apiClient = ClientFilters.SetBaseUriFrom(api.apiEndpoint).then(OkHttp())
+    return { request: Request -> apiClient(request) }
 }
 
-private val client = { request: Request -> apiClient(request) }
-
-class ApiGatewayHttpClientTest : HttpClientContract({ NoOpServerConfig }, client, client) {
+abstract class ApiGatewayHttpClientTest(version: ApiIntegrationVersion) :
+    HttpClientContract({ NoOpServerConfig }, client(version), client(version)) {
     override fun `connection refused are converted into 503`() = assumeTrue(false, "Unsupported client feature")
     override fun `handles response with custom status message`() = assumeTrue(false, "Unsupported client feature")
     override fun `unknown host are converted into 503`()  = assumeTrue(false, "Unsupported client feature")
 }
 
+class ApiGatewayV1ClientTest:ApiGatewayHttpClientTest(v1)
+
+@Disabled("v2 is not supported by the client yet")
+class ApiGatewayV2ClientTest:ApiGatewayHttpClientTest(v2)
