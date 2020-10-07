14a8ca61943fc9d055f64619ed7af7488dac0471
David Denton <denton.david@gmail.com>
2020-10-07 14:21:22 +0100

    [skip travis] java

--- http4k-server-netty/src/test/java/org/http4k/server/UsageFromJava_netty.java
+++ http4k-server-netty/src/test/java/org/http4k/server/UsageFromJava_netty.java
@@ -1,11 +1,16 @@
 package org.http4k.server;
 
+import kotlin.jvm.functions.Function1;
+import org.http4k.core.Body;
+import org.http4k.core.Request;
 import org.http4k.core.Response;
-import org.http4k.core.Status;
 
+import static org.http4k.core.Status.ACCEPTED;
 import static org.http4k.server.Http4kServerKt.asServer;
 
 public interface UsageFromJava_netty {
     Netty netty = new Netty(8000);
-    Http4kServer http4kServer = asServer(req -> Response.Companion.create(Status.ACCEPTED), netty);
+    Response response = Response.Companion.create(ACCEPTED).body(Body.create("hello"));
+    Function1<Request, Response> httpHandler = req -> response;
+    Http4kServer http4kServer = asServer(httpHandler, netty);
 }
