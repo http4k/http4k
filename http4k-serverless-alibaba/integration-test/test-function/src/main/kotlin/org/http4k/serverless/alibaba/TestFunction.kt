1b398bfda746b2755645a636068346865a010b38
David Denton <denton.david@gmail.com>
2020-10-25 17:49:30 +0000

    moving code around to make jacoco happy

--- http4k-serverless-alibaba/integration-test/test-function/src/main/kotlin/org/http4k/serverless/alibaba/TestFunction.kt
+++ http4k-serverless-alibaba/integration-test/test-function/src/main/kotlin/org/http4k/serverless/alibaba/TestFunction.kt
@@ -1,4 +1,6 @@
-package org.http4k.serverless.gcf
+@file:Suppress("unused")
+
+package org.http4k.serverless.alibaba
 
 import org.http4k.client.ServerForClientContract
 import org.http4k.serverless.AlibabaCloudFunction
