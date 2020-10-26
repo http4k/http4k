5babc4f0149eef1e9f7f401da2a7744d2f80b005
David Denton <denton.david@gmail.com>
2020-10-26 19:13:02 +0000

    add new modules

--- release-maven-central-2.sh
+++ release-maven-central-2.sh
@@ -10,9 +10,12 @@ maven_publish "http4k-client-jetty"
 maven_publish "http4k-client-okhttp"
 maven_publish "http4k-client-websocket"
 
+maven_publish "http4k-serverless-alibaba"
 maven_publish "http4k-serverless-gcf"
 maven_publish "http4k-serverless-lambda"
 maven_publish "http4k-serverless-openwhisk"
+maven_publish "http4k-serverless-tencent"
+
 maven_publish "http4k-jsonrpc"
 maven_publish "http4k-multipart"
 maven_publish "http4k-aws"
