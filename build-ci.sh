7ab1496f2c885e37547867afc6de6d90a96bb6c5
David Denton <denton.david@gmail.com>
2020-10-27 14:15:46 +0000

    [skip travis] remove stuff from build-ci.sh

--- build-ci.sh
+++ build-ci.sh
@@ -7,15 +7,3 @@ set -o nounset
 
 ./gradlew -i check jacocoRootReport
 bash <(curl -s https://codecov.io/bash)
-
-LOCAL_VERSION=$(jq -r .http4k.version version.json)
-
-BINTRAY_VERSION=$(curl -s https://bintray.com/api/v1/packages/http4k/maven/http4k-core/versions/_latest | jq -r .name)
-
-if [[ "$LOCAL_VERSION" == "$BINTRAY_VERSION" ]]; then
-    echo "Version has not changed: $BINTRAY_VERSION"
-    exit 0
-fi
-
-echo "Attempting to release $LOCAL_VERSION (old version $BINTRAY_VERSION)"
-
