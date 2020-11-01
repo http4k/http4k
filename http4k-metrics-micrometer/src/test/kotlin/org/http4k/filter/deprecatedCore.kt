6b5075cb61e7e84741853688582c5526647de896
David Denton <denton.david@gmail.com>
2020-11-01 11:36:13 +0000

    rename

--- http4k-metrics-micrometer/src/test/kotlin/org/http4k/filter/deprecatedCore.kt
+++ http4k-metrics-micrometer/src/test/kotlin/org/http4k/filter/deprecatedCore.kt
@@ -1,7 +1,7 @@
 package org.http4k.filter
 
 @Deprecated("Moved filters onto ClientFilters and ServerFilters")
-object MetricsFilters {
+object MetricFilters {
     @Deprecated("Moved to ServerFilters extension", ReplaceWith("ServerFilters.MicrometerMetrics"))
     val Server = ServerFilters.MicrometerMetrics
 
