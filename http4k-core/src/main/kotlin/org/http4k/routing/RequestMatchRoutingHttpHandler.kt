022c22bab52bc4292e843d0dfe022eaf243818c5
Ivan Sanchez <ivan@gourame.com>
2020-11-05 15:50:11 +0000

    Replace usages of RequestMatcher with Router

--- http4k-core/src/main/kotlin/org/http4k/routing/RequestMatchRoutingHttpHandler.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/RequestMatchRoutingHttpHandler.kt
@@ -8,34 +8,39 @@ import org.http4k.core.then
 import org.http4k.routing.RouterMatch.MatchingHandler
 import org.http4k.routing.RouterMatch.Unmatched
 
-typealias RequestMatcher = (Request) -> RouterMatch
+private typealias RequestMatcher = (Request) -> RouterMatch
 
-fun Boolean.asRouterMatch() = if(this) RouterMatch.Matched else Unmatched
+private fun Boolean.asRouterMatch() = if (this) RouterMatch.Matched else Unmatched
+
+private fun RequestMatcher.asRouter() = Router { request -> this@asRouter(request) }
 
 /**
  * For routes where certain queries are required for correct operation. RequestMatch is composable.
  */
-fun queries(vararg names: String): RequestMatcher = { req -> names.all { req.query(it) != null }.asRouterMatch() }
+fun queries(vararg names: String): Router = { req: Request -> names.all { req.query(it) != null }.asRouterMatch() }.asRouter()
 
 /**
  * For routes where certain headers are required for correct operation. RequestMatch is composable.
  */
-fun headers(vararg names: String): RequestMatcher = { req -> names.all { req.header(it) != null }.asRouterMatch() }
+fun headers(vararg names: String): Router = { req: Request -> names.all { req.header(it) != null }.asRouterMatch() }.asRouter()
+
 
 /**
  * Matches the Host header to a matching Handler.
  */
 fun hostDemux(head: Pair<String, RoutingHttpHandler>, vararg tail: Pair<String, RoutingHttpHandler>): RoutingHttpHandler {
     val hostHandlerPairs = listOf(head) + tail
-    return routes(*hostHandlerPairs.map { { req: Request -> (req.header("host") == it.first).asRouterMatch() } bind it.second }.toTypedArray())
+    return routes(*hostHandlerPairs.map { { req: Request -> (req.header("host") == it.first).asRouterMatch() }.asRouter() bind it.second }.toTypedArray())
 }
 
-infix fun RequestMatcher.bind(handler: HttpHandler): RoutingHttpHandler = PredicatedHandler(this, handler)
-infix fun RequestMatcher.bind(handler: RoutingHttpHandler): RoutingHttpHandler = RequestMatchRoutingHttpHandler(this, handler)
-infix fun RequestMatcher.and(that: RequestMatcher): RequestMatcher = { val initial: RouterMatch = RouterMatch.Matched
-    listOf(this, that).fold(initial) { acc, next -> acc.and(next(it)) } }
+infix fun Router.bind(handler: HttpHandler): RoutingHttpHandler = PredicatedHandler(this, handler)
+infix fun Router.bind(handler: RoutingHttpHandler): RoutingHttpHandler = RequestMatchRoutingHttpHandler(this, handler)
+infix fun Router.and(that: Router): Router = { it: Request ->
+    val initial: RouterMatch = RouterMatch.Matched
+    listOf(this, that).fold(initial) { acc, next -> acc.and(next.match(it)) }
+}.asRouter()
 
-internal class PredicatedHandler(private val predicate: (Request) -> RouterMatch, private val handler: HttpHandler) : RoutingHttpHandler {
+internal class PredicatedHandler(private val predicate: Router, private val handler: HttpHandler) : RoutingHttpHandler {
     override fun withFilter(new: Filter) = PredicatedHandler(predicate, when (handler) {
         is RoutingHttpHandler -> handler.withFilter(new)
         else -> new.then(handler)
@@ -46,7 +51,7 @@ internal class PredicatedHandler(private val predicate: (Request) -> RouterMatch
         else -> throw UnsupportedOperationException("Cannot apply new base path without binding to an HTTP verb")
     }
 
-    override fun match(request: Request) = if (predicate(request) == RouterMatch.Matched) MatchingHandler(handler) else Unmatched
+    override fun match(request: Request) = if (predicate.match(request) == RouterMatch.Matched) MatchingHandler(handler) else Unmatched
 
     override fun invoke(request: Request): Response = when (val matchResult = match(request)) {
         is MatchingHandler -> matchResult(request)
@@ -55,12 +60,12 @@ internal class PredicatedHandler(private val predicate: (Request) -> RouterMatch
 }
 
 internal data class RequestMatchRoutingHttpHandler(
-    private val matched: (Request) -> RouterMatch,
+    private val matched: Router,
     private val httpHandler: RoutingHttpHandler,
     private val notFoundHandler: HttpHandler = routeNotFoundHandler,
     private val methodNotAllowedHandler: HttpHandler = routeMethodNotAllowedHandler) : RoutingHttpHandler {
 
-    override fun match(request: Request) = if (matched(request) != RouterMatch.Matched) Unmatched else httpHandler.match(request)
+    override fun match(request: Request) = if (matched.match(request) != RouterMatch.Matched) Unmatched else httpHandler.match(request)
 
     override fun invoke(request: Request): Response = when (val matchResult = match(request)) {
         is MatchingHandler -> matchResult(request)
