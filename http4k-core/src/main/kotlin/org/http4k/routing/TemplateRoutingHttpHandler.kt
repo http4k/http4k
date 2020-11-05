134478b9202738b219532ab0eb60b35d0be45116
Ivan Sanchez <ivan@gourame.com>
2020-11-05 17:25:31 +0000

    Extract Route from TemplateRoutingHttpHandler

--- http4k-core/src/main/kotlin/org/http4k/routing/TemplateRoutingHttpHandler.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/TemplateRoutingHttpHandler.kt
@@ -3,38 +3,49 @@ package org.http4k.routing
 import org.http4k.core.Filter
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
+import org.http4k.core.NoOp
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.UriTemplate
 import org.http4k.core.then
 
 internal data class TemplateRoutingHttpHandler(
-    private val method: Method?,
-    private val template: UriTemplate,
-    private val httpHandler: HttpHandler,
+    private val router: Router,
+    private val filter: Filter = Filter.NoOp,
     private val notFoundHandler: HttpHandler = routeNotFoundHandler,
     private val methodNotAllowedHandler: HttpHandler = routeMethodNotAllowedHandler) : RoutingHttpHandler {
+    constructor(method: Method?,
+                template: UriTemplate,
+                httpHandler: HttpHandler) : this(TemplatingRouter(method, template, httpHandler))
 
-    override fun match(request: Request): RouterMatch =
-        if (template.matches(request.uri.path)) {
-            when (method) {
-                null, request.method -> RouterMatch.MatchingHandler { RoutedResponse(httpHandler(RoutedRequest(it, template)), template) }
-                else -> RouterMatch.MethodNotMatched
-            }
-        } else RouterMatch.Unmatched
+    override fun match(request: Request): RouterMatch = when (val matchResult = router.match(request)) {
+        is RouterMatch.MatchingHandler -> RouterMatch.MatchingHandler(filter.then(matchResult))
+        else -> matchResult
+    }
 
     override fun invoke(request: Request): Response = when (val matchResult = match(request)) {
         is RouterMatch.MatchingHandler -> matchResult(request)
-        is RouterMatch.MethodNotMatched -> methodNotAllowedHandler(request)
-        is RouterMatch.Unmatched -> notFoundHandler(request)
-        is RouterMatch.MatchedWithoutHandler -> notFoundHandler(request)
+        is RouterMatch.MethodNotMatched -> filter.then(methodNotAllowedHandler)(request)
+        is RouterMatch.Unmatched -> filter.then(notFoundHandler)(request)
+        is RouterMatch.MatchedWithoutHandler -> filter.then(notFoundHandler)(request)
     }
 
-    override fun withFilter(new: Filter): RoutingHttpHandler =
-        copy(httpHandler = new.then(httpHandler),
-            notFoundHandler = new.then(notFoundHandler),
-            methodNotAllowedHandler = new.then(methodNotAllowedHandler)
-        )
+    override fun withFilter(new: Filter): RoutingHttpHandler = copy(filter = new.then(filter))
 
-    override fun withBasePath(new: String): RoutingHttpHandler = copy(template = UriTemplate.from("$new/$template"))
+    override fun withBasePath(new: String): RoutingHttpHandler = copy(router = router.withBasePath(new))
+}
+
+data class TemplatingRouter(private val method: Method?,
+                            private val template: UriTemplate,
+                            private val httpHandler: HttpHandler) : Router {
+    override fun match(request: Request): RouterMatch = if (template.matches(request.uri.path)) {
+        when (method) {
+            null, request.method -> RouterMatch.MatchingHandler { RoutedResponse(httpHandler(RoutedRequest(it, template)), template) }
+            else -> RouterMatch.MethodNotMatched
+        }
+    } else RouterMatch.Unmatched
+
+    override fun withBasePath(new: String): Router = copy(
+        template = UriTemplate.from("$new/${template}")
+    )
 }
