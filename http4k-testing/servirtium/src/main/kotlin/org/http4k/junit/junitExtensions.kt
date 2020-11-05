313d214531427e27c7db7e13012a16a5103caf03
David Denton <denton.david@gmail.com>
2020-11-05 13:00:18 +0000

    fix httphandler name in servirtium

--- http4k-testing/servirtium/src/main/kotlin/org/http4k/junit/junitExtensions.kt
+++ http4k-testing/servirtium/src/main/kotlin/org/http4k/junit/junitExtensions.kt
@@ -88,5 +88,5 @@ private fun ParameterContext.isInteractionControl() =
     parameter.parameterizedType.typeName == InteractionControl::class.java.name
 
 private fun ParameterContext.isHttpHandler() =
-    parameter.parameterizedType.typeName == "kotlin.jvm.functions.Function1<? super org.http4k.core.Request, ? extends org.http4k.core.Response>"
+    parameter.parameterizedType.typeName == HttpHandler::class.java.name
 
