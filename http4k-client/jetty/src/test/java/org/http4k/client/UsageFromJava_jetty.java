f252992be279fc99ea707f807027a6418578205a
David Denton <denton.david@gmail.com>
2020-11-06 16:37:01 +0000

    reanimate java examples

--- http4k-client/jetty/src/test/java/org/http4k/client/UsageFromJava_jetty.java
+++ http4k-client/jetty/src/test/java/org/http4k/client/UsageFromJava_jetty.java
@@ -1,5 +1,7 @@
 package org.http4k.client;
 
+import org.http4k.core.HttpHandler;
+
 public interface UsageFromJava_jetty {
-HttpHandler apache = JettyClient.create();
+    HttpHandler apache = JettyClient.create();
 }
