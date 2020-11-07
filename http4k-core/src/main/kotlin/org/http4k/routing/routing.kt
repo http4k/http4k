1f504c03e36e63f44d70d07f41b1f9b98bfcd1bf
David Denton <denton.david@gmail.com>
2020-11-07 15:13:22 +0000

    extract OrRouter

--- http4k-core/src/main/kotlin/org/http4k/routing/routing.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/routing.kt
@@ -9,43 +9,9 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Uri
 import org.http4k.core.UriTemplate
-import org.http4k.routing.RouterMatch.MatchedWithoutHandler
-import org.http4k.routing.RouterMatch.MatchingHandler
 import org.http4k.routing.RouterMatch.MethodNotMatched
-import org.http4k.routing.RouterMatch.Unmatched
 import org.http4k.websocket.WsConsumer
 import java.io.InputStream
-
-/**
- * Provides matching of a Request to an HttpHandler which can service it.
- */
-fun interface Router {
-    /**
-     * Attempt to supply an HttpHandler which can service the passed request.
-     */
-    fun match(request: Request): RouterMatch
-
-    fun withBasePath(new: String): Router = Prefix(new).and(this)
-
-    fun withFilter(new: Filter): Router = this
-}
-
-sealed class RouterMatch(private val priority: Int) : Comparable<RouterMatch> {
-    data class MatchingHandler(private val httpHandler: HttpHandler) : RouterMatch(0), HttpHandler {
-        override fun invoke(request: Request): Response = httpHandler(request)
-    }
-
-    object MatchedWithoutHandler : RouterMatch(1)
-    object MethodNotMatched : RouterMatch(2)
-    object Unmatched : RouterMatch(3)
-
-    override fun compareTo(other: RouterMatch): Int = priority.compareTo(other.priority)
-}
-
-fun RouterMatch.and(other: RouterMatch): RouterMatch = when (this) {
-    is MatchedWithoutHandler -> other
-    is MatchingHandler, MethodNotMatched, Unmatched -> this
-}
 
 /**
  * Composite HttpHandler which can potentially service many different URL patterns. Should
@@ -71,7 +37,7 @@ interface RoutingHttpHandler : Router, HttpHandler {
 
 fun routes(vararg list: Pair<Method, HttpHandler>): RoutingHttpHandler = routes(*list.map { "" bind it.first to it.second }.toTypedArray())
 
-fun routes(vararg list: RoutingHttpHandler): RoutingHttpHandler = AggregateRoutingHttpHandler(*list)
+fun routes(vararg list: RoutingHttpHandler): RoutingHttpHandler = AggregateRoutingHttpHandler(OrRouter(list.toList()))
 
 fun Request.path(name: String): String? = when (this) {
     is RoutedRequest -> xUriTemplate.extract(uri.path)[name]
