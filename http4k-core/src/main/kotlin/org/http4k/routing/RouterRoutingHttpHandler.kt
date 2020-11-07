98f86455e93bc63eabb5451766f8cbb7876b7fdb
David Denton <denton.david@gmail.com>
2020-11-07 15:00:13 +0000

    fixed opentelemetry again

--- http4k-core/src/main/kotlin/org/http4k/routing/RouterRoutingHttpHandler.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/RouterRoutingHttpHandler.kt
@@ -3,7 +3,6 @@ package org.http4k.routing
 import org.http4k.core.Filter
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
-import org.http4k.core.NoOp
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.UriTemplate
@@ -11,23 +10,26 @@ import org.http4k.core.then
 
 internal data class RouterRoutingHttpHandler(
     private val router: Router,
-    private val filter: Filter = Filter.NoOp,
     private val notFoundHandler: HttpHandler = routeNotFoundHandler,
     private val methodNotAllowedHandler: HttpHandler = routeMethodNotAllowedHandler) : RoutingHttpHandler {
 
     override fun match(request: Request): RouterMatch = when (val matchResult = router.match(request)) {
-        is RouterMatch.MatchingHandler -> RouterMatch.MatchingHandler(filter.then(matchResult))
+        is RouterMatch.MatchingHandler -> RouterMatch.MatchingHandler(matchResult)
         else -> matchResult
     }
 
     override fun invoke(request: Request): Response = when (val matchResult = match(request)) {
-        is RouterMatch.MatchingHandler -> matchResult(request)
-        is RouterMatch.MethodNotMatched -> filter.then(methodNotAllowedHandler)(request)
-        is RouterMatch.Unmatched -> filter.then(notFoundHandler)(request)
-        is RouterMatch.MatchedWithoutHandler -> filter.then(notFoundHandler)(request)
-    }
+        is RouterMatch.MatchingHandler -> matchResult
+        is RouterMatch.MethodNotMatched -> methodNotAllowedHandler
+        is RouterMatch.Unmatched -> notFoundHandler
+        is RouterMatch.MatchedWithoutHandler -> notFoundHandler
+    }(request)
 
-    override fun withFilter(new: Filter): RoutingHttpHandler = copy(filter = new.then(filter))
+    override fun withFilter(new: Filter): RoutingHttpHandler = copy(
+        router.withFilter(new),
+        notFoundHandler = new.then(notFoundHandler),
+        methodNotAllowedHandler = new.then(methodNotAllowedHandler)
+    )
 
     override fun withBasePath(new: String): RoutingHttpHandler = copy(router = router.withBasePath(new))
 }
