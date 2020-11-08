6408d2b97ca101be7501360f39413a79ac7dbd45
zsambek <zsambeek.andras@gmail.com>
2020-11-08 16:54:34 +0000

    Implement kotlinx.serialization automarshalling

--- http4k-format/kotlinx-serialization/src/main/kotlin/org/http4k/format/KotlinxSerialization.kt
+++ http4k-format/kotlinx-serialization/src/main/kotlin/org/http4k/format/KotlinxSerialization.kt
@@ -4,4 +4,7 @@ package org.http4k.format
  * To implement custom JSON configuration, create your own object singleton extending
  * ConfigurableKotlinxSerialization, passing in the JSON configuration block
  */
-object KotlinxSerialization : ConfigurableKotlinxSerialization({})
+object KotlinxSerialization : ConfigurableKotlinxSerialization({
+    ignoreUnknownKeys = true
+    asConfigurable().withStandardMappings().done()
+})
