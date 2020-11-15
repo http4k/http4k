5b51ce8da2945f06197005058eb9bc6fc6b05a22
David Denton <denton.david@gmail.com>
2020-11-15 08:58:25 +0000

    [skip travis] extract uri

--- http4k-graphql/src/main/kotlin/org/http4k/client/graphQLExtensions.kt
+++ http4k-graphql/src/main/kotlin/org/http4k/client/graphQLExtensions.kt
@@ -3,11 +3,12 @@ package org.http4k.client
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
+import org.http4k.core.Uri
 import org.http4k.core.with
 import org.http4k.graphql.GraphQLRequest
 import org.http4k.graphql.GraphQLRequest.Companion.requestLens
 import org.http4k.graphql.GraphQLResponse.Companion.responseLens
 
-fun HttpHandler.asGraphQLHandler() = { req: GraphQLRequest ->
-    responseLens(this(Request(POST, "").with(requestLens of req)))
+fun HttpHandler.asGraphQLHandler(uri: Uri = Uri.of("")) = { req: GraphQLRequest ->
+    responseLens(this(Request(POST, uri).with(requestLens of req)))
 }
