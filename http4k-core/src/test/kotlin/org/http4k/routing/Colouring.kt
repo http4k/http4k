00100145b09353db1d513e46fc2b6e72b770ea01
Ivan Sanchez <ivan@gourame.com>
2020-11-18 16:31:30 +0000

    Test routing pretty printing

--- http4k-core/src/test/kotlin/org/http4k/routing/Colouring.kt
+++ http4k-core/src/test/kotlin/org/http4k/routing/Colouring.kt
@@ -1,12 +1,44 @@
 package org.http4k.routing
 
-fun String.coloured(foregroundColour: ForegroundColour, backgroundColour: BackgroundColour? = null) =
-    "${foregroundColour.value}${backgroundColour?.value.orEmpty()}$this$reset"
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.routing.EscapeMode.Ansi
+import org.http4k.routing.EscapeMode.None
+import org.http4k.routing.EscapeMode.Pseudo
+import org.junit.jupiter.api.Test
 
-fun String.styled(style: TextStyle) =
-    "${style.foregroundColour.value}${style.backgroundColour.value}${style.variation.value}$this$reset"
+class ColouringTest {
 
-enum class ForegroundColour(val value: String) {
+    @Test
+    fun `can print with full ansi style`() {
+        val style = TextStyle(ForegroundColour.Red, BackgroundColour.Blue, Variation.Underline)
+        assertThat("test".styled(style), equalTo(ForegroundColour.Red.ansi + BackgroundColour.Blue.ansi + Variation.Underline.ansi + "test" + reset))
+    }
+
+    @Test
+    fun `can print with style disabled`() {
+        val style = TextStyle(ForegroundColour.Red, BackgroundColour.Blue, Variation.Underline)
+        assertThat("test".styled(style, None), equalTo("test"))
+    }
+
+    @Test
+    fun `can print with limited pseudo escaping`() {
+        val style = TextStyle(ForegroundColour.Red, BackgroundColour.Blue, Variation.Underline)
+        assertThat("test".styled(style, Pseudo), equalTo("_[red]test[red]_"))
+    }
+
+    @Test
+    fun `default style is ignored`() {
+        val style = TextStyle()
+        assertThat("test".styled(style, None), equalTo("test"))
+        assertThat("test".styled(style, Pseudo), equalTo("test"))
+        assertThat("test".styled(style, Ansi), equalTo("test$reset"))
+    }
+}
+
+fun String.styled(style: TextStyle, escapeMode: EscapeMode = Ansi) = escapeMode(this, style)
+
+enum class ForegroundColour(val ansi: String) {
     Black("\u001B[30m"),
     Red("\u001B[31m"),
     Green("\u001B[32m"),
@@ -18,7 +50,7 @@ enum class ForegroundColour(val value: String) {
     Default("")
 }
 
-enum class BackgroundColour(val value: String) {
+enum class BackgroundColour(val ansi: String) {
     Red("\u001B[41m"),
     Green("\u001B[42m"),
     Yellow("\u001B[43m"),
@@ -30,9 +62,9 @@ enum class BackgroundColour(val value: String) {
     Default("")
 }
 
-fun String.strikethrough() = "${Variation.Strikethrough.value}.value.orEmpty()}$this$reset"
+fun String.strikethrough() = "${Variation.Strikethrough.ansi}.value.orEmpty()}$this$reset"
 
-enum class Variation(val value: String) {
+enum class Variation(val ansi: String) {
     Bold("\u001B[1m"),
     Italic("\u001B[3m"),
     BoldItalic("\u001B[3m"),
@@ -48,3 +80,24 @@ data class TextStyle(
     val backgroundColour: BackgroundColour = BackgroundColour.Default,
     val variation: Variation = Variation.Default,
 )
+
+enum class EscapeMode(private val converter: (String, TextStyle) -> String) {
+    Ansi({ text, style -> "${style.foregroundColour.ansi}${style.backgroundColour.ansi}${style.variation.ansi}$text$reset" }),
+    Pseudo({ text, style ->
+        style.variation.pseudo() + style.foregroundColour.pseudo() + text + style.foregroundColour.pseudo() + style.variation.pseudo()
+    }),
+    None({ text, _ -> text });
+
+    operator fun invoke(text: String, style: TextStyle) = converter(text, style)
+}
+
+private fun ForegroundColour.pseudo() = if (this == ForegroundColour.Default) "" else "[${name.toLowerCase()}]"
+
+private fun Variation.pseudo(): String = when (this) {
+    Variation.Bold -> "*"
+    Variation.Italic -> "_"
+    Variation.BoldItalic -> "**"
+    Variation.Underline -> "_"
+    Variation.Strikethrough -> "~"
+    Variation.Default -> ""
+}
---
+++ http4k-core/src/test/kotlin/org/http4k/routing/RoutePrintingTest.kt
@@ -0,0 +1,80 @@
+package org.http4k.routing
+
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.routing.EscapeMode.Ansi
+import org.http4k.routing.EscapeMode.Pseudo
+import org.http4k.routing.ForegroundColour.Cyan
+import org.http4k.routing.ForegroundColour.Green
+import org.http4k.routing.ForegroundColour.Red
+import org.http4k.routing.RouterMatch.MatchedWithoutHandler
+import org.http4k.routing.RouterMatch.MatchingHandler
+import org.http4k.routing.RouterMatch.MethodNotMatched
+import org.http4k.routing.RouterMatch.Unmatched
+import org.http4k.routing.Variation.Strikethrough
+import org.http4k.testing.ApprovalTest
+import org.http4k.testing.Approver
+import org.http4k.testing.assertApproved
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+
+@ExtendWith(ApprovalTest::class)
+class RoutePrintingTest {
+
+    private val routes = routes(
+        "/a" bind Method.GET to { Response(Status.OK).body("matched a") },
+        "/b/c" bind routes(
+            "/d" bind Method.GET to { Response(Status.OK).body("matched b/c/d") },
+            "/e" bind routes(
+                "/f" bind Method.GET to { Response(Status.OK).body("matched b/c/e/f") },
+                "/g" bind routes(
+                    Method.GET to { _: Request -> Response(Status.OK).body("matched b/c/e/g/GET") },
+                    Method.POST to { _: Request -> Response(Status.OK).body("matched b/c/e/g/POST") }
+                )
+            ),
+            "/" bind Method.GET to { Response(Status.OK).body("matched b/c") }
+        )
+    )
+
+    @Test
+    fun `describe routes`(approvalTest: Approver) {
+        approvalTest.assertApproved(routes.getDescription().prettify(escapeMode = Pseudo))
+    }
+
+    @Test
+    fun `describe matching`(approvalTest: Approver) {
+        val request = Request(Method.POST, "/b/c/e/g")
+        approvalTest.assertApproved(routes.match(request).prettify(escapeMode = Pseudo))
+    }
+}
+
+fun RouterDescription.prettify(depth: Int = 0, escapeMode: EscapeMode = Ansi): String = (" ".repeat(depth * 2)).let { indent ->
+    val lineBreak = if (description == "or") "\n" else ""
+    val indentValue = if (description == "or") indent else ""
+    if (children.isEmpty()) {
+        description.styled(TextStyle(Cyan), escapeMode)
+    } else {
+        "$lineBreak$indentValue(${children.joinToString("$lineBreak$indentValue $description ") { it.prettify(depth + 1, escapeMode) }})"
+    }
+}
+
+fun RouterMatch.prettify(depth: Int = 0, escapeMode: EscapeMode = Ansi): String = (" ".repeat(depth * 2)).let { indent ->
+    val lineBreak = if (description.description == "or") "\n" else ""
+    val indentValue = if (description.description == "or") indent else ""
+    if (subMatches.isEmpty()) {
+        description.description.styled(colour, escapeMode)
+    } else {
+        "$lineBreak$indentValue${"(".styled(colour, escapeMode)}${subMatches.joinToString("$lineBreak$indentValue ${description.description.styled(colour, escapeMode)} ") { it.prettify(depth + 1, escapeMode) }}${")".styled(colour, escapeMode)}"
+    }
+}
+
+private val RouterMatch.colour: TextStyle
+    get() = when (this) {
+        is MatchingHandler, is MatchedWithoutHandler -> TextStyle(Green)
+        is MethodNotMatched, is Unmatched -> TextStyle(Red, variation = Strikethrough)
+    }
+
+
