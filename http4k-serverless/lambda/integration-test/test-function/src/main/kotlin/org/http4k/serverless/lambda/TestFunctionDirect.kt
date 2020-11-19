a9a8f9cb30e003c031ef94a61df6b37a8892491b
Ivan Sanchez <ivan@gourame.com>
2020-11-19 09:56:51 +0000

    Use DirectLambdaFunction in TestFunctionDirect

--- http4k-serverless/lambda/integration-test/test-function/src/main/kotlin/org/http4k/serverless/lambda/TestFunctionDirect.kt
+++ http4k-serverless/lambda/integration-test/test-function/src/main/kotlin/org/http4k/serverless/lambda/TestFunctionDirect.kt
@@ -3,6 +3,6 @@
 package org.http4k.serverless.lambda
 
 import org.http4k.client.ServerForClientContract
-import org.http4k.serverless.ApplicationLoadBalancerLambdaFunction
+import org.http4k.serverless.DirectLambdaFunction
 
-class TestFunctionDirect : ApplicationLoadBalancerLambdaFunction(ServerForClientContract)
+class TestFunctionDirect : DirectLambdaFunction(ServerForClientContract)
