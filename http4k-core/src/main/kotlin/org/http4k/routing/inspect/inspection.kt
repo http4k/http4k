eb84de2491da7b52de7c189616533ae4bb5990e7
Ivan Sanchez <ivan@gourame.com>
2020-11-19 17:17:23 +0000

    Split three kinds of renderer

--- http4k-core/src/main/kotlin/org/http4k/routing/inspect/inspection.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/inspect/inspection.kt
@@ -7,15 +7,30 @@ import org.http4k.routing.RouterMatch.MatchingHandler
 import org.http4k.routing.RouterMatch.MethodNotMatched
 import org.http4k.routing.RouterMatch.Unmatched
 
-fun RouterDescription.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi): String = (" ".repeat(depth * 2)).let { indent ->
-    val lineBreak = if (description == "or") "\n" else ""
-    val indentValue = if (description == "or") indent else ""
+fun RouterDescription.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi) = when (description) {
+    "or" -> orRendering(depth, escapeMode)
+    "and" -> andRenderer(depth, escapeMode)
+    else -> normalRenderer(escapeMode)
+}
+
+private fun RouterDescription.orRendering(depth: Int, escapeMode: EscapeMode): String =
+    (" ".repeat(depth * 2)).let { indent ->
+        if (children.isEmpty()) {
+            description.styled(TextStyle(ForegroundColour.Cyan), escapeMode)
+        } else {
+            "\n$indent(${children.joinToString("\n$indent $description ") { it.prettify(depth + 1, escapeMode) }})"
+        }
+    }
+
+private fun RouterDescription.andRenderer(depth: Int, escapeMode: EscapeMode): String =
     if (children.isEmpty()) {
         description.styled(TextStyle(ForegroundColour.Cyan), escapeMode)
     } else {
-        "$lineBreak$indentValue(${children.joinToString("$lineBreak$indentValue $description ") { it.prettify(depth + 1, escapeMode) }})"
+        "(${children.joinToString(" $description ") { it.prettify(depth + 1, escapeMode) }})"
     }
-}
+
+private fun RouterDescription.normalRenderer(escapeMode: EscapeMode) =
+    description.styled(TextStyle(ForegroundColour.Cyan), escapeMode)
 
 fun RouterMatch.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi): String = (" ".repeat(depth * 2)).let { indent ->
     val lineBreak = if (description.description == "or") "\n" else ""
