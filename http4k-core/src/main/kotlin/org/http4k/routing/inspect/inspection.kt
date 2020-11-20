d5716d6b11ec1516d39451624e7f6d359d88b0a1
Ivan Sanchez <ivan@gourame.com>
2020-11-20 10:00:44 +0000

    Reuse rendering functions for both RouterDescription and RouterMatch

--- http4k-core/src/main/kotlin/org/http4k/routing/inspect/inspection.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/inspect/inspection.kt
@@ -7,52 +7,49 @@ import org.http4k.routing.RouterMatch.MatchingHandler
 import org.http4k.routing.RouterMatch.MethodNotMatched
 import org.http4k.routing.RouterMatch.Unmatched
 
-private val regularDescriptionStyle = TextStyle(ForegroundColour.Cyan)
-
 fun RouterDescription.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi) = PrettyNode(this).prettify(depth, escapeMode)
 
-fun PrettyNode.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi) = when (description) {
-    "or" -> orRendering(depth, escapeMode, regularDescriptionStyle)
-    "and" -> andRenderer(depth, escapeMode, regularDescriptionStyle)
-    else -> normalRenderer(escapeMode, regularDescriptionStyle)
+fun RouterMatch.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi): String = PrettyNode(this).prettify(depth, escapeMode)
+
+private fun PrettyNode.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi) = when (name) {
+    "or" -> orRendering(depth, escapeMode, textStyle)
+    "and" -> andRenderer(depth, escapeMode)
+    else -> name.styled(textStyle, escapeMode)
 }
 
 private fun PrettyNode.orRendering(depth: Int, escapeMode: EscapeMode, style: TextStyle): String =
     (" ".repeat(depth * 2)).let { indent ->
         if (children.isEmpty()) {
-            description.styled(style, escapeMode)
+            name.styled(style, escapeMode)
         } else {
-            "\n$indent(${children.joinToString("\n$indent $description ") { it.prettify(depth + 1, escapeMode) }})"
+            "\n$indent${"(".styled(groupStyle, escapeMode)}${children.joinToString("\n$indent ${name.styled(groupStyle, escapeMode)} ") { it.prettify(depth + 1, escapeMode) }}${")".styled(groupStyle, escapeMode)}"
         }
     }
 
-private fun PrettyNode.andRenderer(depth: Int, escapeMode: EscapeMode, style: TextStyle): String =
+private fun PrettyNode.andRenderer(depth: Int, escapeMode: EscapeMode): String =
     if (children.isEmpty()) {
-        description.styled(style, escapeMode)
+        name.styled(textStyle, escapeMode)
     } else {
-        "(${children.joinToString(" $description ") { it.prettify(depth + 1, escapeMode) }})"
+        "${"(".styled(groupStyle, escapeMode)}${children.joinToString(" ${name.styled(groupStyle, escapeMode)} ") { it.prettify(depth + 1, escapeMode) }}${")".styled(groupStyle, escapeMode)}"
     }
 
-private fun PrettyNode.normalRenderer(escapeMode: EscapeMode, style: TextStyle) =
-    description.styled(style, escapeMode)
-
-fun RouterMatch.prettify(depth: Int = 0, escapeMode: EscapeMode = EscapeMode.Ansi): String = (" ".repeat(depth * 2)).let { indent ->
-    val lineBreak = if (description.description == "or") "\n" else ""
-    val indentValue = if (description.description == "or") indent else ""
-    if (subMatches.isEmpty()) {
-        description.description.styled(colour, escapeMode)
-    } else {
-        "$lineBreak$indentValue${"(".styled(colour, escapeMode)}${subMatches.joinToString("$lineBreak$indentValue ${description.description.styled(colour, escapeMode)} ") { it.prettify(depth + 1, escapeMode) }}${")".styled(colour, escapeMode)}"
-    }
-}
-
-private val RouterMatch.colour: TextStyle
+private val RouterMatch.style: TextStyle
     get() = when (this) {
         is MatchingHandler, is MatchedWithoutHandler -> TextStyle(ForegroundColour.Green)
         is MethodNotMatched, is Unmatched -> TextStyle(ForegroundColour.Red, variation = Variation.Strikethrough)
     }
 
-data class PrettyNode(val description: String, val children: List<PrettyNode>) {
-    constructor(description: RouterDescription) : this(description.description, description.children.map { PrettyNode(it) })
-    constructor(match: RouterMatch) : this(match.description.description, match.subMatches.map { PrettyNode(it) })
+private data class PrettyNode(val name: String, val textStyle: TextStyle, val groupStyle: TextStyle, val children: List<PrettyNode>) {
+    constructor(description: RouterDescription) : this(
+        description.description,
+        TextStyle(ForegroundColour.Cyan),
+        TextStyle(),
+        description.children.map { PrettyNode(it) }
+    )
+    constructor(match: RouterMatch) : this(
+        match.description.description,
+        match.style,
+        match.style,
+        match.subMatches.map { PrettyNode(it) }
+    )
 }
