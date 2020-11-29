76961d239ccb4743e98df880a54aa81282fa7c76
David Denton <denton.david@gmail.com>
2020-11-29 15:03:52 +0000

    more tests for klaxon

--- http4k-format/klaxon/src/test/kotlin/org/http4k/format/klaxonJsonTests.kt
+++ http4k-format/klaxon/src/test/kotlin/org/http4k/format/klaxonJsonTests.kt
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test
 import com.beust.klaxon.Klaxon as KKlaxon
 
 class KlaxonAutoTest : AutoMarshallingJsonContract(Klaxon) {
-    override val expectedAutoMarshallingResult = """{"bool":false, "child":{"bool":true, "child":null, "numbers":[1], "string":"world"}, "numbers":[], "string":"hello"}"""
+    override val expectedAutoMarshallingResult = """{"bool":false,"child":{"bool":true,"child":null,"numbers":[1],"string":"world"},"numbers":[],"string":"hello"}"""
 
     override val expectedAutoMarshallingResultPrimitives = """{"duration":"PT1S", "instant":"1970-01-01T00:00:00Z", "localDate":"2000-01-01", "localDateTime":"2000-01-01T01:01:01", "localTime":"01:01:01", "offsetDateTime":"2000-01-01T01:01:01Z", "offsetTime":"01:01:01Z", "status":200, "uri":"http://uri:8000", "url":"http://url:9000", "uuid":"1a448854-1687-4f90-9562-7d527d64383c", "zonedDateTime":"2000-01-01T01:01:01Z[UTC]"}"""
 
