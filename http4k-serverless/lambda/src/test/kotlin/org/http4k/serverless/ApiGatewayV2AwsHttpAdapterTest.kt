00dc5b4d1f067a807d508bf52a5f3e755267d660
Ivan Sanchez <ivan@gourame.com>
2020-12-03 20:03:14 +0000

    Bring back cookie testing for apigw v2

--- http4k-serverless/lambda/src/test/kotlin/org/http4k/serverless/ApiGatewayV2AwsHttpAdapterTest.kt
+++ http4k-serverless/lambda/src/test/kotlin/org/http4k/serverless/ApiGatewayV2AwsHttpAdapterTest.kt
@@ -1,7 +1,9 @@
 package org.http4k.serverless
 
+import com.natpryce.hamkrest.and
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.has
 import org.http4k.base64Encode
 import org.http4k.core.Body
 import org.http4k.core.Method
@@ -9,6 +11,12 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
+import org.http4k.core.cookie.Cookie
+import org.http4k.core.cookie.cookies
+import org.http4k.hamkrest.hasBody
+import org.http4k.hamkrest.hasHeader
+import org.http4k.hamkrest.hasMethod
+import org.http4k.hamkrest.hasUri
 import org.junit.jupiter.api.Test
 import java.nio.ByteBuffer
 import java.util.*
@@ -17,21 +25,26 @@ class ApiGatewayV2AwsHttpAdapterTest {
 
     @Test
     fun `converts into http4k request`() {
+        val inputCookie = Cookie("name", "value")
+        val inputCookie2 = Cookie("name1", "value1")
+
         val request = mapOf(
             "rawPath" to "/path",
             "queryStringParameters" to mapOf("query" to "value"),
             "body" to "input body",
+            "cookies" to listOf(inputCookie.keyValueCookieString(), inputCookie2.keyValueCookieString()),
             "headers" to mapOf("c" to "d"),
             "requestContext" to mapOf("http" to mapOf("method" to "GET"))
         )
 
         assertThat(
             ApiGatewayV2AwsHttpAdapter(request, LambdaContextMock()),
-            equalTo(Request(GET, "/path")
-                .query("query", "value")
-                .header("c", "d")
-                .body("input body")
-            ))
+            hasMethod(GET)
+                .and(hasUri("/path?query=value"))
+                .and(hasHeader("c", "d"))
+                .and(has(Request::cookies, equalTo(listOf(inputCookie, inputCookie2))))
+                .and(hasBody("input body"))
+        )
     }
 
     @Test
@@ -54,11 +67,15 @@ class ApiGatewayV2AwsHttpAdapterTest {
 
     @Test
     fun `converts into http4k request when body is base 64 encoded`() {
+        val inputCookie = Cookie("name", "value")
+        val inputCookie2 = Cookie("name1", "value1")
+
         val request = mapOf(
             "rawPath" to "/path",
             "queryStringParameters" to mapOf("query" to "value"),
             "body" to "input body".base64Encode(),
             "headers" to mapOf("c" to "d"),
+            "cookies" to listOf(inputCookie.fullCookieString() + "; " + inputCookie2.fullCookieString()),
             "isBase64Encoded" to true,
             "requestContext" to mapOf("http" to mapOf("method" to "GET"))
         )
@@ -68,6 +85,7 @@ class ApiGatewayV2AwsHttpAdapterTest {
             equalTo(Request(GET, "/path")
                 .query("query", "value")
                 .header("c", "d")
+                .header("Cookie", inputCookie.fullCookieString() + "; " + inputCookie2.fullCookieString())
                 .body("input body")
             ))
     }
