973ba7ea518c8f116dee13fbf053273dd60ae978
David Denton <denton.david@gmail.com>
2020-12-30 17:38:01 +0000

    remove lambda events JAR

--- http4k-serverless/lambda/src/test/kotlin/org/http4k/serverless/BootstrapAppLoaderTest.kt
+++ http4k-serverless/lambda/src/test/kotlin/org/http4k/serverless/BootstrapAppLoaderTest.kt
@@ -1,7 +1,6 @@
 package org.http4k.serverless
 
 import com.amazonaws.services.lambda.runtime.Context
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.natpryce.hamkrest.and
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.isA
@@ -59,14 +58,12 @@ class BootstrapAppLoaderTest {
 private object TestAppWithContexts : AppLoaderWithContexts {
     override fun invoke(env: Map<String, String>, contexts: RequestContexts): HttpHandler = { request ->
         val lambdaContext: Context? = contexts[request][LAMBDA_CONTEXT_KEY]
-        val lambdaRequest: APIGatewayProxyRequestEvent? = contexts[request][LAMBDA_REQUEST_KEY]
 
         env.toList().fold(Response(CREATED)) { memo, (key, value) ->
             memo.header(key, value)
         }.body(
             request
                 .header("LAMBDA_CONTEXT_FUNCTION_NAME", lambdaContext?.functionName)
-                .header("LAMBDA_REQUEST_ACCOUNT_ID", lambdaRequest?.requestContext?.accountId)
                 .removeHeader("x-http4k-context-lambda").toString()
         )
     }
