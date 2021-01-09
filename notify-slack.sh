ac56ea0bc48674b6f8c9965b3fb1d1f0a81b601d
David Denton <denton.david@gmail.com>
2021-01-09 15:03:07 +0000

    [skip travis] slack script

--- notify-slack.sh
+++ notify-slack.sh
@@ -9,5 +9,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LOCAL_VERSION=`jq -r .http4k.version $DIR/version.json`
 
-MESSAGE= "Released version <https://bintray.com/http4k/maven/http4k-core/$LOCAL_VERSION|$LOCAL_VERSION> (See <https://github.com/http4k/http4k/blob/master/CHANGELOG.md#changelog|Changelog> for details)."
+MESSAGE="Released version <https://bintray.com/http4k/maven/http4k-core/$LOCAL_VERSION|$LOCAL_VERSION> (See <https://github.com/http4k/http4k/blob/master/CHANGELOG.md#changelog|Changelog> for details)."
 curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" $SLACK_WEBHOOK
