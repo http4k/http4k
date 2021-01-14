bc4f35b63f3c9b938fdaf419368ed64d8c075040
David Denton <denton.david@gmail.com>
2021-01-14 09:59:10 +0000

    add a couple of helper methods

--- http4k-core/src/main/kotlin/org/http4k/routing/extensions.kt
+++ http4k-core/src/main/kotlin/org/http4k/routing/extensions.kt
@@ -1,5 +1,6 @@
 package org.http4k.routing
 
+import org.http4k.core.HttpHandler
 import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.routing.RouterMatch.MatchedWithoutHandler
@@ -18,13 +19,18 @@ fun Request.path(name: String): String? = when (this) {
     else -> throw IllegalStateException("Request was not routed, so no uri-template present")
 }
 
-internal fun Method.asRouter() = object : Router {
+fun Method.asRouter() = object : Router {
     override fun match(request: Request): RouterMatch =
-        if (this@asRouter == request.method) MatchedWithoutHandler(description) else RouterMatch.MethodNotMatched(description)
+        if (this@asRouter == request.method) MatchedWithoutHandler(description) else RouterMatch.MethodNotMatched(
+            description
+        )
 
     override val description = RouterDescription("method == ${this@asRouter}")
 
     override fun withBasePath(new: String): Router = this
 }
 
-internal fun Method.and(that: Router) = asRouter().and(that)
+fun Method.and(that: Router) = asRouter().and(that)
+
+infix fun Method.bind(routingHandler: RoutingHttpHandler) = asRouter() bind routingHandler
+infix fun Method.bind(httpHandler: HttpHandler) = asRouter() bind httpHandler
