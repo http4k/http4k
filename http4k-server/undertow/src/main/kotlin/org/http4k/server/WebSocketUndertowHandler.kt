e66fd40a7fb9e042a219d5fd4a1f4aae4dcedb82
David Denton <denton.david@gmail.com>
2021-01-31 10:16:08 +0000

    adding websockets to undertow

--- http4k-server/undertow/src/main/kotlin/org/http4k/server/WebSocketUndertowHandler.kt
+++ http4k-server/undertow/src/main/kotlin/org/http4k/server/WebSocketUndertowHandler.kt
@@ -1,21 +1,17 @@
 package org.http4k.server
 
 import io.undertow.Handlers.websocket
+import io.undertow.websockets.core.AbstractReceiveListener
+import io.undertow.websockets.core.BufferedTextMessage
+import io.undertow.websockets.core.WebSocketChannel
+import io.undertow.websockets.core.WebSockets.sendText
 import io.undertow.websockets.spi.WebSocketHttpExchange
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.websocket.WsHandler
-import io.undertow.websockets.core.BufferedTextMessage
-
-import io.undertow.websockets.core.WebSocketChannel
-
-import io.undertow.websockets.core.AbstractReceiveListener
-
-import io.undertow.websockets.WebSocketConnectionCallback
-import io.undertow.websockets.core.WebSockets.sendText
 
 fun WebSocketUndertowHandler(ws: WsHandler) =
-    websocket(WebSocketConnectionCallback { exchange, channel ->
+    websocket { exchange, channel ->
         ws(exchange.asRequest())?.also {
             channel.receiveSetter.set(object : AbstractReceiveListener() {
 
@@ -25,7 +21,7 @@ fun WebSocketUndertowHandler(ws: WsHandler) =
             })
             channel.resumeReceives()
         }
-    })
+    }
 
 private fun WebSocketHttpExchange.asRequest() = Request(GET, requestURI)
         .headers(requestHeaders.toList().flatMap { h -> h.second.map { h.first to it } })
