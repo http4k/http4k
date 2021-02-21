11f81ad2d11cfeec6848aa586ba8607a675c88d6
David Denton <denton.david@gmail.com>
2021-02-21 18:04:36 +0000

    moshi supports AutoEvents out of the box

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,9 +3,12 @@
 This list is not intended to be all-encompassing - it will document major and breaking API 
 changes with their rationale when appropriate:
 
+### v4.3.6.4 (uncut)
+- **http4k-format-moshi** : Moshi now supports AutoMarshallingEvents out of the box.
+
 ### v4.3.5.4
 - **http4k-*** : Disable publishing of gradle module metadata files to Maven Central.
-
+0 (
 ### v4.3.5.3
 - **http4k-*** : Define groupId for all modules so release to Maven Central can use the value from root.
 
--- http4k-format/moshi/src/main/kotlin/org/http4k/format/Moshi.kt
+++ http4k-format/moshi/src/main/kotlin/org/http4k/format/Moshi.kt
@@ -1,12 +1,22 @@
 package org.http4k.format
 
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import org.http4k.events.Event
+import java.lang.reflect.Type
 
 /**
  * To implement custom JSON configuration, create your own object singleton. Extra mappings can be added before done() is called.
  */
-object Moshi : ConfigurableMoshi(Moshi.Builder()
-    .asConfigurable()
-    .withStandardMappings()
-    .done()
+object Moshi : ConfigurableMoshi(
+    Moshi.Builder()
+        .addLast(EventAdapter)
+        .asConfigurable()
+        .withStandardMappings()
+        .done()
 )
+
+object EventAdapter : JsonAdapter.Factory {
+    override fun create(p0: Type, p1: MutableSet<out Annotation>, p2: Moshi) =
+        if (p0.typeName == Event::class.java.typeName) p2.adapter(Any::class.java) else null
+}
