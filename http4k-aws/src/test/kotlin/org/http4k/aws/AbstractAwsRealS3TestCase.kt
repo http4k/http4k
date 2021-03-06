5db94980d3ba542f41b3d3bd67d65399af0cf3f0
Ivan Sanchez <ivan@gourame.com>
2021-03-06 10:23:42 +0000

    Remove pointless lateinits

--- http4k-aws/src/test/kotlin/org/http4k/aws/AbstractAwsRealS3TestCase.kt
+++ http4k-aws/src/test/kotlin/org/http4k/aws/AbstractAwsRealS3TestCase.kt
@@ -8,25 +8,15 @@ import org.http4k.core.then
 import org.http4k.filter.Payload
 import org.http4k.lens.LensFailure
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
 import org.opentest4j.TestAbortedException
 import java.util.*
 
 abstract class AbstractAwsRealS3TestCase {
-    protected lateinit var bucketName: String
-    protected lateinit var key: String
-    protected lateinit var bucketUrl: Uri
-    protected lateinit var keyUrl: Uri
-    protected lateinit var s3Root: Uri
-
-    @BeforeEach
-    fun setup() {
-        bucketName = UUID.randomUUID().toString()
-        key = UUID.randomUUID().toString()
-        bucketUrl = Uri.of("https://$bucketName.s3.amazonaws.com/")
-        keyUrl = Uri.of("https://$bucketName.s3.amazonaws.com/$key")
-        s3Root = Uri.of("https://s3.amazonaws.com/")
-    }
+    val bucketName = UUID.randomUUID().toString()
+    val key = UUID.randomUUID().toString()
+    val bucketUrl = Uri.of("https://$bucketName.s3.amazonaws.com/")
+    val keyUrl = Uri.of("https://$bucketName.s3.amazonaws.com/$key")
+    val s3Root = Uri.of("https://s3.amazonaws.com/")
 
     @AfterEach
     fun removeBucket() {
