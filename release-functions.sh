2a9209bf379bab90b7c2eb2f44a894f7d00c6e1c
Ivan Sanchez <ivan@gourame.com>
2021-03-15 16:34:21 +0000

    Remove unused bintray to maven central sync functions

--- release-functions.sh
+++ release-functions.sh
@@ -9,24 +9,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 LOCAL_VERSION=$(jq -r .http4k.version $DIR/version.json)
 
-function maven_publish {
-    local PACKAGE=$1
-    local PAYLOAD="{\"username\": \"${SONATYPE_USER}\", \"password\": \"${SONATYPE_KEY}\"}"
-
-    local PUBLISHED=$(curl --fail --silent -o /dev/null https://repo.maven.apache.org/maven2/org/http4k/"${PACKAGE}"/"${LOCAL_VERSION}"/"${PACKAGE}"-"${LOCAL_VERSION}".pom ; echo $?)
-
-    if [[ $PUBLISHED == "0" ]]; then
-        echo "$PACKAGE is already published. Skipping"
-    else
-        echo "Publishing $PACKAGE $LOCAL_VERSION into Maven central..."
-        RESULT=$(curl -s -X POST -u "$BINTRAY_USER:$BINTRAY_KEY" -H "Content-Type: application/json" --data "$PAYLOAD" "https://bintray.com/api/v1/maven_central_sync/http4k/maven/$PACKAGE/versions/$LOCAL_VERSION")
-
-        if [[ ! "${RESULT}" =~ .*Successful.* ]]; then
-            echo "Failed: ${RESULT}"
-        fi
-    fi
-}
-
 function create_tag {
     git tag -a "$LOCAL_VERSION" -m "http4k version $LOCAL_VERSION"
     git push origin "$LOCAL_VERSION"
--- release-maven-central.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ./release-functions.sh
-
-for i in $(./listProjects.sh); do
-    maven_publish "$i"
-done
