abfd9f384e973b0a9eebc0c98799fe4a8c937f79
David Denton <denton.david@gmail.com>
2021-03-20 10:21:42 +0000

    test for sunhttp not consuming body

--- http4k-core/src/test/kotlin/org/http4k/server/SunHttpTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/server/SunHttpTest.kt
@@ -1,9 +1,14 @@
 package org.http4k.server
 
+import com.natpryce.hamkrest.Matcher
+import com.natpryce.hamkrest.and
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.containsSubstring
 import org.http4k.client.ApacheClient
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method
 import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.NO_CONTENT
@@ -14,21 +19,35 @@ import java.io.PrintStream
 class SunHttpTest : ServerContract(::SunHttp, ApacheClient()) {
 
     @Test
-    fun `SunHttp does not complain when returning NO_CONTENT`() {
-        val err = ByteArrayOutputStream()
+    fun `does not complain when returning NO_CONTENT`() {
+        checkOutput(
+            !containsSubstring("Exception thread")
+                .and(containsSubstring("WARNING: sendResponseHeaders"))
+        ) { Response(NO_CONTENT) }
+    }
+
+    @Test
+    fun `does not complain when body not consumed`() {
+        checkOutput(!containsSubstring("Exception"), { it.body("hello".byteInputStream()) }) { Response(NO_CONTENT) }
+    }
+
+    private fun checkOutput(
+        expected: Matcher<CharSequence>,
+        modifyRequest: (Request) -> Request = { it },
+        app: HttpHandler
+    ) {
         val orig = System.err
-        System.setErr(PrintStream(err))
+        try {
+            val err = ByteArrayOutputStream()
+            System.setErr(PrintStream(err))
 
-        val app = { _: Request -> Response(NO_CONTENT) }
-        val server = app.asServer(SunHttp(8000))
-        server.start().use {
-            ApacheClient()(Request(GET, "http://localhost:8000/hello"))
-            val output = String(err.toByteArray())
-            println(output)
-            assertThat(output, !containsSubstring("Exception in thread"))
-            assertThat(output, !containsSubstring("WARNING: sendResponseHeaders"))
+            val server = app.asServer(SunHttp(0))
+            server.start().use {
+                ApacheClient()(modifyRequest(Request(POST, "http://localhost:${it.port()}/hello")))
+                assertThat(String(err.toByteArray()).also(::println), expected)
+            }
+        } finally {
+            System.setErr(orig)
         }
-
-        System.setErr(orig)
     }
 }
