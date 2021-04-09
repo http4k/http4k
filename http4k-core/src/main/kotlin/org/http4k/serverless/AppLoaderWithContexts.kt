96e15dea687bbac82082ae002032a7d5fb0ce5a3
David Denton <denton.david@gmail.com>
2021-04-09 21:24:03 +0100

    generify interface

--- http4k-core/src/main/kotlin/org/http4k/serverless/AppLoaderWithContexts.kt
+++ http4k-core/src/main/kotlin/org/http4k/serverless/AppLoaderWithContexts.kt
@@ -2,12 +2,13 @@ package org.http4k.serverless
 
 import org.http4k.core.Filter
 import org.http4k.core.HttpHandler
-import org.http4k.core.RequestContexts
+import org.http4k.core.RequestContext
+import org.http4k.core.Store
 import org.http4k.core.then
 
 /**
  * Http4k app loader - instantiate the application from the environment config and RequestContexts
  */
-fun interface AppLoaderWithContexts : (Map<String, String>, RequestContexts) -> HttpHandler
+fun interface AppLoaderWithContexts : (Map<String, String>, Store<RequestContext>) -> HttpHandler
 
 fun Filter.then(appLoader: AppLoaderWithContexts) = AppLoaderWithContexts { p1, contexts -> then(appLoader(p1, contexts)) }
