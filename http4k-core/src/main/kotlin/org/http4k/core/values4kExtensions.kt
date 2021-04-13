e94016f94f57367974a50ba08de35ce0c451acca
David Denton <denton.david@gmail.com>
2021-04-13 07:24:51 +0100

    added value for Uri

--- http4k-core/src/main/kotlin/org/http4k/core/values4kExtensions.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/values4kExtensions.kt
@@ -1,12 +1,16 @@
 package org.http4k.core
 
+import dev.forkhandles.values.AbstractValue
 import dev.forkhandles.values.Validation
 import dev.forkhandles.values.Value
 import dev.forkhandles.values.ValueFactory
+import java.util.UUID
 
 /**
  * Custom values4k extensions.
  */
+
+typealias UriValue = AbstractValue<Uri>
 
 open class UriValueFactory<DOMAIN : Value<Uri>>(
     fn: (Uri) -> DOMAIN, validation: Validation<Uri>? = null
