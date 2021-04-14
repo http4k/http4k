52512bee01cc7468bb743ad4881e44e59e78579f
Ivan Sanchez <ivan@gourame.com>
2021-04-14 21:00:45 +0100

    Increase timeout :(

--- http4k-client/apache4/src/test/kotlin/org/http4k/client/Apache4ClientStreamingContractTest.kt
+++ http4k-client/apache4/src/test/kotlin/org/http4k/client/Apache4ClientStreamingContractTest.kt
@@ -5,8 +5,9 @@ import org.http4k.core.HttpHandler
 import org.http4k.server.ServerConfig
 import org.http4k.server.SunHttp
 import org.http4k.streaming.StreamingContract
+import org.http4k.streaming.StreamingTestConfiguration
 
-class Apache4ClientStreamingContractTest : StreamingContract() {
+class Apache4ClientStreamingContractTest : StreamingContract(StreamingTestConfiguration(multiplier = 5)) {
     override fun serverConfig(): ServerConfig = SunHttp(0)
 
     override fun createClient(): HttpHandler = Apache4Client(requestBodyMode = BodyMode.Stream, responseBodyMode = BodyMode.Stream)
