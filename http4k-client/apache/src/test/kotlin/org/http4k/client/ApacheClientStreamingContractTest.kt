cd98566d26403008855929a7dfbfa6bab6586f69
Ivan Sanchez <ivan@gourame.com>
2021-04-14 22:17:41 +0100

    Replace SunHttp with Jetty

--- http4k-client/apache/src/test/kotlin/org/http4k/client/ApacheClientStreamingContractTest.kt
+++ http4k-client/apache/src/test/kotlin/org/http4k/client/ApacheClientStreamingContractTest.kt
@@ -2,17 +2,18 @@ package org.http4k.client
 
 import org.http4k.core.BodyMode
 import org.http4k.core.HttpHandler
+import org.http4k.server.Jetty
 import org.http4k.server.ServerConfig
-import org.http4k.server.SunHttp
 import org.http4k.streaming.StreamingContract
 import org.http4k.streaming.StreamingTestConfiguration
 
 class ApacheClientStreamingContractTest : StreamingContract(StreamingTestConfiguration(multiplier = 5, debug = true)) {
-    override fun serverConfig(): ServerConfig = SunHttp(0)
+    override fun serverConfig(): ServerConfig = Jetty(0)
 
-    override fun createClient(): HttpHandler = ApacheClient(requestBodyMode = BodyMode.Stream, responseBodyMode = BodyMode.Stream)
+    override fun createClient(): HttpHandler =
+        ApacheClient(requestBodyMode = BodyMode.Stream, responseBodyMode = BodyMode.Stream)
 
-init {
-println("test started")
-}
+    init {
+        println("test started")
+    }
 }
