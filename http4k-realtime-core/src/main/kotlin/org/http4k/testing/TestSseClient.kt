3fee5c850b9f49bb91fe902f3ff986b7d4f7a07c
David Denton <denton.david@gmail.com>
2021-04-30 18:43:31 +0100

    add TestSseClient

---
+++ http4k-realtime-core/src/main/kotlin/org/http4k/testing/TestSseClient.kt
@@ -0,0 +1,43 @@
+package org.http4k.testing
+
+import org.http4k.core.Request
+import org.http4k.server.PolyHandler
+import org.http4k.sse.PushAdaptingSse
+import org.http4k.sse.SseClient
+import org.http4k.sse.SseConsumer
+import org.http4k.sse.SseHandler
+import org.http4k.sse.SseMessage
+import java.util.ArrayDeque
+
+/**
+ * A class that is used for *offline* testing of a routed Sse, without starting up a Server.
+ */
+class TestSseClient internal constructor(consumer: SseConsumer, request: Request) : SseClient {
+
+    private val queue = ArrayDeque<() -> SseMessage?>()
+
+    override fun received() = generateSequence {
+        queue.remove()()
+    }
+
+    private val socket = object : PushAdaptingSse(request) {
+        init {
+            consumer(this)
+            onClose { queue.add { null } }
+        }
+
+        override fun send(message: SseMessage) {
+            queue.add { message }
+        }
+
+        override fun close() {
+            queue.add { null }
+        }
+    }
+
+    override fun close() = socket.triggerClose()
+}
+
+fun SseHandler.testSseClient(request: Request): TestSseClient = TestSseClient(invoke(request), request)
+fun PolyHandler.testSseClient(request: Request): TestSseClient =
+    sse?.testSseClient(request) ?: error("No SSE handler set.")
