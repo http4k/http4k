08f802c09e169545a1fb0ae4455b2f45fffd6025
David Denton <denton.david@gmail.com>
2021-05-11 18:23:43 +0100

    filling in the tests

--- http4k-serverless/lambda/src/main/kotlin/org/http4k/format/S3EventAdapter.kt
+++ http4k-serverless/lambda/src/main/kotlin/org/http4k/format/S3EventAdapter.kt
@@ -3,8 +3,10 @@ package org.http4k.format
 import com.amazonaws.services.lambda.runtime.events.S3Event
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.RequestParametersEntity
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.ResponseElementsEntity
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3BucketEntity
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3Entity
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3ObjectEntity
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.UserIdentityEntity
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.JsonAdapter
@@ -22,50 +24,32 @@ object S3EventAdapter : JsonAdapter<S3Event>() {
                 when (nextName()) {
                     "records" -> {
                         beginArray()
-                        val recordMap = mutableMapOf<String, Any>()
+                        val map = mutableMapOf<String, Any>()
                         while (hasNext()) {
                             beginObject()
                             val name = nextName()
-                            recordMap[name] = when (name) {
+                            map[name] = when (name) {
                                 "awsRegion" -> nextString()
                                 "eventName" -> nextString()
                                 "eventSource" -> nextString()
                                 "eventTime" -> nextString()
                                 "eventVersion" -> nextString()
-                                "requestParameters" -> {
-                                    beginObject()
-                                    nextName()
-                                    RequestParametersEntity(nextString()).also { endObject() }
-                                }
-                                "responseElements" -> {
-                                    val recordMap = mutableMapOf<String, Any>()
-                                    beginObject()
-                                    while (hasNext()) {
-                                        recordMap[name] = when (name) {
-                                            "xAmzId2" -> nextString()
-                                            "xAmzRequestId" -> nextString()
-                                            else -> error("unknown key")
-                                        }
-                                    }
-                                    ResponseElementsEntity(
-                                        recordMap["xAmzId2"] as? String,
-                                        recordMap["xAmzRequestId"] as? String
-                                    ).also { endObject() }
-                                }
-                                "s3" -> nextString()
-                                "userIdentity" -> nextString()
+                                "requestParameters" -> requestParameters()
+                                "responseElements" -> responseElements()
+                                "s3" -> s3()
+                                "userIdentity" -> userIdentity()
                                 else -> error("unknown key")
                             }
                             records += S3EventNotificationRecord(
-                                recordMap["awsRegion"] as? String,
-                                recordMap["eventName"] as? String,
-                                recordMap["eventSource"] as? String,
-                                recordMap["eventTime"] as? String,
-                                recordMap["eventVersion"] as? String,
-                                recordMap["requestParameters"] as? RequestParametersEntity,
-                                recordMap["responseElements"] as? ResponseElementsEntity,
-                                recordMap["s3"] as? S3Entity,
-                                recordMap["userIdentity"] as? UserIdentityEntity,
+                                map["awsRegion"] as? String,
+                                map["eventName"] as? String,
+                                map["eventSource"] as? String,
+                                map["eventTime"] as? String,
+                                map["eventVersion"] as? String,
+                                map["requestParameters"] as? RequestParametersEntity,
+                                map["responseElements"] as? ResponseElementsEntity,
+                                map["s3"] as? S3Entity,
+                                map["userIdentity"] as? UserIdentityEntity,
                             )
                             endObject()
                         }
@@ -77,6 +61,93 @@ object S3EventAdapter : JsonAdapter<S3Event>() {
             endObject()
             S3Event(records)
         }
+
+    private fun JsonReader.userIdentity() = obj({ UserIdentityEntity(it["principalId"] as? String) }) {
+        when (it) {
+            "principalId" -> nextString()
+            else -> error("unknown key")
+        }
+    }
+
+    private fun JsonReader.s3() = obj(
+        {
+            S3Entity(
+                it["configurationId"] as? String,
+                it["bucket"] as? S3BucketEntity,
+                it["object"] as? S3ObjectEntity,
+                it["s3SchemaVersion"] as? String
+            )
+        }
+    ) {
+        when (it) {
+            "configurationId" -> nextString()
+            "bucket" -> bucket()
+            "object" -> `object`()
+            "s3SchemaVersion" -> nextString()
+            else -> error("unknown key")
+        }
+    }
+
+    private fun JsonReader.`object`() =
+        obj({
+            S3ObjectEntity(
+                it["key"] as? String,
+                it["size"] as? Long,
+                it["eTag"] as? String,
+                it["versionId"] as? String,
+                it["sequencer"] as? String,
+            )
+        }) {
+            when (it) {
+                "key" -> nextString()
+                "size" -> nextLong()
+                "eTag" -> nextString()
+                "versionId" -> nextString()
+                "sequencer" -> nextString()
+                else -> error("unknown key")
+            }
+        }
+
+    private fun JsonReader.bucket() =
+        obj({
+            S3BucketEntity(
+                it["name"] as? String,
+                it["ownerIdentity"] as? UserIdentityEntity,
+                it["arn"] as? String,
+            )
+        }) {
+            when (it) {
+                "name" -> nextString()
+                "ownerIdentity" -> userIdentity()
+                "arn" -> nextString()
+                else -> error("unknown key")
+            }
+        }
+
+
+    private fun JsonReader.requestParameters() =
+        obj({ RequestParametersEntity(it["sourceIPAddress"] as? String) }) {
+            when (it) {
+                "sourceIPAddress" -> nextString()
+                else -> error("unknown key")
+            }
+        }
+
+    private fun JsonReader.responseElements() =
+        obj({ ResponseElementsEntity(it["xAmzId2"] as? String, it["xAmzRequestId"] as? String) }) {
+            when (it) {
+                "xAmzId2" -> nextString()
+                "xAmzRequestId" -> nextString()
+                else -> error("unknown key")
+            }
+        }
+
+    fun <T> JsonReader.obj(build: (Map<String, Any>) -> T, item: (String) -> Any): T {
+        beginObject()
+        val map = mutableMapOf<String, Any>()
+        while (hasNext()) map[nextName()] = item(nextName())
+        return build(map).also { endObject() }
+    }
 
     @ToJson
     override fun toJson(writer: JsonWriter, event: S3Event?) {
