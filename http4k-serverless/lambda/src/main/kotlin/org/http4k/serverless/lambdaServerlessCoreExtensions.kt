f48fed9276b8e689179c1d51940c49aa6d872955
David Denton <denton.david@gmail.com>
2021-05-12 19:55:24 +0100

    lint

--- http4k-serverless/lambda/src/main/kotlin/org/http4k/serverless/lambdaServerlessCoreExtensions.kt
+++ http4k-serverless/lambda/src/main/kotlin/org/http4k/serverless/lambdaServerlessCoreExtensions.kt
@@ -8,4 +8,3 @@ import org.http4k.format.AwsLambdaMoshi
 inline fun <reified In : Any, Ctx, Out : Any> FnLoader(
     noinline makeHandler: (Map<String, String>) -> FnHandler<In, Ctx, Out>
 ) = AutoMarshallingFnLoader(AwsLambdaMoshi, In::class, makeHandler)
-
