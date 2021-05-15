72b8c116f75b9872c899e22ff421bb8464e2e252
David Denton <denton.david@gmail.com>
2021-05-15 11:57:32 +0100

    allow custom automarshallers in lambda event FnLoaders

--- http4k-serverless/alibaba/src/main/kotlin/org/http4k/serverless/alibabaServerlessCoreExtensions.kt
+++ http4k-serverless/alibaba/src/main/kotlin/org/http4k/serverless/alibabaServerlessCoreExtensions.kt
@@ -1,10 +1,13 @@
 package org.http4k.serverless
 
+import com.aliyun.fc.runtime.Context
+import org.http4k.format.AutoMarshalling
 import org.http4k.format.Moshi
 
 /**
  * Convenience DSL for constructing polymorphic Alibaba FnHandlers
  */
-inline fun <reified In : Any, Ctx, Out : Any> FnLoader(
-    noinline makeHandler: (Map<String, String>) -> FnHandler<In, Ctx, Out>
-) = AutoMarshallingFnLoader(Moshi, In::class, makeHandler)
+inline fun <reified In : Any, Out : Any> FnLoader(
+    autoMarshalling: AutoMarshalling = Moshi,
+    noinline makeHandler: (Map<String, String>) -> FnHandler<In, Context, Out>
+) = AutoMarshallingFnLoader(autoMarshalling, In::class, makeHandler)
--- http4k-serverless/lambda/src/main/kotlin/org/http4k/serverless/lambdaServerlessCoreExtensions.kt
+++ http4k-serverless/lambda/src/main/kotlin/org/http4k/serverless/lambdaServerlessCoreExtensions.kt
@@ -1,10 +1,13 @@
 package org.http4k.serverless
 
+import com.amazonaws.services.lambda.runtime.Context
+import org.http4k.format.AutoMarshalling
 import org.http4k.format.AwsLambdaMoshi
 
 /**
  * Convenience DSL for constructing polymorphic AWS FnHandlers
  */
-inline fun <reified In : Any, Ctx, Out : Any> FnLoader(
-    noinline makeHandler: (Map<String, String>) -> FnHandler<In, Ctx, Out>
-) = AutoMarshallingFnLoader(AwsLambdaMoshi, In::class, makeHandler)
+inline fun <reified In : Any, Out : Any> FnLoader(
+    autoMarshalling: AutoMarshalling = AwsLambdaMoshi,
+    noinline makeHandler: (Map<String, String>) -> FnHandler<In, Context, Out>
+) = AutoMarshallingFnLoader(autoMarshalling, In::class, makeHandler)
