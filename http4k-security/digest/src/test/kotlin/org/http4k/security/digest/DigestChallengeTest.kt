653981c017ca23dfe518534189d13402de569240
aohara-cc <aohara@camcloud.com>
2021-07-18 14:18:23 -0400

    be resilient to extra space in challenge header value

---
+++ http4k-security/digest/src/test/kotlin/org/http4k/security/digest/DigestChallengeTest.kt
@@ -0,0 +1,33 @@
+package org.http4k.security.digest
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.security.Nonce
+import org.junit.jupiter.api.Test
+
+class DigestChallengeTest {
+
+    @Test
+    fun `process basic challenge for header with extra space`() {
+        val challenge = DigestChallenge.parse(" Digest realm=\"server\", nonce=\"6a4363b9256176c8c225ee70d191a620\"")
+        assertThat(challenge, equalTo(DigestChallenge(
+            realm = "server",
+            nonce = Nonce("6a4363b9256176c8c225ee70d191a620"),
+            algorithm = null,
+            opaque = null,
+            qop = emptyList()
+        )))
+    }
+
+    @Test
+    fun `process auth challenge`() {
+        val challenge = DigestChallenge.parse("Digest realm=\"http4k\", nonce=\"1234abcd\", algorithm=MD5, qop=\"auth\"")
+        assertThat(challenge, equalTo(DigestChallenge(
+            realm = "http4k",
+            nonce = Nonce("1234abcd"),
+            algorithm = "MD5",
+            opaque = null,
+            qop = listOf(Qop.Auth)
+        )))
+    }
+}
