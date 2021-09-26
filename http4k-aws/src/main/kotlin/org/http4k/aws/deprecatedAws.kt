91c742df6592e2ffeb385a8738df064488ca75d8
David Denton <denton.david@gmail.com>
2021-09-26 08:11:38 +0100

    move Hmac into core.

---
+++ http4k-aws/src/main/kotlin/org/http4k/aws/deprecatedAws.kt
@@ -0,0 +1,8 @@
+package org.http4k.aws
+
+import org.http4k.security.HmacSha256
+
+@Deprecated("renamed", ReplaceWith("org.http4k.util.HmacSha256"))
+typealias AwsHmacSha256 = HmacSha256
+
+val a = ""
