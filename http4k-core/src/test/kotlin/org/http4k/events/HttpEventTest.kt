37ff9f84dfde78ee9c17c4f6b46e4ca125abf81b
David Denton <denton.david@gmail.com>
2021-10-04 14:41:27 +0100

    adding xUriTemplate to HttpEvent

--- http4k-core/src/test/kotlin/org/http4k/events/HttpEventTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/events/HttpEventTest.kt
@@ -3,16 +3,16 @@ package org.http4k.events
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.core.HttpTransaction
-import org.http4k.core.Method
+import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status
+import org.http4k.core.Status.Companion.OK
 import org.junit.jupiter.api.Test
-import java.time.Duration
+import java.time.Duration.ZERO
 
 class HttpEventTest {
 
-    private val tx = HttpTransaction(Request(Method.GET, ""), Response(Status.OK), Duration.ZERO, mapOf())
+    private val tx = HttpTransaction(Request(GET, ""), Response(OK), ZERO, mapOf())
 
     @Test
     fun `outgoing equals`() {
