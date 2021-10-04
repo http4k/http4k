2064917a3542425edda6ad1b7b47fbb377a3d282
David Denton <denton.david@gmail.com>
2021-10-04 14:41:14 +0100

    adding xUriTemplate to HttpEvent

--- http4k-core/src/main/kotlin/org/http4k/events/HttpEvent.kt
+++ http4k-core/src/main/kotlin/org/http4k/events/HttpEvent.kt
@@ -4,14 +4,51 @@ import org.http4k.core.HttpTransaction
 import org.http4k.core.Method
 import org.http4k.core.Status
 import org.http4k.core.Uri
+import org.http4k.routing.RoutedRequest
 
 sealed class HttpEvent(
     val uri: Uri,
     val method: Method,
     val status: Status,
     val latency: Long,
+    val xUriTemplate: String,
 ) : Event {
 
+    class Incoming(
+        uri: Uri,
+        method: Method,
+        status: Status,
+        latency: Long,
+        xUriTemplate: String,
+    ) : HttpEvent(uri, method, status, latency, xUriTemplate) {
+        constructor(tx: HttpTransaction) : this(
+            tx.request.uri,
+            tx.request.method,
+            tx.response.status,
+            tx.duration.toMillis(),
+            if (tx.request is RoutedRequest) tx.request.xUriTemplate.toString() else tx.request.uri.path
+        )
+
+        override fun toString() = "Incoming(uri=$uri, method=$method, status=$status, latency=$latency, xUriTemplate=$xUriTemplate)"
+    }
+
+    class Outgoing(
+        uri: Uri,
+        method: Method,
+        status: Status,
+        latency: Long,
+        xUriTemplate: String,
+    ) : HttpEvent(uri, method, status, latency, xUriTemplate) {
+        constructor(tx: HttpTransaction) : this(
+            tx.request.uri,
+            tx.request.method,
+            tx.response.status,
+            tx.duration.toMillis(),
+            if (tx.request is RoutedRequest) tx.request.xUriTemplate.toString() else tx.request.uri.path
+        )
+
+        override fun toString() = "Outgoing(uri=$uri, method=$method, status=$status, latency=$latency, xUriTemplate=$xUriTemplate)"
+    }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -23,6 +60,7 @@ sealed class HttpEvent(
         if (method != other.method) return false
         if (status != other.status) return false
         if (latency != other.latency) return false
+        if (xUriTemplate != other.xUriTemplate) return false
 
         return true
     }
@@ -32,38 +70,7 @@ sealed class HttpEvent(
         result = 31 * result + method.hashCode()
         result = 31 * result + status.hashCode()
         result = 31 * result + latency.hashCode()
+        result = 31 * result + xUriTemplate.hashCode()
         return result
-    }
-
-    class Incoming(
-        uri: Uri,
-        method: Method,
-        status: Status,
-        latency: Long
-    ) : HttpEvent(uri, method, status, latency) {
-        constructor(tx: HttpTransaction) : this(
-            tx.request.uri,
-            tx.request.method,
-            tx.response.status,
-            tx.duration.toMillis()
-        )
-
-        override fun toString() = "Incoming(uri=$uri, method=$method, status=$status, latency=$latency)"
-    }
-
-    class Outgoing(
-        uri: Uri,
-        method: Method,
-        status: Status,
-        latency: Long
-    ) : HttpEvent(uri, method, status, latency) {
-        constructor(tx: HttpTransaction) : this(
-            tx.request.uri,
-            tx.request.method,
-            tx.response.status,
-            tx.duration.toMillis()
-        )
-
-        override fun toString() = "Outgoing(uri=$uri, method=$method, status=$status, latency=$latency)"
     }
 }
