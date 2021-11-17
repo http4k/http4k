8a3dc88632898482197b0173320137e7bbb83652
Ivan Sanchez <ivan@gourame.com>
2021-11-17 17:00:29 -0300

    Remove graceful shutdown support for Ktor as it doesn't seem robust enough to pass the build

--- http4k-server/ktorcio/src/main/kotlin/org/http4k/server/KtorCIO.kt
+++ http4k-server/ktorcio/src/main/kotlin/org/http4k/server/KtorCIO.kt
@@ -1,23 +1,13 @@
 package org.http4k.server
 
 import io.ktor.application.ApplicationCallPipeline.ApplicationPhase.Call
-import io.ktor.features.origin
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpStatusCode
-import io.ktor.request.ApplicationRequest
-import io.ktor.request.header
-import io.ktor.request.httpMethod
-import io.ktor.request.uri
-import io.ktor.response.ApplicationResponse
-import io.ktor.response.header
-import io.ktor.response.respondOutputStream
-import io.ktor.server.cio.CIO
-import io.ktor.server.cio.CIOApplicationEngine
-import io.ktor.server.engine.EngineAPI
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.engine.stop
-import io.ktor.utils.io.jvm.javaio.toInputStream
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.server.cio.*
+import io.ktor.server.engine.*
+import io.ktor.utils.io.jvm.javaio.*
 import org.http4k.core.Headers
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method
@@ -25,14 +15,11 @@ import org.http4k.core.Request
 import org.http4k.core.RequestSource
 import org.http4k.core.Response
 import org.http4k.lens.Header
-import org.http4k.server.ServerConfig.StopMode
-import java.time.Duration
-import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeUnit.SECONDS
 import io.ktor.http.Headers as KHeaders
 
 @Suppress("EXPERIMENTAL_API_USAGE")
-class KtorCIO(val port: Int = 8000, override val stopMode: StopMode) : ServerConfig {
-    constructor(port: Int = 8000) : this(8000, StopMode.Delayed(Duration.ofSeconds(2)))
+class KtorCIO(val port: Int = 8000) : ServerConfig {
 
     @OptIn(EngineAPI::class)
     override fun toServer(http: HttpHandler): Http4kServer = object : Http4kServer {
@@ -48,12 +35,7 @@ class KtorCIO(val port: Int = 8000, override val stopMode: StopMode) : ServerCon
         }
 
         override fun stop() = apply {
-            val (gracePeriodMillis, timeoutMillis) = when(stopMode) {
-                is StopMode.Graceful -> 1000L to stopMode.timeout.toMillis()
-                is StopMode.Immediate -> 0L to 0L
-                is StopMode.Delayed -> stopMode.timeout.toMillis() to stopMode.timeout.toMillis()
-            }
-            engine.stop(gracePeriodMillis, timeoutMillis, MILLISECONDS)
+            engine.stop(0, 2, SECONDS)
         }
 
         override fun port() = engine.environment.connectors[0].port
--- http4k-server/ktornetty/src/main/kotlin/org/http4k/server/KtorNetty.kt
+++ http4k-server/ktornetty/src/main/kotlin/org/http4k/server/KtorNetty.kt
@@ -1,23 +1,13 @@
 package org.http4k.server
 
 import io.ktor.application.ApplicationCallPipeline.ApplicationPhase.Call
-import io.ktor.features.origin
-import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
-import io.ktor.http.HttpStatusCode
-import io.ktor.request.ApplicationRequest
-import io.ktor.request.header
-import io.ktor.request.httpMethod
-import io.ktor.request.uri
-import io.ktor.response.ApplicationResponse
-import io.ktor.response.header
-import io.ktor.response.respondOutputStream
-import io.ktor.server.engine.EngineAPI
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.engine.stop
-import io.ktor.server.netty.Netty
-import io.ktor.server.netty.NettyApplicationEngine
-import io.ktor.utils.io.jvm.javaio.toInputStream
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.utils.io.jvm.javaio.*
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.withContext
 import org.http4k.core.Headers
@@ -27,20 +17,11 @@ import org.http4k.core.Request
 import org.http4k.core.RequestSource
 import org.http4k.core.Response
 import org.http4k.lens.Header
-import org.http4k.server.ServerConfig.StopMode
-import java.time.Duration.ofSeconds
-import java.util.concurrent.TimeUnit.MILLISECONDS
+import java.util.concurrent.TimeUnit.SECONDS
 import io.ktor.http.Headers as KHeaders
 
 @Suppress("EXPERIMENTAL_API_USAGE")
-class KtorNetty(val port: Int = 8000, override val stopMode: StopMode) : ServerConfig {
-    constructor(port: Int = 8000) : this (port, StopMode.Graceful(ofSeconds(2)))
-
-    val timeout = when (stopMode) {
-        is StopMode.Immediate -> 0
-        is StopMode.Graceful -> stopMode.timeout.toMillis()
-        else -> throw ServerConfig.UnsupportedStopMode(stopMode)
-    }
+class KtorNetty(val port: Int = 8000) : ServerConfig {
 
     @OptIn(EngineAPI::class)
     override fun toServer(http: HttpHandler): Http4kServer = object : Http4kServer {
@@ -58,7 +39,7 @@ class KtorNetty(val port: Int = 8000, override val stopMode: StopMode) : ServerC
         }
 
         override fun stop() = apply {
-            engine.stop(minOf(1000, timeout), timeout, MILLISECONDS)
+            engine.stop(0, 2, SECONDS)
         }
 
         override fun port() = engine.environment.connectors[0].port
