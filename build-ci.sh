bb53551ebf53a4e2341f5ccb7db228f96ce234a6
Ivan Sanchez <ivan@gourame.com>
2021-11-18 13:47:00 -0300

    Add more output to build

--- build-ci.sh
+++ build-ci.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-./gradlew check jacocoRootReport --build-cache
+./gradlew check jacocoRootReport --build-cache -i
 bash <(curl -s https://codecov.io/bash)
