9eaff9942eed17ee52ae68dfd772f898925d6a40
Konrad Szymański <konszymanski443@gmail.com>
2022-03-02 12:41:58 +0100

    Fix #704 dont recreate filters on every call

--- http4k-core/src/test/kotlin/org/http4k/core/FilterTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/core/FilterTest.kt
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.Test
 
 class FilterTest {
 
-    private val echoHeaders = { req: Request -> req.headers.fold(Response(OK)) { memo, next -> memo.header(next.first, next.second) } }
+    private val echoHeaders = { req: Request ->
+        req.headers.fold(Response(OK)) { memo, next -> memo.header(next.first, next.second) }
+    }
 
     private val addRequestHeader = Filter { next -> { next(it.header("hello", "world")) } }
     private val addResponseHeader = Filter { next -> { next(it).header("goodbye", "cruel") } }
@@ -24,11 +26,30 @@ class FilterTest {
 
     @Test
     fun `applies in order of chain`() {
-        val minus10 = Filter { next -> { next(it.replaceHeader("hello", (it.header("hello")!!.toInt() - 10).toString())) } }
-        val double = Filter { next -> { next(it.replaceHeader("hello", (it.header("hello")!!.toInt() * 2).toString())) } }
+        val minus10 =
+            Filter { next -> { next(it.replaceHeader("hello", (it.header("hello")!!.toInt() - 10).toString())) } }
+        val double =
+            Filter { next -> { next(it.replaceHeader("hello", (it.header("hello")!!.toInt() * 2).toString())) } }
 
         val final = double.then(minus10).then(echoHeaders)
         val response = final(Request(GET, of("/")).header("hello", "10"))
         assertThat(response.header("hello"), equalTo("10"))
+    }
+
+    @Test
+    fun `initiates filter only once`() {
+        var count = 0
+        val countFilter = Filter { next ->
+            count++
+            {
+                next(it)
+            }
+        }
+        val http = countFilter.then(echoHeaders)
+
+        http(Request(GET, of("/")))
+        http(Request(GET, of("/")))
+
+        assertThat(count, equalTo(1))
     }
 }
