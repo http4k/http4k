5c2cee119b2de841bc4b3a1a7ae17438aa28d3ce
David Denton <denton.david@gmail.com>
2022-03-21 12:49:36 +0000

    purge now includes result of HEAD after purge

--- purgeProjects.sh
+++ purgeProjects.sh
@@ -6,9 +6,9 @@ VERSION=$1
 
 for ARTIFACT in `cat projects.txt`
 do
-   echo Purging $ARTIFACT $VERSION
-   curl -X PURGE https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom > /dev/null 2>&1
-   curl https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom > /dev/null 2>&1
+    curl -X PURGE https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom > /dev/null 2>&1
+
+    echo Purging $ARTIFACT $VERSION `curl -sq --head https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom | head -1`
 done
 
 rm projects.txt
