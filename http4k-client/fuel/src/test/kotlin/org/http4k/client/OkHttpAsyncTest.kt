bb79e007e19aed7d2223e6e8d874ddd783a87cd4
Ivan Sanchez <ivan@gourame.com>
2022-03-31 21:12:59 +0100

    Fix class name

--- http4k-client/fuel/src/test/kotlin/org/http4k/client/OkHttpAsyncTest.kt
+++
@@ -1,6 +0,0 @@
-package org.http4k.client
-
-import org.http4k.server.SunHttp
-import java.time.Duration
-
-class OkHttpAsyncTest : AsyncHttpHandlerContract(::SunHttp, Fuel(), Fuel(timeout = Duration.ofMillis(100)))
