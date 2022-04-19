b246e9cbd5242e38f25ef7e959319baafe7e7f4d
Daniel <daniel.zappold@gmx.de>
2022-04-19 22:03:07 +0200

    fix CI

--- http4k-core/src/main/kotlin/org/http4k/util/Hex.kt
+++ http4k-core/src/main/kotlin/org/http4k/util/Hex.kt
@@ -1,12 +1,7 @@
 package org.http4k.util
 
 object Hex {
-    fun hex(data: ByteArray): String =
-        data.joinToString("") {
-            it.toUInt()
-                .toString(16)
-                .padStart(2, '0')
-        }
+    fun hex(data: ByteArray): String = data.joinToString("") { (0xFF and it.toInt()).toString(16).padStart(2, '0') }
 
     fun unhex(data: String): ByteArray {
         check(data.length % 2 == 0) { "Must have an even length" }
