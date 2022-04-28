9811129c62691e18e1821b4a627e6c3efc21bb72
Ivan Sanchez <ivan@gourame.com>
2022-04-28 19:21:44 +0100

    Extract ServerBackend

---
+++ http4k-server/shutdown-integration-test/src/main/kotlin/org/http4k/testing/ServerBackend.kt
@@ -0,0 +1,42 @@
+package org.http4k.testing
+
+import org.http4k.server.Apache4Server
+import org.http4k.server.ApacheServer
+import org.http4k.server.Jetty
+import org.http4k.server.KtorCIO
+import org.http4k.server.KtorNetty
+import org.http4k.server.Netty
+import org.http4k.server.Ratpack
+import org.http4k.server.ServerConfig
+import org.http4k.server.SunHttp
+import org.http4k.server.Undertow
+
+enum class ServerBackend : (ServerConfig.StopMode) -> ServerConfig {
+    Apache {
+        override fun invoke(mode: ServerConfig.StopMode) = ApacheServer(8000, stopMode = mode)
+    },
+    Apache4 {
+        override fun invoke(mode: ServerConfig.StopMode) = Apache4Server(8000, stopMode = mode)
+    },
+    KtorCIO {
+        override fun invoke(mode: ServerConfig.StopMode) = KtorCIO(8000, stopMode = mode)
+    },
+    KtorNetty {
+        override fun invoke(mode: ServerConfig.StopMode) = KtorNetty(8000, stopMode = mode)
+    },
+    Netty {
+        override fun invoke(mode: ServerConfig.StopMode) = Netty(8000, stopMode = mode)
+    },
+    Undertow {
+        override fun invoke(mode: ServerConfig.StopMode) = Undertow(port = 8000, enableHttp2 = false, mode)
+    },
+    Ratpack {
+        override fun invoke(mode: ServerConfig.StopMode) = Ratpack(port = 8000, stopMode = mode)
+    },
+    SunHttp {
+        override fun invoke(mode: ServerConfig.StopMode) = SunHttp(port = 8000, stopMode = mode)
+    },
+    Jetty {
+        override fun invoke(mode: ServerConfig.StopMode) = Jetty(port = 8000, stopMode = mode)
+    }
+}
--- http4k-server/shutdown-integration-test/src/main/kotlin/org/http4k/testing/TestServer.kt
+++ http4k-server/shutdown-integration-test/src/main/kotlin/org/http4k/testing/TestServer.kt
@@ -3,17 +3,7 @@ package org.http4k.testing
 import org.http4k.cloudnative.env.Environment
 import org.http4k.cloudnative.env.EnvironmentKey
 import org.http4k.lens.enum
-import org.http4k.server.Apache4Server
-import org.http4k.server.ApacheServer
-import org.http4k.server.Jetty
-import org.http4k.server.KtorCIO
-import org.http4k.server.KtorNetty
-import org.http4k.server.Netty
-import org.http4k.server.Ratpack
-import org.http4k.server.ServerConfig
 import org.http4k.server.ServerConfig.StopMode
-import org.http4k.server.SunHttp
-import org.http4k.server.Undertow
 import org.http4k.server.asServer
 import org.http4k.testing.TestServerEvent.ServerStarted
 import org.http4k.testing.TestServerEvent.ServerStopRequested
@@ -42,38 +32,9 @@ fun main() {
     })
 }
 
-fun resolveStopMode(simpleClassName: String) = when (simpleClassName) {
+private fun resolveStopMode(simpleClassName: String) = when (simpleClassName) {
     StopMode.Immediate::class.java.simpleName -> StopMode.Immediate
     StopMode.Graceful::class.java.simpleName -> StopMode.Graceful(Duration.ofSeconds(10))
     else -> error("Unrecognised stop mode: $simpleClassName")
 }
 
-enum class ServerBackend : (StopMode) -> ServerConfig {
-    Apache {
-        override fun invoke(mode: StopMode) = ApacheServer(8000, stopMode = mode)
-    },
-    Apache4 {
-        override fun invoke(mode: StopMode) = Apache4Server(8000, stopMode = mode)
-    },
-    KtorCIO {
-        override fun invoke(mode: StopMode) = KtorCIO(8000, stopMode = mode)
-    },
-    KtorNetty {
-        override fun invoke(mode: StopMode) = KtorNetty(8000, stopMode = mode)
-    },
-    Netty {
-        override fun invoke(mode: StopMode) = Netty(8000, stopMode = mode)
-    },
-    Undertow {
-        override fun invoke(mode: StopMode) = Undertow(port = 8000, enableHttp2 = false, mode)
-    },
-    Ratpack {
-        override fun invoke(mode: StopMode) = Ratpack(port = 8000, stopMode = mode)
-    },
-    SunHttp {
-        override fun invoke(mode: StopMode) = SunHttp(port = 8000, stopMode = mode)
-    },
-    Jetty {
-        override fun invoke(mode: StopMode) = Jetty(port = 8000, stopMode = mode)
-    }
-}
