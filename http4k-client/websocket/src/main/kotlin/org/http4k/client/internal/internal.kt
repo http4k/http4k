d96d71752c274c1cd3df18ca09e0ce69a00621d1
aohara <aohara@camcloud.com>
2022-09-09 16:29:34 -0400

    receive WsMessage with StreamBody

--- http4k-client/websocket/src/main/kotlin/org/http4k/client/internal/internal.kt
+++ http4k-client/websocket/src/main/kotlin/org/http4k/client/internal/internal.kt
@@ -57,9 +57,7 @@ class NonBlockingClient(uri: Uri, headers: Headers, timeout: Duration, private v
 
     override fun onMessage(message: String) = socket.get().triggerMessage(WsMessage(message))
 
-    override fun onMessage(bytes: ByteBuffer) {
-        socket.get().triggerMessage(WsMessage(Body(bytes)))
-    }
+    override fun onMessage(bytes: ByteBuffer) = socket.get().triggerMessage(WsMessage(Body(bytes.array().inputStream())))
 
     override fun onError(e: Exception) = socket.get().triggerError(e)
 }
