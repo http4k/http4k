70dfa5726cb161240b553e7a233ebd188f26e39f
David Denton <denton.david@gmail.com>
2022-10-09 10:08:41 +0100

    revert upgrade to refreshVersions

--- http4k-server/ktorcio/build.gradle.kts
+++ http4k-server/ktorcio/build.gradle.kts
@@ -2,7 +2,7 @@ description = "Http4k HTTP Server built on top of Ktor CIO engine"
 
 dependencies {
     api(project(":http4k-core"))
-    api(Ktor.server.cio)
+    api("io.ktor:ktor-server-cio:_")
     testImplementation(project(path = ":http4k-core", configuration ="testArtifacts"))
 }
 
