0f7ca26535db16c9f6cea84cf62cb88572e6d4d7
David Denton <denton.david@gmail.com>
2022-10-22 15:01:43 +0100

    move kotlin metadata dependency to test

--- build.gradle.kts
+++ build.gradle.kts
@@ -233,7 +233,7 @@ dependencies {
             testImplementation(project(path = it.name, configuration = "testArtifacts"))
         }
 
-    implementation("dev.zacsweers.moshix:moshi-metadata-reflect:0.19.0")
+    testImplementation("dev.zacsweers.moshix:moshi-metadata-reflect:0.19.0")
 
     testImplementation("software.amazon.awssdk:s3:_") {
         exclude(group = "software.amazon.awssdk", module = "netty-nio-client")
