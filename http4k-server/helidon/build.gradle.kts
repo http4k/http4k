2a106a6f322a30c16e36594148035a12b6e98ec1
Ivan Sanchez <ivan@gourame.com>
2022-12-13 09:08:44 +0000

    Run helidon test via dedicated task

--- http4k-server/helidon/build.gradle.kts
+++ http4k-server/helidon/build.gradle.kts
@@ -4,4 +4,12 @@ dependencies {
     api(project(":http4k-core"))
     api("io.helidon.nima.webserver:helidon-nima-webserver:_")
     testImplementation(project(path = ":http4k-core", configuration = "testArtifacts"))
+    testRuntimeOnly("org.junit.platform:junit-platform-console-standalone:1.9.1")
+}
+
+task("runSingleTest", JavaExec::class) {
+    main = "org.junit.platform.console.ConsoleLauncher"
+    classpath = sourceSets["test"].runtimeClasspath
+    args = listOf("--select-class=org.http4k.stream.HelidonStreamingTest")
+    jvmArgs = listOf("--enable-preview")
 }
