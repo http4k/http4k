f4a60b402d8f46fac1158a64d1ec1e939ee55020
David Denton <denton.david@gmail.com>
2023-01-02 19:36:01 +0000

    fixed sorting of traces in DirectoryTree

--- http4k-incubator/src/main/kotlin/org/http4k/tracing/persistence/DirectoryTreeTraceLoader.kt
+++ http4k-incubator/src/main/kotlin/org/http4k/tracing/persistence/DirectoryTreeTraceLoader.kt
@@ -6,5 +6,5 @@ import java.io.File
 
 fun TraceLoader.Companion.DirectoryTree(start: File) = object : TraceLoader {
     override fun load() = start.walkTopDown().filter { it.isDirectory }
-        .flatMap { TracePersistence.FileSystem(it).load() }.toList()
+        .flatMap { TracePersistence.FileSystem(it).load() }.toList().sortedBy { it.name }
 }
