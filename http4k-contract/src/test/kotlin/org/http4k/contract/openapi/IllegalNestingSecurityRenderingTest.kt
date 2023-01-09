7113ebfd42ecb3de066a21f7f1758c158af4f1a1
David Denton <denton.david@gmail.com>
2023-01-09 17:06:21 +0000

    added illegal rendering test - to trap unsupported cases

--- http4k-contract/src/test/kotlin/org/http4k/contract/openapi/IllegalNestingSecurityRenderingTest.kt
+++ http4k-contract/src/test/kotlin/org/http4k/contract/openapi/IllegalNestingSecurityRenderingTest.kt
@@ -4,7 +4,10 @@ import org.http4k.contract.openapi.v3.OpenApi3SecurityRenderer
 import org.http4k.contract.security.and
 import org.http4k.contract.security.or
 
-class CompositeSecurityRenderer2Test : SecurityRendererContract {
+class IllegalNestingSecurityRenderingTest : SecurityRendererContract {
+    // this is illegal because OR securities can only exist on the top level
+    // the rendering will come out as empty because every security has to have
+    // a name (and ORs are an array instead of an object)
     override val security =
         (security("first").or(security("second")))
             .and((security("third").or(security("fourth"))))
