eb3e656c617f29781c7d41d48d7f2f90c413c34c
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-01-24 16:39:58 -0500

    HTTP Message Parser fixes (#848)
    * header value parser to ignore leading whitepsace
    
    * preserve body line breaks when parsing message

--- http4k-core/src/test/kotlin/org/http4k/core/HttpMessageAsStringTest.kt
+++ http4k-core/src/test/kotlin/org/http4k/core/HttpMessageAsStringTest.kt
@@ -37,11 +37,16 @@ class HttpMessageAsStringTest {
     @Test
     fun `parses request string`() {
         assertThat(Request.parse("""GET http://www.somewhere.com/path HTTP/1.1
-foo: one
+foo:one
 bar: two
+baz:  three
 
-body""".toPayload()), equalTo(Request(GET, Uri.of("http://www.somewhere.com/path"))
-            .header("foo", "one").header("bar", "two").body(Body("body"))
+body1
+body2""".toPayload()), equalTo(Request(GET, Uri.of("http://www.somewhere.com/path"))
+            .header("foo", "one")
+            .header("bar", "two")
+            .header("baz", "three")
+            .body(Body("body1\r\nbody2"))
         ))
     }
 
@@ -49,11 +54,16 @@ body""".toPayload()), equalTo(Request(GET, Uri.of("http://www.somewhere.com/path
     fun `parses response string`() {
         assertThat(Response.parse("""
 HTTP/1.1 200 OK
-foo: one
+foo:one
 bar: two
+baz:  three
 
-body""".toPayload()), equalTo(Response(OK)
-            .header("foo", "one").header("bar", "two").body("body")
+body1
+body2""".toPayload()), equalTo(Response(OK)
+            .header("foo", "one")
+            .header("bar", "two")
+            .header("baz", "three")
+            .body("body1\r\nbody2")
         ))
     }
 
