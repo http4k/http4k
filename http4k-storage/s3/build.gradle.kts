6284f58fc391c7cba34a89e20d20984ef31361a1
David Denton <denton.david@gmail.com>
2023-01-26 20:44:12 +0000

    remove s3 storage impl (will be in connect)

--- http4k-storage/s3/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    api(project(":http4k-format-moshi"))
-    testApi(project(path = ":http4k-storage-core", configuration = "testArtifacts"))
-
-//    implementation(project(":http4k-connect-amazon-s3"))
-//    testImplementation(project(path = ":http4k-connect-amazon-s3", configuration = "testArtifacts"))
-//    testImplementation(project(":http4k-connect-amazon-s3-fake"))
-}
