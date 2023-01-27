2094d91d0390d0f56f10280684e44390fd989e7c
David Denton <denton.david@gmail.com>
2023-01-27 11:21:48 +0000

    migrate versions

--- http4k-storage/jdbc/build.gradle.kts
+++ http4k-storage/jdbc/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
     api(project(":http4k-storage-core"))
-    api("org.jetbrains.exposed:exposed-jdbc:0.41.1")
+    api("org.jetbrains.exposed:exposed-jdbc:_")
 
     testApi(project(path = ":http4k-storage-core", configuration = "testArtifacts"))
     testApi(project(":http4k-format-moshi"))
-    testApi("com.zaxxer:HikariCP:5.0.1")
-    testApi("com.h2database:h2:2.1.214")
+    testApi("com.zaxxer:HikariCP:_")
+    testApi("com.h2database:h2:_")
 }
--- http4k-storage/redis/build.gradle.kts
+++ http4k-storage/redis/build.gradle.kts
@@ -1,9 +1,10 @@
 dependencies {
     api(project(":http4k-storage-core"))
     api(project(":http4k-format-moshi"))
-    api("io.lettuce:lettuce-core:6.2.2.RELEASE")
+    api("io.lettuce:lettuce-core:_")
 
     testApi(project(path = ":http4k-storage-core", configuration = "testArtifacts"))
-    testApi("org.testcontainers:junit-jupiter:1.17.6")
-    testApi("org.testcontainers:testcontainers:1.17.6")
+    testApi(platform("org.testcontainers:testcontainers-bom:_"))
+    testApi("org.testcontainers:junit-jupiter")
+    testApi("org.testcontainers:testcontainers")
 }
