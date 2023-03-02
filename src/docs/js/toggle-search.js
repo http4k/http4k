b0f1da0e495b3dd34b97e37a905294434398a827
Philipp Mayer <phlppmayer@gmail.com>
2023-03-02 17:10:00 +0100

    Toggle search bar via ctrl on Linux/Windows (#863)
    * align code comments to fit 95 chars
    
    * add toggle search bar support for linux/windows

--- src/docs/js/toggle-search.js
+++ src/docs/js/toggle-search.js
@@ -1,6 +1,6 @@
-// toggle search box via command + k
-$(window).keydown(function(event) {
-    if (event.metaKey && (event.keyCode === 75)) {
+// toggle search box via command/ctrl + k
+$(window).keydown(function (event) {
+    if ((event.metaKey || event.ctrlKey) && event.keyCode === 75) {
         event.preventDefault()
         $("#search").focus();
     }
