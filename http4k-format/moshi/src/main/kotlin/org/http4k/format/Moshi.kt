fae0bdb05cc4f66b6d9aaa92d1b87db355dbde53
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-03-24 15:20:37 -0400

    Add functions to customize standard moshi and jackson (#871)
    * wip
    
    * undo changes to less important mappers
    
    * add test coverage for custom jackson and moshi

--- http4k-format/moshi/src/main/kotlin/org/http4k/format/Moshi.kt
+++ http4k-format/moshi/src/main/kotlin/org/http4k/format/Moshi.kt
@@ -1,17 +1,21 @@
 package org.http4k.format
 
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+
+private fun standardConfig(kotlinFactory: JsonAdapter.Factory? = null) = Moshi.Builder()
+    .addLast(EventAdapter)
+    .addLast(ThrowableAdapter)
+    .addLast(ListAdapter)
+    .addLast(MapAdapter)
+    .asConfigurable(kotlinFactory)
+    .withStandardMappings()
 
 /**
  * To implement custom JSON configuration, create your own object singleton. Extra mappings can be added before done() is called.
  */
-object Moshi : ConfigurableMoshi(
-    Moshi.Builder()
-        .addLast(EventAdapter)
-        .addLast(ThrowableAdapter)
-        .addLast(ListAdapter)
-        .addLast(MapAdapter)
-        .asConfigurable()
-        .withStandardMappings()
-        .done()
-)
+object Moshi : ConfigurableMoshi(standardConfig().done()) {
+    fun custom(configureFn: AutoMappingConfiguration<Moshi.Builder>.() -> AutoMappingConfiguration<Moshi.Builder>) =
+        ConfigurableMoshi(standardConfig().let(configureFn).done())
+}
+
