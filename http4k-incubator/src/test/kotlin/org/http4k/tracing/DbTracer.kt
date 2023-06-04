ec0600bab0ad25008bc098c9578ce21de874e6f3
David Denton <denton.david@gmail.com>
2023-06-04 20:39:06 +0100

    simpify tracing mechanic to always add children

--- http4k-incubator/src/test/kotlin/org/http4k/tracing/DbTracer.kt
+++ http4k-incubator/src/test/kotlin/org/http4k/tracing/DbTracer.kt
@@ -4,19 +4,19 @@ import org.http4k.events.Event
 import org.http4k.tracing.ActorType.Database
 import java.time.Instant
 
-data class MyCustomEvent(val serviceIdentifier: String, val myTime: Instant) : Event
+data class DbEvent(val serviceIdentifier: String, val myTime: Instant) : Event
 
-class MyCustomTracer(private val actorFrom: ActorResolver) : Tracer {
+class DbTracer(private val actorFrom: ActorResolver) : Tracer {
     override fun invoke(node: EventNode, tracer: Tracer) = node
-        .takeIf { it.event.event is MyCustomEvent }
+        .takeIf { it.event.event is DbEvent }
         ?.let {
             BiDirectional(
                 actorFrom(it.event),
                 Actor("db", Database),
-                (it.event.event as MyCustomEvent).serviceIdentifier,
+                (it.event.event as DbEvent).serviceIdentifier,
                 emptyList()
             )
         }
-        ?.let { listOf(it) }
+        ?.let(::listOf)
         ?: emptyList()
 }
