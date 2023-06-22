9bbb7b477c375c9a248e09d2102142679e54be8b
David Denton <denton.david@gmail.com>
2023-06-22 09:50:30 +0100

    tidy gzip compression

--- http4k-core/src/main/kotlin/org/http4k/filter/Gzip.kt
+++ http4k-core/src/main/kotlin/org/http4k/filter/Gzip.kt
@@ -39,11 +39,11 @@ data class CompressionResult(
 
     fun apply(response: Response): Response =
         (contentEncoding?.let {
-            response.header("content-encoding", it).let { r ->
-                if (r.header("content-length") != null && body.length != null) {
-                    r.replaceHeader("content-length", body.length.toString())
-                } else {
-                    r
+            response.header("content-encoding", it).let {
+                when {
+                    it.header("content-length") != null && body.length != null ->
+                        it.replaceHeader("content-length", body.length.toString())
+                    else -> it
                 }
             }
         } ?: response)
