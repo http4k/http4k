7bb156c43a807923ebb8e3246e7ed00ebfefad64
David Denton <denton.david@gmail.com>
2023-08-05 09:54:07 +0100

    upgrades

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@
 This list is not intended to be all-encompassing - it will document major and breaking API
 changes with their rationale when appropriate:
 
+### v5.0.1.0 (uncut)
+- **http4k-*** : Upgrade some dependency versions.
+
 ### v5.5.0.0
 - **http4k-*** : Upgrade some dependency versions.
 - **http4k-cloudnative** - Add support for enums in EnvironmentKey
--- http4k-contract/ui/swagger/src/main/kotlin/org/http4k/contract/ui/swagger/swaggerUiWebjar.kt
+++ http4k-contract/ui/swagger/src/main/kotlin/org/http4k/contract/ui/swagger/swaggerUiWebjar.kt
@@ -25,5 +25,5 @@ fun swaggerUiWebjar(configFn: SwaggerUiConfig.() -> Unit = {}) = routes(
         .toFilter()
         .then(static(ResourceLoader.Classpath("org/http4k/contract/ui/swagger-config"))),
 
-    static(ResourceLoader.Classpath( "/META-INF/resources/webjars/swagger-ui/5.1.3"))
+    static(ResourceLoader.Classpath( "/META-INF/resources/webjars/swagger-ui/5.2.0"))
 )
