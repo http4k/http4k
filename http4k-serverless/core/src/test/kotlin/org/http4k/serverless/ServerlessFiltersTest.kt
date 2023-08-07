bfa4c50cfa2dc9939374d0100a737fea4e541ede
David Denton <denton.david@gmail.com>
2023-08-07 16:34:53 +0100

    add test for request tracing

--- http4k-serverless/core/src/test/kotlin/org/http4k/serverless/ServerlessFiltersTest.kt
+++ http4k-serverless/core/src/test/kotlin/org/http4k/serverless/ServerlessFiltersTest.kt
@@ -2,18 +2,21 @@ package org.http4k.serverless
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.present
+import org.http4k.filter.ZipkinTraces
+import org.http4k.filter.ZipkinTracesStorage
 import org.http4k.util.TickingClock
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.time.Duration
 
 class ServerlessFiltersTest {
+    val request = "999"
+    val response = 999
+
     @Test
     fun `reporting latency for function`() {
         var called = false
-
-        val request = "999"
-        val response = 999
 
         ServerlessFilters.ReportFnTransaction<String, String, Int>(TickingClock()) { (req, resp, duration) ->
             called = true
@@ -23,5 +26,32 @@ class ServerlessFiltersTest {
         }.then { `in`: String, _: String -> `in`.toInt() }(request, "")
 
         assertTrue(called)
+    }
+
+    @Test
+    fun `request tracing when set`() {
+        val storage = ZipkinTracesStorage.THREAD_LOCAL
+        val before = storage.forCurrentThread()
+        ServerlessFilters.RequestTracing<String, String, Int>(storage = storage)
+            .then { `in`: String, _: String ->
+                assertThat(storage.forCurrentThread(), equalTo(before))
+                `in`.toInt()
+            }(request, "")
+
+        assertThat(storage.forCurrentThread(), equalTo(before))
+    }
+
+    @Test
+    fun `request tracing when none`() {
+        val storage = ZipkinTracesStorage.THREAD_LOCAL
+        var set: ZipkinTraces? = null
+        ServerlessFilters.RequestTracing<String, String, Int>(storage = storage)
+            .then { `in`: String, _: String ->
+                set = storage.forCurrentThread()
+                assertThat(storage.forCurrentThread(), present())
+                `in`.toInt()
+            }(request, "")
+
+        assertThat(storage.forCurrentThread(), !equalTo(set))
     }
 }
