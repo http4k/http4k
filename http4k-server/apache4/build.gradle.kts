0aaec4f2734cf055953196a1a3b7ab916d013eea
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-08-11 05:13:28 -0400

    upgrade compromised commons-codec version (#960)

--- http4k-server/apache4/build.gradle.kts
+++ http4k-server/apache4/build.gradle.kts
@@ -3,5 +3,6 @@ description = "Http4k HTTP Server built on top of Apache httpcore"
 dependencies {
     api(project(":http4k-core"))
     api("org.apache.httpcomponents:httpcore:_")
+    api("commons-codec:commons-codec:_") // override version provided by httpcore (Cxeb68d52e-5509)
     testImplementation(project(path = ":http4k-core", configuration = "testArtifacts"))
 }
