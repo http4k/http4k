3a80e2c6708aa268516053d81d847b8ecfc8bc50
David Denton <denton.david@gmail.com>
2023-09-03 17:55:32 +0100

    convert kotest tests to use junit assertions since they inline j11 code (and we are on j8)

--- http4k-testing/kotest/src/test/kotlin/org/http4k/filter/kotestExtTest.kt
+++ http4k-testing/kotest/src/test/kotlin/org/http4k/filter/kotestExtTest.kt
@@ -1,6 +1,5 @@
 package org.http4k.filter
 
-import io.kotest.assertions.shouldFail
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
@@ -8,6 +7,7 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.kotest.haveHeader
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ExtensionsTest {
     @Test
@@ -16,14 +16,14 @@ class ExtensionsTest {
 
         app(Request(GET, "").header("bob", "foo"))
 
-        shouldFail { app(Request(GET, "")) }
+        assertThrows<AssertionError> { app(Request(GET, "")) }
     }
 
     @Test
     fun `response matching as a filter`() {
         ResponseFilters.Assert(haveHeader("bob").invert()).then { Response(OK) }(Request(GET, ""))
 
-        shouldFail { ResponseFilters.Assert(haveHeader("bob")).then { Response(OK) }(Request(GET, "")) }
+        assertThrows<AssertionError>  { ResponseFilters.Assert(haveHeader("bob")).then { Response(OK) }(Request(GET, "")) }
     }
 
     @Test
@@ -32,13 +32,13 @@ class ExtensionsTest {
 
         app(Request(GET, ""))
 
-        shouldFail { app(Request(GET, "").header("bob", "foo")) }
+        assertThrows<AssertionError>  { app(Request(GET, "").header("bob", "foo")) }
     }
 
     @Test
     fun `response not matching as a filter`() {
         ResponseFilters.AssertNot(haveHeader("bob")).then { Response(OK) }(Request(GET, ""))
 
-        shouldFail { ResponseFilters.AssertNot(haveHeader("bob").invert()).then { Response(OK) }(Request(GET, "")) }
+        assertThrows<AssertionError>  { ResponseFilters.AssertNot(haveHeader("bob").invert()).then { Response(OK) }(Request(GET, "")) }
     }
 }
