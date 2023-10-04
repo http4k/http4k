a975311983576a775691edbda25af13061dbb03c
David Morgan-Brown <david@morgan-brown.com>
2023-10-04 09:39:14 +0100

    Incubating HMTX emulation on Http4kWebDriver (#979)
    * Initial sketch of architecture to optionally support HTMX in the Http4KWebDriver
    
    * Add support for data-hx-get, prep for other commands.
    
    * Minor cleanup.
    
    * Added all `hx-verb` attributes.
    
    * Added all `hx-swap` attributes.
    
    * Clean up of HtmxJsoupWebElement
    
    * Made the hx-swap test expected responses easier to read
    
    * Can now call myHttp4kWebDriver.withHtmx() to add support for htmx. This will now also use the enhanced Http4kWebDriver handler which includes cookies and such.
    
    * Handle inheritance.
    
    * Add tests for targetting.
    
    * Clean up imports and avoid IDE html warnings
    
    * Fix targetting on ids to use # prefix
    
    * Test cleanup.
    
    * Handles paramaters when sending keys to individual input elements
    
    * Handles class target selectors too.
    
    * Simplify the jsoup selector code.
    
    * Clean up imports.
    
    * Add passing example 3 from Htmx website.
    
    * Submit form fields, including POST, PUT, etc.
    
    * Add HTMX request headers.
    
    * Make the htmx driver test more 'page object fixture' like.
    
    * Move the htmx webdriver to incubating.
    
    * Remove mis-commented disabled test.

---
+++ http4k-incubator/src/main/kotlin/org/http4k/webdriver/HtmxCommand.kt
@@ -0,0 +1,226 @@
+package org.http4k.webdriver
+
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+import org.jsoup.nodes.Node
+import org.jsoup.nodes.TextNode
+import org.jsoup.parser.Parser
+import java.net.URLEncoder
+
+data class HtmxCommand(
+    val method: Method,
+    val uri: String,
+    val target: Element,
+    val swap: HtmxSwap,
+) {
+    fun performOn(element: HtmxJsoupWebElement) {
+        // TODO: re-use headers from http4k.http4k-htmx instead of repeating?
+        val headers = listOf(
+            "hx-request" to "true",
+            element.getAttribute("id")?.let { "hx-trigger" to it },
+            element.getAttribute("name")?.let { "hx-trigger-name" to it },
+            if (target.hasAttr("id")) "hx-target" to target.attr("id") else null,
+        ).filterNotNull()
+
+        val response = element
+            .handler(
+                request(element.delegate.element)
+                    .headers(headers)
+            )
+
+        val mimeType = response.header("content-type")?.split(";")?.firstOrNull()
+
+        val responseBody =
+            when (mimeType) {
+                "text/html" -> Jsoup.parse(response.bodyString(), Parser.xmlParser()).root().children()
+                "text/plain" -> listOf(TextNode(response.bodyString()))
+                null -> throw RuntimeException("No content type on response")
+                else -> throw RuntimeException("Unsupported content type on response ${response.header("content-type")}")
+            }
+
+        swap
+            .performSwap(
+                element = target,
+                newElements = responseBody ?: emptyList()
+            )
+    }
+
+    private fun request(element: Element): Request {
+        val formBody = formBodyOfElement(element)
+        val isInput = listOf("input", "textarea", "select", "button").contains(element.tagName())
+
+        return when {
+            isInput && element.hasAttr("name") && method == Method.GET ->
+                Request(Method.GET, uri).query(element.attr("name"), element.attr("value"))
+
+            formBody != null && method == Method.GET ->
+                Request(Method.GET, "$uri?$formBody")
+
+            formBody != null ->
+                Request(method, uri).body(formBody)
+
+            else ->
+                Request(method, uri)
+        }
+    }
+
+    private fun formBodyOfElement(element: Element): String? =
+        if (element.tagName() == "form")
+            formBody(element)
+        else
+            element
+                .parents()
+                .toList()
+                .firstOrNull { it.tagName() == "form" }
+                ?.let { formBody(it) }
+
+    private fun formBody(formElement: Element): String {
+        // TODO: lots of duplication with JSoupWebElement
+        // and slightly different approaches
+        val inputs =
+            formElement
+                .getElementsByTag("input")
+                .toList()
+                .filter { it.hasAttr("name") }
+                .map { it.attr("name") to it.attr("value") }
+
+        val textAreas =
+            formElement
+                .getElementsByTag("textarea")
+                .toList()
+                .filter { it.hasAttr("name") }
+                .map { it.attr("name") to it.text() }
+
+        val selects =
+            formElement
+                .getElementsByTag("select")
+                .toList()
+                .filter { it.hasAttr("name") }
+                .mapNotNull {
+                    it.getElementsByTag("option")
+                        .toList()
+                        .find { option -> option.hasAttr("selected") }
+                        ?.let { option -> it.attr("name") to option.attr("value") }
+                }
+
+        val buttons =
+            formElement
+                .getElementsByTag("button")
+                .toList()
+                .filter { it.hasAttr("name") }
+                .map { it.attr("name") to it.attr("value") }
+
+        val all = (inputs + textAreas + selects + buttons)
+
+        return all.joinToString("&") {
+            "${URLEncoder.encode(it.first, "UTF-8")}=${URLEncoder.encode(it.second, "UTF-8")}"
+        }
+    }
+
+    companion object {
+
+        private fun withDataPrefix(p: Pair<String, Method>): List<Pair<String, Method>> =
+            listOf(p, "data-${p.first}" to p.second)
+
+        private val hxAttrs = listOf(
+            "hx-get" to Method.GET,
+            "hx-post" to Method.POST,
+            "hx-delete" to Method.DELETE,
+            "hx-patch" to Method.PATCH,
+            "hx-put" to Method.PUT,
+        ).flatMap(::withDataPrefix)
+
+        private fun Element.hxAttr(key: String): String? =
+            this.attr("hx-$key").takeIf { it.isNotEmpty() }
+                ?: this.attr("data-hx-$key").takeIf { it.isNotEmpty() }
+
+        private fun <T> Element.findInheritedValue(f: (Element) -> T?): T? =
+            f(this) ?: this.parents().firstNotNullOfOrNull { f(it) }
+
+        private fun targetElement(element: Element): Element? =
+            element
+                .hxAttr("target")
+                ?.let {
+                    when {
+                        it == "this" -> element
+                        else -> element.root().select(it).firstOrNull()
+                    }
+                }
+
+        private fun swap(element: Element): HtmxSwap? =
+            element
+                .hxAttr("swap")
+                ?.let(::swapFromString)
+
+        private fun swapFromString(s: String): HtmxSwap? =
+            HtmxSwap
+                .entries
+                .firstOrNull { s.lowercase().startsWith(it.toString().lowercase()) }
+
+        private fun fromElement(element: Element): HtmxCommand? =
+            hxAttrs
+                .firstOrNull { element.hasAttr(it.first) }
+                ?.let {
+                    HtmxCommand(
+                        method = it.second,
+                        uri = element.attr(it.first),
+                        target = element.findInheritedValue(::targetElement) ?: element,
+                        swap = element.findInheritedValue(::swap) ?: HtmxSwap.InnerHtml,
+                    )
+                }
+
+        fun from(element: HtmxJsoupWebElement): HtmxCommand? =
+            fromElement(element.delegate.element)
+    }
+}
+
+interface HtmxSwapAction {
+    fun performSwap(element: Element, newElements: List<Node>)
+}
+
+enum class HtmxSwap : HtmxSwapAction {
+    InnerHtml {
+        override fun performSwap(element: Element, newElements: List<Node>) {
+            element.empty()
+            element.appendChildren(newElements)
+        }
+    },
+    OuterHtml {
+        override fun performSwap(element: Element, newElements: List<Node>) {
+            newElements.forEach { element.before(it) }
+            element.remove()
+        }
+    },
+    BeforeBegin {
+        override fun performSwap(element: Element, newElements: List<Node>) {
+            newElements.forEach { element.before(it) }
+        }
+    },
+    AfterBegin {
+        override fun performSwap(element: Element, newElements: List<Node>) {
+            element.insertChildren(0, newElements)
+        }
+    },
+    BeforeEnd {
+        override fun performSwap(element: Element, newElements: List<Node>) {
+            element.appendChildren(newElements)
+        }
+    },
+    AfterEnd {
+        override fun performSwap(element: Element, newElements: List<Node>) {
+            newElements.reversed().forEach { element.after(it) }
+        }
+    },
+    Delete {
+        override fun performSwap(element: Element, newElements: List<Node>) {
+            element.remove()
+        }
+    },
+    None {
+        override fun performSwap(element: Element, newElements: List<Node>) {}
+    },
+}
+
+
---
+++ http4k-incubator/src/test/kotlin/org/http4k/webdriver/HtmxJsoupWebElementTest.kt
@@ -0,0 +1,475 @@
+package org.http4k.webdriver
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.core.Filter
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.body.form
+import org.http4k.core.then
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.openqa.selenium.By
+
+class HtmxJsoupWebElementTest {
+
+    private var newLocation: Pair<Method, String>? = null
+    private val navigate: (Request) -> Unit = { it -> newLocation = it.method to it.uri.toString() }
+    private val getURL: () -> String? = { null }
+    private val contentTypePlainTextFilter = Filter { next: HttpHandler ->
+        { next(it).header("Content-Type", "text/plain") }
+    }
+
+    private val jsoupOutputSettings = Document.OutputSettings().prettyPrint(false)
+
+    private fun createTestElement(
+        htmxVerb: String,
+        htmxSwap: String?,
+        handler: HttpHandler
+    ): HtmxJsoupWebElement {
+        val document =
+            Jsoup
+                .parse("""<body><div>-NONE-</div></body>""")
+                .outputSettings(jsoupOutputSettings)
+
+        val bodyElement = document.getElementsByTag("body").first()!!
+        val tagElement = document.getElementsByTag("div").first()!!
+
+        tagElement.attr(htmxVerb, "/test")
+        if (htmxSwap != null) {
+            tagElement.attr("hx-swap", htmxSwap)
+        }
+
+        return HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, bodyElement), handler)
+    }
+
+    private fun asssertClickResponds(
+        htmxVerb: String,
+        htmxSwap: String?,
+        expectedMethod: Method,
+        expectedResponse: String
+    ) {
+        val handler: HttpHandler = contentTypePlainTextFilter.then { req ->
+            if (req.method == expectedMethod && req.uri.path == "/test") {
+                Response(Status.OK).body("responded")
+            } else {
+                Response(Status.BAD_REQUEST).body("expected $expectedMethod '/test' but got ${req.method} '${req.uri.path}'")
+            }
+        }
+
+        val body = createTestElement(htmxVerb, htmxSwap, handler)
+
+        body.findElement(By.tagName("div"))!!.click()
+
+        body.delegate.element.getElementsByTag("div").first()?.clearAttributes()
+
+        assertThat(body.toString(), equalTo("<body>$expectedResponse</body>"))
+    }
+
+    @Nested
+    inner class `Handles hx-methods` {
+        @Test
+        fun `issues a GET request on click with default swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = null,
+                expectedMethod = Method.GET,
+                expectedResponse = "<div>responded</div>"
+            )
+        }
+
+        @Test
+        fun `issues a GET request on click with innerHtml swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "innerHTML",
+                expectedMethod = Method.GET,
+                expectedResponse = "<div>responded</div>"
+            )
+        }
+
+        @Test
+        fun `issues a data-GET request on click with innerHtml swap`() {
+            asssertClickResponds(
+                htmxVerb = "data-hx-get",
+                htmxSwap = "innerHTML",
+                expectedMethod = Method.GET,
+                expectedResponse = "<div>responded</div>"
+            )
+        }
+
+        @Test
+        fun `issues a POST request on click with default swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-post",
+                htmxSwap = null,
+                expectedMethod = Method.POST,
+                expectedResponse = "<div>responded</div>"
+            )
+        }
+
+        @Test
+        fun `issues a DELETE request on click with default swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-delete",
+                htmxSwap = null,
+                expectedMethod = Method.DELETE,
+                expectedResponse = "<div>responded</div>"
+            )
+        }
+
+        @Test
+        fun `issues a PATCH request on click with default swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-patch",
+                htmxSwap = null,
+                expectedMethod = Method.PATCH,
+                expectedResponse = "<div>responded</div>"
+            )
+        }
+
+        @Test
+        fun `issues a PUT request on click with default swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-put",
+                htmxSwap = null,
+                expectedMethod = Method.PUT,
+                expectedResponse = "<div>responded</div>"
+            )
+        }
+    }
+
+    @Nested
+    inner class `Handles hx-swap` {
+        @Test
+        fun `issues a GET request on click with outerHTML swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "outerHTML",
+                expectedMethod = Method.GET,
+                expectedResponse = "responded"
+            )
+        }
+
+        @Test
+        fun `issues a GET request on click with beforebegin swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "beforebegin",
+                expectedMethod = Method.GET,
+                expectedResponse = "responded<div>-NONE-</div>"
+            )
+        }
+
+        @Test
+        fun `issues a GET request on click with afterbegin swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "afterbegin",
+                expectedMethod = Method.GET,
+                expectedResponse = "<div>responded-NONE-</div>"
+            )
+        }
+
+        @Test
+        fun `issues a GET request on click with beforeend swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "beforeend",
+                expectedMethod = Method.GET,
+                expectedResponse = "<div>-NONE-responded</div>"
+            )
+        }
+
+        @Test
+        fun `issues a GET request on click with afterend swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "afterend",
+                expectedMethod = Method.GET,
+                expectedResponse = "<div>-NONE-</div>responded"
+            )
+        }
+
+        @Test
+        fun `issues a GET request on click with delete swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "delete",
+                expectedMethod = Method.GET,
+                expectedResponse = ""
+            )
+        }
+
+        @Test
+        fun `issues a GET request on click with none swap`() {
+            asssertClickResponds(
+                htmxVerb = "hx-get",
+                htmxSwap = "none",
+                expectedMethod = Method.GET,
+                expectedResponse = "<div>-NONE-</div>"
+            )
+        }
+    }
+
+    @Nested
+    inner class `Handles inheritance and targets` {
+        private val alwaysRespondHandler: HttpHandler =
+            contentTypePlainTextFilter.then { Response(Status.OK).body("responded") }
+
+        @Test
+        fun `targets a parent with hx-target 'this'`() {
+            val html = Jsoup.parse(
+                """
+                    |<body>
+                    |<div hx-target="this">
+                    |<div id="actor" hx-get="/test"/>
+                    |</div>
+                    |</body>
+                """.trimMargin()
+            ).outputSettings(jsoupOutputSettings)
+
+            val element = HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, html), alwaysRespondHandler)
+
+            val body = element.findElement(By.tagName("body"))
+
+            element.findElement(By.id("actor"))!!.click()
+
+            assertThat(
+                body.toString(),
+                equalTo(
+                    """
+                        |<body>
+                        |<div hx-target="this">responded</div>
+                        |</body>
+                    """.trimMargin()
+                )
+            )
+        }
+
+        @Test
+        fun `targets a sibling by id`() {
+            val html = Jsoup.parse(
+                """
+                    |<body>
+                    |<div id="foo" hx-get="/test" hx-target="#bar">foo</div>
+                    |<div id="bar">bar</div>
+                    |</body>
+                """.trimMargin()
+            ).outputSettings(jsoupOutputSettings)
+
+            val element = HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, html), alwaysRespondHandler)
+
+            val body = element.findElement(By.tagName("body"))
+
+            element.findElement(By.id("foo"))!!.click()
+
+            assertThat(
+                body.toString(),
+                equalTo(
+                    """
+                        |<body>
+                        |<div id="foo" hx-get="/test" hx-target="#bar">foo</div>
+                        |<div id="bar">responded</div>
+                        |</body>
+                    """.trimMargin()
+                )
+            )
+        }
+
+        @Test
+        fun `targets a sibling by class`() {
+            val html = Jsoup.parse(
+                """
+                    |<body>
+                    |<div id="foo" hx-get="/test" hx-target=".bar">foo</div>
+                    |<div class="bar">bar</div>
+                    |</body>
+                """.trimMargin()
+            ).outputSettings(jsoupOutputSettings)
+
+            val element = HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, html), alwaysRespondHandler)
+
+            val body = element.findElement(By.tagName("body"))
+
+            element.findElement(By.id("foo"))!!.click()
+
+            assertThat(
+                body.toString(),
+                equalTo(
+                    """
+                        |<body>
+                        |<div id="foo" hx-get="/test" hx-target=".bar">foo</div>
+                        |<div class="bar">responded</div>
+                        |</body>
+                    """.trimMargin()
+                )
+            )
+        }
+
+        @Test
+        fun `inherits hx-swap`() {
+            val html = Jsoup.parse(
+                """
+                    |<body>
+                    |<div hx-swap="outerHTML">
+                    |<div id="foo" hx-get="/test">foo</div>
+                    |</div>
+                    |</body>
+                """.trimMargin()
+            ).outputSettings(jsoupOutputSettings)
+
+            val element = HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, html), alwaysRespondHandler)
+
+            val body = element.findElement(By.tagName("body"))
+
+            element.findElement(By.id("foo"))!!.click()
+
+            assertThat(
+                body.toString(),
+                equalTo(
+                    """
+                        |<body>
+                        |<div hx-swap="outerHTML">
+                        |responded
+                        |</div>
+                        |</body>
+                    """.trimMargin()
+                )
+            )
+        }
+    }
+
+    @Nested
+    inner class `Handles form data` {
+        @Test
+        fun `single input field`() {
+            val getHandler: HttpHandler = contentTypePlainTextFilter.then { req ->
+                if (req.method == Method.GET) {
+                    Response(Status.OK)
+                        .body(req.query("name") ?: "NONE")
+                } else {
+                    Response(Status.BAD_REQUEST).body("expected a GET request, but was ${req.method}")
+                }
+            }
+
+            val html = Jsoup.parse(
+                """
+                    |<body>
+                    |<input name="name" hx-get="/test" hx-swap="outerHTML"/>
+                    |</body>
+                """.trimMargin()
+            ).outputSettings(jsoupOutputSettings)
+
+            val element = HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, html), getHandler)
+
+            val body = element.findElement(By.tagName("body"))
+
+            element.findElement(By.tagName("input"))!!.sendKeys("keys")
+
+            assertThat(
+                body.toString(),
+                equalTo(
+                    """
+                    |<body>
+                    |keys
+                    |</body>
+                    """.trimMargin()
+                )
+            )
+        }
+
+        @Test
+        fun `post a form`() {
+            val getHandler: HttpHandler = contentTypePlainTextFilter.then { req ->
+                if (req.method == Method.POST) {
+                    Response(Status.OK)
+                        .body(req.form().joinToString { "${it.first}: ${it.second}" })
+                } else {
+                    Response(Status.BAD_REQUEST).body("expected a POST request, but was ${req.method}")
+                }
+            }
+
+            val html = Jsoup.parse(
+                """
+                    |<body>
+                    |<form hx-post="/" hx-swap="outerHtml">
+                    |<input type="text" id="foo1" name="foo1" value="bar1"/>
+                    |<input type="text" id="foo2" name="foo2" value="bar2"/>
+                    |</form>
+                    |</body>
+                """.trimMargin()
+            ).outputSettings(jsoupOutputSettings)
+
+            val element = HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, html), getHandler)
+
+            val body = element.findElement(By.tagName("body"))
+
+            element.findElement(By.tagName("form"))!!.submit()
+
+            assertThat(
+                body.toString(),
+                equalTo(
+                    """
+                    |<body>
+                    |foo1: bar1, foo2: bar2
+                    |</body>
+                    """.trimMargin()
+                )
+            )
+        }
+    }
+
+    @Nested
+    inner class `Handles header values` {
+        @Test
+        fun `sends headers for request`() {
+            val getHandler: HttpHandler = contentTypePlainTextFilter.then { req ->
+                Response(Status.OK)
+                    .body(
+                        req
+                            .headers
+                            .sortedBy { it.first }
+                            .map { "${it.first}: ${it.second}" }
+                            .joinToString("\n")
+                    )
+            }
+
+            val html = Jsoup.parse(
+                """
+                    |<body>
+                    |<div id="tgt" hx-target="this" hx-swap="outerHTML">
+                    |<div id="a" name="adiv" hx-get="/"/>
+                    |</div>
+                    |</body>
+                """.trimMargin()
+            ).outputSettings(jsoupOutputSettings)
+
+            val element = HtmxJsoupWebElement(JSoupWebElement(navigate, getURL, html), getHandler)
+
+            val body = element.findElement(By.tagName("body"))
+
+            element.findElement(By.id("a"))!!.click()
+
+            assertThat(
+                body.toString(),
+                equalTo(
+                    """
+                    |<body>
+                    |hx-request: true
+                    |hx-target: tgt
+                    |hx-trigger: a
+                    |hx-trigger-name: adiv
+                    |</body>
+                    """.trimMargin()
+                )
+            )
+
+        }
+    }
+}
