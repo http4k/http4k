601b309d1b6c9aa5f7f01eae067a8e0547e3d448
Andrew O'Hara <ohara.andrew314@gmail.com>
2023-12-19 21:19:36 -0500

    add additional assertions

--- http4k-aws/src/test/kotlin/org/http4k/aws/AwsPreSignRequestsTest.kt
+++ http4k-aws/src/test/kotlin/org/http4k/aws/AwsPreSignRequestsTest.kt
@@ -4,7 +4,9 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.core.Method
 import org.http4k.core.Request
+import org.http4k.hamkrest.hasHost
 import org.http4k.hamkrest.hasQuery
+import org.http4k.hamkrest.hasUriPath
 import org.junit.jupiter.api.Test
 import java.time.Clock
 import java.time.Duration
@@ -15,21 +17,29 @@ class AwsPreSignRequestsTest {
 
     @Test
     fun `signed with standard credentials`() {
+        val time = Instant.parse("2013-05-24T00:00:00Z")
         val signer = AwsPreSignRequests(
-            clock = Clock.fixed(Instant.parse("2013-05-24T00:00:00Z"), ZoneOffset.UTC),
+            clock = Clock.fixed(time, ZoneOffset.UTC),
             credentials = AwsCredentials("AKIAIOSFODNN7EXAMPLE", "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"),
             scope = AwsCredentialScope("us-east-1", "s3")
         )
         val request = Request(Method.GET, "https://examplebucket.s3.amazonaws.com/test.txt")
-        val signed = request
-            .uri(signer(request, Duration.ofHours(24)).uri)
+        val signed = signer(request, Duration.ofHours(24))
+        val signedRequest = request.uri(signed.uri)
 
-        assertThat(signed, hasQuery("X-Amz-Algorithm", equalTo("AWS4-HMAC-SHA256")))
-        assertThat(signed, hasQuery("X-Amz-Credential", equalTo("AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request")))
-        assertThat(signed, hasQuery("X-Amz-Date", equalTo("20130524T000000Z")))
-        assertThat(signed, hasQuery("X-Amz-Expires", equalTo("86400")))
-        assertThat(signed, hasQuery("X-Amz-SignedHeaders", equalTo("host")))
-        assertThat(signed, hasQuery("X-Amz-Signature", equalTo("aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404")))
+        assertThat(signed.uri, hasHost("examplebucket.s3.amazonaws.com"))
+        assertThat(signed.uri, hasUriPath("/test.txt"))
+        assertThat(signedRequest, hasQuery("X-Amz-Algorithm", equalTo("AWS4-HMAC-SHA256")))
+        assertThat(signedRequest, hasQuery("X-Amz-Credential", equalTo("AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request")))
+        assertThat(signedRequest, hasQuery("X-Amz-Date", equalTo("20130524T000000Z")))
+        assertThat(signedRequest, hasQuery("X-Amz-Expires", equalTo("86400")))
+        assertThat(signedRequest, hasQuery("X-Amz-SignedHeaders", equalTo("host")))
+        assertThat(signedRequest, hasQuery("X-Amz-Signature", equalTo("aeeed9bbccd4d02ee5c0109b86d86835f995330da4c265957d157751f604d404")))
+
+        assertThat(signed.signedHeaders, equalTo(listOf(
+            "Host" to "examplebucket.s3.amazonaws.com"
+        )))
+        assertThat(signed.expires, equalTo(time + Duration.ofHours(24)))
     }
 
     @Test
@@ -47,6 +57,8 @@ class AwsPreSignRequestsTest {
         val signed = request
             .uri(signer(request, Duration.ofHours(24)).uri)
 
+        assertThat(signed.uri, hasHost("examplebucket.s3.amazonaws.com"))
+        assertThat(signed.uri, hasUriPath("/test.txt"))
         assertThat(signed, hasQuery("X-Amz-Algorithm", equalTo("AWS4-HMAC-SHA256")))
         assertThat(signed, hasQuery("X-Amz-Credential", equalTo("AKIAIOSFODNN7EXAMPLE/20130524/us-east-1/s3/aws4_request")))
         assertThat(signed, hasQuery("X-Amz-Date", equalTo("20130524T000000Z")))
