4851bfda020e88d1e8c4cbb4e4439dadbe445197
David Denton <denton.david@gmail.com>
2023-12-29 11:51:15 +0000

    changes to example for failsafe

--- src/docs/guide/reference/failsafe/example_multiple_policies.kt
+++ src/docs/guide/reference/failsafe/example_multiple_policies.kt
@@ -8,12 +8,12 @@ import dev.failsafe.Timeout
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status.Companion.BAD_REQUEST
 import org.http4k.core.Status.Companion.INTERNAL_SERVER_ERROR
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.then
 import org.http4k.filter.FailsafeFilter
 import java.time.Duration
+import kotlin.random.Random
 
 fun main() {
     // Configure multiple Failsafe policies
@@ -24,28 +24,32 @@ fun main() {
             .handleResultIf { !it.status.successful }
             .build(),
         CircuitBreaker.builder<Response>()
-            .withFailureThreshold(1)
+            .withFailureThreshold(3)
+            .withDelay(Duration.ofSeconds(3))
+            .onOpen { println("Circuit open") }
+            .onHalfOpen { println("Circuit half open") }
+            .onClose { println("Circuit closed") }
             .handleResultIf { it.status.serverError }
             .build(),
         Timeout.of(Duration.ofMillis(100))
     )
 
     // We then create a very unstable client using the filter
-    var requestCount = 0
     val client = FailsafeFilter(failsafeExecutor).then {
-        requestCount++
-
-        when (requestCount % 4) {
-            1, 3 -> Response(OK).body("All good - $requestCount")
-            0 -> Response(INTERNAL_SERVER_ERROR)
-            else -> Response(BAD_REQUEST)
-        }
+        when (Random.nextInt(0, 7)) {
+            0 -> Response(INTERNAL_SERVER_ERROR).body("Oh no!")
+            1, 2 -> {
+                Thread.sleep(200)
+                Response(OK).body("Slow!")
+            }
+            else -> Response(OK).body("All good!")
+        }.also { println("Call result: ${it.bodyString()}") }
     }
 
     // Throw a bunch of request at the filter - some will fail and be retried until
     // the circuit breaker opens and the fallback value will be used after that.
-    for (it in 1..5) {
-        val response = client(Request(GET, "/"))
-        println("${response.status} - ${response.bodyString()}")
+    repeat(1000) {
+        client(Request(GET, "/"))
+        Thread.sleep(1000)
     }
 }
