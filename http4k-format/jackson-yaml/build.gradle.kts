63b3f7c3f18176d2781f9d5dde8f3d996e4a35e3
David Denton <denton.david@gmail.com>
2024-01-12 11:44:36 +0100

    Add Jackson BOM to jackson modules

--- http4k-format/jackson-yaml/build.gradle.kts
+++ http4k-format/jackson-yaml/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     api(project(":http4k-format-core"))
     api(project(":http4k-realtime-core"))
     api(project(":http4k-format-jackson"))
+    api(platform("com.fasterxml.jackson:jackson-bom:_"))
     api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
     testImplementation(project(":http4k-core"))
     testImplementation(testFixtures(project(":http4k-core")))
