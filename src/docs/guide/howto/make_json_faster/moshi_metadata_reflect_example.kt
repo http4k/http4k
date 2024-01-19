5a071da682d49f78ecfc61976f4b1b793323d9b1
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-01-19 11:18:09 -0500

    replace moshi-lite guide with make json faster guide (#1039)

--- src/docs/guide/howto/make_json_faster/moshi_metadata_reflect_example.kt
+++ src/docs/guide/howto/make_json_faster/moshi_metadata_reflect_example.kt
@@ -1,7 +1,10 @@
-package guide.howto.moshi_lite
+package guide.howto.make_json_faster
 
 import com.squareup.moshi.Moshi
 import dev.zacsweers.moshix.reflect.MetadataKotlinJsonAdapterFactory
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.core.with
 import org.http4k.format.ConfigurableMoshi
 import org.http4k.format.EventAdapter
 import org.http4k.format.ListAdapter
@@ -9,8 +12,10 @@ import org.http4k.format.MapAdapter
 import org.http4k.format.ThrowableAdapter
 import org.http4k.format.asConfigurable
 import org.http4k.format.withStandardMappings
+import se.ansman.kotshi.JsonSerializable
+import java.util.UUID
 
-object MoshiLite: ConfigurableMoshi(
+private val json = ConfigurableMoshi(
     Moshi.Builder()
         .addLast(EventAdapter)
         .addLast(ThrowableAdapter)
@@ -20,3 +25,21 @@ object MoshiLite: ConfigurableMoshi(
         .withStandardMappings()
         .done()
 )
+
+data class MoshiCat(val id: UUID, val name: String)
+
+fun main() {
+    val cat = MoshiCat(UUID.randomUUID(), "Kratos")
+
+    // serialize
+    val string = json.asFormatString(cat)
+        .also(::println)
+
+    // deserialize
+    json.asA<MoshiCat>(string)
+        .also(::println)
+
+    // make a lens
+    val lens = json.autoBody<MoshiCat>().toLens()
+    Request(Method.GET, "foo").with(lens of cat)
+}
