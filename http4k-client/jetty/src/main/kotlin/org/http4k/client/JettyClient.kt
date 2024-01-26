5bd2f3307271469f4d0d7e1a5bae64bc4035b76d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-01-26 11:54:50 +0000

    tidy JettyClient (so that codebeat doesn't complain about function being too long)

--- http4k-client/jetty/src/main/kotlin/org/http4k/client/JettyClient.kt
+++ http4k-client/jetty/src/main/kotlin/org/http4k/client/JettyClient.kt
@@ -37,87 +37,92 @@ object JettyClient {
         requestModifier: (JettyRequest) -> JettyRequest = { it }
     ): DualSyncAsyncHttpHandler {
         if (!client.isRunning) client.start()
+        return JettyHandler(client, bodyMode, requestModifier)
+    }
 
-        return object : DualSyncAsyncHttpHandler {
-            override fun close() = client.stop()
+    private class JettyHandler(
+        private val client: HttpClient,
+        private val bodyMode: BodyMode,
+        private val requestModifier: (JettyRequest) -> JettyRequest
+    ) : DualSyncAsyncHttpHandler {
+        override fun close() = client.stop()
 
-            override fun invoke(request: Request): Response = client.send(request)
+        override fun invoke(request: Request): Response = client.send(request)
 
-            override fun invoke(request: Request, fn: (Response) -> Unit) = client.sendAsync(request, fn)
+        override fun invoke(request: Request, fn: (Response) -> Unit) = client.sendAsync(request, fn)
 
-            private fun HttpClient.send(request: Request): Response = with(newRequest(request)) {
-                try {
-                    when (bodyMode) {
-                        BodyMode.Memory -> send().let { it.toHttp4kResponse().body(it.contentAsString) }
-                        BodyMode.Stream -> InputStreamResponseListener().run {
-                            send(this)
-                            get(timeoutOrMax(), MILLISECONDS).toHttp4kResponse().body(inputStream)
-                        }
-                    }
-                } catch (e: ExecutionException) {
-                    when (e.cause) {
-                        is UnknownHostException -> Response(UNKNOWN_HOST.toClientStatus(e))
-                        is ConnectException -> Response(CONNECTION_REFUSED.toClientStatus(e))
-                        else -> throw e
-                    }
-                } catch (e: TimeoutException) {
-                    Response(CLIENT_TIMEOUT.toClientStatus(e))
-                }
-            }
-
-            private fun HttpClient.sendAsync(request: Request, fn: (Response) -> Unit) {
-                with(newRequest(request)) {
-                    when (bodyMode) {
-                        BodyMode.Memory -> send(object : BufferingResponseListener() {
-                            override fun onComplete(result: Result) {
-                                val response = if (result.isFailed) {
-                                    result.failure.asHttp4kResponse()
-                                } else {
-                                    result.response.toHttp4kResponse().body(contentAsString)
-                                }
-                                fn(response)
-                            }
-                        })
-                        BodyMode.Stream -> send(object : InputStreamResponseListener() {
-                            override fun onHeaders(response: JettyResponse) {
-                                super.onHeaders(response)
-                                executor.execute {
-                                    fn(response.toHttp4kResponse().body(inputStream))
-                                }
-                            }
-
-                            override fun onFailure(response: JettyResponse, failure: Throwable) {
-                                super.onFailure(response, failure)
-                                fn(failure.asHttp4kResponse())
-                            }
-                        })
+        private fun HttpClient.send(request: Request): Response = with(newRequest(request)) {
+            try {
+                when (bodyMode) {
+                    BodyMode.Memory -> send().let { it.toHttp4kResponse().body(it.contentAsString) }
+                    BodyMode.Stream -> InputStreamResponseListener().run {
+                        send(this)
+                        get(timeoutOrMax(), MILLISECONDS).toHttp4kResponse().body(inputStream)
                     }
                 }
+            } catch (e: ExecutionException) {
+                when (e.cause) {
+                    is UnknownHostException -> Response(UNKNOWN_HOST.toClientStatus(e))
+                    is ConnectException -> Response(CONNECTION_REFUSED.toClientStatus(e))
+                    else -> throw e
+                }
+            } catch (e: TimeoutException) {
+                Response(CLIENT_TIMEOUT.toClientStatus(e))
             }
+        }
 
-            private fun HttpClient.newRequest(request: Request) =
-                newRequest(request.uri.toString()).method(request.method.name)
-                    .headers { fields -> request.headers.toParametersMap().forEach { fields.put(it.key, it.value) } }
-                    .body(
-                        when (bodyMode) {
-                            BodyMode.Memory -> ByteBufferRequestContent(request.body.payload)
-                            BodyMode.Stream -> InputStreamRequestContent(request.body.stream)
+        private fun HttpClient.sendAsync(request: Request, fn: (Response) -> Unit) {
+            with(newRequest(request)) {
+                when (bodyMode) {
+                    BodyMode.Memory -> send(object : BufferingResponseListener() {
+                        override fun onComplete(result: Result) {
+                            val response = if (result.isFailed) {
+                                result.failure.asHttp4kResponse()
+                            } else {
+                                result.response.toHttp4kResponse().body(contentAsString)
+                            }
+                            fn(response)
                         }
-                    )
-                    .let(requestModifier)
+                    })
+                    BodyMode.Stream -> send(object : InputStreamResponseListener() {
+                        override fun onHeaders(response: JettyResponse) {
+                            super.onHeaders(response)
+                            executor.execute {
+                                fn(response.toHttp4kResponse().body(inputStream))
+                            }
+                        }
 
-            private fun JettyRequest.timeoutOrMax() = if (timeout <= 0) Long.MAX_VALUE else timeout
+                        override fun onFailure(response: JettyResponse, failure: Throwable) {
+                            super.onFailure(response, failure)
+                            fn(failure.asHttp4kResponse())
+                        }
+                    })
+                }
+            }
+        }
 
-            private fun JettyResponse.toHttp4kResponse(): Response =
-                Response(Status(status, reason)).headers(headers.toHttp4kHeaders())
+        private fun HttpClient.newRequest(request: Request) =
+            newRequest(request.uri.toString()).method(request.method.name)
+                .headers { fields -> request.headers.toParametersMap().forEach { fields.put(it.key, it.value) } }
+                .body(
+                    when (bodyMode) {
+                        BodyMode.Memory -> ByteBufferRequestContent(request.body.payload)
+                        BodyMode.Stream -> InputStreamRequestContent(request.body.stream)
+                    }
+                )
+                .let(requestModifier)
 
-            private fun HttpFields.toHttp4kHeaders(): Headers = flatMap { it.values.map { hValue -> it.name to hValue } }
+        private fun JettyRequest.timeoutOrMax() = if (timeout <= 0) Long.MAX_VALUE else timeout
 
-            private fun Throwable.asHttp4kResponse(): Response = Response(when (this) {
+        private fun JettyResponse.toHttp4kResponse() = Response(Status(status, reason)).headers(headers.toHttp4kHeaders())
+
+        private fun HttpFields.toHttp4kHeaders(): Headers = flatMap { it.values.map { hValue -> it.name to hValue } }
+
+        private fun Throwable.asHttp4kResponse() = Response(
+            when (this) {
                 is TimeoutException -> CLIENT_TIMEOUT
                 else -> SERVICE_UNAVAILABLE
             }.description("Client Error: caused by $localizedMessage"))
-        }
     }
 }
 
