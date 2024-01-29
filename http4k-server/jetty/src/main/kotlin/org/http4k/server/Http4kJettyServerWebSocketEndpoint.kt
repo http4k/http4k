554555e0f92a8ed70abaa95694a88e9cac729177
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-01-29 16:32:04 +0000

    formatting http4k-server

--- http4k-server/jetty/src/main/kotlin/org/http4k/server/Http4kJettyServerWebSocketEndpoint.kt
+++ http4k-server/jetty/src/main/kotlin/org/http4k/server/Http4kJettyServerWebSocketEndpoint.kt
@@ -15,61 +15,61 @@ import java.nio.ByteBuffer
 class Http4kJettyServerWebSocketEndpoint(
     private val consumer: WsConsumer, private val request: Request
 ) : Session.Listener.AbstractAutoDemanding() {
-        private var websocket: PushPullAdaptingWebSocket? = null
+    private var websocket: PushPullAdaptingWebSocket? = null
 
-        override fun onWebSocketOpen(session: Session) {
-            super.onWebSocketOpen(session)
-            websocket = object : PushPullAdaptingWebSocket() {
-                override fun send(message: WsMessage) {
-                    if (!isOpen) {
-                        throw WebSocketException("Connection to ${request.uri} is closed.")
-                    }
-                    try {
-                        when (message.body) {
-                            is StreamBody -> Callback.Completable.with { session.sendBinary(message.body.payload, it) }.get()
-                            else -> Callback.Completable.with { session.sendText(message.body.toString(), it) }.get()
-                        }
-                    } catch (error: Throwable) {
-                        triggerError(error)
-                    }
+    override fun onWebSocketOpen(session: Session) {
+        super.onWebSocketOpen(session)
+        websocket = object : PushPullAdaptingWebSocket() {
+            override fun send(message: WsMessage) {
+                if (!isOpen) {
+                    throw WebSocketException("Connection to ${request.uri} is closed.")
                 }
-
-                override fun close(status: WsStatus) {
-                    if (isOpen) {
-                        Callback.Completable.with { session.close(status.code, status.description, it) }.get()
+                try {
+                    when (message.body) {
+                        is StreamBody -> Callback.Completable.with { session.sendBinary(message.body.payload, it) }.get()
+                        else -> Callback.Completable.with { session.sendText(message.body.toString(), it) }.get()
                     }
+                } catch (error: Throwable) {
+                    triggerError(error)
                 }
-
-            }.apply(consumer)
-        }
-
-        override fun onWebSocketClose(statusCode: Int, reason: String?) {
-            super.onWebSocketClose(statusCode, reason)
-            websocket?.triggerClose(WsStatus(statusCode, reason.orEmpty()))
-        }
-
-        override fun onWebSocketError(cause: Throwable) {
-            super.onWebSocketError(cause)
-            websocket?.triggerError(cause)
-        }
-
-        override fun onWebSocketText(message: String) {
-            super.onWebSocketText(message)
-            try {
-                websocket?.triggerMessage(WsMessage(message))
-            } catch (e: Throwable) {
-                websocket?.triggerError(e)
             }
-        }
 
-        override fun onWebSocketBinary(payload: ByteBuffer, callback: Callback) {
-            super.onWebSocketBinary(payload, callback)
-            try {
-                websocket?.triggerMessage(WsMessage(BufferUtil.toArray(payload).inputStream()))
-                callback.succeed()
-            } catch (e: Throwable) {
-                websocket?.triggerError(e)
-                callback.fail(e)
+            override fun close(status: WsStatus) {
+                if (isOpen) {
+                    Callback.Completable.with { session.close(status.code, status.description, it) }.get()
+                }
             }
+
+        }.apply(consumer)
+    }
+
+    override fun onWebSocketClose(statusCode: Int, reason: String?) {
+        super.onWebSocketClose(statusCode, reason)
+        websocket?.triggerClose(WsStatus(statusCode, reason.orEmpty()))
+    }
+
+    override fun onWebSocketError(cause: Throwable) {
+        super.onWebSocketError(cause)
+        websocket?.triggerError(cause)
+    }
+
+    override fun onWebSocketText(message: String) {
+        super.onWebSocketText(message)
+        try {
+            websocket?.triggerMessage(WsMessage(message))
+        } catch (e: Throwable) {
+            websocket?.triggerError(e)
         }
+    }
+
+    override fun onWebSocketBinary(payload: ByteBuffer, callback: Callback) {
+        super.onWebSocketBinary(payload, callback)
+        try {
+            websocket?.triggerMessage(WsMessage(BufferUtil.toArray(payload).inputStream()))
+            callback.succeed()
+        } catch (e: Throwable) {
+            websocket?.triggerError(e)
+            callback.fail(e)
+        }
+    }
 }
