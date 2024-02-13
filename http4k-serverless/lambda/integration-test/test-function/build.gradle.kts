bb677b43c480782c163db9242cab666a0f0ce882
David Denton <denton.david@gmail.com>
2024-02-13 22:56:36 +0000

    migrate gradle file

---
+++ http4k-serverless/lambda/integration-test/test-function/build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+configurations {
+    named<Configuration>("runtimeClasspath") {
+        isTransitive = true
+    }
+}
+
+dependencies {
+    api(project(":http4k-core"))
+    api(project(":http4k-serverless-lambda"))
+    api(testFixtures(project(":http4k-core")))
+    api(testFixtures(project(":http4k-serverless-core")))
+    api("com.amazonaws:aws-lambda-java-events:_")
+    api("joda-time:joda-time:_")
+}
+
+tasks {
+    register<Zip>("buildZip") {
+        from(named<KotlinCompile>("compileKotlin"))
+        from(named<ProcessResources>("processResources"))
+        into("lib") {
+            from(configurations.named<Configuration>("runtimeClasspath"))
+        }
+    }
+}
