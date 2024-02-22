bf32030ff91fbefa6f2bd488880cbcaf35043c38
Andrew O'Hara <ohara.andrew314@gmail.com>
2024-02-22 14:58:12 -0500

    fix test infinite wait

--- src/docs/guide/howto/testing_websocket_clients/example.kt
+++ src/docs/guide/howto/testing_websocket_clients/example.kt
@@ -2,6 +2,7 @@ package guide.howto.testing_websocket_clients
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
+import com.natpryce.hamkrest.hasSize
 import org.http4k.client.JavaWsHandler
 import org.http4k.core.Method
 import org.http4k.core.Request
@@ -13,26 +14,21 @@ import org.http4k.testing.toSymmetric
 import org.http4k.websocket.SetHostFrom
 import org.http4k.websocket.SymmetricWsFilter
 import org.http4k.websocket.SymmetricWsHandler
+import org.http4k.websocket.Websocket
 import org.http4k.websocket.WsMessage
 import org.http4k.websocket.WsResponse
 import org.http4k.websocket.then
 import org.junit.jupiter.api.Test
-import java.util.concurrent.CountDownLatch
 
 // The Websocket client we want to test
 class GreetingClient(private val wsHandler: SymmetricWsHandler) {
 
-    fun getGreeting(name: String): String? {
-        val waiter = CountDownLatch(1)
-        var greeting: String? = null
+    val messages = mutableListOf<String>()
 
+    fun connect(name: String) {
         wsHandler(Request(Method.GET, "/v1/$name")).onMessage {
-            greeting = it.bodyString()
-            waiter.countDown()
+            messages += it.bodyString()
         }
-
-        waiter.await()
-        return greeting
     }
 }
 
@@ -47,15 +43,18 @@ fun main() {
     // The SymmetricWsHandler can be injected directly into the GreetingClient under test
     val client = GreetingClient(wsHandler)
 
-    println(client.getGreeting("http4k"))
+    // open a new websocket
+    client.connect("http4k")
 }
 
 class GreetingClientTest {
     // build a fake server to test GreetingClient against
+    private val serverSockets = mutableMapOf<String, Websocket>()
     private val server = websockets(
         "/v1/{name}" bind { req ->
+            val name = req.path("name")!!
             WsResponse {
-                it.send(WsMessage("Hello ${req.path("name")}"))
+                serverSockets[name] = it
             }
         }
     )
@@ -65,8 +64,12 @@ class GreetingClientTest {
         // convert the server to a SymmetricWsHandler to inject it into GreetingClient
         val client = GreetingClient(server.toSymmetric())
 
+        // open a new websocket
+        client.connect("http4k")
+
         // test the client-server interaction
-        assertThat(client.getGreeting("http4k"), equalTo("Hello http4k"))
+        serverSockets["http4k"]!!.send(WsMessage("Hello http4k"))
+        assertThat(client.messages, hasSize(equalTo(1)))
     }
 }
 
