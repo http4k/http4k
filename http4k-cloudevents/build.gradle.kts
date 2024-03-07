8e9d26b9afa1a6b86c35348303450da8e2131092
David Denton <denton.david@gmail.com>
2024-03-07 21:38:25 +0000

    CVE workaround in lib

--- http4k-cloudevents/build.gradle.kts
+++ http4k-cloudevents/build.gradle.kts
@@ -5,6 +5,7 @@ dependencies {
     api(project(":http4k-format-core"))
     api("io.cloudevents:cloudevents-core:_")
     api("io.cloudevents:cloudevents-json-jackson:_")
+    api("com.fasterxml.jackson.datatype:jackson-datatype-guava") // for CVE workaround (guava)
     api(project(":http4k-format-jackson"))
     testImplementation(testFixtures(project(":http4k-core")))
     testImplementation(project(":http4k-testing-hamkrest"))
