d8892ca1f930f9a204d4dd6690f54978415e4899
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-04-08 13:55:27 +0100

    formatting InsecureCookieBasedOAuthPersistence

--- http4k-security/oauth/src/main/kotlin/org/http4k/security/InsecureCookieBasedOAuthPersistence.kt
+++ http4k-security/oauth/src/main/kotlin/org/http4k/security/InsecureCookieBasedOAuthPersistence.kt
@@ -22,23 +22,24 @@ class InsecureCookieBasedOAuthPersistence(
 ) : OAuthPersistence {
 
     private val csrfName = "${cookieNamePrefix}Csrf"
-
     private val nonceName = "${cookieNamePrefix}Nonce"
-
     private val originalUriName = "${cookieNamePrefix}OriginalUri"
-
     private val accessTokenCookieName = "${cookieNamePrefix}AccessToken"
 
-    override fun retrieveCsrf(request: Request) = request.cookie(csrfName)?.value?.let(::CrossSiteRequestForgeryToken)
+    override fun retrieveCsrf(request: Request) =
+        request.cookie(csrfName)?.value?.let(::CrossSiteRequestForgeryToken)
 
-    override fun retrieveToken(request: Request): AccessToken? = request.cookie(accessTokenCookieName)?.value?.let { AccessToken(it) }
+    override fun retrieveToken(request: Request): AccessToken? =
+        request.cookie(accessTokenCookieName)?.value?.let { AccessToken(it) }
 
     override fun retrieveNonce(request: Request): Nonce? =
         request.cookie(nonceName)?.value?.let { Nonce(it) }
 
-    override fun retrieveOriginalUri(request: Request): Uri? = request.cookie(originalUriName)?.value?.let { Uri.of(it) }
+    override fun retrieveOriginalUri(request: Request): Uri? =
+        request.cookie(originalUriName)?.value?.let { Uri.of(it) }
 
-    override fun assignCsrf(redirect: Response, csrf: CrossSiteRequestForgeryToken) = redirect.cookie(expiring(csrfName, csrf.value))
+    override fun assignCsrf(redirect: Response, csrf: CrossSiteRequestForgeryToken) =
+        redirect.cookie(expiring(csrfName, csrf.value))
 
     override fun assignToken(request: Request, redirect: Response, accessToken: AccessToken, idToken: IdToken?) =
         redirect.cookie(expiring(accessTokenCookieName, accessToken.value))
@@ -46,15 +47,19 @@ class InsecureCookieBasedOAuthPersistence(
             .invalidateCookie(nonceName)
             .invalidateCookie(originalUriName)
 
-    override fun assignNonce(redirect: Response, nonce: Nonce): Response = redirect.cookie(expiring(nonceName, nonce.value))
+    override fun assignNonce(redirect: Response, nonce: Nonce): Response =
+        redirect.cookie(expiring(nonceName, nonce.value))
 
-    override fun assignOriginalUri(redirect: Response, originalUri: Uri): Response = redirect.cookie(expiring(originalUriName, originalUri.toString()))
+    override fun assignOriginalUri(redirect: Response, originalUri: Uri): Response =
+        redirect.cookie(expiring(originalUriName, originalUri.toString()))
 
-    override fun authFailureResponse(reason: OAuthCallbackError) = Response(FORBIDDEN)
-        .invalidateCookie(csrfName)
-        .invalidateCookie(accessTokenCookieName)
-        .invalidateCookie(nonceName)
-        .invalidateCookie(originalUriName)
+    override fun authFailureResponse(reason: OAuthCallbackError) =
+        Response(FORBIDDEN)
+            .invalidateCookie(csrfName)
+            .invalidateCookie(accessTokenCookieName)
+            .invalidateCookie(nonceName)
+            .invalidateCookie(originalUriName)
 
-    private fun expiring(name: String, value: String) = Cookie(name, value, expires = clock.instant().plus(cookieValidity), path = "/")
+    private fun expiring(name: String, value: String) =
+        Cookie(name, value, expires = clock.instant().plus(cookieValidity), path = "/")
 }
