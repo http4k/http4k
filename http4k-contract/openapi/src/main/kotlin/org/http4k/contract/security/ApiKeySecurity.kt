cbd8e796a45a0c3dde21d7929bfadf9cc11908c5
David Denton <denton.david@gmail.com>
2024-07-09 18:39:51 +0100

    Refactor Apikey security to have a (legacy) constructor and a new companion object invoke when another type is required

--- http4k-contract/openapi/src/main/kotlin/org/http4k/contract/security/ApiKeySecurity.kt
+++ http4k-contract/openapi/src/main/kotlin/org/http4k/contract/security/ApiKeySecurity.kt
@@ -17,70 +17,64 @@ import org.http4k.lens.LensInjector
  * Default implementation of ApiKey. Includes an option to NOT authorise OPTIONS requests, which is
  * currently not enabled for OpenAPI.
  */
-sealed interface ApiKeySecurity<out T> : Security {
-    val param: Lens<Request, T>
-    val name: String
+class ApiKeySecurity<out T> private constructor(
+    val param: Lens<Request, T>,
+    val name: String,
+    override val filter: Filter
+) : Security {
+
+    constructor(
+        param: Lens<Request, T>,
+        validateKey: (T) -> Boolean,
+        authorizeOptionsRequests: Boolean = true,
+        name: String = "api_key"
+    ) : this(param, name, apiKeySecurityFilter(
+        param,
+        validateKey,
+        failureResult = false,
+        authorizeOptionsRequests
+    ) { next, request, result -> if (result) next(request) else null }
+    )
 
     companion object {
-        operator fun <T> invoke(
-            param: Lens<Request, T>,
-            validateKey: (T) -> Boolean,
-            authorizeOptionsRequests: Boolean = true,
-            name: String = "api_key"
-        ): ApiKeySecurity<T> = SimpleApiKeySecurity(param, validateKey, authorizeOptionsRequests, name)
-
         operator fun <T, C> invoke(
             param: Lens<Request, T>,
             consumer: LensInjector<C, Request>,
             consumerForKey: (T) -> C?,
             authorizeOptionsRequests: Boolean = true,
             name: String = "api_key"
-        ): ApiKeySecurity<T> = ApiKeySecurityWithConsumer(param, consumer, consumerForKey, authorizeOptionsRequests, name)
-    }
-}
-
-private fun <T, R> apiKeySecurityFilter(
-    param: Lens<Request, T>,
-    validateAndReturnResult: (T) -> R,
-    failureResult: R,
-    authorizeOptionsRequests: Boolean = true,
-    onSuccess: (next: HttpHandler, request: Request, R) -> Response?
-) = Filter { next ->
-    {
-        if (!authorizeOptionsRequests && it.method == Method.OPTIONS) {
-            next(it)
-        } else {
-            val result = try {
-                validateAndReturnResult(param(it))
-            } catch (e: LensFailure) {
-                failureResult
+        ): ApiKeySecurity<T> = ApiKeySecurity(param, name,
+            apiKeySecurityFilter(
+                param,
+                consumerForKey,
+                failureResult = null,
+                authorizeOptionsRequests
+            ) { next, request, result ->
+                if (result != null) next(request.with(consumer of result)) else null
             }
-            onSuccess(next, it, result) ?: Response(UNAUTHORIZED)
+        )
+
+        private fun <T, R> apiKeySecurityFilter(
+            param: Lens<Request, T>,
+            validateAndReturnResult: (T) -> R,
+            failureResult: R,
+            authorizeOptionsRequests: Boolean = true,
+            onSuccess: (next: HttpHandler, request: Request, R) -> Response?
+        ) = Filter { next ->
+            {
+                if (!authorizeOptionsRequests && it.method == Method.OPTIONS) {
+                    next(it)
+                } else {
+                    val result = try {
+                        validateAndReturnResult(param(it))
+                    } catch (e: LensFailure) {
+                        failureResult
+                    }
+                    onSuccess(next, it, result) ?: Response(UNAUTHORIZED)
+                }
+            }
         }
     }
+
 }
 
-internal class SimpleApiKeySecurity<out T>(
-    override val param: Lens<Request, T>,
-    validateKey: (T) -> Boolean,
-    authorizeOptionsRequests: Boolean = true,
-    override val name: String
-) : ApiKeySecurity<T> {
-    override val filter =
-        apiKeySecurityFilter(param, validateKey, failureResult = false, authorizeOptionsRequests) { next, request, result ->
-            if(result) next(request) else null
-        }
-}
-
-internal class ApiKeySecurityWithConsumer<out T, in C>(
-    override val param: Lens<Request, T>,
-    private val consumer: LensInjector<C, Request>,
-    consumerForKey: (T) -> C?,
-    authorizeOptionsRequests: Boolean = true,
-    override val name: String
-) : ApiKeySecurity<T> {
-    override val filter =
-        apiKeySecurityFilter(param, consumerForKey, failureResult = null, authorizeOptionsRequests) { next, request, result ->
-            if(result != null) next(request.with(consumer of result)) else null
-        }
-}
