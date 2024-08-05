25eceecd16963455f5c6803047306d0af21dce74
Tim Mortimer <tim@kiteframe.co.uk>
2024-08-05 12:39:36 +0100

    Add `form(vararg formData: Pair<String, String>)` request helper (#1138)

--- http4k-core/src/main/kotlin/org/http4k/core/body/FormBody.kt
+++ http4k-core/src/main/kotlin/org/http4k/core/body/FormBody.kt
@@ -24,3 +24,5 @@ fun Form.toBody(): Body = Body(toUrlFormEncoded())
 fun Request.form(): Form = bodyString().toParameters()
 
 fun Request.form(name: String, value: String): Request = body(form().plus(name to value).toBody())
+
+fun Request.form(vararg formData: Pair<String, String>): Request = body(form().plus(formData).toBody())
--- src/docs/guide/howto/use_html_forms/example_standard.kt
+++ src/docs/guide/howto/use_html_forms/example_standard.kt
@@ -9,12 +9,21 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 
 fun main() {
+    // form(name: String, value: String) parses the request body on each invocation
     val request = Request(GET, "/").form("name", "rita").form("age", "55")
 
-    // reparses body every invocation
+    // form(vararg formData: Pair<String, String>) allows you to add multiple form fields to
+    // the request while only parsing the request body once
+    val allInOneRequest = Request(GET, "/").form("name" to "rita", "age" to "55")
+
+    // form(name: String) parses the request body on each invocation
     assertEquals("rita", request.form("name"))
     assertEquals("55", request.form("age"))
     assertNull(request.form("height"))
+
+    assertEquals("rita", allInOneRequest.form("name"))
+    assertEquals("55", allInOneRequest.form("age"))
+    assertNull(allInOneRequest.form("height"))
 
     // toParametersMap() gives form as map
     val parameters: Map<String, List<String?>> = request.form().toParametersMap()
