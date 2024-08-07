5dd9d6678bb9d70826c914e03546bb5a28a54507
David Denton <denton.david@gmail.com>
2024-08-07 12:06:17 +0100

    Add info debugging to build-ci.sh

--- build-ci.sh
+++ build-ci.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-./gradlew check jacocoRootReport --build-cache
+./gradlew check jacocoRootReport --build-cache --info
 bash <(curl -s https://codecov.io/bash)
