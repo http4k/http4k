863d919d779e4cb3545e1210835d4bf22c027116
David Denton <denton.david@gmail.com>
2024-08-12 13:15:01 +0100

    Adding in CSS for docs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 import groovy.namespace.QName
 import groovy.util.Node
 import org.gradle.api.JavaVersion.VERSION_1_8
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import java.time.Duration
@@ -35,6 +38,7 @@ buildscript {
         classpath("org.openapitools:openapi-generator-gradle-plugin:_")
         classpath("org.jetbrains.kotlin:kotlin-serialization:_")
         classpath("gradle.plugin.com.github.johnrengelman:shadow:_")
+        classpath("org.jetbrains.dokka:dokka-base:_")
     }
 }
 
@@ -100,9 +104,6 @@ subprojects {
     apply(plugin = "kotlin")
     apply(plugin = "idea")
 
-    if(hasAnArtifact(project) && !project.name.contains("serverless")) {
-        apply(plugin = "org.jetbrains.dokka")
-    }
 
     val sourcesJar by tasks.creating(Jar::class) {
         archiveClassifier.set("sources")
@@ -130,6 +131,10 @@ subprojects {
     }
 
     if (hasAnArtifact(project)) {
+        if (!project.name.contains("serverless")) {
+            apply(plugin = "org.jetbrains.dokka")
+        }
+
         val enableSigning = project.findProperty("sign") == "true"
 
         apply(plugin = "maven-publish") // required to upload to sonatype
@@ -287,5 +292,15 @@ nexusPublishing {
     transitionCheckOptions {
         maxRetries.set(150)
         delayBetween.set(Duration.ofSeconds(5))
+    }
+}
+
+tasks.withType<DokkaMultiModuleTask>().configureEach {
+    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+        moduleVersion.set(version.toString())
+        customAssets = listOf(file("src/docs/img/favicon-mono.png"))
+        footerMessage = "(c) 2024 http4k"
+        homepageLink = "https://http4k.org"
+        customStyleSheets = listOf(file("src/docs/css/dokka.css"))
     }
 }
