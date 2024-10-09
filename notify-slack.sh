f7768e8f12944e645d7407b9baade756ad0f692d
David Denton <denton.david@gmail.com>
2024-10-09 18:08:09 +0100

    fix link

--- notify-slack.sh
+++ notify-slack.sh
@@ -5,5 +5,5 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-MESSAGE="Released http4k version $1 (See <https://www.http4k.org/changelog/|Changelog> for details)."
+MESSAGE="Released http4k version $1 (See <https://www.http4k.org/ecosystem/http4k/changelog/|Changelog> for details)."
 curl -X POST -H 'Content-type: application/json' --data "{'text':'$MESSAGE'}" $SLACK_WEBHOOK
