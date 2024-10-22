3d1f019a276d04064f17062845f4ff6ef490456a
Ivan Sanchez <29394+s4nchez@users.noreply.github.com>
2024-10-22 11:48:14 +0100

    JUnit reporting (#1193)
    Improve junit reporting
    ---------
    
    Co-authored-by: isanchez <ivan@http4k.org>

--- http4k-core/src/testFixtures/kotlin/org/http4k/junit/OpenTracingTestReporting.kt
+++ http4k-core/src/testFixtures/kotlin/org/http4k/junit/OpenTracingTestReporting.kt
@@ -11,12 +11,15 @@ import io.opentelemetry.sdk.trace.export.BatchSpanProcessor
 import org.junit.platform.engine.TestExecutionResult
 import org.junit.platform.launcher.TestExecutionListener
 import org.junit.platform.launcher.TestIdentifier
+import java.time.Instant
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit.SECONDS
 
 
 class OpenTracingTestReporting : TestExecutionListener {
 
     private val openTelemetry: OpenTelemetrySdk?
+    private val stats = Stats()
 
     init {
         val honeycombApiKey = System.getenv("HONEYCOMB_API_KEY")
@@ -27,7 +30,7 @@ class OpenTracingTestReporting : TestExecutionListener {
             val resource = Resource.getDefault()
                 .merge(Resource.create(Attributes.of(AttributeKey.stringKey("service.name"), "test-main")))
 
-            val dataset = System.getenv("HONEYCOMB_DATASET") ?: "local"
+            val dataset = System.getenv("HONEYCOMB_DATASET") ?: "test-main"
 
             val spanExporter = OtlpHttpSpanExporter.builder()
                 .setEndpoint("https://api.honeycomb.io/v1/traces")
@@ -53,29 +56,37 @@ class OpenTracingTestReporting : TestExecutionListener {
         }
     }
 
+    override fun executionStarted(testIdentifier: TestIdentifier) = stats.start(testIdentifier)
+
     override fun executionFinished(testIdentifier: TestIdentifier, testExecutionResult: TestExecutionResult) {
         if (openTelemetry == null || !testIdentifier.isTest) return
 
-        val tracer: Tracer = openTelemetry.getTracer("http4")
-        val identifier = parseTestIdentifier(testIdentifier.uniqueId)
+        stats.end(testIdentifier)
 
-        val span = tracer.spanBuilder(identifier.spanName()).startSpan()
+        val tracer: Tracer = openTelemetry.getTracer("http4")
+        val betterIdentifier = parseTestIdentifier(testIdentifier.uniqueId)
+
+        val span = tracer.spanBuilder(betterIdentifier.spanName()).startSpan()
 
         try {
             span.makeCurrent().use { _ ->
                 span.setAttribute("status", testExecutionResult.status.toString())
-                when (identifier) {
+                span.setAttribute("junit_id", testIdentifier.uniqueId)
+                span.setAttribute("duration_ms", stats.duration(testIdentifier) ?: -1)
+
+                when (betterIdentifier) {
                     is BetterTestIdentifier.TestId -> {
                         span.setAttribute("identified", true)
-                        span.setAttribute("package", identifier.packageName)
-                        span.setAttribute("class", identifier.className)
-                        span.setAttribute("method", identifier.methodName)
+                        span.setAttribute("package", betterIdentifier.packageName)
+                        span.setAttribute("class", betterIdentifier.className)
+                        span.setAttribute("method", betterIdentifier.methodName)
                     }
 
                     is BetterTestIdentifier.Unknown -> {
                         span.setAttribute("identified", false)
                     }
                 }
+
                 testExecutionResult.throwable.ifPresent {
                     span.recordException(it)
                 }
@@ -90,11 +101,11 @@ class OpenTracingTestReporting : TestExecutionListener {
 }
 
 fun parseTestIdentifier(identifier: String): BetterTestIdentifier {
-    val regex = Regex("""\[class:(.+?)\]/\[method|test-template:(.+?)\]""")
+    val regex = Regex("""\[class:(.+?)\]\/\[(method|template):(.+?)\]""")
     val matchResult = regex.find(identifier)
     return matchResult?.let {
         val classFullyClassifiedName = it.groupValues[1]
-        val methodName = it.groupValues[2].substringBeforeLast('(')
+        val methodName = it.groupValues[3].substringBeforeLast('(')
         val packageName = classFullyClassifiedName.substringBeforeLast('.')
         val className = classFullyClassifiedName.substringAfterLast('.')
         BetterTestIdentifier.TestId(packageName, className, methodName)
@@ -109,4 +120,24 @@ sealed class BetterTestIdentifier {
 
     data class Unknown(val identifier: String) : BetterTestIdentifier()
     data class TestId(val packageName: String, val className: String, val methodName: String) : BetterTestIdentifier()
+}
+
+private class Stats {
+    private val starts = ConcurrentHashMap<TestIdentifier, Instant>()
+    private val ends = ConcurrentHashMap<TestIdentifier, Instant>()
+
+    fun start(testId: TestIdentifier) {
+        starts[testId] = Instant.now()
+    }
+
+    fun end(testId: TestIdentifier) {
+        ends[testId] = Instant.now()
+    }
+
+    fun duration(testId: TestIdentifier) =
+        starts[testId]?.let { start ->
+            ends[testId]?.let { end ->
+                end.toEpochMilli() - start.toEpochMilli()
+            }
+        }
 }
