1a0d7bba89cff24a7068f582a4ece0bf6f6c7340
David Denton <denton.david@gmail.com>
2024-10-24 21:06:22 +0100

    Fix #1195 - Apache httpcomponents server throws when returning 204.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@
 This list is not intended to be all-encompassing - it will document major and breaking API
 changes with their rationale when appropriate:
 
+### v5.33.0.1 (uncut) 
+- **http4k-server-apache** - [Fix] Apache httpcomponents server throws when returning 204.
+
 ### v5.33.0.0
 - **http4k-*** : Upgrade some dependency versions, including Kotlin to 2.0.21
 - **http4k-core** : [Unlikely break] Adding start time to HTTP Transaction. Should be a simple fix if you are already creating your own for some reason. :) H/T @npryce
--- http4k-server/apache/src/test/kotlin/org/http4k/server/ApacheServerTest.kt
+++ http4k-server/apache/src/test/kotlin/org/http4k/server/ApacheServerTest.kt
@@ -1,26 +1,14 @@
 package org.http4k.server
 
 import com.natpryce.hamkrest.Matcher
-import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.client.ApacheClient
 import org.http4k.core.Method
-import org.http4k.core.Method.GET
-import org.http4k.core.Request
-import org.http4k.core.Status.Companion.NO_CONTENT
-import org.junit.jupiter.api.Disabled
-import org.junit.jupiter.api.Test
 
 class ApacheServerTest : ServerContract({ port -> ApacheServer(port, canonicalHostname = "localhost") },
     ApacheClient(),
     Method.entries.filter { it != Method.PURGE }.toTypedArray()) {
 
     override fun requestScheme(): Matcher<String?> = equalTo("http")
-
-    @Test
-    @Disabled("unsupported by the underlying server")
-    override fun `return 204 no content`() {
-        assertThat(client(Request(GET, "$baseUrl/no-content")).status, equalTo(NO_CONTENT))
-    }
 }
 
