986984eff870f1fa3666bdf64f7fe3ea189fa7f9
David Denton <denton.david@gmail.com>
2024-11-05 17:34:29 +0000

    Add LTS slack links to notify slack

--- notify-slack.sh
+++ notify-slack.sh
@@ -12,6 +12,6 @@ if [[ $1 == 4* ]]; then
   MESSAGE="Released http4k LTS version $1 (See <https://github.com/http4k-lts-v4/http4k/blob/master/CHANGELOG.md|Changelog> for details)."
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MESSAGE\"}" $LTS_SLACK_WEBHOOK
 else
-  MESSAGE="Released http4k OSS version $1 (See <h<https://www.http4k.org/ecosystem/http4k/changelog/|Changelog> for details)."
+  MESSAGE="Released http4k OSS version $1 (See <https://www.http4k.org/ecosystem/http4k/changelog/|Changelog> for details)."
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MESSAGE\"}" $LTS_SLACK_WEBHOOK
 fi
