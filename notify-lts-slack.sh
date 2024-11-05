22b2bc69c3c66678ac3be2e38fd26f3f9cec22ae
David Denton <denton.david@gmail.com>
2024-11-05 17:43:17 +0000

    fix script for slack release

--- notify-lts-slack.sh
+++ notify-lts-slack.sh
@@ -6,7 +6,7 @@ set -o pipefail
 set -o nounset
 
 if [[ $1 == 4* ]]; then
-  MESSAGE="Released http4k LTS version $1 (See <https://github.com/http4k-lts-v4/http4k/blob/master/CHANGELOG.md|Changelog> for details)."
+  MESSAGE="Released http4k LTS version $1 (See <https://github.com/http4k/http4k-lts-v4/blob/master/CHANGELOG.md|Changelog> for details)."
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MESSAGE\"}" $LTS_SLACK_WEBHOOK
 else
   MESSAGE="Released http4k OSS version $1 (See <https://www.http4k.org/ecosystem/http4k/changelog/|Changelog> for details)."
