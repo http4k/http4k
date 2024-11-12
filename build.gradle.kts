a5ac85da743e0cf3748f1697c8f73446bd14bf29
Ivan Sanchez <ivan@gourame.com>
2024-11-12 19:55:30 +0000

    Move publishing config to plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import groovy.namespace.QName
-import groovy.util.Node
 import org.gradle.api.JavaVersion.VERSION_1_8
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
@@ -112,65 +110,6 @@ subprojects {
         apply(plugin = "org.http4k.license-check")
         apply(plugin = "org.http4k.publishing")
         apply(plugin = "maven-publish") // required to upload to sonatype
-
-        publishing {
-            repositories {
-                maven {
-                    name = "http4k"
-                    url = URI("s3://http4k-maven")
-
-                    val ltsPublishingUser: String? by project
-                    val ltsPublishingPassword: String? by project
-
-                    credentials(AwsCredentials::class.java) {
-                        accessKey = ltsPublishingUser
-                        secretKey = ltsPublishingPassword
-                    }
-                }
-            }
-
-            publications {
-                val javaComponent = components["java"] as AdhocComponentWithVariants
-
-                javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
-                javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
-
-                val archivesBaseName = tasks.jar.get().archiveBaseName.get()
-                create<MavenPublication>("mavenJava") {
-                    artifactId = archivesBaseName
-                    pom.withXml {
-                        asNode().appendNode("name", archivesBaseName)
-                        asNode().appendNode("description", description)
-                        asNode().appendNode("url", "https://http4k.org")
-                        asNode().appendNode("developers")
-                            .appendNode("developer").appendNode("name", "Ivan Sanchez").parent()
-                            .appendNode("email", "ivan@http4k.org")
-                            .parent().parent()
-                            .appendNode("developer").appendNode("name", "David Denton").parent()
-                            .appendNode("email", "david@http4k.org")
-                        asNode().appendNode("scm")
-                            .appendNode("url", "https://github.com/http4k/http4k").parent()
-                            .appendNode("connection", "scm:git:git@github.com:http4k/http4k.git").parent()
-                            .appendNode("developerConnection", "scm:git:git@github.com:http4k/http4k.git")
-                        asNode().appendNode("licenses").appendNode("license")
-                            .appendNode("name", "Apache License, Version 2.0").parent()
-                            .appendNode("url", "http://www.apache.org/licenses/LICENSE-2.0.html")
-                    }
-                    from(components["java"])
-
-                    // replace all runtime dependencies with provided
-                    pom.withXml {
-                        asNode()
-                            .childrenCalled("dependencies")
-                            .flatMap { it.childrenCalled("dependency") }
-                            .flatMap { it.childrenCalled("scope") }
-                            .forEach { if (it.text() == "runtime") it.setValue("provided") }
-                    }
-                    artifact(tasks.named("sourcesJar"))
-                    artifact(tasks.named("javadocJar"))
-                }
-            }
-        }
     }
 
     sourceSets {
@@ -232,12 +171,6 @@ tasks.named("checkLicense") {
         project != rootProject
     }
 }
-fun Node.childrenCalled(wanted: String) = children()
-    .filterIsInstance<Node>()
-    .filter {
-        val name = it.name()
-        (name is QName) && name.localPart == wanted
-    }
 
 tasks {
     named<KotlinJvmCompile>("compileTestKotlin").configure {
--- gradle/scripts/src/main/kotlin/org.http4k.publishing.gradle.kts
+++ gradle/scripts/src/main/kotlin/org.http4k.publishing.gradle.kts
@@ -1,3 +1,7 @@
+import groovy.namespace.QName
+import groovy.util.Node
+import java.net.URI
+
 plugins {
     kotlin("jvm")
     `java-library`
@@ -16,3 +20,69 @@ if (enableSigning) { // when added it expects signing keys to be configured
         sign(publishing.publications)
     }
 }
+
+publishing {
+    repositories {
+        maven {
+            name = "http4k"
+            url = URI("s3://http4k-maven")
+
+            val ltsPublishingUser: String? by project
+            val ltsPublishingPassword: String? by project
+
+            credentials(AwsCredentials::class.java) {
+                accessKey = ltsPublishingUser
+                secretKey = ltsPublishingPassword
+            }
+        }
+    }
+
+    publications {
+        val javaComponent = components["java"] as AdhocComponentWithVariants
+
+        javaComponent.withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
+        javaComponent.withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
+
+        val archivesBaseName = tasks.jar.get().archiveBaseName.get()
+        create<MavenPublication>("mavenJava") {
+            artifactId = archivesBaseName
+            pom.withXml {
+                asNode().appendNode("name", archivesBaseName)
+                asNode().appendNode("description", description)
+                asNode().appendNode("url", "https://http4k.org")
+                asNode().appendNode("developers")
+                    .appendNode("developer").appendNode("name", "Ivan Sanchez").parent()
+                    .appendNode("email", "ivan@http4k.org")
+                    .parent().parent()
+                    .appendNode("developer").appendNode("name", "David Denton").parent()
+                    .appendNode("email", "david@http4k.org")
+                asNode().appendNode("scm")
+                    .appendNode("url", "https://github.com/http4k/http4k").parent()
+                    .appendNode("connection", "scm:git:git@github.com:http4k/http4k.git").parent()
+                    .appendNode("developerConnection", "scm:git:git@github.com:http4k/http4k.git")
+                asNode().appendNode("licenses").appendNode("license")
+                    .appendNode("name", "Apache License, Version 2.0").parent()
+                    .appendNode("url", "http://www.apache.org/licenses/LICENSE-2.0.html")
+            }
+            from(components["java"])
+
+            // replace all runtime dependencies with provided
+            pom.withXml {
+                asNode()
+                    .childrenCalled("dependencies")
+                    .flatMap { it.childrenCalled("dependency") }
+                    .flatMap { it.childrenCalled("scope") }
+                    .forEach { if (it.text() == "runtime") it.setValue("provided") }
+            }
+            artifact(tasks.named("sourcesJar"))
+            artifact(tasks.named("javadocJar"))
+        }
+    }
+}
+
+private fun Node.childrenCalled(wanted: String) = children()
+    .filterIsInstance<Node>()
+    .filter {
+        val name = it.name()
+        (name is QName) && name.localPart == wanted
+    }
