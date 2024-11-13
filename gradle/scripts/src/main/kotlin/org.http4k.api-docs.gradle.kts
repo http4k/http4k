2f1134aed41d6dcf88b9cb0d7b1717fff6ed721a
David Denton <denton.david@gmail.com>
2024-11-13 09:40:10 +0000

    Tidy dokka code

--- gradle/scripts/src/main/kotlin/org.http4k.api-docs.gradle.kts
+++ gradle/scripts/src/main/kotlin/org.http4k.api-docs.gradle.kts
@@ -1,7 +1,7 @@
-import org.gradle.kotlin.dsl.withType
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+import java.time.Year
 
 plugins {
     id("org.jetbrains.dokka")
@@ -10,8 +10,7 @@ plugins {
 tasks.withType<DokkaMultiModuleTask>().configureEach {
     pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
         moduleVersion.set(version.toString())
-        customAssets = listOf(file("src/docs/img/favicon-mono.png"))
-        footerMessage = "(c) 2024 http4k"
+        footerMessage = "(c) ${Year.now().value} http4k"
         homepageLink = "https://http4k.org"
         customStyleSheets = listOf(file("src/docs/css/dokka.css"))
     }
