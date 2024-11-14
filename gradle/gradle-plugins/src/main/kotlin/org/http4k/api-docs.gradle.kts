af394914bb3613da490dae543ec4ee9e493739f8
David Denton <denton.david@gmail.com>
2024-11-14 12:15:19 +0000

    fix up docs

--- gradle/gradle-plugins/src/main/kotlin/org/http4k/api-docs.gradle.kts
+++ gradle/gradle-plugins/src/main/kotlin/org/http4k/api-docs.gradle.kts
@@ -1,16 +1,15 @@
 package org.http4k
 
-import org.jetbrains.dokka.DokkaDefaults.moduleVersion
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
-import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+import org.jetbrains.dokka.gradle.AbstractDokkaTask
 import java.time.Year
 
 plugins {
     id("org.jetbrains.dokka")
 }
 
-tasks.withType<DokkaMultiModuleTask>().configureEach {
+tasks.withType<AbstractDokkaTask>().configureEach {
     pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
         moduleVersion.set(version.toString())
         footerMessage = "(c) ${Year.now().value} http4k"
