1952159a31b3a289eadc1997711743361e7627ef
David Denton <daviddenton@users.noreply.github.com>
2024-11-14 12:15:47 +0000

    Fixing dokka (#1201)
    * Add local http4k-connect repo for publishing
    
    * fix up docs

--- gradle/gradle-plugins/src/main/kotlin/org/http4k/api-docs.gradle.kts
+++ gradle/gradle-plugins/src/main/kotlin/org/http4k/api-docs.gradle.kts
@@ -1,19 +1,19 @@
-package org.http4k.internal
+package org.http4k
 
 import org.jetbrains.dokka.base.DokkaBase
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
-import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+import org.jetbrains.dokka.gradle.AbstractDokkaTask
 import java.time.Year
 
 plugins {
     id("org.jetbrains.dokka")
 }
 
-tasks.withType<DokkaMultiModuleTask>().configureEach {
+tasks.withType<AbstractDokkaTask>().configureEach {
     pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
         moduleVersion.set(version.toString())
         footerMessage = "(c) ${Year.now().value} http4k"
         homepageLink = "https://http4k.org"
-        customStyleSheets = listOf(file("src/docs/css/dokka.css"))
+        customStyleSheets = listOf(file("${rootProject.projectDir}/src/docs/css/dokka.css"))
     }
 }
