07374ac21a2113e989fa37f9320028a11b480a9c
Ivan Sanchez <ivan@gourame.com>
2024-11-18 09:12:14 +0000

    Move purgeProjects.sh to bin (+ rename it)

--- bin/purge_projects.sh
+++ bin/purge_projects.sh
@@ -2,13 +2,15 @@
 
 VERSION=$1
 
-./gradlew listProjects -q 2> projects.txt
+BASE_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 
-for ARTIFACT in `cat projects.txt`
+$BASE_DIR/gradlew listProjects -q 2> $BASE_DIR/projects.txt
+
+for ARTIFACT in `cat $BASE_DIR/projects.txt`
 do
     curl -X PURGE https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom > /dev/null 2>&1
 
     echo Purging $ARTIFACT $VERSION `curl -sq --head https://repo.maven.apache.org/maven2/org/http4k/$ARTIFACT/$VERSION/$ARTIFACT-$VERSION.pom | head -1`
 done
 
-rm projects.txt
+rm $BASE_DIR/projects.txt
