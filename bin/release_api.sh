487a5d75149338b726a205d5149fb572d4dfa9aa
Ivan Sanchez <ivan@gourame.com>
2024-11-18 08:54:54 +0000

    Move release-api.sh to bin (+ rename it)

--- bin/release_api.sh
+++ bin/release_api.sh
@@ -5,11 +5,14 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-./gradlew dokkaHtmlMultiModule
+BASE_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
+
+$BASE_DIR/gradlew dokkaHtmlMultiModule
+
 rm -rf /tmp/http4k-api/
 git clone https://"${GH_TOKEN}"@github.com/http4k/api.git /tmp/http4k-api/
 rm -rf /tmp/http4k-api/*
-cp -R build/ddoc/http4k/* /tmp/http4k-api/
+cp -R $BASE_DIR/build/ddoc/http4k/* /tmp/http4k-api/
 
 cd /tmp/http4k-api
 git add .
