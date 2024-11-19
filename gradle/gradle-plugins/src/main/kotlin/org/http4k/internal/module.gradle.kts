9a5dea48c64ccc561bf718d4087c323f105d0522
David Denton <denton.david@gmail.com>
2024-11-19 16:47:18 +0000

    Add module dependency dependencyList task

--- gradle/gradle-plugins/src/main/kotlin/org/http4k/internal/module.gradle.kts
+++ gradle/gradle-plugins/src/main/kotlin/org/http4k/internal/module.gradle.kts
@@ -1,8 +1,17 @@
 package org.http4k.internal
 
 plugins {
+    java
     id("org.http4k.conventions")
     id("org.http4k.internal.license-check")
     id("org.http4k.internal.code-coverage")
     id("org.http4k.internal.publishing")
+}
+
+tasks.create("dependencyList") {
+    doLast {
+        configurations.runtimeClasspath.get().resolvedConfiguration.lenientConfiguration.allModuleDependencies.forEach { resolved ->
+            println("${project.name} -> ${resolved.module.id}")
+        }
+    }
 }
