75d56ec1e7aed242832d9332de8fea65a4b8375c
David Denton <denton.david@gmail.com>
2024-11-23 14:44:08 +0000

    Add RestJson base action

---
+++ connect/amazon/core/src/main/kotlin/org/http4k/connect/amazon/AwsRestJsonAction.kt
@@ -0,0 +1,35 @@
+package org.http4k.connect.amazon
+
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import org.http4k.connect.Action
+import org.http4k.connect.RemoteFailure
+import org.http4k.connect.asRemoteFailure
+import org.http4k.core.ContentType.Companion.APPLICATION_JSON
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.with
+import org.http4k.format.AutoMarshalling
+import org.http4k.lens.Header
+import kotlin.reflect.KClass
+
+abstract class AwsRestJsonAction<R : Any>(
+    private val base: Request,
+    private val clazz: KClass<R>,
+    private val autoMarshalling: AutoMarshalling,
+) : Action<Result4k<R, RemoteFailure>> {
+    override fun toRequest() = base
+        .with(Header.CONTENT_TYPE of APPLICATION_JSON)
+        .body(autoMarshalling.asFormatString(this))
+
+    override fun toResult(response: Response) = with(response) {
+        when {
+            status.successful -> Success(autoMarshalling.asA(
+                bodyString().takeIf { it.isNotEmpty() } ?: "{}",
+                clazz))
+
+            else -> Failure(asRemoteFailure(this))
+        }
+    }
+}
