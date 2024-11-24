104d34d42c6a03777025e4bac81d253d07f5b5bc
David Denton <denton.david@gmail.com>
2024-11-24 09:14:40 +0000

    Specify methods for sse

--- core/server/jetty/src/test/kotlin/org/http4k/sse/JettyLoomSseTest.kt
+++ core/server/jetty/src/test/kotlin/org/http4k/sse/JettyLoomSseTest.kt
@@ -1,10 +1,7 @@
 package org.http4k.sse
 
 import org.http4k.client.JavaHttpClient
-import org.http4k.core.Method.GET
 import org.http4k.server.JettyLoom
 import org.http4k.server.ServerConfig
 
-class JettyLoomSseTest : SseServerContract({ JettyLoom(it, ServerConfig.StopMode.Immediate) }, JavaHttpClient()) {
-    override val supportedMethods = setOf(GET)
-}
+class JettyLoomSseTest : SseServerContract({ JettyLoom(it, ServerConfig.StopMode.Immediate) }, JavaHttpClient())
