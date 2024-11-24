21697121803f4fc0546def7039cb2f7be8d15b49
David Denton <denton.david@gmail.com>
2024-11-24 09:01:06 +0000

    Add tests for SSE method support

--- core/server/jetty/src/test/kotlin/org/http4k/sse/JettyLoomSseTest.kt
+++ core/server/jetty/src/test/kotlin/org/http4k/sse/JettyLoomSseTest.kt
@@ -1,7 +1,10 @@
 package org.http4k.sse
 
 import org.http4k.client.JavaHttpClient
+import org.http4k.core.Method.GET
 import org.http4k.server.JettyLoom
 import org.http4k.server.ServerConfig
 
-class JettyLoomSseTest : SseServerContract({ JettyLoom(it, ServerConfig.StopMode.Immediate) }, JavaHttpClient())
+class JettyLoomSseTest : SseServerContract({ JettyLoom(it, ServerConfig.StopMode.Immediate) }, JavaHttpClient()) {
+    override val supportedMethods = setOf(GET)
+}
