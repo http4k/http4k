66c34f5b11f84f81ed3d44bb044d3ade171f8524
David Denton <denton.david@gmail.com>
2024-11-25 09:47:25 +0000

    Add roundtripping of ssemessage

--- core/realtime-core/src/main/kotlin/org/http4k/sse/sse.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/sse/sse.kt
@@ -24,13 +24,17 @@ data class SseResponse(val status: Status = OK, val headers: Headers = emptyList
 
 typealias SseHandler = (Request) -> SseResponse
 
-sealed class SseMessage {
-    data class Data(val data: String) : SseMessage() {
+sealed interface SseMessage {
+
+    fun toMessage(): String
+
+    data class Data(val data: String) : SseMessage {
         constructor(data: ByteArray) : this(data.base64Encode())
         constructor(data: InputStream) : this(data.readAllBytes())
+        override fun toMessage() = "data: $data"
     }
 
-    data class Event(val event: String, val data: String, val id: String? = null) : SseMessage() {
+    data class Event(val event: String, val data: String, val id: String? = null) : SseMessage {
         constructor(event: String, data: ByteArray, id: String? = null) : this(
             event,
             data.base64Encode(),
@@ -38,11 +42,31 @@ sealed class SseMessage {
         )
 
         constructor(event: String, data: InputStream, id: String? = null) : this(event, data.readAllBytes(), id)
+
+        override fun toMessage() = (listOf("event: $event") + data.split("\n")
+            .map { "data: $it" } + listOfNotNull(id?.let { "id: $it" }))
+            .joinToString("\n")
     }
 
-    data class Retry(val backoff: Duration) : SseMessage()
+    data class Retry(val backoff: Duration) : SseMessage {
+        override fun toMessage() = "retry: ${backoff.toMillis()}"
+    }
 
-    companion object
+    companion object {
+        fun parse(message: String): SseMessage {
+            val parts = message.split("\n")
+            return when {
+                parts.first().startsWith("data: ") -> Data(parts.first().removePrefix("data: "))
+                parts.first().startsWith("event: ") -> Event(
+                    parts.first().removePrefix("event: "),
+                    parts.filter { it.startsWith("data: ") }.joinToString("\n") { it.removePrefix("data: ") },
+                    parts.find { it.startsWith("id: ") }?.removePrefix("id: ")
+                )
+                parts.first().startsWith("retry: ") -> Retry(Duration.ofMillis(parts.first().removePrefix("retry: ").toLong()))
+                else -> throw IllegalArgumentException("Unrecognised message format: $message")
+            }
+        }
+    }
 }
 
 fun interface SseFilter : (SseHandler) -> SseHandler {
--- core/realtime-core/src/test/kotlin/org/http4k/sse/SseMessageTest.kt
+++ core/realtime-core/src/test/kotlin/org/http4k/sse/SseMessageTest.kt
@@ -4,11 +4,38 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.base64Encode
 import org.junit.jupiter.api.Test
+import java.time.Duration
 
 class SseMessageTest {
 
     @Test
     fun `encodes binary as base 64`() {
         assertThat(SseMessage.Data("body".byteInputStream()).data, equalTo("body".base64Encode()))
+    }
+
+    @Test
+    fun `converts data toMessage`() {
+        assertRoundtrip(SseMessage.Data("body"), "data: body")
+    }
+
+    @Test
+    fun `converts event toMessage`() {
+        assertRoundtrip(
+            SseMessage.Event("event", "data1\ndata2", "id"), """event: event
+data: data1
+data: data2
+id: id"""
+        )
+    }
+
+    @Test
+    fun `converts retry toMessage`() {
+        assertRoundtrip(SseMessage.Retry(Duration.ofMillis(1000)), "retry: 1000")
+    }
+
+
+    private fun assertRoundtrip(message: SseMessage, string: String) {
+        assertThat(message.toMessage(), equalTo(string))
+        assertThat(SseMessage.parse(string), equalTo(message))
     }
 }
