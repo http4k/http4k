0725130c4a9b7ba406cbde540290716c1e4b59ad
Ivan Sanchez <ivan@gourame.com>
2024-11-29 08:27:17 +0000

    Support method constraint

--- core/core/src/testFixtures/kotlin/org/http4k/routing/NewRoutingTests.kt
+++ core/core/src/testFixtures/kotlin/org/http4k/routing/NewRoutingTests.kt
@@ -8,6 +8,8 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.UriTemplate
+import org.http4k.routing.MethodConstraint.Any
+import org.http4k.routing.MethodConstraint.Specific
 import org.junit.jupiter.api.Test
 
 class NewRoutingTests {
@@ -21,23 +23,52 @@ class NewRoutingTests {
         assertThat(app(Request(Method.PUT, "/foo")).status, equalTo(Status.OK))
     }
 
-    private fun newRoutes(routes: Pair<String, HttpHandler>): HttpHandler = RoutedHttpHandler(
-        TemplatedHttpHandler(UriTemplate.from(routes.first), routes.second)
-    )
+    @Test
+    fun `routes a template with method`() {
+        val app: HttpHandler = newRoutes("/foo" newBind Method.GET to { req: Request -> Response(Status.OK) })
+
+        assertThat(app(Request(Method.GET, "/bar")).status, equalTo(Status.NOT_FOUND))
+        assertThat(app(Request(Method.GET, "/foo")).status, equalTo(Status.OK))
+        assertThat(app(Request(Method.PUT, "/foo")).status, equalTo(Status.METHOD_NOT_ALLOWED))
+    }
 }
+
+private fun newRoutes(routes: Pair<String, HttpHandler>): HttpHandler = RoutedHttpHandler(
+    TemplatedHttpHandler(UriTemplate.from(routes.first), routes.second)
+)
+
+private fun newRoutes(routes: Triple<String, Method, HttpHandler>): HttpHandler = RoutedHttpHandler(
+    TemplatedHttpHandler(UriTemplate.from(routes.first), routes.third, Specific(routes.second))
+)
+
 
 class RoutedHttpHandler(private val templatedHandler: TemplatedHttpHandler) : HttpHandler {
     override fun invoke(request: Request) = templatedHandler.match(request).toHandler()(request)
-
 }
 
-class TemplatedHttpHandler(private val uriTemplate: UriTemplate, private val handler: HttpHandler) {
-    fun match(request: Request): RoutingMatchResult {
+class TemplatedHttpHandler(
+    private val uriTemplate: UriTemplate,
+    private val handler: HttpHandler,
+    private val method: MethodConstraint = Any
+) {
+    fun match(request: Request): RoutingMatchResult =
         if (uriTemplate.matches(request.uri.path)) {
-            return RoutingMatchResult.Matched(handler)
-        }
-        return RoutingMatchResult.NotFound
-    }
+            if (!method.matches(request))
+                RoutingMatchResult.MethodNotMatched
+            else
+                RoutingMatchResult.Matched(handler)
+        } else
+            RoutingMatchResult.NotFound
+}
+
+sealed class MethodConstraint {
+    object Any : MethodConstraint()
+    data class Specific(val method: Method) : MethodConstraint()
+}
+
+fun MethodConstraint.matches(request: Request): Boolean = when (this) {
+    is Any -> true
+    is Specific -> request.method == method
 }
 
 sealed class RoutingMatchResult {
@@ -46,8 +77,13 @@ sealed class RoutingMatchResult {
     object NotFound : RoutingMatchResult()
 }
 
-fun RoutingMatchResult.toHandler() = when(this){
-     is RoutingMatchResult.Matched -> handler
-     is RoutingMatchResult.MethodNotMatched -> routeMethodNotAllowedHandler
-     is RoutingMatchResult.NotFound -> routeNotFoundHandler
- }
+fun RoutingMatchResult.toHandler() = when (this) {
+    is RoutingMatchResult.Matched -> handler
+    is RoutingMatchResult.MethodNotMatched -> routeMethodNotAllowedHandler
+    is RoutingMatchResult.NotFound -> routeNotFoundHandler
+}
+
+infix fun String.newBind(pair: Method): Pair<String, Method> = Pair(this, pair)
+
+infix fun Pair<String, Method>.to(handler: HttpHandler): Triple<String, Method, HttpHandler> =
+    Triple(this.first, this.second, handler)
