33af9b86eead8a7ed2b7f4146b52d0a4b521c74f
David Denton <denton.david@gmail.com>
2024-12-01 12:46:08 +0000

    Undo making ws autoclosable as it clashes with the existing API

--- core/realtime-core/src/main/kotlin/org/http4k/websocket/websocket.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/websocket/websocket.kt
@@ -12,13 +12,12 @@ import java.nio.ByteBuffer
  * Represents a connected Websocket instance, and can be passed around an application. This is configured
  * to react to events on the WS event stream by attaching listeners.
  */
-interface Websocket : AutoCloseable {
+interface Websocket {
     fun send(message: WsMessage)
-    fun close(status: WsStatus)
+    fun close(status: WsStatus = NORMAL)
     fun onError(fn: (Throwable) -> Unit)
     fun onClose(fn: (WsStatus) -> Unit)
     fun onMessage(fn: (WsMessage) -> Unit)
-    override fun close() = close(NORMAL)
 }
 
 data class WsResponse(val subprotocol: String? = null, val consumer: WsConsumer) : WsConsumer by consumer {
