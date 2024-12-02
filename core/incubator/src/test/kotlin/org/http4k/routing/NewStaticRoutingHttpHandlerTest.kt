4f3dcff18230556fa243ae1e637064c16db03ed2
David Denton <denton.david@gmail.com>
2024-12-02 13:29:00 +0000

    Unrename test

--- core/incubator/src/test/kotlin/org/http4k/routing/NewStaticRoutingHttpHandlerTest.kt
+++ core/incubator/src/test/kotlin/org/http4k/routing/NewStaticRoutingHttpHandlerTest.kt
@@ -3,7 +3,7 @@ package org.http4k.routing
 import org.http4k.routing.experimental.ResourceLoaders.Classpath
 import org.http4k.routing.experimental.StaticRoutingHttpHandler
 
-class NewNewStaticRoutingHttpHandlerTest : StaticRoutingHttpHandlerTest() {
+class NewStaticRoutingHttpHandlerTest : StaticRoutingHttpHandlerTest() {
     override val handler: RoutingHttpHandler = StaticRoutingHttpHandler(
         pathSegments = validPath,
         resourceLoader = Classpath()
