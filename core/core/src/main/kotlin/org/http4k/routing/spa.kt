050d12592a821b249b77516dd9fd35be7123255a
David Denton <denton.david@gmail.com>
2024-12-02 21:38:06 +0000

    Reimplemented SPA as a similar router to static, but falling back to the path segments if required

--- core/core/src/main/kotlin/org/http4k/routing/spa.kt
+++ core/core/src/main/kotlin/org/http4k/routing/spa.kt
@@ -3,8 +3,13 @@ package org.http4k.routing
 import org.http4k.core.ContentType
 import org.http4k.core.Filter
 import org.http4k.core.Method.GET
+import org.http4k.core.NoOp
 import org.http4k.core.Request
-import org.http4k.core.Status
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.NOT_FOUND
+import org.http4k.core.then
+import org.http4k.routing.PredicateResult.Matched
+import org.http4k.routing.PredicateResult.NotMatched
 
 /**
  * For SPAs we serve static content as usual, or fall back to the index page. The resource loader is configured to look at
@@ -15,32 +20,35 @@ fun singlePageApp(
     vararg extraFileExtensionToContentTypes: Pair<String, ContentType>
 ): RoutingHttpHandler =
     RoutingHttpHandler(
-        listOf(
-            SinglePageAppRouteMatcher(
-                "",
-                StaticRouteMatcher("", resourceLoader, extraFileExtensionToContentTypes.asList().toMap())
-            )
-        )
+        listOf(SinglePageAppRouteMatcher("", resourceLoader, extraFileExtensionToContentTypes.asList().toMap()))
     )
 
 internal data class SinglePageAppRouteMatcher(
     private val pathSegments: String,
-    private val staticMatcher: StaticRouteMatcher,
+    private val resourceLoader: ResourceLoader,
+    private val extraFileExtensionToContentTypes: Map<String, ContentType>,
+    private val predicate: Predicate = All,
+    private val filter: Filter = Filter.NoOp
 ) : RouteMatcher {
 
-    override fun match(request: Request): HttpMatchResult {
-        val staticMatch = staticMatcher.match(request)
-        return when (staticMatch.handler(request).status) {
-            Status.NOT_FOUND -> staticMatcher.match(Request(GET, pathSegments))
-            else -> staticMatch
+    private val handler = ResourceLoadingHandler(pathSegments, resourceLoader, extraFileExtensionToContentTypes)
+
+    override fun match(request: Request): HttpMatchResult = when (predicate(request)) {
+        is Matched -> {
+            handler(request).let {
+                when {
+                    it.status != NOT_FOUND -> HttpMatchResult(0, filter.then { _: Request -> it })
+                    else -> HttpMatchResult(2, filter.then { _: Request -> handler(Request(GET, pathSegments)) })
+                }
+            }
         }
+
+        is NotMatched -> HttpMatchResult(2, filter.then { _: Request -> Response(NOT_FOUND) })
     }
 
-    override fun withBasePath(prefix: String) =
-        SinglePageAppRouteMatcher(prefix + pathSegments, staticMatcher.withBasePath(prefix) as StaticRouteMatcher)
-
-    override fun withPredicate(other: Predicate): RouteMatcher = this
-    override fun withFilter(new: Filter): RouteMatcher = staticMatcher.withFilter(new)
+    override fun withBasePath(prefix: String): RouteMatcher = copy(pathSegments = prefix + pathSegments)
+    override fun withFilter(new: Filter): RouteMatcher = copy(filter = new.then(filter))
+    override fun withPredicate(other: Predicate): RouteMatcher = copy(predicate = predicate.and(other))
 
     override fun toString() = "SPA at $pathSegments"
 
