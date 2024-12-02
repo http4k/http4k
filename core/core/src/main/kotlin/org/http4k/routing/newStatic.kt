78546c513059261e7b4d5756c9c98e73d123c1a6
Ivan Sanchez <ivan@gourame.com>
2024-12-02 09:36:59 +0000

    Duplicate impl

--- core/core/src/main/kotlin/org/http4k/routing/newStatic.kt
+++ core/core/src/main/kotlin/org/http4k/routing/newStatic.kt
@@ -21,12 +21,12 @@ import org.http4k.routing.ResourceLoader.Companion.Classpath
  * Serve static content using the passed ResourceLoader. Note that for security, by default ONLY mime-types registered in
  * mime.types (resource file) will be served. All other types are registered as application/octet-stream and are not served.
  */
-fun static(
+fun newStatic(
     resourceLoader: ResourceLoader = Classpath(),
     vararg extraFileExtensionToContentTypes: Pair<String, ContentType>
 ): RoutingHttpHandler = NewStaticRoutingHttpHandler("", resourceLoader, extraFileExtensionToContentTypes.asList().toMap())
 
-data class StaticRoutingHttpHandler(
+data class NewStaticRoutingHttpHandler(
     private val pathSegments: String,
     private val resourceLoader: ResourceLoader,
     private val extraFileExtensionToContentTypes: Map<String, ContentType>,
@@ -49,45 +49,4 @@ data class StaticRoutingHttpHandler(
     override fun invoke(request: Request): Response = handlerWithFilter(request)
 
     override fun toString() = description.friendlyToString()
-}
-
-internal class ResourceLoadingHandler(
-    private val pathSegments: String,
-    private val resourceLoader: ResourceLoader,
-    extraFileExtensionToContentTypes: Map<String, ContentType>
-) : HttpHandler {
-    private val extMap = MimeTypes(extraFileExtensionToContentTypes)
-
-    override fun invoke(p1: Request): Response = if (isStartingWithPathSegment(p1) && p1.method == GET) {
-        load(convertPath(p1.uri.path))
-    } else Response(NOT_FOUND)
-
-    private fun load(path: String): Response =
-        loadPath(path)
-            ?: loadPath(pathWithIndex(path))
-            ?: Response(NOT_FOUND)
-
-    private fun loadPath(path: String): Response? =
-        resourceLoader.load(path)?.let { url ->
-            val lookupType = extMap.forFile(path)
-            if (lookupType != OCTET_STREAM) {
-                Response(OK)
-                    .with(CONTENT_TYPE of lookupType)
-                    .body(Body(url.openStream()))
-            } else null
-        }
-
-    private fun pathWithIndex(path: String): String {
-        val newPath = if (path.endsWith("/")) "${path}index.html" else "$path/index.html"
-        return newPath.trimStart('/')
-    }
-
-    private fun convertPath(path: String): String {
-        val newPath = if (pathSegments == "/" || pathSegments == "") path else path.replaceFirst(pathSegments, "")
-        val resolved = if (newPath == "/" || newPath.isBlank()) "/index.html" else newPath
-        return resolved.trimStart('/')
-    }
-
-    private fun isStartingWithPathSegment(p1: Request) =
-        (p1.uri.path.startsWith(pathSegments) || p1.uri.path.startsWith("/$pathSegments"))
 }
