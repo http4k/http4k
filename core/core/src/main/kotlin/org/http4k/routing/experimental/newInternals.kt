0bcaf5b09c6a48d49a68705160074581d328b663
Ivan Sanchez <ivan@gourame.com>
2024-12-02 10:51:32 +0000

    Implement newStatic()

--- core/core/src/main/kotlin/org/http4k/routing/experimental/newInternals.kt
+++ core/core/src/main/kotlin/org/http4k/routing/experimental/newInternals.kt
@@ -15,45 +15,53 @@ import org.http4k.routing.experimental.PredicateResult.Matched
 import org.http4k.routing.experimental.PredicateResult.NotMatched
 
 data class RoutedHttpHandler(
-    val routes: List<TemplatedHttpRoute>,
-    private val filter: Filter = Filter.NoOp
+    val routes: List<NewRouteMatcher>
 ) : HttpHandler {
-    override fun invoke(request: Request) = filter.then(routes
+    override fun invoke(request: Request) = routes
         .map { it.match(request) }
         .sortedBy(HttpMatchResult::priority)
-        .first().handler)(request)
+        .first().handler(request)
 
     fun withBasePath(prefix: String) = copy(routes = routes.map { it.withBasePath(prefix) })
 
-    fun withFilter(filter: Filter) = copy(filter = filter.then(this.filter))
+    fun withFilter(filter: Filter) = copy(routes = routes.map { it.withFilter(filter) })
 
     fun withPredicate(predicate: Predicate) =
         copy(routes = routes.map { it.withPredicate(predicate) })
 
-    override fun toString() = routes.sortedBy(TemplatedHttpRoute::toString).joinToString("\n")
+    override fun toString() = routes.sortedBy(NewRouteMatcher::toString).joinToString("\n")
+}
+
+interface NewRouteMatcher {
+    fun match(request: Request): HttpMatchResult
+    fun withBasePath(prefix: String): NewRouteMatcher
+    fun withPredicate(other: Predicate): NewRouteMatcher
+    fun withFilter(new: Filter): NewRouteMatcher
 }
 
 data class TemplatedHttpRoute(
     private val uriTemplate: UriTemplate,
     private val handler: HttpHandler,
-    private val predicate: Predicate = Any
-) {
+    private val predicate: Predicate = Any,
+    private val filter: Filter = Filter.NoOp
+) : NewRouteMatcher {
     init {
         require(handler !is RoutedHttpHandler)
     }
 
-    internal fun match(request: Request) = when {
+    override fun match(request: Request) = when {
         uriTemplate.matches(request.uri.path) -> when (val result = predicate(request)) {
-            is Matched -> HttpMatchResult(0, AddUriTemplate(uriTemplate).then(handler))
-            is NotMatched -> HttpMatchResult(1) { _: Request -> Response(result.status) }
+            is Matched -> HttpMatchResult(0, filter.then(AddUriTemplate(uriTemplate).then(handler)))
+            is NotMatched -> HttpMatchResult(1, filter.then { _: Request -> Response(result.status) })
         }
 
-        else -> HttpMatchResult(2) { _: Request -> Response(NOT_FOUND) }
+        else -> HttpMatchResult(2,  filter.then { _: Request -> Response(NOT_FOUND) })
     }
 
-    fun withBasePath(prefix: String) = copy(uriTemplate = UriTemplate.from("$prefix/${uriTemplate}"))
+    override fun withBasePath(prefix: String) = copy(uriTemplate = UriTemplate.from("$prefix/${uriTemplate}"))
 
-    fun withPredicate(other: Predicate) = copy(predicate = predicate.and(other))
+    override fun withPredicate(other: Predicate) = copy(predicate = predicate.and(other))
+    override fun withFilter(new: Filter): NewRouteMatcher = copy(filter = new.then(this.filter))
 
     override fun toString() = "template=$uriTemplate AND ${predicate.description}"
 
