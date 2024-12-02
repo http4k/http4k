62ada80a418103e21fbe109f912a7d229a774133
David Denton <denton.david@gmail.com>
2024-12-02 23:12:32 +0000

    Moving legacy routing to the contract module which is the only place it's still used

--- core/contract/src/test/kotlin/org/http4k/contract/ContractRouteTest.kt
+++ core/contract/src/test/kotlin/org/http4k/contract/ContractRouteTest.kt
@@ -3,6 +3,8 @@ package org.http4k.contract
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import com.natpryce.hamkrest.throws
+import org.http4k.contract.RouterMatch.MatchingHandler
+import org.http4k.contract.RouterMatch.Unmatched
 import org.http4k.contract.security.ApiKeySecurity
 import org.http4k.core.Body
 import org.http4k.core.ContentType
@@ -25,9 +27,6 @@ import org.http4k.lens.Path
 import org.http4k.lens.Query
 import org.http4k.lens.int
 import org.http4k.lens.string
-import org.http4k.routing.RouterMatch
-import org.http4k.routing.RouterMatch.MatchingHandler
-import org.http4k.routing.RouterMatch.Unmatched
 import org.junit.jupiter.api.Test
 
 class ContractRouteTest {
@@ -43,7 +42,10 @@ class ContractRouteTest {
         val route = pair to { _, _ -> { Response(OK) } }
         val request = route.newRequest(Uri.of("http://rita.com"))
 
-        assertThat(request.with(path1 of 123, path2 of "hello world"), equalTo(Request(GET, "http://rita.com/123/hello%20world")))
+        assertThat(
+            request.with(path1 of 123, path2 of "hello world"),
+            equalTo(Request(GET, "http://rita.com/123/hello%20world"))
+        )
     }
 
     @Test
@@ -64,7 +66,10 @@ class ContractRouteTest {
             queries += Query.required("")
         } bindContract GET).newRequest(Uri.of("http://rita.com/base"))
 
-        assertThat(request.with(path1 of 123, path2 of "hello world"), equalTo(Request(GET, "http://rita.com/123/hello%20world")))
+        assertThat(
+            request.with(path1 of 123, path2 of "hello world"),
+            equalTo(Request(GET, "http://rita.com/123/hello%20world"))
+        )
     }
 
     @Test
@@ -84,7 +89,10 @@ class ContractRouteTest {
         val route = "/" bindContract GET to handler
         "/" bindContract GET to { -> handler }
         val router = route.toRouter(Root)
-        assertThat(router.match(Request(GET, "/")), equalTo(MatchingHandler(handler, router.description) as RouterMatch))
+        assertThat(
+            router.match(Request(GET, "/")),
+            equalTo(MatchingHandler(handler, router.description) as RouterMatch)
+        )
         assertThat(router.match(Request(POST, "/")), equalTo(Unmatched(router.description) as RouterMatch))
         assertThat(router.match(Request(GET, "/bob")), equalTo(Unmatched(router.description) as RouterMatch))
     }
@@ -99,12 +107,34 @@ class ContractRouteTest {
 
         assertRequest("/" bindContract GET to handler, "http://foo.com")
         assertRequest(Path.of("value") bindContract GET to { handler }, "http://foo.com/{value}")
-        assertRequest(Path.of("value") / Path.of("value2") bindContract GET to { _, _ -> handler }, "http://foo.com/{value}/{value2}")
-        assertRequest(Path.of("value") / Path.of("value2") / Path.of("value3") bindContract GET to { _, _, _ -> handler }, "http://foo.com/{value}/{value2}/{value3}")
-        assertRequest(Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") bindContract GET to { _, _, _, _ -> handler }, "http://foo.com/{value}/{value2}/{value3}/{value4}")
-        assertRequest(Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") / Path.of("value5") bindContract GET to { _, _, _, _, _ -> handler }, "http://foo.com/{value}/{value2}/{value3}/{value4}/{value5}")
-        assertRequest(Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") / Path.of("value5") / Path.of("value6") bindContract GET to { _, _, _, _, _, _ -> handler }, "http://foo.com/{value}/{value2}/{value3}/{value4}/{value5}/{value6}")
-        assertRequest(Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") / Path.of("value5") / Path.of("value6") / Path.of("value7") bindContract GET to { _, _, _, _, _, _, _ -> handler }, "http://foo.com/{value}/{value2}/{value3}/{value4}/{value5}/{value6}/{value7}")
+        assertRequest(
+            Path.of("value") / Path.of("value2") bindContract GET to { _, _ -> handler },
+            "http://foo.com/{value}/{value2}"
+        )
+        assertRequest(
+            Path.of("value") / Path.of("value2") / Path.of("value3") bindContract GET to { _, _, _ -> handler },
+            "http://foo.com/{value}/{value2}/{value3}"
+        )
+        assertRequest(
+            Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") bindContract GET to { _, _, _, _ -> handler },
+            "http://foo.com/{value}/{value2}/{value3}/{value4}"
+        )
+        assertRequest(
+            Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") / Path.of("value5") bindContract GET to { _, _, _, _, _ -> handler },
+            "http://foo.com/{value}/{value2}/{value3}/{value4}/{value5}"
+        )
+        assertRequest(
+            Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") / Path.of("value5") / Path.of(
+                "value6"
+            ) bindContract GET to { _, _, _, _, _, _ -> handler },
+            "http://foo.com/{value}/{value2}/{value3}/{value4}/{value5}/{value6}"
+        )
+        assertRequest(
+            Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") / Path.of("value5") / Path.of(
+                "value6"
+            ) / Path.of("value7") bindContract GET to { _, _, _, _, _, _, _ -> handler },
+            "http://foo.com/{value}/{value2}/{value3}/{value4}/{value5}/{value6}/{value7}"
+        )
     }
 
     @Test
@@ -145,19 +175,25 @@ class ContractRouteTest {
     fun `2 parts - matches route`() {
         fun matched(value1: String, value2: String) = { _: Request -> Response(OK).body(value1 + value2) }
 
-        checkMatching(Path.of("value") / "value2" meta {} bindContract GET to ::matched, "/value1/value2", "value1value2")
+        checkMatching(Path.of("value") / "value2" meta {} bindContract GET to ::matched,
+            "/value1/value2",
+            "value1value2")
     }
 
     @Test
     fun `3 parts - matches route`() {
-        fun matched(value1: String, value2: String, value3: String) = { _: Request -> Response(OK).body(value1 + value2 + value3) }
+        fun matched(value1: String, value2: String, value3: String) =
+            { _: Request -> Response(OK).body(value1 + value2 + value3) }
 
-        checkMatching(Path.of("value") / Path.of("value2") / "value3" meta {} bindContract GET to ::matched, "/value1/value2/value3", "value1value2value3")
+        checkMatching(Path.of("value") / Path.of("value2") / "value3" meta {} bindContract GET to ::matched,
+            "/value1/value2/value3",
+            "value1value2value3")
     }
 
     @Test
     fun `4 parts - matches route`() {
-        fun matched(value1: String, value2: String, value3: String, value4: String) = { _: Request -> Response(OK).body(value1 + value2 + value3 + value4) }
+        fun matched(value1: String, value2: String, value3: String, value4: String) =
+            { _: Request -> Response(OK).body(value1 + value2 + value3 + value4) }
 
         checkMatching(Path.of("value") / Path.of("value2") / Path.of("value3") / "value4" meta {}
             bindContract GET to ::matched, "/value1/value2/value3/value4", "value1value2value3value4")
@@ -242,7 +278,8 @@ class ContractRouteTest {
         checkMatching(Path.of("value") / Path.of("value2") / Path.of("value3") / Path.of("value4") /
             Path.of("value5") / Path.of("value6") / Path.of("value7") / Path.of("value8") /
             Path.of("value9") / "value10" meta {}
-            bindContract GET to ::matched, "/value1/value2/value3/value4/value5/value6/value7/value8/value9/value10",
+            bindContract GET to ::matched,
+            "/value1/value2/value3/value4/value5/value6/value7/value8/value9/value10",
             "value1value2value3value4value5value6value7value8value9value10")
     }
 
@@ -295,14 +332,32 @@ class ContractRouteTest {
         assertThat(route(Request(DELETE, valid)), hasStatus(NOT_FOUND))
 
         val routerOnNoPrefix = route.toRouter(Root)
-        assertThat(routerOnNoPrefix.match(Request(GET, "")), equalTo(Unmatched(routerOnNoPrefix.description) as RouterMatch))
-        assertThat(routerOnNoPrefix.match(Request(POST, valid)), equalTo(Unmatched(routerOnNoPrefix.description) as RouterMatch))
-        assertThat(routerOnNoPrefix.match(Request(GET, valid)).matchOrNull()?.invoke(Request(GET, valid))?.bodyString(), equalTo(expected))
+        assertThat(
+            routerOnNoPrefix.match(Request(GET, "")),
+            equalTo(Unmatched(routerOnNoPrefix.description) as RouterMatch)
+        )
+        assertThat(
+            routerOnNoPrefix.match(Request(POST, valid)),
+            equalTo(Unmatched(routerOnNoPrefix.description) as RouterMatch)
+        )
+        assertThat(
+            routerOnNoPrefix.match(Request(GET, valid)).matchOrNull()?.invoke(Request(GET, valid))?.bodyString(),
+            equalTo(expected)
+        )
 
         val routerOnPrefix = route.toRouter(Root / "somePrefix")
-        assertThat(routerOnPrefix.match(Request(GET, "/somePrefix")), equalTo(Unmatched(routerOnPrefix.description) as RouterMatch))
-        assertThat(routerOnPrefix.match(Request(POST, "/somePrefix/$valid")), equalTo(Unmatched(routerOnPrefix.description) as RouterMatch))
-        assertThat(routerOnPrefix.match(Request(GET, "/somePrefix/$valid")).matchOrNull()?.invoke(Request(GET, valid))?.bodyString(), equalTo(expected))
+        assertThat(
+            routerOnPrefix.match(Request(GET, "/somePrefix")),
+            equalTo(Unmatched(routerOnPrefix.description) as RouterMatch)
+        )
+        assertThat(
+            routerOnPrefix.match(Request(POST, "/somePrefix/$valid")),
+            equalTo(Unmatched(routerOnPrefix.description) as RouterMatch)
+        )
+        assertThat(
+            routerOnPrefix.match(Request(GET, "/somePrefix/$valid")).matchOrNull()?.invoke(Request(GET, valid))
+                ?.bodyString(), equalTo(expected)
+        )
     }
 
     private fun RouterMatch.matchOrNull(): HttpHandler? = when (this) {
