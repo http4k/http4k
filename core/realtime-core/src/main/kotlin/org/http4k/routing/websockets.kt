63e653cabb7721445405b17d495eb594f2935d59
Ivan Sanchez <ivan@gourame.com>
2024-12-02 16:12:12 +0000

    Remove ambiguous definition

--- core/realtime-core/src/main/kotlin/org/http4k/routing/websockets.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/routing/websockets.kt
@@ -1,14 +1,9 @@
 package org.http4k.routing
 
-import org.http4k.core.Method
 import org.http4k.routing.ws.RoutingWsHandler
-import org.http4k.routing.ws.bind
 import org.http4k.websocket.WsConsumer
 import org.http4k.websocket.WsHandler
 import org.http4k.websocket.WsResponse
-
-fun websockets(vararg list: Pair<Method, WsHandler>) =
-    websockets(*list.map { "" bind it.first to it.second }.toTypedArray())
 
 fun websockets(vararg list: RoutingWsHandler) = websockets(list.toList())
 
