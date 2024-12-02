c92ab6c4ba7ae4cfc395904ac96be74d37152774
David Denton <denton.david@gmail.com>
2024-12-02 22:04:49 +0000

    Fix reverse proxy impleemntation

--- connect/amazon/iamidentitycenter/fake/src/test/kotlin/org/http4k/connect/amazon/iamidentitycenter/FakeIAMIdentityCenterTest.kt
+++ connect/amazon/iamidentitycenter/fake/src/test/kotlin/org/http4k/connect/amazon/iamidentitycenter/FakeIAMIdentityCenterTest.kt
@@ -5,6 +5,7 @@ import org.http4k.core.HttpHandler
 import org.http4k.routing.reverseProxy
 
 class FakeIAMIdentityCenterTest : IAMIdentityCenterContract, FakeAwsContract {
+
     override val http: HttpHandler = reverseProxy(
         "sso" to FakeSSO(),
         "oidc" to FakeOIDC()
