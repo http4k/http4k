777129a7640398696a89ce0bd773b69940169274
Ivan Sanchez <ivan@gourame.com>
2024-12-02 10:57:07 +0000

    delete unused

--- core/core/src/main/kotlin/org/http4k/routing/newStatic.kt
+++ core/core/src/main/kotlin/org/http4k/routing/newStatic.kt
@@ -1,20 +1,9 @@
 package org.http4k.routing
 
-import org.http4k.core.Body
 import org.http4k.core.ContentType
-import org.http4k.core.ContentType.Companion.OCTET_STREAM
-import org.http4k.core.Filter
-import org.http4k.core.HttpHandler
-import org.http4k.core.Method.GET
-import org.http4k.core.MimeTypes
-import org.http4k.core.NoOp
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.NOT_FOUND
-import org.http4k.core.Status.Companion.OK
-import org.http4k.core.then
-import org.http4k.core.with
-import org.http4k.lens.Header.CONTENT_TYPE
 import org.http4k.routing.ResourceLoader.Companion.Classpath
 import org.http4k.routing.experimental.HttpMatchResult
 import org.http4k.routing.experimental.NewRouteMatcher
@@ -49,29 +38,4 @@ data class NewStaticRouteMatcher(
     override fun withBasePath(prefix: String): NewRouteMatcher = copy(pathSegments = prefix + pathSegments)
 
     override fun withPredicate(other: Predicate): NewRouteMatcher = this
-}
-
-data class NewStaticRoutingHttpHandler(
-    private val pathSegments: String,
-    private val resourceLoader: ResourceLoader,
-    private val extraFileExtensionToContentTypes: Map<String, ContentType>,
-    private val filter: Filter = Filter.NoOp
-) : RoutingHttpHandler {
-
-    override val description = RouterDescription("Static files $pathSegments")
-
-    override fun withFilter(new: Filter): RoutingHttpHandler = copy(filter = new.then(filter))
-
-    override fun withBasePath(new: String): RoutingHttpHandler = copy(pathSegments = new + pathSegments)
-
-    private val handlerNoFilter = ResourceLoadingHandler(pathSegments, resourceLoader, extraFileExtensionToContentTypes)
-    private val handlerWithFilter = filter.then(handlerNoFilter)
-
-    override fun match(request: Request): RouterMatch = handlerNoFilter(request).let {
-        if (it.status != NOT_FOUND) RouterMatch.MatchingHandler(filter.then { _: Request -> it }, description) else null
-    } ?: RouterMatch.Unmatched(description)
-
-    override fun invoke(request: Request): Response = handlerWithFilter(request)
-
-    override fun toString() = description.friendlyToString()
 }
