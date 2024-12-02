a9c3199490ea4dbe9049cad540cd657764eeece4
David Denton <denton.david@gmail.com>
2024-12-02 23:12:32 +0000

    Moving legacy routing to the contract module which is the only place it's still used

--- core/core/src/main/kotlin/org/http4k/routing/RouterDescription.kt
+++ core/core/src/main/kotlin/org/http4k/routing/RouterDescription.kt
@@ -1,15 +1,6 @@
 package org.http4k.routing
 
-import org.http4k.core.Request
-
-interface Router {
-    fun match(request: Request): RouterMatch
-
-    val description: RouterDescription get() = RouterDescription.unavailable
-}
-
 data class RouterDescription(val description: String, val children: List<RouterDescription> = listOf()) {
-
     override fun toString(): String = friendlyToString()
 
     companion object {
