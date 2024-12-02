31cdcd1a84b5b726269ecb03d85025696ce3e75e
David Denton <denton.david@gmail.com>
2024-12-02 23:01:46 +0000

    Ignore production of 405

--- core/incubator/src/main/kotlin/org/http4k/routing/experimental/StaticRoutingHttpHandler.kt
+++ core/incubator/src/main/kotlin/org/http4k/routing/experimental/StaticRoutingHttpHandler.kt
@@ -1,70 +1,49 @@
 package org.http4k.routing.experimental
 
+import org.http4k.core.ContentType
 import org.http4k.core.Filter
-import org.http4k.core.HttpHandler
-import org.http4k.core.Method.GET
 import org.http4k.core.NoOp
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status.Companion.METHOD_NOT_ALLOWED
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Uri.Companion.of
 import org.http4k.core.then
-import org.http4k.routing.Router
-import org.http4k.routing.RouterDescription
-import org.http4k.routing.RouterMatch
-import org.http4k.routing.RouterMatch.MatchedWithoutHandler
-import org.http4k.routing.RouterMatch.MatchingHandler
-import org.http4k.routing.RouterMatch.MethodNotMatched
-import org.http4k.routing.RouterMatch.Unmatched
+import org.http4k.routing.All
+import org.http4k.routing.HttpMatchResult
+import org.http4k.routing.Predicate
+import org.http4k.routing.PredicateResult.Matched
+import org.http4k.routing.PredicateResult.NotMatched
+import org.http4k.routing.RouteMatcher
 import org.http4k.routing.RoutingHttpHandler
-import org.http4k.routing.friendlyToString
+import org.http4k.routing.and
 
-fun static(resourceLoader: Router): RoutingHttpHandler = StaticRoutingHttpHandler("", resourceLoader)
+fun static(
+    resourceLoader: RouteMatcher,
+    vararg extraFileExtensionToContentTypes: Pair<String, ContentType>
+) = RoutingHttpHandler(
+    listOf(
+        NewStaticRouteMatcher("", resourceLoader, extraFileExtensionToContentTypes.asList().toMap())
+    )
+)
 
-internal data class StaticRoutingHttpHandler(
+data class NewStaticRouteMatcher(
     private val pathSegments: String,
-    private val resourceLoader: Router,
+    private val resourceLoader: RouteMatcher,
+    private val extraFileExtensionToContentTypes: Map<String, ContentType>,
+    private val predicate: Predicate = All,
     private val filter: Filter = Filter.NoOp
-) : RoutingHttpHandler {
-
-    override val description = RouterDescription(pathSegments)
-
-    override fun withFilter(new: Filter): RoutingHttpHandler = copy(filter = new.then(filter))
-
-    override fun withBasePath(new: String): RoutingHttpHandler = copy(pathSegments = new + pathSegments)
-
-    private val handlerNoFilter = ResourceLoadingHandler(pathSegments, resourceLoader)
-    private val handlerWithFilter = filter.then(handlerNoFilter)
-
-    override fun match(request: Request): RouterMatch = handlerNoFilter(request).let {
-        if (it.status != NOT_FOUND)
-            MatchingHandler(filter.then { _: Request -> it }, description)
-        else
-            Unmatched(description)
+) : RouteMatcher {
+    override fun match(request: Request): HttpMatchResult = when (predicate(request)) {
+        is Matched -> resourceLoader.match(request.uri(of(convertPath(request.uri.path))))
+        is NotMatched -> HttpMatchResult(2, filter.then { _: Request -> Response(NOT_FOUND) })
     }
 
-    override fun toString() = description.friendlyToString()
-
-    override fun invoke(request: Request): Response = handlerWithFilter(request)
-}
-
-internal class ResourceLoadingHandler(
-    private val pathSegments: String,
-    private val resourceLoader: Router
-) : HttpHandler {
-
-    override fun invoke(request: Request): Response =
-        if (request.method == GET && request.uri.path.startsWith(pathSegments))
-            when (val matchResult = resourceLoader.match(request.uri(of(convertPath(request.uri.path))))) {
-                is MatchingHandler -> matchResult(request)
-                is MethodNotMatched -> Response(METHOD_NOT_ALLOWED)
-                is Unmatched -> Response(NOT_FOUND)
-                is MatchedWithoutHandler -> Response(NOT_FOUND)
-            }
-        else
-            Response(NOT_FOUND)
+    override fun withBasePath(prefix: String): RouteMatcher = copy(pathSegments = prefix + pathSegments)
+    override fun withFilter(new: Filter): RouteMatcher = copy(filter = new.then(filter))
+    override fun withPredicate(other: Predicate): RouteMatcher = copy(predicate = predicate.and(other))
 
     private fun convertPath(path: String) =
         if (pathSegments == "/" || pathSegments == "") path else path.replace(pathSegments, "")
+
+    override fun toString() = "Static files $pathSegments"
 }
--- core/incubator/src/test/kotlin/org/http4k/routing/experimental/ResourceLoadingHandlerTest.kt
+++ core/incubator/src/test/kotlin/org/http4k/routing/experimental/ResourceLoadingHandlerTest.kt
@@ -8,7 +8,7 @@ import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.apache.hc.core5.http.io.entity.EmptyInputStream
 import org.http4k.core.ContentType.Companion.TEXT_PLAIN
-import org.http4k.core.MemoryRequest
+import org.http4k.core.Filter
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
 import org.http4k.core.Response
@@ -21,25 +21,28 @@ import org.http4k.hamkrest.hasBody
 import org.http4k.hamkrest.hasContentType
 import org.http4k.hamkrest.hasHeader
 import org.http4k.hamkrest.hasStatus
-import org.http4k.routing.Router
-import org.http4k.routing.RouterMatch
+import org.http4k.routing.HttpMatchResult
+import org.http4k.routing.Predicate
+import org.http4k.routing.RouteMatcher
+import org.http4k.routing.bind
 import org.junit.jupiter.api.Test
 import java.time.Instant
 
 class NewResourceLoadingHandlerTest {
 
     private val resources = HashMap<String, Resource>()
-    private val handler = ResourceLoadingHandler("/root", InMemoryResourceLoader(resources))
+    private val handler = "/root" bind static(InMemoryResourceLoader(resources))
     private val now = Instant.parse("2018-08-09T23:06:00Z")
 
     @Test
     fun `no resource returns NOT_FOUND`() {
-        assertThat(handler(MemoryRequest(GET, Uri.of("/root/nosuch"))), equalTo(Response(NOT_FOUND)))
+        assertThat(handler(Request(GET, Uri.of("/root/nosuch"))), equalTo(Response(NOT_FOUND)))
     }
 
     @Test
     fun `returns content, content type, length and body`() {
-        resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN, lastModified = now, etag = ETag("etag-value", weak = true))
+        resources["/file.txt"] =
+            InMemoryResource("content", TEXT_PLAIN, lastModified = now, etag = ETag("etag-value", weak = true))
         assertThat(
             handler(Request(GET, Uri.of("/root/file.txt"))),
             allOf(
@@ -49,7 +52,8 @@ class NewResourceLoadingHandlerTest {
                 hasHeader("Last-Modified", "Thu, 9 Aug 2018 23:06:00 GMT"),
                 hasHeader("ETag", """W/"etag-value""""),
                 hasBody("content")
-            ))
+            )
+        )
     }
 
     @Test
@@ -62,121 +66,172 @@ class NewResourceLoadingHandlerTest {
                 hasHeader("Content-Length", absent()),
                 hasHeader("Last-Modified", absent()),
                 hasHeader("ETag", absent())
-            ))
+            )
+        )
     }
 
     @Test
     fun `returns content if resource is modified by time`() {
         resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN, lastModified = now)
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:05:59 GMT"))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:05:59 GMT")
+                )
+            ),
             allOf(
                 hasStatus(OK),
                 hasHeader("Last-Modified", "Thu, 9 Aug 2018 23:06:00 GMT"),
                 hasBody("content")
-            ))
+            )
+        )
     }
 
     @Test
     fun `returns NOT_MODIFIED if resource is not modified by time`() {
         resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN, lastModified = now)
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:06:00 GMT"))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:06:00 GMT")
+                )
+            ),
             allOf(
                 hasStatus(NOT_MODIFIED),
                 hasHeader("Last-Modified", "Thu, 9 Aug 2018 23:06:00 GMT"),
                 hasBody("")
-            ))
+            )
+        )
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:06:01 GMT"))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:06:01 GMT")
+                )
+            ),
             allOf(
                 hasStatus(NOT_MODIFIED),
                 hasHeader("Last-Modified", "Thu, 9 Aug 2018 23:06:00 GMT"),
                 hasBody("")
-            ))
+            )
+        )
     }
 
     @Test
     fun `returns content if no last modified property`() {
         resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN, lastModified = null)
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:05:59 GMT"))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-Modified-Since" to "Thu, 9 Aug 2018 23:05:59 GMT")
+                )
+            ),
             allOf(
                 hasStatus(OK),
                 hasHeader("Last-Modified", absent()),
                 hasBody("content")
-            ))
+            )
+        )
     }
 
     @Test
     fun `returns content for incorrect date format`() {
         resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN)
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-Modified-Since" to "NOT A DATE"))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-Modified-Since" to "NOT A DATE")
+                )
+            ),
             allOf(
                 hasStatus(OK),
                 hasBody("content")
-            ))
+            )
+        )
     }
 
     @Test
     fun `returns content if resource does not match etag`() {
         resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN, etag = ETag("etag-value", weak = true))
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-None-Match" to """"something-else""""))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-None-Match" to """"something-else"""")
+                )
+            ),
             allOf(
                 hasStatus(OK),
                 hasHeader("ETag", """W/"etag-value""""),
                 hasBody("content")
-            ))
+            )
+        )
     }
 
     @Test
     fun `returns NOT_MODIFIED if resource does match etag`() {
         resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN, etag = ETag("etag-value", weak = true))
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-None-Match" to """"something-else", W/"etag-value""""))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-None-Match" to """"something-else", W/"etag-value"""")
+                )
+            ),
             allOf(
                 hasStatus(NOT_MODIFIED),
                 hasHeader("ETag", """W/"etag-value""""),
                 hasBody("")
-            ))
+            )
+        )
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-None-Match" to """*"""))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-None-Match" to """*""")
+                )
+            ),
             allOf(
                 hasStatus(NOT_MODIFIED),
                 hasHeader("ETag", """W/"etag-value""""),
                 hasBody("")
-            ))
+            )
+        )
         assertThat( // should match strong etag even though resource is weak
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-None-Match" to """"etag-value""""))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-None-Match" to """"etag-value"""")
+                )
+            ),
             allOf(
                 hasStatus(NOT_MODIFIED),
                 hasHeader("ETag", """W/"etag-value""""),
                 hasBody("")
-            ))
+            )
+        )
     }
 
     @Test
     fun `returns content if no etag property`() {
         resources["/file.txt"] = InMemoryResource("content", TEXT_PLAIN, etag = null)
         assertThat(
-            handler(MemoryRequest(GET, Uri.of("/root/file.txt"),
-                listOf("If-None-Match" to """*"""))),
+            handler(
+                Request(
+                    GET, Uri.of("/root/file.txt"),
+                    listOf("If-None-Match" to """*""")
+                )
+            ),
             allOf(
                 hasStatus(OK),
                 hasHeader("ETag", absent()),
                 hasBody("content")
-            ))
+            )
+        )
     }
 }
 
@@ -184,10 +239,17 @@ private class IndeterminateLengthResource : Resource {
     override fun openStream() = EmptyInputStream.INSTANCE!!
 }
 
-private class InMemoryResourceLoader(val resources: Map<String, Resource>) : Router {
-    override fun match(request: Request): RouterMatch = resources[request.uri.path]?.let {
-        RouterMatch.MatchingHandler(it, description)
-    } ?: RouterMatch.Unmatched(description)
+private class InMemoryResourceLoader(val resources: Map<String, Resource>) : RouteMatcher {
+    override fun match(request: Request) = resources[request.uri.path]
+        ?.let {
+            HttpMatchResult(0, it)
+        } ?: HttpMatchResult(2, { req: Request -> Response(NOT_FOUND) })
+
+    override fun withBasePath(prefix: String): RouteMatcher = this
+
+    override fun withPredicate(other: Predicate): RouteMatcher = this
+
+    override fun withFilter(new: Filter): RouteMatcher = this
 }
 
 /**
