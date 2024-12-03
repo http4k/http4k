a7199debadab2ed939953aad00f8dd93e5be9b5f
David Denton <denton.david@gmail.com>
2024-12-03 22:24:04 +0000

    Introduce RoutedSseResponse and RoutedWsResponse

--- core/realtime-core/src/main/kotlin/org/http4k/routing/sse.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/routing/sse.kt
@@ -1,6 +1,7 @@
 package org.http4k.routing
 
 import org.http4k.core.Method
+import org.http4k.core.UriTemplate
 import org.http4k.routing.sse.RoutingSseHandler
 import org.http4k.sse.SseConsumer
 import org.http4k.sse.SseHandler
@@ -16,3 +17,8 @@ fun sse(vararg list: RoutingSseHandler): RoutingSseHandler = sse(list.toList())
 
 fun sse(routers: List<RoutingSseHandler>) =
     RoutingSseHandler(routers.flatMap { it.routes })
+
+data class RoutedSseResponse(
+    val delegate: SseResponse,
+    override val xUriTemplate: UriTemplate,
+) : SseResponse by delegate, RoutedMessage
