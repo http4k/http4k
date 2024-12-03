00a3e25a822f5ea3bebaff62121623b6004d4cac
David Denton <denton.david@gmail.com>
2024-12-03 23:49:51 +0000

    disable experimental test

--- core/incubator/src/test/kotlin/org/http4k/routing/ExperimentalStaticRoutingHttpHandlerTest.kt
+++ core/incubator/src/test/kotlin/org/http4k/routing/ExperimentalStaticRoutingHttpHandlerTest.kt
@@ -1,7 +1,9 @@
 package org.http4k.routing
 
 import org.http4k.routing.experimental.ResourceLoaders.Classpath
+import org.junit.jupiter.api.Disabled
 
+@Disabled
 class ExperimentalStaticRoutingHttpHandlerTest : StaticRoutingHttpHandlerTest() {
     override val handler = validPath bind org.http4k.routing.experimental.static(
         resourceLoader = Classpath()
