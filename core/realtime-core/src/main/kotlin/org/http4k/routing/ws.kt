e728c329aae6524eb3eae7dde2a63ca0256f21cf
David Denton <denton.david@gmail.com>
2024-12-03 22:24:04 +0000

    Introduce RoutedSseResponse and RoutedWsResponse

--- core/realtime-core/src/main/kotlin/org/http4k/routing/ws.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/routing/ws.kt
@@ -1,5 +1,6 @@
 package org.http4k.routing
 
+import org.http4k.core.UriTemplate
 import org.http4k.routing.websocket.RoutingWsHandler
 import org.http4k.websocket.WsConsumer
 import org.http4k.websocket.WsHandler
@@ -10,3 +11,8 @@ fun websockets(vararg list: RoutingWsHandler) = websockets(list.toList())
 fun websockets(routers: List<RoutingWsHandler>) = RoutingWsHandler(routers.flatMap { it.routes })
 
 fun websockets(ws: WsConsumer): WsHandler = { WsResponse(ws) }
+
+data class RoutedWsResponse(
+    val delegate: WsResponse,
+    override val xUriTemplate: UriTemplate,
+) : WsResponse by delegate, RoutedMessage
