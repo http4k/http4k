6bf2a8425e25ada2d178c7e40da8fa4d98f4e855
David Denton <denton.david@gmail.com>
2024-12-04 15:51:31 +0000

    Collapse all the event types Http/SSE/WS into one and add the protocol to it

--- core/core/src/main/kotlin/org/http4k/events/HttpEvent.kt
+++ core/core/src/main/kotlin/org/http4k/events/HttpEvent.kt
@@ -4,77 +4,41 @@ import org.http4k.core.HttpTransaction
 import org.http4k.core.Method
 import org.http4k.core.Status
 import org.http4k.core.Uri
+import org.http4k.events.ProtocolEvent.Incoming
 import org.http4k.routing.RoutedMessage
 import org.http4k.routing.RoutedRequest
 
-sealed class HttpEvent(
-    uri: Uri,
-    val method: Method,
-    status: Status,
-    latency: Long,
-    xUriTemplate: String,
-) : ProtocolEvent(uri, ProtocolStatus(status.code, status.description, status.clientGenerated), latency, xUriTemplate, "http") {
-    class Incoming(
+object HttpEvent {
+
+    fun Incoming(
         uri: Uri,
         method: Method,
         status: Status,
         latency: Long,
         xUriTemplate: String,
-    ) : HttpEvent(uri, method, status, latency, xUriTemplate) {
-        constructor(tx: HttpTransaction) : this(
-            tx.request.uri,
-            tx.request.method,
-            tx.response.status,
-            tx.duration.toMillis(),
-            if (tx.request is RoutedRequest) tx.request.xUriTemplate.toString() else tx.request.uri.path.trimStart('/')
-        )
+    ) = Incoming(uri, method, status, latency, xUriTemplate, "http")
 
-        override fun toString() =
-            "Incoming(uri=$uri, method=$method, status=$status, latency=$latency, xUriTemplate=$xUriTemplate, protocol=$protocol)"
+    fun Incoming(tx: HttpTransaction) = Incoming(
+        tx.request.uri,
+        tx.request.method,
+        tx.response.status,
+        tx.duration.toMillis(),
+        if (tx.request is RoutedRequest) tx.request.xUriTemplate.toString() else tx.request.uri.path.trimStart('/')
+    )
 
-        companion object
-    }
-
-    class Outgoing(
+    fun Outgoing(
         uri: Uri,
         method: Method,
         status: Status,
         latency: Long,
         xUriTemplate: String,
-    ) : HttpEvent(uri, method, status, latency, xUriTemplate) {
-        constructor(tx: HttpTransaction) : this(
-            tx.request.uri,
-            tx.request.method,
-            tx.response.status,
-            tx.duration.toMillis(),
-            if (tx.response is RoutedMessage) tx.response.xUriTemplate.toString() else tx.request.uri.path.trimStart('/')
-        )
+    ) = ProtocolEvent.Outgoing(uri, method, status, latency, xUriTemplate, "http")
 
-        override fun toString() =
-            "Outgoing(uri=$uri, method=$method, status=$status, latency=$latency, xUriTemplate=$xUriTemplate, protocol=$protocol)"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as HttpEvent
-
-        if (uri != other.uri) return false
-        if (method != other.method) return false
-        if (status != other.status) return false
-        if (latency != other.latency) return false
-        if (xUriTemplate != other.xUriTemplate) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = uri.hashCode()
-        result = 31 * result + method.hashCode()
-        result = 31 * result + status.hashCode()
-        result = 31 * result + latency.hashCode()
-        result = 31 * result + xUriTemplate.hashCode()
-        return result
-    }
+    fun Outgoing(tx: HttpTransaction) = Outgoing(
+        tx.request.uri,
+        tx.request.method,
+        tx.response.status,
+        tx.duration.toMillis(),
+        if (tx.response is RoutedMessage) tx.response.xUriTemplate.toString() else tx.request.uri.path.trimStart('/')
+    )
 }
