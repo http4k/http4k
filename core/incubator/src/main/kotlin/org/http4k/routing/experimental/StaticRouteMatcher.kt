39e60fd6e106b08b2501e5f711b9659cdc5396fa
David Denton <denton.david@gmail.com>
2024-12-04 10:45:46 +0000

    Collapse HttpMatchResult, SseMatchResult, WsMatchResult

--- core/incubator/src/main/kotlin/org/http4k/routing/experimental/StaticRouteMatcher.kt
+++ core/incubator/src/main/kotlin/org/http4k/routing/experimental/StaticRouteMatcher.kt
@@ -9,16 +9,16 @@ import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.Uri.Companion.of
 import org.http4k.core.then
 import org.http4k.routing.All
-import org.http4k.routing.HttpMatchResult
 import org.http4k.routing.RouteMatcher
 import org.http4k.routing.Router
 import org.http4k.routing.RoutingHttpHandler
+import org.http4k.routing.RoutingMatchResult
 import org.http4k.routing.RoutingResult.Matched
 import org.http4k.routing.RoutingResult.NotMatched
 import org.http4k.routing.and
 
 fun static(
-    resourceLoader: RouteMatcher,
+    resourceLoader: RouteMatcher<Response>,
     vararg extraFileExtensionToContentTypes: Pair<String, ContentType>
 ) = RoutingHttpHandler(
     listOf(StaticRouteMatcher("", resourceLoader, extraFileExtensionToContentTypes.asList().toMap()))
@@ -26,19 +26,19 @@ fun static(
 
 data class StaticRouteMatcher(
     private val pathSegments: String,
-    private val resourceLoader: RouteMatcher,
+    private val resourceLoader: RouteMatcher<Response>,
     private val extraFileExtensionToContentTypes: Map<String, ContentType>,
     private val router: Router = All,
     private val filter: Filter = Filter.NoOp
-) : RouteMatcher {
-    override fun match(request: Request): HttpMatchResult = when (router(request)) {
+) : RouteMatcher<Response>{
+    override fun match(request: Request) = when (router(request)) {
         is Matched -> resourceLoader.match(request.uri(of(convertPath(request.uri.path))))
-        is NotMatched -> HttpMatchResult(2, filter.then { _: Request -> Response(NOT_FOUND) })
+        is NotMatched -> RoutingMatchResult(2, filter.then { _: Request -> Response(NOT_FOUND) })
     }
 
-    override fun withBasePath(prefix: String): RouteMatcher = copy(pathSegments = prefix + pathSegments)
-    override fun withFilter(new: Filter): RouteMatcher = copy(filter = new.then(filter))
-    override fun withRouter(other: Router): RouteMatcher = copy(router = router.and(other))
+    override fun withBasePath(prefix: String): RouteMatcher<Response> = copy(pathSegments = prefix + pathSegments)
+    override fun withFilter(new: Filter): RouteMatcher<Response> = copy(filter = new.then(filter))
+    override fun withRouter(other: Router): RouteMatcher<Response> = copy(router = router.and(other))
 
     private fun convertPath(path: String) =
         if (pathSegments == "/" || pathSegments == "") path else path.replace(pathSegments, "")
