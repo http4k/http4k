52ed7966d32cc6a92480ac4860036ade41c95440
David Denton <denton.david@gmail.com>
2024-12-04 00:12:48 +0000

    Remove deprecations from entire codebase

--- core/security/oauth/src/main/kotlin/org/http4k/security/oauth/server/deprecatedOAuth.kt
+++
@@ -1,6 +0,0 @@
-package org.http4k.security.oauth.server
-
-import org.http4k.security.oauth.format.OAuthMoshi
-
-@Deprecated("Moved", ReplaceWith("org.http4k.security.oauth.format.OAuthMoshi"))
-typealias OAuthServerMoshi = OAuthMoshi
--- core/template/pug4j/src/main/kotlin/org/http4k/template/deprecatedJade4j.kt
+++
@@ -1,4 +0,0 @@
-package org.http4k.template
-
-@Deprecated("Lib has been renamed to Pug4j", ReplaceWith("Pug4jTemplates"))
-typealias Jade4jTemplates = Pug4jTemplates
