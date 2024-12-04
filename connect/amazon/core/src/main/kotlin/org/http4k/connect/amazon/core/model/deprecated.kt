138871c23e2f836c738fad47661d11edb674a5bc
David Denton <denton.david@gmail.com>
2024-12-04 00:12:48 +0000

    Remove deprecations from entire codebase

--- connect/amazon/core/src/main/kotlin/org/http4k/connect/amazon/core/model/deprecated.kt
+++
@@ -1,7 +0,0 @@
-package org.http4k.connect.amazon.core.model
-
-@Deprecated("Use TimestampMillis", ReplaceWith("org.http4k.connect.model.TimestampMillis"))
-typealias TimestampMillis = org.http4k.connect.model.TimestampMillis
-
-@Deprecated("Use Timestamp", ReplaceWith("org.http4k.connect.model.Timestamp"))
-typealias Timestamp = org.http4k.connect.model.Timestamp
