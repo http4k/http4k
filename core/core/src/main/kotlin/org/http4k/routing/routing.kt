0c26e1880a884c1814f02100f575e2aab269a8c5
David Denton <denton.david@gmail.com>
2024-12-04 10:08:09 +0000

    Rename Predicate to Router

--- core/core/src/main/kotlin/org/http4k/routing/routing.kt
+++ core/core/src/main/kotlin/org/http4k/routing/routing.kt
@@ -19,14 +19,14 @@ infix fun String.bind(httpHandler: RoutingHttpHandler) = httpHandler.withBasePat
 infix fun String.bind(action: HttpHandler) =
     RoutingHttpHandler(listOf(TemplatedHttpRoute(UriTemplate.from(this), action)))
 
-infix fun Predicate.bind(handler: HttpHandler): RoutingHttpHandler =
-    RoutingHttpHandler(listOf(PredicateRouteMatcher(handler, this)))
+infix fun Router.bind(handler: HttpHandler): RoutingHttpHandler =
+    RoutingHttpHandler(listOf(SimpleRouteMatcher(handler, this)))
 
-infix fun Predicate.bind(handler: RoutingHttpHandler): RoutingHttpHandler = handler.withPredicate(this)
+infix fun Router.bind(handler: RoutingHttpHandler): RoutingHttpHandler = handler.withRouter(this)
 
-fun Method.bind(handler: HttpHandler): RoutingHttpHandler = asPredicate().bind(handler)
+fun Method.bind(handler: HttpHandler): RoutingHttpHandler = asRouter().bind(handler)
 
-fun RoutingHttpHandler.and(predicate: Predicate) = withPredicate(predicate)
+fun RoutingHttpHandler.and(router: Router) = withRouter(router)
 
 /**
  * Simple Reverse Proxy which will split and direct traffic to the appropriate
@@ -42,8 +42,8 @@ fun reverseProxyRouting(vararg hostToHandler: Pair<String, HttpHandler>): Routin
     RoutingHttpHandler(
         hostToHandler.flatMap { (host, handler) ->
             when (handler) {
-                is RoutingHttpHandler -> handler.routes.map { it.withPredicate(hostHeaderOrUriHost(host)) }
-                else -> listOf(PredicateRouteMatcher(handler, hostHeaderOrUriHost(host)))
+                is RoutingHttpHandler -> handler.routes.map { it.withRouter(hostHeaderOrUriHost(host)) }
+                else -> listOf(SimpleRouteMatcher(handler, hostHeaderOrUriHost(host)))
             }
         }
     )
@@ -51,18 +51,18 @@ fun reverseProxyRouting(vararg hostToHandler: Pair<String, HttpHandler>): Routin
 private fun hostHeaderOrUriHost(host: String) =
     { req: Request ->
         (req.headerValues("host").firstOrNull() ?: req.uri.authority).contains(host)
-    }.asPredicate("host header or uri host = $host")
+    }.asRouter("host header or uri host = $host")
 
-fun Method.asPredicate() =
-    Predicate("method == $this", notMatchedStatus = METHOD_NOT_ALLOWED) { it.method == this }
+fun Method.asRouter() =
+    Router("method == $this", notMatchedStatus = METHOD_NOT_ALLOWED) { it.method == this }
 
-fun Method.and(predicate: Predicate) = asPredicate().and(predicate)
+fun Method.and(router: Router) = asRouter().and(router)
 
 /**
  * Apply routing predicate to a query
  */
 fun query(name: String, fn: (String) -> Boolean) =
-    { req: Request -> req.queries(name).filterNotNull().any(fn) }.asPredicate("Query $name matching $fn")
+    { req: Request -> req.queries(name).filterNotNull().any(fn) }.asRouter("Query $name matching $fn")
 
 /**
  * Apply routing predicate to a query
@@ -73,13 +73,13 @@ fun query(name: String, value: String) = query(name) { it == value }
  * Ensure all queries are present
  */
 fun queries(vararg names: String) =
-    { req: Request -> names.all { req.query(it) != null } }.asPredicate("Queries ${names.toList()}")
+    { req: Request -> names.all { req.query(it) != null } }.asRouter("Queries ${names.toList()}")
 
 /**
  * Apply routing predicate to a header
  */
 fun header(name: String, fn: (String) -> Boolean) =
-    { req: Request -> req.headerValues(name).filterNotNull().any(fn) }.asPredicate("Header $name matching $fn")
+    { req: Request -> req.headerValues(name).filterNotNull().any(fn) }.asRouter("Header $name matching $fn")
 
 /**
  * Apply routing predicate to a header
@@ -90,15 +90,15 @@ fun header(name: String, value: String) = header(name) { it == value }
  * Ensure all headers are present
  */
 fun headers(vararg names: String) =
-    { req: Request -> names.all { req.header(it) != null } }.asPredicate("Headers ${names.toList()}")
+    { req: Request -> names.all { req.header(it) != null } }.asRouter("Headers ${names.toList()}")
 
 /**
  * Ensure body matches predicate
  */
-fun body(fn: (Body) -> Boolean) = { it: Request -> fn(it.body) }.asPredicate("Body matching $fn")
+fun body(fn: (Body) -> Boolean) = { it: Request -> fn(it.body) }.asRouter("Body matching $fn")
 
 /**
  * Ensure body string matches predicate
  */
 @JvmName("bodyMatches")
-fun body(fn: (String) -> Boolean) = { req: Request -> fn(req.bodyString()) }.asPredicate("Body matching $fn")
+fun body(fn: (String) -> Boolean) = { req: Request -> fn(req.bodyString()) }.asRouter("Body matching $fn")
--- core/core/src/test/kotlin/org/http4k/routing/RouterMatchingTest.kt
+++ core/core/src/test/kotlin/org/http4k/routing/RouterMatchingTest.kt
@@ -18,113 +18,113 @@ class RouterMatchingTest {
     @Test
     fun `lens router`() {
         val router = Query.int().required("foo").matches { it > 5 }
-        assertThat(router(Request(GET, "").query("foo", "6")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(GET, "").query("foo", "5")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").query("foo", "bar")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("foo", "6")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(GET, "").query("foo", "5")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("foo", "bar")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `headers router`() {
         val router = headers("foo", "bar")
-        assertThat(router(Request(GET, "").header("foo", "1").header("bar", "2")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(GET, "").header("foo", "1")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").header("bar", "2")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").header("foo2", "5")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("foo", "1").header("bar", "2")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(GET, "").header("foo", "1")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("bar", "2")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("foo2", "5")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `header predicate router`() {
         val router = header("foo") { it == "bar" }
-        assertThat(router(Request(GET, "").header("foo", "1").header("foo", "bar")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(GET, "").header("foo", "1")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("foo", "1").header("foo", "bar")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(GET, "").header("foo", "1")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `header value router`() {
         val router = header("foo", "bar")
-        assertThat(router(Request(GET, "").header("foo", "1").header("foo", "bar")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(GET, "").header("foo", "1")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("foo", "1").header("foo", "bar")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(GET, "").header("foo", "1")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `query predicate router`() {
         val router = query("foo") { it == "bar" }
-        assertThat(router(Request(GET, "").query("foo", "1").query("foo", "bar")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(GET, "").query("foo", "1")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("foo", "1").query("foo", "bar")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(GET, "").query("foo", "1")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `query value router`() {
         val router = query("foo", "bar")
-        assertThat(router(Request(GET, "").query("foo", "1").query("foo", "bar")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(GET, "").query("foo", "1")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("foo", "1").query("foo", "bar")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(GET, "").query("foo", "1")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `queries router`() {
         val router = queries("foo", "bar")
-        assertThat(router(Request(GET, "").query("foo", "1").query("bar", "2")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(GET, "").query("foo", "1")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").query("bar", "2")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").query("foo2", "5")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("foo", "1").query("bar", "2")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(GET, "").query("foo", "1")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("bar", "2")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("foo2", "5")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `generic router`() {
-        val router = Predicate("foo", Status.NOT_FOUND, { r: Request -> r.method == GET })
-        assertThat(router(Request(GET, "")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(POST, "")), equalTo(PredicateResult.NotMatched()))
+        val router = Router("foo", Status.NOT_FOUND, { r: Request -> r.method == GET })
+        assertThat(router(Request(GET, "")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(POST, "")), equalTo(RoutingResult.NotMatched()))
         assertThat(router.description, equalTo("foo"))
     }
 
     @Test
     fun `method router`() {
-        val router = GET.asPredicate()
-        assertThat(router(Request(GET, "")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(POST, "")), equalTo(PredicateResult.NotMatched(METHOD_NOT_ALLOWED)))
+        val router = GET.asRouter()
+        assertThat(router(Request(GET, "")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(POST, "")), equalTo(RoutingResult.NotMatched(METHOD_NOT_ALLOWED)))
     }
 
     @Test
     fun `all router`() {
-        assertThat(All(Request(GET, "")), equalTo(PredicateResult.Matched))
-        assertThat(All(Request(POST, "")), equalTo(PredicateResult.Matched))
+        assertThat(All(Request(GET, "")), equalTo(RoutingResult.Matched))
+        assertThat(All(Request(POST, "")), equalTo(RoutingResult.Matched))
     }
 
     @Test
     fun `body string router`() {
         val router = body { it: String -> it == "hello" }
-        assertThat(router(Request(POST, "")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(POST, "").body("anything")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(POST, "").body("hello")), equalTo(PredicateResult.Matched))
+        assertThat(router(Request(POST, "")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(POST, "").body("anything")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(POST, "").body("hello")), equalTo(RoutingResult.Matched))
     }
 
     @Test
     fun `body router`() {
         val router = body { it: Body -> it.length?.toInt() == 3 }
-        assertThat(router(Request(POST, "")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(POST, "").body("anything")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(POST, "").body("foo")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(POST, "").body("bar")), equalTo(PredicateResult.Matched))
+        assertThat(router(Request(POST, "")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(POST, "").body("anything")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(POST, "").body("foo")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(POST, "").body("bar")), equalTo(RoutingResult.Matched))
     }
 
     @Test
     fun `composite router`() {
         val router = GET.and(header("foo", "bar").and(query("bar", "foo")))
-        assertThat(router(Request(GET, "").header("foo", "bar").query("bar", "foo")), equalTo(PredicateResult.Matched))
-        assertThat(router(Request(POST, "").header("foo", "bar").query("bar", "foo")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").header("foo", "bar")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").query("bar", "foo")), equalTo(PredicateResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("foo", "bar").query("bar", "foo")), equalTo(RoutingResult.Matched))
+        assertThat(router(Request(POST, "").header("foo", "bar").query("bar", "foo")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("foo", "bar")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").query("bar", "foo")), equalTo(RoutingResult.NotMatched()))
     }
 
     @Test
     fun `composite router with a bound router`() {
-        val router = GET.asPredicate().and(header("foo", "bar"))
+        val router = GET.asRouter().and(header("foo", "bar"))
 
-        assertThat(router(Request(GET, "")), equalTo(PredicateResult.NotMatched()))
-        assertThat(router(Request(GET, "").header("foo", "bar")), equalTo(PredicateResult.Matched))
+        assertThat(router(Request(GET, "")), equalTo(RoutingResult.NotMatched()))
+        assertThat(router(Request(GET, "").header("foo", "bar")), equalTo(RoutingResult.Matched))
     }
 }
