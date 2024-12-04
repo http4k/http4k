29e43b22d8ba2947d39298c92c62ef33ef7a02d6
David Denton <denton.david@gmail.com>
2024-12-04 12:52:30 +0000

    Tweak to RoutingHandler

--- core/core/src/main/kotlin/org/http4k/routing/RoutingHandler.kt
+++ core/core/src/main/kotlin/org/http4k/routing/RoutingHandler.kt
@@ -3,8 +3,8 @@ package org.http4k.routing
 import org.http4k.core.Request
 
 /**
- * Composite HttpHandler which can potentially service many different URL patterns. Should
- * return a 404 Response if it cannot service a particular Request.
+ * Composite andler which can potentially service many different URL patterns. Should
+ * return a specified Response if it cannot service a particular Request.
  *
  * Note that generally there should be no reason for the API user to implement this interface over and above the
  * implementations that already exist. The interface is public only because we have not found a way to hide it from
@@ -12,13 +12,13 @@ import org.http4k.core.Request
  */
 abstract class RoutingHandler<R, F, Self>(
     val routes: List<RouteMatcher<R, F>>,
-    private val defaultResponse: R,
+    private val notFoundResponse: R,
     private val copy: (List<RouteMatcher<R, F>>) -> Self
 ) : (Request) -> R {
     override fun invoke(request: Request) = routes
         .minOfOrNull { it.match(request) }
         ?.invoke(request)
-        ?: defaultResponse
+        ?: notFoundResponse
 
     fun withBasePath(prefix: String) = copy(routes.map { it.withBasePath(prefix) })
 
