dc121b79a18353c920f621c4171b5e8ce83aab35
David Denton <denton.david@gmail.com>
2024-12-04 11:59:40 +0000

    Collapse RoutingHandlers for WS and SSE

--- core/realtime-core/src/main/kotlin/org/http4k/routing/sse/sse.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/routing/sse/sse.kt
@@ -1,33 +1,52 @@
 package org.http4k.routing.sse
 
+import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.Status.Companion.NOT_FOUND
 import org.http4k.core.UriTemplate
 import org.http4k.routing.All
+import org.http4k.routing.RouteMatcher
 import org.http4k.routing.RoutedRequest
 import org.http4k.routing.RoutedSseResponse
 import org.http4k.routing.Router
+import org.http4k.routing.RoutingHandler
 import org.http4k.routing.RoutingMatchResult
 import org.http4k.routing.RoutingResult.Matched
 import org.http4k.routing.RoutingResult.NotMatched
 import org.http4k.routing.and
+import org.http4k.routing.asRouter
 import org.http4k.sse.NoOp
 import org.http4k.sse.SseFilter
 import org.http4k.sse.SseHandler
 import org.http4k.sse.SseResponse
 import org.http4k.sse.then
 
+class RoutingSseHandler(
+    routes: List<RouteMatcher<SseResponse, SseFilter>>
+) : RoutingHandler<SseResponse, SseFilter, RoutingSseHandler>(
+    routes,
+    SseResponse(NOT_FOUND, handled = false) { it.close() },
+    ::RoutingSseHandler
+)
+
+data class SsePathMethod(val path: String, val method: Method) {
+    infix fun to(handler: SseHandler) = when (handler) {
+        is RoutingSseHandler -> handler.withRouter(method.asRouter()).withBasePath(path)
+        else -> RoutingSseHandler(listOf(TemplatedSseRoute(UriTemplate.from(path), handler, method.asRouter())))
+    }
+}
+
 data class TemplatedSseRoute(
     private val uriTemplate: UriTemplate,
     private val handler: SseHandler,
     private val router: Router = All,
     private val filter: SseFilter = SseFilter.NoOp
-) {
+): RouteMatcher<SseResponse, SseFilter> {
     init {
         require(handler !is RoutingSseHandler)
     }
 
-    internal fun match(request: Request) = when {
+    override fun match(request: Request) = when {
         uriTemplate.matches(request.uri.path) -> when (val result = router(request)) {
             is Matched -> RoutingMatchResult(0, AddUriTemplate(uriTemplate).then(filter).then(handler))
             is NotMatched -> RoutingMatchResult(1, filter.then { _: Request -> SseResponse(result.status, handled = false) { it.close() } })
@@ -36,11 +55,11 @@ data class TemplatedSseRoute(
         else -> RoutingMatchResult(2, filter.then { _: Request -> SseResponse(NOT_FOUND, handled = false) { it.close() } })
     }
 
-    fun withBasePath(prefix: String) = copy(uriTemplate = UriTemplate.from("$prefix/${uriTemplate}"))
+    override fun withBasePath(prefix: String) = copy(uriTemplate = UriTemplate.from("$prefix/${uriTemplate}"))
 
-    fun withFilter(new: SseFilter) = copy(filter = new.then(filter))
+    override fun withFilter(new: SseFilter) = copy(filter = new.then(filter))
 
-    fun withRouter(other: Router) = copy(router = router.and(other))
+    override fun withRouter(other: Router) = copy(router = router.and(other))
 
     override fun toString() = "template=$uriTemplate AND ${router.description}"
 
--- core/realtime-core/src/main/kotlin/org/http4k/routing/websocket/ws.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/routing/websocket/ws.kt
@@ -1,15 +1,19 @@
 package org.http4k.routing.websocket
 
+import org.http4k.core.Method
 import org.http4k.core.Request
 import org.http4k.core.UriTemplate
 import org.http4k.routing.All
+import org.http4k.routing.RouteMatcher
 import org.http4k.routing.RoutedRequest
 import org.http4k.routing.RoutedWsResponse
 import org.http4k.routing.Router
+import org.http4k.routing.RoutingHandler
 import org.http4k.routing.RoutingMatchResult
 import org.http4k.routing.RoutingResult.Matched
 import org.http4k.routing.RoutingResult.NotMatched
 import org.http4k.routing.and
+import org.http4k.routing.asRouter
 import org.http4k.websocket.NoOp
 import org.http4k.websocket.WsFilter
 import org.http4k.websocket.WsHandler
@@ -17,17 +21,25 @@ import org.http4k.websocket.WsResponse
 import org.http4k.websocket.WsStatus.Companion.REFUSE
 import org.http4k.websocket.then
 
+class RoutingWsHandler(
+    routes: List<RouteMatcher<WsResponse, WsFilter>>
+) : RoutingHandler<WsResponse, WsFilter, RoutingWsHandler>(
+    routes,
+    WsResponse { it.close(REFUSE) },
+    ::RoutingWsHandler
+)
+
 data class TemplatedWsRoute(
     private val uriTemplate: UriTemplate,
     private val handler: WsHandler,
     private val router: Router = All,
     private val filter: WsFilter = WsFilter.NoOp
-) {
+) : RouteMatcher<WsResponse, WsFilter> {
     init {
         require(handler !is RoutingWsHandler)
     }
 
-    internal fun match(request: Request) = when {
+    override fun match(request: Request) = when {
         uriTemplate.matches(request.uri.path) -> when (router(request)) {
             is Matched -> RoutingMatchResult(0, AddUriTemplate(uriTemplate).then(filter).then(handler))
             is NotMatched -> RoutingMatchResult(1, filter.then { _: Request -> WsResponse { it.close(REFUSE) } })
@@ -36,11 +48,11 @@ data class TemplatedWsRoute(
         else -> RoutingMatchResult(1, filter.then { _: Request -> WsResponse { it.close(REFUSE) } })
     }
 
-    fun withBasePath(prefix: String) = copy(uriTemplate = UriTemplate.from("$prefix/${uriTemplate}"))
+    override fun withBasePath(prefix: String) = copy(uriTemplate = UriTemplate.from("$prefix/${uriTemplate}"))
 
-    fun withPredicate(other: Router) = copy(router = router.and(other))
+    override fun withRouter(other: Router) = copy(router = router.and(other))
 
-    fun withFilter(new: WsFilter) = copy(filter = new.then(filter))
+    override fun withFilter(new: WsFilter) = copy(filter = new.then(filter))
 
     override fun toString() = "template=$uriTemplate AND ${router.description}"
 
@@ -48,5 +60,12 @@ data class TemplatedWsRoute(
         {
             RoutedWsResponse(next(RoutedRequest(it, uriTemplate)), uriTemplate)
         }
+    }
+}
+
+data class WsPathMethod(val path: String, val method: Method) {
+    infix fun to(handler: WsHandler) = when (handler) {
+        is RoutingWsHandler -> handler.withRouter(method.asRouter()).withBasePath(path)
+        else -> RoutingWsHandler(listOf(TemplatedWsRoute(UriTemplate.from(path), handler, method.asRouter())))
     }
 }
