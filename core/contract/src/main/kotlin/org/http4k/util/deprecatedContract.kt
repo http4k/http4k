f61e61d3f50eb22def291a2d1a00e06c0c9b6357
David Denton <denton.david@gmail.com>
2024-12-04 00:12:48 +0000

    Remove deprecations from entire codebase

--- core/contract/src/main/kotlin/org/http4k/util/deprecatedContract.kt
+++
@@ -1,7 +0,0 @@
-package org.http4k.util
-
-@Deprecated("moved", ReplaceWith("org.http4k.contract.jsonschema.JsonSchema<T>"))
-typealias JsonSchema<T> = org.http4k.contract.jsonschema.JsonSchema<T>
-
-@Deprecated("moved", ReplaceWith("org.http4k.contract.jsonschema.JsonSchemaCreator<IN, OUT>"))
-typealias JsonSchemaCreator<IN, OUT> = org.http4k.contract.jsonschema.JsonSchemaCreator<IN, OUT>
