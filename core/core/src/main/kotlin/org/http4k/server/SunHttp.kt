b8ff26c92c6d408abba62a47faa9b856afc86765
David Denton <denton.david@gmail.com>
2024-12-06 11:13:07 +0000

    All server implementations stripped back to the minimum

--- core/core/src/main/kotlin/org/http4k/server/SunHttp.kt
+++ core/core/src/main/kotlin/org/http4k/server/SunHttp.kt
@@ -5,10 +5,12 @@ import org.http4k.server.ServerConfig.StopMode
 import org.http4k.server.ServerConfig.StopMode.Immediate
 import java.util.concurrent.Executors.newWorkStealingPool
 
-class SunHttp(
-    val port: Int = 8000,
-    override val stopMode: StopMode = Immediate
-) : ServerConfig {
+/**
+ * Stock version of an SunHttp Server. Not that if you want to configure your own server instance you
+ * can duplicate this code and modify it as required. We are purposefully trying to limit options
+ * here to keep the API simple for the 99% of use-cases.
+ */
+class SunHttp(private val port: Int = 8000, override val stopMode: StopMode = Immediate) : ServerConfig {
     constructor(port: Int = 8000) : this(port, Immediate)
 
     override fun toServer(http: HttpHandler) = SunHttpServer(http, port, stopMode, newWorkStealingPool())
