0f3e17b3a9eeaae88611a191aa33e4f6790f1896
David Denton <denton.david@gmail.com>
2024-12-06 11:13:07 +0000

    All server implementations stripped back to the minimum

--- core/server/ratpack/src/main/kotlin/org/http4k/server/Ratpack.kt
+++ core/server/ratpack/src/main/kotlin/org/http4k/server/Ratpack.kt
@@ -8,30 +8,30 @@ import ratpack.server.RatpackServer
 import ratpack.server.RatpackServerSpec
 import ratpack.server.ServerConfig.builder
 
+/**
+ * Stock version of an Ratpack Server. Not that if you want to configure your own server instance you
+ * can duplicate this code and modify it as required. We are purposefully trying to limit options
+ * here to keep the API simple for the 99% of use-cases.
+ */
 class Ratpack(port: Int = 8000, stopMode: StopMode) : ServerConfig {
     constructor(port: Int = 8000) : this(port, StopMode.Immediate)
 
     init {
-        when (stopMode) {
-            is Graceful -> throw UnsupportedStopMode(stopMode)
-            else -> {}
-        }
+        if (stopMode is Graceful) throw UnsupportedStopMode(stopMode)
     }
 
     private val serverConfig = builder().connectQueueSize(1000).port(port)
 
-    override fun toServer(http: HttpHandler): Http4kServer {
+    override fun toServer(http: HttpHandler) = object : Http4kServer {
         val server = RatpackServer.of { server: RatpackServerSpec ->
             server.serverConfig(serverConfig)
                 .handler { RatpackHttp4kHandler(http) }
         }
 
-        return object : Http4kServer {
-            override fun start() = apply { server.start() }
+        override fun start() = apply { server.start() }
 
-            override fun stop() = apply { server.stop() }
+        override fun stop() = apply { server.stop() }
 
-            override fun port() = server.bindPort
-        }
+        override fun port() = server.bindPort
     }
 }
