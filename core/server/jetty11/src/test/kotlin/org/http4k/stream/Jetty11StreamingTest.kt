d7f7ab4e89faa979ea8f287edf9088ba2485ca2b
David Denton <denton.david@gmail.com>
2024-12-06 10:56:19 +0000

    Tidy type signatures

--- core/server/jetty11/src/test/kotlin/org/http4k/stream/Jetty11StreamingTest.kt
+++ core/server/jetty11/src/test/kotlin/org/http4k/stream/Jetty11StreamingTest.kt
@@ -4,12 +4,11 @@ import org.http4k.core.BodyMode.Stream
 import org.http4k.core.HttpHandler
 import org.http4k.server.ClientForServerTesting
 import org.http4k.server.Jetty11
-import org.http4k.server.ServerConfig
 import org.http4k.streaming.StreamingContract
 import org.http4k.testingStopMode
 
 class Jetty11StreamingTest : StreamingContract() {
-    override fun serverConfig(): ServerConfig = Jetty11(0, testingStopMode)
+    override fun serverConfig() = Jetty11(0, testingStopMode)
 
     override fun createClient(): HttpHandler =
         ClientForServerTesting(bodyMode = Stream)
