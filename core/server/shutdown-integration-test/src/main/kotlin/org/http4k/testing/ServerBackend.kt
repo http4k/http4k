d94fb3159d0b6ead783d59a629a2c6c2f574d21d
David Denton <denton.david@gmail.com>
2024-12-06 10:52:22 +0000

    Refactor Undertow to remove excess server config

--- core/server/shutdown-integration-test/src/main/kotlin/org/http4k/testing/ServerBackend.kt
+++ core/server/shutdown-integration-test/src/main/kotlin/org/http4k/testing/ServerBackend.kt
@@ -10,45 +10,46 @@ import org.http4k.server.KtorNetty
 import org.http4k.server.Netty
 import org.http4k.server.Ratpack
 import org.http4k.server.ServerConfig
+import org.http4k.server.ServerConfig.StopMode
 import org.http4k.server.SunHttp
 import org.http4k.server.SunHttpLoom
 import org.http4k.server.Undertow
 
-enum class ServerBackend : (ServerConfig.StopMode) -> ServerConfig {
+enum class ServerBackend : (StopMode) -> ServerConfig {
     Apache {
-        override fun invoke(mode: ServerConfig.StopMode) = ApacheServer(8000, stopMode = mode)
+        override fun invoke(mode: StopMode) = ApacheServer(8000, stopMode = mode)
     },
     Apache4 {
-        override fun invoke(mode: ServerConfig.StopMode) = Apache4Server(8000, stopMode = mode)
+        override fun invoke(mode: StopMode) = Apache4Server(8000, stopMode = mode)
     },
     Jetty {
-        override fun invoke(mode: ServerConfig.StopMode) = Jetty(port = 8000, stopMode = mode)
+        override fun invoke(mode: StopMode) = Jetty(port = 8000, stopMode = mode)
     },
     JettyLoom {
-        override fun invoke(mode: ServerConfig.StopMode) = JettyLoom(port = 8000, stopMode = mode)
-    },
-    KtorCIO {
-        override fun invoke(mode: ServerConfig.StopMode) = KtorCIO(8000, stopMode = mode)
-    },
-    KtorNetty {
-        override fun invoke(mode: ServerConfig.StopMode) = KtorNetty(8000, stopMode = mode)
-    },
-    Netty {
-        override fun invoke(mode: ServerConfig.StopMode) = Netty(8000, stopMode = mode)
-    },
-    Ratpack {
-        override fun invoke(mode: ServerConfig.StopMode) = Ratpack(port = 8000, stopMode = mode)
+        override fun invoke(mode: StopMode) = JettyLoom(port = 8000, stopMode = mode)
     },
     Helidon {
-        override fun invoke(mode: ServerConfig.StopMode) = Helidon(port = 8000, stopMode = mode)
+        override fun invoke(mode: StopMode) = Helidon(port = 8000, stopMode = mode)
+    },
+    KtorCIO {
+        override fun invoke(mode: StopMode) = KtorCIO(8000, stopMode = mode)
+    },
+    KtorNetty {
+        override fun invoke(mode: StopMode) = KtorNetty(8000, stopMode = mode)
+    },
+    Netty {
+        override fun invoke(mode: StopMode) = Netty(8000, stopMode = mode)
+    },
+    Ratpack {
+        override fun invoke(mode: StopMode) = Ratpack(port = 8000, stopMode = mode)
     },
     SunHttp {
-        override fun invoke(mode: ServerConfig.StopMode) = SunHttp(port = 8000, stopMode = mode)
+        override fun invoke(mode: StopMode) = SunHttp(port = 8000, stopMode = mode)
     },
     SunHttpLoom {
-        override fun invoke(mode: ServerConfig.StopMode) = SunHttpLoom(port = 8000, stopMode = mode)
+        override fun invoke(mode: StopMode) = SunHttpLoom(port = 8000, stopMode = mode)
     },
     Undertow {
-        override fun invoke(mode: ServerConfig.StopMode) = Undertow(port = 8000, enableHttp2 = false, mode)
+        override fun invoke(mode: StopMode) = Undertow(port = 8000, mode)
     }
 }
