2331eda5f39c072f52d2baf0ff45f1417d46215e
David Denton <denton.david@gmail.com>
2024-12-10 10:08:24 +0000

    Fix up contract for event bridge

--- connect/amazon/eventbridge/client/src/testFixtures/kotlin/org/http4k/connect/amazon/eventbridge/EventBridgeContract.kt
+++ connect/amazon/eventbridge/client/src/testFixtures/kotlin/org/http4k/connect/amazon/eventbridge/EventBridgeContract.kt
@@ -2,7 +2,6 @@ package org.http4k.connect.amazon.eventbridge
 
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
-import org.http4k.client.JavaHttpClient
 import org.http4k.connect.amazon.AwsContract
 import org.http4k.connect.amazon.eventbridge.model.Event
 import org.http4k.connect.amazon.model.EventBusName
@@ -10,23 +9,21 @@ import org.http4k.connect.amazon.model.EventDetail
 import org.http4k.connect.amazon.model.EventDetailType
 import org.http4k.connect.amazon.model.EventSource
 import org.http4k.connect.successValue
-import org.http4k.core.HttpHandler
 import org.junit.jupiter.api.Test
-import java.util.UUID
 
 interface EventBridgeContract : AwsContract {
     private val eventBridge get() = EventBridge.Http(aws.region, { aws.credentials }, http)
 
-    private val eventBusName get() = EventBusName.of(uuid().toString())
-
     @Test
     fun `delivery stream lifecycle`() {
+        val eventBusName = EventBusName.of(uuid().toString().take(8))
+
         with(eventBridge) {
             try {
                 val arn = createEventBus(eventBusName).successValue().EventBusArn
 
                 assertThat(
-                    listEventBuses().successValue().EventBuses.map { it.Name }.contains(eventBusName),
+                    listEventBuses().successValue().EventBuses.map { it.Name?.value }.contains(eventBusName.value),
                     equalTo(true)
                 )
                 assertThat(
