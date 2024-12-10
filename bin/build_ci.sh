80442a6ff0f462da673b77e4a0ec13695b8e8222
David Denton <denton.david@gmail.com>
2024-12-10 12:06:26 +0000

    Remove info flag

--- bin/build_ci.sh
+++ bin/build_ci.sh
@@ -7,5 +7,5 @@ set -o nounset
 
 BASE_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 
-"$BASE_DIR"/gradlew check jacocoRootReport --build-cache --no-parallel --info
+"$BASE_DIR"/gradlew check jacocoRootReport --build-cache --no-parallel
 bash <(curl -s https://codecov.io/bash)
