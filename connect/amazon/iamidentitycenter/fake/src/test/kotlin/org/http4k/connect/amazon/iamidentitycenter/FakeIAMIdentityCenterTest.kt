e41f40814305510d4f82e30bb0bcd12b9928292e
David Denton <denton.david@gmail.com>
2024-12-27 15:17:26 +0000

    Release 5.42.0.0

--- connect/amazon/iamidentitycenter/fake/src/test/kotlin/org/http4k/connect/amazon/iamidentitycenter/FakeIAMIdentityCenterTest.kt
+++ connect/amazon/iamidentitycenter/fake/src/test/kotlin/org/http4k/connect/amazon/iamidentitycenter/FakeIAMIdentityCenterTest.kt
@@ -3,7 +3,9 @@ package org.http4k.connect.amazon.iamidentitycenter
 import org.http4k.connect.amazon.FakeAwsContract
 import org.http4k.core.HttpHandler
 import org.http4k.routing.reverseProxy
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
 
+@DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = ".*")
 class FakeIAMIdentityCenterTest : IAMIdentityCenterContract, FakeAwsContract {
     override val http: HttpHandler = reverseProxy(
         "sso" to FakeSSO(),
