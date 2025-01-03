ffa2e82b1513fbe9c50514053c7bddd4ac653cbe
Ivan Sanchez <ivan@gourame.com>
2025-01-03 11:22:43 +0000

    Create SSO cache directory during test if it doesn't exist

--- connect/amazon/iamidentitycenter/fake/src/test/kotlin/org/http4k/connect/amazon/iamidentitycenter/FakeIAMIdentityCenterTest.kt
+++ connect/amazon/iamidentitycenter/fake/src/test/kotlin/org/http4k/connect/amazon/iamidentitycenter/FakeIAMIdentityCenterTest.kt
@@ -4,9 +4,17 @@ import org.http4k.connect.amazon.FakeAwsContract
 import org.http4k.core.HttpHandler
 import org.http4k.routing.reverseProxy
 import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable
+import java.io.File
+import kotlin.io.path.Path
 
 @DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = ".*")
 class FakeIAMIdentityCenterTest : IAMIdentityCenterContract, FakeAwsContract {
+
+    init {
+        //create cache directory if it doesn't exist
+        Path(System.getProperty("user.home")).resolve(".aws/sso/cache").toFile().mkdirs()
+    }
+
     override val http: HttpHandler = reverseProxy(
         "sso" to FakeSSO(),
         "oidc" to FakeOIDC()
