40f3edfab2a340d83f34711af227184da78c10eb
David Denton <denton.david@gmail.com>
2025-01-04 23:13:57 +0000

    Migrate GCP module to new structure

--- core/platform/gcp/build.gradle.kts
+++ core/platform/gcp/build.gradle.kts
@@ -9,6 +9,13 @@ plugins {
 }
 
 dependencies {
-    api(project(":http4k-gcp"))
+    api(project(":http4k-core"))
+    api("com.google.http-client:google-http-client:_")
+
+    testImplementation(project(":http4k-testing-hamkrest"))
+    testFixturesImplementation(testFixtures(project(":http4k-core")))
+    testFixturesImplementation(project(":http4k-platform-core"))
+    testFixturesImplementation(project(":http4k-config"))
+    testFixturesImplementation(project(":http4k-client-okhttp"))
 }
 
