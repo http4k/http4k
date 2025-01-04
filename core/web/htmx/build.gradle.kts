cd3296f09d90183734da3180b1026bcfd569532b
David Denton <denton.david@gmail.com>
2025-01-04 23:02:40 +0000

    Migrate HTMX module

--- core/web/htmx/build.gradle.kts
+++ core/web/htmx/build.gradle.kts
@@ -9,5 +9,13 @@ plugins {
 }
 
 dependencies {
-    api(project(":http4k-htmx"))
+    api(project(":http4k-core"))
+    api("org.webjars.npm:htmx.org:_")
+    api("org.webjars.npm:hyperscript.org:_")
+    api("dev.forkhandles:values4k:_")
+
+    // this is here to force the version of something which is no longer in maven
+    implementation("org.webjars.npm:jridgewell__sourcemap-codec:1.5.0")
+
+    testImplementation(testFixtures(project(":http4k-core")))
 }
