eb020e8df0506d0c76f2720c6b423c6a56987ab7
David Denton <denton.david@gmail.com>
2025-01-04 23:07:53 +0000

    Migrate JSONRPC module

--- core/format/jackson/build.gradle.kts
+++ core/format/jackson/build.gradle.kts
@@ -21,10 +21,10 @@ dependencies {
 
     testImplementation(project(":http4k-core"))
     testImplementation(project(":http4k-api-openapi"))
-    testImplementation(project(":http4k-jsonrpc"))
+    testImplementation(project(":http4k-api-jsonrpc"))
     testImplementation(testFixtures(project(":http4k-core")))
     testImplementation(testFixtures(project(":http4k-format-core")))
     testImplementation(testFixtures(project(":http4k-contract")))
-    testImplementation(testFixtures(project(":http4k-jsonrpc")))
+    testImplementation(testFixtures(project(":http4k-api-jsonrpc")))
     testImplementation(project(":http4k-testing-approval"))
 }
