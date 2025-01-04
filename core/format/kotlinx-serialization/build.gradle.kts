c038ebc51c528760ea36b7efcf6f22c4c289bd0a
David Denton <denton.david@gmail.com>
2025-01-04 23:07:53 +0000

    Migrate JSONRPC module

--- core/format/kotlinx-serialization/build.gradle.kts
+++ core/format/kotlinx-serialization/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     testImplementation(testFixtures(project(":http4k-core")))
     testImplementation(testFixtures(project(":http4k-format-core")))
     testImplementation(testFixtures(project(":http4k-contract")))
-    testImplementation(testFixtures(project(":http4k-jsonrpc")))
+    testImplementation(testFixtures(project(":http4k-api-jsonrpc")))
     testImplementation(project(":http4k-testing-approval"))
 }
 
