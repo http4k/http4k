43fe80a2626d1133e90704709cf7cd341abfa84b
David Denton <denton.david@gmail.com>
2025-01-05 15:36:48 +0000

    Reefactoring H/RL

--- pro/tools/hotreload/src/main/kotlin/org/http4k/hotreload/ProjectSourceWatcher.kt
+++ pro/tools/hotreload/src/main/kotlin/org/http4k/hotreload/ProjectSourceWatcher.kt
@@ -1,46 +1,57 @@
 package org.http4k.hotreload
 
+import org.http4k.hotreload.CompileProject.Companion.Gradle
 import org.http4k.hotreload.CompileProject.Companion.Result.Failed
 import org.http4k.hotreload.CompileProject.Companion.Result.Ok
-import java.io.InputStream
 import java.nio.file.FileSystems
 import java.nio.file.FileVisitResult
-import java.nio.file.Files
+import java.nio.file.FileVisitResult.CONTINUE
+import java.nio.file.Files.walkFileTree
 import java.nio.file.Path
-import java.nio.file.Paths
 import java.nio.file.SimpleFileVisitor
-import java.nio.file.StandardWatchEventKinds
+import java.nio.file.StandardWatchEventKinds.ENTRY_CREATE
+import java.nio.file.StandardWatchEventKinds.ENTRY_DELETE
+import java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY
 import java.nio.file.attribute.BasicFileAttributes
 import java.time.Duration
 import kotlin.concurrent.thread
 import kotlin.io.path.exists
 
+// This is the default set of directories to watch for changes
+val DEFAULT_WATCH_PATTERNS = setOf("src/main", "src/test", "build/classes")
+
+/**
+ * Watches for changes on the classpath source and rebuilds the project when changes are detected.
+ */
 class ProjectSourceWatcher(
-    private val watchedDirs: Set<String>,
-    private val compileProject: CompileProject,
-    private val rebuildBackoff: Duration,
-    private val onChange: () -> Unit,
-    private val onSuccess: () -> Unit,
-    private val onError: (InputStream) -> Unit
+    private val compileProject: CompileProject = Gradle(),
+    private val watchedDirs: Set<String> = DEFAULT_WATCH_PATTERNS,
+    private val rebuildBackoff: Duration = Duration.ofSeconds(1)
 ) {
-    fun watchFiles(classpathRoots: List<String>) {
+    private val changeListeners = mutableListOf<(List<Path>) -> Unit>()
+
+    fun onChange(fn: (List<Path>) -> Unit) = apply { changeListeners.add(fn) }
+
+    private val successListeners = mutableListOf<() -> Unit>()
+
+    fun onSuccess(fn: () -> Unit) = apply { successListeners.add(fn) }
+
+    private val failureListeners = mutableListOf<(String) -> Unit>()
+
+    fun onFailure(fn: (String) -> Unit) = apply { failureListeners.add(fn) }
+
+    fun watch(sourcePaths: List<Path>) {
         val watchService = FileSystems.getDefault().newWatchService()
 
-        classpathRoots
-            .map { Paths.get(it.substringBefore("build/classes")).resolve("src") }
+        sourcePaths
             .filter { it.exists() }
             .forEach {
-                Files.walkFileTree(it, object : SimpleFileVisitor<Path>() {
+                walkFileTree(it, object : SimpleFileVisitor<Path>() {
                     override fun preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult {
                         if (shouldWatchDirectory(dir)) {
-                            dir.register(
-                                watchService,
-                                StandardWatchEventKinds.ENTRY_MODIFY,
-                                StandardWatchEventKinds.ENTRY_CREATE,
-                                StandardWatchEventKinds.ENTRY_DELETE
-                            )
+                            dir.register(watchService, ENTRY_MODIFY, ENTRY_CREATE, ENTRY_DELETE)
                         }
-                        return FileVisitResult.CONTINUE
+                        return CONTINUE
                     }
                 })
             }
@@ -50,16 +61,17 @@ class ProjectSourceWatcher(
         thread {
             while (true) {
                 val key = watchService.take()
-                val events = key.pollEvents()
+                val fileChanges = key.pollEvents().mapNotNull { it.context() as? Path }
 
-                if (!isRebuilding && events.any { ((it.context() as? Path)?.toString() ?: "").contains(".") }) {
+                if (!isRebuilding && fileChanges.any { it.toString().contains(".") }) {
                     isRebuilding = true
                     try {
-                        onChange()
+                        changeListeners.forEach { it(fileChanges) }
 
                         when (val result = compileProject()) {
-                            Ok -> onSuccess()
-                            is Failed -> onError(result.errorStream)
+                            Ok -> successListeners.forEach { it() }
+                            is Failed -> result.errorStream.reader().readText()
+                                .also { stream -> failureListeners.forEach { it(stream) } }
                         }
                         Thread.sleep(rebuildBackoff)
                     } finally {
--- pro/tools/hotreload/src/test/kotlin/org/http4k/hotreload/polyApp.kt
+++ pro/tools/hotreload/src/test/kotlin/org/http4k/hotreload/polyApp.kt
@@ -1,10 +1,11 @@
 package org.http4k.hotreload
 
+import org.http4k.hotreload.CompileProject.Companion.Gradle
 import org.http4k.server.Helidon
 
 fun main() {
     HotReloadServer.poly<ExamplePolyApp>(
         serverConfig = Helidon(8000),
-        compileProject = CompileProject.Gradle(":http4k-incubator:compileTestKotlin")
+        watcher = ProjectSourceWatcher(Gradle(":http4k-incubator:compileTestKotlin"))
     ).start()
 }
