b50dd4ccfbcc67ecfa1df2978eebb1f0f95db545
David Denton <denton.david@gmail.com>
2025-01-05 14:19:40 +0000

    Introducing HotReload module

--- core/incubator/build.gradle.kts
+++ core/incubator/build.gradle.kts
@@ -15,8 +15,6 @@ dependencies {
     api(Square.moshi.adapters)
     implementation(project(mapOf("path" to ":http4k-testing-webdriver")))
 
-    implementation(project(":http4k-realtime-core"))
-
     compileOnly("org.junit.jupiter:junit-jupiter-api:_")
 
     testImplementation(project(":http4k-client-apache"))
---
+++ pro/tools/hotreload/build.gradle.kts
@@ -0,0 +1,17 @@
+import org.http4k.internal.ModuleLicense.Http4kCommercial
+
+description = "http4k Tools: Hot Reload support"
+
+val license by project.extra { Http4kCommercial }
+
+plugins {
+    id("org.http4k.pro")
+}
+
+dependencies {
+    implementation(project(":http4k-realtime-core"))
+
+    testImplementation(project(":http4k-testing-approval"))
+    testImplementation(project(":http4k-testing-hamkrest"))
+    testImplementation(project(":http4k-server-helidon"))
+}
