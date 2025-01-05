2a02666b9691cac3d468be79c75abf622f200688
David Denton <denton.david@gmail.com>
2025-01-05 21:16:53 +0000

    Revert "Add PolyHandler DSL"
    This reverts commit f451875840725de7b2703b7f77d658fbc3c05bf2.

--- core/realtime-core/src/main/kotlin/org/http4k/server/PolyHandler.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/server/PolyHandler.kt
@@ -7,7 +7,7 @@ import org.http4k.websocket.WsHandler
 /**
  * A PolyHandler represents the combined routing logic of an multiple protocol handlers
  */
-data class PolyHandler @JvmOverloads constructor(
+class PolyHandler @JvmOverloads constructor(
     val http: HttpHandler? = null,
     val ws: WsHandler? = null,
     val sse: SseHandler? = null
