85b0aeec9ea31e824a0a92277537bff4c4e43551
David Denton <denton.david@gmail.com>
2025-01-05 16:06:22 +0000

    Comment [skip-ci]

--- pro/tools/hotreload/src/main/kotlin/org/http4k/hotreload/PathWatcher.kt
+++ pro/tools/hotreload/src/main/kotlin/org/http4k/hotreload/PathWatcher.kt
@@ -2,6 +2,9 @@ package org.http4k.hotreload
 
 import java.nio.file.Path
 
+/**
+ * Responsible for watching a set of paths for changes and acting on the changes.
+ */
 interface PathWatcher : AutoCloseable {
     fun onChange(fn: (List<Path>) -> Unit): PathWatcher
     fun onSuccess(fn: () -> Unit): PathWatcher
