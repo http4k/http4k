484185d5d7b2609bd45e7d4abf5f81ae065bc9c5
David Denton <denton.david@gmail.com>
2025-01-05 18:00:37 +0000

    Migrating traffic code into http4k-tools-traffic-capture

--- core/tools/traffic-capture/src/test/kotlin/org/http4k/filter/TrafficFiltersTest.kt
+++ core/tools/traffic-capture/src/test/kotlin/org/http4k/filter/TrafficFiltersTest.kt
@@ -7,15 +7,11 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
-import org.http4k.core.Status.Companion.ACCEPTED
-import org.http4k.core.Status.Companion.BAD_REQUEST
-import org.http4k.core.Status.Companion.NOT_FOUND
+import org.http4k.core.Status
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Uri
 import org.http4k.core.then
-import org.http4k.filter.ClientFilters.SetBaseUriFrom
 import org.http4k.filter.TrafficFilters.RecordTo
-import org.http4k.hamkrest.hasBody
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 import org.http4k.traffic.ReadWriteCache
@@ -51,16 +47,16 @@ class TrafficFiltersTest : PortBasedTest {
             .then { responseFor(it) }
             .asServer(SunHttp(0))
             .start().use {
-                val client = SetBaseUriFrom(Uri.of("http://localhost:${it.port()}"))
+                val client = ClientFilters.SetBaseUriFrom(Uri.of("http://localhost:${it.port()}"))
                     .then(JavaHttpClient())
-                assertThat(client(request1), hasBody("helloworld".reversed()))
-                assertThat(client(request2), hasBody("goodbyeworld".reversed()))
+                assertThat(client(request1), org.http4k.hamkrest.hasBody("helloworld".reversed()))
+                assertThat(client(request2), org.http4k.hamkrest.hasBody("goodbyeworld".reversed()))
             }
 
-        assertThat(stream.requests().toList()[0], hasBody(request1.bodyString()))
-        assertThat(stream.requests().toList()[1], hasBody(request2.bodyString()))
-        assertThat(stream.responses().toList()[0], hasBody(responseFor(request1).bodyString()))
-        assertThat(stream.responses().toList()[1], hasBody(responseFor(request2).bodyString()))
+        assertThat(stream.requests().toList()[0], org.http4k.hamkrest.hasBody(request1.bodyString()))
+        assertThat(stream.requests().toList()[1], org.http4k.hamkrest.hasBody(request2.bodyString()))
+        assertThat(stream.responses().toList()[0], org.http4k.hamkrest.hasBody(responseFor(request1).bodyString()))
+        assertThat(stream.responses().toList()[1], org.http4k.hamkrest.hasBody(responseFor(request2).bodyString()))
     }
 
     private fun responseFor(req: Request) =
@@ -70,8 +66,8 @@ class TrafficFiltersTest : PortBasedTest {
     fun `ServeCachedFrom serves stored requests later or falls back`() {
         val cache = ReadWriteCache.Memory()
         cache[request] = response
-        val notFound = Response(NOT_FOUND)
-        val handler = TrafficFilters.ServeCachedFrom(cache).then { notFound }
+        val notFound = Response(Status.NOT_FOUND)
+        val handler = org.http4k.filter.TrafficFilters.ServeCachedFrom(cache).then { notFound }
 
         assertThat(handler(request), equalTo(response))
         assertThat(handler(Request(GET, "/bob2")), equalTo(notFound))
@@ -81,13 +77,13 @@ class TrafficFiltersTest : PortBasedTest {
     fun `ReplayFrom serves stored requests later or returns 400`() {
         val cache = ReadWriteStream.Memory()
         cache[Request(GET, "/bob1")] = Response(OK)
-        cache[Request(GET, "/bob2")] = Response(ACCEPTED)
-        cache[Request(GET, "/bob3")] = Response(NOT_FOUND)
-        val handler = TrafficFilters.ReplayFrom(cache).then { fail("") }
+        cache[Request(GET, "/bob2")] = Response(Status.ACCEPTED)
+        cache[Request(GET, "/bob3")] = Response(Status.NOT_FOUND)
+        val handler = org.http4k.filter.TrafficFilters.ReplayFrom(cache).then { fail("") }
 
         assertThat(handler(Request(GET, "/bob1")), equalTo(Response(OK)))
-        assertThat(handler(Request(GET, "/bob2")), equalTo(Response(ACCEPTED)))
-        assertThat(handler(Request(GET, "/bob3")), equalTo(Response(NOT_FOUND)))
-        assertThat(handler(Request(GET, "/bob2")), equalTo(Response(BAD_REQUEST)))
+        assertThat(handler(Request(GET, "/bob2")), equalTo(Response(Status.ACCEPTED)))
+        assertThat(handler(Request(GET, "/bob3")), equalTo(Response(Status.NOT_FOUND)))
+        assertThat(handler(Request(GET, "/bob2")), equalTo(Response(Status.BAD_REQUEST)))
     }
 }
