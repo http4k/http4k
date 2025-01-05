d89f357f1be7556321de41ab3d0e331cad7cc745
David Denton <denton.david@gmail.com>
2025-01-05 17:16:29 +0000

    Remove openAI plugins module as they have been discontinued

--- connect/ai/openai/fake/src/main/kotlin/org/http4k/connect/openai/FakeOpenAI.kt
+++ connect/ai/openai/fake/src/main/kotlin/org/http4k/connect/openai/FakeOpenAI.kt
@@ -3,17 +3,12 @@ package org.http4k.connect.openai
 import org.http4k.chaos.ChaoticHttpHandler
 import org.http4k.chaos.defaultLocalUri
 import org.http4k.chaos.start
-import org.http4k.client.JavaHttpClient
 import org.http4k.connect.model.ModelName
 import org.http4k.connect.openai.action.Model
-import org.http4k.connect.openai.plugins.PluginIntegration
 import org.http4k.connect.storage.Storage
-import org.http4k.contract.ui.swagger.swaggerUiWebjar
-import org.http4k.core.HttpHandler
 import org.http4k.core.Uri
 import org.http4k.core.then
 import org.http4k.filter.ServerFilters.BearerAuth
-import org.http4k.routing.bind
 import org.http4k.routing.routes
 import java.time.Clock
 import java.time.Clock.systemUTC
@@ -22,41 +17,23 @@ class FakeOpenAI(
     val models: Storage<Model> = DEFAULT_OPEN_AI_MODELS,
     val completionGenerators: Map<ModelName, ChatCompletionGenerator> = emptyMap(),
     clock: Clock = systemUTC(),
-    baseUri: Uri = FakeOpenAI::class.defaultLocalUri,
-    http: HttpHandler = JavaHttpClient(),
-    vararg plugins: PluginIntegration
+    baseUri: Uri = FakeOpenAI::class.defaultLocalUri
 ) : ChaoticHttpHandler() {
 
     override val app =
         routes(
-            *(
-                plugins
-                    .map { it.buildIntegration(baseUri, http, clock) }
-                    .flatMap {
-                        listOf(
-                            it.filter.then(
-                                "/${it.pluginId}" bind swaggerUiWebjar {
-                                    pageTitle = "OpenAI plugin " + it.pluginId
-                                    url = baseUri.path("/${it.pluginId}/openapi.json").toString()
-                                }
-                            ),
-                            it.httpHandler
+            listOf(
+                BearerAuth { true }
+                    .then(
+                        routes(
+                            getModels(models),
+                            chatCompletion(clock, completionGenerators),
+                            createEmbeddings(models),
+                            generateImage(clock, baseUri),
                         )
-                    } +
-                    listOf(
-                        BearerAuth { true }
-                            .then(
-                                routes(
-                                    getModels(models),
-                                    chatCompletion(clock, completionGenerators),
-                                    createEmbeddings(models),
-                                    generateImage(clock, baseUri),
-                                )
-                            ),
-                        serveGeneratedContent(),
-                        index(plugins.map { it.pluginId })
-                    )
-                ).toTypedArray()
+                    ),
+                serveGeneratedContent(),
+            )
         )
 
     /**
--- connect/ai/openai/plugin/src/main/kotlin/org/http4k/connect/openai/model/VerificationToken.kt
+++
@@ -1,8 +0,0 @@
-package org.http4k.connect.openai.model
-
-import dev.forkhandles.values.NonBlankStringValueFactory
-import dev.forkhandles.values.StringValue
-
-class VerificationToken private constructor(value: String) : StringValue(value) {
-    companion object : NonBlankStringValueFactory<VerificationToken>(::VerificationToken)
-}
