c459d2fd79ad6d88e47fea8c6e6403fe7be2a8e1
David Denton <denton.david@gmail.com>
2025-01-05 18:24:19 +0000

    Add PolyHandler DSL

---
+++ core/realtime-core/src/test/kotlin/org/http4k/routing/PolyRoutingTest.kt
@@ -0,0 +1,29 @@
+package org.http4k.routing
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.core.HttpHandler
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.server.PolyHandler
+import org.junit.jupiter.api.Test
+
+class PolyRoutingTest {
+
+    @Test
+    fun `can use DSL to construct polyhandler`() {
+        val websockets = websockets {
+
+        }
+        val serverSentEvents = sse {}
+        val httpHandler: HttpHandler = { Response(OK) }
+
+        val poly = routes {
+            http = httpHandler
+            ws = websockets
+            sse = serverSentEvents
+        }
+
+        assertThat(poly, equalTo(PolyHandler(httpHandler, websockets, serverSentEvents)))
+    }
+}
