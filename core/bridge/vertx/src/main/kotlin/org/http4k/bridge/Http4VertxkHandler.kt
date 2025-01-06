17e8a4ea68c7061289d2770d367ff86470c7b6b5
David Denton <denton.david@gmail.com>
2025-01-06 15:51:00 +0000

    Extracting out code for bridging in either fallback or individual route method

--- core/bridge/vertx/src/main/kotlin/org/http4k/bridge/Http4VertxkHandler.kt
+++ core/bridge/vertx/src/main/kotlin/org/http4k/bridge/Http4VertxkHandler.kt
@@ -1,32 +1,13 @@
 package org.http4k.bridge
 
-import io.vertx.core.buffer.Buffer
-import io.vertx.core.http.HttpServerRequest
-import io.vertx.core.http.HttpServerResponse
 import io.vertx.ext.web.RoutingContext
 import org.http4k.core.HttpHandler
-import org.http4k.core.Method
-import org.http4k.core.Request
-import org.http4k.core.Response
-import java.io.ByteArrayInputStream
 
-
+/**
+ * Bridge a single Vertx request to an Http4k handler.
+ */
 fun VertxToHttp4kHandler(http: HttpHandler): (RoutingContext) -> Unit = { ctx ->
     ctx.request().asHttp4k()
         .map(http)
         .map { it.into(ctx.response()) }
 }
-
-fun Response.into(response: HttpServerResponse) =
-    headers
-        .fold(response) { acc, (k, v) -> acc.putHeader(k, v) }
-        .setStatusCode(status.code).setStatusMessage(status.description)
-        .end(Buffer.buffer(body.payload.array()))
-
-fun HttpServerRequest.asHttp4k() = body()
-    .map {
-        headers()
-            .fold(Request(Method.valueOf(method().name()), uri())) { acc, (k, v) -> acc.header(k, v) }
-            .body(ByteArrayInputStream(it.bytes))
-    }
-
