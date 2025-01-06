a6f275023fcd14bfa9f352d8a3c012210c2ac64e
David Denton <denton.david@gmail.com>
2025-01-06 12:51:41 +0000

    Adding micronaut migrate module

---
+++ core/migrate/micronaut/build.gradle.kts
@@ -0,0 +1,17 @@
+import org.http4k.internal.ModuleLicense.Apache2
+
+description = "http4k Migrate: from Micronaut to http4k"
+
+val license by project.extra { Apache2 }
+
+plugins {
+    id("org.http4k.community")
+}
+
+dependencies {
+    api(project(":http4k-core"))
+
+    api("io.micronaut:micronaut-http:4.7.10")
+
+    testFixturesApi(testFixtures(project(":http4k-core")))
+}
