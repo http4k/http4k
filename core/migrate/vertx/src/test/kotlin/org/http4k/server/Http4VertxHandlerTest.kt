4cb05baea7be5da4e34781e8fcfbe953b99da9d6
David Denton <denton.david@gmail.com>
2025-01-06 13:55:03 +0000

    Add vertx migrate

---
+++ core/migrate/vertx/src/test/kotlin/org/http4k/server/Http4VertxHandlerTest.kt
@@ -0,0 +1,43 @@
+package org.http4k.server
+
+import com.natpryce.hamkrest.and
+import com.natpryce.hamkrest.assertion.assertThat
+import io.vertx.core.Vertx
+import io.vertx.ext.web.Router
+import org.http4k.client.JavaHttpClient
+import org.http4k.core.Method.POST
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.hamkrest.hasBody
+import org.http4k.hamkrest.hasHeader
+import org.http4k.hamkrest.hasStatus
+import org.http4k.util.PortBasedTest
+import org.junit.jupiter.api.Test
+
+class Http4kVertxHandlerTest : PortBasedTest {
+
+    @Test
+    fun `passes requests through and adapts`() {
+        val vertx = Vertx.builder().build()
+
+        val router = Router.router(vertx)
+            .apply {
+                route("/*").blockingHandler(Http4VertxHandler { req: Request -> Response(OK)
+                .headers(req.headers)
+                .body(req.body) })
+            }
+
+        val requestHandler = vertx.createHttpServer().requestHandler(router)
+            .apply { listen(0) }
+
+        val request = Request(POST, "http://localhost:${requestHandler.actualPort()}")
+            .header("foo", "bar")
+            .body("hello")
+        val response = JavaHttpClient()(request)
+
+        assertThat(response, hasStatus(OK).and(hasBody("hello")).and(hasHeader("foo", "bar")))
+
+        requestHandler.close()
+    }
+}
