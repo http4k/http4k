bc9a3da3eff31c4e8d75ab319bf3ded4bc112ada
David Denton <denton.david@gmail.com>
2025-01-06 15:22:06 +0000

    Introduce ktor bridge module

--- core/server/ktornetty/build.gradle.kts
+++ core/server/ktornetty/build.gradle.kts
@@ -11,6 +11,7 @@ plugins {
 
 dependencies {
     api(project(":http4k-core"))
+    api(project(":http4k-bridge-ktor"))
     api(Ktor.server.netty)
     testImplementation(testFixtures(project(":http4k-core")))
 }
