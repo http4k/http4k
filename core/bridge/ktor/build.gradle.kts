fc20add896550fb0097220dde0e3d1fbbad66388
David Denton <denton.david@gmail.com>
2025-01-06 15:22:06 +0000

    Introduce ktor bridge module

---
+++ core/bridge/ktor/build.gradle.kts
@@ -0,0 +1,16 @@
+import org.http4k.internal.ModuleLicense.Apache2
+
+description = "http4k Bridge: from Ktor to http4k"
+
+val license by project.extra { Apache2 }
+
+plugins {
+    id("org.http4k.community")
+}
+
+dependencies {
+    api(project(":http4k-core"))
+    api("io.ktor:ktor-server-core-jvm:3.0.3")
+
+    testFixturesApi(testFixtures(project(":http4k-core")))
+}
