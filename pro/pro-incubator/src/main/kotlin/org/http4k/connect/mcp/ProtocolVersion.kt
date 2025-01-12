0b5b20318eb81fb939679aa19c95efa6dd479a39
David Denton <denton.david@gmail.com>
2025-01-12 22:04:03 +0000

    Make enum into a class

--- pro/pro-incubator/src/main/kotlin/org/http4k/connect/mcp/ProtocolVersion.kt
+++ pro/pro-incubator/src/main/kotlin/org/http4k/connect/mcp/ProtocolVersion.kt
@@ -1,6 +1,11 @@
 package org.http4k.connect.mcp
 
-enum class ProtocolVersion {
-    `2024-11-05`,
-    `2024-10-07`
+import dev.forkhandles.values.NonBlankStringValueFactory
+import dev.forkhandles.values.StringValue
+
+class ProtocolVersion private constructor(value: String) : StringValue(value) {
+    companion object : NonBlankStringValueFactory<ProtocolVersion>(::ProtocolVersion) {
+        val `2024-11-05` = ProtocolVersion.of("2024-11-05")
+        val `2024-10-07` = ProtocolVersion.of("2024-10-07")
+    }
 }
--- pro/pro-incubator/src/main/kotlin/org/http4k/connect/mcp/util/McpJson.kt
+++ pro/pro-incubator/src/main/kotlin/org/http4k/connect/mcp/util/McpJson.kt
@@ -3,6 +3,7 @@ package org.http4k.connect.mcp.util
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.http4k.connect.mcp.McpRpcMethod
+import org.http4k.connect.mcp.ProtocolVersion
 import org.http4k.format.ConfigurableJackson
 import org.http4k.format.asConfigurable
 import org.http4k.format.value
@@ -13,6 +14,7 @@ object McpJson : ConfigurableJackson(
         .asConfigurable()
         .withStandardMappings()
         .value(McpRpcMethod)
+        .value(ProtocolVersion)
         .done()
         .setSerializationInclusion(JsonInclude.Include.NON_NULL)
 )
