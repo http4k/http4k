9129ac08cd4db2d298f541fb7d282f90cdda2d40
David Denton <denton.david@gmail.com>
2025-01-12 23:15:07 +0000

    Move extension method to jsonrpc module

--- core/api/jsonrpc/src/main/kotlin/org/http4k/format/jsonRpcExtensions.kt
+++ core/api/jsonrpc/src/main/kotlin/org/http4k/format/jsonRpcExtensions.kt
@@ -1,7 +1,10 @@
 package org.http4k.format
 
+import org.http4k.core.Body
 import org.http4k.jsonrpc.ErrorMessage
+import org.http4k.jsonrpc.JsonRpcRequest
 import org.http4k.jsonrpc.jsonRpcVersion
+import org.http4k.lens.ContentNegotiation.Companion.None
 
 fun <NODE> Json<NODE>.renderResult(result: NODE, id: NODE): NODE = this {
     obj(
@@ -18,3 +21,8 @@ fun <NODE> Json<NODE>.renderError(errorMessage: ErrorMessage, id: NODE? = null) 
         "id" to (id ?: nullNode())
     )
 }
+
+fun <NODE> Body.Companion.jsonRpcRequest(json: Json<NODE>) =
+    json.body("body", None)
+        .map { JsonRpcRequest(json, json.fields(it).toMap()) }
+
--- pro/pro-incubator/src/test/kotlin/org/http4k/connect/mcp/main2.kt
+++ pro/pro-incubator/src/test/kotlin/org/http4k/connect/mcp/main2.kt
@@ -1,16 +1,25 @@
 package org.http4k.connect.mcp
 
+import org.http4k.connect.mcp.util.McpJson
+import org.http4k.core.Body
 import org.http4k.filter.debug
+import org.http4k.format.jsonRpcRequest
 import org.http4k.routing.sse
 import org.http4k.server.Helidon
 import org.http4k.server.asServer
 import org.http4k.sse.SseMessage.Event
 
 fun main() {
-    sse {
+    val sse = sse {
+        val request = Body.jsonRpcRequest(McpJson).toLens()(it.connectRequest)
+
+        val jsonNode = request.params ?: McpJson.nullNode()
+
+        McpRpcMethod.of(request.method)
+
         it.send(Event("endpoint", "/message?sessionId=f9f60485-697e-4564-8c23-5a86dd174a92\t\n"))
         println(it.connectRequest.bodyString())
-    }.debug(debugStream = true).asServer(Helidon(4001)).start()
+    }
+
+    sse.debug(debugStream = true).asServer(Helidon(4001)).start()
 }
-
-
