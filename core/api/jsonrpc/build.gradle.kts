8305e474eb6274f5908f1766582aa8325447b4c6
David Denton <denton.david@gmail.com>
2025-01-12 19:49:41 +0000

    remove moshi

--- core/api/jsonrpc/build.gradle.kts
+++ core/api/jsonrpc/build.gradle.kts
@@ -12,6 +12,5 @@ dependencies {
     api(project(":http4k-core"))
     api(project(":http4k-format-core"))
     testImplementation(testFixtures(project(":http4k-core")))
-    testImplementation(project(":http4k-format-moshi"))
 }
 
