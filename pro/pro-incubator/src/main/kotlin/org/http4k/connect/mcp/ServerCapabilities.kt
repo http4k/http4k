97af5271c20ef48feaeb4f72af291422078ce69b
David Denton <denton.david@gmail.com>
2025-01-13 15:34:05 +0000

    tweak mcp into some type of order

--- pro/pro-incubator/src/main/kotlin/org/http4k/connect/mcp/ServerCapabilities.kt
+++ pro/pro-incubator/src/main/kotlin/org/http4k/connect/mcp/ServerCapabilities.kt
@@ -1,14 +1,14 @@
 package org.http4k.connect.mcp
 
 data class ServerCapabilities(
-    val tools: Tools? = null,
-    val prompts: Prompts? = null,
-    val resources: Resources? = null,
+    val tools: Tools = Tools(),
+    val prompts: Prompts = Prompts(),
+    val resources: Resources = Resources(),
     val experimental: Unit? = null,
     val logging: Unit? = null,
     val sampling: Unit? = null,
 ) {
-    data class Tools(val listChanged: Boolean? = false)
-    data class Prompts(val listChanged: Boolean = false)
-    data class Resources(val subscribe: Boolean? = false, val listChanged: Boolean? = false)
+    data class Tools(val listChanged: Boolean? = true)
+    data class Prompts(val listChanged: Boolean = true)
+    data class Resources(val subscribe: Boolean? = true, val listChanged: Boolean? = true)
 }
