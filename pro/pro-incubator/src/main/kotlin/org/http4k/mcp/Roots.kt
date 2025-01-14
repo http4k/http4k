0c2bead7ba915dee629cab708320ac277b4de7ba
David Denton <denton.david@gmail.com>
2025-01-14 18:31:24 +0000

    Add roots updating

--- pro/pro-incubator/src/main/kotlin/org/http4k/mcp/Roots.kt
+++ pro/pro-incubator/src/main/kotlin/org/http4k/mcp/Roots.kt
@@ -1,13 +1,10 @@
 package org.http4k.mcp
 
 import org.http4k.connect.mcp.Root
+import org.http4k.util.ObservableList
 
-class Roots : Iterable<Root> {
-    private var list = emptyList<Root>()
-
+class Roots : ObservableList<Root>(emptyList()) {
     fun update(req: Root.List.Response) {
-        list = req.roots
+        items = req.roots
     }
-
-    override fun iterator() = list.iterator()
 }
