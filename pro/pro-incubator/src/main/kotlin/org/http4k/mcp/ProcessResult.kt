a2d230f5f2a907ded636e27336b63de36d4caa55
David Denton <denton.david@gmail.com>
2025-01-18 14:07:54 +0000

    Remove excess code

--- pro/pro-incubator/src/main/kotlin/org/http4k/mcp/ProcessResult.kt
+++
@@ -1,8 +0,0 @@
-package org.http4k.mcp
-
-import org.http4k.core.Status
-
-sealed interface ProcessResult {
-    data object Ok : ProcessResult
-    data class Fail(val status: Status) : ProcessResult
-}
