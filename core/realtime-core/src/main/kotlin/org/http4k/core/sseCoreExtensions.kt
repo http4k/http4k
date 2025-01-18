fb4f0290cc4d97484cf4459fb173fb484c159e4e
David Denton <denton.david@gmail.com>
2025-01-18 12:47:20 +0000

    Adding filters to polyhandler

--- core/realtime-core/src/main/kotlin/org/http4k/core/sseCoreExtensions.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/core/sseCoreExtensions.kt
@@ -1,6 +1,8 @@
 package org.http4k.core
 
+import org.http4k.sse.SseFilter
 import org.http4k.sse.SseResponse
+import org.http4k.sse.then
 import java.time.Duration
 import java.time.Instant
 
@@ -13,3 +15,5 @@ data class SseTransaction(
 ) : ProtocolTransaction<SseResponse> {
     fun label(name: String, value: String) = copy(labels = labels + (name to value))
 }
+
+fun SseFilter.then(poly: PolyHandler): PolyHandler = poly.copy(sse = poly.sse?.let { then(it) })
--- core/realtime-core/src/main/kotlin/org/http4k/routing/websocket/bind.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/routing/websocket/bind.kt
@@ -5,7 +5,7 @@ import org.http4k.routing.RoutingWsHandler
 import org.http4k.routing.TemplatedWsRoute
 import org.http4k.websocket.WsHandler
 
-infix fun String.bind(handler: RoutingWsHandler) = handler.withBasePath(this)
 infix fun String.bind(action: WsHandler) =
     RoutingWsHandler(listOf(TemplatedWsRoute(UriTemplate.from(this), action)))
 
+infix fun String.bind(handler: RoutingWsHandler) = handler.withBasePath(this)
