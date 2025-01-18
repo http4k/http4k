0e08ae15414b0efdca48827c358c13463126f1df
David Denton <denton.david@gmail.com>
2025-01-18 10:42:40 +0000

    Fix timestamp millis conversion to millis instead of second

--- connect/core/client/src/main/kotlin/org/http4k/connect/model/TimestampMillis.kt
+++ connect/core/client/src/main/kotlin/org/http4k/connect/model/TimestampMillis.kt
@@ -9,7 +9,7 @@ import java.time.Instant
  * Note: This timestamp is measured to the epoch milli
  */
 class TimestampMillis private constructor(value: Long) : LongValue(value) {
-    fun toInstant(): Instant = Instant.ofEpochSecond(value)
+    fun toInstant(): Instant = Instant.ofEpochMilli(value)
 
     companion object : LongValueFactory<TimestampMillis>(::TimestampMillis, 0L.minValue) {
         fun of(value: Instant) = TimestampMillis(value.toEpochMilli())
