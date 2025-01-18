7dcd30c79d5beaeec29ff6016f9bb99b063d50c4
David Denton <denton.david@gmail.com>
2025-01-18 18:58:14 +0000

    Extract various bindings

--- pro/pro-incubator/src/test/kotlin/org/http4k/connect/mcp/main.kt
+++ pro/pro-incubator/src/test/kotlin/org/http4k/connect/mcp/main.kt
@@ -30,32 +30,45 @@ fun main() {
             McpEntity("mcp-kotlin test server", Version.of("0.1.0")),
             LATEST_VERSION,
         ),
-        Prompt("prompt1", "description1") bind {
-            PromptResponse("description", listOf(Message(Role.assistant, Content.Text(it.input.toString()))))
-        },
-        Prompt("prompt1", "description1") bind {
-            PromptResponse("description", listOf(Message(Role.assistant, Content.Text(it.input.toString()))))
-        },
-        Resource.Static(Uri.of("https://www.http4k.org"), "HTTP4K", "description") bind LinksOnPage(JavaHttpClient()),
-        Resource.Templated(
-            Uri.of("https://www.http4k.org/ecosystem/{+ecosystem}/"),
-            "HTTP4K ecosystem page",
-            "view ecosystem"
-        ) bind LinksOnPage(JavaHttpClient()),
-        Tool(
-            "reverse",
-            "description",
-            Reverse("name")
-        ) bind { ToolResponse.Ok(listOf(Content.Text(it.input.input.reversed()))) },
-        Tool("count", "description", Multiply(1, 2)) bind {
-            ToolResponse.Ok(listOf(Content.Text(it.input.first + it.input.second)))
-        },
-        ModelSelector(ModelName.of("my model")) { 1 } bind {
-            SampleResponse(ModelName.of("my model"), StopReason.of("stop"), Role.assistant, Content.Text("content"))
-        }
+        prompt1(),
+        prompt2(),
+        staticResource(),
+        templatedResource(),
+        reverseTool(),
+        countingTool(),
+        llm()
     )
 
     mcpServer.debug(debugStream = true).asServer(Helidon(3001)).start()
+}
+
+private fun llm() = ModelSelector(ModelName.of("my model")) { 1 } bind {
+    SampleResponse(ModelName.of("my model"), StopReason.of("stop"), Role.assistant, Content.Text("content"))
+}
+
+private fun countingTool() = Tool("count", "description", Multiply(1, 2)) bind {
+    ToolResponse.Ok(listOf(Content.Text(it.input.first + it.input.second)))
+}
+
+private fun reverseTool() = Tool("reverse", "description", Reverse("name")) bind {
+    ToolResponse.Ok(listOf(Content.Text(it.input.input.reversed())))
+}
+
+private fun templatedResource() = Resource.Templated(
+    Uri.of("https://www.http4k.org/ecosystem/{+ecosystem}/"),
+    "HTTP4K ecosystem page",
+    "view ecosystem"
+) bind LinksOnPage(JavaHttpClient())
+
+private fun staticResource() =
+    Resource.Static(Uri.of("https://www.http4k.org"), "HTTP4K", "description") bind LinksOnPage(JavaHttpClient())
+
+private fun prompt1() = Prompt("prompt1", "description1") bind {
+    PromptResponse("description", listOf(Message(Role.assistant, Content.Text(it.input.toString()))))
+}
+
+private fun prompt2() = Prompt("prompt2", "description1") bind {
+    PromptResponse("description", listOf(Message(Role.assistant, Content.Text(it.input.toString()))))
 }
 
 data class Reverse(val input: String)
