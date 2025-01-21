cf35398b6c201b82faedb7149ee9e6172d061c33
David Denton <denton.david@gmail.com>
2025-01-21 08:39:14 +0000

    Split MCP into desktop and sdk

--- pro/mcp/build.gradle.kts
+++ pro/mcp/build.gradle.kts
@@ -1,27 +1,10 @@
-import org.http4k.internal.ModuleLicense.Http4kCommercial
-
-description = "http4k MCP server support"
-
-val license by project.extra { Http4kCommercial }
-
 plugins {
-    id("org.http4k.pro")
-    id("org.http4k.connect.module")
+    id("org.http4k.conventions")
 }
 
 dependencies {
-    api(project(":http4k-realtime-core"))
-    api(project(":http4k-format-jackson"))
-    api(project(":http4k-api-jsonrpc"))
-    api(project(":http4k-api-jsonschema"))
-    api(project(":http4k-connect-core"))
-    api("dev.forkhandles:values4k")
-    api("dev.forkhandles:result4k")
-
+    testImplementation(project(":http4k-mcp-sdk"))
+    testImplementation(project(":http4k-mcp-desktop"))
     testImplementation(project(":http4k-server-helidon"))
-
-    testImplementation("org.jsoup:jsoup:_")
-
     testImplementation(testFixtures(project(":http4k-core")))
-    testImplementation(project(":http4k-testing-approval"))
 }
---
+++ pro/mcp/desktop/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.http4k.internal.ModuleLicense.Http4kCommercial
+
+description = "http4k MCP server support"
+
+val license by project.extra { Http4kCommercial }
+
+plugins {
+    id("org.http4k.pro")
+    id("org.http4k.connect.module")
+//    id("org.graalvm.buildtools.native") version "0.9.28"
+}
+
+//graalvmNative {
+//    toolchainDetection.set(true)
+//    binaries {
+//        named("main") {
+//            imageName.set("rebackupable")
+//            mainClass.set("rebackupable.RebackupableCLI")
+//            useFatJar.set(true)
+//        }
+//    }
+//}
+
+dependencies {
+    api(project(":http4k-realtime-core"))
+//    api(project(":http4k-format-jackson"))
+//    api(project(":http4k-api-jsonrpc"))
+//    api(project(":http4k-api-jsonschema"))
+//    api(project(":http4k-connect-core"))
+//    api("dev.forkhandles:values4k")
+//    api("dev.forkhandles:result4k")
+
+//    testImplementation(project(":http4k-server-helidon"))
+//
+//    testImplementation("org.jsoup:jsoup:_")
+//
+//    testImplementation(testFixtures(project(":http4k-core")))
+//    testImplementation(project(":http4k-testing-approval"))
+}
