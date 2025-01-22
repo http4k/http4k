85210f2882a55744fb752252c9c4859a8fefd62f
David Denton <denton.david@gmail.com>
2025-01-22 18:01:09 +0000

    Remove Client Sse Connection

--- pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/server/ClientSseConnection.kt
+++
@@ -1,8 +0,0 @@
-package org.http4k.mcp.server
-
-import org.http4k.mcp.processing.McpMessageHandler
-import org.http4k.sse.Sse
-
-class ClientSseConnection<NODE : Any>(val sse: Sse, val handler: McpMessageHandler<NODE>) {
-
-}
