4a600add374bd19aa1c82c53270a96ffe9586957
David Denton <denton.david@gmail.com>
2025-01-22 18:52:30 +0000

    Extracting common logic from protocol

--- pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/server/ClientCapabilities.kt
+++ pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/server/ClientCapabilities.kt
@@ -4,11 +4,11 @@ package org.http4k.mcp.server
  * Determines what features the client supports.
  */
 data class ClientCapabilities(
-    val roots: RootCapabilities? = null,
+    val roots: Root? = null,
     val experimental: Unit? = null,
     val sampling: Unit? = null,
 ) {
     companion object {
-        data class RootCapabilities(val listChanged: Boolean? = false)
+        data class Root(val listChanged: Boolean? = false)
     }
 }
