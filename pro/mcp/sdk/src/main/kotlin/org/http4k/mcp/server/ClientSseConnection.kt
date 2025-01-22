7f625f0fa36cbfeff2c49655d46dd64241872b0e
David Denton <denton.david@gmail.com>
2025-01-22 17:50:09 +0000

    Rejig to use handler sensibly

--- pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/server/ClientSseConnection.kt
+++ pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/server/ClientSseConnection.kt
@@ -1,18 +1,12 @@
 package org.http4k.mcp.server
 
-import org.http4k.jsonrpc.JsonRpcRequest
 import org.http4k.mcp.processing.McpMessageHandler
-import org.http4k.mcp.protocol.ClientMessage.Request
 import org.http4k.mcp.protocol.HasMethod
 import org.http4k.mcp.protocol.ServerMessage
 import org.http4k.mcp.protocol.ServerMessage.Notification
-import org.http4k.mcp.protocol.ServerMessage.Response
 import org.http4k.sse.Sse
 
 class ClientSseConnection<NODE : Any>(val sse: Sse, val handler: McpMessageHandler<NODE>) {
-    inline fun <reified IN : Request, OUT : Response> send(req: JsonRpcRequest<NODE>, fn: (IN) -> OUT) =
-        sse.send(handler(req, fn))
-
     fun send(hasMethod: HasMethod, req: ServerMessage.Request, id: NODE? = null) = sse.send(
         handler(hasMethod, req, id)
     )
