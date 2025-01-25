81ba3ae4f6238077ae531f94381ce580c23caac7
David Denton <denton.david@gmail.com>
2025-01-25 23:51:38 +0000

    Switch to concurrent hashmap

--- pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/util/ObservableList.kt
+++ pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/util/ObservableList.kt
@@ -1,10 +1,11 @@
 package org.http4k.mcp.util
 
 import org.http4k.mcp.protocol.SessionId
+import java.util.concurrent.ConcurrentHashMap
 import kotlin.properties.Delegates
 
 abstract class ObservableList<T>(initial: List<T>) : Iterable<T> {
-    private val callbacks = mutableMapOf<SessionId, () -> Any>()
+    private val callbacks = ConcurrentHashMap<SessionId, () -> Any>()
 
     var items by Delegates.observable(initial) { _, _, _ -> callbacks.values.forEach { it() } }
 
