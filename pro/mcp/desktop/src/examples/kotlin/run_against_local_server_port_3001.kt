4bdc17af78a106d971c491d5ebdaca9463e35f16
David Denton <denton.david@gmail.com>
2025-01-27 17:34:51 +0000

    Turn off debug

--- pro/mcp/desktop/src/examples/kotlin/run_against_local_server_port_3001.kt
+++ pro/mcp/desktop/src/examples/kotlin/run_against_local_server_port_3001.kt
@@ -1,4 +1,4 @@
-import org.http4k.filter.debug
+import org.http4k.mcp.McpDesktop
 import org.http4k.mcp.model.McpEntity
 import org.http4k.mcp.protocol.ServerMetaData
 import org.http4k.mcp.protocol.Version
@@ -7,6 +7,6 @@ import org.http4k.server.Helidon
 import org.http4k.server.asServer
 
 fun main() {
-    val server = mcpHttp(ServerMetaData(McpEntity.of("foo"), Version.of("bar"))).debug(debugStream = true).asServer(Helidon(12000)).start()
-//    McpDesktop.main("--url", "http://localhost:${server.port()}", "--debug")
+    val server = mcpHttp(ServerMetaData(McpEntity.of("foo"), Version.of("bar"))).asServer(Helidon(3001)).start()
+    McpDesktop.main("--url", "http://localhost:${server.port()}")
 }
