8a4840482cf52ff5f5e9ef4511d94f41f98fa9a7
Ivan Sanchez <ivan@gourame.com>
2025-01-28 09:36:12 +0000

    Use new RequestContext in TransactorPerRequestFilter

--- pro/incubator/db/src/main/kotlin/org/http4k/db/TransactorPerRequestFilter.kt
+++ pro/incubator/db/src/main/kotlin/org/http4k/db/TransactorPerRequestFilter.kt
@@ -3,29 +3,26 @@ package org.http4k.db
 import org.http4k.core.Filter
 import org.http4k.core.NoOp
 import org.http4k.core.Request
-import org.http4k.core.RequestContexts
 import org.http4k.core.then
 import org.http4k.core.with
-import org.http4k.filter.ServerFilters.InitialiseRequestContext
 import org.http4k.lens.BiDiLens
 import org.http4k.lens.RequestContextKey
+import org.http4k.lens.RequestKey
 
 private const val KEY = "http4k-db-transactor"
 
-fun <T> TransactionPerRequestFilter(contexts: RequestContexts, transactor: Transactor<T>) =
+fun <T : Any> TransactionPerRequestFilter(transactor: Transactor<T>) =
     Filter.NoOp
-        .then(InitialiseRequestContext(contexts))
         .then(Filter { next ->
             { request: Request ->
-                val lens: BiDiLens<Request, T> = transactionResourceLens(contexts)
+                val lens: BiDiLens<Request, T> = transactionResourceLens()
                 transactor.perform { resource ->
                     next(request.with(lens of resource))
                 }
             }
         })
 
-fun <T> transactionResourceLens(contexts: RequestContexts) = RequestContextKey.required<T>(contexts, KEY)
+fun <T : Any> transactionResourceLens(): BiDiLens<Request, T> = RequestKey.of(KEY)
 
-fun <Resource> Request.transactionResource(contexts: RequestContexts) =
-    transactionResourceLens<Resource>(contexts)(this)
+fun <Resource : Any> Request.transactionResource() = transactionResourceLens<Resource>()(this)
 
--- pro/incubator/db/src/test/kotlin/org/http4k/db/TransactorFilterTest.kt
+++ pro/incubator/db/src/test/kotlin/org/http4k/db/TransactorFilterTest.kt
@@ -1,44 +1,28 @@
 package org.http4k.db
 
-import org.http4k.client.JavaHttpClient
-import org.http4k.core.Filter
-import org.http4k.core.Method
+import org.http4k.core.*
 import org.http4k.core.Method.GET
-import org.http4k.core.NoOp
-import org.http4k.core.Request
-import org.http4k.core.RequestContexts
-import org.http4k.core.Response
-import org.http4k.core.Status
-import org.http4k.core.then
 import org.http4k.db.testing.AccountRepository
 import org.http4k.db.testing.PlainSqlAccountRepository
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
-import org.http4k.server.SunHttp
-import org.http4k.server.asServer
-
-class TransactorFilterTest {
-}
 
 fun main() {
-    val contexts = RequestContexts()
     val transactor = DataSourceTransactor<AccountRepository>(createDataSource(), ::PlainSqlAccountRepository)
 
     val app = routes(
         "/balance/{account}" bind GET to
             { req: Request ->
-                val repository: AccountRepository = req.transactionResource(contexts)
+                val repository: AccountRepository = req.transactionResource()
 
                 val balance = repository.getBalance(req.path("account")!!)
                 Response(Status.OK).body(balance.toString())
             })
 
-    Filter.NoOp
-        .then(TransactionPerRequestFilter(contexts, transactor))
+    val server = Filter.NoOp
+        .then(TransactionPerRequestFilter(transactor))
         .then(app)
-        .asServer(SunHttp(8000))
-        .start()
 
-    JavaHttpClient()(Request(GET, "http://localhost:8000/balance/DEPOSITS")).let(::println)
+    server(Request(GET, "/balance/DEPOSITS")).let(::println)
 }
