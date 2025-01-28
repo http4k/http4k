b80a1262f0f4c42727ed830dbc54c4294a9b202e
David Denton <denton.david@gmail.com>
2025-01-28 07:39:43 +0000

    Remove default values for request and response keys

--- core/core/src/main/kotlin/org/http4k/lens/request.kt
+++ core/core/src/main/kotlin/org/http4k/lens/request.kt
@@ -3,12 +3,11 @@ package org.http4k.lens
 import org.http4k.core.Request
 import org.http4k.lens.ParamMeta.ObjectParam
 import org.http4k.routing.RequestWithContext
-import java.util.UUID
 
 typealias RequestLens<T> = BiDiLens<Request, T>
 
 object RequestKey {
-    fun <T : Any> of(name: String = UUID.randomUUID().toString()): RequestLens<T> {
+    fun <T : Any> of(name: String): RequestLens<T> {
         val meta = Meta(true, "context", ObjectParam, name, null, emptyMap())
         val get: (Request) -> T = { target ->
             @Suppress("UNCHECKED_CAST")
--- core/core/src/main/kotlin/org/http4k/lens/response.kt
+++ core/core/src/main/kotlin/org/http4k/lens/response.kt
@@ -3,12 +3,11 @@ package org.http4k.lens
 import org.http4k.core.Response
 import org.http4k.lens.ParamMeta.ObjectParam
 import org.http4k.routing.ResponseWithContext
-import java.util.UUID
 
 typealias ResponseLens<T> = BiDiLens<Response, T>
 
 object ResponseKey {
-    fun <T : Any> of(name: String = UUID.randomUUID().toString()): ResponseLens<T> {
+    fun <T : Any> of(name: String): ResponseLens<T> {
         val meta = Meta(true, "context", ObjectParam, name, null, emptyMap())
         val get: (Response) -> T = { target ->
             @Suppress("UNCHECKED_CAST")
