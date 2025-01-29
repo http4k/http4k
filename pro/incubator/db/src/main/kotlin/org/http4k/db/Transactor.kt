4ed378391d7d306cb9ef9475f097a188fbe27ce1
Ivan Sanchez <ivan@gourame.com>
2025-01-29 13:32:49 +0000

    Refactor

--- pro/incubator/db/src/main/kotlin/org/http4k/db/Transactor.kt
+++ pro/incubator/db/src/main/kotlin/org/http4k/db/Transactor.kt
@@ -1,20 +1,25 @@
 package org.http4k.db
 
-import org.http4k.core.Filter
-import org.http4k.core.NoOp
 import org.http4k.core.Request
-import org.http4k.core.RequestContexts
-import org.http4k.core.then
 import org.http4k.core.with
 import org.http4k.db.Transactor.Mode.ReadWrite
-import org.http4k.filter.ServerFilters.InitialiseRequestContext
-import org.http4k.lens.BiDiLens
-import org.http4k.lens.RequestContextKey
+import org.http4k.lens.RequestKey
 
 interface Transactor<out Resource> {
     enum class Mode { ReadOnly, ReadWrite }
 
     fun <T> perform(mode: Mode = ReadWrite, work: (Resource) -> T): T
+
+    companion object {
+        private const val KEY = "http4k-db-transactor"
+
+        fun <Resource : Any> transactionResourceFor(request: Request): Resource = RequestKey.of<Resource>(KEY)(request)
+
+        fun <Resource : Any> withTransactionResource(request: Request, resource: Resource): Request =
+            request.with(
+                RequestKey.of<Resource>(KEY) of resource
+            )
+    }
 }
 
 
