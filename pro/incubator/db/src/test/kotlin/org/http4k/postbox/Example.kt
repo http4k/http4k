879121a3d3b0a6628e829fbf7087e60fc571a6d5
Ivan Sanchez <ivan@gourame.com>
2025-01-30 20:11:38 +0000

    Add example

---
+++ pro/incubator/db/src/test/kotlin/org/http4k/postbox/Example.kt
@@ -0,0 +1,59 @@
+package org.http4k.postbox
+
+import org.http4k.client.JavaHttpClient
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.POST
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.core.UriTemplate
+import org.http4k.db.InMemoryTransactor
+import org.http4k.db.Transactor
+import org.http4k.routing.bind
+import org.http4k.routing.path
+import org.http4k.routing.routes
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+import java.util.UUID
+import java.util.concurrent.Executors
+
+
+val statusUriTemplate = UriTemplate.from("/api/status/{requestId}")
+
+fun main() {
+    ThirdPartySlowService().asServer(SunHttp(8000)).start()
+
+    val transactor = InMemoryTransactor<Postbox>(InMemoryPostbox()).also {
+        startOutboxBackgroundProcessor(it)
+    }
+
+    val transactionalOutbox = PostboxInterceptorHandler(transactor, requestIdResolver(), statusUriTemplate)
+
+    routes(
+        "/api/reverse/{message}" bind POST to MyServiceHandler(transactionalOutbox),
+        PostboxStatusHandler(transactor, statusUriTemplate)
+    ).asServer(SunHttp(9000)).start()
+}
+
+fun ThirdPartySlowService() = { req: Request -> Thread.sleep(10000); Response(OK).body(req.bodyString().reversed()) }
+
+fun startOutboxBackgroundProcessor(transactor: Transactor<Postbox>) {
+    Executors.newSingleThreadExecutor().execute {
+        while (true) {
+            ProcessPendingRequests(transactor, JavaHttpClient())
+            Thread.sleep(5000)
+        }
+    }
+}
+
+private fun requestIdResolver() = { req: Request ->
+    RequestId.of(req.header("x-idempotency-key") ?: UUID.randomUUID().toString())
+}
+
+private fun MyServiceHandler(client: HttpHandler) = { request: Request ->
+    val message = request.path("message")!!
+    println("Received message: $message with identity ${request.header("x-idempotency-key")}")
+    client(Request(POST, "http://localhost:8000")
+        .header("x-idempotency-key", request.header("x-idempotency-key"))
+        .body(message))
+}
