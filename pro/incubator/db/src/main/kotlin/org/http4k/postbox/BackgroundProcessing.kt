1bcdf4c1c73acd11953bc0d4f49adc823c44de8e
Ivan Sanchez <ivan@gourame.com>
2025-01-31 09:48:59 +0000

    Extract PostboxProcessing

--- pro/incubator/db/src/main/kotlin/org/http4k/postbox/BackgroundProcessing.kt
+++ pro/incubator/db/src/main/kotlin/org/http4k/postbox/BackgroundProcessing.kt
@@ -5,12 +5,18 @@ import dev.forkhandles.result4k.Result
 import org.http4k.core.HttpHandler
 import org.http4k.core.Response
 import org.http4k.db.Transactor
+import org.http4k.events.Event
+import org.http4k.events.Events
+import java.util.concurrent.Executors
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.measureTime
 
 
 fun ProcessPendingRequests(
     transactor: Transactor<Postbox>,
     finalServer: HttpHandler
-) = transactor.perform { postbox ->
+): Unit = transactor.perform { postbox ->
     // TODO: define a limit for the number of requests to process
     // TODO: exclude requests marked as permanent failures
     // TODO: mark requests as "processing" to allow for multiple instances of this function to run concurrently?
@@ -29,6 +35,43 @@ fun ProcessPendindRequest(
         postbox.markProcessed(pending.requestId, response)
     } else {
         Failure(PostboxError.RequestProcessingFailure("response was not successful"))
+    }
+}
+
+sealed class ProcessingEvent: Event{
+    data class BatchProcessed(val batchSize: Int, val duration: Duration): ProcessingEvent()
+    data class PollWait(val duration: Duration): ProcessingEvent()
+}
+
+class PostboxProcessing(
+    private val transactor: Transactor<Postbox>,
+    private val target: HttpHandler,
+    private val batchSize: Int = 10,
+    private val maxPollingTime: Duration = 5.seconds,
+    private val events: Events = { }
+) : Runnable {
+    private var running = true
+
+    override fun run() {
+        while (running) {
+            val elapsedTime = measureTime {
+                ProcessPendingRequests(transactor, target)
+            }
+            events(ProcessingEvent.BatchProcessed(batchSize, elapsedTime))
+            val remainingTime = maxPollingTime - elapsedTime
+            if (remainingTime > Duration.ZERO) {
+                events(ProcessingEvent.PollWait(remainingTime))
+                Thread.sleep(remainingTime.inWholeMilliseconds)
+            }
+        }
+    }
+
+    fun stop() {
+        running = false
+    }
+
+    fun start() {
+        Executors.newVirtualThreadPerTaskExecutor().execute(this)
     }
 }
 
--- pro/incubator/db/src/test/kotlin/org/http4k/postbox/Example.kt
+++ pro/incubator/db/src/test/kotlin/org/http4k/postbox/Example.kt
@@ -8,14 +8,13 @@ import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.UriTemplate
 import org.http4k.db.InMemoryTransactor
-import org.http4k.db.Transactor
+import org.http4k.events.StdOutEvents
 import org.http4k.routing.bind
 import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
-import java.util.UUID
-import java.util.concurrent.Executors
+import java.util.*
 
 
 val statusUriTemplate = UriTemplate.from("/api/status/{requestId}")
@@ -25,7 +24,7 @@ fun main() {
 
     val transactor = InMemoryTransactor<Postbox>(InMemoryPostbox()).also {
         // please notice in-memory transactor locks the postbox so multiple threads can't access it at the same time
-        startOutboxBackgroundProcessor(it)
+        PostboxProcessing(it, JavaHttpClient(), events = StdOutEvents).start()
     }
 
     val transactionalOutbox = PostboxInterceptorHandler(transactor, requestIdResolver(), statusUriTemplate)
@@ -38,14 +37,6 @@ fun main() {
 
 fun ThirdPartySlowService() = { req: Request -> Thread.sleep(10000); Response(OK).body(req.bodyString().reversed()) }
 
-fun startOutboxBackgroundProcessor(transactor: Transactor<Postbox>) {
-    Executors.newVirtualThreadPerTaskExecutor().execute {
-        while (true) {
-            ProcessPendingRequests(transactor, JavaHttpClient())
-            Thread.sleep(5000)
-        }
-    }
-}
 
 private fun requestIdResolver() = { req: Request ->
     RequestId.of(req.header("x-idempotency-key") ?: UUID.randomUUID().toString())
@@ -56,9 +47,11 @@ private fun MyServiceHandler(client: HttpHandler) = { request: Request ->
 
     // Makes the request to the third party service.
     // If the client is a transactional outbox, rather than the real thing:
-        // On first call it'll store the request and return a 202 with a Link header to check the status of the request.
-        // On subsequent calls it'll return either 202 again or the response from the third party service.
-    client(Request(POST, "http://localhost:8000")
-        .header("x-idempotency-key", request.header("x-idempotency-key"))
-        .body(message))
+    // On first call it'll store the request and return a 202 with a Link header to check the status of the request.
+    // On subsequent calls it'll return either 202 again or the response from the third party service.
+    client(
+        Request(POST, "http://localhost:8000")
+            .header("x-idempotency-key", request.header("x-idempotency-key"))
+            .body(message)
+    )
 }
