1e3adfb5c146a2093b32cb43f7142ddea6832b03
Ivan Sanchez <ivan@gourame.com>
2025-01-31 11:35:43 +0000

    Split id resolver for interceptor and status handlers

--- pro/incubator/db/src/test/kotlin/org/http4k/postbox/ExampleOutbox.kt
+++ pro/incubator/db/src/test/kotlin/org/http4k/postbox/ExampleOutbox.kt
@@ -7,7 +7,7 @@ import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.UriTemplate
+import org.http4k.core.Uri
 import org.http4k.db.InMemoryTransactor
 import org.http4k.events.StdOutEvents
 import org.http4k.routing.bind
@@ -15,44 +15,43 @@ import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
-import java.util.*
 
-
-val statusUriTemplate = UriTemplate.from("/api/status/{requestId}")
 
 fun main() {
-    ThirdPartySlowService().asServer(SunHttp(8000)).start()
+    ThirdPartySlowSmsService().asServer(SunHttp(8000)).start()
 
     val transactor = InMemoryTransactor<Postbox>(InMemoryPostbox()).also { transactor ->
         // please notice in-memory transactor locks the postbox so multiple threads can't access it at the same time
         PostboxProcessing(transactor, JavaHttpClient(), events = StdOutEvents).start()
     }
 
-    val transactionalOutbox = PostboxHandlers(transactor, requestIdResolver(), statusUriTemplate)
+    val transactionalOutbox = PostboxHandlers(transactor,
+        { it.header("x-order-id")?.let(RequestId.Companion::of) },
+        { Uri.of("/api/notificationStatus/${it.value}") },
+        { it.path("orderId")?.let(RequestId.Companion::of) }
+    )
 
     routes(
-        "/api/reverse/{message}" bind POST to MyServiceHandler(transactionalOutbox.interceptor),
-        "/api/status/{requestId}" bind GET to transactionalOutbox.status
+        "/api/{orderId}/notify" bind POST to NotificationHandler(transactionalOutbox.interceptor),
+        "/api/notificationStatus/{orderId}" bind GET to transactionalOutbox.status
     ).asServer(SunHttp(9000)).start()
 }
 
-fun ThirdPartySlowService() = { req: Request -> Thread.sleep(10000); Response(OK).body(req.bodyString().reversed()) }
+fun ThirdPartySlowSmsService() = routes("/sms/send" bind POST to
+    { req: Request -> Thread.sleep(10000); Response(OK).body("SMS sent. Message= ${req.bodyString()}") })
 
-
-private fun requestIdResolver() = { req: Request ->
-    RequestId.of(req.header("x-idempotency-key") ?: UUID.randomUUID().toString())
-}
-
-private fun MyServiceHandler(client: HttpHandler) = { request: Request ->
-    val message = request.path("message")!!
+private fun NotificationHandler(smsClient: HttpHandler) = { request: Request ->
+    val message = request.bodyString()
 
     // Makes the request to the third party service.
     // If the client is a transactional outbox, rather than the real thing:
     // On first call it'll store the request and return a 202 with a Link header to check the status of the request.
     // On subsequent calls it'll return either 202 again or the response from the third party service.
-    client(
-        Request(POST, "http://localhost:8000")
-            .header("x-idempotency-key", request.header("x-idempotency-key"))
-            .body(message)
+    smsClient(
+        Request(POST, "http://localhost:8000/sms/send")
+            .header("x-order-id", request.path("orderId")!!)
+            .body("Notified a user with message '$message'")
     )
+
+    Response(OK).body("Notification accepted")
 }
