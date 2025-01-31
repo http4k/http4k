eabd97fb1bb134672bfac9b53d61ff869302aed4
Ivan Sanchez <ivan@gourame.com>
2025-01-31 10:41:34 +0000

    Add API docs

--- pro/incubator/db/src/main/kotlin/org/http4k/postbox/Handlers.kt
+++ pro/incubator/db/src/main/kotlin/org/http4k/postbox/Handlers.kt
@@ -16,11 +16,22 @@ import org.http4k.db.Transactor
 import org.http4k.db.performAsResult
 import java.util.*
 
+/**
+ * Configures HTTP handlers for a transactional postbox.
+ */
 class PostboxHandlers(
     private val transactor: Transactor<Postbox>,
     private val requestIdResolver: (Request) -> RequestId  = { RequestId.of(UUID.randomUUID().toString()) },
     private val statusTemplate: UriTemplate = UriTemplate.from("/postbox/{requestId}")
 ) {
+
+    /**
+     * The interceptor to be used in the application to store requests in the postbox.
+     * It relies on the `requestIdResolver` to define the identity of the request and allow for idempotency.
+     *
+     * It'll return a 202 with a Link header to check the status of the request.
+     * If the request has already been processed, it'll return the response obtained as part of processing it.
+     */
     val interceptor: HttpHandler = { request: Request ->
         val requestId = requestIdResolver(request)
         transactor.performAsResult { it.store(Postbox.PendingRequest(requestId, request)) }
@@ -31,6 +42,13 @@ class PostboxHandlers(
             .get()
     }
 
+    /**
+     * Allows to check the status of a request.
+     *
+     * If the request is pending, It'll return a 202 with a Link header to check the status of the request.
+     * If the request has already been processed, it'll return the response obtained as part of processing it.
+     * If the request is not found, it'll return a 404.
+     */
     val status: HttpHandler = { req: Request ->
         RequestId.lens(req)
             .mapFailure { Response(BAD_REQUEST.description(it.message.orEmpty())) }
