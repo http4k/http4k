54ad6c50c935134a3592d3843886a614053db94e
Ivan Sanchez <ivan@gourame.com>
2025-01-31 16:30:49 +0000

    Import

--- pro/incubator/db/src/test/kotlin/org/http4k/postbox/ExampleInbox.kt
+++ pro/incubator/db/src/test/kotlin/org/http4k/postbox/ExampleInbox.kt
@@ -5,7 +5,6 @@ import org.http4k.core.Method.POST
 import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.core.UriTemplate
 import org.http4k.core.UriTemplate.Companion.from
 import org.http4k.db.InMemoryTransactor
 import org.http4k.events.StdOutEvents
