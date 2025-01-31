8c28ce66bb14e8035257a89f79a94e63590290c0
Ivan Sanchez <ivan@gourame.com>
2025-01-31 08:39:49 +0000

    Add comments

--- pro/incubator/db/src/main/kotlin/org/http4k/postbox/BackgroundProcessing.kt
+++ pro/incubator/db/src/main/kotlin/org/http4k/postbox/BackgroundProcessing.kt
@@ -11,6 +11,9 @@ fun ProcessPendingRequests(
     transactor: Transactor<Postbox>,
     finalServer: HttpHandler
 ) = transactor.perform { postbox ->
+    // TODO: define a limit for the number of requests to process
+    // TODO: exclude requests marked as permanent failures
+    // TODO: mark requests as "processing" to allow for multiple instances of this function to run concurrently?
     for (pending in postbox.pendingRequests()) {
         ProcessPendindRequest(postbox, pending, finalServer)
     }
