c67f7d2dec01e9d0f6640073af4ef80070bd8ce6
David Denton <denton.david@gmail.com>
2025-02-01 14:35:01 +0000

    Add moshi node data container

--- core/format/moshi/src/main/kotlin/dev/forkhandles/data/MoshiNodeDataContainer.kt
+++ core/format/moshi/src/main/kotlin/dev/forkhandles/data/MoshiNodeDataContainer.kt
@@ -4,13 +4,13 @@ import org.http4k.format.MoshiArray
 import org.http4k.format.MoshiBoolean
 import org.http4k.format.MoshiDecimal
 import org.http4k.format.MoshiInteger
+import org.http4k.format.MoshiLong
 import org.http4k.format.MoshiNode
 import org.http4k.format.MoshiNull
 import org.http4k.format.MoshiObject
 import org.http4k.format.MoshiString
 import java.math.BigDecimal
 import java.math.BigInteger
-import kotlin.Int.Companion.MIN_VALUE
 
 /**
  * Moshi MoshiNode-based implementation of the DataContainer
@@ -20,13 +20,10 @@ open class MoshiNodeDataContainer(input: MoshiNode) :
         input,
         { content, it -> (content as? MoshiObject)?.attributes?.containsKey(it) ?: false },
         { content, it ->
-            println("content: $content, it: $it")
             (content as? MoshiObject)?.attributes?.get(it)?.let { nodeToValue(it) }
         },
         { node, name, value ->
-            println("node: $node, name: $name, value: $value")
             (node as? MoshiObject)?.also { it.attributes[name] = value.toNode() }
-                ?.also { println("it: $it") }
                 ?: error("Invalid node type ${input::class.java}")
         }
     ) {
@@ -37,11 +34,12 @@ open class MoshiNodeDataContainer(input: MoshiNode) :
             is MoshiNode -> this
             is DataContainer<*> -> unwrap().toNode()
             is Boolean -> MoshiBoolean(this)
-            is Short -> MoshiInteger(toLong())
-            is Int -> MoshiInteger(toLong())
-            is Long -> MoshiInteger(this)
-            is BigInteger -> when (canConvertToLong()) {
-                true -> MoshiInteger(toLong())
+            is Short -> MoshiInteger(toInt())
+            is Int -> MoshiInteger(toInt())
+            is Long -> MoshiLong(this)
+            is BigInteger -> when {
+                canConvertToInt() -> MoshiInteger(toInt())
+                canConvertToLong() -> MoshiLong(toLong())
                 else -> MoshiString(toString())
             }
 
@@ -65,22 +63,17 @@ open class MoshiNodeDataContainer(input: MoshiNode) :
             is MoshiObject -> input
             is MoshiBoolean -> input.value
             is MoshiDecimal -> input.value
-            is MoshiInteger -> {
-                println(input)
-                when {
-                    input.canConvertToInt() -> input.value.toInt()
-                    else -> input.value
-                }
-            }
+            is MoshiLong -> input.value
+            is MoshiInteger -> input.value
         }
+
+        private fun BigInteger.canConvertToInt() =
+            (this >= BigInteger.valueOf(Int.MIN_VALUE.toLong())) && (this <= BigInteger.valueOf(Int.MAX_VALUE.toLong()))
 
         private fun BigInteger.canConvertToLong() =
             (this >= BigInteger.valueOf(Long.MIN_VALUE)) && (this <= BigInteger.valueOf(Long.MAX_VALUE))
 
         private fun BigDecimal.canConvertToDouble() =
             (this >= BigDecimal.valueOf(Double.MIN_VALUE)) && (this <= BigDecimal.valueOf(Double.MAX_VALUE))
-
-        private fun MoshiInteger.canConvertToInt() =
-            (value >= MIN_VALUE.toLong()) && (value <= Int.MAX_VALUE.toLong())
     }
 }
