ddfb79235c12209c2988806d43eba55fa8e1440d
Ivan Sanchez <ivan@gourame.com>
2025-02-04 10:31:55 +0000

    Check retrieval order

--- pro/incubator/db/src/main/kotlin/org/http4k/postbox/exposed/ExposedPostbox.kt
+++ pro/incubator/db/src/main/kotlin/org/http4k/postbox/exposed/ExposedPostbox.kt
@@ -15,18 +15,21 @@ import org.http4k.postbox.exposed.ExposedPostbox.Companion.PostboxTable.requestI
 import org.http4k.postbox.exposed.ExposedPostbox.Companion.PostboxTable.response
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.CustomFunction
+import org.jetbrains.exposed.sql.SortOrder.ASC
 import org.jetbrains.exposed.sql.SortOrder.DESC
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.isNull
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.insertReturning
 import org.jetbrains.exposed.sql.javatime.JavaInstantColumnType
 import org.jetbrains.exposed.sql.javatime.timestamp
+import org.jetbrains.exposed.sql.update
 import org.jetbrains.exposed.sql.upsertReturning
 import java.time.Instant
 
 class ExposedPostbox : Postbox {
-    companion object{
+    companion object {
         private val dbTimestampNow = object : CustomFunction<Instant>("now", JavaInstantColumnType()) {}
+
         object PostboxTable : Table("postbox") {
             val requestId: Column<String> = varchar("request_id", 36)
             val createdAt: Column<Instant> = timestamp("created_at").defaultExpression(dbTimestampNow)
@@ -39,7 +42,7 @@ class ExposedPostbox : Postbox {
     override fun store(pending: Postbox.PendingRequest): Result<RequestProcessingStatus, PostboxError> =
         PostboxTable.upsertReturning(
             returning = listOf(requestId, response),
-            onUpdateExclude = listOf(createdAt, request)
+            onUpdateExclude = listOf(request)
         ) { row ->
             row[requestId] = pending.requestId.value
             row[request] = pending.request.toString()
@@ -56,13 +59,16 @@ class ExposedPostbox : Postbox {
     }
 
     override fun markProcessed(requestId: RequestId, response: Response): Result<Unit, PostboxError> {
-        TODO("Not yet implemented")
+        PostboxTable.update(where = { PostboxTable.requestId eq requestId.value }) { row ->
+            row[PostboxTable.response] = response.toString()
+        }
+        return Success(Unit)
     }
 
     override fun pendingRequests(batchSize: Int) =
         PostboxTable.select(listOf(requestId, request))
             .where(response.isNull())
-            .orderBy(createdAt, DESC)
+            .orderBy(createdAt, ASC)
             .limit(batchSize)
             .map {
                 Postbox.PendingRequest(
