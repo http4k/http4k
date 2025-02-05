cb3675dc2fbb3422dd8d97f202da857577ab0759
Ivan Sanchez <ivan@gourame.com>
2025-02-05 08:31:34 +0000

    Add comment on why this extra encoding is required

--- core/aws/src/main/kotlin/org/http4k/aws/requestExtensions.kt
+++ core/aws/src/main/kotlin/org/http4k/aws/requestExtensions.kt
@@ -2,4 +2,6 @@ package org.http4k.aws
 
 import org.http4k.core.Request
 
+// AWS fail to match the signature if it contains a '+' character in the path
+// See https://jamesd3142.wordpress.com/2018/02/28/amazon-s3-and-the-plus-symbol/
 internal fun Request.encodePlusCharInPath() = uri(uri.path(uri.path.replace("+", "%2B")))
