6ba7d3c889e9bd3301409f78a7223aad7c6566f3
David Denton <denton.david@gmail.com>
2025-02-08 17:48:33 +0000

    Update first digit of lts version

--- bin/notify_lts_slack.sh
+++ bin/notify_lts_slack.sh
@@ -5,7 +5,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-if [[ $1 == 4* ]]; then
+if [[ $1 == 5* ]]; then
   MESSAGE="Released http4k LTS version $1 (See <https://github.com/http4k/http4k-lts-v4/blob/master/CHANGELOG.md|Changelog> for details)."
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$MESSAGE\"}" $LTS_SLACK_WEBHOOK
 else
