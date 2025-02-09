01ba0293c4d05f858f07175840ca64901182e31a
David Denton <denton.david@gmail.com>
2025-02-09 15:35:15 +0000

    Comment

--- pro/mcp/sdk/src/main/kotlin/org/http4k/lens/McpLensTarget.kt
+++ pro/mcp/sdk/src/main/kotlin/org/http4k/lens/McpLensTarget.kt
@@ -1,5 +1,8 @@
 package org.http4k.lens
 
+/**
+ * Represents a target for a Lens to extract or inject values from.
+ */
 interface McpLensTarget
 
 fun <T : McpLensTarget> T.with(vararg modifiers: (T) -> T): T = modifiers.fold(this) { memo, next -> next(memo) }
