752f94f6ca702ebe65f96be73708cdde58d4c14a
David Denton <denton.david@gmail.com>
2025-02-09 21:10:06 +0000

    Adding missing bind functions

--- core/realtime-core/src/main/kotlin/org/http4k/routing/bindExtensions.kt
+++ core/realtime-core/src/main/kotlin/org/http4k/routing/bindExtensions.kt
@@ -1,6 +1,7 @@
 package org.http4k.routing
 
 import org.http4k.core.HttpHandler
+import org.http4k.core.Method
 import org.http4k.routing.sse.bind
 import org.http4k.routing.websocket.bind
 import org.http4k.sse.SseHandler
@@ -8,9 +9,11 @@ import org.http4k.websocket.WsHandler
 
 infix fun String.bindHttp(action: HttpHandler): RoutingHttpHandler = this bind action
 infix fun String.bindHttp(action: RoutingHttpHandler): RoutingHttpHandler = this bind action
+infix fun String.bindHttp(method: Method): PathMethod = PathMethod(this, method)
 
 infix fun String.bindSse(action: SseHandler): RoutingSseHandler = this bind action
 infix fun String.bindSse(action: RoutingSseHandler): RoutingSseHandler = this bind action
+infix fun String.bindSse(method: Method): PathMethod = PathMethod(this, method)
 
 infix fun String.bindWs(action: WsHandler): RoutingWsHandler = this bind action
 infix fun String.bindWs(action: RoutingWsHandler): RoutingWsHandler = this bind action
