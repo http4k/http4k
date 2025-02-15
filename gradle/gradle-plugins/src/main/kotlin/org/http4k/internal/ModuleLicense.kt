f2fe72a361d6971b14c1e9a53f130716e2487915
David Denton <denton.david@gmail.com>
2025-02-15 15:00:21 +0000

    Add license files to the source and binary JAR files

--- gradle/gradle-plugins/src/main/kotlin/org/http4k/internal/ModuleLicense.kt
+++ gradle/gradle-plugins/src/main/kotlin/org/http4k/internal/ModuleLicense.kt
@@ -1,6 +1,38 @@
 package org.http4k.internal
 
-enum class ModuleLicense(val commonName: String, val url: String) {
-    Apache2("Apache-2.0", "http://www.apache.org/licenses/LICENSE-2.0"),
-    Http4kCommercial("http4k Commercial License", "http://http4k.org/commercial-license")
+import gradle.kotlin.dsl.accessors._39e284271b7eee8a4fbb4c6cbae1df14.jar
+import gradle.kotlin.dsl.accessors._39e284271b7eee8a4fbb4c6cbae1df14.main
+import gradle.kotlin.dsl.accessors._39e284271b7eee8a4fbb4c6cbae1df14.sourceSets
+import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.named
+
+enum class ModuleLicense(val commonName: String, val url: String, val licenseDir: String) {
+    Apache2(
+        "Apache-2.0",
+        "http://www.apache.org/licenses/LICENSE-2.0",
+        "."
+    ),
+    Http4kCommercial(
+        "http4k Commercial License",
+        "http://http4k.org/commercial-license",
+        "./pro"
+    )
+}
+
+fun Project.addLicenseToJars(license: ModuleLicense) {
+    tasks.jar {
+        from(rootProject.file(license.licenseDir).absolutePath) {
+            include("LICENSE")
+        }
+    }
+
+    tasks.named<Jar>("sourcesJar") {
+        from(sourceSets.main.get().allSource)
+        from(rootProject.file(license.licenseDir).absolutePath) {
+            include("LICENSE")
+        }
+        archiveClassifier.set("sources")
+    }
 }
