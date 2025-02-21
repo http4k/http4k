db5735b0feb229f4ffe1fa2ec378ad0ce00c214c
David Denton <denton.david@gmail.com>
2025-02-21 17:36:19 +0000

    Further tweaks to not get "null" strings into events

--- connect/google/analytics-core/src/main/kotlin/org/http4k/connect/google/analytics/filter/CollectPageView.kt
+++ connect/google/analytics-core/src/main/kotlin/org/http4k/connect/google/analytics/filter/CollectPageView.kt
@@ -6,7 +6,7 @@ import org.http4k.connect.google.analytics.model.PageView
 import org.http4k.connect.google.analytics.model.UserAgent
 import org.http4k.core.Filter
 import org.http4k.core.Request
-import org.http4k.routing.RequestWithContext
+import org.http4k.routing.RoutedMessage
 import java.util.UUID
 
 fun CollectPageView(
@@ -17,10 +17,9 @@ fun CollectPageView(
         handler(request).also {
             if (it.status.successful || it.status.informational || it.status.redirection) {
                 val host = request.header("host") ?: request.uri.host
-                val path = when (request) {
-                    is RequestWithContext -> request.xUriTemplate.toString()
-                    else -> request.uri.path
-                }
+                val path =  if (request is RoutedMessage && request.xUriTemplate != null)
+                    request.xUriTemplate.toString() else request.uri.path.trimStart('/')
+
                 val userAgent = it.header("User-Agent")?.let(UserAgent::of) ?: UserAgent.Default
                 collector(PageView(path, path, host, clientId(request), userAgent))
             }
