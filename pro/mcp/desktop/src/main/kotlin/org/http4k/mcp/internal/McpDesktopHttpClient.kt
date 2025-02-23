23d07be9bbf031caf05d4565365910d226811a0e
David Denton <denton.david@gmail.com>
2025-02-23 14:15:35 +0000

    Imports

--- pro/mcp/desktop/src/main/kotlin/org/http4k/mcp/internal/McpDesktopHttpClient.kt
+++ pro/mcp/desktop/src/main/kotlin/org/http4k/mcp/internal/McpDesktopHttpClient.kt
@@ -6,12 +6,15 @@ import org.http4k.core.Filter
 import org.http4k.core.NoOp
 import org.http4k.core.then
 import org.http4k.filter.ClientFilters
+import org.http4k.filter.ClientFilters.Cookies
+import org.http4k.filter.ClientFilters.FollowRedirects
 import org.http4k.filter.DebuggingFilters
+import org.http4k.filter.DebuggingFilters.PrintRequestAndResponse
 import org.http4k.filter.cookie.BasicCookieStorage
 import java.time.Clock
 
 fun McpDesktopHttpClient(clock: Clock, debug: Boolean) =
-    (if (debug) DebuggingFilters.PrintRequestAndResponse(System.err, true) else Filter.NoOp)
-        .then(ClientFilters.FollowRedirects())
-        .then(ClientFilters.Cookies(clock, BasicCookieStorage()))
+    (if (debug) PrintRequestAndResponse(System.err, true) else Filter.NoOp)
+        .then(FollowRedirects())
+        .then(Cookies(clock, BasicCookieStorage()))
         .then(JavaHttpClient(responseBodyMode = BodyMode.Stream))
