6810d4671c5ff553906f38bde08aacd29e752353
David Denton <denton.david@gmail.com>
2025-02-23 14:04:17 +0000

    Add redirects and cookie storage to the Mcp desktop

--- pro/mcp/desktop/src/main/kotlin/org/http4k/mcp/McpDesktop.kt
+++ pro/mcp/desktop/src/main/kotlin/org/http4k/mcp/McpDesktop.kt
@@ -9,20 +9,27 @@ import org.http4k.core.Method.GET
 import org.http4k.core.NoOp
 import org.http4k.core.Request
 import org.http4k.core.then
-import org.http4k.filter.DebuggingFilters
+import org.http4k.filter.ClientFilters.Cookies
+import org.http4k.filter.ClientFilters.FollowRedirects
+import org.http4k.filter.DebuggingFilters.PrintRequestAndResponse
+import org.http4k.filter.cookie.BasicCookieStorage
 import org.http4k.mcp.internal.pipeSseTraffic
 import org.http4k.mcp.util.DebuggingReader
 import org.http4k.mcp.util.DebuggingWriter
+import java.time.Clock
 
 object McpDesktop {
     @JvmStatic
     fun main(vararg args: String) = McpOptions(args.toList().toTypedArray())
         .use {
+            val clock = Clock.systemUTC()
             pipeSseTraffic(
                 if (debug) DebuggingReader(System.`in`.reader()) else System.`in`.reader(),
                 if (debug) DebuggingWriter(System.out.writer()) else System.out.writer(),
                 Request(GET, url),
-                (if (debug) DebuggingFilters.PrintRequestAndResponse(System.err, true) else Filter.NoOp)
+                (if (debug) PrintRequestAndResponse(System.err, true) else Filter.NoOp)
+                    .then(FollowRedirects())
+                    .then(Cookies(clock, BasicCookieStorage()))
                     .then(JavaHttpClient(responseBodyMode = Stream)),
                 Immediate
             )
