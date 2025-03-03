e6b3a4946af0217bbbeba247ea79cf0dc4e1df0f
David Denton <denton.david@gmail.com>
2025-03-03 22:06:41 +0000

    Adding pipeHttpTraffic

---
+++ pro/mcp/desktop/src/main/kotlin/org/http4k/mcp/internal/pipeHttpTraffic.kt
@@ -0,0 +1,32 @@
+package org.http4k.mcp.internal
+
+import org.http4k.client.chunkedSseSequence
+import org.http4k.core.ContentType.Companion.APPLICATION_JSON
+import org.http4k.core.ContentType.Companion.TEXT_EVENT_STREAM
+import org.http4k.core.HttpHandler
+import org.http4k.core.Method.POST
+import org.http4k.core.Request
+import org.http4k.filter.debug
+import org.http4k.lens.accept
+import org.http4k.lens.contentType
+import org.http4k.sse.SseMessage.Event
+import java.io.Reader
+import java.io.Writer
+
+/**
+ * Connect to the HTTP endpoint, constructing the request using the passed function
+ */
+fun pipeHttpTraffic(input: Reader, output: Writer, sseRequest: Request, http: HttpHandler) {
+    input.buffered().lineSequence().forEach { input ->
+        http(
+            Request(POST, sseRequest.uri)
+                .accept(TEXT_EVENT_STREAM)
+                .contentType(APPLICATION_JSON)
+                .body(input)
+        ).body.stream.chunkedSseSequence()
+            .filterIsInstance<Event>()
+            .forEach {
+                output.apply { write("${input + it.data}\n") }.flush()
+            }
+    }
+}
---
+++ pro/mcp/desktop/src/test/kotlin/org/http4k/mcp/internal/PipeHttpTraffictTest.kt
@@ -0,0 +1,49 @@
+package org.http4k.mcp.internal
+
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.equalTo
+import org.http4k.core.ContentType.Companion.TEXT_EVENT_STREAM
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.http4k.lens.contentType
+import org.http4k.sse.SseMessage.Event
+import org.http4k.util.PortBasedTest
+import org.junit.jupiter.api.Test
+import java.io.StringWriter
+
+class PipeHttpTrafficTest : PortBasedTest {
+
+    @Test
+    fun `pipes input and output to correct place`() {
+        val inputMessages = listOf("hello", "world")
+        val output = StringWriter()
+        val sentToHttp = mutableListOf<String>()
+
+        val expectedList = listOf(
+            Event("message", "data1"),
+            Event("message", "data2")
+        )
+
+        pipeHttpTraffic(
+            inputMessages.joinToString("\n").reader(),
+            output,
+            Request(POST, "http://host/sse"),
+            { req: Request ->
+                sentToHttp += req.bodyString()
+                Response(OK)
+                    .contentType(TEXT_EVENT_STREAM)
+                    .body(expectedList.joinToString("\n") { it.toMessage() })
+            }
+        )
+
+        assertThat(sentToHttp, equalTo(inputMessages))
+
+        assertThat(
+            output.toString().trimEnd().split("\n"),
+            equalTo(listOf("hellodata1", "hellodata2", "worlddata1", "worlddata2"))
+        )
+    }
+}
