dc5c969cefd2a69d17a2b065b8c0d4d2a9587578
Uberto Barbini <uberto.gama@gmail.com>
2025-03-19 17:08:52 +0900

    updating JBiDiMapping to Kondor 3.4.2

--- core/format/kondor-json/src/main/kotlin/org/http4k/format/JBiDiMappingValues.kt
+++ core/format/kondor-json/src/main/kotlin/org/http4k/format/JBiDiMappingValues.kt
@@ -1,14 +1,15 @@
 package org.http4k.format
 
-import com.ubertob.kondor.json.JBigDecimal
-import com.ubertob.kondor.json.JBigInteger
+import com.ubertob.kondor.json.JBigDecimalRepresentable
+import com.ubertob.kondor.json.JBigIntegerRepresentable
 import com.ubertob.kondor.json.JBooleanRepresentable
-import com.ubertob.kondor.json.JDouble
-import com.ubertob.kondor.json.JFloat
-import com.ubertob.kondor.json.JInt
-import com.ubertob.kondor.json.JNumRepresentable
+import com.ubertob.kondor.json.JDoubleRepresentable
+import com.ubertob.kondor.json.JFloatRepresentable
+import com.ubertob.kondor.json.JIntRepresentable
+import com.ubertob.kondor.json.JLongRepresentable
 import com.ubertob.kondor.json.JStringRepresentable
-import com.ubertob.kondor.json.JsonOutcome
+import com.ubertob.kondor.json.JsonConverter
+import com.ubertob.kondor.json.jsonnode.JsonNodeNumber
 import org.http4k.lens.BiDiMapping
 import java.math.BigDecimal
 import java.math.BigInteger
@@ -18,43 +19,50 @@ fun <OUT> BiDiMapping<String, OUT>.asJConverter() = object : JStringRepresentabl
     override val render: (OUT) -> String = ::invoke
 }
 
-inline fun <reified IN : Number, OUT : Any> BiDiMapping<IN, OUT>.asJConverter() = object : JNumRepresentable<OUT>() {
-    override val cons: (Number) -> OUT = { number ->
-        when (IN::class) {
-            Float::class -> asOut(JFloat.cons(number) as IN)
-            Double::class -> asOut(JDouble.cons(number) as IN)
-            Int::class -> asOut(JInt.cons(number) as IN)
-            BigDecimal::class -> asOut(JBigDecimal.cons(number) as IN)
-            BigInteger::class -> asOut(JBigInteger.cons(number) as IN)
-            else -> throw IllegalArgumentException("Unsupported type for IN: ${IN::class}")
+inline fun <reified IN : Number, OUT : Any> BiDiMapping<IN, OUT>.asJConverter() =
+    when (IN::class) {
+        Float::class -> object : JFloatRepresentable<OUT>() {
+            override val cons: (Float) -> OUT = { asOut(it as IN) }
+            override val render: (OUT) -> Float = { asIn(it) as Float }
         }
-    }
 
-    override val render: (OUT) -> Number = { outObj ->
-        when (IN::class) {
-            Float::class -> JFloat.render(asIn(outObj) as Float)
-            Double::class -> JDouble.render(asIn(outObj) as Double)
-            Int::class -> JInt.render(asIn(outObj) as Int)
-            BigDecimal::class -> JBigDecimal.render(asIn(outObj) as BigDecimal)
-            BigInteger::class -> JBigInteger.render(asIn(outObj) as BigInteger)
-            else -> throw IllegalArgumentException("Unsupported type for IN: ${IN::class}")
+        Double::class -> object : JDoubleRepresentable<OUT>() {
+            override val cons: (Double) -> OUT = { asOut(it as IN) }
+            override val render: (OUT) -> Double = { asIn(it) as Double }
+
         }
-    }
 
-    override fun parser(value: String): JsonOutcome<Number> = when (IN::class) {
-        Float::class -> JFloat.parser(value)
-        Double::class -> JDouble.parser(value)
-        Int::class -> JInt.parser(value)
-        BigDecimal::class -> JBigDecimal.parser(value)
-        BigInteger::class -> JBigInteger.parser(value)
-        else -> throw IllegalArgumentException("Unsupported type for IN: ${IN::class}")
-    }
+        Int::class -> object : JIntRepresentable<OUT>() {
+            override val cons: (Int) -> OUT = { asOut(it as IN) }
+            override val render: (OUT) -> Int = { asIn(it) as Int }
 
-}
+        }
+
+        Long::class -> object : JLongRepresentable<OUT>() {
+            override val cons: (Long) -> OUT = { asOut(it as IN) }
+            override val render: (OUT) -> Long = { asIn(it) as Long }
+
+        }
+        BigDecimal::class -> object : JBigDecimalRepresentable<OUT>() {
+            override val cons: (BigDecimal) -> OUT = { asOut(it as IN) }
+            override val render: (OUT) -> BigDecimal = { asIn(it) as BigDecimal }
+
+        }
+
+        BigInteger::class -> object : JBigIntegerRepresentable<OUT>() {
+            override val cons: (BigInteger) -> OUT = { asOut(it as IN) }
+            override val render: (OUT) -> BigInteger = { asIn(it) as BigInteger }
+
+        }
+
+        else -> {
+            throw IllegalArgumentException("Unsupported type for IN: ${IN::class}")
+        }
+    } as JsonConverter<OUT, JsonNodeNumber>
 
 fun <OUT : Any> BiDiMapping<Boolean, OUT>.asJConverter() = object : JBooleanRepresentable<OUT>() {
     override val cons: (Boolean) -> OUT = { asOut(it) }
     override val render: (OUT) -> Boolean = { asIn(it) }
 }
 
-internal fun Number.toBigDecimal() = if (this is BigDecimal) this else toString().toBigDecimal()
+internal fun Number.toBigDecimal() = this as? BigDecimal ?: toString().toBigDecimal()
