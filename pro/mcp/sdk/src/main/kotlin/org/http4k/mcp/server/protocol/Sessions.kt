b28b3f5d8e3f155cc3cefc704576e676966e5b33
David Denton <denton.david@gmail.com>
2025-03-29 21:51:49 +0000

    Refactoring to client contexts

--- pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/server/protocol/Sessions.kt
+++ pro/mcp/sdk/src/main/kotlin/org/http4k/mcp/server/protocol/Sessions.kt
@@ -9,13 +9,13 @@ import org.http4k.mcp.util.McpNodeType
  * transport to session, and the sending of messages to the client.
  */
 interface Sessions<Transport> {
+    fun retrieveSession(connectRequest: Request): SessionState
+
     fun respond(transport: Transport, session: Session, message: McpNodeType): Result4k<McpNodeType, McpNodeType>
     fun transportFor(context: ClientRequestContext): Transport
     fun onClose(context: ClientRequestContext, fn: () -> Unit)
 
     fun request(context: ClientRequestContext, message: McpNodeType)
-
-    fun retrieveSession(connectRequest: Request): SessionState
 
     fun assign(context: ClientRequestContext, transport: Transport, connectRequest: Request)
     fun end(context: ClientRequestContext)
