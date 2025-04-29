4a42d874c596f8f245f9b3c374d424bc179221d4
David Denton <denton.david@gmail.com>
2025-04-29 20:06:58 +0100

    adding lookup from protected resource

--- core/security/oauth/src/main/kotlin/org/http4k/security/oauth/client/AutoDiscoveryOAuthToken.kt
+++ core/security/oauth/src/main/kotlin/org/http4k/security/oauth/client/AutoDiscoveryOAuthToken.kt
@@ -1,53 +1,34 @@
 package org.http4k.security.oauth.client
 
-import dev.forkhandles.result4k.Failure
-import dev.forkhandles.result4k.Success
+import dev.forkhandles.result4k.onFailure
 import org.http4k.core.Credentials
 import org.http4k.core.Filter
 import org.http4k.core.HttpHandler
-import org.http4k.core.Uri
 import org.http4k.filter.ClientFilters
 import org.http4k.security.OAuthProviderConfig
 import java.time.Clock
 import java.time.Duration
 
-
 fun ClientFilters.AutoDiscoveryOAuthToken(
-    authServerUri: Uri,
+    authServerDiscovery: AuthServerDiscovery,
     credentials: Credentials,
     backend: HttpHandler,
     clock: Clock = Clock.systemUTC(),
     scopes: List<String> = emptyList(),
     oAuthFlowFilter: Filter = ClientFilters.OAuthClientCredentials(credentials, scopes),
     gracePeriod: Duration = Duration.ofSeconds(10),
-    authServerDiscovery: AuthServerDiscovery = AuthServerDiscovery.fromKnownAuthServer(authServerUri)
-): Filter = when (val discoveryResult = authServerDiscovery(backend)) {
-    is Success -> {
-        val (authServerUri, metadata) = discoveryResult.value
-        val config = OAuthProviderConfig(
-            authBase = authServerUri,
-            authPath = metadata.authorization_endpoint.path,
-            tokenPath = metadata.token_endpoint.path,
-            credentials = credentials
-        )
+): Filter {
+    val (authServerUri, metadata) = authServerDiscovery(backend)
+        .onFailure { throw it.reason }
+    val config = OAuthProviderConfig(
+        authBase = authServerUri,
+        authPath = metadata.authorization_endpoint.path,
+        tokenPath = metadata.token_endpoint.path,
+        credentials = credentials
+    )
 
-        ClientFilters.RefreshingOAuthToken(
-            config = config,
-            backend = backend,
-            oAuthFlowFilter = oAuthFlowFilter,
-            gracePeriod = gracePeriod,
-            clock = clock,
-            scopes = scopes
-        )
-    }
-
-    is Failure -> ClientFilters.RefreshingOAuthToken(
-        config = OAuthProviderConfig(
-            authBase = authServerUri,
-            authPath = "/authorize",
-            tokenPath = "/token",
-            credentials = credentials
-        ),
+    return ClientFilters.RefreshingOAuthToken(
+        config = config,
         backend = backend,
         oAuthFlowFilter = oAuthFlowFilter,
         gracePeriod = gracePeriod,
