813c16a144278d1454f0211f42cc48767a7f2a5a
David Denton <denton.david@gmail.com>
2025-04-29 21:40:25 +0100

    Move server endpoints

--- core/security/oauth/src/main/kotlin/org/http4k/security/oauth/server/OAuthJwks.kt
+++ core/security/oauth/src/main/kotlin/org/http4k/security/oauth/server/OAuthJwks.kt
@@ -1,10 +1,11 @@
-package org.http4k.security.oauth.metadata
+package org.http4k.security.oauth.server
 
 import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.routing.bind
 import org.http4k.security.oauth.format.OAuthMoshi.json
+import org.http4k.security.oauth.metadata.JsonWebKeySet
 
 fun OAuthJwks(keySet: JsonWebKeySet) =
     ".well-known/jwks.json" bind GET to { Response(OK).json(keySet) }
