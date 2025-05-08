005a65feff844072d30da2cb5a12323f5c396e25
David Denton <denton.david@gmail.com>
2025-05-08 18:46:08 +0100

    Extract out ConfigurableMcpJson

---
+++ pro/mcp/core/src/main/kotlin/org/http4k/mcp/util/ConfigurableMcpJson.kt
@@ -0,0 +1,110 @@
+package org.http4k.mcp.util
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import dev.forkhandles.result4k.Failure
+import dev.forkhandles.result4k.Result4k
+import dev.forkhandles.result4k.Success
+import dev.forkhandles.result4k.get
+import org.http4k.connect.model.Base64Blob
+import org.http4k.connect.withAiMappings
+import org.http4k.contract.jsonschema.JsonSchemaCollapser
+import org.http4k.contract.jsonschema.v3.AutoJsonToJsonSchema
+import org.http4k.core.Body
+import org.http4k.core.Response
+import org.http4k.core.Status
+import org.http4k.core.with
+import org.http4k.format.AutoMappingConfiguration
+import org.http4k.format.ConfigurableMoshi
+import org.http4k.format.ListAdapter
+import org.http4k.format.MapAdapter
+import org.http4k.format.MoshiNode
+import org.http4k.format.MoshiNodeAdapter
+import org.http4k.format.MoshiNull
+import org.http4k.format.SetAdapter
+import org.http4k.format.ThrowableAdapter
+import org.http4k.format.asConfigurable
+import org.http4k.format.value
+import org.http4k.format.withStandardMappings
+import org.http4k.lens.LensGet
+import org.http4k.lens.LensSet
+import org.http4k.lens.ParamMeta
+import org.http4k.mcp.model.McpEntity
+import org.http4k.mcp.model.McpMessageId
+import org.http4k.mcp.model.Priority
+import org.http4k.mcp.model.PromptName
+import org.http4k.mcp.model.ResourceName
+import org.http4k.mcp.model.ResourceUriTemplate
+import org.http4k.mcp.model.Size
+import org.http4k.mcp.model.Tool
+import org.http4k.mcp.model.ToolArgLensSpec
+import org.http4k.mcp.protocol.McpRpcMethod
+import org.http4k.mcp.protocol.ProtocolVersion
+import org.http4k.mcp.protocol.SessionId
+import org.http4k.mcp.protocol.Version
+import org.http4k.mcp.util.McpJson.json
+import se.ansman.kotshi.KotshiJsonAdapterFactory
+
+typealias McpNodeType = MoshiNode
+
+abstract class ConfigurableMcpJson : ConfigurableMoshi(
+    Moshi.Builder()
+        .add(McpJsonFactory)
+        .addLast(ThrowableAdapter)
+        .addLast(ListAdapter)
+        .addLast(SetAdapter)
+        .addLast(MapAdapter)
+        .addLast(MoshiNodeAdapter)
+        .addLast(ErrorMessageAdapter)
+        .asConfigurable()
+        .withMcpMappings()
+        .done()
+) {
+    /**
+     * Auto-marshalled lens for a tool argument. You will need Kotlin reflection on the classpath for this to work.
+     */
+    inline fun <reified T : Any> Tool.Arg.auto(example: T): ToolArgLensSpec<T> {
+        val autoJsonToJsonSchema = AutoJsonToJsonSchema(this@ConfigurableMcpJson)
+        val jsonSchemaCollapser = JsonSchemaCollapser(this@ConfigurableMcpJson)
+
+        return ToolArgLensSpec(
+            ParamMeta.ObjectParam,
+            LensGet { name, target -> listOf(convert<Any, T>(target.args[name]!!)) },
+            LensSet { name, values, target ->
+                values.fold(target) { acc, next -> target.copy(args = acc.args + (name to asJsonObject(next))) }
+            },
+            { jsonSchemaCollapser.collapseToNode(autoJsonToJsonSchema.toSchema(example)) }
+        )
+    }
+}
+
+@KotshiJsonAdapterFactory
+object McpJsonFactory : JsonAdapter.Factory by KotshiMcpJsonFactory
+
+fun <T> AutoMappingConfiguration<T>.withMcpMappings() = apply {
+    withStandardMappings()
+    withAiMappings()
+    value(Base64Blob)
+    value(McpEntity)
+    value(McpRpcMethod)
+    value(McpMessageId)
+    value(Priority)
+    value(ProtocolVersion)
+    value(PromptName)
+    value(ResourceName)
+    value(ResourceUriTemplate)
+    value(SessionId)
+    value(Size)
+    value(Version)
+}
+
+fun Result4k<McpNodeType, McpNodeType>.asHttp() =
+    when (val response = this) {
+        is Success<McpNodeType> -> response.get().asHttp(org.http4k.core.Status.ACCEPTED)
+        is Failure<McpNodeType> -> response.get().asHttp(org.http4k.core.Status.BAD_REQUEST)
+    }
+
+private fun McpNodeType.asHttp(status: Status) = when (this) {
+    is MoshiNull -> Response(status)
+    else -> Response(status).with(Body.json().toLens() of this)
+}
