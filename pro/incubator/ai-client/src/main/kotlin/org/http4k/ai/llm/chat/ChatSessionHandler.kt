3b99319c924428483ec7a75d2a2f6e1d08c8ed03
David Denton <denton.david@gmail.com>
2025-06-07 18:36:52 +0100

    Remove unused states

--- pro/incubator/ai-client/src/main/kotlin/org/http4k/ai/llm/chat/ChatSessionHandler.kt
+++ pro/incubator/ai-client/src/main/kotlin/org/http4k/ai/llm/chat/ChatSessionHandler.kt
@@ -10,24 +10,18 @@ class ChatSessionHandler(private val stateMachine: ChatSessionStateMachine) {
     fun onUserMessage(message: String) = stateMachine(UserInput(message))
 
     fun onToolApprove(toolName: ToolName) = when (val currentState = stateMachine.currentState()) {
-        is AwaitingApproval -> {
-            val toolRequest = currentState.pendingTools.find { it.name == toolName }
-            when(toolRequest) {
-                null -> currentState
-                else -> stateMachine(ToolApproved(toolRequest))
-            }
-        }
+        is AwaitingApproval -> currentState.pendingTools
+            .find { it.name == toolName }
+            ?.let { stateMachine(ToolApproved(it)) } ?: currentState
+
         else -> currentState
     }
 
     fun onToolReject(toolName: ToolName) = when (val currentState = stateMachine.currentState()) {
-        is AwaitingApproval ->  {
-            val toolRequest = currentState.pendingTools.find { it.name == toolName }
-            when(toolRequest) {
-                null -> currentState
-                else -> stateMachine(ToolRejected(toolRequest))
-            }
-        }
+        is AwaitingApproval -> currentState.pendingTools
+            .find { it.name == toolName }
+            ?.let { stateMachine(ToolRejected(it)) } ?: currentState
+
         else -> currentState
     }
 }
