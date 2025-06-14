4139efefa052b00bd5b740b35d78da8f3093c29a
David Denton <denton.david@gmail.com>
2025-06-14 14:03:36 +0100

    Added Annotation source

--- pro/ai/mcp/core/src/main/kotlin/org/http4k/ai/mcp/model/Annotations.kt
+++ pro/ai/mcp/core/src/main/kotlin/org/http4k/ai/mcp/model/Annotations.kt
@@ -2,6 +2,7 @@ package org.http4k.ai.mcp.model
 
 import org.http4k.ai.model.Role
 import se.ansman.kotshi.JsonSerializable
+import java.time.Instant
 
 @JsonSerializable
-data class Annotations(val audience: List<Role>, val priority: Priority)
+data class Annotations(val audience: List<Role>? = null, val priority: Priority? = null, val lastModified: Instant? = null)
