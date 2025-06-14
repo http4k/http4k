a8fff6d28268c93d2dfcb6bd9094c613d83c6746
David Denton <denton.david@gmail.com>
2025-06-14 12:55:18 +0100

    Helidon stop test changes mean one test is flakey in docker

--- pro/incubator/ai-client/src/test/kotlin/chatzilla/main2.kt
+++ pro/incubator/ai-client/src/test/kotlin/chatzilla/main2.kt
@@ -1,5 +1,6 @@
 package chatzilla
 
+import org.http4k.ai.llm.util.LLMJson
 import org.http4k.ai.mcp.ElicitationResponse
 import org.http4k.ai.mcp.ToolRequest
 import org.http4k.ai.mcp.client.http.HttpStreamingMcpClient
@@ -8,6 +9,8 @@ import org.http4k.ai.mcp.model.McpEntity
 import org.http4k.ai.mcp.model.Meta
 import org.http4k.ai.mcp.protocol.Version
 import org.http4k.client.JavaHttpClient
+import org.http4k.client.ReconnectionMode.Disconnect
+import org.http4k.core.BodyMode.Stream
 import org.http4k.core.Uri
 import org.http4k.filter.debug
 import org.http4k.lens.with
@@ -20,14 +23,14 @@ fun main() {
         McpEntity.of("mcp"),
         Version.of("1.0"),
         Uri.of("http://localhost:${mcp.port()}/mcp"),
-        JavaHttpClient()
+        JavaHttpClient(responseBodyMode = Stream).debug(),
+        notificationSseReconnectionMode = Disconnect
     )
     client.start()
 
     Thread.sleep(1000)
     client.elicitations().onElicitation {
-        System.err.println("GOT ELICITATION: $it")
-        ElicitationResponse(ElicitationAction.cancel)
+        ElicitationResponse(ElicitationAction.accept, LLMJson.obj("foo" to LLMJson.string("bar")), Meta(it.progressToken))
     }
 
     println(client.tools().call(getFullNameTool.name, ToolRequest(meta = Meta("123")).with(name of "John")))
