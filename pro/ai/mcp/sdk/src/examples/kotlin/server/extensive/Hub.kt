2d86ed7b0272b5a4d98b6e4d4ca17de146a76e97
David Denton <denton.david@gmail.com>
2025-06-20 09:37:07 +0100

    Remove example

--- pro/ai/mcp/sdk/src/examples/kotlin/server/extensive/Hub.kt
+++
@@ -1,8 +0,0 @@
-package server.extensive
-
-import dev.forkhandles.result4k.Result4k
-
-class Hub(private val fooBar: FooBarRepo, private val service: RemoteService) {
-    fun doSomethingToRepo(): Result4k<List<String>, Exception> = fooBar.doSomething()
-    fun doSomethingWithService(): Result4k<Int, Exception> = service.doSomething()
-}
