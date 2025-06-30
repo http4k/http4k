56ba27dfefbdb70ed1d66075a0f5f1b82073222a
David Denton <denton.david@gmail.com>
2025-06-30 18:22:44 +0100

    Replace generated dataframe extensions with inlined

--- core/format/dataframe/src/test/kotlin/org/http4k/format/dataframe/Repository$Extensions.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("UNCHECKED_CAST", "USELESS_CAST")
-
-package org.http4k.format.dataframe
-
-import org.jetbrains.kotlinx.dataframe.DataRow
-
-val DataRow<Repository>.fullName: String @JvmName("Repository_fullName") get() = this["full_name"] as String
-val DataRow<Repository>.htmlUrl: java.net.URL @JvmName("Repository_htmlUrl") get() = this["html_url"] as java.net.URL
-val DataRow<Repository>.stargazersCount: Int @JvmName("Repository_stargazersCount") get() = this["stargazers_count"] as Int
