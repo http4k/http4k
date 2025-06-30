f050f377abc975334e847d8d422fc75de902b025
David Denton <denton.david@gmail.com>
2025-06-30 18:19:45 +0100

    Replace generated dataframe extensions with inlined

--- core/format/dataframe/src/test/kotlin/org/http4k/format/dataframe/Repository.Generated.kt
+++ core/format/dataframe/src/test/kotlin/org/http4k/format/dataframe/Repository.Generated.kt
@@ -1,38 +1,18 @@
 package org.http4k.format.dataframe
 
-import org.jetbrains.kotlinx.dataframe.ColumnsScope
-import org.jetbrains.kotlinx.dataframe.DataColumn
-import org.jetbrains.kotlinx.dataframe.DataFrame
-import org.jetbrains.kotlinx.dataframe.DataRow
-import org.jetbrains.kotlinx.dataframe.columns.ColumnGroup
 import org.jetbrains.kotlinx.dataframe.annotations.ColumnName
 import org.jetbrains.kotlinx.dataframe.annotations.DataSchema
-import org.jetbrains.kotlinx.dataframe.api.cast
-import org.jetbrains.kotlinx.dataframe.io.readCsv
 
 @DataSchema
 interface Repository {
     @ColumnName("full_name")
     val fullName: String
+
     @ColumnName("html_url")
     val htmlUrl: java.net.URL
+
     @ColumnName("stargazers_count")
     val stargazersCount: Int
     val topics: String
     val watchers: Int
-    public companion object {
-      public const val defaultPath: kotlin.String = "src/test/resources/repositories.csv"
-
-      public val defaultDelimiter: kotlin.Char = ','
-
-      public fun readCsv(
-        path: kotlin.String = defaultPath,
-        delimiter: kotlin.Char = defaultDelimiter,
-        verify: kotlin.Boolean? = null,
-      ): org.jetbrains.kotlinx.dataframe.DataFrame<Repository> {
-        val df = DataFrame.readCsv(path, delimiter = delimiter)
-        return if (verify != null) df.cast(verify = verify) else df.cast()
-      }
-    }
-
 }
