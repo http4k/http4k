d0e67f60bdbb92517ba7e9e9b4b54b3f005bce7c
David Denton <denton.david@gmail.com>
2025-07-27 17:59:47 +0100

    Disable client generation for openapi while get try to get publishing working again

--- core/api/openapi/build.gradle.kts
+++ core/api/openapi/build.gradle.kts
@@ -1,5 +1,4 @@
 import org.http4k.internal.ModuleLicense.Apache2
-import org.jetbrains.dokka.gradle.DokkaTaskPartial
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 description = "http4k typesafe HTTP contracts and OpenApi support"
@@ -8,7 +7,9 @@ val license by project.extra { Apache2 }
 
 plugins {
     id("org.http4k.community")
-    id("org.openapi.generator")
+    if (false) {
+        id("org.openapi.generator")
+    }
 }
 
 buildscript {
@@ -42,62 +43,49 @@ dependencies {
 }
 
 tasks {
-    register<GenerateTask>("generateOpenApi3AutoClient") {
-        generatorName = "kotlin"
-        outputDir = "./build"
-        validateSpec = false
-        inputSpec =
-            "$projectDir/src/test/resources/org/http4k/contract/openapi/v3/OpenApi3AutoTest.renders as expected.approved".toString()
-        inputs.file(inputSpec)
+    if (false) {
+        register<GenerateTask>("generateOpenApi3AutoClient") {
+            generatorName = "kotlin"
+            outputDir = "./build"
+            validateSpec = false
+            inputSpec =
+                "$projectDir/src/test/resources/org/http4k/contract/openapi/v3/OpenApi3AutoTest.renders as expected.approved".toString()
+            inputs.file(inputSpec)
+        }
+
+        register<GenerateTask>("generateOpenApi3Client") {
+            generatorName = "kotlin"
+            outputDir = "./build"
+            validateSpec = false
+            inputSpec =
+                "$projectDir/src/test/resources/org/http4k/contract/openapi/v3/OpenApi3Test.renders as expected.approved".toString()
+            inputs.file(inputSpec)
+            mustRunAfter("generateOpenApi3AutoClient")
+        }
+
+        register<GenerateTask>("generateOpenApi2Client") {
+            generatorName = "kotlin"
+            outputDir = "./build"
+            validateSpec = false
+            inputSpec =
+                "$projectDir/src/test/resources/org/http4k/contract/openapi/v2/OpenApi2Test.renders as expected.approved".toString()
+            inputs.file(inputSpec)
+            mustRunAfter("generateOpenApi3Client")
+        }
+
+        listOf(
+            "compileKotlin",
+            "compileTestKotlin",
+            "processResources",
+            "processTestResources",
+            "dokkaHtmlPartial",
+        ).forEach {
+            named(it).get().dependsOn(
+                named("generateOpenApi3AutoClient").get(),
+                named("generateOpenApi3Client").get(),
+                named("generateOpenApi2Client").get()
+            )
+        }
     }
-
-    register<GenerateTask>("generateOpenApi3Client") {
-        generatorName = "kotlin"
-        outputDir = "./build"
-        validateSpec = false
-        inputSpec =
-            "$projectDir/src/test/resources/org/http4k/contract/openapi/v3/OpenApi3Test.renders as expected.approved".toString()
-        inputs.file(inputSpec)
-        mustRunAfter("generateOpenApi3AutoClient")
-    }
-
-    register<GenerateTask>("generateOpenApi2Client") {
-        generatorName = "kotlin"
-        outputDir = "./build"
-        validateSpec = false
-        inputSpec =
-            "$projectDir/src/test/resources/org/http4k/contract/openapi/v2/OpenApi2Test.renders as expected.approved".toString()
-        inputs.file(inputSpec)
-        mustRunAfter("generateOpenApi3Client")
-    }
-
-    named("checkLicense") {
-        dependsOn("generateOpenApi2Client")
-        dependsOn("generateOpenApi3Client")
-        dependsOn("generateOpenApi3AutoClient")
-    }
-
-    named("compileKotlin").get().dependsOn(
-        named("generateOpenApi3AutoClient").get(),
-        named("generateOpenApi3Client").get(),
-        named("generateOpenApi2Client").get()
-    )
-    named("processResources").get().dependsOn(
-        named("generateOpenApi3AutoClient").get(),
-        named("generateOpenApi3Client").get(),
-        named("generateOpenApi2Client").get()
-    )
-    named("processTestResources").get().dependsOn(
-        named("generateOpenApi3AutoClient").get(),
-        named("generateOpenApi3Client").get(),
-        named("generateOpenApi2Client").get()
-    )
-
-    named("dokkaHtmlPartial", DokkaTaskPartial::class) {
-        dependsOn(named("generateOpenApi2Client"))
-        dependsOn(named("generateOpenApi3Client"))
-        dependsOn(named("generateOpenApi3AutoClient"))
-    }
-
 }
 
