f670d2ec27303d26b80c3a1530eb0cb9bc3cbf1b
David Denton <denton.david@gmail.com>
2025-08-06 23:19:26 +0100

    Update refresh versions workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,8 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 plugins {
     id("org.http4k.project-metadata")
     id("org.http4k.api-docs")
     id("nl.littlerobots.version-catalog-update") version "1.0.0"
     alias(libs.plugins.versions)
-}
-
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
-    }
 }
 
 metadata {
