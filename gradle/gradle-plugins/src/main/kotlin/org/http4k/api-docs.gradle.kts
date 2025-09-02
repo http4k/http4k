f55347ee4001d583de1c0ac171855e9bc13d6a04
David Denton <denton.david@gmail.com>
2025-09-02 20:42:11 +0100

    Release 6.17.0.0

--- gradle/gradle-plugins/src/main/kotlin/org/http4k/api-docs.gradle.kts
+++ gradle/gradle-plugins/src/main/kotlin/org/http4k/api-docs.gradle.kts
@@ -1,22 +1,23 @@
 package org.http4k
 
-import org.jetbrains.dokka.base.DokkaBase
-import org.jetbrains.dokka.base.DokkaBaseConfiguration
-import org.jetbrains.dokka.gradle.AbstractDokkaTask
 import java.time.Year
 
 plugins {
     id("org.jetbrains.dokka")
 }
 
-tasks.withType<AbstractDokkaTask>().configureEach {
-    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
-        moduleVersion.set(version.toString())
-        footerMessage = "(c) ${Year.now().value} http4k"
-        homepageLink = "https://http4k.org"
-        customAssets = listOf(
-            file("${rootProject.projectDir}/gradle/gradle-plugins/src/main/resources/logo-icon.svg"),
-        )
-        customStyleSheets = listOf(file("${rootProject.projectDir}/gradle/gradle-plugins/src/main/resources/dokka.css"))
+dokka {
+    dokkaPublications.html {
+        pluginsConfiguration.html {
+            moduleVersion.set(version.toString())
+            footerMessage.set("(c) ${Year.now().value} http4k")
+            homepageLink.set("https://http4k.org")
+            customAssets.from(
+                file("${rootProject.projectDir}/gradle/gradle-plugins/src/main/resources/logo-icon.svg")
+            )
+            customStyleSheets.from(
+                file("${rootProject.projectDir}/gradle/gradle-plugins/src/main/resources/dokka.css")
+            )
+        }
     }
 }
