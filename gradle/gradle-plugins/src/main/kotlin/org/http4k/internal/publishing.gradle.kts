c839490de1b5f3be4451c9b9a08f2e00cc2f1983
Ivan Sanchez <ivan@gourame.com>
2025-09-04 20:46:18 +0100

    Set automatic maven central release to true

--- gradle/gradle-plugins/src/main/kotlin/org/http4k/internal/publishing.gradle.kts
+++ gradle/gradle-plugins/src/main/kotlin/org/http4k/internal/publishing.gradle.kts
@@ -48,7 +48,7 @@ configure<MavenPublishBaseExtension> {
             }
         }
 
-        publishToMavenCentral(automaticRelease = false)
+        publishToMavenCentral(automaticRelease = true)
 
         coordinates(
             when (license) {
