41216c03dded9b5863ada129e03298859ce4228f
Alessandro <alessandro.ciccimarra@gmail.com>
2025-09-09 22:48:33 +0100

    Handle malformed response chunk in fuel client

--- core/client/fuel/src/main/kotlin/org/http4k/client/Fuel.kt
+++ core/client/fuel/src/main/kotlin/org/http4k/client/Fuel.kt
@@ -11,6 +11,7 @@ import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.http4k.core.toParametersMap
+import java.io.IOException
 import java.net.ConnectException
 import java.net.SocketException
 import java.net.SocketTimeoutException
@@ -44,6 +45,7 @@ class Fuel(
             is UnknownHostException -> return Response(Status.UNKNOWN_HOST.toClientStatus(error.exception as UnknownHostException))
             is SocketTimeoutException -> return Response(Status.CLIENT_TIMEOUT.toClientStatus(error.exception as SocketTimeoutException))
             is SocketException -> return Response(Status.SERVICE_UNAVAILABLE.toClientStatus(error.exception as SocketException))
+            is IOException -> return Response(Status.SERVICE_UNAVAILABLE.toClientStatus(error.exception as IOException))
         }
         val headers: Parameters = response.headers.toList().fold(listOf()) { acc, next ->
             acc + next.second.fold(listOf()) { keyAcc, nextValue -> keyAcc + (next.first to nextValue) }
