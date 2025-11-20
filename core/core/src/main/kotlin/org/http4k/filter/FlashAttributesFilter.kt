d7de7805b4032984f6bd7d88db0131601e2af899
cies <cies-AT@kde-DOT.nl>
2025-11-20 11:18:11 +0100

    Fix #1438: url encode flash cookie content

--- core/core/src/main/kotlin/org/http4k/filter/FlashAttributesFilter.kt
+++ core/core/src/main/kotlin/org/http4k/filter/FlashAttributesFilter.kt
@@ -7,6 +7,9 @@ import org.http4k.core.Response
 import org.http4k.core.cookie.Cookie
 import org.http4k.core.cookie.cookie
 import org.http4k.core.cookie.cookies
+import java.net.URLDecoder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 const val FLASH_COOKIE = "flash-cookie"
 
@@ -23,12 +26,14 @@ object FlashAttributesFilter : Filter {
 }
 
 fun Request.flash(): String? = cookie(FLASH_COOKIE)?.value
+    ?.let { URLDecoder.decode(it, StandardCharsets.UTF_8) }
 fun Request.withFlash(value: String): Request =
-    cookie(Cookie(name = FLASH_COOKIE, value, maxAge = 3600, path = "/"))
+    cookie(Cookie(name = FLASH_COOKIE, URLEncoder.encode(value, StandardCharsets.UTF_8), maxAge = 3600, path = "/"))
 
 fun Response.flash(): String? = cookies().firstOrNull { it.name == FLASH_COOKIE }?.value
+    ?.let { URLDecoder.decode(it, StandardCharsets.UTF_8) }
 fun Response.withFlash(value: String): Response =
-    cookie(Cookie(name = FLASH_COOKIE, value, maxAge = 3600, path = "/"))
+    cookie(Cookie(name = FLASH_COOKIE, URLEncoder.encode(value, StandardCharsets.UTF_8), maxAge = 3600, path = "/"))
 
 fun Response.removeFlash(): Response =
     cookie(Cookie(name = FLASH_COOKIE, "", maxAge = 0, path = "/"))
