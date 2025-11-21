27890b4d807fa21a9e3e10950381b8b1cad7f927
Nat Pryce <sw@natpryce.com>
2025-11-21 08:11:51 +0000

    Ignore QUERY method in Apache4Server tests

--- core/server/apache4/src/test/kotlin/org/http4k/server/Apache4ServerTest.kt
+++ core/server/apache4/src/test/kotlin/org/http4k/server/Apache4ServerTest.kt
@@ -4,7 +4,7 @@ import org.http4k.core.Method
 import org.junit.jupiter.api.Disabled
 
 class Apache4ServerTest : ServerContract(::Apache4Server, ClientForServerTesting(),
-    Method.entries.filter { it != Method.PURGE }.toTypedArray()) {
+    Method.entries.filter { it != Method.PURGE && it != Method.QUERY }.toTypedArray()) {
 
     @Disabled("resets connection for some reason")
     override fun `can act as a simple proxy`() {
