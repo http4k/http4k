9c847359009607f42461aa702645ac74a6952b28
David Denton <daviddenton@users.noreply.github.com>
2025-12-13 22:30:48 +0000

    Add convienience methods to Toon

--- core/format/toon/src/main/kotlin/org/http4k/format/ConfigurableToon.kt
+++ core/format/toon/src/main/kotlin/org/http4k/format/ConfigurableToon.kt
@@ -6,6 +6,8 @@ import dev.toonformat.jtoon.JToon
 import org.http4k.core.Body
 import org.http4k.core.ContentType
 import org.http4k.core.ContentType.Companion.Text
+import org.http4k.core.HttpMessage
+import org.http4k.core.with
 import org.http4k.lens.BiDiBodyLensSpec
 import org.http4k.lens.ContentNegotiation
 import org.http4k.lens.ContentNegotiation.Companion.None
@@ -45,4 +47,15 @@ open class ConfigurableToon(
         Body.string(contentType, description, contentNegotiation).map({ asA(it, T::class) }, {
             asFormatString(it)
         })
+
+    /**
+     * Convenience function to write the object as Toon to the message body and set the content type.
+     */
+    inline fun <reified T : Any, R : HttpMessage> R.toon(t: T): R = with(Body.auto<T>().toLens() of t)
+
+    /**
+     * Convenience function to read an object as Toon from the message body.
+     */
+    inline fun <reified T : Any> HttpMessage.toon(): T = Body.auto<T>().toLens()(this)
+
 }
