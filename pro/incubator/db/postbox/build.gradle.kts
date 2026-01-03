9a9f9faa1adb28c02454f3c3a1c1f8990c92c069
Ivan Sanchez <ivan@gourame.com>
2026-01-03 10:33:17 +0000

    Extract postbox from core

---
+++ pro/incubator/db/postbox/build.gradle.kts
@@ -0,0 +1,33 @@
+
+
+description = "http4k postbox"
+
+plugins {
+    id("org.http4k.pro")
+}
+
+dependencies {
+    api(platform(libs.forkhandles.bom))
+    compileOnly(platform(libs.junit.bom))
+    compileOnly(libs.junit.jupiter.api)
+    api(project(":http4k-core"))
+    api(project(":http4k-incubator-db-core"))
+    testImplementation(testFixtures(project(":http4k-core")))
+
+    testImplementation(project(":http4k-testing-strikt"))
+    api(libs.result4k)
+    api(libs.values4k)
+    api(libs.time4k)
+
+    testImplementation(libs.hsqldb)
+
+    testImplementation(libs.postgresql)
+
+    testImplementation(libs.mysql.connector.java)
+
+    api(libs.hikaricp)
+    api(platform(libs.exposed.bom))
+    api(libs.exposed.core)
+    api(libs.exposed.jdbc)
+    api(libs.exposed.java.time)
+}
