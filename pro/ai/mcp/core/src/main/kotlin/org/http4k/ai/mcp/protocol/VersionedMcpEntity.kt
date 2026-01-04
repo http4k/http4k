99a95b5de13c97cda46ff0fe65159e771a3e8d79
David Denton <daviddenton@users.noreply.github.com>
2026-01-04 20:14:10 +0000

    Added icons

--- pro/ai/mcp/core/src/main/kotlin/org/http4k/ai/mcp/protocol/VersionedMcpEntity.kt
+++ pro/ai/mcp/core/src/main/kotlin/org/http4k/ai/mcp/protocol/VersionedMcpEntity.kt
@@ -1,17 +1,36 @@
 package org.http4k.ai.mcp.protocol
 
+import org.http4k.ai.mcp.model.Icon
 import org.http4k.ai.mcp.model.McpEntity
+import org.http4k.core.Uri
 import se.ansman.kotshi.JsonSerializable
 
 /**
  * A description of an entity taking part in the MCP protocol - can be a client or a server.
  */
 @JsonSerializable
-data class VersionedMcpEntity(val name: McpEntity, val version: Version, val title: String? = null) {
-    constructor(name: String, version: String, title: String? = null) : this(
+data class VersionedMcpEntity(
+    val name: McpEntity,
+    val version: Version,
+    val title: String? = null,
+    val description: String? = null,
+    val websiteUrl: Uri? = null,
+    val icons: List<Icon>? = null
+) {
+    constructor(
+        name: String,
+        version: String,
+        title: String? = null,
+        description: String? = null,
+        websiteUrl: Uri? = null,
+        icons: List<Icon>? = null
+    ) : this(
         McpEntity.of(name),
         Version.of(version),
-        title
+        title,
+        description,
+        websiteUrl,
+        icons
     )
 }
 
