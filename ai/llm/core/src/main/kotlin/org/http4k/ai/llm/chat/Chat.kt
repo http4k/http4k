31cebe9f31c024d42a8e044d51fa73ff6ce8ff67
David Denton <daviddenton@users.noreply.github.com>
2026-01-09 22:17:11 +0000

    Tidy

--- ai/llm/core/src/main/kotlin/org/http4k/ai/llm/chat/Chat.kt
+++ ai/llm/core/src/main/kotlin/org/http4k/ai/llm/chat/Chat.kt
@@ -1,9 +1,6 @@
 package org.http4k.ai.llm.chat
 
-import dev.forkhandles.result4k.map
 import org.http4k.ai.llm.LLMResult
-import org.http4k.ai.llm.model.Message
-import java.io.PrintStream
 
 fun interface Chat {
     operator fun invoke(request: ChatRequest): LLMResult<ChatResponse>
