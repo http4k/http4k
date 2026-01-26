d84bc59ee691de98604df35b88c29818d475c253
David Denton <daviddenton@users.noreply.github.com>
2026-01-26 21:14:37 +0000

    Remove filter

--- pro/ai/mcp/conformance/src/main/kotlin/org/http4k/ai/mcp/conformance/server/McpConformanceServer.kt
+++ pro/ai/mcp/conformance/src/main/kotlin/org/http4k/ai/mcp/conformance/server/McpConformanceServer.kt
@@ -9,6 +9,16 @@ import org.http4k.ai.mcp.protocol.ServerMetaData
 import org.http4k.ai.mcp.protocol.ServerProtocolCapability
 import org.http4k.ai.mcp.protocol.Version
 import org.http4k.ai.mcp.server.security.NoMcpSecurity
+import org.http4k.core.Method.DELETE
+import org.http4k.core.Method.GET
+import org.http4k.core.Method.POST
+import org.http4k.core.PolyHandler
+import org.http4k.core.then
+import org.http4k.filter.AnyOf
+import org.http4k.filter.CorsAndRebindProtection
+import org.http4k.filter.CorsPolicy
+import org.http4k.filter.OriginPolicy
+import org.http4k.filter.ServerFilters
 import org.http4k.filter.debugMcp
 import org.http4k.routing.mcpHttpStreaming
 import org.http4k.server.JettyLoom
@@ -17,17 +27,28 @@ import org.http4k.server.asServer
 /**
  * Server which implements the MCP Conformance test suite using the http4k MCP SDK
  */
-fun McpConformanceServer() = mcpHttpStreaming(
-    ServerMetaData(
-        McpEntity.of("http4k MCP conformance"), Version.of("0.1.0"),
-        *ServerProtocolCapability.entries.toTypedArray()
-    ),
-    NoMcpSecurity,
-    ConformanceTools(),
-    ConformanceResources(),
-    CondormancePrompts(),
-    ConformanceMisc()
-)
+fun McpConformanceServer(): PolyHandler {
+    val next = mcpHttpStreaming(
+        ServerMetaData(
+            McpEntity.of("http4k MCP conformance"), Version.of("0.1.0"),
+            *ServerProtocolCapability.entries.toTypedArray()
+        ),
+        NoMcpSecurity,
+        ConformanceTools(),
+        ConformanceResources(),
+        CondormancePrompts(),
+        ConformanceMisc()
+    )
+
+    val corsPolicy = CorsPolicy(
+        OriginPolicy.AnyOf("http://localhost:4001"),
+        listOf("allowed-header"), listOf(GET, POST, DELETE)
+    )
+
+    val corsAndRebindProtection = ServerFilters.CorsAndRebindProtection(corsPolicy)
+
+    return if (false) corsAndRebindProtection.then(next) else next
+}
 
 fun main() {
     McpConformanceServer().debugMcp().asServer(JettyLoom(4001)).start()
