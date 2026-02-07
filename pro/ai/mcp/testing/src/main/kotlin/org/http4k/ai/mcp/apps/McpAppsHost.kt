99b713d365f83c369716e02ca36a7751a1d188dd
David Denton <daviddenton@users.noreply.github.com>
2026-02-07 16:23:37 +0000

    Adding example McpApp

--- pro/ai/mcp/testing/src/main/kotlin/org/http4k/ai/mcp/apps/McpAppsHost.kt
+++ pro/ai/mcp/testing/src/main/kotlin/org/http4k/ai/mcp/apps/McpAppsHost.kt
@@ -1,8 +1,11 @@
 package org.http4k.ai.mcp.apps
 
 import dev.forkhandles.result4k.map
+import dev.forkhandles.result4k.recover
 import dev.forkhandles.result4k.valueOrNull
+import org.http4k.ai.mcp.ResourceRequest
 import org.http4k.ai.mcp.client.http.HttpNonStreamingMcpClient
+import org.http4k.ai.mcp.model.Resource.Content.Text
 import org.http4k.ai.model.ToolName
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.ContentType.Companion.APPLICATION_JSON
@@ -14,6 +17,8 @@ import org.http4k.core.Status.Companion.OK
 import org.http4k.core.Uri
 import org.http4k.core.with
 import org.http4k.lens.Header.CONTENT_TYPE
+import org.http4k.lens.Query
+import org.http4k.lens.uri
 import org.http4k.routing.RoutingHttpHandler
 import org.http4k.routing.bind
 import org.http4k.routing.routes
@@ -27,7 +32,7 @@ data class ToolOption(
 )
 
 fun McpAppsHost(servers: List<Uri>, http: HttpHandler = JavaHttpClient()): RoutingHttpHandler {
-    val serverClients = servers.map { it to HttpNonStreamingMcpClient(it, http) }
+    val serverClients = servers.associateWith { HttpNonStreamingMcpClient(it, http) }
 
     val tools = serverClients
         .mapNotNull { (serverUri, client) ->
@@ -38,28 +43,37 @@ fun McpAppsHost(servers: List<Uri>, http: HttpHandler = JavaHttpClient()): Routi
                             tool._meta?.ui?.resourceUri
                                 ?.let { tool.name to it }
                         }
-                        .map { ToolOption(serverUri.host, serverUri.host, it.first, it.second) }
+                        .map { ToolOption(serverUri.toString(), serverUri.host, it.first, it.second) }
                 }.valueOrNull()
         }.flatten()
 
     val renderer = HandlebarsTemplates().CachingClasspath()
 
     return routes(
-        "/" bind GET to {
-            Response(OK).body(renderer(Index(tools)))
-        },
-
         "/api/resources" bind GET to {
+            val serverUri = Query.uri().required("serverId")(it)
+            val uiResource = Query.uri().required("uri")(it)
+
             Response(OK)
-                .with(CONTENT_TYPE of APPLICATION_JSON)
-                .body("""{"html": "<!DOCTYPE html><html><body><h1>Dummy App</h1></body></html>"}""")
+                .body(
+                    when (val client = serverClients[serverUri]) {
+                        null -> "Server not found"
+                        else -> client.resources().read(ResourceRequest(uiResource))
+                            .map { it.list.filterIsInstance<Text>().joinToString("") { it.text.replace("\"", "'") } }
+                            .recover { it.toString() }
+                    })
         },
 
         "/api/tools/call" bind POST to {
+
+            println(it.bodyString())
+
             Response(OK)
                 .with(CONTENT_TYPE of APPLICATION_JSON)
                 .body("""{"content": [{"type": "text", "text": "dummy tool result"}]}""")
-        }
+        },
+
+        "/" bind GET to { Response(OK).body(renderer(Index(tools))) }
     )
 }
 
--- pro/ai/mcp/testing/src/test/kotlin/org/http4k/ai/mcp/apps/exampleMcpHost.kt
+++ pro/ai/mcp/testing/src/test/kotlin/org/http4k/ai/mcp/apps/exampleMcpHost.kt
@@ -1,30 +1,17 @@
 package org.http4k.ai.mcp.apps
 
-import org.http4k.ai.mcp.protocol.ServerMetaData
-import org.http4k.ai.mcp.server.security.NoMcpSecurity
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.Uri
-import org.http4k.filter.debug
-import org.http4k.routing.mcpHttpStreaming
-import org.http4k.server.Helidon
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
 fun main() {
-    val server = mcpHttpStreaming(
-        ServerMetaData("server", "0.0.1"),
-        NoMcpSecurity,
-        OrderFormUi.resource,
-        showOrderFormTool(),
-        submitOrderTool()
-    )
-        .asServer(Helidon(0)).start()
+    val server = McpServer(0).start()
 
     val host = McpAppsHost(
         listOf(Uri.of("http://localhost:${server.port()}/mcp")),
-        JavaHttpClient().debug()
+        JavaHttpClient()
     )
-        .debug()
         .asServer(SunHttp(9000)).start()
 
     println("MCP Apps Host running on http://localhost:${host.port()}")
