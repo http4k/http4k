3f940f647a34e66026967ede376ca9ea9682b32c
David Denton <daviddenton@users.noreply.github.com>
2026-02-07 18:33:38 +0000

    Update

--- pro/ai/mcp/testing/src/test/kotlin/org/http4k/ai/mcp/apps/runner.kt
+++ pro/ai/mcp/testing/src/test/kotlin/org/http4k/ai/mcp/apps/runner.kt
@@ -3,16 +3,15 @@ package org.http4k.ai.mcp.apps
 import org.http4k.ai.mcp.apps.server.ExampleMcpServer
 import org.http4k.client.JavaHttpClient
 import org.http4k.core.Uri
-import org.http4k.filter.debug
 import org.http4k.server.SunHttp
 import org.http4k.server.asServer
 
 fun main() {
-    val server = ExampleMcpServer(0).start()
+    val server = ExampleMcpServer(3001).start()
 
     val host = McpAppsHost(
         listOf(Uri.of("http://localhost:${server.port()}/mcp")),
-        JavaHttpClient().debug()
+        JavaHttpClient()
     )
         .asServer(SunHttp(9000)).start()
 
--- pro/ai/mcp/testing/src/test/kotlin/org/http4k/ai/mcp/apps/server/ExampleMcpServer.kt
+++ pro/ai/mcp/testing/src/test/kotlin/org/http4k/ai/mcp/apps/server/ExampleMcpServer.kt
@@ -1,16 +1,19 @@
 package org.http4k.ai.mcp.apps.server
 
+import org.http4k.ai.mcp.model.extension.McpApps
 import org.http4k.ai.mcp.protocol.ServerMetaData
+import org.http4k.ai.mcp.protocol.withExtensions
 import org.http4k.ai.mcp.server.security.NoMcpSecurity
+import org.http4k.filter.debugMcp
 import org.http4k.routing.mcpHttpStreaming
 import org.http4k.server.Helidon
 import org.http4k.server.asServer
 import org.http4k.template.HandlebarsTemplates
 
 fun ExampleMcpServer(port: Int) = mcpHttpStreaming(
-    ServerMetaData("server", "0.0.1"),
+    ServerMetaData("order server", "0.0.1").withExtensions(McpApps),
     NoMcpSecurity,
     OrderFormUi.resource(HandlebarsTemplates().CachingClasspath()),
     ShowOrderFormTool(),
     SubmitOrderTool()
-).asServer(Helidon(port))
+).debugMcp().asServer(Helidon(port))
