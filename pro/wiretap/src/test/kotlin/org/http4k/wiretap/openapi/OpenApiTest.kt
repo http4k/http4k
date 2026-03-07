acb08956f98bd51ee738dc0954f4647e2290286c
David Denton <daviddenton@users.noreply.github.com>
2026-03-07 14:17:12 +0000

    Add open api test

---
+++ pro/wiretap/src/test/kotlin/org/http4k/wiretap/openapi/OpenApiTest.kt
@@ -0,0 +1,20 @@
+package org.http4k.wiretap.openapi
+
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.testing.ApprovalTest
+import org.http4k.testing.Approver
+import org.http4k.wiretap.HttpWiretapFunctionContract
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(ApprovalTest::class)
+class OpenApiTest : HttpWiretapFunctionContract {
+
+    override val function = OpenApi()
+
+    @Test
+    fun `http returns openapi index page`(approver: Approver) {
+        approver.assertApproved(httpClient()(Request(GET, "/openapi")))
+    }
+}
