287145d26fc25deb11c2e7a34ea34e1569c658f8
Nat Pryce <sw@natpryce.com>
2026-03-15 15:10:32 +0000

    Correct variance declaration of Action type parameter

--- connect/core/client/src/main/kotlin/org/http4k/connect/Action.kt
+++ connect/core/client/src/main/kotlin/org/http4k/connect/Action.kt
@@ -3,7 +3,7 @@ package org.http4k.connect
 import org.http4k.core.Request
 import org.http4k.core.Response
 
-interface Action<R> {
+interface Action<out R> {
     fun toRequest(): Request
     fun toResult(response: Response): R
 }
