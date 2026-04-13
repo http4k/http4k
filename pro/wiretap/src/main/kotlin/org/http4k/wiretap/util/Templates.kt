4911a0b203245aa1dc2ea6f8c677560a3b0fc77e
David Denton <daviddenton@users.noreply.github.com>
2026-04-13 20:04:15 +0100

    Rename templates to use .ftl.html suffix

--- pro/wiretap/src/main/kotlin/org/http4k/wiretap/util/Templates.kt
+++ pro/wiretap/src/main/kotlin/org/http4k/wiretap/util/Templates.kt
@@ -4,13 +4,24 @@
  */
 package org.http4k.wiretap.util
 
+import freemarker.cache.ClassTemplateLoader
 import freemarker.core.HTMLOutputFormat.INSTANCE
 import freemarker.template.Configuration
 import freemarker.template.Configuration.VERSION_2_3_34
 import freemarker.template.TemplateExceptionHandler.IGNORE_HANDLER
-import org.http4k.template.FreemarkerTemplates
+import org.http4k.template.TemplateRenderer
+import org.http4k.template.ViewModel
+import java.io.StringWriter
 
-fun Templates() = FreemarkerTemplates(Configuration(VERSION_2_3_34).apply {
-    outputFormat = INSTANCE
-    templateExceptionHandler = IGNORE_HANDLER
-}).CachingClasspath()
+fun Templates(): TemplateRenderer {
+    val cfg = Configuration(VERSION_2_3_34).apply {
+        outputFormat = INSTANCE
+        templateExceptionHandler = IGNORE_HANDLER
+        templateLoader = ClassTemplateLoader(ClassLoader.getSystemClassLoader(), "")
+    }
+    return { viewModel: ViewModel ->
+        val writer = StringWriter()
+        cfg.getTemplate(viewModel.template() + ".ftl.html").process(viewModel, writer)
+        writer.toString()
+    }
+}
