9c427c9ce3c18a1fff9d53ab86b9c481179f48d1
Andrew O'Hara <aohara@camcloud.com>
2026-05-21 09:34:48 -0400

    test getting identity of expired role

--- connect/amazon/sts/fake/src/test/kotlin/org/http4k/connect/amazon/sts/FakeSTSTest.kt
+++ connect/amazon/sts/fake/src/test/kotlin/org/http4k/connect/amazon/sts/FakeSTSTest.kt
@@ -3,6 +3,7 @@ package org.http4k.connect.amazon.sts
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.aws.AwsCredentials
+import org.http4k.connect.RemoteFailure
 import org.http4k.connect.TestClock
 import org.http4k.connect.amazon.CredentialsProvider
 import org.http4k.connect.amazon.FakeAwsContract
@@ -10,7 +11,11 @@ import org.http4k.connect.amazon.core.model.ARN
 import org.http4k.connect.amazon.core.model.Credentials
 import org.http4k.connect.amazon.core.model.RoleSessionName
 import org.http4k.connect.amazon.core.model.WebIdentityToken
+import org.http4k.connect.failureValue
 import org.http4k.connect.successValue
+import org.http4k.core.Method
+import org.http4k.core.Status
+import org.http4k.core.Uri
 import org.junit.jupiter.api.Test
 import java.time.Duration
 import java.time.ZonedDateTime
@@ -66,6 +71,24 @@ class FakeSTSTest : STSContract, FakeAwsContract {
         assertThat(identity.Account.value, equalTo("123456789012"))
         assertThat(identity.UserId, equalTo("ARO123EXAMPLE123:my-role-session-name"))
         assertThat(identity.Arn.value, equalTo("arn:aws:sts::123456789012:assumed-role/my-role-name/my-role-session-name"))
+    }
+
+    @Test
+    fun `get caller identity for expired role`() {
+        val assumedRole = sts.assumeRole(
+            RoleArn = ARN.of("arn:aws:sts::123456789012:role/my-role-name"),
+            RoleSessionName = RoleSessionName.of("my-role-session-name"),
+            DurationSeconds = Duration.ofMinutes(10)
+        ).successValue()
+
+        val assumedSts = STS.Http(aws.region, assumedRole.Credentials.provider(), http, clock)
+        assumedSts.getCallerIdentity().successValue()
+
+        clock.tickBy(Duration.ofMinutes(20))
+        assertThat(
+            assumedSts.getCallerIdentity().failureValue(),
+            equalTo(RemoteFailure(Method.POST, Uri.of(""), Status.UNAUTHORIZED, ""))
+        )
     }
 }
 
