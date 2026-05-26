2ed841f3519c01658031ed0be7e649dad04238c1
Andrew O'Hara <ohara.andrew314@gmail.com>
2026-05-26 14:13:31 -0400

    Update amazon sso grant type wire values (#1548)
    Co-authored-by: Andrew O'Hara <aohara@camcloud.com>

--- connect/amazon/iamidentitycenter/client/src/main/kotlin/org/http4k/connect/amazon/iamidentitycenter/model/GrantType.kt
+++ connect/amazon/iamidentitycenter/client/src/main/kotlin/org/http4k/connect/amazon/iamidentitycenter/model/GrantType.kt
@@ -2,8 +2,10 @@ package org.http4k.connect.amazon.iamidentitycenter.model
 
 enum class GrantType(val wireValue: String) {
     AuthorizationCode("authorization_code"),
+    AuthorizationCode2("AuthorizationCode"),
     DeviceCode("urn:ietf:params:oauth:grant-type:device_code"),
-    RefreshToken("refresh_token");
+    RefreshToken("refresh_token"),
+    RefreshToken2("RefreshToken");
 
     companion object {
         fun fromWire(wireValue: String) = entries.first { it.wireValue == wireValue }
