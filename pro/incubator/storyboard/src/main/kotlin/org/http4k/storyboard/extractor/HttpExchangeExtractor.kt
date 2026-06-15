12fbe1c2d79e2933d0a3103b0ff67789718f4e64
David Denton <daviddenton@users.noreply.github.com>
2026-06-15 21:28:05 +0100

    Consolidate `HttpExchangeView` into `HttpExchangeExtractor.View` and update references

--- pro/incubator/storyboard/src/main/kotlin/org/http4k/storyboard/extractor/HttpExchangeExtractor.kt
+++ pro/incubator/storyboard/src/main/kotlin/org/http4k/storyboard/extractor/HttpExchangeExtractor.kt
@@ -11,6 +11,7 @@ import org.http4k.storyboard.StoryFrame
 import org.http4k.storyboard.StoryFrame.Level.Detail
 import org.http4k.storyboard.frame.Html
 import org.http4k.storyboard.util.StoryboardTemplates
+import org.http4k.storyboard.util.StoryboardViewModel
 
 object HttpExchangeExtractor : FrameExtractor {
     override operator fun invoke(input: EventContext): StoryFrame? {
@@ -25,7 +26,7 @@ object HttpExchangeExtractor : FrameExtractor {
         val respSize = attrs["http.response.body.size"] ?: "0"
         val path = route ?: pathOf(url)
 
-        val view = HttpExchangeView(
+        val view = View(
             kind = kind,
             method = method,
             url = url,
@@ -56,4 +57,16 @@ object HttpExchangeExtractor : FrameExtractor {
         status.startsWith("4") -> "s4xx"
         else -> ""
     }
+
+    data class View(
+        val kind: String,
+        val method: String,
+        val url: String,
+        val path: String,
+        val status: String,
+        val statusClass: String,
+        val durationMs: String,
+        val requestSize: String,
+        val responseSize: String
+    ) : StoryboardViewModel
 }
