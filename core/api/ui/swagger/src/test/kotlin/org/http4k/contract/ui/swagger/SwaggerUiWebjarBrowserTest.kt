dedaffa8b8bec82214f22e139c53a59e2f0ef94e
Andrew O'Hara <ohara.andrew314@gmail.com>
2026-07-09 15:05:07 -0400

    Disable flakey SwaggerUiWebjarBrowserTest

--- core/api/ui/swagger/src/test/kotlin/org/http4k/contract/ui/swagger/SwaggerUiWebjarBrowserTest.kt
+++ core/api/ui/swagger/src/test/kotlin/org/http4k/contract/ui/swagger/SwaggerUiWebjarBrowserTest.kt
@@ -1,5 +1,7 @@
 package org.http4k.contract.ui.swagger
 
 import org.http4k.contract.ui.SwaggerUiBrowserContract
+import org.junit.jupiter.api.Disabled
 
+@Disabled("flakey")
 class SwaggerUiWebjarBrowserTest : SwaggerUiBrowserContract(::swaggerUiWebjar)
